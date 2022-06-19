982971a0b69172551f75147726303591bba14cd5
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-06-19 08:01:21 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-applicationinsights_1.0.0-beta.5
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-04-01/examples-java/WorkbookGet1.java
@@ -0,0 +1,19 @@
+import com.azure.core.util.Context;
+
+/** Samples for Workbooks GetByResourceGroup. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-04-01/examples/WorkbookGet1.json
+     */
+    /**
+     * Sample code: WorkbookGet1.
+     *
+     * @param manager Entry point to ApplicationInsightsManager.
+     */
+    public static void workbookGet1(com.azure.resourcemanager.applicationinsights.ApplicationInsightsManager manager) {
+        manager
+            .workbooks()
+            .getByResourceGroupWithResponse(
+                "my-resource-group", "deadb33f-5e0d-4064-8ebb-1a4ed0313eb2", null, Context.NONE);
+    }
+}
---
+++ specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-04-01/examples-java/WorkbookRevisionsList.java
@@ -0,0 +1,17 @@
+import com.azure.core.util.Context;
+
+/** Samples for Workbooks RevisionsList. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-04-01/examples/WorkbookRevisionsList.json
+     */
+    /**
+     * Sample code: WorkbookRevisionsList.
+     *
+     * @param manager Entry point to ApplicationInsightsManager.
+     */
+    public static void workbookRevisionsList(
+        com.azure.resourcemanager.applicationinsights.ApplicationInsightsManager manager) {
+        manager.workbooks().revisionsList("my-resource-group", "deadb33f-5e0d-4064-8ebb-1a4ed0313eb2", Context.NONE);
+    }
+}

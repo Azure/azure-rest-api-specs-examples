ccbfcf99ef23249dab9e4fd0ca47fdd264e17e72
azure-sdk <azuresdk@microsoft.com>
2022-06-29 02:03:39 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/mediaservices/armmediaservices/v2.0.0

---
+++ specification/mediaservices/resource-manager/Microsoft.Media/stable/2021-11-01/examples-go/private-link-resources-list.go
@@ -0,0 +1,31 @@
+package armmediaservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mediaservices/armmediaservices/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/mediaservices/resource-manager/Microsoft.Media/stable/2021-11-01/examples/private-link-resources-list.json
+func ExamplePrivateLinkResourcesClient_List() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armmediaservices.NewPrivateLinkResourcesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.List(ctx,
+		"contoso",
+		"contososports",
+		nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

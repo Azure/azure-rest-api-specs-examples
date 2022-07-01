53a72a526e9b4adae5ba86af95eb54d5aee3f4fe
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-01 08:02:08 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/resourceconnector/armresourceconnector/v0.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/preview/2022-04-15-preview/examples-go/UpgradeGraph.go
@@ -0,0 +1,32 @@
+package armresourceconnector_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resourceconnector/armresourceconnector"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/preview/2022-04-15-preview/examples/UpgradeGraph.json
+func ExampleAppliancesClient_GetUpgradeGraph() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armresourceconnector.NewAppliancesClient("11111111-2222-3333-4444-555555555555", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.GetUpgradeGraph(ctx,
+		"testresourcegroup",
+		"appliance01",
+		"stable",
+		nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

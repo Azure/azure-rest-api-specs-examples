8607346c0c5ccafdb4c0e72d7d4bc051436d46be
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-01 08:01:52 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/mediaservices/armmediaservices/v3.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/mediaservices/resource-manager/Microsoft.Media/stable/2021-11-01/examples-go/asset-tracks-operation-status-by-id-terminal-state-failed.go
+++ specification/mediaservices/resource-manager/Microsoft.Media/stable/2021-11-01/examples-go/asset-tracks-operation-status-by-id-terminal-state-failed.go
@@ -5,17 +5,17 @@ import (
 	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mediaservices/armmediaservices/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mediaservices/armmediaservices/v3"
 )
 
 // Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/mediaservices/resource-manager/Microsoft.Media/stable/2021-11-01/examples/asset-tracks-operation-status-by-id-terminal-state-failed.json
-func ExampleOperationStatusesClient_Get() {
+func ExampleAssetTrackOperationStatusesClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmediaservices.NewOperationStatusesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	client, err := armmediaservices.NewAssetTrackOperationStatusesClient("00000000-0000-0000-0000-000000000000", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}

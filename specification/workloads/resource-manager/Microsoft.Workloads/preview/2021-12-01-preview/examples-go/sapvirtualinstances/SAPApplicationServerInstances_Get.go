e6dc16dc47581af4ed58d6825bc391e19752acdd
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-02 08:01:45 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/workloads/armworkloads/v0.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/workloads/resource-manager/Microsoft.Workloads/preview/2021-12-01-preview/examples-go/sapvirtualinstances/SAPApplicationServerInstances_Get.go
@@ -0,0 +1,32 @@
+package armworkloads_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/workloads/armworkloads"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/workloads/resource-manager/Microsoft.Workloads/preview/2021-12-01-preview/examples/sapvirtualinstances/SAPApplicationServerInstances_Get.json
+func ExampleSAPApplicationServerInstancesClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armworkloads.NewSAPApplicationServerInstancesClient("6d875e77-e412-4d7d-9af4-8895278b4443", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.Get(ctx,
+		"test-rg",
+		"X00",
+		"app01",
+		nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

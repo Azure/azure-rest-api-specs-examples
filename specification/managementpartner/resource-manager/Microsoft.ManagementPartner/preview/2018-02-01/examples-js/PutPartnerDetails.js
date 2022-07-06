5eea348f1608bf56cc5716bb1b97da8e8f14ec1b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-06 08:01:57 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-managementpartner_3.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples-js/PutPartnerDetails.js
+++ specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples-js/PutPartnerDetails.js
@@ -1,6 +1,12 @@
 const { ACEProvisioningManagementPartnerAPI } = require("@azure/arm-managementpartner");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Create a management partner for the objectId and tenantId.
+ *
+ * @summary Create a management partner for the objectId and tenantId.
+ * x-ms-original-file: specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples/PutPartnerDetails.json
+ */
 async function putPartnerDetails() {
   const partnerId = "123456";
   const credential = new DefaultAzureCredential();

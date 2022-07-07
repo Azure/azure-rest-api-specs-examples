487e2716e12acad518d6bbf1100b83047243689c
azure-sdk <azuresdk@microsoft.com>
2022-07-07 00:10:36 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-support_2.0.2

--- specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples-js/CheckNameAvailabilityWithSubscription.js
+++ specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples-js/CheckNameAvailabilityWithSubscription.js
@@ -1,6 +1,12 @@
 const { MicrosoftSupport } = require("@azure/arm-support");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Check the availability of a resource name. This API should be used to check the uniqueness of the name for support ticket creation for the selected subscription.
+ *
+ * @summary Check the availability of a resource name. This API should be used to check the uniqueness of the name for support ticket creation for the selected subscription.
+ * x-ms-original-file: specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples/CheckNameAvailabilityWithSubscription.json
+ */
 async function checksWhetherNameIsAvailableForSupportTicketResource() {
   const subscriptionId = "subid";
   const checkNameAvailabilityInput = {
--- specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples-js/ListServices.js
+++ specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples-js/ListServices.js
@@ -1,6 +1,12 @@
 const { MicrosoftSupport } = require("@azure/arm-support");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Lists all the Azure services available for support ticket creation. For **Technical** issues, select the Service Id that maps to the Azure service/product as displayed in the **Services** drop-down list on the Azure portal's [New support request](https://portal.azure.com/#blade/Microsoft_Azure_Support/HelpAndSupportBlade/overview) page. Always use the service and its corresponding problem classification(s) obtained programmatically for support ticket creation. This practice ensures that you always have the most recent set of service and problem classification Ids.
+ *
+ * @summary Lists all the Azure services available for support ticket creation. For **Technical** issues, select the Service Id that maps to the Azure service/product as displayed in the **Services** drop-down list on the Azure portal's [New support request](https://portal.azure.com/#blade/Microsoft_Azure_Support/HelpAndSupportBlade/overview) page. Always use the service and its corresponding problem classification(s) obtained programmatically for support ticket creation. This practice ensures that you always have the most recent set of service and problem classification Ids.
+ * x-ms-original-file: specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples/ListServices.json
+ */
 async function getsListOfServicesForWhichASupportTicketCanBeCreated() {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();

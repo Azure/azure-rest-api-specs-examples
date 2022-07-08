e2c5c77009e35b0d658b2b66d848074fb96df349
azure-sdk <azuresdk@microsoft.com>
2022-07-08 00:07:13 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-education_1.0.0-beta.2

--- specification/education/resource-manager/Microsoft.Education/preview/2021-12-01-preview/examples-java/JoinRequestApproveAndDeny.java
+++ specification/education/resource-manager/Microsoft.Education/preview/2021-12-01-preview/examples-java/JoinRequestApproveAndDeny.java
@@ -1,19 +1,19 @@
 import com.azure.core.util.Context;
 
-/** Samples for JoinRequests Deny. */
+/** Samples for JoinRequests Approve. */
 public final class Main {
     /*
      * x-ms-original-file: specification/education/resource-manager/Microsoft.Education/preview/2021-12-01-preview/examples/JoinRequestApproveAndDeny.json
      */
     /**
-     * Sample code: JoinRequestDeny.
+     * Sample code: JoinRequestApprove.
      *
      * @param manager Entry point to EducationManager.
      */
-    public static void joinRequestDeny(com.azure.resourcemanager.education.EducationManager manager) {
+    public static void joinRequestApprove(com.azure.resourcemanager.education.EducationManager manager) {
         manager
             .joinRequests()
-            .denyWithResponse(
+            .approveWithResponse(
                 "{billingAccountName}",
                 "{billingProfileName}",
                 "{invoiceSectionName}",

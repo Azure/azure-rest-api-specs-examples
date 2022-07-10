a503c99fad4f869dd7d26758bccc5eb5e5c41ffb
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-10 08:01:44 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-powerbidedicated_4.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-js/checkNameAvailability.js
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-js/checkNameAvailability.js
@@ -1,6 +1,12 @@
 const { PowerBIDedicated } = require("@azure/arm-powerbidedicated");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Check the name availability in the target location.
+ *
+ * @summary Check the name availability in the target location.
+ * x-ms-original-file: specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/checkNameAvailability.json
+ */
 async function checkNameAvailabilityOfACapacity() {
   const subscriptionId = "613192d7-503f-477a-9cfe-4efc3ee2bd60";
   const location = "West US";
--- specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-js/createAutoScaleVCore.js
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-js/createAutoScaleVCore.js
@@ -1,6 +1,12 @@
 const { PowerBIDedicated } = require("@azure/arm-powerbidedicated");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Provisions the specified auto scale v-core based on the configuration specified in the request.
+ *
+ * @summary Provisions the specified auto scale v-core based on the configuration specified in the request.
+ * x-ms-original-file: specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/createAutoScaleVCore.json
+ */
 async function createAutoScaleVCore() {
   const subscriptionId = "613192d7-503f-477a-9cfe-4efc3ee2bd60";
   const resourceGroupName = "TestRG";

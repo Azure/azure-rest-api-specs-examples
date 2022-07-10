a7faf6d5552da40a8c16a08b65c85869778b828f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-10 08:01:37 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-marketplaceordering_3.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-js/GetMarketplaceTerms.js
+++ specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-js/GetMarketplaceTerms.js
@@ -1,6 +1,12 @@
 const { MarketplaceOrderingAgreements } = require("@azure/arm-marketplaceordering");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Get marketplace terms.
+ *
+ * @summary Get marketplace terms.
+ * x-ms-original-file: specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples/GetMarketplaceTerms.json
+ */
 async function getMarketplaceTerms() {
   const subscriptionId = "subid";
   const offerType = "virtualmachine";

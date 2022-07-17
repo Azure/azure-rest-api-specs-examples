5313c867838952f050218b06b0cc82fa29533441
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-17 08:01:29 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-networkfunction_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-05-01/examples-js/AzureTrafficCollectorDelete.js
@@ -0,0 +1,23 @@
+const { AzureTrafficCollectorClient } = require("@azure/arm-networkfunction");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a specified Azure Traffic Collector resource.
+ *
+ * @summary Deletes a specified Azure Traffic Collector resource.
+ * x-ms-original-file: specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-05-01/examples/AzureTrafficCollectorDelete.json
+ */
+async function deleteTrafficCollector() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const azureTrafficCollectorName = "atc";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureTrafficCollectorClient(credential, subscriptionId);
+  const result = await client.azureTrafficCollectors.beginDeleteAndWait(
+    resourceGroupName,
+    azureTrafficCollectorName
+  );
+  console.log(result);
+}
+
+deleteTrafficCollector().catch(console.error);

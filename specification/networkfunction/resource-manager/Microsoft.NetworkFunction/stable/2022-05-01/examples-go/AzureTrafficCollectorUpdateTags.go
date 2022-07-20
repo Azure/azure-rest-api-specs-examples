a6d5550484fcf225f8120c73bfb1e259105ca9be
azure-sdk <azuresdk@microsoft.com>
2022-07-20 00:11:51 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/networkfunction/armnetworkfunction/v1.0.0

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-05-01/examples-go/AzureTrafficCollectorUpdateTags.go
@@ -0,0 +1,38 @@
+package armnetworkfunction_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/networkfunction/armnetworkfunction"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-05-01/examples/AzureTrafficCollectorUpdateTags.json
+func ExampleAzureTrafficCollectorsClient_UpdateTags() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnetworkfunction.NewAzureTrafficCollectorsClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.UpdateTags(ctx,
+		"rg1",
+		"atc",
+		armnetworkfunction.TagsObject{
+			Tags: map[string]*string{
+				"key1": to.Ptr("value1"),
+				"key2": to.Ptr("value2"),
+			},
+		},
+		nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

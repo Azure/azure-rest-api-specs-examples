ac6497bf691ad6e97d9333d6179fbcb0ca47ed01
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-21 08:01:39 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/networkfunction/armnetworkfunction/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-05-01/examples-go/CollectorPolicyDelete.go
@@ -0,0 +1,34 @@
+package armnetworkfunction_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/networkfunction/armnetworkfunction"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-05-01/examples/CollectorPolicyDelete.json
+func ExampleCollectorPoliciesClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnetworkfunction.NewCollectorPoliciesClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginDelete(ctx,
+		"rg1",
+		"atc",
+		"cp1",
+		nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

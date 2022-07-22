9fd69d9be359b4697fb34950d56d0a211211597f
azure-sdk <azuresdk@microsoft.com>
2022-07-22 00:13:51 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v2.0.0-beta.2

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-06-02-preview/examples-go/Fleets_CreateOrUpdate.go
@@ -0,0 +1,50 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-06-02-preview/examples/Fleets_CreateOrUpdate.json
+func ExampleFleetsClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcontainerservice.NewFleetsClient("subid1", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx,
+		"rg1",
+		"fleet-1",
+		armcontainerservice.Fleet{
+			Location: to.Ptr("East US"),
+			Tags: map[string]*string{
+				"archv2": to.Ptr(""),
+				"tier":   to.Ptr("production"),
+			},
+			Properties: &armcontainerservice.FleetProperties{
+				HubProfile: &armcontainerservice.FleetHubProfile{
+					DNSPrefix: to.Ptr("dnsprefix1"),
+				},
+			},
+		},
+		&armcontainerservice.FleetsClientBeginCreateOrUpdateOptions{IfMatch: nil,
+			IfNoneMatch: nil,
+		})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

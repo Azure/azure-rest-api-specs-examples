ec92c9c9bb7f9a93fb8ee5e2da6fa80daabe8dcf
azure-sdk <azuresdk@microsoft.com>
2022-07-23 00:09:31 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/netapp/armnetapp/v2.1.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-03-01/examples-go/VolumeQuotaRules_List.go
@@ -0,0 +1,37 @@
+package armnetapp_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/netapp/armnetapp/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-03-01/examples/VolumeQuotaRules_List.json
+func ExampleVolumeQuotaRulesClient_NewListByVolumePager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnetapp.NewVolumeQuotaRulesClient("5275316f-a498-48d6-b324-2cbfdc4311b9", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := client.NewListByVolumePager("myRG",
+		"account-9957",
+		"pool-5210",
+		"volume-6387",
+		nil)
+	for pager.More() {
+		nextResult, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range nextResult.Value {
+			// TODO: use page item
+			_ = v
+		}
+	}
+}

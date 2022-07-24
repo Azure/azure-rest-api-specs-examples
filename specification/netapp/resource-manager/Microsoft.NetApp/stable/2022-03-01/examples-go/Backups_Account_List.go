8af7b209af4f11ffd74b3175fe24d47d22c3e9eb
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-24 08:02:10 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/netapp/armnetapp/v2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-03-01/examples-go/Backups_Account_List.go
@@ -0,0 +1,35 @@
+package armnetapp_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/netapp/armnetapp/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-03-01/examples/Backups_Account_List.json
+func ExampleAccountBackupsClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnetapp.NewAccountBackupsClient("D633CC2E-722B-4AE1-B636-BBD9E4C60ED9", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := client.NewListPager("myRG",
+		"account1",
+		nil)
+	for pager.More() {
+		nextResult, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range nextResult.Value {
+			// TODO: use page item
+			_ = v
+		}
+	}
+}

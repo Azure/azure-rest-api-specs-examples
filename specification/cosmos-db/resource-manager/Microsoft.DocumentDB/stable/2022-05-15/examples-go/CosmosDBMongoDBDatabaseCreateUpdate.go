1a475d1069892ad552e911fbf93e1e0b5fa84205
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-24 08:02:02 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/cosmos/armcosmos/v2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2022-05-15/examples-go/CosmosDBMongoDBDatabaseCreateUpdate.go
@@ -0,0 +1,47 @@
+package armcosmos_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cosmos/armcosmos/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2022-05-15/examples/CosmosDBMongoDBDatabaseCreateUpdate.json
+func ExampleMongoDBResourcesClient_BeginCreateUpdateMongoDBDatabase() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcosmos.NewMongoDBResourcesClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateUpdateMongoDBDatabase(ctx,
+		"rg1",
+		"ddb1",
+		"databaseName",
+		armcosmos.MongoDBDatabaseCreateUpdateParameters{
+			Location: to.Ptr("West US"),
+			Tags:     map[string]*string{},
+			Properties: &armcosmos.MongoDBDatabaseCreateUpdateProperties{
+				Options: &armcosmos.CreateUpdateOptions{},
+				Resource: &armcosmos.MongoDBDatabaseResource{
+					ID: to.Ptr("databaseName"),
+				},
+			},
+		},
+		nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

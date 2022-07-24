333f0ada5563d10b0fa1d8997a3379389ff655ee
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-24 08:01:55 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-netapp_16.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-03-01/examples-js/Accounts_List.js
@@ -0,0 +1,22 @@
+const { NetAppManagementClient } = require("@azure/arm-netapp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List and describe all NetApp accounts in the resource group.
+ *
+ * @summary List and describe all NetApp accounts in the resource group.
+ * x-ms-original-file: specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-03-01/examples/Accounts_List.json
+ */
+async function accountsList() {
+  const subscriptionId = "D633CC2E-722B-4AE1-B636-BBD9E4C60ED9";
+  const resourceGroupName = "myRG";
+  const credential = new DefaultAzureCredential();
+  const client = new NetAppManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.accounts.list(resourceGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+accountsList().catch(console.error);

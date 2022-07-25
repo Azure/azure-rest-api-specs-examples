b015c41dcce067530c402bb851259e04f0c1de58
azure-sdk <azuresdk@microsoft.com>
2022-07-25 04:43:53 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_17.0.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-06-01/examples-js/AgentPoolsList.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a list of agent pools in the specified managed cluster.
+ *
+ * @summary Gets a list of agent pools in the specified managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-06-01/examples/AgentPoolsList.json
+ */
+async function listAgentPoolsByManagedCluster() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.agentPools.list(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+listAgentPoolsByManagedCluster().catch(console.error);
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-06-01/examples-js/PrivateEndpointConnectionsList.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to To learn more about private clusters, see: https://docs.microsoft.com/azure/aks/private-clusters
+ *
+ * @summary To learn more about private clusters, see: https://docs.microsoft.com/azure/aks/private-clusters
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-06-01/examples/PrivateEndpointConnectionsList.json
+ */
+async function listPrivateEndpointConnectionsByManagedCluster() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.privateEndpointConnections.list(resourceGroupName, resourceName);
+  console.log(result);
+}
+
+listPrivateEndpointConnectionsByManagedCluster().catch(console.error);

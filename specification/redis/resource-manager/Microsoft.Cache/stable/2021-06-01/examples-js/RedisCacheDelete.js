e90acf449c50ab3e548fb9d063511925ca90ce34
azure-sdk <azuresdk@microsoft.com>
2022-07-25 08:10:45 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-rediscache_6.1.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2021-06-01/examples-js/RedisCacheDelete.js
@@ -0,0 +1,20 @@
+const { RedisManagementClient } = require("@azure/arm-rediscache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a Redis cache.
+ *
+ * @summary Deletes a Redis cache.
+ * x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2021-06-01/examples/RedisCacheDelete.json
+ */
+async function redisCacheDelete() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const name = "cache1";
+  const credential = new DefaultAzureCredential();
+  const client = new RedisManagementClient(credential, subscriptionId);
+  const result = await client.redis.beginDeleteAndWait(resourceGroupName, name);
+  console.log(result);
+}
+
+redisCacheDelete().catch(console.error);

6cadb65c738c5331354dcb850d1dc75e351d8f61
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-25 13:38:46 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_17.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-06-01/examples-js/ManagedClustersStart.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to See [starting a cluster](https://docs.microsoft.com/azure/aks/start-stop-cluster) for more details about starting a cluster.
+ *
+ * @summary See [starting a cluster](https://docs.microsoft.com/azure/aks/start-stop-cluster) for more details about starting a cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-06-01/examples/ManagedClustersStart.json
+ */
+async function startManagedCluster() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginStartAndWait(resourceGroupName, resourceName);
+  console.log(result);
+}
+
+startManagedCluster().catch(console.error);

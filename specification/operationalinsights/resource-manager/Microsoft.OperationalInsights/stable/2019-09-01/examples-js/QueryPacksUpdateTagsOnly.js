70a6ef6a85005a8f8faeb3907bcb6c302d42dcb1
azure-sdk <azuresdk@microsoft.com>
2022-07-25 04:53:24 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-operationalinsights_9.0.0-beta.1

---
+++ specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/stable/2019-09-01/examples-js/QueryPacksUpdateTagsOnly.js
@@ -0,0 +1,27 @@
+const { OperationalInsightsManagementClient } = require("@azure/arm-operationalinsights");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates an existing QueryPack's tags. To update other fields use the CreateOrUpdate method.
+ *
+ * @summary Updates an existing QueryPack's tags. To update other fields use the CreateOrUpdate method.
+ * x-ms-original-file: specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/stable/2019-09-01/examples/QueryPacksUpdateTagsOnly.json
+ */
+async function queryPackUpdateTagsOnly() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "my-resource-group";
+  const queryPackName = "my-querypack";
+  const queryPackTags = {
+    tags: { tag1: "Value1", tag2: "Value2" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new OperationalInsightsManagementClient(credential, subscriptionId);
+  const result = await client.queryPacks.updateTags(
+    resourceGroupName,
+    queryPackName,
+    queryPackTags
+  );
+  console.log(result);
+}
+
+queryPackUpdateTagsOnly().catch(console.error);

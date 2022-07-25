6957d1fa321db562a210341bdac1d143311b86c6
azure-sdk <azuresdk@microsoft.com>
2022-07-25 04:47:59 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-portal_1.0.0-beta.4

---
+++ specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples-js/TenantConfiguration/GetTenantConfiguration.js
@@ -0,0 +1,19 @@
+const { Portal } = require("@azure/arm-portal");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the tenant configuration.
+ *
+ * @summary Gets the tenant configuration.
+ * x-ms-original-file: specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples/TenantConfiguration/GetTenantConfiguration.json
+ */
+async function getTenantConfiguration() {
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const configurationName = "default";
+  const credential = new DefaultAzureCredential();
+  const client = new Portal(credential, subscriptionId);
+  const result = await client.tenantConfigurations.get(configurationName);
+  console.log(result);
+}
+
+getTenantConfiguration().catch(console.error);
---
+++ specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples-js/deleteDashboard.js
@@ -0,0 +1,20 @@
+const { Portal } = require("@azure/arm-portal");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes the Dashboard.
+ *
+ * @summary Deletes the Dashboard.
+ * x-ms-original-file: specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples/deleteDashboard.json
+ */
+async function deleteADashboard() {
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = "testRG";
+  const dashboardName = "testDashboard";
+  const credential = new DefaultAzureCredential();
+  const client = new Portal(credential, subscriptionId);
+  const result = await client.dashboards.delete(resourceGroupName, dashboardName);
+  console.log(result);
+}
+
+deleteADashboard().catch(console.error);

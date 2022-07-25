0be195ed14a1b289894bce9c3413d0bd0dee1faf
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-25 13:37:07 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-machinelearningexperimentation_2.0.0-beta.3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/machinelearningexperimentation/resource-manager/Microsoft.MachineLearningExperimentation/preview/2017-05-01-preview/examples-js/ListAccount.js
@@ -0,0 +1,21 @@
+const { MLTeamAccountManagementClient } = require("@azure/arm-machinelearningexperimentation");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists all the available machine learning team accounts under the specified subscription.
+ *
+ * @summary Lists all the available machine learning team accounts under the specified subscription.
+ * x-ms-original-file: specification/machinelearningexperimentation/resource-manager/Microsoft.MachineLearningExperimentation/preview/2017-05-01-preview/examples/ListAccount.json
+ */
+async function accountList() {
+  const subscriptionId = "00000000-1111-2222-3333-444444444444";
+  const credential = new DefaultAzureCredential();
+  const client = new MLTeamAccountManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.accounts.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+accountList().catch(console.error);

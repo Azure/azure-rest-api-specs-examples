ef87034b47d925aacd9c5d6912ef420b8bb524db
azure-sdk <azuresdk@microsoft.com>
2022-07-25 04:47:18 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-deviceupdate_1.0.0-beta.2

---
+++ specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/preview/2022-04-01-preview/examples-js/CheckNameAvailability_AlreadyExists.js
@@ -0,0 +1,22 @@
+const { DeviceUpdate } = require("@azure/arm-deviceupdate");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Checks ADU resource name availability.
+ *
+ * @summary Checks ADU resource name availability.
+ * x-ms-original-file: specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/preview/2022-04-01-preview/examples/CheckNameAvailability_AlreadyExists.json
+ */
+async function checkNameAvailabilityAlreadyExists() {
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const request = {
+    name: "contoso",
+    type: "Microsoft.DeviceUpdate/accounts",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new DeviceUpdate(credential, subscriptionId);
+  const result = await client.checkNameAvailability(request);
+  console.log(result);
+}
+
+checkNameAvailabilityAlreadyExists().catch(console.error);
---
+++ specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/preview/2022-04-01-preview/examples-js/Instances/Instances_Get.js
@@ -0,0 +1,21 @@
+const { DeviceUpdate } = require("@azure/arm-deviceupdate");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Returns instance details for the given instance and account name.
+ *
+ * @summary Returns instance details for the given instance and account name.
+ * x-ms-original-file: specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/preview/2022-04-01-preview/examples/Instances/Instances_Get.json
+ */
+async function getsListOfInstances() {
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = "test-rg";
+  const accountName = "contoso";
+  const instanceName = "blue";
+  const credential = new DefaultAzureCredential();
+  const client = new DeviceUpdate(credential, subscriptionId);
+  const result = await client.instances.get(resourceGroupName, accountName, instanceName);
+  console.log(result);
+}
+
+getsListOfInstances().catch(console.error);

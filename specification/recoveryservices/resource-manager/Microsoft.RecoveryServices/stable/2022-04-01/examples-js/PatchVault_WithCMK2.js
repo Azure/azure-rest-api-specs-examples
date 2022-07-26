54bf26299edbc5e6a36036d0ff3a841d79f804d5
azure-sdk <azuresdk@microsoft.com>
2022-07-26 00:12:56 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-recoveryservices_5.1.0

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2022-04-01/examples-js/PatchVault_WithCMK2.js
@@ -0,0 +1,27 @@
+const { RecoveryServicesClient } = require("@azure/arm-recoveryservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates the vault.
+ *
+ * @summary Updates the vault.
+ * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2022-04-01/examples/PatchVault_WithCMK2.json
+ */
+async function updateResourceWithCustomerManagedKeys2() {
+  const subscriptionId = "77777777-b0c6-47a2-b37c-d8e65a629c18";
+  const resourceGroupName = "HelloWorld";
+  const vaultName = "swaggerExample";
+  const vault = {
+    identity: { type: "SystemAssigned" },
+    properties: {
+      encryption: { kekIdentity: { useSystemAssignedIdentity: true } },
+    },
+    tags: { patchKey: "PatchKeyUpdated" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RecoveryServicesClient(credential, subscriptionId);
+  const result = await client.vaults.beginUpdateAndWait(resourceGroupName, vaultName, vault);
+  console.log(result);
+}
+
+updateResourceWithCustomerManagedKeys2().catch(console.error);

e214a27825c4bd7cb089c3128b43772509a6a5e6
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-07-27 08:01:55 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-recoveryservices_5.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2022-04-01/examples-js/PATCHVault_WithMonitoringSettings.js
@@ -0,0 +1,29 @@
+const { RecoveryServicesClient } = require("@azure/arm-recoveryservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates the vault.
+ *
+ * @summary Updates the vault.
+ * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2022-04-01/examples/PATCHVault_WithMonitoringSettings.json
+ */
+async function updateVaultWithMonitoringSetting() {
+  const subscriptionId = "77777777-b0c6-47a2-b37c-d8e65a629c18";
+  const resourceGroupName = "HelloWorld";
+  const vaultName = "swaggerExample";
+  const vault = {
+    properties: {
+      monitoringSettings: {
+        azureMonitorAlertSettings: { alertsForAllJobFailures: "Enabled" },
+        classicAlertSettings: { alertsForCriticalOperations: "Disabled" },
+      },
+    },
+    tags: { patchKey: "PatchKeyUpdated" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RecoveryServicesClient(credential, subscriptionId);
+  const result = await client.vaults.beginUpdateAndWait(resourceGroupName, vaultName, vault);
+  console.log(result);
+}
+
+updateVaultWithMonitoringSetting().catch(console.error);

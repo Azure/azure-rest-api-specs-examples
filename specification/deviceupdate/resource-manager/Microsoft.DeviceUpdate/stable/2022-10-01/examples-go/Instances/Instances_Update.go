c9453899ff5826452948befc1f02abcb0651275e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-08-24 08:03:09 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/deviceupdate/armdeviceupdate/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/stable/2022-10-01/examples-go/Instances/Instances_Update.go
@@ -0,0 +1,33 @@
+package armdeviceupdate_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/deviceupdate/armdeviceupdate"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/stable/2022-10-01/examples/Instances/Instances_Update.json
+func ExampleInstancesClient_Update() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armdeviceupdate.NewInstancesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.Update(ctx, "test-rg", "contoso", "blue", armdeviceupdate.TagUpdate{
+		Tags: map[string]*string{
+			"tagKey": to.Ptr("tagValue"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}
---
+++ specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/stable/2022-10-01/examples-go/PrivateEndpointConnections/PrivateEndpointConnection_CreateOrUpdate.go
@@ -0,0 +1,38 @@
+package armdeviceupdate_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/deviceupdate/armdeviceupdate"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/stable/2022-10-01/examples/PrivateEndpointConnections/PrivateEndpointConnection_CreateOrUpdate.json
+func ExamplePrivateEndpointConnectionsClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armdeviceupdate.NewPrivateEndpointConnectionsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "test-rg", "contoso", "peexample01", armdeviceupdate.PrivateEndpointConnection{
+		Properties: &armdeviceupdate.PrivateEndpointConnectionProperties{
+			PrivateLinkServiceConnectionState: &armdeviceupdate.PrivateLinkServiceConnectionState{
+				Description: to.Ptr("Auto-Approved"),
+				Status:      to.Ptr(armdeviceupdate.PrivateEndpointServiceConnectionStatusApproved),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

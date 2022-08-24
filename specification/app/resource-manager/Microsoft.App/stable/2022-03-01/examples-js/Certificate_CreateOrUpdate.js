9bb7570810f02f2a4a611d7f41bce75a52d5ed40
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-08-24 08:02:30 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-appcontainers_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/app/resource-manager/Microsoft.App/stable/2022-03-01/examples-js/Certificate_CreateOrUpdate.js
+++ specification/app/resource-manager/Microsoft.App/stable/2022-03-01/examples-js/Certificate_CreateOrUpdate.js
@@ -16,6 +16,7 @@ async function createOrUpdateCertificate() {
     location: "East US",
     properties: {
       password: "private key password",
+      value: Buffer.from("PFX-or-PEM-blob"),
     },
   };
   const options = {

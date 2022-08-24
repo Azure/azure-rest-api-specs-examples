96dd87b3fbb7989da3b6bcab595ceac63aa3f402
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-08-24 08:03:01 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/dashboard/armdashboard/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2022-08-01/examples-go/Grafana_Create.go
@@ -0,0 +1,56 @@
+package armdashboard_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dashboard/armdashboard"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2022-08-01/examples/Grafana_Create.json
+func ExampleGrafanaClient_BeginCreate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armdashboard.NewGrafanaClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreate(ctx, "myResourceGroup", "myWorkspace", armdashboard.ManagedGrafana{
+		Identity: &armdashboard.ManagedServiceIdentity{
+			Type: to.Ptr(armdashboard.ManagedServiceIdentityTypeSystemAssigned),
+		},
+		Location: to.Ptr("West US"),
+		Properties: &armdashboard.ManagedGrafanaProperties{
+			APIKey:                  to.Ptr(armdashboard.APIKeyEnabled),
+			DeterministicOutboundIP: to.Ptr(armdashboard.DeterministicOutboundIPEnabled),
+			GrafanaIntegrations: &armdashboard.GrafanaIntegrations{
+				AzureMonitorWorkspaceIntegrations: []*armdashboard.AzureMonitorWorkspaceIntegration{
+					{
+						AzureMonitorWorkspaceResourceID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/myResourceGroup/providers/microsoft.monitor/accounts/myAzureMonitorWorkspace"),
+					}},
+			},
+			PublicNetworkAccess: to.Ptr(armdashboard.PublicNetworkAccessEnabled),
+			ZoneRedundancy:      to.Ptr(armdashboard.ZoneRedundancyEnabled),
+		},
+		SKU: &armdashboard.ResourceSKU{
+			Name: to.Ptr("Standard"),
+		},
+		Tags: map[string]*string{
+			"Environment": to.Ptr("Dev"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}
---
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2022-08-01/examples-go/PrivateEndpointConnections_List.go
@@ -0,0 +1,33 @@
+package armdashboard_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dashboard/armdashboard"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2022-08-01/examples/PrivateEndpointConnections_List.json
+func ExamplePrivateEndpointConnectionsClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armdashboard.NewPrivateEndpointConnectionsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := client.NewListPager("myResourceGroup", "myWorkspace", nil)
+	for pager.More() {
+		nextResult, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range nextResult.Value {
+			// TODO: use page item
+			_ = v
+		}
+	}
+}

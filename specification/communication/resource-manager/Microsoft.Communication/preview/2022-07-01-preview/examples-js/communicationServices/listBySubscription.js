7934512064d1c5ae0869c37884ac8b6d0aa61b7b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-08-25 17:11:01 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-communication_4.0.0-beta.2 (#1198)
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2022-07-01-preview/examples-js/communicationServices/listBySubscription.js
@@ -0,0 +1,21 @@
+const { CommunicationServiceManagementClient } = require("@azure/arm-communication");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Handles requests to list all resources in a subscription.
+ *
+ * @summary Handles requests to list all resources in a subscription.
+ * x-ms-original-file: specification/communication/resource-manager/Microsoft.Communication/preview/2022-07-01-preview/examples/communicationServices/listBySubscription.json
+ */
+async function listBySubscription() {
+  const subscriptionId = "12345";
+  const credential = new DefaultAzureCredential();
+  const client = new CommunicationServiceManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.communicationServices.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+listBySubscription().catch(console.error);
---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2022-07-01-preview/examples-js/domains/get.js
@@ -0,0 +1,21 @@
+const { CommunicationServiceManagementClient } = require("@azure/arm-communication");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get the Domains resource and its properties.
+ *
+ * @summary Get the Domains resource and its properties.
+ * x-ms-original-file: specification/communication/resource-manager/Microsoft.Communication/preview/2022-07-01-preview/examples/domains/get.json
+ */
+async function getDomainsResource() {
+  const subscriptionId = "12345";
+  const resourceGroupName = "MyResourceGroup";
+  const emailServiceName = "MyEmailServiceResource";
+  const domainName = "mydomain.com";
+  const credential = new DefaultAzureCredential();
+  const client = new CommunicationServiceManagementClient(credential, subscriptionId);
+  const result = await client.domains.get(resourceGroupName, emailServiceName, domainName);
+  console.log(result);
+}
+
+getDomainsResource().catch(console.error);

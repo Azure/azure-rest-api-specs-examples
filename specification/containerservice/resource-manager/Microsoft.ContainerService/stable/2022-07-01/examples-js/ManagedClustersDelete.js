58950bee9af9e5980845c8018754f0f468fd909f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-08-31 08:01:43 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_17.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-07-01/examples-js/ManagedClustersDelete.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a managed cluster.
+ *
+ * @summary Deletes a managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-07-01/examples/ManagedClustersDelete.json
+ */
+async function deleteManagedCluster() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginDeleteAndWait(resourceGroupName, resourceName);
+  console.log(result);
+}
+
+deleteManagedCluster().catch(console.error);

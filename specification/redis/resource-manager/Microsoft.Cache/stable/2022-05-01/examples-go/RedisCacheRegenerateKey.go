523d45d8e73f9e74629fc7561854836c03119384
azure-sdk <azuresdk@microsoft.com>
2022-09-03 00:04:09 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/redis/armredis/v2.0.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples-go/RedisCacheRegenerateKey.go
@@ -0,0 +1,31 @@
+package armredis_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redis/armredis/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples/RedisCacheRegenerateKey.json
+func ExampleClient_RegenerateKey() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armredis.NewClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.RegenerateKey(ctx, "rg1", "cache1", armredis.RegenerateKeyParameters{
+		KeyType: to.Ptr(armredis.RedisKeyTypePrimary),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

10c2ca8d710d282c13462949023d283e600f20de
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-09-04 08:02:11 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/redis/armredis/v2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples-go/RedisCacheLinkedServer_Get.go
@@ -0,0 +1,28 @@
+package armredis_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redis/armredis/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples/RedisCacheLinkedServer_Get.json
+func ExampleLinkedServerClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armredis.NewLinkedServerClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.Get(ctx, "rg1", "cache1", "cache2", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

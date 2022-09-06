2a84f46597fbd1b34391ea42536a4f287d314b4a
azure-sdk <azuresdk@microsoft.com>
2022-09-06 00:06:09 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-rediscache_7.0.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples-js/RedisCacheGet.js
@@ -0,0 +1,20 @@
+const { RedisManagementClient } = require("@azure/arm-rediscache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a Redis cache (resource description).
+ *
+ * @summary Gets a Redis cache (resource description).
+ * x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples/RedisCacheGet.json
+ */
+async function redisCacheGet() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const name = "cache1";
+  const credential = new DefaultAzureCredential();
+  const client = new RedisManagementClient(credential, subscriptionId);
+  const result = await client.redis.get(resourceGroupName, name);
+  console.log(result);
+}
+
+redisCacheGet().catch(console.error);
---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples-js/RedisCacheLinkedServer_Get.js
@@ -0,0 +1,21 @@
+const { RedisManagementClient } = require("@azure/arm-rediscache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the detailed information about a linked server of a redis cache (requires Premium SKU).
+ *
+ * @summary Gets the detailed information about a linked server of a redis cache (requires Premium SKU).
+ * x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples/RedisCacheLinkedServer_Get.json
+ */
+async function linkedServerGet() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const name = "cache1";
+  const linkedServerName = "cache2";
+  const credential = new DefaultAzureCredential();
+  const client = new RedisManagementClient(credential, subscriptionId);
+  const result = await client.linkedServer.get(resourceGroupName, name, linkedServerName);
+  console.log(result);
+}
+
+linkedServerGet().catch(console.error);

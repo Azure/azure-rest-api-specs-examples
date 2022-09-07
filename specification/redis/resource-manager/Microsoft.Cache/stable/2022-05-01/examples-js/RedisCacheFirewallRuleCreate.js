d503ea86f86c121af77ca8e93a5130f643e63d92
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-09-07 08:02:19 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-rediscache_7.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples-js/RedisCacheFirewallRuleCreate.js
@@ -0,0 +1,30 @@
+const { RedisManagementClient } = require("@azure/arm-rediscache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Create or update a redis cache firewall rule
+ *
+ * @summary Create or update a redis cache firewall rule
+ * x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2022-05-01/examples/RedisCacheFirewallRuleCreate.json
+ */
+async function redisCacheFirewallRuleCreate() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const cacheName = "cache1";
+  const ruleName = "rule1";
+  const parameters = {
+    endIP: "192.168.1.4",
+    startIP: "192.168.1.1",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RedisManagementClient(credential, subscriptionId);
+  const result = await client.firewallRules.createOrUpdate(
+    resourceGroupName,
+    cacheName,
+    ruleName,
+    parameters
+  );
+  console.log(result);
+}
+
+redisCacheFirewallRuleCreate().catch(console.error);

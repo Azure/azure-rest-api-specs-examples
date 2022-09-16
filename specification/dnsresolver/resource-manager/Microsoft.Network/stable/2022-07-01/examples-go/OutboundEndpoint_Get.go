039ad8a3304f9fd624a3d9311d2482adb75cb2dd
azure-sdk <azuresdk@microsoft.com>
2022-09-16 00:11:19 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/dnsresolver/armdnsresolver/v1.0.0

---
+++ specification/dnsresolver/resource-manager/Microsoft.Network/stable/2022-07-01/examples-go/OutboundEndpoint_Get.go
@@ -0,0 +1,28 @@
+package armdnsresolver_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dnsresolver/armdnsresolver"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/dnsresolver/resource-manager/Microsoft.Network/stable/2022-07-01/examples/OutboundEndpoint_Get.json
+func ExampleOutboundEndpointsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armdnsresolver.NewOutboundEndpointsClient("abdd4249-9f34-4cc6-8e42-c2e32110603e", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.Get(ctx, "sampleResourceGroup", "sampleDnsResolver", "sampleOutboundEndpoint", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

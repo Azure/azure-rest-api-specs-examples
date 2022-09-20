61a316169c6fda81e56befb061936b55763d0b9d
azure-sdk <azuresdk@microsoft.com>
2022-09-20 00:07:13 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-netapp_1.0.0-beta.11

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-05-01/examples-java/QuotaLimits_List.java
@@ -0,0 +1,16 @@
+import com.azure.core.util.Context;
+
+/** Samples for NetAppResourceQuotaLimits List. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/netapp/resource-manager/Microsoft.NetApp/stable/2022-05-01/examples/QuotaLimits_List.json
+     */
+    /**
+     * Sample code: QuotaLimits.
+     *
+     * @param manager Entry point to NetAppFilesManager.
+     */
+    public static void quotaLimits(com.azure.resourcemanager.netapp.NetAppFilesManager manager) {
+        manager.netAppResourceQuotaLimits().list("eastus", Context.NONE);
+    }
+}

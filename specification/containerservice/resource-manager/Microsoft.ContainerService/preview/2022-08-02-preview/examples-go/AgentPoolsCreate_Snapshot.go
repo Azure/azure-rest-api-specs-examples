3da0e0deff3331b90c7bbcbdecd14bd0ec7d271d
azure-sdk <azuresdk@microsoft.com>
2022-09-21 00:17:44 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v2.2.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-08-02-preview/examples-go/AgentPoolsCreate_Snapshot.go
@@ -0,0 +1,44 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-08-02-preview/examples/AgentPoolsCreate_Snapshot.json
+func ExampleAgentPoolsClient_BeginCreateOrUpdate_createAgentPoolUsingAnAgentPoolSnapshot() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcontainerservice.NewAgentPoolsClient("subid1", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "rg1", "clustername1", "agentpool1", armcontainerservice.AgentPool{
+		Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+			Count: to.Ptr[int32](3),
+			CreationData: &armcontainerservice.CreationData{
+				SourceResourceID: to.Ptr("/subscriptions/subid1/resourceGroups/rg1/providers/Microsoft.ContainerService/snapshots/snapshot1"),
+			},
+			EnableFIPS:          to.Ptr(true),
+			OrchestratorVersion: to.Ptr(""),
+			OSType:              to.Ptr(armcontainerservice.OSTypeLinux),
+			VMSize:              to.Ptr("Standard_DS2_v2"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

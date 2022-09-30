0fe403c51e5d09ff8d96b022e5fca70b33991133
azure-sdk <azuresdk@microsoft.com>
2022-09-30 00:05:58 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_18.0.0-beta.2

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-08-03-preview/examples-js/SnapshotsGet.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a snapshot.
+ *
+ * @summary Gets a snapshot.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-08-03-preview/examples/SnapshotsGet.json
+ */
+async function getSnapshot() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const resourceName = "snapshot1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.snapshots.get(resourceGroupName, resourceName);
+  console.log(result);
+}
+
+getSnapshot().catch(console.error);

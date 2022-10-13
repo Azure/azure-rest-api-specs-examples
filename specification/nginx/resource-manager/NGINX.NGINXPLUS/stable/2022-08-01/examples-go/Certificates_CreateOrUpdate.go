4d95046a77ab60a395dec5331018515b2d48f3db
azure-sdk <azuresdk@microsoft.com>
2022-10-13 00:11:26 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/nginx/armnginx/v1.0.1

---
+++ specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2022-08-01/examples-go/Certificates_CreateOrUpdate.go
@@ -0,0 +1,32 @@
+package armnginx_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/nginx/armnginx"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2022-08-01/examples/Certificates_CreateOrUpdate.json
+func ExampleCertificatesClient_BeginCreate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnginx.NewCertificatesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreate(ctx, "myResourceGroup", "myDeployment", "default", &armnginx.CertificatesClientBeginCreateOptions{Body: nil})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

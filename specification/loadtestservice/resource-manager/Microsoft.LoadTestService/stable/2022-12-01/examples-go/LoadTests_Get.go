990715903169407e1b12c6493c62ba198df5677a
azure-sdk <azuresdk@microsoft.com>
2022-10-18 00:11:23 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/loadtestservice/armloadtestservice/v1.0.0

---
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-go/LoadTests_Get.go
@@ -0,0 +1,28 @@
+package armloadtestservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/loadtestservice/armloadtestservice"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples/LoadTests_Get.json
+func ExampleLoadTestsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armloadtestservice.NewLoadTestsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.Get(ctx, "dummyrg", "myLoadTest", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

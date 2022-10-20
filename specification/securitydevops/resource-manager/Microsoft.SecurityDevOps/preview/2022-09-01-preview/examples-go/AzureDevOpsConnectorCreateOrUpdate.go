b7a3663d8c5fe2117b7077236d3e595504aef4db
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-10-20 08:02:28 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/securitydevops/armsecuritydevops/v0.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/securitydevops/resource-manager/Microsoft.SecurityDevOps/preview/2022-09-01-preview/examples-go/AzureDevOpsConnectorCreateOrUpdate.go
@@ -0,0 +1,50 @@
+package armsecuritydevops_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/securitydevops/armsecuritydevops"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/securitydevops/resource-manager/Microsoft.SecurityDevOps/preview/2022-09-01-preview/examples/AzureDevOpsConnectorCreateOrUpdate.json
+func ExampleAzureDevOpsConnectorClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armsecuritydevops.NewAzureDevOpsConnectorClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "westusrg", "testconnector", armsecuritydevops.AzureDevOpsConnector{
+		Location: to.Ptr("West US"),
+		Properties: &armsecuritydevops.AzureDevOpsConnectorProperties{
+			Authorization: &armsecuritydevops.AuthorizationInfo{
+				Code: to.Ptr("00000000000000000000"),
+			},
+			Orgs: []*armsecuritydevops.AzureDevOpsOrgMetadata{
+				{
+					Name: to.Ptr("testOrg"),
+					Projects: []*armsecuritydevops.AzureDevOpsProjectMetadata{
+						{
+							Name: to.Ptr("testProject"),
+							Repos: []*string{
+								to.Ptr("testRepo")},
+						}},
+				}},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}
---
+++ specification/securitydevops/resource-manager/Microsoft.SecurityDevOps/preview/2022-09-01-preview/examples-go/AzureDevOpsConnectorUpdate.go
@@ -0,0 +1,39 @@
+package armsecuritydevops_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/securitydevops/armsecuritydevops"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/securitydevops/resource-manager/Microsoft.SecurityDevOps/preview/2022-09-01-preview/examples/AzureDevOpsConnectorUpdate.json
+func ExampleAzureDevOpsConnectorClient_BeginUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armsecuritydevops.NewAzureDevOpsConnectorClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginUpdate(ctx, "westusrg", "testconnector", armsecuritydevops.AzureDevOpsConnector{
+		Location: to.Ptr("West US"),
+		Tags: map[string]*string{
+			"client": to.Ptr("dev-client"),
+			"env":    to.Ptr("dev"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

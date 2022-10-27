9af118cd2e2b3722d49af7cfe48ba2719ddbb7f5
azure-sdk <azuresdk@microsoft.com>
2022-10-27 03:44:28 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-rediscache_7.1.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-06-01/examples-js/RedisCacheExport.js
@@ -0,0 +1,25 @@
+const { RedisManagementClient } = require("@azure/arm-rediscache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Export data from the redis cache to blobs in a container.
+ *
+ * @summary Export data from the redis cache to blobs in a container.
+ * x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2022-06-01/examples/RedisCacheExport.json
+ */
+async function redisCacheExport() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const name = "cache1";
+  const parameters = {
+    format: "RDB",
+    container: "https://contosostorage.blob.core.window.net/urltoBlobContainer?sasKeyParameters",
+    prefix: "datadump1",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RedisManagementClient(credential, subscriptionId);
+  const result = await client.redis.beginExportDataAndWait(resourceGroupName, name, parameters);
+  console.log(result);
+}
+
+redisCacheExport().catch(console.error);
---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2022-06-01/examples-js/RedisCacheGetPrivateEndpointConnection.js
@@ -0,0 +1,25 @@
+const { RedisManagementClient } = require("@azure/arm-rediscache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the specified private endpoint connection associated with the redis cache.
+ *
+ * @summary Gets the specified private endpoint connection associated with the redis cache.
+ * x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2022-06-01/examples/RedisCacheGetPrivateEndpointConnection.json
+ */
+async function redisCacheGetPrivateEndpointConnection() {
+  const subscriptionId = "{subscriptionId}";
+  const resourceGroupName = "rgtest01";
+  const cacheName = "cachetest01";
+  const privateEndpointConnectionName = "pectest01";
+  const credential = new DefaultAzureCredential();
+  const client = new RedisManagementClient(credential, subscriptionId);
+  const result = await client.privateEndpointConnections.get(
+    resourceGroupName,
+    cacheName,
+    privateEndpointConnectionName
+  );
+  console.log(result);
+}
+
+redisCacheGetPrivateEndpointConnection().catch(console.error);

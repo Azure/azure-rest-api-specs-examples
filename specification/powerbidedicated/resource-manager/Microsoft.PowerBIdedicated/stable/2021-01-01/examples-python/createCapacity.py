e7ba8b71f5932e4688f8a68547ffd9bbf1f6c5ae
azure-sdk <azuresdk@microsoft.com>
2022-11-01 00:09:15 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-powerbidedicated_1.1.0b1

---
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-python/createCapacity.py
@@ -0,0 +1,39 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.powerbidedicated import PowerBIDedicated
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-powerbidedicated
+# USAGE
+    python create_capacity.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PowerBIDedicated(
+        credential=DefaultAzureCredential(),
+        subscription_id="613192d7-503f-477a-9cfe-4efc3ee2bd60",
+    )
+
+    response = client.capacities.begin_create(
+        resource_group_name="TestRG",
+        dedicated_capacity_name="azsdktest",
+        capacity_parameters={
+            "location": "West US",
+            "properties": {"administration": {"members": ["azsdktest@microsoft.com", "azsdktest2@microsoft.com"]}},
+            "sku": {"name": "A1", "tier": "PBIE_Azure"},
+            "tags": {"testKey": "testValue"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/createCapacity.json
+if __name__ == "__main__":
+    main()
---
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-python/listSKUsForNew.py
@@ -0,0 +1,30 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.powerbidedicated import PowerBIDedicated
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-powerbidedicated
+# USAGE
+    python list_eligible_sk_us_for_a_new_capacity.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PowerBIDedicated(
+        credential=DefaultAzureCredential(),
+        subscription_id="613192d7-503f-477a-9cfe-4efc3ee2bd60",
+    )
+
+    response = client.capacities.list_skus()
+    print(response)
+
+
+# x-ms-original-file: specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/listSKUsForNew.json
+if __name__ == "__main__":
+    main()

cf883916659c685ff281360ac75cc49d3053a0ff
azure-sdk <azuresdk@microsoft.com>
2022-11-01 00:13:06 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-advisor_10.0.0b1

---
+++ specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-python/GenerateRecommendations.py
@@ -0,0 +1,30 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.advisor import AdvisorManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-advisor
+# USAGE
+    python generate_recommendations.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AdvisorManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscriptionId",
+    )
+
+    response = client.recommendations.generate()
+    print(response)
+
+
+# x-ms-original-file: specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples/GenerateRecommendations.json
+if __name__ == "__main__":
+    main()

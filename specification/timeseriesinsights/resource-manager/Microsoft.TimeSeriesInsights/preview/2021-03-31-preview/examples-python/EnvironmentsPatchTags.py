3a504b402d77685d2b501e20119d2f22b2be620f
azure-sdk <azuresdk@microsoft.com>
2022-11-01 00:06:54 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-timeseriesinsights_2.0.0b1

---
+++ specification/timeseriesinsights/resource-manager/Microsoft.TimeSeriesInsights/preview/2021-03-31-preview/examples-python/EnvironmentsPatchTags.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.timeseriesinsights import TimeSeriesInsightsClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-timeseriesinsights
+# USAGE
+    python environments_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TimeSeriesInsightsClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.environments.begin_update(
+        resource_group_name="rg1",
+        environment_name="env1",
+        environment_update_parameters={"tags": {"someTag": "someTagValue"}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/timeseriesinsights/resource-manager/Microsoft.TimeSeriesInsights/preview/2021-03-31-preview/examples/EnvironmentsPatchTags.json
+if __name__ == "__main__":
+    main()
---
+++ specification/timeseriesinsights/resource-manager/Microsoft.TimeSeriesInsights/preview/2021-03-31-preview/examples-python/EventSourcesPatchTags.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.timeseriesinsights import TimeSeriesInsightsClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-timeseriesinsights
+# USAGE
+    python update_event_source.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TimeSeriesInsightsClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.event_sources.update(
+        resource_group_name="rg1",
+        environment_name="env1",
+        event_source_name="es1",
+        event_source_update_parameters={"tags": {"someKey": "someValue"}},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/timeseriesinsights/resource-manager/Microsoft.TimeSeriesInsights/preview/2021-03-31-preview/examples/EventSourcesPatchTags.json
+if __name__ == "__main__":
+    main()

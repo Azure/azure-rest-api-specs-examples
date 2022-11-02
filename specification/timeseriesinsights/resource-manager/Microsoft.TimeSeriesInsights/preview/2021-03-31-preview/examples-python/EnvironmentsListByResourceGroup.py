7c1d8624758437455a70d7b3d30a04146aab00e7
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-02 08:02:45 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-timeseriesinsights_2.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/timeseriesinsights/resource-manager/Microsoft.TimeSeriesInsights/preview/2021-03-31-preview/examples-python/EnvironmentsListByResourceGroup.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.timeseriesinsights import TimeSeriesInsightsClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-timeseriesinsights
+# USAGE
+    python environments_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TimeSeriesInsightsClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.environments.list_by_resource_group(
+        resource_group_name="rg1",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/timeseriesinsights/resource-manager/Microsoft.TimeSeriesInsights/preview/2021-03-31-preview/examples/EnvironmentsListByResourceGroup.json
+if __name__ == "__main__":
+    main()

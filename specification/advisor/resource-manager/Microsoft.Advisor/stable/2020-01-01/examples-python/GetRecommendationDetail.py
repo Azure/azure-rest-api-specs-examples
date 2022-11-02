99c6088f950a502672511a3fe3cde0c6b127d39a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-02 08:01:49 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-advisor_10.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-python/GetRecommendationDetail.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.advisor import AdvisorManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-advisor
+# USAGE
+    python get_recommendation_detail.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AdvisorManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.recommendations.get(
+        resource_uri="resourceUri",
+        recommendation_id="recommendationId",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples/GetRecommendationDetail.json
+if __name__ == "__main__":
+    main()

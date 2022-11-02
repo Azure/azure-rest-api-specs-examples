e8b9550f451a10ab13e9a9419412402267f3b03b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-02 08:02:31 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-privatedns_1.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/privatedns/resource-manager/Microsoft.Network/stable/2020-06-01/examples-python/RecordSetTXTList.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.privatedns import PrivateDnsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-privatedns
+# USAGE
+    python get_private_dns_zone_txt_record_sets.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PrivateDnsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscriptionId",
+    )
+
+    response = client.record_sets.list_by_type(
+        resource_group_name="resourceGroup1",
+        private_zone_name="privatezone1.com",
+        record_type="TXT",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/privatedns/resource-manager/Microsoft.Network/stable/2020-06-01/examples/RecordSetTXTList.json
+if __name__ == "__main__":
+    main()

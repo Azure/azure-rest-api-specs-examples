f150240b87a5e414960cb6d8f5c3af9ae676bc87
azure-sdk <azuresdk@microsoft.com>
2022-11-03 00:19:37 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-serialconsole_1.1.0b1

---
+++ specification/serialconsole/resource-manager/Microsoft.SerialConsole/stable/2018-05-01/examples-python/ListSerialPortSubscription.py
@@ -0,0 +1,30 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.serialconsole import MicrosoftSerialConsoleClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-serialconsole
+# USAGE
+    python list_serial_ports_for_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MicrosoftSerialConsoleClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-00000-0000-0000-000000000000",
+    )
+
+    response = client.serial_ports.list_by_subscriptions()
+    print(response)
+
+
+# x-ms-original-file: specification/serialconsole/resource-manager/Microsoft.SerialConsole/stable/2018-05-01/examples/ListSerialPortSubscription.json
+if __name__ == "__main__":
+    main()

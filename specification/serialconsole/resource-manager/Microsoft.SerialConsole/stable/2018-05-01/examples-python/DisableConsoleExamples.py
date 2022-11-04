89e88c0d146ddb02a94d8c3838c7573bb055f9a9
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-04 08:03:15 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-serialconsole_1.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/serialconsole/resource-manager/Microsoft.SerialConsole/stable/2018-05-01/examples-python/DisableConsoleExamples.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.serialconsole import MicrosoftSerialConsoleClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-serialconsole
+# USAGE
+    python disable_serial_console_for_a_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MicrosoftSerialConsoleClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-00000-0000-0000-000000000000",
+    )
+
+    response = client.disable_console(
+        default="default",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/serialconsole/resource-manager/Microsoft.SerialConsole/stable/2018-05-01/examples/DisableConsoleExamples.json
+if __name__ == "__main__":
+    main()

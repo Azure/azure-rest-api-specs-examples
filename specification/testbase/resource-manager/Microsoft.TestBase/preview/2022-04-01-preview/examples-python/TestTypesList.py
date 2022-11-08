6c36b5a5e7aa341530f3ea7bc4edc15a3e75913a
azure-sdk <azuresdk@microsoft.com>
2022-11-08 00:26:53 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-testbase_1.0.0b2

---
+++ specification/testbase/resource-manager/Microsoft.TestBase/preview/2022-04-01-preview/examples-python/TestTypesList.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.testbase import TestBase
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-testbase
+# USAGE
+    python test_types_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TestBase(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscription-id",
+    )
+
+    response = client.test_types.list(
+        resource_group_name="contoso-rg",
+        test_base_account_name="contoso-testBaseAccount",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/testbase/resource-manager/Microsoft.TestBase/preview/2022-04-01-preview/examples/TestTypesList.json
+if __name__ == "__main__":
+    main()

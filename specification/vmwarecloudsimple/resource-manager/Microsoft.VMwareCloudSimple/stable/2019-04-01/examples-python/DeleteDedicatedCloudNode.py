0831a1717e0b5c4e70626c2f8a26b91833f8a722
azure-sdk <azuresdk@microsoft.com>
2022-11-08 00:26:07 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-vmwarecloudsimple_1.0.0b2

---
+++ specification/vmwarecloudsimple/resource-manager/Microsoft.VMwareCloudSimple/stable/2019-04-01/examples-python/DeleteDedicatedCloudNode.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.vmwarecloudsimple import VMwareCloudSimple
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-vmwarecloudsimple
+# USAGE
+    python delete_dedicated_cloud_node.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = VMwareCloudSimple(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.dedicated_cloud_nodes.delete(
+        resource_group_name="myResourceGroup",
+        dedicated_cloud_node_name="myNode",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/vmwarecloudsimple/resource-manager/Microsoft.VMwareCloudSimple/stable/2019-04-01/examples/DeleteDedicatedCloudNode.json
+if __name__ == "__main__":
+    main()

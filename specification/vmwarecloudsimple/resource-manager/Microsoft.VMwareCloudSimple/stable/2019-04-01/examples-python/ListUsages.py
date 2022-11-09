4a6d7b0fdec64f914c675076394c2c992a68d418
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-09 08:02:29 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-vmwarecloudsimple_1.0.0b2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/vmwarecloudsimple/resource-manager/Microsoft.VMwareCloudSimple/stable/2019-04-01/examples-python/ListUsages.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.vmwarecloudsimple import VMwareCloudSimple
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-vmwarecloudsimple
+# USAGE
+    python list_usages.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = VMwareCloudSimple(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.usages.list(
+        region_id="westus2",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/vmwarecloudsimple/resource-manager/Microsoft.VMwareCloudSimple/stable/2019-04-01/examples/ListUsages.json
+if __name__ == "__main__":
+    main()

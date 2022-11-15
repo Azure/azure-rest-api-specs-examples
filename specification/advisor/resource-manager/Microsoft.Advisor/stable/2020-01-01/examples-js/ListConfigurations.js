c451ae92ce07b487311ec9fc0e60322d026df330
azure-sdk <azuresdk@microsoft.com>
2022-11-15 00:06:10 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-advisor_3.1.0

--- specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-js/ListConfigurations.js
+++ specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-js/ListConfigurations.js
@@ -2,18 +2,17 @@ const { AdvisorManagementClient } = require("@azure/arm-advisor");
 const { DefaultAzureCredential } = require("@azure/identity");
 
 /**
- * This sample demonstrates how to Retrieve Azure Advisor configurations.
+ * This sample demonstrates how to Retrieve Azure Advisor configurations and also retrieve configurations of contained resource groups.
  *
- * @summary Retrieve Azure Advisor configurations.
+ * @summary Retrieve Azure Advisor configurations and also retrieve configurations of contained resource groups.
  * x-ms-original-file: specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples/ListConfigurations.json
  */
 async function getConfigurations() {
   const subscriptionId = "subscriptionId";
-  const resourceGroup = "resourceGroup";
   const credential = new DefaultAzureCredential();
   const client = new AdvisorManagementClient(credential, subscriptionId);
   const resArray = new Array();
-  for await (let item of client.configurations.listByResourceGroup(resourceGroup)) {
+  for await (let item of client.configurations.listBySubscription()) {
     resArray.push(item);
   }
   console.log(resArray);

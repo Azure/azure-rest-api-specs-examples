6bc2328048142ba442554d6724270bb3df2f4507
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-23 08:03:42 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_17.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-09-01/examples-js/ContainerServiceGetOSOptions.js
@@ -0,0 +1,19 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets supported OS options in the specified subscription.
+ *
+ * @summary Gets supported OS options in the specified subscription.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-09-01/examples/ContainerServiceGetOSOptions.json
+ */
+async function getContainerServiceOSOptions() {
+  const subscriptionId = "subid1";
+  const location = "location1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.getOSOptions(location);
+  console.log(result);
+}
+
+getContainerServiceOSOptions().catch(console.error);
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-09-01/examples-js/ManagedClustersResetAADProfile.js
@@ -0,0 +1,30 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Reset the AAD Profile of a managed cluster.
+ *
+ * @summary Reset the AAD Profile of a managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-09-01/examples/ManagedClustersResetAADProfile.json
+ */
+async function resetAadProfile() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const resourceName = "clustername1";
+  const parameters = {
+    clientAppID: "clientappid",
+    serverAppID: "serverappid",
+    serverAppSecret: "serverappsecret",
+    tenantID: "tenantid",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginResetAADProfileAndWait(
+    resourceGroupName,
+    resourceName,
+    parameters
+  );
+  console.log(result);
+}
+
+resetAadProfile().catch(console.error);

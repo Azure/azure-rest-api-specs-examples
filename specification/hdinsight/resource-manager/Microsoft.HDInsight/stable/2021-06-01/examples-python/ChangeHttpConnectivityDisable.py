7ad1d611aab4c2ffc25fc8b01ba8673fa2ed3e81
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-23 08:02:30 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-hdinsight_9.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/stable/2021-06-01/examples-python/ChangeHttpConnectivityDisable.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.hdinsight import HDInsightManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hdinsight
+# USAGE
+    python change_http_connectivity_disable.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HDInsightManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.configurations.begin_update(
+        resource_group_name="rg1",
+        cluster_name="cluster1",
+        configuration_name="gateway",
+        parameters={"restAuthCredential.isEnabled": "false"},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/stable/2021-06-01/examples/ChangeHttpConnectivityDisable.json
+if __name__ == "__main__":
+    main()

adda84bcec18c89fdaaa1f4d2dd6e9be9236d15d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-23 08:02:23 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-datamigration_10.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2022-03-30-preview/examples-python/Services_Delete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.datamigration import DataMigrationManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-datamigration
+# USAGE
+    python services_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataMigrationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="fc04246f-04c5-437e-ac5e-206a19e7193f",
+    )
+
+    response = client.services.begin_delete(
+        group_name="DmsSdkRg",
+        service_name="DmsSdkService",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2022-03-30-preview/examples/Services_Delete.json
+if __name__ == "__main__":
+    main()
---
+++ specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2022-03-30-preview/examples-python/Services_List.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.datamigration import DataMigrationManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-datamigration
+# USAGE
+    python services_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataMigrationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="fc04246f-04c5-437e-ac5e-206a19e7193f",
+    )
+
+    response = client.services.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2022-03-30-preview/examples/Services_List.json
+if __name__ == "__main__":
+    main()

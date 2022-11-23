1794088c1245487a305bc315bc0733afaa756401
azure-sdk <azuresdk@microsoft.com>
2022-11-23 00:14:55 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-signalr_5.2.0-beta.1

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples-js/SignalRCustomCertificates_Get.js
@@ -0,0 +1,25 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get a custom certificate.
+ *
+ * @summary Get a custom certificate.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples/SignalRCustomCertificates_Get.json
+ */
+async function signalRCustomCertificatesGet() {
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const certificateName = "myCert";
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const result = await client.signalRCustomCertificates.get(
+    resourceGroupName,
+    resourceName,
+    certificateName
+  );
+  console.log(result);
+}
+
+signalRCustomCertificatesGet().catch(console.error);
---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples-js/SignalRCustomDomains_List.js
@@ -0,0 +1,23 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List all custom domains.
+ *
+ * @summary List all custom domains.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples/SignalRCustomDomains_List.json
+ */
+async function signalRCustomDomainsList() {
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.signalRCustomDomains.list(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+signalRCustomDomainsList().catch(console.error);

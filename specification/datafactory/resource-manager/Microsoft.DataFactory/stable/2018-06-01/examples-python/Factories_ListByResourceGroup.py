19794477631ef71570404210db5a69902d113051
azure-sdk <azuresdk@microsoft.com>
2022-11-23 00:22:57 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-datafactory_2.10.0

---
+++ specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples-python/Factories_ListByResourceGroup.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.datafactory import DataFactoryManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-datafactory
+# USAGE
+    python factories_list_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataFactoryManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="12345678-1234-1234-1234-12345678abc",
+    )
+
+    response = client.factories.list_by_resource_group(
+        resource_group_name="exampleResourceGroup",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples/Factories_ListByResourceGroup.json
+if __name__ == "__main__":
+    main()

0a0d488aad62d11bc20f1834950d6b411abcb1dc
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-24 08:02:05 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_17.2.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-09-02-preview/examples-js/FleetMembers_Get.js
@@ -0,0 +1,21 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a Fleet member.
+ *
+ * @summary Gets a Fleet member.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/preview/2022-09-02-preview/examples/FleetMembers_Get.json
+ */
+async function getsAFleetMemberResource() {
+  const subscriptionId = "subid1";
+  const resourceGroupName = "rg1";
+  const fleetName = "fleet-1";
+  const fleetMemberName = "member-1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.fleetMembers.get(resourceGroupName, fleetName, fleetMemberName);
+  console.log(result);
+}
+
+getsAFleetMemberResource().catch(console.error);

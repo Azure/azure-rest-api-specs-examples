18dcd38a150465ec5c4d051941b37d0f1067292a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-24 08:01:58 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-datafactory_2.10.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples-python/Factories_GetDataPlaneAccess.py
@@ -0,0 +1,40 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.datafactory import DataFactoryManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-datafactory
+# USAGE
+    python factories_get_data_plane_access.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataFactoryManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="12345678-1234-1234-1234-12345678abc",
+    )
+
+    response = client.factories.get_data_plane_access(
+        resource_group_name="exampleResourceGroup",
+        factory_name="exampleFactoryName",
+        policy={
+            "accessResourcePath": "",
+            "expireTime": "2018-11-10T09:46:20.2659347Z",
+            "permissions": "r",
+            "profileName": "DefaultProfile",
+            "startTime": "2018-11-10T02:46:20.2659347Z",
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples/Factories_GetDataPlaneAccess.json
+if __name__ == "__main__":
+    main()
---
+++ specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples-python/Triggers_Create.py
@@ -0,0 +1,54 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.datafactory import DataFactoryManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-datafactory
+# USAGE
+    python triggers_create.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataFactoryManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="12345678-1234-1234-1234-12345678abc",
+    )
+
+    response = client.triggers.create_or_update(
+        resource_group_name="exampleResourceGroup",
+        factory_name="exampleFactoryName",
+        trigger_name="exampleTrigger",
+        trigger={
+            "properties": {
+                "pipelines": [
+                    {
+                        "parameters": {"OutputBlobNameList": ["exampleoutput.csv"]},
+                        "pipelineReference": {"referenceName": "examplePipeline", "type": "PipelineReference"},
+                    }
+                ],
+                "type": "ScheduleTrigger",
+                "typeProperties": {
+                    "recurrence": {
+                        "endTime": "2018-06-16T00:55:13.8441801Z",
+                        "frequency": "Minute",
+                        "interval": 4,
+                        "startTime": "2018-06-16T00:39:13.8441801Z",
+                        "timeZone": "UTC",
+                    }
+                },
+            }
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples/Triggers_Create.json
+if __name__ == "__main__":
+    main()

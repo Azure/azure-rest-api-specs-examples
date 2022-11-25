709de6e0fe0d3030875347cca8317e7ff9db2513
azure-sdk <azuresdk@microsoft.com>
2022-11-25 00:53:58 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/signalr/armsignalr/v1.1.0-beta.1

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples-go/SignalRCustomCertificates_CreateOrUpdate.go
@@ -0,0 +1,39 @@
+package armsignalr_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/signalr/armsignalr"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/signalr/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples/SignalRCustomCertificates_CreateOrUpdate.json
+func ExampleCustomCertificatesClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armsignalr.NewCustomCertificatesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "myResourceGroup", "mySignalRService", "myCert", armsignalr.CustomCertificate{
+		Properties: &armsignalr.CustomCertificateProperties{
+			KeyVaultBaseURI:       to.Ptr("https://myvault.keyvault.azure.net/"),
+			KeyVaultSecretName:    to.Ptr("mycert"),
+			KeyVaultSecretVersion: to.Ptr("bb6a44b2743f47f68dad0d6cc9756432"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

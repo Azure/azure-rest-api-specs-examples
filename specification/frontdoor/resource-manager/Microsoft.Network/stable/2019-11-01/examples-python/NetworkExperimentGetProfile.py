d1247d6288437d06d90322ab890b05cc42524f36
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-25 08:02:04 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-frontdoor_1.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/frontdoor/resource-manager/Microsoft.Network/stable/2019-11-01/examples-python/NetworkExperimentGetProfile.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.frontdoor import FrontDoorManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-frontdoor
+# USAGE
+    python network_experiment_get_profile.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = FrontDoorManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.network_experiment_profiles.get(
+        resource_group_name="MyResourceGroup",
+        profile_name="MyProfile",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/frontdoor/resource-manager/Microsoft.Network/stable/2019-11-01/examples/NetworkExperimentGetProfile.json
+if __name__ == "__main__":
+    main()

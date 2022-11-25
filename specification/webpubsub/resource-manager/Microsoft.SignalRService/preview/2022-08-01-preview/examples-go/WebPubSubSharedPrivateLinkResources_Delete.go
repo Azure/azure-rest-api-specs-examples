1fc111781054deb5d965c2ed77af75818f96d42b
azure-sdk <azuresdk@microsoft.com>
2022-11-25 00:53:01 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/webpubsub/armwebpubsub/v1.1.0-beta.1

---
+++ specification/webpubsub/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples-go/WebPubSubSharedPrivateLinkResources_Delete.go
@@ -0,0 +1,30 @@
+package armwebpubsub_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/webpubsub/armwebpubsub"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/webpubsub/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples/WebPubSubSharedPrivateLinkResources_Delete.json
+func ExampleSharedPrivateLinkResourcesClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armwebpubsub.NewSharedPrivateLinkResourcesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginDelete(ctx, "upstream", "myResourceGroup", "myWebPubSubService", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}
---
+++ specification/webpubsub/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples-go/WebPubSub_CheckNameAvailability.go
@@ -0,0 +1,32 @@
+package armwebpubsub_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/webpubsub/armwebpubsub"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/webpubsub/resource-manager/Microsoft.SignalRService/preview/2022-08-01-preview/examples/WebPubSub_CheckNameAvailability.json
+func ExampleClient_CheckNameAvailability() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armwebpubsub.NewClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.CheckNameAvailability(ctx, "eastus", armwebpubsub.NameAvailabilityParameters{
+		Name: to.Ptr("myWebPubSubService"),
+		Type: to.Ptr("Microsoft.SignalRService/WebPubSub"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

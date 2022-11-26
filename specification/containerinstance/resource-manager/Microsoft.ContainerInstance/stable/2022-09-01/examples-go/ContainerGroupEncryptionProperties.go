6d0c44c1ce288bf77f3cd183183e8a50534afb33
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-26 08:03:20 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerinstance/armcontainerinstance/v2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2022-09-01/examples-go/ContainerGroupEncryptionProperties.go
@@ -0,0 +1,78 @@
+package armcontainerinstance_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerinstance/armcontainerinstance/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2022-09-01/examples/ContainerGroupEncryptionProperties.json
+func ExampleContainerGroupsClient_BeginCreateOrUpdate_containerGroupWithEncryptionProperties() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcontainerinstance.NewContainerGroupsClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "demo", "demo1", armcontainerinstance.ContainerGroup{
+		Identity: &armcontainerinstance.ContainerGroupIdentity{
+			Type: to.Ptr(armcontainerinstance.ResourceIdentityTypeUserAssigned),
+			UserAssignedIdentities: map[string]*armcontainerinstance.UserAssignedIdentities{
+				"/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/container-group-identity": {},
+			},
+		},
+		Properties: &armcontainerinstance.ContainerGroupPropertiesProperties{
+			Containers: []*armcontainerinstance.Container{
+				{
+					Name: to.Ptr("demo1"),
+					Properties: &armcontainerinstance.ContainerProperties{
+						Command:              []*string{},
+						EnvironmentVariables: []*armcontainerinstance.EnvironmentVariable{},
+						Image:                to.Ptr("nginx"),
+						Ports: []*armcontainerinstance.ContainerPort{
+							{
+								Port: to.Ptr[int32](80),
+							}},
+						Resources: &armcontainerinstance.ResourceRequirements{
+							Requests: &armcontainerinstance.ResourceRequests{
+								CPU:        to.Ptr[float64](1),
+								MemoryInGB: to.Ptr[float64](1.5),
+							},
+						},
+					},
+				}},
+			EncryptionProperties: &armcontainerinstance.EncryptionProperties{
+				Identity:     to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/container-group-identity"),
+				KeyName:      to.Ptr("test-key"),
+				KeyVersion:   to.Ptr("<key version>"),
+				VaultBaseURL: to.Ptr("https://testkeyvault.vault.azure.net"),
+			},
+			ImageRegistryCredentials: []*armcontainerinstance.ImageRegistryCredential{},
+			IPAddress: &armcontainerinstance.IPAddress{
+				Type: to.Ptr(armcontainerinstance.ContainerGroupIPAddressTypePublic),
+				Ports: []*armcontainerinstance.Port{
+					{
+						Port:     to.Ptr[int32](80),
+						Protocol: to.Ptr(armcontainerinstance.ContainerGroupNetworkProtocolTCP),
+					}},
+			},
+			OSType: to.Ptr(armcontainerinstance.OperatingSystemTypesLinux),
+		},
+		Location: to.Ptr("eastus2"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}
---
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2022-09-01/examples-go/ContainerGroupsGet_Failed.go
@@ -0,0 +1,28 @@
+package armcontainerinstance_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerinstance/armcontainerinstance/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2022-09-01/examples/ContainerGroupsGet_Failed.json
+func ExampleContainerGroupsClient_Get_containerGroupsGetFailed() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcontainerinstance.NewContainerGroupsClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.Get(ctx, "demo", "demo1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

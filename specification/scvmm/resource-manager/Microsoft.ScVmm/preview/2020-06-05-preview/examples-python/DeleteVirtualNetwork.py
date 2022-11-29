fd544f377af5d2225f65b5014e2e8dd767b5bdbd
azure-sdk <azuresdk@microsoft.com>
2022-11-29 05:02:24 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-scvmm_1.0.0b2

---
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples-python/DeleteVirtualNetwork.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.scvmm import SCVMM
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-scvmm
+# USAGE
+    python delete_virtual_network.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SCVMM(
+        credential=DefaultAzureCredential(),
+        subscription_id="fd3c3665-1729-4b7b-9a38-238e83b0f98b",
+    )
+
+    response = client.virtual_networks.begin_delete(
+        resource_group_name="testrg",
+        virtual_network_name="HRVirtualNetwork",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples/DeleteVirtualNetwork.json
+if __name__ == "__main__":
+    main()

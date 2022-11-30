5409fbcd20c82321348aec9e33d26496a2ee8fec
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-11-30 08:02:03 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-communication_2.0.0b2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2022-07-01-preview/examples-python/communicationServices/regenerateKey.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.communication import CommunicationServiceManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-communication
+# USAGE
+    python regenerate_key.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CommunicationServiceManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="12345",
+    )
+
+    response = client.communication_services.begin_regenerate_key(
+        resource_group_name="MyResourceGroup",
+        communication_service_name="MyCommunicationResource",
+        parameters={"keyType": "Primary"},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/communication/resource-manager/Microsoft.Communication/preview/2022-07-01-preview/examples/communicationServices/regenerateKey.json
+if __name__ == "__main__":
+    main()

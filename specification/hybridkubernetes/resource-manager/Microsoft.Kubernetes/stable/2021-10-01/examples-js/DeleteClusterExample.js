7687c170a7e1cf1f467a00275af9f8ac9da0a8cf
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-01 08:02:38 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-hybridkubernetes_2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-js/DeleteClusterExample.js
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-js/DeleteClusterExample.js
@@ -1,6 +1,12 @@
 const { ConnectedKubernetesClient } = require("@azure/arm-hybridkubernetes");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Delete a connected cluster, removing the tracked resource in Azure Resource Manager (ARM).
+ *
+ * @summary Delete a connected cluster, removing the tracked resource in Azure Resource Manager (ARM).
+ * x-ms-original-file: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/DeleteClusterExample.json
+ */
 async function deleteClusterExample() {
   const subscriptionId = "1bfbb5d0-917e-4346-9026-1d3b344417f5";
   const resourceGroupName = "k8sc-rg";

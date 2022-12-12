bc315196aa73895c71085b06db0a20551ca78e04
azure-sdk <azuresdk@microsoft.com>
2022-12-12 12:23:22 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-automation_1.1.0b3

---
+++ specification/automation/resource-manager/Microsoft.Automation/stable/2022-08-08/examples-python/createOrUpdateCertificate.py
@@ -0,0 +1,43 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.automation import AutomationClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-automation
+# USAGE
+    python create_or_update_certificate.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AutomationClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.certificate.create_or_update(
+        resource_group_name="rg",
+        automation_account_name="myAutomationAccount18",
+        certificate_name="testCert",
+        parameters={
+            "name": "testCert",
+            "properties": {
+                "base64Value": "base 64 value of cert",
+                "description": "Sample Cert",
+                "isExportable": False,
+                "thumbprint": "thumbprint of cert",
+            },
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/automation/resource-manager/Microsoft.Automation/stable/2022-08-08/examples/createOrUpdateCertificate.json
+if __name__ == "__main__":
+    main()
---
+++ specification/automation/resource-manager/Microsoft.Automation/stable/2022-08-08/examples-python/listActivitiesByModule.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.automation import AutomationClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-automation
+# USAGE
+    python list_activities_by_module.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AutomationClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.activity.list_by_module(
+        resource_group_name="rg",
+        automation_account_name="myAutomationAccount33",
+        module_name="OmsCompositeResources",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/automation/resource-manager/Microsoft.Automation/stable/2022-08-08/examples/listActivitiesByModule.json
+if __name__ == "__main__":
+    main()

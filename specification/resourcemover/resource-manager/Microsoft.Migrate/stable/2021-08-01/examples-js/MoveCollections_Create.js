c92da9d153eb699bf0b53d11ad78677cdc526f3a
azure-sdk <azuresdk@microsoft.com>
2022-12-13 12:04:27 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-resourcemover_2.1.0

--- specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples-js/MoveCollections_Create.js
+++ specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples-js/MoveCollections_Create.js
@@ -1,6 +1,12 @@
 const { ResourceMoverServiceAPI } = require("@azure/arm-resourcemover");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Creates or updates a move collection.
+ *
+ * @summary Creates or updates a move collection.
+ * x-ms-original-file: specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples/MoveCollections_Create.json
+ */
 async function moveCollectionsCreate() {
   const subscriptionId = "subid";
   const resourceGroupName = "rg1";
@@ -10,7 +16,7 @@ async function moveCollectionsCreate() {
     location: "eastus2",
     properties: { sourceRegion: "eastus", targetRegion: "westus" },
   };
-  const options = { body: body };
+  const options = { body };
   const credential = new DefaultAzureCredential();
   const client = new ResourceMoverServiceAPI(credential, subscriptionId);
   const result = await client.moveCollections.create(
--- specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples-js/MoveCollections_Discard.js
+++ specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples-js/MoveCollections_Discard.js
@@ -1,6 +1,12 @@
 const { ResourceMoverServiceAPI } = require("@azure/arm-resourcemover");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+/**
+ * This sample demonstrates how to Discards the set of resources included in the request body. The discard operation is triggered on the moveResources in the moveState 'CommitPending' or 'DiscardFailed', on a successful completion the moveResource moveState do a transition to MovePending. To aid the user to prerequisite the operation the client can call operation with validateOnly property set to true.
+ *
+ * @summary Discards the set of resources included in the request body. The discard operation is triggered on the moveResources in the moveState 'CommitPending' or 'DiscardFailed', on a successful completion the moveResource moveState do a transition to MovePending. To aid the user to prerequisite the operation the client can call operation with validateOnly property set to true.
+ * x-ms-original-file: specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples/MoveCollections_Discard.json
+ */
 async function moveCollectionsDiscard() {
   const subscriptionId = "subid";
   const resourceGroupName = "rg1";
@@ -11,7 +17,7 @@ async function moveCollectionsDiscard() {
     ],
     validateOnly: false,
   };
-  const options = { body: body };
+  const options = { body };
   const credential = new DefaultAzureCredential();
   const client = new ResourceMoverServiceAPI(credential, subscriptionId);
   const result = await client.moveCollections.beginDiscardAndWait(

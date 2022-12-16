75967a11542717f6ba84200fcf5e0b08b43ba5ea
azure-sdk <azuresdk@microsoft.com>
2022-12-16 12:29:57 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-automanage_2.0.0b1

---
+++ specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-python/listConfigurationProfilesByResourceGroup.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.automanage import AutomanageClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-automanage
+# USAGE
+    python list_configuration_profiles_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AutomanageClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="mySubscriptionId",
+    )
+
+    response = client.configuration_profiles.list_by_resource_group(
+        resource_group_name="myResourceGroupName",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples/listConfigurationProfilesByResourceGroup.json
+if __name__ == "__main__":
+    main()

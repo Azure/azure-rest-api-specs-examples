572daaf59f314c1350daa385cda0071094b2935c
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-17 08:02:27 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-fluidrelay_1.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/fluidrelay/resource-manager/Microsoft.FluidRelay/stable/2022-06-01/examples-python/FluidRelayServers_ListKeys.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.fluidrelay import FluidRelayManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-fluidrelay
+# USAGE
+    python fluid_relay_servers_list_keys.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = FluidRelayManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="xxxx-xxxx-xxxx-xxxx",
+    )
+
+    response = client.fluid_relay_servers.list_keys(
+        resource_group="myResourceGroup",
+        fluid_relay_server_name="myFluidRelayServer",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/fluidrelay/resource-manager/Microsoft.FluidRelay/stable/2022-06-01/examples/FluidRelayServers_ListKeys.json
+if __name__ == "__main__":
+    main()

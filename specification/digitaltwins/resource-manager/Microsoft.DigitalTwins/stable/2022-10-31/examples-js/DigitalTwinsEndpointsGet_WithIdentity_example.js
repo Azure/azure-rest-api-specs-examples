a1922e8085b0b0ade65ed9317c61d2981425f66d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-19 20:16:08 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-digitaltwins_3.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-js/DigitalTwinsEndpointsGet_WithIdentity_example.js
@@ -0,0 +1,23 @@
+const { AzureDigitalTwinsManagementClient } = require("@azure/arm-digitaltwins");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get DigitalTwinsInstance Endpoints.
+ *
+ * @summary Get DigitalTwinsInstance Endpoints.
+ * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsEndpointsGet_WithIdentity_example.json
+ */
+async function getADigitalTwinsInstanceEndpointsWithIdentity() {
+  const subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+  const resourceGroupName = "resRg";
+  const resourceName = "myDigitalTwinsService";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureDigitalTwinsManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.digitalTwinsEndpoint.list(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+getADigitalTwinsInstanceEndpointsWithIdentity().catch(console.error);

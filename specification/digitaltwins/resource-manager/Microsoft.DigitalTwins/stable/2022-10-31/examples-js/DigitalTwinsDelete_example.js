fd15fcd0115ed4945f51d3a9a328ca60c47de7a8
azure-sdk <azuresdk@microsoft.com>
2022-12-19 11:58:53 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-digitaltwins_3.2.0

---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-js/DigitalTwinsDelete_example.js
@@ -0,0 +1,20 @@
+const { AzureDigitalTwinsManagementClient } = require("@azure/arm-digitaltwins");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Delete a DigitalTwinsInstance.
+ *
+ * @summary Delete a DigitalTwinsInstance.
+ * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsDelete_example.json
+ */
+async function deleteADigitalTwinsInstanceResource() {
+  const subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+  const resourceGroupName = "resRg";
+  const resourceName = "myDigitalTwinsService";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureDigitalTwinsManagementClient(credential, subscriptionId);
+  const result = await client.digitalTwins.beginDeleteAndWait(resourceGroupName, resourceName);
+  console.log(result);
+}
+
+deleteADigitalTwinsInstanceResource().catch(console.error);
---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-js/PrivateLinkResourcesList_example.js
@@ -0,0 +1,20 @@
+const { AzureDigitalTwinsManagementClient } = require("@azure/arm-digitaltwins");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List private link resources for given Digital Twin.
+ *
+ * @summary List private link resources for given Digital Twin.
+ * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/PrivateLinkResourcesList_example.json
+ */
+async function listPrivateLinkResourcesForGivenDigitalTwin() {
+  const subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+  const resourceGroupName = "resRg";
+  const resourceName = "myDigitalTwinsService";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureDigitalTwinsManagementClient(credential, subscriptionId);
+  const result = await client.privateLinkResources.list(resourceGroupName, resourceName);
+  console.log(result);
+}
+
+listPrivateLinkResourcesForGivenDigitalTwin().catch(console.error);

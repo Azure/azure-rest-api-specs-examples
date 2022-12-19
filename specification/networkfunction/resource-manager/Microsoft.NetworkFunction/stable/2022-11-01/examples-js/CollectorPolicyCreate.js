8e0d55b0f95b6f9492d08e8700844e397a0242df
azure-sdk <azuresdk@microsoft.com>
2022-12-19 11:50:15 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-networkfunction_2.0.0

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-js/CollectorPolicyCreate.js
@@ -0,0 +1,27 @@
+const { AzureTrafficCollectorClient } = require("@azure/arm-networkfunction");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates a Collector Policy resource
+ *
+ * @summary Creates or updates a Collector Policy resource
+ * x-ms-original-file: specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/CollectorPolicyCreate.json
+ */
+async function createACollectionPolicy() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "rg1";
+  const azureTrafficCollectorName = "atc";
+  const collectorPolicyName = "cp1";
+  const location = "West US";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureTrafficCollectorClient(credential, subscriptionId);
+  const result = await client.collectorPolicies.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    azureTrafficCollectorName,
+    collectorPolicyName,
+    location
+  );
+  console.log(result);
+}
+
+createACollectionPolicy().catch(console.error);

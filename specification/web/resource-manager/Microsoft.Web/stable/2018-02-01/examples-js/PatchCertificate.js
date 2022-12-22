2baee8fd2091c9ffdd45f8113d7191864fbd4a96
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-22 08:01:58 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-appservice-profile-2020-09-01-hybrid_2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/web/resource-manager/Microsoft.Web/stable/2018-02-01/examples-js/PatchCertificate.js
@@ -0,0 +1,25 @@
+const { WebSiteManagementClient } = require("@azure/arm-appservice-profile-2020-09-01-hybrid");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Create or update a certificate.
+ *
+ * @summary Create or update a certificate.
+ * x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2018-02-01/examples/PatchCertificate.json
+ */
+async function patchCertificate() {
+  const subscriptionId =
+    process.env["APPSERVICE_SUBSCRIPTION_ID"] || "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
+  const resourceGroupName = process.env["APPSERVICE_RESOURCE_GROUP"] || "testrg123";
+  const name = "testc6282";
+  const certificateEnvelope = {
+    name: "testc6282",
+    type: "Microsoft.Web/certificates",
+    id: "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/testrg123/providers/Microsoft.Web/certificates/testc6282",
+    password: "<password>",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new WebSiteManagementClient(credential, subscriptionId);
+  const result = await client.certificates.update(resourceGroupName, name, certificateEnvelope);
+  console.log(result);
+}
---
+++ specification/web/resource-manager/Microsoft.Web/stable/2018-02-01/examples-js/StopWebSiteNetworkTrace.js
@@ -0,0 +1,20 @@
+const { WebSiteManagementClient } = require("@azure/arm-appservice-profile-2020-09-01-hybrid");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Stop ongoing capturing network packets for the site.
+ *
+ * @summary Stop ongoing capturing network packets for the site.
+ * x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2018-02-01/examples/StopWebSiteNetworkTrace.json
+ */
+async function stopACurrentlyRunningNetworkTraceOperationForASite() {
+  const subscriptionId =
+    process.env["APPSERVICE_SUBSCRIPTION_ID"] || "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
+  const resourceGroupName = process.env["APPSERVICE_RESOURCE_GROUP"] || "testrg123";
+  const name = "SampleApp";
+  const slot = "Production";
+  const credential = new DefaultAzureCredential();
+  const client = new WebSiteManagementClient(credential, subscriptionId);
+  const result = await client.webApps.stopNetworkTraceSlot(resourceGroupName, name, slot);
+  console.log(result);
+}

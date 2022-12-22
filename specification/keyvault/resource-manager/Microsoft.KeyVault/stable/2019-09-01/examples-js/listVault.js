6ba491fe6df9ff1373d2b964550879b7bdd6b844
azure-sdk <azuresdk@microsoft.com>
2022-12-22 12:12:14 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-keyvault-profile-2020-09-01-hybrid_2.1.0

---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2019-09-01/examples-js/listVault.js
@@ -0,0 +1,22 @@
+const { KeyVaultManagementClient } = require("@azure/arm-keyvault-profile-2020-09-01-hybrid");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to The List operation gets information about the vaults associated with the subscription.
+ *
+ * @summary The List operation gets information about the vaults associated with the subscription.
+ * x-ms-original-file: specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2019-09-01/examples/listVault.json
+ */
+async function listVaultsInTheSpecifiedSubscription() {
+  const subscriptionId =
+    process.env["KEYVAULT_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const top = 1;
+  const options = { top };
+  const credential = new DefaultAzureCredential();
+  const client = new KeyVaultManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.vaults.list(options)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

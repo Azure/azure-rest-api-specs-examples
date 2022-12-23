ad879a119f5b5dd11206ea1ddb55af56ea5879d3
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-23 08:01:59 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-keyvault-profile-2020-09-01-hybrid_2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2019-09-01/examples-js/createVault.js
@@ -0,0 +1,76 @@
+const { KeyVaultManagementClient } = require("@azure/arm-keyvault-profile-2020-09-01-hybrid");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Create or update a key vault in the specified subscription.
+ *
+ * @summary Create or update a key vault in the specified subscription.
+ * x-ms-original-file: specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2019-09-01/examples/createVault.json
+ */
+async function createANewVaultOrUpdateAnExistingVault() {
+  const subscriptionId =
+    process.env["KEYVAULT_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["KEYVAULT_RESOURCE_GROUP"] || "sample-resource-group";
+  const vaultName = "sample-vault";
+  const parameters = {
+    location: "westus",
+    properties: {
+      accessPolicies: [
+        {
+          objectId: "00000000-0000-0000-0000-000000000000",
+          permissions: {
+            certificates: [
+              "get",
+              "list",
+              "delete",
+              "create",
+              "import",
+              "update",
+              "managecontacts",
+              "getissuers",
+              "listissuers",
+              "setissuers",
+              "deleteissuers",
+              "manageissuers",
+              "recover",
+              "purge",
+            ],
+            keys: [
+              "encrypt",
+              "decrypt",
+              "wrapKey",
+              "unwrapKey",
+              "sign",
+              "verify",
+              "get",
+              "list",
+              "create",
+              "update",
+              "import",
+              "delete",
+              "backup",
+              "restore",
+              "recover",
+              "purge",
+            ],
+            secrets: ["get", "list", "set", "delete", "backup", "restore", "recover", "purge"],
+          },
+          tenantId: "00000000-0000-0000-0000-000000000000",
+        },
+      ],
+      enabledForDeployment: true,
+      enabledForDiskEncryption: true,
+      enabledForTemplateDeployment: true,
+      sku: { name: "standard", family: "A" },
+      tenantId: "00000000-0000-0000-0000-000000000000",
+    },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new KeyVaultManagementClient(credential, subscriptionId);
+  const result = await client.vaults.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    vaultName,
+    parameters
+  );
+  console.log(result);
+}

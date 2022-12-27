8c76070f312b1abd8eb2c100a871f8f25609184b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-27 08:01:54 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/networkfunction/armnetworkfunction/v2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-go/CollectorPolicyCreate.go
@@ -0,0 +1,53 @@
+package armnetworkfunction_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/networkfunction/armnetworkfunction/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/CollectorPolicyCreate.json
+func ExampleCollectorPoliciesClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnetworkfunction.NewCollectorPoliciesClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "rg1", "atc", "cp1", armnetworkfunction.CollectorPolicy{
+		Location: to.Ptr("West US"),
+		Properties: &armnetworkfunction.CollectorPolicyPropertiesFormat{
+			EmissionPolicies: []*armnetworkfunction.EmissionPoliciesPropertiesFormat{
+				{
+					EmissionDestinations: []*armnetworkfunction.EmissionPolicyDestination{
+						{
+							DestinationType: to.Ptr(armnetworkfunction.DestinationTypeAzureMonitor),
+						}},
+					EmissionType: to.Ptr(armnetworkfunction.EmissionTypeIPFIX),
+				}},
+			IngestionPolicy: &armnetworkfunction.IngestionPolicyPropertiesFormat{
+				IngestionSources: []*armnetworkfunction.IngestionSourcesPropertiesFormat{
+					{
+						ResourceID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/expressRouteCircuits/circuitName"),
+						SourceType: to.Ptr(armnetworkfunction.SourceTypeResource),
+					}},
+				IngestionType: to.Ptr(armnetworkfunction.IngestionTypeIPFIX),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}
---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-go/CollectorPolicyUpdateTags.go
@@ -0,0 +1,34 @@
+package armnetworkfunction_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/networkfunction/armnetworkfunction/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/CollectorPolicyUpdateTags.json
+func ExampleCollectorPoliciesClient_UpdateTags() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armnetworkfunction.NewCollectorPoliciesClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.UpdateTags(ctx, "rg1", "atc", "cp1", armnetworkfunction.TagsObject{
+		Tags: map[string]*string{
+			"key1": to.Ptr("value1"),
+			"key2": to.Ptr("value2"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

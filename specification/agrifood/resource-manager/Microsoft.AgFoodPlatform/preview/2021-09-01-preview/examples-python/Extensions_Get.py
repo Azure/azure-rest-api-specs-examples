7ce02934d16ef6c88ef9b06d60a0e283d78034da
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-28 08:02:06 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-agrifood_1.0.0b3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/agrifood/resource-manager/Microsoft.AgFoodPlatform/preview/2021-09-01-preview/examples-python/Extensions_Get.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.agrifood import AgriFoodMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-agrifood
+# USAGE
+    python extensions_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AgriFoodMgmtClient(
+        credential=DefaultAzureCredential(),
+        solution_id="SOLUTION_ID",
+        subscription_id="11111111-2222-3333-4444-555555555555",
+    )
+
+    response = client.extensions.get(
+        resource_group_name="examples-rg",
+        farm_beats_resource_name="examples-farmbeatsResourceName",
+        extension_id="provider.extension",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/agrifood/resource-manager/Microsoft.AgFoodPlatform/preview/2021-09-01-preview/examples/Extensions_Get.json
+if __name__ == "__main__":
+    main()

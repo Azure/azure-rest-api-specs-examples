92f78a5c9d4d0c6f65c5d54e2f91dba0f90c1f56
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2022-12-29 08:02:11 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-dnsresolver_1.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dnsresolver/resource-manager/Microsoft.Network/stable/2022-07-01/examples-python/VirtualNetworkLink_List.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.dnsresolver import DnsResolverManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dnsresolver
+# USAGE
+    python virtual_network_link_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DnsResolverManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="abdd4249-9f34-4cc6-8e42-c2e32110603e",
+    )
+
+    response = client.virtual_network_links.list(
+        resource_group_name="sampleResourceGroup",
+        dns_forwarding_ruleset_name="sampleDnsForwardingRuleset",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/dnsresolver/resource-manager/Microsoft.Network/stable/2022-07-01/examples/VirtualNetworkLink_List.json
+if __name__ == "__main__":
+    main()

df2ecf7a6041f5cc59da1d06f04b7a633bb8ed9c
azure-sdk <azuresdk@microsoft.com>
2022-12-30 12:19:41 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-policyinsights_1.1.0b4

---
+++ specification/policyinsights/resource-manager/Microsoft.PolicyInsights/stable/2021-10-01/examples-python/Remediations_GetManagementGroupScope.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.policyinsights import PolicyInsightsClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-policyinsights
+# USAGE
+    python remediations_get_management_group_scope.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PolicyInsightsClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.remediations.get_at_management_group(
+        management_group_id="financeMg",
+        remediation_name="storageRemediation",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/policyinsights/resource-manager/Microsoft.PolicyInsights/stable/2021-10-01/examples/Remediations_GetManagementGroupScope.json
+if __name__ == "__main__":
+    main()

7e4000e0e93ad8b7a91b7c44757f9eadc0674b34
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-07 08:02:36 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-deviceprovisioningservices_5.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/stable/2022-02-05/examples-js/DPSGet.js
@@ -0,0 +1,21 @@
+const { IotDpsClient } = require("@azure/arm-deviceprovisioningservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get the metadata of the provisioning service without SAS keys.
+ *
+ * @summary Get the metadata of the provisioning service without SAS keys.
+ * x-ms-original-file: specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/stable/2022-02-05/examples/DPSGet.json
+ */
+async function dpsGet() {
+  const subscriptionId =
+    process.env["DEVICEPROVISIONINGSERVICES_SUBSCRIPTION_ID"] ||
+    "91d12660-3dec-467a-be2a-213b5544ddc0";
+  const provisioningServiceName = "myFirstProvisioningService";
+  const resourceGroupName =
+    process.env["DEVICEPROVISIONINGSERVICES_RESOURCE_GROUP"] || "myResourceGroup";
+  const credential = new DefaultAzureCredential();
+  const client = new IotDpsClient(credential, subscriptionId);
+  const result = await client.iotDpsResource.get(provisioningServiceName, resourceGroupName);
+  console.log(result);
+}

53ed18092ac78e10611216e8079314dd747d6c50
azure-sdk <azuresdk@microsoft.com>
2023-01-10 12:10:36 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-postgresql-flexible_7.0.0

---
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples-js/ServerUpdateWithDataEncryptionEnabled.js
@@ -0,0 +1,40 @@
+const { PostgreSQLManagementFlexibleServerClient } = require("@azure/arm-postgresql-flexible");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates an existing server. The request body can contain one to many of the properties present in the normal server definition.
+ *
+ * @summary Updates an existing server. The request body can contain one to many of the properties present in the normal server definition.
+ * x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples/ServerUpdateWithDataEncryptionEnabled.json
+ */
+async function serverUpdateWithDataEncryptionEnabled() {
+  const subscriptionId =
+    process.env["POSTGRESQL_SUBSCRIPTION_ID"] || "ffffffff-ffff-ffff-ffff-ffffffffffff";
+  const resourceGroupName = process.env["POSTGRESQL_RESOURCE_GROUP"] || "TestGroup";
+  const serverName = "pgtestsvc4";
+  const parameters = {
+    administratorLoginPassword: "newpassword",
+    backup: { backupRetentionDays: 20 },
+    createMode: "Update",
+    dataEncryption: {
+      type: "AzureKeyVault",
+      primaryKeyURI:
+        "https://test-kv.vault.azure.net/keys/test-key1/77f57315bab34b0189daa113fbc78787",
+      primaryUserAssignedIdentityId:
+        "/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/testresourcegroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/test-usermanagedidentity",
+    },
+    identity: {
+      type: "UserAssigned",
+      userAssignedIdentities: {
+        "/subscriptions/ffffffffFfffFfffFfffFfffffffffff/resourceGroups/testresourcegroup/providers/MicrosoftManagedIdentity/userAssignedIdentities/testUsermanagedidentity":
+          {},
+      },
+    },
+    sku: { name: "Standard_D8s_v3", tier: "GeneralPurpose" },
+    storage: { storageSizeGB: 1024 },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new PostgreSQLManagementFlexibleServerClient(credential, subscriptionId);
+  const result = await client.servers.beginUpdateAndWait(resourceGroupName, serverName, parameters);
+  console.log(result);
+}

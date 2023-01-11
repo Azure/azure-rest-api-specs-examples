15b301bb96d95827d25e79b1e4a39c958faccda0
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-11 08:02:08 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-postgresql-flexible_7.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples-js/ServerCreateGeoRestore.js
@@ -0,0 +1,26 @@
+const { PostgreSQLManagementFlexibleServerClient } = require("@azure/arm-postgresql-flexible");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates a new server.
+ *
+ * @summary Creates a new server.
+ * x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples/ServerCreateGeoRestore.json
+ */
+async function createADatabaseAsAGeoRestoreInGeoPairedLocation() {
+  const subscriptionId =
+    process.env["POSTGRESQL_SUBSCRIPTION_ID"] || "ffffffff-ffff-ffff-ffff-ffffffffffff";
+  const resourceGroupName = process.env["POSTGRESQL_RESOURCE_GROUP"] || "testrg";
+  const serverName = "pgtestsvc5geo";
+  const parameters = {
+    createMode: "GeoRestore",
+    location: "eastus",
+    pointInTimeUTC: new Date("2021-06-27T00:04:59.4078005+00:00"),
+    sourceServerResourceId:
+      "/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/testrg/providers/Microsoft.DBforPostgreSQL/flexibleServers/sourcepgservername",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new PostgreSQLManagementFlexibleServerClient(credential, subscriptionId);
+  const result = await client.servers.beginCreateAndWait(resourceGroupName, serverName, parameters);
+  console.log(result);
+}

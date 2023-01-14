3b2573c5771dbf0accc87e944161baeeac7da224
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-14 08:02:16 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerinstance/armcontainerinstance/v2.2.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerGroupCreateConfidential.go
@@ -0,0 +1,70 @@
+package armcontainerinstance_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerinstance/armcontainerinstance/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerGroupCreateConfidential.json
+func ExampleContainerGroupsClient_BeginCreateOrUpdate_confidentialContainerGroup() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcontainerinstance.NewContainerGroupsClient("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginCreateOrUpdate(ctx, "demo", "demo1", armcontainerinstance.ContainerGroup{
+		Properties: &armcontainerinstance.ContainerGroupPropertiesProperties{
+			ConfidentialComputeProperties: &armcontainerinstance.ConfidentialComputeProperties{
+				CcePolicy: to.Ptr("eyJhbGxvd19hbGwiOiB0cnVlLCAiY29udGFpbmVycyI6IHsibGVuZ3RoIjogMCwgImVsZW1lbnRzIjogbnVsbH19"),
+			},
+			Containers: []*armcontainerinstance.Container{
+				{
+					Name: to.Ptr("accdemo"),
+					Properties: &armcontainerinstance.ContainerProperties{
+						Command:              []*string{},
+						EnvironmentVariables: []*armcontainerinstance.EnvironmentVariable{},
+						Image:                to.Ptr("confiimage"),
+						Ports: []*armcontainerinstance.ContainerPort{
+							{
+								Port: to.Ptr[int32](8000),
+							}},
+						Resources: &armcontainerinstance.ResourceRequirements{
+							Requests: &armcontainerinstance.ResourceRequests{
+								CPU:        to.Ptr[float64](1),
+								MemoryInGB: to.Ptr[float64](1.5),
+							},
+						},
+					},
+				}},
+			ImageRegistryCredentials: []*armcontainerinstance.ImageRegistryCredential{},
+			IPAddress: &armcontainerinstance.IPAddress{
+				Type: to.Ptr(armcontainerinstance.ContainerGroupIPAddressTypePublic),
+				Ports: []*armcontainerinstance.Port{
+					{
+						Port:     to.Ptr[int32](8000),
+						Protocol: to.Ptr(armcontainerinstance.ContainerGroupNetworkProtocolTCP),
+					}},
+			},
+			OSType: to.Ptr(armcontainerinstance.OperatingSystemTypesLinux),
+			SKU:    to.Ptr(armcontainerinstance.ContainerGroupSKUConfidential),
+		},
+		Location: to.Ptr("westeurope"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// TODO: use response item
+	_ = res
+}

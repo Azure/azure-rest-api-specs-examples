bdd00ae993911d4a7c2d3c15c64ff090eb560097
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-16 13:10:00 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Kusto_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2022-11-11/examples-dotnet/KustoScriptsGet.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Kusto;
+
+// Generated from example definition: specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2022-11-11/examples/KustoScriptsGet.json
+// this example is just showing the usage of "Scripts_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this KustoDatabaseResource created on azure
+// for more information of creating KustoDatabaseResource, please refer to the document of KustoDatabaseResource
+string subscriptionId = "12345678-1234-1234-1234-123456789098";
+string resourceGroupName = "kustorptest";
+string clusterName = "kustoCluster";
+string databaseName = "Kustodatabase8";
+ResourceIdentifier kustoDatabaseResourceId = KustoDatabaseResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName, databaseName);
+KustoDatabaseResource kustoDatabase = client.GetKustoDatabaseResource(kustoDatabaseResourceId);
+
+// get the collection of this KustoScriptResource
+KustoScriptCollection collection = kustoDatabase.GetKustoScripts();
+
+// invoke the operation
+string scriptName = "kustoScript";
+bool result = await collection.ExistsAsync(scriptName);
+
+Console.WriteLine($"Succeeded: {result}");

884d2d0d4bb8640cb849f4205957b20eb0240b57
azure-sdk <azuresdk@microsoft.com>
2023-01-17 12:25:20 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-botservice_2.0.0

---
+++ specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-python/UpdateBot.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.botservice import AzureBotService
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-botservice
+# USAGE
+    python update_bot.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureBotService(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscription-id",
+    )
+
+    response = client.bots.update(
+        resource_group_name="OneResourceGroupName",
+        resource_name="samplebotname",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples/UpdateBot.json
+if __name__ == "__main__":
+    main()

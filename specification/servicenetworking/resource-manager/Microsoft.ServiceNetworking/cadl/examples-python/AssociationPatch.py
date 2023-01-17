0cc025b5724963da44a5e4c9976c0e585e9beeef
azure-sdk <azuresdk@microsoft.com>
2023-01-17 12:26:47 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-servicenetworking_1.0.0b1

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-python/AssociationPatch.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.servicenetworking import ServiceNetworkingMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-servicenetworking
+# USAGE
+    python association_patch.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ServiceNetworkingMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.associations_interface.update(
+        resource_group_name="rg1",
+        traffic_controller_name="TC1",
+        association_name="associatedvnet-1",
+        properties={"properties": {"associationType": "subnets", "subnet": {"id": "subnetFullRef"}}},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/AssociationPatch.json
+if __name__ == "__main__":
+    main()

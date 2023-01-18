a76076c38be3f114ffbc5f761b5489780fcd6ab8
azure-sdk <azuresdk@microsoft.com>
2023-01-18 12:18:42 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-maps_1.0.0-beta.2

---
+++ specification/maps/resource-manager/Microsoft.Maps/stable/2021-02-01/examples-java/UpdateMapsCreator.java
@@ -0,0 +1,34 @@
+import com.azure.resourcemanager.maps.models.Creator;
+import java.util.HashMap;
+import java.util.Map;
+
+/** Samples for Creators Update. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/stable/2021-02-01/examples/UpdateMapsCreator.json
+     */
+    /**
+     * Sample code: Update Creator Resource.
+     *
+     * @param manager Entry point to AzureMapsManager.
+     */
+    public static void updateCreatorResource(com.azure.resourcemanager.maps.AzureMapsManager manager) {
+        Creator resource =
+            manager
+                .creators()
+                .getWithResponse("myResourceGroup", "myMapsAccount", "myCreator", com.azure.core.util.Context.NONE)
+                .getValue();
+        resource.update().withTags(mapOf("specialTag", "true")).withStorageUnits(10).apply();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> Map<String, T> mapOf(Object... inputs) {
+        Map<String, T> map = new HashMap<>();
+        for (int i = 0; i < inputs.length; i += 2) {
+            String key = (String) inputs[i];
+            T value = (T) inputs[i + 1];
+            map.put(key, value);
+        }
+        return map;
+    }
+}

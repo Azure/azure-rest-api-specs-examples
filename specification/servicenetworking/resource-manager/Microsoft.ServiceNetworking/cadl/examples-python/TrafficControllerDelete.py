1d072b05db0a960c9b0476105c98d9114f424c54
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-18 08:02:37 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-servicenetworking_1.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-python/TrafficControllerDelete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.servicenetworking import ServiceNetworkingMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-servicenetworking
+# USAGE
+    python traffic_controller_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ServiceNetworkingMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.traffic_controller_interface.begin_delete(
+        resource_group_name="rg1",
+        traffic_controller_name="TC1",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/TrafficControllerDelete.json
+if __name__ == "__main__":
+    main()

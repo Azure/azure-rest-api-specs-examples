4e49c804670e7b1837262898e906be3480229da4
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-18 08:02:29 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-dataprotection_1.0.0b4
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/preview/2022-11-01-preview/examples-python/BackupInstanceOperations/TriggerRehydrate.py
@@ -0,0 +1,39 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.dataprotection import DataProtectionClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dataprotection
+# USAGE
+    python trigger_rehydrate.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataProtectionClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="04cf684a-d41f-4550-9f70-7708a3a2283b",
+    )
+
+    response = client.backup_instances.begin_trigger_rehydrate(
+        resource_group_name="000pikumar",
+        vault_name="PratikPrivatePreviewVault1",
+        backup_instance_name="testInstance1",
+        parameters={
+            "recoveryPointId": "hardcodedRP",
+            "rehydrationPriority": "High",
+            "rehydrationRetentionDuration": "7D",
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/preview/2022-11-01-preview/examples/BackupInstanceOperations/TriggerRehydrate.json
+if __name__ == "__main__":
+    main()

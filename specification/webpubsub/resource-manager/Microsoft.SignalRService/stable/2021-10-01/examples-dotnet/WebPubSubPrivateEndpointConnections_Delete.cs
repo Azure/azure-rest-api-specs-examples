b036742a0f7f73781491657357abe18f55500067
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 19:38:22 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.WebPubSub_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/webpubsub/resource-manager/Microsoft.SignalRService/stable/2021-10-01/examples-dotnet/WebPubSubPrivateEndpointConnections_Delete.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.WebPubSub;
+using Azure.ResourceManager.WebPubSub.Models;
+
+// Generated from example definition: specification/webpubsub/resource-manager/Microsoft.SignalRService/stable/2021-10-01/examples/WebPubSubPrivateEndpointConnections_Delete.json
+// this example is just showing the usage of "WebPubSubPrivateEndpointConnections_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this WebPubSubPrivateEndpointConnectionResource created on azure
+// for more information of creating WebPubSubPrivateEndpointConnectionResource, please refer to the document of WebPubSubPrivateEndpointConnectionResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string resourceName = "myWebPubSubService";
+string privateEndpointConnectionName = "mywebpubsubservice.1fa229cd-bf3f-47f0-8c49-afb36723997e";
+ResourceIdentifier webPubSubPrivateEndpointConnectionResourceId = WebPubSubPrivateEndpointConnectionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, privateEndpointConnectionName);
+WebPubSubPrivateEndpointConnectionResource webPubSubPrivateEndpointConnection = client.GetWebPubSubPrivateEndpointConnectionResource(webPubSubPrivateEndpointConnectionResourceId);
+
+// invoke the operation
+await webPubSubPrivateEndpointConnection.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");
---
+++ specification/webpubsub/resource-manager/Microsoft.SignalRService/stable/2021-10-01/examples-dotnet/WebPubSub_Delete.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.WebPubSub;
+using Azure.ResourceManager.WebPubSub.Models;
+
+// Generated from example definition: specification/webpubsub/resource-manager/Microsoft.SignalRService/stable/2021-10-01/examples/WebPubSub_Delete.json
+// this example is just showing the usage of "WebPubSub_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this WebPubSubResource created on azure
+// for more information of creating WebPubSubResource, please refer to the document of WebPubSubResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string resourceName = "myWebPubSubService";
+ResourceIdentifier webPubSubResourceId = WebPubSubResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
+WebPubSubResource webPubSub = client.GetWebPubSubResource(webPubSubResourceId);
+
+// invoke the operation
+await webPubSub.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

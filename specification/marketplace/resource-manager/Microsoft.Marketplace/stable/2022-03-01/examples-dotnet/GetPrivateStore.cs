f1063adb4956d7e50a16ee1726d6f6de4b827204
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 16:52:43 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Marketplace_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples-dotnet/GetPrivateStore.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Marketplace;
+using Azure.ResourceManager.Marketplace.Models;
+
+// Generated from example definition: specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples/GetPrivateStore.json
+// this example is just showing the usage of "PrivateStore_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TenantResource created on azure
+// for more information of creating TenantResource, please refer to the document of TenantResource
+var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+// get the collection of this PrivateStoreResource
+PrivateStoreCollection collection = tenantResource.GetPrivateStores();
+
+// invoke the operation
+Guid privateStoreId = Guid.Parse("a0e28e55-90c4-41d8-8e34-bb7ef7775406");
+bool result = await collection.ExistsAsync(privateStoreId);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples-dotnet/PrivateStores_update.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Marketplace;
+using Azure.ResourceManager.Marketplace.Models;
+
+// Generated from example definition: specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples/PrivateStores_update.json
+// this example is just showing the usage of "PrivateStore_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TenantResource created on azure
+// for more information of creating TenantResource, please refer to the document of TenantResource
+var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+// get the collection of this PrivateStoreResource
+PrivateStoreCollection collection = tenantResource.GetPrivateStores();
+
+// invoke the operation
+Guid privateStoreId = Guid.Parse("a0e28e55-90c4-41d8-8e34-bb7ef7775406");
+PrivateStoreData data = new PrivateStoreData()
+{
+    Availability = PrivateStoreAvailability.Disabled,
+    ETag = new ETag("\"9301f4fd-0000-0100-0000-5e248b350345\""),
+};
+await collection.CreateOrUpdateAsync(WaitUntil.Completed, privateStoreId, data);
+
+Console.WriteLine($"Succeeded");

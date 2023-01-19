a2145f084a6a40b568299d3ffe33b685f4193121
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 16:50:12 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HealthBot_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-08-24/examples-dotnet/ResourceCreationPut.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.HealthBot;
+using Azure.ResourceManager.HealthBot.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-08-24/examples/ResourceCreationPut.json
+// this example is just showing the usage of "Bots_Create" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "subid";
+string resourceGroupName = "healthbotClient";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this HealthBotResource
+HealthBotCollection collection = resourceGroupResource.GetHealthBots();
+
+// invoke the operation
+string botName = "samplebotname";
+HealthBotData data = new HealthBotData(new AzureLocation("East US"), new HealthBotSku(HealthBotSkuName.F0))
+{
+    Identity = new ManagedServiceIdentity("SystemAssigned, UserAssigned")
+    {
+        UserAssignedIdentities =
+        {
+        [new ResourceIdentifier("/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi")] = new UserAssignedIdentity(),
+        [new ResourceIdentifier("/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi2")] = new UserAssignedIdentity(),
+        },
+    },
+};
+ArmOperation<HealthBotResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, botName, data);
+HealthBotResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+HealthBotData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

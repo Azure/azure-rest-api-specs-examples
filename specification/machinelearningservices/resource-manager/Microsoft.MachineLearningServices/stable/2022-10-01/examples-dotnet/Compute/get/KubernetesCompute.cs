6b4c5b9566da1b191eab30d3b1064d79082b52fa
azure-sdk <azuresdk@microsoft.com>
2023-01-19 05:56:58 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MachineLearning_1.0.0

---
+++ specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2022-10-01/examples-dotnet/Compute/get/KubernetesCompute.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.MachineLearning;
+using Azure.ResourceManager.MachineLearning.Models;
+
+// Generated from example definition: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2022-10-01/examples/Compute/get/KubernetesCompute.json
+// this example is just showing the usage of "Compute_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this MachineLearningWorkspaceResource created on azure
+// for more information of creating MachineLearningWorkspaceResource, please refer to the document of MachineLearningWorkspaceResource
+string subscriptionId = "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
+string resourceGroupName = "testrg123";
+string workspaceName = "workspaces123";
+ResourceIdentifier machineLearningWorkspaceResourceId = MachineLearningWorkspaceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName);
+MachineLearningWorkspaceResource machineLearningWorkspace = client.GetMachineLearningWorkspaceResource(machineLearningWorkspaceResourceId);
+
+// get the collection of this MachineLearningComputeResource
+MachineLearningComputeCollection collection = machineLearningWorkspace.GetMachineLearningComputes();
+
+// invoke the operation
+string computeName = "compute123";
+bool result = await collection.ExistsAsync(computeName);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2022-10-01/examples-dotnet/PrivateEndpointConnection/get.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.MachineLearning;
+using Azure.ResourceManager.MachineLearning.Models;
+
+// Generated from example definition: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2022-10-01/examples/PrivateEndpointConnection/get.json
+// this example is just showing the usage of "PrivateEndpointConnections_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this MachineLearningWorkspaceResource created on azure
+// for more information of creating MachineLearningWorkspaceResource, please refer to the document of MachineLearningWorkspaceResource
+string subscriptionId = "00000000-1111-2222-3333-444444444444";
+string resourceGroupName = "rg-1234";
+string workspaceName = "testworkspace";
+ResourceIdentifier machineLearningWorkspaceResourceId = MachineLearningWorkspaceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName);
+MachineLearningWorkspaceResource machineLearningWorkspace = client.GetMachineLearningWorkspaceResource(machineLearningWorkspaceResourceId);
+
+// get the collection of this MachineLearningPrivateEndpointConnectionResource
+MachineLearningPrivateEndpointConnectionCollection collection = machineLearningWorkspace.GetMachineLearningPrivateEndpointConnections();
+
+// invoke the operation
+string privateEndpointConnectionName = "{privateEndpointConnectionName}";
+bool result = await collection.ExistsAsync(privateEndpointConnectionName);
+
+Console.WriteLine($"Succeeded: {result}");

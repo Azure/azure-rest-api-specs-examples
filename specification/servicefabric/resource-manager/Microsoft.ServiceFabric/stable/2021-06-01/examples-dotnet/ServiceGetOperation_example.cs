4564b9a4911490a6ec8388dd1e3551eef608466b
azure-sdk <azuresdk@microsoft.com>
2023-01-19 08:31:41 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceFabric_1.0.0

---
+++ specification/servicefabric/resource-manager/Microsoft.ServiceFabric/stable/2021-06-01/examples-dotnet/ServiceGetOperation_example.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ServiceFabric;
+using Azure.ResourceManager.ServiceFabric.Models;
+
+// Generated from example definition: specification/servicefabric/resource-manager/Microsoft.ServiceFabric/stable/2021-06-01/examples/ServiceGetOperation_example.json
+// this example is just showing the usage of "Services_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ServiceFabricApplicationResource created on azure
+// for more information of creating ServiceFabricApplicationResource, please refer to the document of ServiceFabricApplicationResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "resRg";
+string clusterName = "myCluster";
+string applicationName = "myApp";
+ResourceIdentifier serviceFabricApplicationResourceId = ServiceFabricApplicationResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName, applicationName);
+ServiceFabricApplicationResource serviceFabricApplication = client.GetServiceFabricApplicationResource(serviceFabricApplicationResourceId);
+
+// get the collection of this ServiceFabricServiceResource
+ServiceFabricServiceCollection collection = serviceFabricApplication.GetServiceFabricServices();
+
+// invoke the operation
+string serviceName = "myService";
+bool result = await collection.ExistsAsync(serviceName);
+
+Console.WriteLine($"Succeeded: {result}");

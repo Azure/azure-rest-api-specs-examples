0a16934fd4ca06a2e50781c48049e0fc1c5d9b55
azure-sdk <azuresdk@microsoft.com>
2023-01-19 08:21:07 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.BotService_1.0.0-beta.1

---
+++ specification/botservice/resource-manager/Microsoft.BotService/preview/2022-06-15-preview/examples-dotnet/GetPrivateEndpointConnection.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.BotService;
+using Azure.ResourceManager.BotService.Models;
+
+// Generated from example definition: specification/botservice/resource-manager/Microsoft.BotService/preview/2022-06-15-preview/examples/GetPrivateEndpointConnection.json
+// this example is just showing the usage of "PrivateEndpointConnections_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this BotResource created on azure
+// for more information of creating BotResource, please refer to the document of BotResource
+string subscriptionId = "{subscription-id}";
+string resourceGroupName = "res6977";
+string resourceName = "sto2527";
+ResourceIdentifier botResourceId = BotResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
+BotResource bot = client.GetBotResource(botResourceId);
+
+// get the collection of this BotServicePrivateEndpointConnectionResource
+BotServicePrivateEndpointConnectionCollection collection = bot.GetBotServicePrivateEndpointConnections();
+
+// invoke the operation
+string privateEndpointConnectionName = "{privateEndpointConnectionName}";
+bool result = await collection.ExistsAsync(privateEndpointConnectionName);
+
+Console.WriteLine($"Succeeded: {result}");

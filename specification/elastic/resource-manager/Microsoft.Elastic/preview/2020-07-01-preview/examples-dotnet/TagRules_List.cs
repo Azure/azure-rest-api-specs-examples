0efcba934411e6bcdf66c397181aebd5c277d1f0
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 16:50:28 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Elastic_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/elastic/resource-manager/Microsoft.Elastic/preview/2020-07-01-preview/examples-dotnet/TagRules_List.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Elastic;
+
+// Generated from example definition: specification/elastic/resource-manager/Microsoft.Elastic/preview/2020-07-01-preview/examples/TagRules_List.json
+// this example is just showing the usage of "TagRules_List" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ElasticMonitorResource created on azure
+// for more information of creating ElasticMonitorResource, please refer to the document of ElasticMonitorResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string monitorName = "myMonitor";
+ResourceIdentifier elasticMonitorResourceId = ElasticMonitorResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, monitorName);
+ElasticMonitorResource elasticMonitorResource = client.GetElasticMonitorResource(elasticMonitorResourceId);
+
+// get the collection of this MonitoringTagRuleResource
+MonitoringTagRuleCollection collection = elasticMonitorResource.GetMonitoringTagRules();
+
+// invoke the operation and iterate over the result
+await foreach (MonitoringTagRuleResource item in collection.GetAllAsync())
+{
+    // the variable item is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    MonitoringTagRuleData resourceData = item.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
+
+Console.WriteLine($"Succeeded");

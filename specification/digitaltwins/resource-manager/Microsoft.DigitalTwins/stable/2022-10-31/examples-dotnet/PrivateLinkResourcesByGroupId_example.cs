e8430394b1d7aa1b9f574dc64a97a042f256b5d9
azure-sdk <azuresdk@microsoft.com>
2023-01-19 06:03:53 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DigitalTwins_1.1.0

---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-dotnet/PrivateLinkResourcesByGroupId_example.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.DigitalTwins;
+
+// Generated from example definition: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/PrivateLinkResourcesByGroupId_example.json
+// this example is just showing the usage of "PrivateLinkResources_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DigitalTwinsDescriptionResource created on azure
+// for more information of creating DigitalTwinsDescriptionResource, please refer to the document of DigitalTwinsDescriptionResource
+string subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+string resourceGroupName = "resRg";
+string resourceName = "myDigitalTwinsService";
+ResourceIdentifier digitalTwinsDescriptionResourceId = DigitalTwinsDescriptionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
+DigitalTwinsDescriptionResource digitalTwinsDescription = client.GetDigitalTwinsDescriptionResource(digitalTwinsDescriptionResourceId);
+
+// get the collection of this DigitalTwinsPrivateLinkResource
+DigitalTwinsPrivateLinkResourceCollection collection = digitalTwinsDescription.GetDigitalTwinsPrivateLinkResources();
+
+// invoke the operation
+string resourceId = "subResource";
+bool result = await collection.ExistsAsync(resourceId);
+
+Console.WriteLine($"Succeeded: {result}");

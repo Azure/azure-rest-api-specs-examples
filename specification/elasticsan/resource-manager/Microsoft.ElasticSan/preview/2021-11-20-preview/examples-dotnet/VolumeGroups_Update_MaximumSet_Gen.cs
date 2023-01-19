b783a0095491dd92f8fe6dd55311a77b324c3ef3
azure-sdk <azuresdk@microsoft.com>
2023-01-19 07:38:00 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ElasticSan_1.0.0-beta.1

---
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2021-11-20-preview/examples-dotnet/VolumeGroups_Update_MaximumSet_Gen.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ElasticSan;
+using Azure.ResourceManager.ElasticSan.Models;
+
+// Generated from example definition: specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2021-11-20-preview/examples/VolumeGroups_Update_MaximumSet_Gen.json
+// this example is just showing the usage of "VolumeGroups_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ElasticSanVolumeGroupResource created on azure
+// for more information of creating ElasticSanVolumeGroupResource, please refer to the document of ElasticSanVolumeGroupResource
+string subscriptionId = "aaaaaaaaaaaaaaaaaa";
+string resourceGroupName = "rgelasticsan";
+string elasticSanName = "ti7q-k952-1qB3J_5";
+string volumeGroupName = "u_5I_1j4t3";
+ResourceIdentifier elasticSanVolumeGroupResourceId = ElasticSanVolumeGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, elasticSanName, volumeGroupName);
+ElasticSanVolumeGroupResource elasticSanVolumeGroup = client.GetElasticSanVolumeGroupResource(elasticSanVolumeGroupResourceId);
+
+// invoke the operation
+ElasticSanVolumeGroupPatch patch = new ElasticSanVolumeGroupPatch()
+{
+    Tags =
+    {
+    ["key7542"] = "aaaaaaaaaaaaaaaaaaaa",
+    },
+    ProtocolType = StorageTargetType.Iscsi,
+    Encryption = ElasticSanEncryptionType.EncryptionAtRestWithPlatformKey,
+    VirtualNetworkRules =
+    {
+    new ElasticSanVirtualNetworkRule(new ResourceIdentifier("aaaaaaaaaaaaaaaa"))
+    {
+    Action = ElasticSanVirtualNetworkRuleAction.Allow,
+    }
+    },
+};
+ArmOperation<ElasticSanVolumeGroupResource> lro = await elasticSanVolumeGroup.UpdateAsync(WaitUntil.Completed, patch);
+ElasticSanVolumeGroupResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ElasticSanVolumeGroupData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2021-11-20-preview/examples-dotnet/Volumes_Delete_MinimumSet_Gen.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ElasticSan;
+using Azure.ResourceManager.ElasticSan.Models;
+
+// Generated from example definition: specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2021-11-20-preview/examples/Volumes_Delete_MinimumSet_Gen.json
+// this example is just showing the usage of "Volumes_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ElasticSanVolumeResource created on azure
+// for more information of creating ElasticSanVolumeResource, please refer to the document of ElasticSanVolumeResource
+string subscriptionId = "aaaaaaaaaaaaaaaaaa";
+string resourceGroupName = "rgelasticsan";
+string elasticSanName = "ti7q-k952-1qB3J_5";
+string volumeGroupName = "u_5I_1j4t3";
+string volumeName = "9132y";
+ResourceIdentifier elasticSanVolumeResourceId = ElasticSanVolumeResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, elasticSanName, volumeGroupName, volumeName);
+ElasticSanVolumeResource elasticSanVolume = client.GetElasticSanVolumeResource(elasticSanVolumeResourceId);
+
+// invoke the operation
+await elasticSanVolume.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

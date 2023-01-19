ace3ce9b584d18dccb749608005d662215c018c1
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 15:24:10 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MixedReality_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-dotnet/proxy/CheckNameAvailabilityForLocalUniqueness.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.MixedReality.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples/proxy/CheckNameAvailabilityForLocalUniqueness.json
+// this example is just showing the usage of "CheckNameAvailabilityLocal" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation
+AzureLocation location = new AzureLocation("eastus2euap");
+MixedRealityNameAvailabilityContent content = new MixedRealityNameAvailabilityContent("MyAccount", "Microsoft.MixedReality/spatialAnchorsAccounts");
+MixedRealityNameAvailabilityResult result = await subscriptionResource.CheckMixedRealityNameAvailabilityAsync(location, content);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-dotnet/remote-rendering/RegenerateKey.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.MixedReality;
+using Azure.ResourceManager.MixedReality.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples/remote-rendering/RegenerateKey.json
+// this example is just showing the usage of "RemoteRenderingAccounts_RegenerateKeys" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this RemoteRenderingAccountResource created on azure
+// for more information of creating RemoteRenderingAccountResource, please refer to the document of RemoteRenderingAccountResource
+string subscriptionId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+string resourceGroupName = "MyResourceGroup";
+string accountName = "MyAccount";
+ResourceIdentifier remoteRenderingAccountResourceId = RemoteRenderingAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
+RemoteRenderingAccountResource remoteRenderingAccount = client.GetRemoteRenderingAccountResource(remoteRenderingAccountResourceId);
+
+// invoke the operation
+MixedRealityAccountKeyRegenerateContent content = new MixedRealityAccountKeyRegenerateContent()
+{
+    Serial = MixedRealityAccountKeySerial.Primary,
+};
+MixedRealityAccountKeys result = await remoteRenderingAccount.RegenerateKeysAsync(content);
+
+Console.WriteLine($"Succeeded: {result}");

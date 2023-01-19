21252864db76f6a932d5e72036cdb00665125349
azure-sdk <azuresdk@microsoft.com>
2023-01-19 08:06:04 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Kubernetes_1.0.0-beta.1

---
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2022-05-01-preview/examples-dotnet/ConnectedClustersListClusterCredentialResultHPAAD.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Kubernetes;
+using Azure.ResourceManager.Kubernetes.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2022-05-01-preview/examples/ConnectedClustersListClusterCredentialResultHPAAD.json
+// this example is just showing the usage of "ConnectedCluster_ListClusterUserCredential" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ConnectedClusterResource created on azure
+// for more information of creating ConnectedClusterResource, please refer to the document of ConnectedClusterResource
+string subscriptionId = "1bfbb5d0-917e-4346-9026-1d3b344417f5";
+string resourceGroupName = "k8sc-rg";
+string clusterName = "testCluster";
+ResourceIdentifier connectedClusterResourceId = ConnectedClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName);
+ConnectedClusterResource connectedCluster = client.GetConnectedClusterResource(connectedClusterResourceId);
+
+// invoke the operation
+ListClusterUserCredentialProperties properties = new ListClusterUserCredentialProperties(AuthenticationMethod.AAD, false);
+CredentialResults result = await connectedCluster.GetClusterUserCredentialAsync(properties);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2022-05-01-preview/examples-dotnet/GetClusterExample.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Kubernetes;
+using Azure.ResourceManager.Kubernetes.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2022-05-01-preview/examples/GetClusterExample.json
+// this example is just showing the usage of "ConnectedCluster_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "1bfbb5d0-917e-4346-9026-1d3b344417f5";
+string resourceGroupName = "k8sc-rg";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ConnectedClusterResource
+ConnectedClusterCollection collection = resourceGroupResource.GetConnectedClusters();
+
+// invoke the operation
+string clusterName = "testCluster";
+bool result = await collection.ExistsAsync(clusterName);
+
+Console.WriteLine($"Succeeded: {result}");

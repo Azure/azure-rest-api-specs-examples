3b9d1d3a8bcd2ecf6c22253876007fce04398840
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 16:53:53 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Avs_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/vmware/resource-manager/Microsoft.AVS/stable/2022-05-01/examples-dotnet/CloudLinks_CreateOrUpdate.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Avs;
+
+// Generated from example definition: specification/vmware/resource-manager/Microsoft.AVS/stable/2022-05-01/examples/CloudLinks_CreateOrUpdate.json
+// this example is just showing the usage of "CloudLinks_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this AvsPrivateCloudResource created on azure
+// for more information of creating AvsPrivateCloudResource, please refer to the document of AvsPrivateCloudResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "group1";
+string privateCloudName = "cloud1";
+ResourceIdentifier avsPrivateCloudResourceId = AvsPrivateCloudResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, privateCloudName);
+AvsPrivateCloudResource avsPrivateCloud = client.GetAvsPrivateCloudResource(avsPrivateCloudResourceId);
+
+// get the collection of this AvsCloudLinkResource
+AvsCloudLinkCollection collection = avsPrivateCloud.GetAvsCloudLinks();
+
+// invoke the operation
+string cloudLinkName = "cloudLink1";
+AvsCloudLinkData data = new AvsCloudLinkData()
+{
+    LinkedCloud = new ResourceIdentifier("/subscriptions/12341234-1234-1234-1234-123412341234/resourceGroups/mygroup/providers/Microsoft.AVS/privateClouds/cloud2"),
+};
+ArmOperation<AvsCloudLinkResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, cloudLinkName, data);
+AvsCloudLinkResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+AvsCloudLinkData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

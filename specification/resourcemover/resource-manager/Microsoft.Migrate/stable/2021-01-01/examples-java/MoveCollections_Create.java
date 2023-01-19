64c9b222f8b7dd76b6900d00895b80c1be722b85
azure-sdk <azuresdk@microsoft.com>
2023-01-19 11:57:08 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-resourcemover_1.0.0-beta.2

---
+++ specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-01-01/examples-java/MoveCollections_Create.java
@@ -0,0 +1,25 @@
+import com.azure.resourcemanager.resourcemover.models.Identity;
+import com.azure.resourcemanager.resourcemover.models.MoveCollectionProperties;
+import com.azure.resourcemanager.resourcemover.models.ResourceIdentityType;
+
+/** Samples for MoveCollections Create. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-01-01/examples/MoveCollections_Create.json
+     */
+    /**
+     * Sample code: MoveCollections_Create.
+     *
+     * @param manager Entry point to ResourceMoverManager.
+     */
+    public static void moveCollectionsCreate(com.azure.resourcemanager.resourcemover.ResourceMoverManager manager) {
+        manager
+            .moveCollections()
+            .define("movecollection1")
+            .withRegion("eastus2")
+            .withExistingResourceGroup("rg1")
+            .withIdentity(new Identity().withType(ResourceIdentityType.SYSTEM_ASSIGNED))
+            .withProperties(new MoveCollectionProperties().withSourceRegion("eastus").withTargetRegion("westus"))
+            .create();
+    }
+}

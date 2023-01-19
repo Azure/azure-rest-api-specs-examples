aeacaf0eca582f64c36d488f12f3a17ebb31ba5d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 15:23:48 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DigitalTwins_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-dotnet/DigitalTwinsEndpointGet_WithIdentity_example.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.DigitalTwins;
+using Azure.ResourceManager.DigitalTwins.Models;
+
+// Generated from example definition: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsEndpointGet_WithIdentity_example.json
+// this example is just showing the usage of "DigitalTwinsEndpoint_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DigitalTwinsEndpointResource created on azure
+// for more information of creating DigitalTwinsEndpointResource, please refer to the document of DigitalTwinsEndpointResource
+string subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+string resourceGroupName = "resRg";
+string resourceName = "myDigitalTwinsService";
+string endpointName = "myServiceBus";
+ResourceIdentifier digitalTwinsEndpointResourceId = DigitalTwinsEndpointResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, endpointName);
+DigitalTwinsEndpointResource digitalTwinsEndpointResource = client.GetDigitalTwinsEndpointResource(digitalTwinsEndpointResourceId);
+
+// invoke the operation
+DigitalTwinsEndpointResource result = await digitalTwinsEndpointResource.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DigitalTwinsEndpointResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-dotnet/TimeSeriesDatabaseConnectionsDelete_example.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.DigitalTwins;
+using Azure.ResourceManager.DigitalTwins.Models;
+
+// Generated from example definition: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/TimeSeriesDatabaseConnectionsDelete_example.json
+// this example is just showing the usage of "TimeSeriesDatabaseConnections_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TimeSeriesDatabaseConnectionResource created on azure
+// for more information of creating TimeSeriesDatabaseConnectionResource, please refer to the document of TimeSeriesDatabaseConnectionResource
+string subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+string resourceGroupName = "resRg";
+string resourceName = "myDigitalTwinsService";
+string timeSeriesDatabaseConnectionName = "myConnection";
+ResourceIdentifier timeSeriesDatabaseConnectionResourceId = TimeSeriesDatabaseConnectionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, timeSeriesDatabaseConnectionName);
+TimeSeriesDatabaseConnectionResource timeSeriesDatabaseConnection = client.GetTimeSeriesDatabaseConnectionResource(timeSeriesDatabaseConnectionResourceId);
+
+// invoke the operation
+ArmOperation<TimeSeriesDatabaseConnectionResource> lro = await timeSeriesDatabaseConnection.DeleteAsync(WaitUntil.Completed);
+TimeSeriesDatabaseConnectionResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+TimeSeriesDatabaseConnectionData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

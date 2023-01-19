0335b6123a27c1c0debf4d8d37eee43bb30bce31
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-19 16:49:48 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DeviceProvisioningServices_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/stable/2022-02-05/examples-dotnet/DPSCertificateCreateOrUpdate.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.DeviceProvisioningServices;
+using Azure.ResourceManager.DeviceProvisioningServices.Models;
+
+// Generated from example definition: specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/stable/2022-02-05/examples/DPSCertificateCreateOrUpdate.json
+// this example is just showing the usage of "DpsCertificate_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DeviceProvisioningServicesCertificateResource created on azure
+// for more information of creating DeviceProvisioningServicesCertificateResource, please refer to the document of DeviceProvisioningServicesCertificateResource
+string subscriptionId = "91d12660-3dec-467a-be2a-213b5544ddc0";
+string resourceGroupName = "myResourceGroup";
+string provisioningServiceName = "myFirstProvisioningService";
+string certificateName = "cert";
+ResourceIdentifier deviceProvisioningServicesCertificateResourceId = DeviceProvisioningServicesCertificateResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, provisioningServiceName, certificateName);
+DeviceProvisioningServicesCertificateResource deviceProvisioningServicesCertificate = client.GetDeviceProvisioningServicesCertificateResource(deviceProvisioningServicesCertificateResourceId);
+
+// invoke the operation
+DeviceProvisioningServicesCertificateData data = new DeviceProvisioningServicesCertificateData()
+{
+    Properties = new DeviceProvisioningServicesCertificateProperties()
+    {
+        Certificate = BinaryData.FromString("############################################"),
+    },
+};
+ArmOperation<DeviceProvisioningServicesCertificateResource> lro = await deviceProvisioningServicesCertificate.UpdateAsync(WaitUntil.Completed, data);
+DeviceProvisioningServicesCertificateResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DeviceProvisioningServicesCertificateData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/stable/2022-02-05/examples-dotnet/DPSGetPrivateEndpointConnection.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.DeviceProvisioningServices;
+using Azure.ResourceManager.DeviceProvisioningServices.Models;
+
+// Generated from example definition: specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/stable/2022-02-05/examples/DPSGetPrivateEndpointConnection.json
+// this example is just showing the usage of "IotDpsResource_GetPrivateEndpointConnection" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DeviceProvisioningServicesPrivateEndpointConnectionResource created on azure
+// for more information of creating DeviceProvisioningServicesPrivateEndpointConnectionResource, please refer to the document of DeviceProvisioningServicesPrivateEndpointConnectionResource
+string subscriptionId = "91d12660-3dec-467a-be2a-213b5544ddc0";
+string resourceGroupName = "myResourceGroup";
+string resourceName = "myFirstProvisioningService";
+string privateEndpointConnectionName = "myPrivateEndpointConnection";
+ResourceIdentifier deviceProvisioningServicesPrivateEndpointConnectionResourceId = DeviceProvisioningServicesPrivateEndpointConnectionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, privateEndpointConnectionName);
+DeviceProvisioningServicesPrivateEndpointConnectionResource deviceProvisioningServicesPrivateEndpointConnection = client.GetDeviceProvisioningServicesPrivateEndpointConnectionResource(deviceProvisioningServicesPrivateEndpointConnectionResourceId);
+
+// invoke the operation
+DeviceProvisioningServicesPrivateEndpointConnectionResource result = await deviceProvisioningServicesPrivateEndpointConnection.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DeviceProvisioningServicesPrivateEndpointConnectionData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

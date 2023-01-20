0e5e21f4c76ff121b235bd0b989257e83aca08a1
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-01-20 20:01:16 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-managedapplications_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/resources/resource-manager/Microsoft.Solutions/stable/2018-06-01/examples-java/deleteApplicationDefinition.java
+++ specification/resources/resource-manager/Microsoft.Solutions/stable/2018-06-01/examples-java/deleteApplicationDefinition.java
@@ -1,15 +1,15 @@
-/** Samples for ApplicationDefinitions DeleteById. */
+/** Samples for ApplicationDefinitions Delete. */
 public final class Main {
     /*
      * x-ms-original-file: specification/resources/resource-manager/Microsoft.Solutions/stable/2018-06-01/examples/deleteApplicationDefinition.json
      */
     /**
-     * Sample code: Delete application definition.
+     * Sample code: Deletes a managed application.
      *
      * @param manager Entry point to ApplicationManager.
      */
-    public static void deleteApplicationDefinition(
+    public static void deletesAManagedApplication(
         com.azure.resourcemanager.managedapplications.ApplicationManager manager) {
-        manager.applicationDefinitions().deleteById("rg", "myManagedApplicationDef", com.azure.core.util.Context.NONE);
+        manager.applicationDefinitions().delete("rg", "myManagedApplicationDef", com.azure.core.util.Context.NONE);
     }
 }

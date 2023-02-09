7d07e3da57f7a62f67b4e670d19577dad6d7fcbd
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-09 08:02:32 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dataprotection_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/preview/2022-11-01-preview/examples-js/ResourceGuardProxyCRUD/GetResourceGuardProxy.js
@@ -0,0 +1,24 @@
+const { DataProtectionClient } = require("@azure/arm-dataprotection");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to
+ *
+ * @summary
+ * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/preview/2022-11-01-preview/examples/ResourceGuardProxyCRUD/GetResourceGuardProxy.json
+ */
+async function getResourceGuardProxy() {
+  const subscriptionId =
+    process.env["DATAPROTECTION_SUBSCRIPTION_ID"] || "5e13b949-1218-4d18-8b99-7e12155ec4f7";
+  const resourceGroupName = process.env["DATAPROTECTION_RESOURCE_GROUP"] || "SampleResourceGroup";
+  const vaultName = "sampleVault";
+  const resourceGuardProxyName = "swaggerExample";
+  const credential = new DefaultAzureCredential();
+  const client = new DataProtectionClient(credential, subscriptionId);
+  const result = await client.dppResourceGuardProxy.get(
+    resourceGroupName,
+    vaultName,
+    resourceGuardProxyName
+  );
+  console.log(result);
+}
---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/preview/2022-11-01-preview/examples-js/VaultCRUD/PutBackupVault.js
@@ -0,0 +1,34 @@
+const { DataProtectionClient } = require("@azure/arm-dataprotection");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates a BackupVault resource belonging to a resource group.
+ *
+ * @summary Creates or updates a BackupVault resource belonging to a resource group.
+ * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/preview/2022-11-01-preview/examples/VaultCRUD/PutBackupVault.json
+ */
+async function createBackupVault() {
+  const subscriptionId =
+    process.env["DATAPROTECTION_SUBSCRIPTION_ID"] || "0b352192-dcac-4cc7-992e-a96190ccc68c";
+  const resourceGroupName = process.env["DATAPROTECTION_RESOURCE_GROUP"] || "SampleResourceGroup";
+  const vaultName = "swaggerExample";
+  const parameters = {
+    identity: { type: "None" },
+    location: "WestUS",
+    properties: {
+      monitoringSettings: {
+        azureMonitorAlertSettings: { alertsForAllJobFailures: "Enabled" },
+      },
+      storageSettings: [{ type: "LocallyRedundant", datastoreType: "VaultStore" }],
+    },
+    tags: { key1: "val1" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new DataProtectionClient(credential, subscriptionId);
+  const result = await client.backupVaults.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    vaultName,
+    parameters
+  );
+  console.log(result);
+}

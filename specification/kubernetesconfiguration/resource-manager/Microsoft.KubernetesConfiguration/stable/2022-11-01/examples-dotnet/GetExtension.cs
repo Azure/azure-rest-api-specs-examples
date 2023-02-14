0bf8a41d8b337808ed20bfec97ade4843a6e9842
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-14 20:02:01 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.KubernetesConfiguration_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples-dotnet/GetExtension.cs
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples-dotnet/GetExtension.cs
@@ -4,32 +4,33 @@ using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
-using Azure.ResourceManager.KubernetesConfiguration.Models;
-using Azure.ResourceManager.Models;
-using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.KubernetesConfiguration;
+using Azure.ResourceManager.KubernetesConfiguration.Models;
 
 // Generated from example definition: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples/GetExtension.json
 // this example is just showing the usage of "Extensions_Get" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this KubernetesClusterExtensionResource created on azure
+// for more information of creating KubernetesClusterExtensionResource, please refer to the document of KubernetesClusterExtensionResource
 string subscriptionId = "subId1";
 string resourceGroupName = "rg1";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this KubernetesClusterExtensionResource
 string clusterRp = "Microsoft.Kubernetes";
 string clusterResourceName = "connectedClusters";
 string clusterName = "clusterName1";
-KubernetesClusterExtensionCollection collection = resourceGroupResource.GetKubernetesClusterExtensions(clusterRp, clusterResourceName, clusterName);
+string extensionName = "ClusterMonitor";
+ResourceIdentifier kubernetesClusterExtensionResourceId = KubernetesClusterExtensionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterRp, clusterResourceName, clusterName, extensionName);
+KubernetesClusterExtensionResource kubernetesClusterExtension = client.GetKubernetesClusterExtensionResource(kubernetesClusterExtensionResourceId);
 
 // invoke the operation
-string extensionName = "ClusterMonitor";
-bool result = await collection.ExistsAsync(extensionName);
+KubernetesClusterExtensionResource result = await kubernetesClusterExtension.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+KubernetesClusterExtensionData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

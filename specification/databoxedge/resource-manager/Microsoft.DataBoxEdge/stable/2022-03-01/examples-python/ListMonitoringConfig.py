95bd048e5ee35044a916b620d92083a5510ef308
azure-sdk <azuresdk@microsoft.com>
2023-02-14 12:30:38 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-databoxedge_2.0.0b1

---
+++ specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples-python/ListMonitoringConfig.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.databoxedge import DataBoxEdgeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-databoxedge
+# USAGE
+    python list_monitoring_config.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataBoxEdgeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="4385cf00-2d3a-425a-832f-f4285b1c9dce",
+    )
+
+    response = client.monitoring_config.list(
+        device_name="testedgedevice",
+        role_name="testrole",
+        resource_group_name="GroupForEdgeAutomation",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples/ListMonitoringConfig.json
+if __name__ == "__main__":
+    main()

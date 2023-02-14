701a7693c1f0ea11d343ccd1fe42496d26245d38
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-14 08:02:25 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Resources_1.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/resources/resource-manager/Microsoft.Resources/stable/2020-10-01/examples-dotnet/DeploymentScripts_Get.cs
+++ specification/resources/resource-manager/Microsoft.Resources/stable/2020-10-01/examples-dotnet/DeploymentScripts_Get.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using System.Xml;
 using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Models;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Resources.Models;
 
@@ -15,19 +17,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ArmDeploymentScriptResource created on azure
-// for more information of creating ArmDeploymentScriptResource, please refer to the document of ArmDeploymentScriptResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "script-rg";
-string scriptName = "MyDeploymentScript";
-ResourceIdentifier armDeploymentScriptResourceId = ArmDeploymentScriptResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, scriptName);
-ArmDeploymentScriptResource armDeploymentScript = client.GetArmDeploymentScriptResource(armDeploymentScriptResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ArmDeploymentScriptResource
+ArmDeploymentScriptCollection collection = resourceGroupResource.GetArmDeploymentScripts();
 
 // invoke the operation
-ArmDeploymentScriptResource result = await armDeploymentScript.GetAsync();
+string scriptName = "MyDeploymentScript";
+bool result = await collection.ExistsAsync(scriptName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ArmDeploymentScriptData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

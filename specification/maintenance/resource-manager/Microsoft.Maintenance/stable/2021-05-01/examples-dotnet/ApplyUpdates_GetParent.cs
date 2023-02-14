d55352ac4aa674797cd9baf494be887e570be922
azure-sdk <azuresdk@microsoft.com>
2023-02-14 00:17:51 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Maintenance_1.1.0

--- specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-dotnet/ApplyUpdates_GetParent.cs
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-dotnet/ApplyUpdates_GetParent.cs
@@ -3,14 +3,17 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
-using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Maintenance;
+using Azure.ResourceManager.Maintenance.Models;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples/ApplyUpdates_GetParent.json
 // this example is just showing the usage of "ApplyUpdates_GetParent" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
 // this example assumes you already have this ResourceGroupResource created on azure
 // for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
@@ -20,13 +23,8 @@ ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourc
 ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
 
 // invoke the operation
-string providerName = "Microsoft.Compute";
-string resourceParentType = "virtualMachineScaleSets";
-string resourceParentName = "smdtest1";
-string resourceType = "virtualMachines";
-string resourceName = "smdvm1";
-string applyUpdateName = "e9b9685d-78e4-44c4-a81c-64a14f9b87b6";
-MaintenanceApplyUpdateResource result = await resourceGroupResource.GetApplyUpdatesByParentAsync(providerName, resourceParentType, resourceParentName, resourceType, resourceName, applyUpdateName);
+ResourceGroupResourceGetApplyUpdatesByParentOptions options = new ResourceGroupResourceGetApplyUpdatesByParentOptions(providerName: "Microsoft.Compute", resourceParentType: "virtualMachineScaleSets", resourceParentName: "smdtest1", resourceType: "virtualMachines", resourceName: "smdvm1", applyUpdateName: "e9b9685d-78e4-44c4-a81c-64a14f9b87b6") { };
+MaintenanceApplyUpdateResource result = await resourceGroupResource.GetApplyUpdatesByParentAsync(options);
 
 // the variable result is a resource, you could call other operations on this instance as well
 // but just for demo, we get its data from this resource instance
--- specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-dotnet/MaintenanceConfigurations_DeleteForResource.cs
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-dotnet/MaintenanceConfigurations_DeleteForResource.cs
@@ -4,15 +4,17 @@ using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Maintenance;
 using Azure.ResourceManager.Maintenance.Models;
 using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.Maintenance;
 
 // Generated from example definition: specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples/MaintenanceConfigurations_DeleteForResource.json
 // this example is just showing the usage of "MaintenanceConfigurations_Delete" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
 // this example assumes you already have this MaintenanceConfigurationResource created on azure
 // for more information of creating MaintenanceConfigurationResource, please refer to the document of MaintenanceConfigurationResource

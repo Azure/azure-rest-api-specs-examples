c31ce9806f9c167f65755361310557c94698e7c1
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-15 08:02:54 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-applicationinsights_4.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-04-01/examples-python/WorkbooksManagedList.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.applicationinsights import ApplicationInsightsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-applicationinsights
+# USAGE
+    python workbooks_managed_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ApplicationInsightsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="6b643656-33eb-422f-aee8-3ac119r124af",
+    )
+
+    response = client.workbooks.list_by_resource_group(
+        resource_group_name="my-resource-group",
+        category="workbook",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-04-01/examples/WorkbooksManagedList.json
+if __name__ == "__main__":
+    main()

311ec15b71c0571ef30a5656663c9e855a848382
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-15 08:02:31 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-databoxedge_2.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples-python/OperationsGet.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.databoxedge import DataBoxEdgeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-databoxedge
+# USAGE
+    python operations_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataBoxEdgeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.operations.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples/OperationsGet.json
+if __name__ == "__main__":
+    main()

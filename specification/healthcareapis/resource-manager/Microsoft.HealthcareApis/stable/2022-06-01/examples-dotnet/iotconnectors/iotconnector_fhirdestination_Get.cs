8d4730dda8a061848eb509d2b47dd59e946b7dd7
azure-sdk <azuresdk@microsoft.com>
2023-02-15 12:10:29 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HealthcareApis_1.0.1

--- specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2022-06-01/examples-dotnet/iotconnectors/iotconnector_fhirdestination_Get.cs
+++ specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2022-06-01/examples-dotnet/iotconnectors/iotconnector_fhirdestination_Get.cs
@@ -16,21 +16,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this HealthcareApisIotFhirDestinationResource created on azure
-// for more information of creating HealthcareApisIotFhirDestinationResource, please refer to the document of HealthcareApisIotFhirDestinationResource
+// this example assumes you already have this HealthcareApisIotConnectorResource created on azure
+// for more information of creating HealthcareApisIotConnectorResource, please refer to the document of HealthcareApisIotConnectorResource
 string subscriptionId = "subid";
 string resourceGroupName = "testRG";
 string workspaceName = "workspace1";
 string iotConnectorName = "blue";
-string fhirDestinationName = "dest1";
-ResourceIdentifier healthcareApisIotFhirDestinationResourceId = HealthcareApisIotFhirDestinationResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName, iotConnectorName, fhirDestinationName);
-HealthcareApisIotFhirDestinationResource healthcareApisIotFhirDestination = client.GetHealthcareApisIotFhirDestinationResource(healthcareApisIotFhirDestinationResourceId);
+ResourceIdentifier healthcareApisIotConnectorResourceId = HealthcareApisIotConnectorResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName, iotConnectorName);
+HealthcareApisIotConnectorResource healthcareApisIotConnector = client.GetHealthcareApisIotConnectorResource(healthcareApisIotConnectorResourceId);
+
+// get the collection of this HealthcareApisIotFhirDestinationResource
+HealthcareApisIotFhirDestinationCollection collection = healthcareApisIotConnector.GetHealthcareApisIotFhirDestinations();
 
 // invoke the operation
-HealthcareApisIotFhirDestinationResource result = await healthcareApisIotFhirDestination.GetAsync();
+string fhirDestinationName = "dest1";
+bool result = await collection.ExistsAsync(fhirDestinationName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-HealthcareApisIotFhirDestinationData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

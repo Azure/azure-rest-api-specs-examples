27f334d515949554bf35f965dd791811c4e2b807
azure-sdk <azuresdk@microsoft.com>
2023-02-16 12:36:10 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Analysis_1.0.1

--- specification/analysisservices/resource-manager/Microsoft.AnalysisServices/stable/2017-08-01/examples-dotnet/updateServer.cs
+++ specification/analysisservices/resource-manager/Microsoft.AnalysisServices/stable/2017-08-01/examples-dotnet/updateServer.cs
@@ -4,15 +4,17 @@ using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Analysis;
 using Azure.ResourceManager.Analysis.Models;
 using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.Analysis;
 
 // Generated from example definition: specification/analysisservices/resource-manager/Microsoft.AnalysisServices/stable/2017-08-01/examples/updateServer.json
 // this example is just showing the usage of "Servers_Update" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
 // this example assumes you already have this AnalysisServerResource created on azure
 // for more information of creating AnalysisServerResource, please refer to the document of AnalysisServerResource

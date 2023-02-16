8fc112065e1dcf6816e1b153fb64a688ae1a1478
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-16 08:02:35 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-authorization_3.1.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/authorization/resource-manager/Microsoft.Authorization/stable/2022-04-01/examples-python/GetResourcePermissions.py
@@ -0,0 +1,37 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.authorization import AuthorizationManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-authorization
+# USAGE
+    python get_resource_permissions.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AuthorizationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subId",
+    )
+
+    response = client.permissions.list_for_resource(
+        resource_group_name="rgname",
+        resource_provider_namespace="rpnamespace",
+        parent_resource_path="parentResourcePath",
+        resource_type="resourceType",
+        resource_name="resourceName",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/authorization/resource-manager/Microsoft.Authorization/stable/2022-04-01/examples/GetResourcePermissions.json
+if __name__ == "__main__":
+    main()

b4b7f124de61904f7f0dea66de79f8508584ad13
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-16 08:02:50 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceLinker_1.0.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples-dotnet/Link.cs
+++ specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples-dotnet/Link.cs
@@ -15,16 +15,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ArmResource created on azure
-// for more information of creating ArmResource, please refer to the document of ArmResource
-
-// get the collection of this LinkerResource
+// this example assumes you already have this LinkerResource created on azure
+// for more information of creating LinkerResource, please refer to the document of LinkerResource
 string resourceUri = "subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.Web/sites/test-app";
-ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", resourceUri));
-LinkerResourceCollection collection = client.GetLinkerResources(scopeId);
+string linkerName = "linkName";
+ResourceIdentifier linkerResourceId = LinkerResource.CreateResourceIdentifier(resourceUri, linkerName);
+LinkerResource linkerResource = client.GetLinkerResource(linkerResourceId);
 
 // invoke the operation
-string linkerName = "linkName";
-bool result = await collection.ExistsAsync(linkerName);
+LinkerResource result = await linkerResource.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+LinkerResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

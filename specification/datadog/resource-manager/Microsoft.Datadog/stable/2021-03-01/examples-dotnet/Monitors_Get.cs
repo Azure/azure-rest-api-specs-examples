789c1fa73cd340eb469c6b0841f66f2824c6e513
azure-sdk <azuresdk@microsoft.com>
2023-02-16 12:26:21 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Datadog_1.0.0-beta.2

--- specification/datadog/resource-manager/Microsoft.Datadog/stable/2021-03-01/examples-dotnet/Monitors_Get.cs
+++ specification/datadog/resource-manager/Microsoft.Datadog/stable/2021-03-01/examples-dotnet/Monitors_Get.cs
@@ -5,7 +5,9 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Datadog;
+using Azure.ResourceManager.Datadog.Models;
 using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
 
 // Generated from example definition: specification/datadog/resource-manager/Microsoft.Datadog/stable/2021-03-01/examples/Monitors_Get.json
 // this example is just showing the usage of "Monitors_Get" operation, for the dependent resources, they will have to be created separately.
@@ -15,18 +17,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this DatadogMonitorResource created on azure
+// for more information of creating DatadogMonitorResource, please refer to the document of DatadogMonitorResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this DatadogMonitorResource
-DatadogMonitorResourceCollection collection = resourceGroupResource.GetDatadogMonitorResources();
+string monitorName = "myMonitor";
+ResourceIdentifier datadogMonitorResourceId = DatadogMonitorResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, monitorName);
+DatadogMonitorResource datadogMonitorResource = client.GetDatadogMonitorResource(datadogMonitorResourceId);
 
 // invoke the operation
-string monitorName = "myMonitor";
-bool result = await collection.ExistsAsync(monitorName);
+DatadogMonitorResource result = await datadogMonitorResource.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DatadogMonitorResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

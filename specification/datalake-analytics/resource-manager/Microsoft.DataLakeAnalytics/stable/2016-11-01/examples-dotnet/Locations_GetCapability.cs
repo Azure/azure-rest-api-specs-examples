5009edf6f8f68a937331335a72a2cf6be7f3ae06
azure-sdk <azuresdk@microsoft.com>
2023-02-16 12:22:08 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DataLakeAnalytics_1.1.0-beta.1

--- specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples-dotnet/Locations_GetCapability.cs
+++ specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples-dotnet/Locations_GetCapability.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.DataLakeAnalytics;
 using Azure.ResourceManager.DataLakeAnalytics.Models;
 using Azure.ResourceManager.Resources;
 
--- specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples-dotnet/StorageAccounts_GetStorageContainer.cs
+++ specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples-dotnet/StorageAccounts_GetStorageContainer.cs
@@ -4,6 +4,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.DataLakeAnalytics;
+using Azure.ResourceManager.DataLakeAnalytics.Models;
 
 // Generated from example definition: specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples/StorageAccounts_GetStorageContainer.json
 // this example is just showing the usage of "StorageAccounts_GetStorageContainer" operation, for the dependent resources, they will have to be created separately.
@@ -13,20 +14,21 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DataLakeAnalyticsStorageAccountInformationResource created on azure
-// for more information of creating DataLakeAnalyticsStorageAccountInformationResource, please refer to the document of DataLakeAnalyticsStorageAccountInformationResource
+// this example assumes you already have this DataLakeAnalyticsStorageContainerResource created on azure
+// for more information of creating DataLakeAnalyticsStorageContainerResource, please refer to the document of DataLakeAnalyticsStorageContainerResource
 string subscriptionId = "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
 string resourceGroupName = "contosorg";
 string accountName = "contosoadla";
 string storageAccountName = "test_storage";
-ResourceIdentifier dataLakeAnalyticsStorageAccountInformationResourceId = DataLakeAnalyticsStorageAccountInformationResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, storageAccountName);
-DataLakeAnalyticsStorageAccountInformationResource dataLakeAnalyticsStorageAccountInformation = client.GetDataLakeAnalyticsStorageAccountInformationResource(dataLakeAnalyticsStorageAccountInformationResourceId);
-
-// get the collection of this DataLakeAnalyticsStorageContainerResource
-DataLakeAnalyticsStorageContainerCollection collection = dataLakeAnalyticsStorageAccountInformation.GetDataLakeAnalyticsStorageContainers();
+string containerName = "test_container";
+ResourceIdentifier dataLakeAnalyticsStorageContainerResourceId = DataLakeAnalyticsStorageContainerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, storageAccountName, containerName);
+DataLakeAnalyticsStorageContainerResource dataLakeAnalyticsStorageContainer = client.GetDataLakeAnalyticsStorageContainerResource(dataLakeAnalyticsStorageContainerResourceId);
 
 // invoke the operation
-string containerName = "test_container";
-bool result = await collection.ExistsAsync(containerName);
+DataLakeAnalyticsStorageContainerResource result = await dataLakeAnalyticsStorageContainer.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DataLakeAnalyticsStorageContainerData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

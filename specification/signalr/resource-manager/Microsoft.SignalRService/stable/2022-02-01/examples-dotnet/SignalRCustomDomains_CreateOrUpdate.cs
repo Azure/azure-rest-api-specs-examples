3746114aaed74eddb0c73bc36f24cce55e71d64a
azure-sdk <azuresdk@microsoft.com>
2023-02-17 12:58:13 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.SignalR_1.0.1

--- specification/signalr/resource-manager/Microsoft.SignalRService/stable/2022-02-01/examples-dotnet/SignalRCustomDomains_CreateOrUpdate.cs
+++ specification/signalr/resource-manager/Microsoft.SignalRService/stable/2022-02-01/examples-dotnet/SignalRCustomDomains_CreateOrUpdate.cs
@@ -10,27 +10,26 @@ using Azure.ResourceManager.SignalR;
 // Generated from example definition: specification/signalr/resource-manager/Microsoft.SignalRService/stable/2022-02-01/examples/SignalRCustomDomains_CreateOrUpdate.json
 // this example is just showing the usage of "SignalRCustomDomains_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SignalRResource created on azure
-// for more information of creating SignalRResource, please refer to the document of SignalRResource
+// this example assumes you already have this SignalRCustomDomainResource created on azure
+// for more information of creating SignalRCustomDomainResource, please refer to the document of SignalRCustomDomainResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
 string resourceName = "mySignalRService";
-ResourceIdentifier signalRResourceId = SignalRResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
-SignalRResource signalR = client.GetSignalRResource(signalRResourceId);
-
-// get the collection of this SignalRCustomDomainResource
-SignalRCustomDomainCollection collection = signalR.GetSignalRCustomDomains();
+string name = "myDomain";
+ResourceIdentifier signalRCustomDomainResourceId = SignalRCustomDomainResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, name);
+SignalRCustomDomainResource signalRCustomDomain = client.GetSignalRCustomDomainResource(signalRCustomDomainResourceId);
 
 // invoke the operation
-string name = "myDomain";
 SignalRCustomDomainData data = new SignalRCustomDomainData("example.com", new WritableSubResource()
 {
     Id = new ResourceIdentifier("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/myResourceGroup/providers/Microsoft.SignalRService/SignalR/mySignalRService/customCertificates/myCert"),
 });
-ArmOperation<SignalRCustomDomainResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name, data);
+ArmOperation<SignalRCustomDomainResource> lro = await signalRCustomDomain.UpdateAsync(WaitUntil.Completed, data);
 SignalRCustomDomainResource result = lro.Value;
 
 // the variable result is a resource, you could call other operations on this instance as well
--- specification/signalr/resource-manager/Microsoft.SignalRService/stable/2022-02-01/examples-dotnet/SignalR_Get.cs
+++ specification/signalr/resource-manager/Microsoft.SignalRService/stable/2022-02-01/examples-dotnet/SignalR_Get.cs
@@ -6,28 +6,29 @@ using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Models;
 using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.SignalR.Models;
 using Azure.ResourceManager.SignalR;
+using Azure.ResourceManager.SignalR.Models;
 
 // Generated from example definition: specification/signalr/resource-manager/Microsoft.SignalRService/stable/2022-02-01/examples/SignalR_Get.json
 // this example is just showing the usage of "SignalR_Get" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SignalRResource created on azure
-// for more information of creating SignalRResource, please refer to the document of SignalRResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
-string resourceName = "mySignalRService";
-ResourceIdentifier signalRResourceId = SignalRResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
-SignalRResource signalR = client.GetSignalRResource(signalRResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this SignalRResource
+SignalRCollection collection = resourceGroupResource.GetSignalRs();
 
 // invoke the operation
-SignalRResource result = await signalR.GetAsync();
+string resourceName = "mySignalRService";
+bool result = await collection.ExistsAsync(resourceName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-SignalRData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

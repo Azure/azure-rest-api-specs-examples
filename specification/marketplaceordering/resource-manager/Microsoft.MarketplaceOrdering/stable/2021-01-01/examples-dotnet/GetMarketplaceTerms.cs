1dbb1ca7859a8b0e19e21ed0a8c065ce815bf3d8
azure-sdk <azuresdk@microsoft.com>
2023-02-17 12:50:58 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MarketplaceOrdering_1.0.1

---
+++ specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-dotnet/GetMarketplaceTerms.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.MarketplaceOrdering;
+using Azure.ResourceManager.MarketplaceOrdering.Models;
+
+// Generated from example definition: specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples/GetMarketplaceTerms.json
+// this example is just showing the usage of "MarketplaceAgreements_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this MarketplaceAgreementTermResource created on azure
+// for more information of creating MarketplaceAgreementTermResource, please refer to the document of MarketplaceAgreementTermResource
+string subscriptionId = "subid";
+AgreementOfferType offerType = AgreementOfferType.Virtualmachine;
+string publisherId = "pubid";
+string offerId = "offid";
+string planId = "planid";
+ResourceIdentifier marketplaceAgreementTermResourceId = MarketplaceAgreementTermResource.CreateResourceIdentifier(subscriptionId, offerType, publisherId, offerId, planId);
+MarketplaceAgreementTermResource marketplaceAgreementTerm = client.GetMarketplaceAgreementTermResource(marketplaceAgreementTermResourceId);
+
+// invoke the operation
+MarketplaceAgreementTermResource result = await marketplaceAgreementTerm.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+MarketplaceAgreementTermData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

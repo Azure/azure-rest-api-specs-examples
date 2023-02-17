d40096bd6f62abe17b84df1ee35117fb0a10f8dd
azure-sdk <azuresdk@microsoft.com>
2023-02-17 12:20:41 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceNetworking_1.0.0-beta.1

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-dotnet/FrontendPut.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ServiceNetworking;
+using Azure.ResourceManager.ServiceNetworking.Models;
+
+// Generated from example definition: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/FrontendPut.json
+// this example is just showing the usage of "FrontendsInterface_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TrafficControllerResource created on azure
+// for more information of creating TrafficControllerResource, please refer to the document of TrafficControllerResource
+string subscriptionId = "subid";
+string resourceGroupName = "rg1";
+string trafficControllerName = "TC1";
+ResourceIdentifier trafficControllerResourceId = TrafficControllerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, trafficControllerName);
+TrafficControllerResource trafficController = client.GetTrafficControllerResource(trafficControllerResourceId);
+
+// get the collection of this FrontendResource
+FrontendCollection collection = trafficController.GetFrontends();
+
+// invoke the operation
+string frontendName = "publicIp1";
+FrontendData data = new FrontendData(new AzureLocation("West US"))
+{
+    Mode = FrontendMode.Public,
+    IPAddressVersion = FrontendIPAddressVersion.IPv4,
+    PublicIPAddressId = new ResourceIdentifier("resourceUriAsString"),
+};
+ArmOperation<FrontendResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, frontendName, data);
+FrontendResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+FrontendData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

1213c6604d78e2aa6136187bd814d2d56568be32
azure-sdk <azuresdk@microsoft.com>
2023-02-17 12:35:21 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.EdgeOrder_1.0.1

--- specification/edgeorder/resource-manager/Microsoft.EdgeOrder/stable/2021-12-01/examples-dotnet/GetOrderByName.cs
+++ specification/edgeorder/resource-manager/Microsoft.EdgeOrder/stable/2021-12-01/examples-dotnet/GetOrderByName.cs
@@ -4,6 +4,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.EdgeOrder;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/edgeorder/resource-manager/Microsoft.EdgeOrder/stable/2021-12-01/examples/GetOrderByName.json
 // this example is just showing the usage of "GetOrderByName" operation, for the dependent resources, they will have to be created separately.
@@ -13,20 +14,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this EdgeOrderResource created on azure
-// for more information of creating EdgeOrderResource, please refer to the document of EdgeOrderResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "fa68082f-8ff7-4a25-95c7-ce9da541242f";
 string resourceGroupName = "TestRG";
-AzureLocation location = new AzureLocation("%7B%7B%7Blocation%7D%7D");
-string orderName = "TestOrderItemName901";
-ResourceIdentifier edgeOrderResourceId = EdgeOrderResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, location, orderName);
-EdgeOrderResource edgeOrder = client.GetEdgeOrderResource(edgeOrderResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this EdgeOrderResource
+EdgeOrderCollection collection = resourceGroupResource.GetEdgeOrders();
 
 // invoke the operation
-EdgeOrderResource result = await edgeOrder.GetAsync();
+AzureLocation location = new AzureLocation("%7B%7B%7Blocation%7D%7D");
+string orderName = "TestOrderItemName901";
+bool result = await collection.ExistsAsync(location, orderName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-EdgeOrderData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

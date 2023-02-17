d9f0db5393dee9644f2d573494939411ec7f543f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-17 08:04:25 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DevSpaces_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/devspaces/resource-manager/Microsoft.DevSpaces/stable/2019-04-01/examples-dotnet/ControllersUpdate_example.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.DevSpaces;
+using Azure.ResourceManager.DevSpaces.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/devspaces/resource-manager/Microsoft.DevSpaces/stable/2019-04-01/examples/ControllersUpdate_example.json
+// this example is just showing the usage of "Controllers_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ControllerResource created on azure
+// for more information of creating ControllerResource, please refer to the document of ControllerResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string name = "myControllerResource";
+ResourceIdentifier controllerResourceId = ControllerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, name);
+ControllerResource controller = client.GetControllerResource(controllerResourceId);
+
+// invoke the operation
+ControllerPatch patch = new ControllerPatch()
+{
+    Tags =
+    {
+    ["key"] = "value",
+    },
+    TargetContainerHostCredentialsBase64 = "QmFzZTY0IEVuY29kZWQgVmFsdWUK",
+};
+ControllerResource result = await controller.UpdateAsync(patch);
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ControllerData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

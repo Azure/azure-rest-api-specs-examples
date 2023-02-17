ddd1027fe5b84a76562e8cf46679ba23324324b6
azure-sdk <azuresdk@microsoft.com>
2023-02-17 12:53:24 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceGraph_1.0.1

---
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples-dotnet/ResourcesMgBasicQuery.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ResourceGraph;
+using Azure.ResourceManager.ResourceGraph.Models;
+
+// Generated from example definition: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples/ResourcesMgBasicQuery.json
+// this example is just showing the usage of "Resources" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TenantResource created on azure
+// for more information of creating TenantResource, please refer to the document of TenantResource
+var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+// invoke the operation
+ResourceQueryContent content = new ResourceQueryContent("Resources | project id, name, type, location, tags | limit 3")
+{
+    ManagementGroups =
+    {
+    "e927f598-c1d4-4f72-8541-95d83a6a4ac8","ProductionMG"
+    },
+};
+ResourceQueryResult result = await tenantResource.GetResourcesAsync(content);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples-dotnet/ResourcesRandomPageQuery.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ResourceGraph;
+using Azure.ResourceManager.ResourceGraph.Models;
+
+// Generated from example definition: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples/ResourcesRandomPageQuery.json
+// this example is just showing the usage of "Resources" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TenantResource created on azure
+// for more information of creating TenantResource, please refer to the document of TenantResource
+var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+// invoke the operation
+ResourceQueryContent content = new ResourceQueryContent("Resources | where name contains 'test' | project id, name, type, location")
+{
+    Subscriptions =
+    {
+    "cfbbd179-59d2-4052-aa06-9270a38aa9d6"
+    },
+    Options = new ResourceQueryRequestOptions()
+    {
+        Top = 2,
+        Skip = 10,
+    },
+};
+ResourceQueryResult result = await tenantResource.GetResourcesAsync(content);
+
+Console.WriteLine($"Succeeded: {result}");

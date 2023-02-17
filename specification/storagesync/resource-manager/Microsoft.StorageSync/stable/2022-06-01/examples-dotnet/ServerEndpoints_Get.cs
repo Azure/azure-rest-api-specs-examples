088343580adc87885325232e13aeab5e38cf3758
azure-sdk <azuresdk@microsoft.com>
2023-02-17 12:48:33 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StorageSync_1.1.1

--- specification/storagesync/resource-manager/Microsoft.StorageSync/stable/2022-06-01/examples-dotnet/ServerEndpoints_Get.cs
+++ specification/storagesync/resource-manager/Microsoft.StorageSync/stable/2022-06-01/examples-dotnet/ServerEndpoints_Get.cs
@@ -15,20 +15,21 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this StorageSyncGroupResource created on azure
-// for more information of creating StorageSyncGroupResource, please refer to the document of StorageSyncGroupResource
+// this example assumes you already have this StorageSyncServerEndpointResource created on azure
+// for more information of creating StorageSyncServerEndpointResource, please refer to the document of StorageSyncServerEndpointResource
 string subscriptionId = "52b8da2f-61e0-4a1f-8dde-336911f367fb";
 string resourceGroupName = "SampleResourceGroup_1";
 string storageSyncServiceName = "SampleStorageSyncService_1";
 string syncGroupName = "SampleSyncGroup_1";
-ResourceIdentifier storageSyncGroupResourceId = StorageSyncGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, storageSyncServiceName, syncGroupName);
-StorageSyncGroupResource storageSyncGroup = client.GetStorageSyncGroupResource(storageSyncGroupResourceId);
-
-// get the collection of this StorageSyncServerEndpointResource
-StorageSyncServerEndpointCollection collection = storageSyncGroup.GetStorageSyncServerEndpoints();
+string serverEndpointName = "SampleServerEndpoint_1";
+ResourceIdentifier storageSyncServerEndpointResourceId = StorageSyncServerEndpointResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, storageSyncServiceName, syncGroupName, serverEndpointName);
+StorageSyncServerEndpointResource storageSyncServerEndpoint = client.GetStorageSyncServerEndpointResource(storageSyncServerEndpointResourceId);
 
 // invoke the operation
-string serverEndpointName = "SampleServerEndpoint_1";
-bool result = await collection.ExistsAsync(serverEndpointName);
+StorageSyncServerEndpointResource result = await storageSyncServerEndpoint.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+StorageSyncServerEndpointData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

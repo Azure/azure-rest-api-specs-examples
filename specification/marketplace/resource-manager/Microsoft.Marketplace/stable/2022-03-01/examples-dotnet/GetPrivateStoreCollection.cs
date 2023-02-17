374565688a780466c173fe7c91d28c2fb8147858
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-17 20:02:57 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Marketplace_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples-dotnet/GetPrivateStoreCollection.cs
+++ specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples-dotnet/GetPrivateStoreCollection.cs
@@ -5,7 +5,6 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Marketplace;
-using Azure.ResourceManager.Marketplace.Models;
 
 // Generated from example definition: specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples/GetPrivateStoreCollection.json
 // this example is just showing the usage of "PrivateStoreCollection_Get" operation, for the dependent resources, they will have to be created separately.
@@ -15,18 +14,17 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this PrivateStoreCollectionInfoResource created on azure
-// for more information of creating PrivateStoreCollectionInfoResource, please refer to the document of PrivateStoreCollectionInfoResource
+// this example assumes you already have this PrivateStoreResource created on azure
+// for more information of creating PrivateStoreResource, please refer to the document of PrivateStoreResource
 Guid privateStoreId = Guid.Parse("a0e28e55-90c4-41d8-8e34-bb7ef7775406");
-Guid collectionId = Guid.Parse("56a1a02d-8cf8-45df-bf37-d5f7120fcb3d");
-ResourceIdentifier privateStoreCollectionInfoResourceId = PrivateStoreCollectionInfoResource.CreateResourceIdentifier(privateStoreId, collectionId);
-PrivateStoreCollectionInfoResource privateStoreCollectionInfo = client.GetPrivateStoreCollectionInfoResource(privateStoreCollectionInfoResourceId);
+ResourceIdentifier privateStoreResourceId = PrivateStoreResource.CreateResourceIdentifier(privateStoreId);
+PrivateStoreResource privateStore = client.GetPrivateStoreResource(privateStoreResourceId);
+
+// get the collection of this PrivateStoreCollectionInfoResource
+PrivateStoreCollectionInfoCollection collection = privateStore.GetPrivateStoreCollectionInfos();
 
 // invoke the operation
-PrivateStoreCollectionInfoResource result = await privateStoreCollectionInfo.GetAsync();
+Guid collectionId = Guid.Parse("56a1a02d-8cf8-45df-bf37-d5f7120fcb3d");
+bool result = await collection.ExistsAsync(collectionId);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-PrivateStoreCollectionInfoData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

4121da430c69c8a03612a848f48d3c544d802a48
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-18 08:02:42 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MarketplaceOrdering_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-dotnet/GetAgreementMarketplaceTerms.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.MarketplaceOrdering;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples/GetAgreementMarketplaceTerms.json
+// this example is just showing the usage of "MarketplaceAgreements_GetAgreement" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "subid";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// get the collection of this MarketplaceAgreementResource
+MarketplaceAgreementCollection collection = subscriptionResource.GetMarketplaceAgreements();
+
+// invoke the operation
+string publisherId = "pubid";
+string offerId = "offid";
+string planId = "planid";
+bool result = await collection.ExistsAsync(publisherId, offerId, planId);
+
+Console.WriteLine($"Succeeded: {result}");

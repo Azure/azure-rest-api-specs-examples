7cf49f45af5d78727f1040f4e9a9df0ba43243bb
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-19 20:02:47 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Support_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples-dotnet/GetSubscriptionSupportTicketDetails.cs
+++ specification/support/resource-manager/Microsoft.Support/stable/2020-04-01/examples-dotnet/GetSubscriptionSupportTicketDetails.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -15,18 +16,17 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SupportTicketResource created on azure
-// for more information of creating SupportTicketResource, please refer to the document of SupportTicketResource
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
 string subscriptionId = "subid";
-string supportTicketName = "testticket";
-ResourceIdentifier supportTicketResourceId = SupportTicketResource.CreateResourceIdentifier(subscriptionId, supportTicketName);
-SupportTicketResource supportTicket = client.GetSupportTicketResource(supportTicketResourceId);
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// get the collection of this SupportTicketResource
+SupportTicketCollection collection = subscriptionResource.GetSupportTickets();
 
 // invoke the operation
-SupportTicketResource result = await supportTicket.GetAsync();
+string supportTicketName = "testticket";
+bool result = await collection.ExistsAsync(supportTicketName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-SupportTicketData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

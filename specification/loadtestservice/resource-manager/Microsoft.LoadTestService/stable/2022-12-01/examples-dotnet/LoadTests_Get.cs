07fc1c0596e2b1b1d077b9087ea7d2f0223d41df
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-19 20:02:12 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.LoadTesting_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-dotnet/LoadTests_Get.cs
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-dotnet/LoadTests_Get.cs
@@ -17,19 +17,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this LoadTestingResource created on azure
-// for more information of creating LoadTestingResource, please refer to the document of LoadTestingResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "dummyrg";
-string loadTestName = "myLoadTest";
-ResourceIdentifier loadTestingResourceId = LoadTestingResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, loadTestName);
-LoadTestingResource loadTestingResource = client.GetLoadTestingResource(loadTestingResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this LoadTestingResource
+LoadTestingResourceCollection collection = resourceGroupResource.GetLoadTestingResources();
 
 // invoke the operation
-LoadTestingResource result = await loadTestingResource.GetAsync();
+string loadTestName = "myLoadTest";
+bool result = await collection.ExistsAsync(loadTestName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-LoadTestingResourceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

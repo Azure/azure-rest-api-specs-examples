d13195a6d551afbb811d623d73c0f80e4d5bc63b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-19 20:02:19 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HybridCompute_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2022-05-10-preview/examples-dotnet/PrivateLinkScopePrivateLinkResourceGet.cs
+++ specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2022-05-10-preview/examples-dotnet/PrivateLinkScopePrivateLinkResourceGet.cs
@@ -13,19 +13,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this HybridComputePrivateLinkScopeResource created on azure
-// for more information of creating HybridComputePrivateLinkScopeResource, please refer to the document of HybridComputePrivateLinkScopeResource
+// this example assumes you already have this HybridComputePrivateLinkResource created on azure
+// for more information of creating HybridComputePrivateLinkResource, please refer to the document of HybridComputePrivateLinkResource
 string subscriptionId = "00000000-1111-2222-3333-444444444444";
 string resourceGroupName = "myResourceGroup";
 string scopeName = "myPrivateLinkScope";
-ResourceIdentifier hybridComputePrivateLinkScopeResourceId = HybridComputePrivateLinkScopeResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, scopeName);
-HybridComputePrivateLinkScopeResource hybridComputePrivateLinkScope = client.GetHybridComputePrivateLinkScopeResource(hybridComputePrivateLinkScopeResourceId);
-
-// get the collection of this HybridComputePrivateLinkResource
-HybridComputePrivateLinkResourceCollection collection = hybridComputePrivateLinkScope.GetHybridComputePrivateLinkResources();
+string groupName = "hybridcompute";
+ResourceIdentifier hybridComputePrivateLinkResourceId = HybridComputePrivateLinkResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, scopeName, groupName);
+HybridComputePrivateLinkResource hybridComputePrivateLinkResource = client.GetHybridComputePrivateLinkResource(hybridComputePrivateLinkResourceId);
 
 // invoke the operation
-string groupName = "hybridcompute";
-bool result = await collection.ExistsAsync(groupName);
+HybridComputePrivateLinkResource result = await hybridComputePrivateLinkResource.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+HybridComputePrivateLinkResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

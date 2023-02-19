19be1360927cae450275633d772265d3b871e83e
azure-sdk <azuresdk@microsoft.com>
2023-02-19 12:42:34 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-servicebus_8.2.0

---
+++ specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2021-11-01/examples-python/Subscriptions/SBSubscriptionGet.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.servicebus import ServiceBusManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-servicebus
+# USAGE
+    python sb_subscription_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ServiceBusManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="Subscriptionid",
+    )
+
+    response = client.subscriptions.get(
+        resource_group_name="ResourceGroup",
+        namespace_name="sdk-Namespace-1349",
+        topic_name="sdk-Topics-8740",
+        subscription_name="sdk-Subscriptions-2178",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2021-11-01/examples/Subscriptions/SBSubscriptionGet.json
+if __name__ == "__main__":
+    main()

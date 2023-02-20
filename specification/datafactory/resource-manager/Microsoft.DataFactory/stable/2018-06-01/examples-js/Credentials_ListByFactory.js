a11417ab682d89d2b6cebc95d639214157c3dfe9
azure-sdk <azuresdk@microsoft.com>
2023-02-20 12:09:26 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-datafactory_11.0.0

---
+++ specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples-js/Credentials_ListByFactory.js
@@ -0,0 +1,25 @@
+const { DataFactoryManagementClient } = require("@azure/arm-datafactory");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List credentials.
+ *
+ * @summary List credentials.
+ * x-ms-original-file: specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples/Credentials_ListByFactory.json
+ */
+async function credentialsListByFactory() {
+  const subscriptionId =
+    process.env["DATAFACTORY_SUBSCRIPTION_ID"] || "12345678-1234-1234-1234-12345678abc";
+  const resourceGroupName = process.env["DATAFACTORY_RESOURCE_GROUP"] || "exampleResourceGroup";
+  const factoryName = "exampleFactoryName";
+  const credential = new DefaultAzureCredential();
+  const client = new DataFactoryManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.credentialOperations.listByFactory(
+    resourceGroupName,
+    factoryName
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

7477b076ec92595a308d15b60bd147a2b0fe6e7f
azure-sdk <azuresdk@microsoft.com>
2023-02-20 12:13:53 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ManagedServiceIdentities_1.1.0

---
+++ specification/msi/resource-manager/Microsoft.ManagedIdentity/stable/2023-01-31/examples-dotnet/IdentityGet.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ManagedServiceIdentities;
+using Azure.ResourceManager.ManagedServiceIdentities.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/msi/resource-manager/Microsoft.ManagedIdentity/stable/2023-01-31/examples/IdentityGet.json
+// this example is just showing the usage of "UserAssignedIdentities_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this UserAssignedIdentityResource created on azure
+// for more information of creating UserAssignedIdentityResource, please refer to the document of UserAssignedIdentityResource
+string subscriptionId = "subid";
+string resourceGroupName = "rgName";
+string resourceName = "resourceName";
+ResourceIdentifier userAssignedIdentityResourceId = UserAssignedIdentityResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
+UserAssignedIdentityResource userAssignedIdentity = client.GetUserAssignedIdentityResource(userAssignedIdentityResourceId);
+
+// invoke the operation
+UserAssignedIdentityResource result = await userAssignedIdentity.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+UserAssignedIdentityData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

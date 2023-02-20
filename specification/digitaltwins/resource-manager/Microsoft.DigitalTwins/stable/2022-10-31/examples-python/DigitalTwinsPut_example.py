5b7d8867d10f959946c0426bf8befef884751639
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-20 08:04:17 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-digitaltwins_6.4.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples-python/DigitalTwinsPut_example.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.digitaltwins import AzureDigitalTwinsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-digitaltwins
+# USAGE
+    python digital_twins_put_example.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureDigitalTwinsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="50016170-c839-41ba-a724-51e9df440b9e",
+    )
+
+    response = client.digital_twins.begin_create_or_update(
+        resource_group_name="resRg",
+        resource_name="myDigitalTwinsService",
+        digital_twins_create={"location": "WestUS2"},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsPut_example.json
+if __name__ == "__main__":
+    main()

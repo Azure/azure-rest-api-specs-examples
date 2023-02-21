e2db63a56b600d2e2c342d03a7ceeb1f8a677a72
azure-sdk <azuresdk@microsoft.com>
2023-02-21 00:43:38 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.LabServices_1.0.1

--- specification/labservices/resource-manager/Microsoft.LabServices/stable/2022-08-01/examples-dotnet/Skus/listSkus.cs
+++ specification/labservices/resource-manager/Microsoft.LabServices/stable/2022-08-01/examples-dotnet/Skus/listSkus.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.LabServices;
 using Azure.ResourceManager.LabServices.Models;
 using Azure.ResourceManager.Resources;
 

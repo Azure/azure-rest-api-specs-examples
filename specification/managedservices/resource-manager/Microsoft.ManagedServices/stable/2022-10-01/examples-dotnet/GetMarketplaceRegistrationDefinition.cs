797f4fefbd1b6ca6ef6e11d8dfaea0037b752d9a
azure-sdk <azuresdk@microsoft.com>
2023-02-21 00:30:36 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ManagedServices_1.0.1

--- specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples-dotnet/GetMarketplaceRegistrationDefinition.cs
+++ specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples-dotnet/GetMarketplaceRegistrationDefinition.cs
@@ -8,19 +8,23 @@ using Azure.ResourceManager.ManagedServices;
 // Generated from example definition: specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples/GetMarketplaceRegistrationDefinition.json
 // this example is just showing the usage of "MarketplaceRegistrationDefinitions_Get" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ArmResource created on azure
-// for more information of creating ArmResource, please refer to the document of ArmResource
-
-// get the collection of this ManagedServicesMarketplaceRegistrationResource
+// this example assumes you already have this ManagedServicesMarketplaceRegistrationResource created on azure
+// for more information of creating ManagedServicesMarketplaceRegistrationResource, please refer to the document of ManagedServicesMarketplaceRegistrationResource
 string scope = "subscription/0afefe50-734e-4610-8a82-a144ahf49dea";
-ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", scope));
-ManagedServicesMarketplaceRegistrationCollection collection = client.GetManagedServicesMarketplaceRegistrations(scopeId);
+string marketplaceIdentifier = "publisher.product.planName.version";
+ResourceIdentifier managedServicesMarketplaceRegistrationResourceId = ManagedServicesMarketplaceRegistrationResource.CreateResourceIdentifier(scope, marketplaceIdentifier);
+ManagedServicesMarketplaceRegistrationResource managedServicesMarketplaceRegistration = client.GetManagedServicesMarketplaceRegistrationResource(managedServicesMarketplaceRegistrationResourceId);
 
 // invoke the operation
-string marketplaceIdentifier = "publisher.product.planName.version";
-bool result = await collection.ExistsAsync(marketplaceIdentifier);
+ManagedServicesMarketplaceRegistrationResource result = await managedServicesMarketplaceRegistration.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ManagedServicesMarketplaceRegistrationData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
--- specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples-dotnet/GetRegistrationDefinition.cs
+++ specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples-dotnet/GetRegistrationDefinition.cs
@@ -5,25 +5,29 @@ using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
-using Azure.ResourceManager.ManagedServices.Models;
 using Azure.ResourceManager.ManagedServices;
+using Azure.ResourceManager.ManagedServices.Models;
 
 // Generated from example definition: specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples/GetRegistrationDefinition.json
 // this example is just showing the usage of "RegistrationDefinitions_Get" operation, for the dependent resources, they will have to be created separately.
 
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
-ArmClient client = new ArmClient(new DefaultAzureCredential());
+ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ArmResource created on azure
-// for more information of creating ArmResource, please refer to the document of ArmResource
-
-// get the collection of this ManagedServicesRegistrationResource
+// this example assumes you already have this ManagedServicesRegistrationResource created on azure
+// for more information of creating ManagedServicesRegistrationResource, please refer to the document of ManagedServicesRegistrationResource
 string scope = "subscription/0afefe50-734e-4610-8a82-a144ahf49dea";
-ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", scope));
-ManagedServicesRegistrationCollection collection = client.GetManagedServicesRegistrations(scopeId);
+string registrationId = "26c128c2-fefa-4340-9bb1-6e081c90ada2";
+ResourceIdentifier managedServicesRegistrationResourceId = ManagedServicesRegistrationResource.CreateResourceIdentifier(scope, registrationId);
+ManagedServicesRegistrationResource managedServicesRegistration = client.GetManagedServicesRegistrationResource(managedServicesRegistrationResourceId);
 
 // invoke the operation
-string registrationId = "26c128c2-fefa-4340-9bb1-6e081c90ada2";
-bool result = await collection.ExistsAsync(registrationId);
+ManagedServicesRegistrationResource result = await managedServicesRegistration.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ManagedServicesRegistrationData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

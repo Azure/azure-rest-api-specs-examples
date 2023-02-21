6258b139deeb2805623b7ddbb564ec078c191115
azure-sdk <azuresdk@microsoft.com>
2023-02-21 00:41:00 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MixedReality_1.0.1

--- specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-dotnet/proxy/CheckNameAvailabilityForLocalUniqueness.cs
+++ specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-dotnet/proxy/CheckNameAvailabilityForLocalUniqueness.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.MixedReality;
 using Azure.ResourceManager.MixedReality.Models;
 using Azure.ResourceManager.Resources;
 

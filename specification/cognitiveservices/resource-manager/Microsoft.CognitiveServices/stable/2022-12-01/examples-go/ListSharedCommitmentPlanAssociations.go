dd2213aeb395eef3a2591a04b130c6e018a90405
azure-sdk <azuresdk@microsoft.com>
2023-02-22 12:08:22 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/cognitiveservices/armcognitiveservices/v1.3.0

---
+++ specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2022-12-01/examples-go/ListSharedCommitmentPlanAssociations.go
@@ -0,0 +1,45 @@
+package armcognitiveservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cognitiveservices/armcognitiveservices"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/630ec444f8dd7c09b9cdd5fa99951f8a0d1ad41f/specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2022-12-01/examples/ListSharedCommitmentPlanAssociations.json
+func ExampleCommitmentPlansClient_NewListAssociationsPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcognitiveservices.NewCommitmentPlansClient("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := client.NewListAssociationsPager("resourceGroupName", "commitmentPlanName", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.CommitmentPlanAccountAssociationListResult = armcognitiveservices.CommitmentPlanAccountAssociationListResult{
+		// 	Value: []*armcognitiveservices.CommitmentPlanAccountAssociation{
+		// 		{
+		// 			Name: to.Ptr("accountAssociationName"),
+		// 			Type: to.Ptr("Microsoft.CognitiveServices/commitmentPlans/accountAssociations"),
+		// 			ID: to.Ptr("/subscriptions/subscriptionId/resourceGroups/resourceGroupName/providers/Microsoft.CognitiveServices/commitmentPlans/commitmentPlanName/accountAssociations/accountAssociationName"),
+		// 			Properties: &armcognitiveservices.CommitmentPlanAccountAssociationProperties{
+		// 				AccountID: to.Ptr("/subscriptions/subscriptionId/resourceGroups/resourceGroupName/providers/Microsoft.CognitiveServices/accounts/accountName"),
+		// 			},
+		// 	}},
+		// }
+	}
+}
---
+++ specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2022-12-01/examples-go/ListSkus.go
@@ -0,0 +1,46 @@
+package armcognitiveservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cognitiveservices/armcognitiveservices"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/630ec444f8dd7c09b9cdd5fa99951f8a0d1ad41f/specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2022-12-01/examples/ListSkus.json
+func ExampleAccountsClient_ListSKUs() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armcognitiveservices.NewAccountsClient("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.ListSKUs(ctx, "myResourceGroup", "myAccount", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AccountSKUListResult = armcognitiveservices.AccountSKUListResult{
+	// 	Value: []*armcognitiveservices.AccountSKU{
+	// 		{
+	// 			ResourceType: to.Ptr("Microsoft.CognitiveServices/accounts"),
+	// 			SKU: &armcognitiveservices.SKU{
+	// 				Name: to.Ptr("F0"),
+	// 				Tier: to.Ptr(armcognitiveservices.SKUTierFree),
+	// 			},
+	// 		},
+	// 		{
+	// 			ResourceType: to.Ptr("Microsoft.CognitiveServices/accounts"),
+	// 			SKU: &armcognitiveservices.SKU{
+	// 				Name: to.Ptr("S0"),
+	// 				Tier: to.Ptr(armcognitiveservices.SKUTierStandard),
+	// 			},
+	// 	}},
+	// }
+}

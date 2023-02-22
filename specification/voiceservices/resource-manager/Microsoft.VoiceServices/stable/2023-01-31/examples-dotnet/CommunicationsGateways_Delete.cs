bd83debd49e888e7a0eeadba39c541ed1c3fc013
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-22 08:03:49 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.VoiceServices_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples-dotnet/CommunicationsGateways_Delete.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.VoiceServices;
+using Azure.ResourceManager.VoiceServices.Models;
+
+// Generated from example definition: specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples/CommunicationsGateways_Delete.json
+// this example is just showing the usage of "CommunicationsGateways_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this CommunicationsGatewayResource created on azure
+// for more information of creating CommunicationsGatewayResource, please refer to the document of CommunicationsGatewayResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "testrg";
+string communicationsGatewayName = "myname";
+ResourceIdentifier communicationsGatewayResourceId = CommunicationsGatewayResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, communicationsGatewayName);
+CommunicationsGatewayResource communicationsGateway = client.GetCommunicationsGatewayResource(communicationsGatewayResourceId);
+
+// invoke the operation
+await communicationsGateway.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");
---
+++ specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples-dotnet/NameAvailability_CheckLocal.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.VoiceServices;
+using Azure.ResourceManager.VoiceServices.Models;
+
+// Generated from example definition: specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples/NameAvailability_CheckLocal.json
+// this example is just showing the usage of "NameAvailability_CheckLocal" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation
+AzureLocation location = new AzureLocation("useast");
+CheckNameAvailabilityContent content = new CheckNameAvailabilityContent()
+{
+    Name = "myname",
+    ResourceType = "Microsoft.VoiceServices/CommunicationsGateway",
+};
+CheckNameAvailabilityResponse result = await subscriptionResource.CheckLocalNameAvailabilityAsync(location, content);
+
+Console.WriteLine($"Succeeded: {result}");

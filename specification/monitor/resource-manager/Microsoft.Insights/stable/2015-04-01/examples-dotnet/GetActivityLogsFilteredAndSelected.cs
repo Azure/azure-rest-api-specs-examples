45a37372939cbf7a63b981fbe40d56decc7ebb05
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-02-22 08:03:41 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Monitor_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/monitor/resource-manager/Microsoft.Insights/stable/2015-04-01/examples-dotnet/GetActivityLogsFilteredAndSelected.cs
+++ specification/monitor/resource-manager/Microsoft.Insights/stable/2015-04-01/examples-dotnet/GetActivityLogsFilteredAndSelected.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Monitor;
 using Azure.ResourceManager.Monitor.Models;
 using Azure.ResourceManager.Resources;
 

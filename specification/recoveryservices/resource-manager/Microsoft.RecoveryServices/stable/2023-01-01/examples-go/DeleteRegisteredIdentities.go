4515ae799e7a45f16ad44bd80105b263a768f6e4
azure-sdk <azuresdk@microsoft.com>
2023-02-24 12:12:49 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/recoveryservices/armrecoveryservices/v1.2.0

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-01-01/examples-go/DeleteRegisteredIdentities.go
@@ -0,0 +1,26 @@
+package armrecoveryservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/630ec444f8dd7c09b9cdd5fa99951f8a0d1ad41f/specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-01-01/examples/DeleteRegisteredIdentities.json
+func ExampleRegisteredIdentitiesClient_Delete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armrecoveryservices.NewRegisteredIdentitiesClient("77777777-d41f-4550-9f70-7708a3a2283b", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	_, err = client.Delete(ctx, "BCDRIbzRG", "BCDRIbzVault", "dpmcontainer01", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+}

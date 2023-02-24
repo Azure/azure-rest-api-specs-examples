4e1f57fcfdc582b5af7206fa7319e97a3b43abe3
azure-sdk <azuresdk@microsoft.com>
2023-02-24 12:11:32 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/storagecache/armstoragecache/v3.0.0

---
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-01-01/examples-go/StorageTargets_Delete.go
@@ -0,0 +1,30 @@
+package armstoragecache_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storagecache/armstoragecache/v3"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/402006d2796cdd3894d013d83e77b46a5c844005/specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-01-01/examples/StorageTargets_Delete.json
+func ExampleStorageTargetsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armstoragecache.NewStorageTargetsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := client.BeginDelete(ctx, "scgroup", "sc1", "st1", &armstoragecache.StorageTargetsClientBeginDeleteOptions{Force: nil})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}
---
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-01-01/examples-go/UsageModels_List.go
@@ -0,0 +1,51 @@
+package armstoragecache_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storagecache/armstoragecache/v3"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/402006d2796cdd3894d013d83e77b46a5c844005/specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-01-01/examples/UsageModels_List.json
+func ExampleUsageModelsClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armstoragecache.NewUsageModelsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := client.NewListPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.UsageModelsResult = armstoragecache.UsageModelsResult{
+		// 	Value: []*armstoragecache.UsageModel{
+		// 		{
+		// 			Display: &armstoragecache.UsageModelDisplay{
+		// 				Description: to.Ptr("Read only, with a default verification timer of 30 seconds. Verification timer has a minimum value of 1 and maximum value of 31536000. Write-back timer must have value of 0 or be null."),
+		// 			},
+		// 			ModelName: to.Ptr("READ_ONLY"),
+		// 			TargetType: to.Ptr("nfs3"),
+		// 		},
+		// 		{
+		// 			Display: &armstoragecache.UsageModelDisplay{
+		// 				Description: to.Ptr("Read-write, with a default verification timer of 8 hours and default write-back timer of 1 hour. Verification timer and write-back timer have a minimum value of 1 and maximum value of 31536000."),
+		// 			},
+		// 			ModelName: to.Ptr("READ_WRITE"),
+		// 			TargetType: to.Ptr("nfs3"),
+		// 	}},
+		// }
+	}
+}

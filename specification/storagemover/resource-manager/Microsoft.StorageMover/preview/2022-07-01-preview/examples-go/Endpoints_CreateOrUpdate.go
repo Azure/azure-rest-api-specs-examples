a2882b0c2ab993dd64bf387e9a33e5e24ac2bd69
azure-sdk <azuresdk@microsoft.com>
2023-02-24 12:10:16 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/storagemover/armstoragemover/v0.1.0

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/preview/2022-07-01-preview/examples-go/Endpoints_CreateOrUpdate.go
@@ -0,0 +1,48 @@
+package armstoragemover_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storagemover/armstoragemover"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/3d7a3848106b831a4a7f46976fe38aa605c4f44d/specification/storagemover/resource-manager/Microsoft.StorageMover/preview/2022-07-01-preview/examples/Endpoints_CreateOrUpdate.json
+func ExampleEndpointsClient_CreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	client, err := armstoragemover.NewEndpointsClient("11111111-2222-3333-4444-555555555555", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := client.CreateOrUpdate(ctx, "examples-rg", "examples-storageMoverName", "examples-endpointName", armstoragemover.Endpoint{
+		Properties: &armstoragemover.AzureStorageBlobContainerEndpointProperties{
+			Description:              to.Ptr("Example Storage Container Endpoint Description"),
+			EndpointType:             to.Ptr(armstoragemover.EndpointTypeAzureStorageBlobContainer),
+			BlobContainerName:        to.Ptr("examples-blobContainerName"),
+			StorageAccountResourceID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/examples-rg/providers/Microsoft.Storage/storageAccounts/examples-storageAccountName/"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Endpoint = armstoragemover.Endpoint{
+	// 	Name: to.Ptr("examples-endpointName"),
+	// 	Type: to.Ptr("Microsoft.StorageMover/storageMovers/endpoints"),
+	// 	ID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/examples-rg/providers/Microsoft.StorageMover/storageMovers/examples-storageMoverName/endpoints/examples-endpointName"),
+	// 	Properties: &armstoragemover.AzureStorageBlobContainerEndpointProperties{
+	// 		Description: to.Ptr("Example Storage Container Endpoint Description"),
+	// 		EndpointType: to.Ptr(armstoragemover.EndpointTypeAzureStorageBlobContainer),
+	// 		BlobContainerName: to.Ptr("examples-blobContainerName"),
+	// 		StorageAccountResourceID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/examples-rg/providers/Microsoft.Storage/storageAccounts/examples-storageAccountName/"),
+	// 	},
+	// }
+}

26cc3be8606995713a75659b51edf43bf88eab8e
azure-sdk <azuresdk@microsoft.com>
2023-02-27 12:07:10 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-recoveryservices_1.0.0

--- specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-01-01/examples-java/UpdateVaultExtendedInfo.java
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-01-01/examples-java/UpdateVaultExtendedInfo.java
@@ -1,18 +1,18 @@
-/** Samples for VaultExtendedInfo Update. */
+/** Samples for VaultExtendedInfo CreateOrUpdate. */
 public final class Main {
     /*
      * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-01-01/examples/UpdateVaultExtendedInfo.json
      */
     /**
-     * Sample code: PATCH ExtendedInfo of Resource.
+     * Sample code: Put ExtendedInfo of Resource.
      *
      * @param manager Entry point to RecoveryServicesManager.
      */
-    public static void pATCHExtendedInfoOfResource(
+    public static void putExtendedInfoOfResource(
         com.azure.resourcemanager.recoveryservices.RecoveryServicesManager manager) {
         manager
             .vaultExtendedInfoes()
-            .updateWithResponse(
+            .createOrUpdateWithResponse(
                 "Default-RecoveryServices-ResourceGroup", "swaggerExample", null, com.azure.core.util.Context.NONE);
     }
 }

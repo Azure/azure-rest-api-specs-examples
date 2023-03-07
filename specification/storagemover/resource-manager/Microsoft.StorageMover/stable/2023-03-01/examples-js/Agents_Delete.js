e292550b9c162f75f3b8b04b7b5ae6ce971e9386
azure-sdk <azuresdk@microsoft.com>
2023-03-07 12:08:37 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-storagemover_1.0.0

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-03-01/examples-js/Agents_Delete.js
@@ -0,0 +1,24 @@
+const { StorageMoverClient } = require("@azure/arm-storagemover");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes an Agent resource.
+ *
+ * @summary Deletes an Agent resource.
+ * x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-03-01/examples/Agents_Delete.json
+ */
+async function agentsDelete() {
+  const subscriptionId =
+    process.env["STORAGEMOVER_SUBSCRIPTION_ID"] || "11111111-2222-3333-4444-555555555555";
+  const resourceGroupName = process.env["STORAGEMOVER_RESOURCE_GROUP"] || "examples-rg";
+  const storageMoverName = "examples-storageMoverName";
+  const agentName = "examples-agentName";
+  const credential = new DefaultAzureCredential();
+  const client = new StorageMoverClient(credential, subscriptionId);
+  const result = await client.agents.beginDeleteAndWait(
+    resourceGroupName,
+    storageMoverName,
+    agentName
+  );
+  console.log(result);
+}

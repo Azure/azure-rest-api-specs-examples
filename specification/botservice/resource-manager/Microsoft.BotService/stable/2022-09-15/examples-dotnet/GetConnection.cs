5e72dd69e05ba61347bccce64ae65189ffa38f23
azure-sdk <azuresdk@microsoft.com>
2023-03-10 00:14:52 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.BotService_1.0.0

--- specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-dotnet/GetConnection.cs
+++ specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-dotnet/GetConnection.cs
@@ -6,6 +6,7 @@ using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.BotService;
 using Azure.ResourceManager.BotService.Models;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples/GetConnection.json
 // this example is just showing the usage of "BotConnection_Get" operation, for the dependent resources, they will have to be created separately.
@@ -15,19 +16,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this BotResource created on azure
-// for more information of creating BotResource, please refer to the document of BotResource
+// this example assumes you already have this BotConnectionSettingResource created on azure
+// for more information of creating BotConnectionSettingResource, please refer to the document of BotConnectionSettingResource
 string subscriptionId = "subscription-id";
 string resourceGroupName = "OneResourceGroupName";
 string resourceName = "samplebotname";
-ResourceIdentifier botResourceId = BotResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
-BotResource bot = client.GetBotResource(botResourceId);
-
-// get the collection of this BotConnectionSettingResource
-BotConnectionSettingCollection collection = bot.GetBotConnectionSettings();
+string connectionName = "sampleConnection";
+ResourceIdentifier botConnectionSettingResourceId = BotConnectionSettingResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, connectionName);
+BotConnectionSettingResource botConnectionSetting = client.GetBotConnectionSettingResource(botConnectionSettingResourceId);
 
 // invoke the operation
-string connectionName = "sampleConnection";
-bool result = await collection.ExistsAsync(connectionName);
+BotConnectionSettingResource result = await botConnectionSetting.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+BotConnectionSettingData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

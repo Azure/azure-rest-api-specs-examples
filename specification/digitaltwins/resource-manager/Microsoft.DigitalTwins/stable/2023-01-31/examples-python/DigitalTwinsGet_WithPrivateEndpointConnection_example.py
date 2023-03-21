815a94e19101a46f6141373c6c838bd74597933c
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-21 08:02:51 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-digitaltwins_6.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2023-01-31/examples-python/DigitalTwinsGet_WithPrivateEndpointConnection_example.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.digitaltwins import AzureDigitalTwinsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-digitaltwins
+# USAGE
+    python digital_twins_get_with_private_endpoint_connection_example.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureDigitalTwinsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="50016170-c839-41ba-a724-51e9df440b9e",
+    )
+
+    response = client.digital_twins.get(
+        resource_group_name="resRg",
+        resource_name="myDigitalTwinsService",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2023-01-31/examples/DigitalTwinsGet_WithPrivateEndpointConnection_example.json
+if __name__ == "__main__":
+    main()

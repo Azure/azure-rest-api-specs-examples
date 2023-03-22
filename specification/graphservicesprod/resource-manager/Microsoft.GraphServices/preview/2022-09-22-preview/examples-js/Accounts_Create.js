4a99464413c5e3cbbcee9779ab3e3e8648fd310b
azure-sdk <azuresdk@microsoft.com>
2023-03-22 00:21:31 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-graphservices_1.0.0-beta.1

---
+++ specification/graphservicesprod/resource-manager/Microsoft.GraphServices/preview/2022-09-22-preview/examples-js/Accounts_Create.js
@@ -0,0 +1,27 @@
+const { GraphServices } = require("@azure/arm-graphservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Create or update account resource.
+ *
+ * @summary Create or update account resource.
+ * x-ms-original-file: specification/graphservicesprod/resource-manager/Microsoft.GraphServices/preview/2022-09-22-preview/examples/Accounts_Create.json
+ */
+async function createAccountResource() {
+  const subscriptionId =
+    process.env["GRAPHSERVICES_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["GRAPHSERVICES_RESOURCE_GROUP"] || "testResourceGroupGRAM";
+  const resourceName = "11111111-aaaa-1111-bbbb-1111111111111";
+  const accountResource = {
+    properties: { appId: "11111111-aaaa-1111-bbbb-111111111111" },
+    tags: { tag1: "value1", tag2: "value2" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new GraphServices(credential, subscriptionId);
+  const result = await client.account.beginCreateAndUpdateAndWait(
+    resourceGroupName,
+    resourceName,
+    accountResource
+  );
+  console.log(result);
+}

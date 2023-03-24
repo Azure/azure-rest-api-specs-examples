3800c53380fc8e01c0c82ed2ed1484c915c902ce
azure-sdk <azuresdk@microsoft.com>
2023-03-24 12:09:11 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/voiceservices/armvoiceservices/v1.0.0

---
+++ specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples-go/CommunicationsGateways_Delete.go
@@ -0,0 +1,30 @@
+package armvoiceservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/voiceservices/armvoiceservices"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/aa85f59e259c4b12197b57b221067c40fa2fe3f1/specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples/CommunicationsGateways_Delete.json
+func ExampleCommunicationsGatewaysClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armvoiceservices.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewCommunicationsGatewaysClient().BeginDelete(ctx, "testrg", "myname", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

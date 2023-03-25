361a9fc2960b620c88b36f6dacefb563850270b4
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-25 08:03:32 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/redisenterprise/armredisenterprise/v1.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2023-03-01-preview/examples-go/RedisEnterpriseDatabasesExport.go
@@ -0,0 +1,33 @@
+package armredisenterprise_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redisenterprise/armredisenterprise"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/9565a97e3efbeb3691c9100d5473b0a884c1b786/specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2023-03-01-preview/examples/RedisEnterpriseDatabasesExport.json
+func ExampleDatabasesClient_BeginExport() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armredisenterprise.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewDatabasesClient().BeginExport(ctx, "rg1", "cache1", "default", armredisenterprise.ExportClusterParameters{
+		SasURI: to.Ptr("https://contosostorage.blob.core.window.net/urlToBlobContainer?sasKeyParameters"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}
---
+++ specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2023-03-01-preview/examples-go/RedisEnterpriseGet.go
@@ -0,0 +1,84 @@
+package armredisenterprise_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redisenterprise/armredisenterprise"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/9565a97e3efbeb3691c9100d5473b0a884c1b786/specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2023-03-01-preview/examples/RedisEnterpriseGet.json
+func ExampleClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armredisenterprise.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewClient().Get(ctx, "rg1", "cache1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Cluster = armredisenterprise.Cluster{
+	// 	Name: to.Ptr("cache1"),
+	// 	Type: to.Ptr("Microsoft.Cache/redisEnterprise"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Cache/redisEnterprise/cache1"),
+	// 	Location: to.Ptr("West US"),
+	// 	Tags: map[string]*string{
+	// 	},
+	// 	Identity: &armredisenterprise.ManagedServiceIdentity{
+	// 		Type: to.Ptr(armredisenterprise.ManagedServiceIdentityTypeUserAssigned),
+	// 		UserAssignedIdentities: map[string]*armredisenterprise.UserAssignedIdentity{
+	// 			"/subscriptions/your-subscription/resourceGroups/your-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/your-identity": &armredisenterprise.UserAssignedIdentity{
+	// 				ClientID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 				PrincipalID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 			},
+	// 		},
+	// 	},
+	// 	Properties: &armredisenterprise.ClusterProperties{
+	// 		Encryption: &armredisenterprise.ClusterPropertiesEncryption{
+	// 			CustomerManagedKeyEncryption: &armredisenterprise.ClusterPropertiesEncryptionCustomerManagedKeyEncryption{
+	// 				KeyEncryptionKeyIdentity: &armredisenterprise.ClusterPropertiesEncryptionCustomerManagedKeyEncryptionKeyIdentity{
+	// 					IdentityType: to.Ptr(armredisenterprise.CmkIdentityTypeUserAssignedIdentity),
+	// 					UserAssignedIdentityResourceID: to.Ptr("/subscriptions/your-subscription/resourceGroups/your-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/your-identity"),
+	// 				},
+	// 				KeyEncryptionKeyURL: to.Ptr("https://your-kv.vault.azure.net/keys/your-key/your-key-version"),
+	// 			},
+	// 		},
+	// 		HostName: to.Ptr("cache1.westus.something.azure.com"),
+	// 		MinimumTLSVersion: to.Ptr(armredisenterprise.TLSVersionOne2),
+	// 		PrivateEndpointConnections: []*armredisenterprise.PrivateEndpointConnection{
+	// 			{
+	// 				ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Cache/redisEnterprise/cache1/privateEndpointConnections/cachePec"),
+	// 				Properties: &armredisenterprise.PrivateEndpointConnectionProperties{
+	// 					PrivateEndpoint: &armredisenterprise.PrivateEndpoint{
+	// 						ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/privateEndpoints/cachePe"),
+	// 					},
+	// 					PrivateLinkServiceConnectionState: &armredisenterprise.PrivateLinkServiceConnectionState{
+	// 						Description: to.Ptr("Please approve my connection"),
+	// 						ActionsRequired: to.Ptr("None"),
+	// 						Status: to.Ptr(armredisenterprise.PrivateEndpointServiceConnectionStatusApproved),
+	// 					},
+	// 				},
+	// 		}},
+	// 		ProvisioningState: to.Ptr(armredisenterprise.ProvisioningStateSucceeded),
+	// 		RedisVersion: to.Ptr("6"),
+	// 		ResourceState: to.Ptr(armredisenterprise.ResourceStateRunning),
+	// 	},
+	// 	SKU: &armredisenterprise.SKU{
+	// 		Name: to.Ptr(armredisenterprise.SKUNameEnterpriseFlashF300),
+	// 		Capacity: to.Ptr[int32](3),
+	// 	},
+	// 	Zones: []*string{
+	// 		to.Ptr("1"),
+	// 		to.Ptr("2"),
+	// 		to.Ptr("3")},
+	// 	}
+}

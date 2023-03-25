fd391d1b0babd1f3c637d8b3191ca19ed1188e7a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-25 08:04:17 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/appservice/armappservice/v2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/web/resource-manager/Microsoft.CertificateRegistration/stable/2022-09-01/examples-go/RetrieveCertificateEmailHistory.go
@@ -0,0 +1,38 @@
+package armappservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appservice/armappservice/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/10c740b7224771c889cfb92f128168f5a0568c26/specification/web/resource-manager/Microsoft.CertificateRegistration/stable/2022-09-01/examples/RetrieveCertificateEmailHistory.json
+func ExampleCertificateOrdersClient_RetrieveCertificateEmailHistory() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armappservice.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewCertificateOrdersClient().RetrieveCertificateEmailHistory(ctx, "testrg123", "SampleCertOrder", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.CertificateEmailArray = []*armappservice.CertificateEmail{
+	// 	{
+	// 		EmailID: to.Ptr("customer@email.com"),
+	// 		TimeStamp: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-11-12T23:40:25+00:00"); return t}()),
+	// 	},
+	// 	{
+	// 		EmailID: to.Ptr("customer@email.com"),
+	// 		TimeStamp: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-11-12T23:40:25+00:00"); return t}()),
+	// }}
+}

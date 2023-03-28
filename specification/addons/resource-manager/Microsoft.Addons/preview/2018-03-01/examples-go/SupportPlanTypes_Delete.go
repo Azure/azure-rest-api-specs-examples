0534290cb9c425b7ab4c11a0ca095951fd82b188
azure-sdk <azuresdk@microsoft.com>
2023-03-28 12:10:16 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/addons/armaddons/v0.2.0

--- specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples-go/SupportPlanTypes_Delete.go
+++ specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples-go/SupportPlanTypes_Delete.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/addons/armaddons"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples/SupportPlanTypes_Delete.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/163e27c0ca7570bc39e00a46f255740d9b3ba3cb/specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples/SupportPlanTypes_Delete.json
 func ExampleSupportPlanTypesClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armaddons.NewSupportPlanTypesClient("d18d258f-bdba-4de1-8b51-e79d6c181d5e", cred, nil)
+	clientFactory, err := armaddons.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx,
-		"Canonical",
-		armaddons.PlanTypeNameStandard,
-		nil)
+	poller, err := clientFactory.NewSupportPlanTypesClient().BeginDelete(ctx, "Canonical", armaddons.PlanTypeNameStandard, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

86f207245c62735feaf11a69963ddeb3bf318875
azure-sdk <azuresdk@microsoft.com>
2023-03-28 12:24:11 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/resources/armsubscriptions/v1.1.0

--- specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples-go/GetSubscription.go
+++ specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples-go/GetSubscription.go
@@ -8,23 +8,43 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armsubscriptions"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples/GetSubscription.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0cc5e2efd6ffccf30e80d1e150b488dd87198b94/specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples/GetSubscription.json
 func ExampleClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armsubscriptions.NewClient(cred, nil)
+	clientFactory, err := armsubscriptions.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Get(ctx,
-		"291bba3f-e0a5-47bc-a099-3bdcb2a50a05",
-		nil)
+	res, err := clientFactory.NewClient().Get(ctx, "291bba3f-e0a5-47bc-a099-3bdcb2a50a05", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Subscription = armsubscriptions.Subscription{
+	// 	AuthorizationSource: to.Ptr("Bypassed"),
+	// 	DisplayName: to.Ptr("Example Subscription"),
+	// 	ID: to.Ptr("/subscriptions/291bba3f-e0a5-47bc-a099-3bdcb2a50a05"),
+	// 	ManagedByTenants: []*armsubscriptions.ManagedByTenant{
+	// 		{
+	// 			TenantID: to.Ptr("8f70baf1-1f6e-46a2-a1ff-238dac1ebfb7"),
+	// 	}},
+	// 	State: to.Ptr(armsubscriptions.SubscriptionStateEnabled),
+	// 	SubscriptionID: to.Ptr("291bba3f-e0a5-47bc-a099-3bdcb2a50a05"),
+	// 	SubscriptionPolicies: &armsubscriptions.SubscriptionPolicies{
+	// 		LocationPlacementID: to.Ptr("Internal_2014-09-01"),
+	// 		QuotaID: to.Ptr("Internal_2014-09-01"),
+	// 		SpendingLimit: to.Ptr(armsubscriptions.SpendingLimitOff),
+	// 	},
+	// 	Tags: map[string]*string{
+	// 		"tagKey1": to.Ptr("tagValue1"),
+	// 		"tagKey2": to.Ptr("tagValue2"),
+	// 	},
+	// 	TenantID: to.Ptr("31c75423-32d6-4322-88b7-c478bdde4858"),
+	// }
 }
--- specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples-go/PostCheckZonePeers.go
+++ specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples-go/PostCheckZonePeers.go
@@ -9,28 +9,55 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armsubscriptions"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples/PostCheckZonePeers.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0cc5e2efd6ffccf30e80d1e150b488dd87198b94/specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/examples/PostCheckZonePeers.json
 func ExampleClient_CheckZonePeers() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armsubscriptions.NewClient(cred, nil)
+	clientFactory, err := armsubscriptions.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.CheckZonePeers(ctx,
-		"00000000-0000-0000-0000-00000000000000",
-		armsubscriptions.CheckZonePeersRequest{
-			Location: to.Ptr("eastus"),
-			SubscriptionIDs: []*string{
-				to.Ptr("subscriptions/11111111-1111-1111-1111-111111111111")},
-		},
-		nil)
+	res, err := clientFactory.NewClient().CheckZonePeers(ctx, "00000000-0000-0000-0000-00000000000000", armsubscriptions.CheckZonePeersRequest{
+		Location: to.Ptr("eastus"),
+		SubscriptionIDs: []*string{
+			to.Ptr("subscriptions/11111111-1111-1111-1111-111111111111")},
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.CheckZonePeersResult = armsubscriptions.CheckZonePeersResult{
+	// 	AvailabilityZonePeers: []*armsubscriptions.AvailabilityZonePeers{
+	// 		{
+	// 			AvailabilityZone: to.Ptr("1"),
+	// 			Peers: []*armsubscriptions.Peers{
+	// 				{
+	// 					AvailabilityZone: to.Ptr("3"),
+	// 					SubscriptionID: to.Ptr("11111111-1111-1111-1111-111111111111"),
+	// 			}},
+	// 		},
+	// 		{
+	// 			AvailabilityZone: to.Ptr("2"),
+	// 			Peers: []*armsubscriptions.Peers{
+	// 				{
+	// 					AvailabilityZone: to.Ptr("2"),
+	// 					SubscriptionID: to.Ptr("11111111-1111-1111-1111-111111111111"),
+	// 			}},
+	// 		},
+	// 		{
+	// 			AvailabilityZone: to.Ptr("3"),
+	// 			Peers: []*armsubscriptions.Peers{
+	// 				{
+	// 					AvailabilityZone: to.Ptr("1"),
+	// 					SubscriptionID: to.Ptr("11111111-1111-1111-1111-111111111111"),
+	// 			}},
+	// 	}},
+	// 	Location: to.Ptr("eastus2"),
+	// 	SubscriptionID: to.Ptr("00000000-0000-0000-0000-00000000000000"),
+	// }
 }

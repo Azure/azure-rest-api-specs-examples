8aba5a732a52b59aaf2eb41849cdef38ffb60ffb
azure-sdk <azuresdk@microsoft.com>
2023-03-28 12:09:05 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/advisor/armadvisor/v1.1.0

--- specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-go/EmptyResponse.go
+++ specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-go/EmptyResponse.go
@@ -8,20 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/advisor/armadvisor"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples/EmptyResponse.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples/EmptyResponse.json
 func ExampleRecommendationsClient_GetGenerateStatus() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armadvisor.NewRecommendationsClient("subscriptionId", cred, nil)
+	clientFactory, err := armadvisor.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = client.GetGenerateStatus(ctx,
-		"operationGUID",
-		nil)
+	_, err = clientFactory.NewRecommendationsClient().GetGenerateStatus(ctx, "operationGUID", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

e3da728c51e020870d3b2482c65b5864b2735899
azure-sdk <azuresdk@microsoft.com>
2023-03-28 12:06:07 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/appcomplianceautomation/armappcomplianceautomation/v0.2.0

--- specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-go/Report_Delete.go
+++ specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-go/Report_Delete.go
@@ -8,18 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appcomplianceautomation/armappcomplianceautomation"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples/Report_Delete.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/82ea406b73d671269217053d7ef336450d860345/specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples/Report_Delete.json
 func ExampleReportClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armappcomplianceautomation.NewReportClient(cred, nil)
+	clientFactory, err := armappcomplianceautomation.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx, "testReportName", nil)
+	poller, err := clientFactory.NewReportClient().BeginDelete(ctx, "testReportName", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
--- specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-go/Report_Update.go
+++ specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-go/Report_Update.go
@@ -11,18 +11,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appcomplianceautomation/armappcomplianceautomation"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples/Report_Update.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/82ea406b73d671269217053d7ef336450d860345/specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples/Report_Update.json
 func ExampleReportClient_BeginUpdate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armappcomplianceautomation.NewReportClient(cred, nil)
+	clientFactory, err := armappcomplianceautomation.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginUpdate(ctx, "testReportName", armappcomplianceautomation.ReportResourcePatch{
+	poller, err := clientFactory.NewReportClient().BeginUpdate(ctx, "testReportName", armappcomplianceautomation.ReportResourcePatch{
 		Properties: &armappcomplianceautomation.ReportProperties{
 			OfferGUID: to.Ptr("0000"),
 			Resources: []*armappcomplianceautomation.ResourceMetadata{
@@ -43,6 +43,48 @@ func ExampleReportClient_BeginUpdate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ReportResource = armappcomplianceautomation.ReportResource{
+	// 	Name: to.Ptr("testReportName"),
+	// 	Type: to.Ptr("Microsfot.AppComplianceAutomation/reports"),
+	// 	ID: to.Ptr("/provider/Microsfot.AppComplianceAutomation/reports/testReportName"),
+	// 	SystemData: &armappcomplianceautomation.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-05-14T22:34:55.4499903Z"); return t}()),
+	// 		CreatedBy: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 		CreatedByType: to.Ptr(armappcomplianceautomation.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-05-14T22:34:55.4499903Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 		LastModifiedByType: to.Ptr(armappcomplianceautomation.CreatedByTypeUser),
+	// 	},
+	// 	Properties: &armappcomplianceautomation.ReportProperties{
+	// 		ComplianceStatus: &armappcomplianceautomation.ReportComplianceStatus{
+	// 			M365: &armappcomplianceautomation.OverviewStatus{
+	// 				FailedCount: to.Ptr[int32](0),
+	// 				ManualCount: to.Ptr[int32](0),
+	// 				PassedCount: to.Ptr[int32](0),
+	// 			},
+	// 		},
+	// 		ID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 		LastTriggerTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-02T05:17:23.922Z"); return t}()),
+	// 		NextTriggerTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-02T05:17:23.922Z"); return t}()),
+	// 		OfferGUID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 		ProvisioningState: to.Ptr(armappcomplianceautomation.ProvisioningStateSucceeded),
+	// 		ReportName: to.Ptr("testReportName"),
+	// 		Resources: []*armappcomplianceautomation.ResourceMetadata{
+	// 			{
+	// 				ResourceID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/myResourceGroup/providers/Microsoft.SignalRService/SignalR/mySignalRService"),
+	// 				Tags: map[string]*string{
+	// 					"key1": to.Ptr("value1"),
+	// 				},
+	// 		}},
+	// 		Status: to.Ptr(armappcomplianceautomation.ReportStatusActive),
+	// 		Subscriptions: []*string{
+	// 			to.Ptr("00000000-0000-0000-0000-000000000000")},
+	// 			TenantID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 			TimeZone: to.Ptr("GMT Standard Time"),
+	// 			TriggerTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-02T05:17:23.922Z"); return t}()),
+	// 		},
+	// 	}
 }

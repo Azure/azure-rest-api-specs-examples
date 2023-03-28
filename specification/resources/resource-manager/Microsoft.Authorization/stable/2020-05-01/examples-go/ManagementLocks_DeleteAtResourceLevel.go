3a317c7905ed56028fb20aed827706c7a23a11c1
azure-sdk <azuresdk@microsoft.com>
2023-03-28 12:25:09 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/resources/armlocks/v1.1.0

--- specification/resources/resource-manager/Microsoft.Authorization/stable/2020-05-01/examples-go/ManagementLocks_DeleteAtResourceLevel.go
+++ specification/resources/resource-manager/Microsoft.Authorization/stable/2020-05-01/examples-go/ManagementLocks_DeleteAtResourceLevel.go
@@ -8,25 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armlocks"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/resources/resource-manager/Microsoft.Authorization/stable/2020-05-01/examples/ManagementLocks_DeleteAtResourceLevel.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0cc5e2efd6ffccf30e80d1e150b488dd87198b94/specification/resources/resource-manager/Microsoft.Authorization/stable/2020-05-01/examples/ManagementLocks_DeleteAtResourceLevel.json
 func ExampleManagementLocksClient_DeleteAtResourceLevel() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armlocks.NewManagementLocksClient("subscriptionId", cred, nil)
+	clientFactory, err := armlocks.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = client.DeleteAtResourceLevel(ctx,
-		"resourcegroupname",
-		"Microsoft.Storage",
-		"parentResourcePath",
-		"storageAccounts",
-		"teststorageaccount",
-		"testlock",
-		nil)
+	_, err = clientFactory.NewManagementLocksClient().DeleteAtResourceLevel(ctx, "resourcegroupname", "Microsoft.Storage", "parentResourcePath", "storageAccounts", "teststorageaccount", "testlock", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

3acc0d1a6290ad44e35503be768d5584baa02862
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-29 08:07:40 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/addons/armaddons/v0.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples-go/CanonicalListSupportPlanInfo_Post.go
+++ specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples-go/CanonicalListSupportPlanInfo_Post.go
@@ -8,22 +8,38 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/addons/armaddons"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples/CanonicalListSupportPlanInfo_Post.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/163e27c0ca7570bc39e00a46f255740d9b3ba3cb/specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples/CanonicalListSupportPlanInfo_Post.json
 func ExampleSupportPlanTypesClient_ListInfo() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armaddons.NewSupportPlanTypesClient("d18d258f-bdba-4de1-8b51-e79d6c181d5e", cred, nil)
+	clientFactory, err := armaddons.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.ListInfo(ctx,
-		nil)
+	res, err := clientFactory.NewSupportPlanTypesClient().ListInfo(ctx, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.CanonicalSupportPlanInfoDefinitionArray = []*armaddons.CanonicalSupportPlanInfoDefinition{
+	// 	{
+	// 		Enabled: to.Ptr(false),
+	// 		OneTimeCharge: to.Ptr(armaddons.OneTimeChargeOnReenabled),
+	// 		SupportPlanType: to.Ptr(armaddons.SupportPlanTypeStandard),
+	// 	},
+	// 	{
+	// 		Enabled: to.Ptr(false),
+	// 		OneTimeCharge: to.Ptr(armaddons.OneTimeChargeOnReenabled),
+	// 		SupportPlanType: to.Ptr(armaddons.SupportPlanTypeAdvanced),
+	// 	},
+	// 	{
+	// 		Enabled: to.Ptr(true),
+	// 		OneTimeCharge: to.Ptr(armaddons.OneTimeChargeNo),
+	// 		SupportPlanType: to.Ptr(armaddons.SupportPlanTypeEssential),
+	// }}
 }
--- specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples-go/SupportPlanTypes_CreateOrUpdate.go
+++ specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples-go/SupportPlanTypes_CreateOrUpdate.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/addons/armaddons"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples/SupportPlanTypes_CreateOrUpdate.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/163e27c0ca7570bc39e00a46f255740d9b3ba3cb/specification/addons/resource-manager/Microsoft.Addons/preview/2018-03-01/examples/SupportPlanTypes_CreateOrUpdate.json
 func ExampleSupportPlanTypesClient_BeginCreateOrUpdate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armaddons.NewSupportPlanTypesClient("d18d258f-bdba-4de1-8b51-e79d6c181d5e", cred, nil)
+	clientFactory, err := armaddons.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx,
-		"Canonical",
-		armaddons.PlanTypeNameStandard,
-		nil)
+	poller, err := clientFactory.NewSupportPlanTypesClient().BeginCreateOrUpdate(ctx, "Canonical", armaddons.PlanTypeNameStandard, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -30,6 +27,15 @@ func ExampleSupportPlanTypesClient_BeginCreateOrUpdate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.CanonicalSupportPlanResponseEnvelope = armaddons.CanonicalSupportPlanResponseEnvelope{
+	// 	Name: to.Ptr("Standard"),
+	// 	Type: to.Ptr("Microsoft.Addons/supportProvider"),
+	// 	ID: to.Ptr("subscriptions/d18d258f-bdba-4de1-8b51-e79d6c181d5e/providers/Microsoft.Addons/supportProviders/canonical/supportPlanTypes/Standard"),
+	// 	Properties: &armaddons.CanonicalSupportPlanProperties{
+	// 		ProvisioningState: to.Ptr(armaddons.ProvisioningStateSucceeded),
+	// 	},
+	// }
 }

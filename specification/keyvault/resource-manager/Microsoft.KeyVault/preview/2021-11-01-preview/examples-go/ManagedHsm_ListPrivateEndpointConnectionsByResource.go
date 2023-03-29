f2466a9831e84ab87138ea475479666cc8a9cf2f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-29 08:03:40 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/keyvault/armkeyvault/v1.1.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples-go/ManagedHsm_ListPrivateEndpointConnectionsByResource.go
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples-go/ManagedHsm_ListPrivateEndpointConnectionsByResource.go
@@ -8,28 +8,64 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/keyvault/armkeyvault"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples/ManagedHsm_ListPrivateEndpointConnectionsByResource.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/2f2b633d940230cbbd5bcf1339a2e1c48674e4a2/specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples/ManagedHsm_ListPrivateEndpointConnectionsByResource.json
 func ExampleMHSMPrivateEndpointConnectionsClient_NewListByResourcePager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armkeyvault.NewMHSMPrivateEndpointConnectionsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	clientFactory, err := armkeyvault.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListByResourcePager("sample-group",
-		"sample-mhsm",
-		nil)
+	pager := clientFactory.NewMHSMPrivateEndpointConnectionsClient().NewListByResourcePager("sample-group", "sample-mhsm", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.MHSMPrivateEndpointConnectionsListResult = armkeyvault.MHSMPrivateEndpointConnectionsListResult{
+		// 	Value: []*armkeyvault.MHSMPrivateEndpointConnection{
+		// 		{
+		// 			Name: to.Ptr("sample-pec1"),
+		// 			Type: to.Ptr("Microsoft.KeyVault/managedhsms/privateEndpointConnections"),
+		// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/sample-group/providers/Microsoft.KeyVault/managedhsms/sample-mhsm/privateEndpointConnections/sample-pec1"),
+		// 			Etag: to.Ptr(""),
+		// 			Properties: &armkeyvault.MHSMPrivateEndpointConnectionProperties{
+		// 				PrivateEndpoint: &armkeyvault.MHSMPrivateEndpoint{
+		// 					ID: to.Ptr("/subscriptions/00000000-0000-0000-1234-000000000000/resourceGroups/sample-group/providers/Microsoft.Network/privateEndpoints/sample-pe1"),
+		// 				},
+		// 				PrivateLinkServiceConnectionState: &armkeyvault.MHSMPrivateLinkServiceConnectionState{
+		// 					Description: to.Ptr("This was automatically approved by user1234@contoso.com"),
+		// 					ActionsRequired: to.Ptr(armkeyvault.ActionsRequiredNone),
+		// 					Status: to.Ptr(armkeyvault.PrivateEndpointServiceConnectionStatusApproved),
+		// 				},
+		// 				ProvisioningState: to.Ptr(armkeyvault.PrivateEndpointConnectionProvisioningStateSucceeded),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("sample-pec2"),
+		// 			Type: to.Ptr("Microsoft.KeyVault/managedhsms/privateEndpointConnections"),
+		// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/sample-group/providers/Microsoft.KeyVault/managedhsms/sample-mhsm/privateEndpointConnections/sample-pec2"),
+		// 			Etag: to.Ptr(""),
+		// 			Properties: &armkeyvault.MHSMPrivateEndpointConnectionProperties{
+		// 				PrivateEndpoint: &armkeyvault.MHSMPrivateEndpoint{
+		// 					ID: to.Ptr("/subscriptions/00000000-0000-0000-1234-000000000000/resourceGroups/sample-group/providers/Microsoft.Network/privateEndpoints/sample-pe2"),
+		// 				},
+		// 				PrivateLinkServiceConnectionState: &armkeyvault.MHSMPrivateLinkServiceConnectionState{
+		// 					Description: to.Ptr("This was automatically approved by user1234@contoso.com"),
+		// 					ActionsRequired: to.Ptr(armkeyvault.ActionsRequiredNone),
+		// 					Status: to.Ptr(armkeyvault.PrivateEndpointServiceConnectionStatusApproved),
+		// 				},
+		// 				ProvisioningState: to.Ptr(armkeyvault.PrivateEndpointConnectionProvisioningStateSucceeded),
+		// 			},
+		// 	}},
+		// }
 	}
 }
--- specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples-go/getPrivateEndpointConnection.go
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples-go/getPrivateEndpointConnection.go
@@ -8,25 +8,39 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/keyvault/armkeyvault"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples/getPrivateEndpointConnection.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/2f2b633d940230cbbd5bcf1339a2e1c48674e4a2/specification/keyvault/resource-manager/Microsoft.KeyVault/preview/2021-11-01-preview/examples/getPrivateEndpointConnection.json
 func ExamplePrivateEndpointConnectionsClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armkeyvault.NewPrivateEndpointConnectionsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	clientFactory, err := armkeyvault.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Get(ctx,
-		"sample-group",
-		"sample-vault",
-		"sample-pec",
-		nil)
+	res, err := clientFactory.NewPrivateEndpointConnectionsClient().Get(ctx, "sample-group", "sample-vault", "sample-pec", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.PrivateEndpointConnection = armkeyvault.PrivateEndpointConnection{
+	// 	Name: to.Ptr("sample-pec"),
+	// 	Type: to.Ptr("Microsoft.KeyVault/vaults/privateEndpointConnections"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/sample-group/providers/Microsoft.KeyVault/vaults/sample-vault/privateEndpointConnections/sample-pec"),
+	// 	Etag: to.Ptr(""),
+	// 	Properties: &armkeyvault.PrivateEndpointConnectionProperties{
+	// 		PrivateEndpoint: &armkeyvault.PrivateEndpoint{
+	// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-1234-000000000000/resourceGroups/sample-group/providers/Microsoft.Network/privateEndpoints/sample-pe"),
+	// 		},
+	// 		PrivateLinkServiceConnectionState: &armkeyvault.PrivateLinkServiceConnectionState{
+	// 			Description: to.Ptr("This was automatically approved by user1234@contoso.com"),
+	// 			ActionsRequired: to.Ptr(armkeyvault.ActionsRequiredNone),
+	// 			Status: to.Ptr(armkeyvault.PrivateEndpointServiceConnectionStatusApproved),
+	// 		},
+	// 		ProvisioningState: to.Ptr(armkeyvault.PrivateEndpointConnectionProvisioningStateSucceeded),
+	// 	},
+	// }
 }

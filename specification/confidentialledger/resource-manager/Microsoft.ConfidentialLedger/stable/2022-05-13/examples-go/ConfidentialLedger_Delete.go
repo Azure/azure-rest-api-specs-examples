9d5c9a47c6edddcd1a5fdd53ea61f79f01563b0b
azure-sdk <azuresdk@microsoft.com>
2023-03-29 12:11:27 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/confidentialledger/armconfidentialledger/v1.1.0

--- specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples-go/ConfidentialLedger_Delete.go
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples-go/ConfidentialLedger_Delete.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/confidentialledger/armconfidentialledger"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples/ConfidentialLedger_Delete.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/792db17291c758b2bfdbbc0d35d0e2f5b5a1bd05/specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples/ConfidentialLedger_Delete.json
 func ExampleLedgerClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armconfidentialledger.NewLedgerClient("0000000-0000-0000-0000-000000000001", cred, nil)
+	clientFactory, err := armconfidentialledger.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx,
-		"DummyResourceGroupName",
-		"DummyLedgerName",
-		nil)
+	poller, err := clientFactory.NewLedgerClient().BeginDelete(ctx, "DummyResourceGroupName", "DummyLedgerName", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

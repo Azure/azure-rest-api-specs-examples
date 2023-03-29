e6fe8cbbea0a16ca0ed19865f3bd079a047f13e7
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-29 08:04:30 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/resources/armfeatures/v1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples-go/FeatureRegistration/SubscriptionFeatureRegistrationLIST.go
+++ specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples-go/FeatureRegistration/SubscriptionFeatureRegistrationLIST.go
@@ -8,27 +8,46 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armfeatures"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples/FeatureRegistration/SubscriptionFeatureRegistrationLIST.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0cc5e2efd6ffccf30e80d1e150b488dd87198b94/specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples/FeatureRegistration/SubscriptionFeatureRegistrationLIST.json
 func ExampleSubscriptionFeatureRegistrationsClient_NewListBySubscriptionPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armfeatures.NewSubscriptionFeatureRegistrationsClient("00000000-1111-2222-3333-444444444444", cred, nil)
+	clientFactory, err := armfeatures.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListBySubscriptionPager("subscriptionFeatureRegistrationGroupTestRG",
-		nil)
+	pager := clientFactory.NewSubscriptionFeatureRegistrationsClient().NewListBySubscriptionPager("subscriptionFeatureRegistrationGroupTestRG", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.SubscriptionFeatureRegistrationList = armfeatures.SubscriptionFeatureRegistrationList{
+		// 	Value: []*armfeatures.SubscriptionFeatureRegistration{
+		// 		{
+		// 			Name: to.Ptr("testFeature"),
+		// 			Type: to.Ptr("Microsoft.Features/featureProviders/subscriptionFeatureRegistrations"),
+		// 			ID: to.Ptr("/subscriptions/00000000-1111-2222-3333-444444444444/providers/Microsoft.Features/featureProviders/Microsoft.TestRP/subscriptionFeatureRegistrations/testFeature"),
+		// 			Properties: &armfeatures.SubscriptionFeatureRegistrationProperties{
+		// 				ApprovalType: to.Ptr(armfeatures.SubscriptionFeatureRegistrationApprovalTypeApprovalRequired),
+		// 				AuthorizationProfile: &armfeatures.AuthorizationProfile{
+		// 				},
+		// 				FeatureName: to.Ptr("testFeature"),
+		// 				ProviderNamespace: to.Ptr("Microsoft.TestRP"),
+		// 				RegistrationDate: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-02-26T01:57:51.734777Z"); return t}()),
+		// 				ReleaseDate: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-11-05T00:34:53.1243228Z"); return t}()),
+		// 				State: to.Ptr(armfeatures.SubscriptionFeatureRegistrationStatePending),
+		// 				SubscriptionID: to.Ptr("00000000-1111-2222-3333-444444444444"),
+		// 			},
+		// 	}},
+		// }
 	}
 }
--- specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples-go/unregisterFeature.go
+++ specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples-go/unregisterFeature.go
@@ -8,24 +8,30 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armfeatures"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples/unregisterFeature.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0cc5e2efd6ffccf30e80d1e150b488dd87198b94/specification/resources/resource-manager/Microsoft.Features/stable/2021-07-01/examples/unregisterFeature.json
 func ExampleClient_Unregister() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armfeatures.NewClient("ff23096b-f5a2-46ea-bd62-59c3e93fef9a", cred, nil)
+	clientFactory, err := armfeatures.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Unregister(ctx,
-		"Resource Provider Namespace",
-		"feature",
-		nil)
+	res, err := clientFactory.NewClient().Unregister(ctx, "Resource Provider Namespace", "feature", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.FeatureResult = armfeatures.FeatureResult{
+	// 	Name: to.Ptr("Feature1"),
+	// 	Type: to.Ptr("Microsoft.Features/providers/features"),
+	// 	ID: to.Ptr("/subscriptions/ff23096b-f5a2-46ea-bd62-59c3e93fef9a/providers/Microsoft.Features/providers/Microsoft.Test/features/Feature1"),
+	// 	Properties: &armfeatures.FeatureProperties{
+	// 		State: to.Ptr("unregistered"),
+	// 	},
+	// }
 }

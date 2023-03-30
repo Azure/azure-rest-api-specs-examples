8aa73bce066a238cc484152fab5b774212baa3c3
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:14:26 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/marketplaceordering/armmarketplaceordering/v1.1.0

--- specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-go/SignMarketplaceTerms.go
+++ specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-go/SignMarketplaceTerms.go
@@ -8,25 +8,38 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/marketplaceordering/armmarketplaceordering"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples/SignMarketplaceTerms.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples/SignMarketplaceTerms.json
 func ExampleMarketplaceAgreementsClient_Sign() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmarketplaceordering.NewMarketplaceAgreementsClient("subid", cred, nil)
+	clientFactory, err := armmarketplaceordering.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Sign(ctx,
-		"pubid",
-		"offid",
-		"planid",
-		nil)
+	res, err := clientFactory.NewMarketplaceAgreementsClient().Sign(ctx, "pubid", "offid", "planid", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AgreementTerms = armmarketplaceordering.AgreementTerms{
+	// 	Name: to.Ptr("planid"),
+	// 	Type: to.Ptr("Microsoft.MarketplaceOrdering/offertypes"),
+	// 	ID: to.Ptr("id"),
+	// 	Properties: &armmarketplaceordering.AgreementProperties{
+	// 		Accepted: to.Ptr(true),
+	// 		LicenseTextLink: to.Ptr("test.licenseLink"),
+	// 		MarketplaceTermsLink: to.Ptr("test.marketplaceTermsLink"),
+	// 		Plan: to.Ptr("planid"),
+	// 		PrivacyPolicyLink: to.Ptr("test.privacyPolicyLink"),
+	// 		Product: to.Ptr("offid"),
+	// 		Publisher: to.Ptr("pubid"),
+	// 		RetrieveDatetime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-08-15T11:33:07.12132Z"); return t}()),
+	// 		Signature: to.Ptr("ASDFSDAFWEFASDGWERLWER"),
+	// 	},
+	// }
 }

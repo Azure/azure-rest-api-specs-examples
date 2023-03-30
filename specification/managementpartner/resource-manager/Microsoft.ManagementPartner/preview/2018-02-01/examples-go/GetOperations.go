74ea283caffec76290f29ea99e1f312114f54a47
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:17:07 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/managementpartner/armmanagementpartner/v0.6.0

--- specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples-go/GetOperations.go
+++ specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples-go/GetOperations.go
@@ -8,26 +8,66 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/managementpartner/armmanagementpartner"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples/GetOperations.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples/GetOperations.json
 func ExampleOperationClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmanagementpartner.NewOperationClient(cred, nil)
+	clientFactory, err := armmanagementpartner.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListPager(nil)
+	pager := clientFactory.NewOperationClient().NewListPager(nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.OperationList = armmanagementpartner.OperationList{
+		// 	Value: []*armmanagementpartner.OperationResponse{
+		// 		{
+		// 			Name: to.Ptr("Microsoft.ManagementPartner/partners/read"),
+		// 			Display: &armmanagementpartner.OperationDisplay{
+		// 				Description: to.Ptr("Read All ManagementPartner"),
+		// 				Operation: to.Ptr("Get ManagementPartner"),
+		// 				Provider: to.Ptr("Microsoft ManagementPartner"),
+		// 				Resource: to.Ptr("ManagementPartner"),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("Microsoft.ManagementPartner/partners/write"),
+		// 			Display: &armmanagementpartner.OperationDisplay{
+		// 				Description: to.Ptr("Create any ManagementPartner"),
+		// 				Operation: to.Ptr("Create ManagementPartner"),
+		// 				Provider: to.Ptr("Microsoft ManagementPartner"),
+		// 				Resource: to.Ptr("ManagementPartner"),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("Microsoft.ManagementPartner/partners/action"),
+		// 			Display: &armmanagementpartner.OperationDisplay{
+		// 				Description: to.Ptr("Update any ManagementPartner"),
+		// 				Operation: to.Ptr("Update ManagementPartner"),
+		// 				Provider: to.Ptr("Microsoft ManagementPartner"),
+		// 				Resource: to.Ptr("ManagementPartner"),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("Microsoft.ManagementPartner/partners/delete"),
+		// 			Display: &armmanagementpartner.OperationDisplay{
+		// 				Description: to.Ptr("Delete any ManagementPartner"),
+		// 				Operation: to.Ptr("Delete ManagementPartner"),
+		// 				Provider: to.Ptr("Microsoft ManagementPartner"),
+		// 				Resource: to.Ptr("ManagementPartner"),
+		// 			},
+		// 	}},
+		// }
 	}
 }

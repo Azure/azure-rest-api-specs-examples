74b6193c741489b202a18f30a81c590c999026e9
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:30:49 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/hanaonazure/armhanaonazure/v0.6.0

--- specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples-go/HanaOperations_List.go
+++ specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples-go/HanaOperations_List.go
@@ -8,26 +8,50 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hanaonazure/armhanaonazure"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples/HanaOperations_List.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples/HanaOperations_List.json
 func ExampleOperationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhanaonazure.NewOperationsClient(cred, nil)
+	clientFactory, err := armhanaonazure.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListPager(nil)
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.OperationList = armhanaonazure.OperationList{
+		// 	Value: []*armhanaonazure.Operation{
+		// 		{
+		// 			Name: to.Ptr("HanaOp1"),
+		// 			Display: &armhanaonazure.Display{
+		// 				Description: to.Ptr("HanaOp1Description"),
+		// 				Operation: to.Ptr("HanaOp1OperationName"),
+		// 				Origin: to.Ptr("HanaOp1Origin"),
+		// 				Provider: to.Ptr("HanaOp1ProviderName"),
+		// 				Resource: to.Ptr("HanaOp1ResourceName"),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("HanaOp2"),
+		// 			Display: &armhanaonazure.Display{
+		// 				Description: to.Ptr("HanaOp2Description"),
+		// 				Operation: to.Ptr("HanaOp2OperationName"),
+		// 				Origin: to.Ptr("HanaOp2Origin"),
+		// 				Provider: to.Ptr("HanaOp2ProviderName"),
+		// 				Resource: to.Ptr("HanaOp2ResourceName"),
+		// 			},
+		// 	}},
+		// }
 	}
 }
--- specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples-go/ProviderInstances_List.go
+++ specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples-go/ProviderInstances_List.go
@@ -8,28 +8,60 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hanaonazure/armhanaonazure"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples/ProviderInstances_List.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/hanaonazure/resource-manager/Microsoft.HanaOnAzure/preview/2020-02-07-preview/examples/ProviderInstances_List.json
 func ExampleProviderInstancesClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhanaonazure.NewProviderInstancesClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	clientFactory, err := armhanaonazure.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListPager("myResourceGroup",
-		"mySapMonitor",
-		nil)
+	pager := clientFactory.NewProviderInstancesClient().NewListPager("myResourceGroup", "mySapMonitor", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.ProviderInstanceListResult = armhanaonazure.ProviderInstanceListResult{
+		// 	Value: []*armhanaonazure.ProviderInstance{
+		// 		{
+		// 			Name: to.Ptr("myProviderInstance1"),
+		// 			Type: to.Ptr("Microsoft.HanaOnAzure/sapMonitors/providerInstances"),
+		// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.HanaOnAzure/sapMonitors/mySapMonitor/providerInstances/myProviderInstance1"),
+		// 			Properties: &armhanaonazure.ProviderInstanceProperties{
+		// 				Type: to.Ptr("SapHana"),
+		// 				Metadata: to.Ptr("{\"key\":\"value\"}"),
+		// 				Properties: to.Ptr("{\"hostname\":\"10.0.0.6\",\"dbName\":\"SYSTEMDB\",\"sqlPort\":30013,\"dbUsername\":\"SYSTEM\"}"),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("myProviderInstance2"),
+		// 			Type: to.Ptr("Microsoft.HanaOnAzure/sapMonitors/providerInstances"),
+		// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.HanaOnAzure/sapMonitors/mySapMonitor/providerInstances/myProviderInstance2"),
+		// 			Properties: &armhanaonazure.ProviderInstanceProperties{
+		// 				Type: to.Ptr("PrometheusHaCluster"),
+		// 				Metadata: to.Ptr("{\"key\":\"value\"}"),
+		// 				Properties: to.Ptr("{\"prometheusUrl\":\"http://10.0.0.21:9664/metrics\"}"),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("myProviderInstance2"),
+		// 			Type: to.Ptr("Microsoft.HanaOnAzure/sapMonitors/providerInstances"),
+		// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.HanaOnAzure/sapMonitors/mySapMonitor/providerInstances/myProviderInstance2"),
+		// 			Properties: &armhanaonazure.ProviderInstanceProperties{
+		// 				Type: to.Ptr("MsSqlServer"),
+		// 				Metadata: to.Ptr("{\"key\":\"value\"}"),
+		// 				Properties: to.Ptr("{\"sqlHostname\":\"10.0.0.6\",\"sqlPort\":1433,\"sqlUsername\":\"sqladmin\"}"),
+		// 			},
+		// 	}},
+		// }
 	}
 }

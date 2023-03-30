61af7d40bb8e625ee02b7bd2523b6ed5ac817d82
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:28:05 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/v1.1.0

--- specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-go/DeleteClusterExample.go
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-go/DeleteClusterExample.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridkubernetes/armhybridkubernetes"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/DeleteClusterExample.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/DeleteClusterExample.json
 func ExampleConnectedClusterClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
+	clientFactory, err := armhybridkubernetes.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx,
-		"k8sc-rg",
-		"testCluster",
-		nil)
+	poller, err := clientFactory.NewConnectedClusterClient().BeginDelete(ctx, "k8sc-rg", "testCluster", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

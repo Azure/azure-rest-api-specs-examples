3a4749c901c59400c82561cf55e88f8951618326
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:08:46 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/oep/armoep/v0.5.0

--- specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples-go/OepResource_Delete.go
+++ specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples-go/OepResource_Delete.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/oep/armoep"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples/OepResource_Delete.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples/OepResource_Delete.json
 func ExampleEnergyServicesClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armoep.NewEnergyServicesClient("0000000-0000-0000-0000-000000000001", cred, nil)
+	clientFactory, err := armoep.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx,
-		"DummyResourceGroupName",
-		"DummyResourceName",
-		nil)
+	poller, err := clientFactory.NewEnergyServicesClient().BeginDelete(ctx, "DummyResourceGroupName", "DummyResourceName", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
--- specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples-go/OepResource_Get.go
+++ specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples-go/OepResource_Get.go
@@ -8,24 +8,42 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/oep/armoep"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples/OepResource_Get.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/oep/resource-manager/Microsoft.OpenEnergyPlatform/preview/2022-04-04-preview/examples/OepResource_Get.json
 func ExampleEnergyServicesClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armoep.NewEnergyServicesClient("0000000-0000-0000-0000-000000000001", cred, nil)
+	clientFactory, err := armoep.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Get(ctx,
-		"DummyResourceGroupName",
-		"DummyResourceName",
-		nil)
+	res, err := clientFactory.NewEnergyServicesClient().Get(ctx, "DummyResourceGroupName", "DummyResourceName", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.EnergyService = armoep.EnergyService{
+	// 	Name: to.Ptr("DummyResourceName"),
+	// 	Type: to.Ptr("Microsoft.OEP/oepResource"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000001/resourceGroups/DummyResourceGroupName/providers/Microsoft.OEP/oepResource/DummyResourceName"),
+	// 	Location: to.Ptr("WestUS"),
+	// 	Properties: &armoep.EnergyServiceProperties{
+	// 		AuthAppID: to.Ptr("sample-id"),
+	// 		DataPartitionNames: []*armoep.DataPartitionNames{
+	// 			{
+	// 				Name: to.Ptr("dataPartition1"),
+	// 			},
+	// 			{
+	// 				Name: to.Ptr("dataPartition2"),
+	// 		}},
+	// 		ProvisioningState: to.Ptr(armoep.ProvisioningStateSucceeded),
+	// 	},
+	// 	Tags: map[string]*string{
+	// 		"additionalProps1": to.Ptr("additional properties"),
+	// 	},
+	// }
 }

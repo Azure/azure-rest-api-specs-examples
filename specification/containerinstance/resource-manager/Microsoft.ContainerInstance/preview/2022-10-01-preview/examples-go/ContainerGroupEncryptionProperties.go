f7e97da3b886a36300b7b2e2ffb63793b40e383d
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:50:07 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerinstance/armcontainerinstance/v2.2.0-beta.2

--- specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerGroupEncryptionProperties.go
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerGroupEncryptionProperties.go
@@ -9,18 +9,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerinstance/armcontainerinstance/v2"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerGroupEncryptionProperties.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/71121282e39bccae590462648e77bca283df6d2b/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerGroupEncryptionProperties.json
 func ExampleContainerGroupsClient_BeginCreateOrUpdate_containerGroupWithEncryptionProperties() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armcontainerinstance.NewContainerGroupsClient("subid", cred, nil)
+	clientFactory, err := armcontainerinstance.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx, "demo", "demo1", armcontainerinstance.ContainerGroup{
+	poller, err := clientFactory.NewContainerGroupsClient().BeginCreateOrUpdate(ctx, "demo", "demo1", armcontainerinstance.ContainerGroup{
 		Identity: &armcontainerinstance.ContainerGroupIdentity{
 			Type: to.Ptr(armcontainerinstance.ResourceIdentityTypeUserAssigned),
 			UserAssignedIdentities: map[string]*armcontainerinstance.UserAssignedIdentities{
@@ -73,6 +73,72 @@ func ExampleContainerGroupsClient_BeginCreateOrUpdate_containerGroupWithEncrypti
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ContainerGroup = armcontainerinstance.ContainerGroup{
+	// 	Identity: &armcontainerinstance.ContainerGroupIdentity{
+	// 		Type: to.Ptr(armcontainerinstance.ResourceIdentityTypeUserAssigned),
+	// 		TenantID: to.Ptr("<tenant id>"),
+	// 		UserAssignedIdentities: map[string]*armcontainerinstance.UserAssignedIdentities{
+	// 			"/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/container-group-identity": &armcontainerinstance.UserAssignedIdentities{
+	// 				ClientID: to.Ptr("<client id>"),
+	// 				PrincipalID: to.Ptr("<principal id>"),
+	// 			},
+	// 		},
+	// 	},
+	// 	Properties: &armcontainerinstance.ContainerGroupPropertiesProperties{
+	// 		Containers: []*armcontainerinstance.Container{
+	// 			{
+	// 				Name: to.Ptr("demo1"),
+	// 				Properties: &armcontainerinstance.ContainerProperties{
+	// 					Command: []*string{
+	// 					},
+	// 					EnvironmentVariables: []*armcontainerinstance.EnvironmentVariable{
+	// 					},
+	// 					Image: to.Ptr("nginx"),
+	// 					Ports: []*armcontainerinstance.ContainerPort{
+	// 						{
+	// 							Port: to.Ptr[int32](80),
+	// 					}},
+	// 					Resources: &armcontainerinstance.ResourceRequirements{
+	// 						Requests: &armcontainerinstance.ResourceRequests{
+	// 							CPU: to.Ptr[float64](1),
+	// 							MemoryInGB: to.Ptr[float64](1.5),
+	// 						},
+	// 					},
+	// 				},
+	// 		}},
+	// 		EncryptionProperties: &armcontainerinstance.EncryptionProperties{
+	// 			Identity: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/container-group-identity"),
+	// 			KeyName: to.Ptr("test-key"),
+	// 			KeyVersion: to.Ptr("<key version>"),
+	// 			VaultBaseURL: to.Ptr("https://testkeyvault.vault.azure.net/"),
+	// 		},
+	// 		ImageRegistryCredentials: []*armcontainerinstance.ImageRegistryCredential{
+	// 		},
+	// 		InitContainers: []*armcontainerinstance.InitContainerDefinition{
+	// 		},
+	// 		InstanceView: &armcontainerinstance.ContainerGroupPropertiesInstanceView{
+	// 			Events: []*armcontainerinstance.Event{
+	// 			},
+	// 			State: to.Ptr("Pending"),
+	// 		},
+	// 		IPAddress: &armcontainerinstance.IPAddress{
+	// 			Type: to.Ptr(armcontainerinstance.ContainerGroupIPAddressTypePublic),
+	// 			Ports: []*armcontainerinstance.Port{
+	// 				{
+	// 					Port: to.Ptr[int32](80),
+	// 					Protocol: to.Ptr(armcontainerinstance.ContainerGroupNetworkProtocolTCP),
+	// 			}},
+	// 		},
+	// 		OSType: to.Ptr(armcontainerinstance.OperatingSystemTypesLinux),
+	// 		ProvisioningState: to.Ptr("Succeeded"),
+	// 		SKU: to.Ptr(armcontainerinstance.ContainerGroupSKUStandard),
+	// 	},
+	// 	Name: to.Ptr("demo1"),
+	// 	Type: to.Ptr("Microsoft.ContainerInstance/containerGroups"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/test-rg/providers/Microsoft.ContainerInstance/containerGroups/demo1"),
+	// 	Location: to.Ptr("eastus2"),
+	// }
 }

755182b3ec9fd505de828f44f0e27ffaa264a190
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:29:09 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/healthbot/armhealthbot/v1.1.0

--- specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples-go/ResourceDeletionDelete.go
+++ specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples-go/ResourceDeletionDelete.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/healthbot/armhealthbot"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples/ResourceDeletionDelete.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples/ResourceDeletionDelete.json
 func ExampleBotsClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhealthbot.NewBotsClient("subid", cred, nil)
+	clientFactory, err := armhealthbot.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx,
-		"healthbotClient",
-		"samplebotname",
-		nil)
+	poller, err := clientFactory.NewBotsClient().BeginDelete(ctx, "healthbotClient", "samplebotname", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

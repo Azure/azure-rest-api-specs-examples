06ef4d0bed8152bbb93d43c6858a4a42d51a4328
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:10:16 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/networkfunction/armnetworkfunction/v2.1.0

--- specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-go/AzureTrafficCollectorCreate.go
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-go/AzureTrafficCollectorCreate.go
@@ -9,18 +9,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/networkfunction/armnetworkfunction/v2"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/AzureTrafficCollectorCreate.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/527f6d35fb0d85c48210ca0f6f6f42814d63bd33/specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/AzureTrafficCollectorCreate.json
 func ExampleAzureTrafficCollectorsClient_BeginCreateOrUpdate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armnetworkfunction.NewAzureTrafficCollectorsClient("subid", cred, nil)
+	clientFactory, err := armnetworkfunction.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx, "rg1", "atc", armnetworkfunction.AzureTrafficCollector{
+	poller, err := clientFactory.NewAzureTrafficCollectorsClient().BeginCreateOrUpdate(ctx, "rg1", "atc", armnetworkfunction.AzureTrafficCollector{
 		Location: to.Ptr("West US"),
 		Tags: map[string]*string{
 			"key1": to.Ptr("value1"),
@@ -34,6 +34,22 @@ func ExampleAzureTrafficCollectorsClient_BeginCreateOrUpdate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AzureTrafficCollector = armnetworkfunction.AzureTrafficCollector{
+	// 	Name: to.Ptr("atc"),
+	// 	Type: to.Ptr("Microsoft.NetworkFunction/azureTrafficCollectors"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.NetworkFunction/azureTrafficCollectors/atc"),
+	// 	Location: to.Ptr("West US"),
+	// 	Tags: map[string]*string{
+	// 		"key1": to.Ptr("value1"),
+	// 	},
+	// 	Etag: to.Ptr("w/\\00000000-0000-0000-0000-000000000000\\"),
+	// 	Properties: &armnetworkfunction.AzureTrafficCollectorPropertiesFormat{
+	// 		CollectorPolicies: []*armnetworkfunction.ResourceReference{
+	// 		},
+	// 		ProvisioningState: to.Ptr(armnetworkfunction.ProvisioningStateSucceeded),
+	// 	},
+	// }
 }

e9cc6b41e6a549a8d14fe9e3009b775d69ea228e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-30 08:06:11 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-newrelicobservability_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples-java/Plans_List_MaximumSet_Gen.java
@@ -0,0 +1,15 @@
+/** Samples for Plans List. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples/Plans_List_MaximumSet_Gen.json
+     */
+    /**
+     * Sample code: Plans_List_MaximumSet_Gen.
+     *
+     * @param manager Entry point to NewRelicObservabilityManager.
+     */
+    public static void plansListMaximumSetGen(
+        com.azure.resourcemanager.newrelicobservability.NewRelicObservabilityManager manager) {
+        manager.plans().list("pwuxgvrmkk", "hilawwjz", com.azure.core.util.Context.NONE);
+    }
+}

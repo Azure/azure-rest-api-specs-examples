58d01d6d908bbf811422247bba3765c6e50f9e5c
azure-sdk <azuresdk@microsoft.com>
2023-03-30 12:47:31 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/customerlockbox/armcustomerlockbox/v0.6.0

--- specification/customerlockbox/resource-manager/Microsoft.CustomerLockbox/preview/2018-02-28-preview/examples-go/DisableLockbox.go
+++ specification/customerlockbox/resource-manager/Microsoft.CustomerLockbox/preview/2018-02-28-preview/examples-go/DisableLockbox.go
@@ -8,19 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/customerlockbox/armcustomerlockbox"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/customerlockbox/resource-manager/Microsoft.CustomerLockbox/preview/2018-02-28-preview/examples/DisableLockbox.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/customerlockbox/resource-manager/Microsoft.CustomerLockbox/preview/2018-02-28-preview/examples/DisableLockbox.json
 func ExamplePostClient_DisableLockbox() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armcustomerlockbox.NewPostClient(cred, nil)
+	clientFactory, err := armcustomerlockbox.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = client.DisableLockbox(ctx,
-		nil)
+	_, err = clientFactory.NewPostClient().DisableLockbox(ctx, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

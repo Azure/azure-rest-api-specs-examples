f5f20c2cd12d6dc6547c98f7b775cab446011d1e
azure-sdk <azuresdk@microsoft.com>
2023-03-31 12:14:55 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/sqlvirtualmachine/armsqlvirtualmachine/v0.8.0

--- specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples-go/CreateOrUpdateSqlVirtualMachineAutomatedBackupWeekly.go
+++ specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples-go/CreateOrUpdateSqlVirtualMachineAutomatedBackupWeekly.go
@@ -9,18 +9,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/sqlvirtualmachine/armsqlvirtualmachine"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples/CreateOrUpdateSqlVirtualMachineAutomatedBackupWeekly.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/e24bbf6a66cb0a19c072c6f15cee163acbd7acf7/specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples/CreateOrUpdateSqlVirtualMachineAutomatedBackupWeekly.json
 func ExampleSQLVirtualMachinesClient_BeginCreateOrUpdate_createsOrUpdatesASqlVirtualMachineForAutomatedBackUpSettingsWithWeeklyAndDaysOfTheWeekToRunTheBackUp() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armsqlvirtualmachine.NewSQLVirtualMachinesClient("00000000-1111-2222-3333-444444444444", cred, nil)
+	clientFactory, err := armsqlvirtualmachine.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx, "testrg", "testvm", armsqlvirtualmachine.SQLVirtualMachine{
+	poller, err := clientFactory.NewSQLVirtualMachinesClient().BeginCreateOrUpdate(ctx, "testrg", "testvm", armsqlvirtualmachine.SQLVirtualMachine{
 		Location: to.Ptr("northeurope"),
 		Properties: &armsqlvirtualmachine.Properties{
 			AutoBackupSettings: &armsqlvirtualmachine.AutoBackupSettings{
@@ -82,6 +82,22 @@ func ExampleSQLVirtualMachinesClient_BeginCreateOrUpdate_createsOrUpdatesASqlVir
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.SQLVirtualMachine = armsqlvirtualmachine.SQLVirtualMachine{
+	// 	Name: to.Ptr("testvm"),
+	// 	Type: to.Ptr("Microsoft.SqlVirtualMachine/sqlVirtualMachines"),
+	// 	ID: to.Ptr("/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/testrg/providers/Microsoft.SqlVirtualMachine/sqlVirtualMachines/testvm"),
+	// 	Location: to.Ptr("northeurope"),
+	// 	Properties: &armsqlvirtualmachine.Properties{
+	// 		EnableAutomaticUpgrade: to.Ptr(false),
+	// 		LeastPrivilegeMode: to.Ptr(armsqlvirtualmachine.LeastPrivilegeMode("NotSet")),
+	// 		ProvisioningState: to.Ptr("Succeeded"),
+	// 		SQLImageSKU: to.Ptr(armsqlvirtualmachine.SQLImageSKUEnterprise),
+	// 		SQLManagement: to.Ptr(armsqlvirtualmachine.SQLManagementModeFull),
+	// 		SQLServerLicenseType: to.Ptr(armsqlvirtualmachine.SQLServerLicenseTypePAYG),
+	// 		VirtualMachineResourceID: to.Ptr("/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/testrg/providers/Microsoft.Compute/virtualMachines/testvm"),
+	// 	},
+	// }
 }
--- specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples-go/GetSqlVirtualMachineGroup.go
+++ specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples-go/GetSqlVirtualMachineGroup.go
@@ -8,21 +8,44 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/sqlvirtualmachine/armsqlvirtualmachine"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples/GetSqlVirtualMachineGroup.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/e24bbf6a66cb0a19c072c6f15cee163acbd7acf7/specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-07-01-preview/examples/GetSqlVirtualMachineGroup.json
 func ExampleGroupsClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armsqlvirtualmachine.NewGroupsClient("00000000-1111-2222-3333-444444444444", cred, nil)
+	clientFactory, err := armsqlvirtualmachine.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Get(ctx, "testrg", "testvmgroup", nil)
+	res, err := clientFactory.NewGroupsClient().Get(ctx, "testrg", "testvmgroup", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Group = armsqlvirtualmachine.Group{
+	// 	Name: to.Ptr("testvmgroup"),
+	// 	Type: to.Ptr("Microsoft.SqlVirtualMachine/sqlVirtualMachineGroups"),
+	// 	ID: to.Ptr("/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/testrg/providers/Microsoft.SqlVirtualMachine/sqlVirtualMachineGroups/testvmgroup"),
+	// 	Location: to.Ptr("northeurope"),
+	// 	Tags: map[string]*string{
+	// 		"mytag": to.Ptr("myval"),
+	// 	},
+	// 	Properties: &armsqlvirtualmachine.GroupProperties{
+	// 		ProvisioningState: to.Ptr("Succeeded"),
+	// 		SQLImageOffer: to.Ptr("SQL2016-WS2016"),
+	// 		SQLImageSKU: to.Ptr(armsqlvirtualmachine.SQLVMGroupImageSKUEnterprise),
+	// 		WsfcDomainProfile: &armsqlvirtualmachine.WsfcDomainProfile{
+	// 			ClusterBootstrapAccount: to.Ptr("testrpadmin"),
+	// 			ClusterOperatorAccount: to.Ptr("testrp@testdomain.com"),
+	// 			DomainFqdn: to.Ptr("testdomain.com"),
+	// 			OuPath: to.Ptr("OU=WSCluster,DC=testdomain,DC=com"),
+	// 			SQLServiceAccount: to.Ptr("sqlservice@testdomain.com"),
+	// 			StorageAccountURL: to.Ptr("https://storgact.blob.core.windows.net/"),
+	// 		},
+	// 	},
+	// }
 }

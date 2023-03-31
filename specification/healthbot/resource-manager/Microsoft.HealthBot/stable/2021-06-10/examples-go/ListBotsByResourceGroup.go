d5cca99cd47728f2b650b6ef77a85790b229d388
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-31 08:06:28 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/healthbot/armhealthbot/v1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples-go/ListBotsByResourceGroup.go
+++ specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples-go/ListBotsByResourceGroup.go
@@ -8,27 +8,81 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/healthbot/armhealthbot"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples/ListBotsByResourceGroup.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples/ListBotsByResourceGroup.json
 func ExampleBotsClient_NewListByResourceGroupPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhealthbot.NewBotsClient("subscription-id", cred, nil)
+	clientFactory, err := armhealthbot.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListByResourceGroupPager("OneResourceGroupName",
-		nil)
+	pager := clientFactory.NewBotsClient().NewListByResourceGroupPager("OneResourceGroupName", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.BotResponseList = armhealthbot.BotResponseList{
+		// 	Value: []*armhealthbot.HealthBot{
+		// 		{
+		// 			Name: to.Ptr("samplebotname"),
+		// 			Type: to.Ptr("Microsoft.HealthBot/healthBots"),
+		// 			ID: to.Ptr("/subscriptions/subscription-id/resourceGroups/OneResourceGroupName/providers/Microsoft.HealthBot/healthBots/samplebotname"),
+		// 			SystemData: &armhealthbot.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-05-05T17:18:19.1234567Z"); return t}()),
+		// 				CreatedBy: to.Ptr("jack@outlook.com"),
+		// 				CreatedByType: to.Ptr(armhealthbot.IdentityTypeUser),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-05-06T17:18:19.1234567Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("ryan@outlook.com"),
+		// 				LastModifiedByType: to.Ptr(armhealthbot.IdentityTypeUser),
+		// 			},
+		// 			Location: to.Ptr("East US"),
+		// 			Properties: &armhealthbot.Properties{
+		// 				BotManagementPortalLink: to.Ptr("https://us.healthbot.microsoft.com/account/samplebotname-1yhd91k"),
+		// 			},
+		// 			SKU: &armhealthbot.SKU{
+		// 				Name: to.Ptr(armhealthbot.SKUNameF0),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("samplebotname2"),
+		// 			Type: to.Ptr("Microsoft.HealthBot/healthBots"),
+		// 			ID: to.Ptr("/subscriptions/subscription-id/resourceGroups/OneResourceGroupName/providers/Microsoft.HealthBot/healthBots/samplebotname2"),
+		// 			SystemData: &armhealthbot.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-05-05T17:18:19.1234567Z"); return t}()),
+		// 				CreatedBy: to.Ptr("jack@outlook.com"),
+		// 				CreatedByType: to.Ptr(armhealthbot.IdentityTypeUser),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-05-06T17:18:19.1234567Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("ryan@outlook.com"),
+		// 				LastModifiedByType: to.Ptr(armhealthbot.IdentityTypeUser),
+		// 			},
+		// 			Location: to.Ptr("East US"),
+		// 			Identity: &armhealthbot.Identity{
+		// 				Type: to.Ptr(armhealthbot.ResourceIdentityTypeSystemAssignedUserAssigned),
+		// 				PrincipalID: to.Ptr("principalId"),
+		// 				TenantID: to.Ptr("tenantId"),
+		// 				UserAssignedIdentities: map[string]*armhealthbot.UserAssignedIdentity{
+		// 					"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi": &armhealthbot.UserAssignedIdentity{
+		// 					},
+		// 					"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi2": &armhealthbot.UserAssignedIdentity{
+		// 					},
+		// 				},
+		// 			},
+		// 			Properties: &armhealthbot.Properties{
+		// 				BotManagementPortalLink: to.Ptr("https://us.healthbot.microsoft.com/account/samplebotname2-hdi1osc"),
+		// 			},
+		// 			SKU: &armhealthbot.SKU{
+		// 				Name: to.Ptr(armhealthbot.SKUNameS1),
+		// 			},
+		// 	}},
+		// }
 	}
 }
--- specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples-go/ResourceCreationPut.go
+++ specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples-go/ResourceCreationPut.go
@@ -9,34 +9,30 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/healthbot/armhealthbot"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples/ResourceCreationPut.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2021-06-10/examples/ResourceCreationPut.json
 func ExampleBotsClient_BeginCreate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhealthbot.NewBotsClient("subid", cred, nil)
+	clientFactory, err := armhealthbot.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreate(ctx,
-		"healthbotClient",
-		"samplebotname",
-		armhealthbot.HealthBot{
-			Location: to.Ptr("East US"),
-			Identity: &armhealthbot.Identity{
-				Type: to.Ptr(armhealthbot.ResourceIdentityTypeSystemAssignedUserAssigned),
-				UserAssignedIdentities: map[string]*armhealthbot.UserAssignedIdentity{
-					"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi":  {},
-					"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi2": {},
-				},
-			},
-			SKU: &armhealthbot.SKU{
-				Name: to.Ptr(armhealthbot.SKUNameF0),
+	poller, err := clientFactory.NewBotsClient().BeginCreate(ctx, "healthbotClient", "samplebotname", armhealthbot.HealthBot{
+		Location: to.Ptr("East US"),
+		Identity: &armhealthbot.Identity{
+			Type: to.Ptr(armhealthbot.ResourceIdentityTypeSystemAssignedUserAssigned),
+			UserAssignedIdentities: map[string]*armhealthbot.UserAssignedIdentity{
+				"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi":  {},
+				"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi2": {},
 			},
 		},
-		nil)
+		SKU: &armhealthbot.SKU{
+			Name: to.Ptr(armhealthbot.SKUNameF0),
+		},
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -44,6 +40,38 @@ func ExampleBotsClient_BeginCreate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.HealthBot = armhealthbot.HealthBot{
+	// 	Name: to.Ptr("samplebotname"),
+	// 	Type: to.Ptr("Microsoft.HealthBot/healthBots"),
+	// 	ID: to.Ptr("/subscriptions/subscription-id/resourceGroups/OneResourceGroupName/providers/Microsoft.HealthBot/healthBots/samplebotname"),
+	// 	SystemData: &armhealthbot.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-05-05T17:18:19.1234567Z"); return t}()),
+	// 		CreatedBy: to.Ptr("jack@outlook.com"),
+	// 		CreatedByType: to.Ptr(armhealthbot.IdentityTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-05-06T17:18:19.1234567Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("ryan@outlook.com"),
+	// 		LastModifiedByType: to.Ptr(armhealthbot.IdentityTypeUser),
+	// 	},
+	// 	Location: to.Ptr("East US"),
+	// 	Identity: &armhealthbot.Identity{
+	// 		Type: to.Ptr(armhealthbot.ResourceIdentityTypeSystemAssignedUserAssigned),
+	// 		PrincipalID: to.Ptr("principalId"),
+	// 		TenantID: to.Ptr("tenantId"),
+	// 		UserAssignedIdentities: map[string]*armhealthbot.UserAssignedIdentity{
+	// 			"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi": &armhealthbot.UserAssignedIdentity{
+	// 			},
+	// 			"/subscriptions/subscription-id/resourcegroups/myrg/providers/microsoft.managedidentity/userassignedidentities/my-mi2": &armhealthbot.UserAssignedIdentity{
+	// 			},
+	// 		},
+	// 	},
+	// 	Properties: &armhealthbot.Properties{
+	// 		BotManagementPortalLink: to.Ptr("https://us.healthbot.microsoft.com/account/contoso"),
+	// 	},
+	// 	SKU: &armhealthbot.SKU{
+	// 		Name: to.Ptr(armhealthbot.SKUNameF0),
+	// 	},
+	// }
 }

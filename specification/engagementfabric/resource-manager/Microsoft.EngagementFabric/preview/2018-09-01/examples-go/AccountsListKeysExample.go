ddbe613c03363dd27fadcb16653e67282838b470
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-31 08:05:21 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/engagementfabric/armengagementfabric/v0.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples-go/AccountsListKeysExample.go
+++ specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples-go/AccountsListKeysExample.go
@@ -8,28 +8,50 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/engagementfabric/armengagementfabric"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples/AccountsListKeysExample.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/163e27c0ca7570bc39e00a46f255740d9b3ba3cb/specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples/AccountsListKeysExample.json
 func ExampleAccountsClient_NewListKeysPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armengagementfabric.NewAccountsClient("EDBF0095-A524-4A84-95FB-F72DA41AA6A1", cred, nil)
+	clientFactory, err := armengagementfabric.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListKeysPager("ExampleRg",
-		"ExampleAccount",
-		nil)
+	pager := clientFactory.NewAccountsClient().NewListKeysPager("ExampleRg", "ExampleAccount", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.KeyDescriptionList = armengagementfabric.KeyDescriptionList{
+		// 	Value: []*armengagementfabric.KeyDescription{
+		// 		{
+		// 			Name: to.Ptr("Full"),
+		// 			Rank: to.Ptr(armengagementfabric.KeyRankPrimaryKey),
+		// 			Value: to.Ptr("<ExampleFullPrimaryKeyValue>"),
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("Full"),
+		// 			Rank: to.Ptr(armengagementfabric.KeyRankSecondaryKey),
+		// 			Value: to.Ptr("<ExampleFullSecondaryKeyValue>"),
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("Device"),
+		// 			Rank: to.Ptr(armengagementfabric.KeyRankPrimaryKey),
+		// 			Value: to.Ptr("<ExampleDevicePrimaryKeyValue>"),
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("Device"),
+		// 			Rank: to.Ptr(armengagementfabric.KeyRankSecondaryKey),
+		// 			Value: to.Ptr("<ExampleDeviceSecondaryKeyValue>"),
+		// 	}},
+		// }
 	}
 }
--- specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples-go/ChannelsDeleteExample.go
+++ specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples-go/ChannelsDeleteExample.go
@@ -8,22 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/engagementfabric/armengagementfabric"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples/ChannelsDeleteExample.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/163e27c0ca7570bc39e00a46f255740d9b3ba3cb/specification/engagementfabric/resource-manager/Microsoft.EngagementFabric/preview/2018-09-01/examples/ChannelsDeleteExample.json
 func ExampleChannelsClient_Delete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armengagementfabric.NewChannelsClient("EDBF0095-A524-4A84-95FB-F72DA41AA6A1", cred, nil)
+	clientFactory, err := armengagementfabric.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = client.Delete(ctx,
-		"ExampleRg",
-		"ExampleAccount",
-		"ExampleChannel",
-		nil)
+	_, err = clientFactory.NewChannelsClient().Delete(ctx, "ExampleRg", "ExampleAccount", "ExampleChannel", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

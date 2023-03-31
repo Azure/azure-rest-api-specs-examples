e1443c64c28c68b5c2c16528a89da12db2fb4a20
azure-sdk <azuresdk@microsoft.com>
2023-03-31 12:26:57 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/quota/armquota/v0.6.0

--- specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples-go/getNetworkOneSkuUsages.go
+++ specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples-go/getNetworkOneSkuUsages.go
@@ -8,8 +8,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/quota/armquota"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples/getComputeOneSkuUsages.json
-func ExampleUsagesClient_Get_quotasUsagesRequestForCompute() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples/getNetworkOneSkuUsages.json
+func ExampleUsagesClient_Get_quotasUsagesRequestForNetwork() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -19,7 +19,7 @@ func ExampleUsagesClient_Get_quotasUsagesRequestForCompute() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := clientFactory.NewUsagesClient().Get(ctx, "standardNDSFamily", "subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Compute/locations/eastus", nil)
+	res, err := clientFactory.NewUsagesClient().Get(ctx, "MinPublicIpInterNetworkPrefixLength", "subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Network/locations/eastus", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -27,18 +27,18 @@ func ExampleUsagesClient_Get_quotasUsagesRequestForCompute() {
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res.CurrentUsagesBase = armquota.CurrentUsagesBase{
-	// 	Name: to.Ptr("standardNDSFamily"),
-	// 	Type: to.Ptr("Microsoft.Quota/Usages"),
-	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Compute/locations/eastus/providers/Microsoft.Quota/Usages/standardNDSFamily"),
+	// 	Name: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
+	// 	Type: to.Ptr("Microsoft.Quota/usages"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Network/locations/eastus/providers/Microsoft.Quota/usages/MinPublicIpInterNetworkPrefixLength"),
 	// 	Properties: &armquota.UsagesProperties{
 	// 		Name: &armquota.ResourceName{
-	// 			LocalizedValue: to.Ptr("Standard NDS Family vCPUs"),
-	// 			Value: to.Ptr("standardNDSFamily"),
+	// 			LocalizedValue: to.Ptr("Min Public Ip InterNetwork Prefix Length"),
+	// 			Value: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
 	// 		},
 	// 		IsQuotaApplicable: to.Ptr(true),
+	// 		ResourceType: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
 	// 		Unit: to.Ptr("Count"),
 	// 		Usages: &armquota.UsagesObject{
-	// 			UsagesType: to.Ptr(armquota.UsagesTypesIndividual),
 	// 			Value: to.Ptr[int32](10),
 	// 		},
 	// 	},
--- specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples-go/putNetworkOneSkuQuotaRequest.go
+++ specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples-go/putNetworkOneSkuQuotaRequest.go
@@ -9,33 +9,29 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/quota/armquota"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples/putNetworkOneSkuQuotaRequest.json
-func ExampleClient_BeginCreateOrUpdate() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/quota/resource-manager/Microsoft.Quota/preview/2021-03-15-preview/examples/putNetworkOneSkuQuotaRequest.json
+func ExampleClient_BeginCreateOrUpdate_quotasPutRequestForNetwork() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armquota.NewClient(cred, nil)
+	clientFactory, err := armquota.NewClientFactory(cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx,
-		"MinPublicIpInterNetworkPrefixLength",
-		"subscriptions/D7EC67B3-7657-4966-BFFC-41EFD36BAAB3/providers/Microsoft.Network/locations/eastus",
-		armquota.CurrentQuotaLimitBase{
-			Properties: &armquota.Properties{
-				Name: &armquota.ResourceName{
-					Value: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
-				},
-				Limit: &armquota.LimitObject{
-					LimitObjectType: to.Ptr(armquota.LimitTypeLimitValue),
-					Value:           to.Ptr[int32](10),
-				},
-				ResourceType: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
+	poller, err := clientFactory.NewClient().BeginCreateOrUpdate(ctx, "MinPublicIpInterNetworkPrefixLength", "subscriptions/D7EC67B3-7657-4966-BFFC-41EFD36BAAB3/providers/Microsoft.Network/locations/eastus", armquota.CurrentQuotaLimitBase{
+		Properties: &armquota.Properties{
+			Name: &armquota.ResourceName{
+				Value: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
 			},
+			Limit: &armquota.LimitObject{
+				LimitObjectType: to.Ptr(armquota.LimitTypeLimitValue),
+				Value:           to.Ptr[int32](10),
+			},
+			ResourceType: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
 		},
-		nil)
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -43,6 +39,22 @@ func ExampleClient_BeginCreateOrUpdate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.CurrentQuotaLimitBase = armquota.CurrentQuotaLimitBase{
+	// 	Name: to.Ptr("2B5C8515-37D8-4B6A-879B-CD641A2CF605"),
+	// 	Type: to.Ptr("Microsoft.Quota/quotas"),
+	// 	ID: to.Ptr("/subscriptions/D7EC67B3-7657-4966-BFFC-41EFD36BAAB3/providers/Microsoft.Network/locations/eastus/providers/Microsoft.Quota/quotaRequests/2B5C8515-37D8-4B6A-879B-CD641A2CF605"),
+	// 	Properties: &armquota.Properties{
+	// 		Name: &armquota.ResourceName{
+	// 			Value: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
+	// 		},
+	// 		Limit: &armquota.LimitObject{
+	// 			LimitObjectType: to.Ptr(armquota.LimitTypeLimitValue),
+	// 			Value: to.Ptr[int32](10),
+	// 		},
+	// 		ResourceType: to.Ptr("MinPublicIpInterNetworkPrefixLength"),
+	// 	},
+	// }
 }

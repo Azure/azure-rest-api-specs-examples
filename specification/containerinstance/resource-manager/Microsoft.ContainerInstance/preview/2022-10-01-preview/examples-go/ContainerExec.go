38386f7034d6e646e042fa6e33c0ff4e842b6f61
azure-sdk <azuresdk@microsoft.com>
2023-03-31 00:50:17 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerinstance/armcontainerinstance/v2.2.0-beta.2

--- specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerExec.go
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerExec.go
@@ -9,18 +9,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerinstance/armcontainerinstance/v2"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerExec.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/71121282e39bccae590462648e77bca283df6d2b/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerExec.json
 func ExampleContainersClient_ExecuteCommand() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armcontainerinstance.NewContainersClient("subid", cred, nil)
+	clientFactory, err := armcontainerinstance.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.ExecuteCommand(ctx, "demo", "demo1", "container1", armcontainerinstance.ContainerExecRequest{
+	res, err := clientFactory.NewContainersClient().ExecuteCommand(ctx, "demo", "demo1", "container1", armcontainerinstance.ContainerExecRequest{
 		Command: to.Ptr("/bin/bash"),
 		TerminalSize: &armcontainerinstance.ContainerExecRequestTerminalSize{
 			Cols: to.Ptr[int32](12),
@@ -30,6 +30,11 @@ func ExampleContainersClient_ExecuteCommand() {
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ContainerExecResponse = armcontainerinstance.ContainerExecResponse{
+	// 	Password: to.Ptr("password"),
+	// 	WebSocketURI: to.Ptr("wss://web-socket-uri"),
+	// }
 }
--- specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerGroupsStop.go
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples-go/ContainerGroupsStop.go
@@ -8,18 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerinstance/armcontainerinstance/v2"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerGroupsStop.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/71121282e39bccae590462648e77bca283df6d2b/specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2022-10-01-preview/examples/ContainerGroupsStop.json
 func ExampleContainerGroupsClient_Stop() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armcontainerinstance.NewContainerGroupsClient("subid", cred, nil)
+	clientFactory, err := armcontainerinstance.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = client.Stop(ctx, "demo", "demo1", nil)
+	_, err = clientFactory.NewContainerGroupsClient().Stop(ctx, "demo", "demo1", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

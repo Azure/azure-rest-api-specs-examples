9d6718535df53b4355c49a0650b242451c0f2126
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-31 08:06:43 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/v1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-go/ConnectedClustersListClusterCredentialResultCSPAAD.go
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-go/ConnectedClustersListClusterCredentialResultCSPAAD.go
@@ -9,8 +9,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridkubernetes/armhybridkubernetes"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/ConnectedClustersListClusterCredentialResultHPAAD.json
-func ExampleConnectedClusterClient_ListClusterUserCredential_listClusterUserCredentialCspExample() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/ConnectedClustersListClusterCredentialResultCSPAAD.json
+func ExampleConnectedClusterClient_ListClusterUserCredential_listClusterUserCredentialExample() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -22,7 +22,7 @@ func ExampleConnectedClusterClient_ListClusterUserCredential_listClusterUserCred
 	}
 	res, err := clientFactory.NewConnectedClusterClient().ListClusterUserCredential(ctx, "k8sc-rg", "testCluster", armhybridkubernetes.ListClusterUserCredentialProperties{
 		AuthenticationMethod: to.Ptr(armhybridkubernetes.AuthenticationMethodAAD),
-		ClientProxy:          to.Ptr(false),
+		ClientProxy:          to.Ptr(true),
 	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
@@ -31,6 +31,12 @@ func ExampleConnectedClusterClient_ListClusterUserCredential_listClusterUserCred
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res.CredentialResults = armhybridkubernetes.CredentialResults{
+	// 	HybridConnectionConfig: &armhybridkubernetes.HybridConnectionConfig{
+	// 		ExpirationTime: to.Ptr[int64](1631196183),
+	// 		HybridConnectionName: to.Ptr("microsoft.kubernetes/connectedclusters/229dc73f7b07196c79a93d4362d9c7fc4ed34df3e95290d27c56cec2dbb82865/1631185383340987904"),
+	// 		Relay: to.Ptr("azgnrelay-ph0-l1"),
+	// 		Token: to.Ptr("SharedAccessSignature 123456789034675890pbduwegiavifkuw647o02423bbhfouseb"),
+	// 	},
 	// 	Kubeconfigs: []*armhybridkubernetes.CredentialResult{
 	// 		{
 	// 			Name: to.Ptr("credentialName1"),
--- specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-go/GetClustersByResourceGroupExample.go
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples-go/GetClustersByResourceGroupExample.go
@@ -8,27 +8,86 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridkubernetes/armhybridkubernetes"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/GetClustersByResourceGroupExample.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2021-10-01/examples/GetClustersByResourceGroupExample.json
 func ExampleConnectedClusterClient_NewListByResourceGroupPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
+	clientFactory, err := armhybridkubernetes.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListByResourceGroupPager("k8sc-rg",
-		nil)
+	pager := clientFactory.NewConnectedClusterClient().NewListByResourceGroupPager("k8sc-rg", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.ConnectedClusterList = armhybridkubernetes.ConnectedClusterList{
+		// 	Value: []*armhybridkubernetes.ConnectedCluster{
+		// 		{
+		// 			Name: to.Ptr("connectedCluster1"),
+		// 			Type: to.Ptr("Microsoft.Kubernetes/connectedClusters"),
+		// 			ID: to.Ptr("/subscriptions/1bfbb5d0-917e-4346-9026-1d3b344417f5/resourceGroups/akkeshar/providers/Microsoft.Kubernetes/connectedClusters/connectedCluster1"),
+		// 			Location: to.Ptr("East US"),
+		// 			Tags: map[string]*string{
+		// 			},
+		// 			Identity: &armhybridkubernetes.ConnectedClusterIdentity{
+		// 				Type: to.Ptr(armhybridkubernetes.ResourceIdentityTypeSystemAssigned),
+		// 				PrincipalID: to.Ptr("d8cd1fd9-154f-4da7-b348-595f283c13a3"),
+		// 				TenantID: to.Ptr("72f988bf-86f1-41af-91ab-2d7cd011db47"),
+		// 			},
+		// 			Properties: &armhybridkubernetes.ConnectedClusterProperties{
+		// 				AgentPublicKeyCertificate: to.Ptr("MIICYzCCAcygAwIBAgIBADANBgkqhkiG9w0BAQUFADAuMQswCQYDVQQGEwJVUzEMMAoGA1UEChMDSUJNMREwDwYDVQQLEwhMb2NhbCBDQTAeFw05OTEyMjIwNTAwMDBaFw0wMDEyMjMwNDU5NTlaMC4xCzAJBgNVBAYTAlVTMQwwCgYDVQQKEwNJQk0xETAPBgNVBAsTCExvY2FsIENBMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD2bZEo7xGaX2/0GHkrNFZvlxBou9v1Jmt/PDiTMPve8r9FeJAQ0QdvFST/0JPQYD20rH0bimdDLgNdNynmyRoS2S/IInfpmf69iyc2G0TPyRvmHIiOZbdCd+YBHQi1adkj17NDcWj6S14tVurFX73zx0sNoMS79q3tuXKrDsxeuwIDAQABo4GQMIGNMEsGCVUdDwGG+EIBDQQ+EzxHZW5lcmF0ZWQgYnkgdGhlIFNlY3VyZVdheSBTZWN1cml0eSBTZXJ2ZXIgZm9yIE9TLzM5MCAoUkFDRikwDgYDVR0PAQH/BAQDAgAGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFJ3+ocRyCTJw067dLSwr/nalx6YMMA0GCSqGSIb3DQEBBQUAA4GBAMaQzt+zaj1GU77yzlr8iiMBXgdQrwsZZWJo5exnAucJAEYQZmOfyLiM D6oYq+ZnfvM0n8G/Y79q8nhwvuxpYOnRSAXFp6xSkrIOeZtJMY1h00LKp/JX3Ng1svZ2agE126JHsQ0bhzN5TKsYfbwfTwfjdWAGy6Vf1nYi/rO+ryMO"),
+		// 				AgentVersion: to.Ptr("0.1.0"),
+		// 				KubernetesVersion: to.Ptr("1.17.0"),
+		// 				ProvisioningState: to.Ptr(armhybridkubernetes.ProvisioningStateSucceeded),
+		// 				TotalNodeCount: to.Ptr[int32](2),
+		// 			},
+		// 			SystemData: &armhybridkubernetes.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-12-17T07:06:33.9173186Z"); return t}()),
+		// 				CreatedBy: to.Ptr("sikasire@microsoft.com"),
+		// 				CreatedByType: to.Ptr(armhybridkubernetes.CreatedByTypeUser),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-12-17T07:06:33.9173186Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("sikasire@microsoft.com"),
+		// 				LastModifiedByType: to.Ptr(armhybridkubernetes.LastModifiedByTypeUser),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("connectedCluster1"),
+		// 			Type: to.Ptr("Microsoft.Kubernetes/connectedClusters"),
+		// 			ID: to.Ptr("/subscriptions/1bfbb5d0-917e-4346-9026-1d3b344417f5/resourceGroups/akkeshar/providers/Microsoft.Kubernetes/connectedClusters/connectedCluster1"),
+		// 			Location: to.Ptr("East US"),
+		// 			Tags: map[string]*string{
+		// 			},
+		// 			Identity: &armhybridkubernetes.ConnectedClusterIdentity{
+		// 				Type: to.Ptr(armhybridkubernetes.ResourceIdentityTypeSystemAssigned),
+		// 				PrincipalID: to.Ptr("d8cd1fd9-154f-4da7-b348-595f283c13a3"),
+		// 				TenantID: to.Ptr("72f988bf-86f1-41af-91ab-2d7cd011db47"),
+		// 			},
+		// 			Properties: &armhybridkubernetes.ConnectedClusterProperties{
+		// 				AgentPublicKeyCertificate: to.Ptr("XIICYzCCAcygAwIBAgIBADANBgkqhkiG9w0BAQUFADAuMQswCQYDVQQGEwJVUzEMMAoGA1UEChMDSUJNMREwDwYDVQQLEwhMb2NhbCBDQTAeFw05OTEyMjIwNTAwMDBaFw0wMDEyMjMwNDU5NTlaMC4xCzAJBgNVBAYTAlVTMQwwCgYDVQQKEwNJQk0xETAPBgNVBAsTCExvY2FsIENBMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD2bZEo7xGaX2/0GHkrNFZvlxBou9v1Jmt/PDiTMPve8r9FeJAQ0QdvFST/0JPQYD20rH0bimdDLgNdNynmyRoS2S/IInfpmf69iyc2G0TPyRvmHIiOZbdCd+YBHQi1adkj17NDcWj6S14tVurFX73zx0sNoMS79q3tuXKrDsxeuwIDAQABo4GQMIGNMEsGCVUdDwGG+EIBDQQ+EzxHZW5lcmF0ZWQgYnkgdGhlIFNlY3VyZVdheSBTZWN1cml0eSBTZXJ2ZXIgZm9yIE9TLzM5MCAoUkFDRikwDgYDVR0PAQH/BAQDAgAGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFJ3+ocRyCTJw067dLSwr/nalx6YMMA0GCSqGSIb3DQEBBQUAA4GBAMaQzt+zaj1GU77yzlr8iiMBXgdQrwsZZWJo5exnAucJAEYQZmOfyLiM D6oYq+ZnfvM0n8G/Y79q8nhwvuxpYOnRSAXFp6xSkrIOeZtJMY1h00LKp/JX3Ng1svZ2agE126JHsQ0bhzN5TKsYfbwfTwfjdWAGy6Vf1nYi/rO+ryMO"),
+		// 				AgentVersion: to.Ptr("0.1.0"),
+		// 				KubernetesVersion: to.Ptr("1.1"),
+		// 				ProvisioningState: to.Ptr(armhybridkubernetes.ProvisioningStateSucceeded),
+		// 				TotalNodeCount: to.Ptr[int32](2),
+		// 			},
+		// 			SystemData: &armhybridkubernetes.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-12-17T07:06:33.9173186Z"); return t}()),
+		// 				CreatedBy: to.Ptr("sikasire@microsoft.com"),
+		// 				CreatedByType: to.Ptr(armhybridkubernetes.CreatedByTypeUser),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-12-17T07:06:33.9173186Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("sikasire@microsoft.com"),
+		// 				LastModifiedByType: to.Ptr(armhybridkubernetes.LastModifiedByTypeUser),
+		// 			},
+		// 	}},
+		// }
 	}
 }

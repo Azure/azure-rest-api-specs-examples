82b585034b91bf8957e0ed2b35ffb8da1a81cf27
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-03-31 08:11:09 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/operationsmanagement/armoperationsmanagement/v0.7.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples-go/ManagementAssociationCreate.go
+++ specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples-go/ManagementAssociationCreate.go
@@ -9,33 +9,36 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/operationsmanagement/armoperationsmanagement"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples/ManagementAssociationCreate.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples/ManagementAssociationCreate.json
 func ExampleManagementAssociationsClient_CreateOrUpdate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armoperationsmanagement.NewManagementAssociationsClient("subid", cred, nil)
+	clientFactory, err := armoperationsmanagement.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.CreateOrUpdate(ctx,
-		"rg1",
-		"providerName",
-		"resourceType",
-		"resourceName",
-		"managementAssociation1",
-		armoperationsmanagement.ManagementAssociation{
-			Location: to.Ptr("East US"),
-			Properties: &armoperationsmanagement.ManagementAssociationProperties{
-				ApplicationID: to.Ptr("/subscriptions/sub1/resourcegroups/rg1/providers/Microsoft.Appliance/Appliances/appliance1"),
-			},
+	res, err := clientFactory.NewManagementAssociationsClient().CreateOrUpdate(ctx, "rg1", "providerName", "resourceType", "resourceName", "managementAssociation1", armoperationsmanagement.ManagementAssociation{
+		Location: to.Ptr("East US"),
+		Properties: &armoperationsmanagement.ManagementAssociationProperties{
+			ApplicationID: to.Ptr("/subscriptions/sub1/resourcegroups/rg1/providers/Microsoft.Appliance/Appliances/appliance1"),
 		},
-		nil)
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ManagementAssociation = armoperationsmanagement.ManagementAssociation{
+	// 	Name: to.Ptr("managementAssociation1"),
+	// 	Type: to.Ptr("Microsoft.OperationsManagement/ManagementAssociations"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourcegroups/rg1/providers/Microsoft.OperationalInsights/workspaces/ws1/Microsoft.OperationsManagement/ManagementAssociations/managementAssociation1"),
+	// 	Location: to.Ptr("East US"),
+	// 	Properties: &armoperationsmanagement.ManagementAssociationProperties{
+	// 		ApplicationID: to.Ptr("/subscriptions/sub1/resourcegroups/rg1/providers/Microsoft.Appliance/Appliances/appliance1"),
+	// 	},
+	// }
 }
--- specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples-go/SolutionGet.go
+++ specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples-go/SolutionGet.go
@@ -8,24 +8,44 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/operationsmanagement/armoperationsmanagement"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples/SolutionGet.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples/SolutionGet.json
 func ExampleSolutionsClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armoperationsmanagement.NewSolutionsClient("subid", cred, nil)
+	clientFactory, err := armoperationsmanagement.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Get(ctx,
-		"rg1",
-		"solution1",
-		nil)
+	res, err := clientFactory.NewSolutionsClient().Get(ctx, "rg1", "solution1", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Solution = armoperationsmanagement.Solution{
+	// 	Name: to.Ptr("solution1"),
+	// 	Type: to.Ptr("Microsoft.OperationsManagement/solutions"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.OperationsManagement/solutions/solution1"),
+	// 	Location: to.Ptr("East US"),
+	// 	Plan: &armoperationsmanagement.SolutionPlan{
+	// 		Name: to.Ptr("name1"),
+	// 		Product: to.Ptr("product1"),
+	// 		PromotionCode: to.Ptr("promocode1"),
+	// 		Publisher: to.Ptr("publisher1"),
+	// 	},
+	// 	Properties: &armoperationsmanagement.SolutionProperties{
+	// 		ContainedResources: []*string{
+	// 			to.Ptr("/subscriptions/sub2/resourceGroups/rg2/providers/provider1/resources/resource1"),
+	// 			to.Ptr("/subscriptions/sub2/resourceGroups/rg2/providers/provider2/resources/resource2")},
+	// 			ProvisioningState: to.Ptr("Succeeded"),
+	// 			ReferencedResources: []*string{
+	// 				to.Ptr("/subscriptions/sub2/resourceGroups/rg2/providers/provider1/resources/resource2"),
+	// 				to.Ptr("/subscriptions/sub2/resourceGroups/rg2/providers/provider2/resources/resource3")},
+	// 				WorkspaceResourceID: to.Ptr("/subscriptions/sub2/resourceGroups/rg2/providers/Microsoft.OperationalInsights/workspaces/ws1"),
+	// 			},
+	// 		}
 }

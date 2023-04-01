0b817e0a0c79bfaff4df8ae24c7b533e50b6e51e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-01 08:04:53 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/solutions/armmanagedapplications/v1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/createOrUpdateApplicationDefinition.go
+++ specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/createOrUpdateApplicationDefinition.go
@@ -9,34 +9,30 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/solutions/armmanagedapplications"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/createOrUpdateApplicationDefinition.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/createOrUpdateApplicationDefinition.json
 func ExampleApplicationDefinitionsClient_BeginCreateOrUpdate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmanagedapplications.NewApplicationDefinitionsClient("subid", cred, nil)
+	clientFactory, err := armmanagedapplications.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx,
-		"rg",
-		"myManagedApplicationDef",
-		armmanagedapplications.ApplicationDefinition{
-			Properties: &armmanagedapplications.ApplicationDefinitionProperties{
-				Description: to.Ptr("myManagedApplicationDef description"),
-				Authorizations: []*armmanagedapplications.ApplicationAuthorization{
-					{
-						PrincipalID:      to.Ptr("validprincipalguid"),
-						RoleDefinitionID: to.Ptr("validroleguid"),
-					}},
-				DisplayName:    to.Ptr("myManagedApplicationDef"),
-				LockLevel:      to.Ptr(armmanagedapplications.ApplicationLockLevelNone),
-				PackageFileURI: to.Ptr("https://path/to/packagezipfile"),
-			},
+	poller, err := clientFactory.NewApplicationDefinitionsClient().BeginCreateOrUpdate(ctx, "rg", "myManagedApplicationDef", armmanagedapplications.ApplicationDefinition{
+		Properties: &armmanagedapplications.ApplicationDefinitionProperties{
+			Description: to.Ptr("myManagedApplicationDef description"),
+			Authorizations: []*armmanagedapplications.ApplicationAuthorization{
+				{
+					PrincipalID:      to.Ptr("validprincipalguid"),
+					RoleDefinitionID: to.Ptr("validroleguid"),
+				}},
+			DisplayName:    to.Ptr("myManagedApplicationDef"),
+			LockLevel:      to.Ptr(armmanagedapplications.ApplicationLockLevelNone),
+			PackageFileURI: to.Ptr("https://path/to/packagezipfile"),
 		},
-		nil)
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -44,6 +40,34 @@ func ExampleApplicationDefinitionsClient_BeginCreateOrUpdate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ApplicationDefinition = armmanagedapplications.ApplicationDefinition{
+	// 	Name: to.Ptr("myManagedApplicationDef"),
+	// 	Type: to.Ptr("Microsoft.Solutions/applicationDefinitions"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.Solutions/applicationDefinitions/myManagedApplicationDef"),
+	// 	Properties: &armmanagedapplications.ApplicationDefinitionProperties{
+	// 		Description: to.Ptr("myManagedApplicationDef description"),
+	// 		Artifacts: []*armmanagedapplications.ApplicationDefinitionArtifact{
+	// 			{
+	// 				Name: to.Ptr(armmanagedapplications.ApplicationDefinitionArtifactNameCreateUIDefinition),
+	// 				Type: to.Ptr(armmanagedapplications.ApplicationArtifactTypeCustom),
+	// 				URI: to.Ptr("https://path/to/managedApplicationCreateUiDefinition.json"),
+	// 			},
+	// 			{
+	// 				Name: to.Ptr(armmanagedapplications.ApplicationDefinitionArtifactNameApplicationResourceTemplate),
+	// 				Type: to.Ptr(armmanagedapplications.ApplicationArtifactTypeTemplate),
+	// 				URI: to.Ptr("https://path/to/mainTemplate.json"),
+	// 		}},
+	// 		Authorizations: []*armmanagedapplications.ApplicationAuthorization{
+	// 			{
+	// 				PrincipalID: to.Ptr("validprincipalguid"),
+	// 				RoleDefinitionID: to.Ptr("validroleguid"),
+	// 		}},
+	// 		DisplayName: to.Ptr("myManagedApplicationDef"),
+	// 		LockLevel: to.Ptr(armmanagedapplications.ApplicationLockLevelNone),
+	// 		PackageFileURI: to.Ptr("https://path/to/packagezipfile"),
+	// 	},
+	// }
 }
--- specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/deleteApplication.go
+++ specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/deleteApplication.go
@@ -8,21 +8,18 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/solutions/armmanagedapplications"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/deleteApplication.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/deleteApplication.json
 func ExampleApplicationsClient_BeginDelete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmanagedapplications.NewApplicationsClient("subid", cred, nil)
+	clientFactory, err := armmanagedapplications.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginDelete(ctx,
-		"rg",
-		"myManagedApplication",
-		nil)
+	poller, err := clientFactory.NewApplicationsClient().BeginDelete(ctx, "rg", "myManagedApplication", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

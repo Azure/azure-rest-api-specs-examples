76a7f616ba0aa818e2ac6f0ce1f39c656e24bde1
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-04 08:03:49 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/storageimportexport/armstorageimportexport/v0.6.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples-go/CreateExportJob.go
+++ specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples-go/CreateExportJob.go
@@ -9,56 +9,114 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storageimportexport/armstorageimportexport"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples/CreateExportJob.json
-func ExampleJobsClient_Create() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples/CreateExportJob.json
+func ExampleJobsClient_Create_createExportJob() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armstorageimportexport.NewJobsClient("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-		nil, cred, nil)
+	clientFactory, err := armstorageimportexport.NewClientFactory("<subscription-id>", nil, cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Create(ctx,
-		"myExportJob",
-		"myResourceGroup",
-		armstorageimportexport.PutJobParameters{
-			Location: to.Ptr("West US"),
-			Properties: &armstorageimportexport.JobDetails{
-				BackupDriveManifest: to.Ptr(true),
-				DiagnosticsPath:     to.Ptr("waimportexport"),
-				Export: &armstorageimportexport.Export{
-					BlobList: &armstorageimportexport.ExportBlobList{
-						BlobPathPrefix: []*string{
-							to.Ptr("/")},
-					},
+	res, err := clientFactory.NewJobsClient().Create(ctx, "myExportJob", "myResourceGroup", armstorageimportexport.PutJobParameters{
+		Location: to.Ptr("West US"),
+		Properties: &armstorageimportexport.JobDetails{
+			BackupDriveManifest: to.Ptr(true),
+			DiagnosticsPath:     to.Ptr("waimportexport"),
+			Export: &armstorageimportexport.Export{
+				BlobList: &armstorageimportexport.ExportBlobList{
+					BlobPathPrefix: []*string{
+						to.Ptr("/")},
 				},
-				JobType:  to.Ptr("Export"),
-				LogLevel: to.Ptr("Verbose"),
-				ReturnAddress: &armstorageimportexport.ReturnAddress{
-					City:            to.Ptr("Redmond"),
-					CountryOrRegion: to.Ptr("USA"),
-					Email:           to.Ptr("Test@contoso.com"),
-					Phone:           to.Ptr("4250000000"),
-					PostalCode:      to.Ptr("98007"),
-					RecipientName:   to.Ptr("Test"),
-					StateOrProvince: to.Ptr("wa"),
-					StreetAddress1:  to.Ptr("Street1"),
-					StreetAddress2:  to.Ptr("street2"),
-				},
-				ReturnShipping: &armstorageimportexport.ReturnShipping{
-					CarrierAccountNumber: to.Ptr("989ffff"),
-					CarrierName:          to.Ptr("FedEx"),
-				},
-				StorageAccountID: to.Ptr("/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.ClassicStorage/storageAccounts/test"),
 			},
+			JobType:  to.Ptr("Export"),
+			LogLevel: to.Ptr("Verbose"),
+			ReturnAddress: &armstorageimportexport.ReturnAddress{
+				City:            to.Ptr("Redmond"),
+				CountryOrRegion: to.Ptr("USA"),
+				Email:           to.Ptr("Test@contoso.com"),
+				Phone:           to.Ptr("4250000000"),
+				PostalCode:      to.Ptr("98007"),
+				RecipientName:   to.Ptr("Test"),
+				StateOrProvince: to.Ptr("wa"),
+				StreetAddress1:  to.Ptr("Street1"),
+				StreetAddress2:  to.Ptr("street2"),
+			},
+			ReturnShipping: &armstorageimportexport.ReturnShipping{
+				CarrierAccountNumber: to.Ptr("989ffff"),
+				CarrierName:          to.Ptr("FedEx"),
+			},
+			StorageAccountID: to.Ptr("/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.ClassicStorage/storageAccounts/test"),
 		},
-		&armstorageimportexport.JobsClientCreateOptions{ClientTenantID: nil})
+	}, &armstorageimportexport.JobsClientCreateOptions{ClientTenantID: nil})
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.JobResponse = armstorageimportexport.JobResponse{
+	// 	Name: to.Ptr("myExportJob"),
+	// 	Type: to.Ptr("Microsoft.ImportExport/jobs"),
+	// 	ID: to.Ptr("/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.ImportExport/jobs/myExportJob"),
+	// 	Identity: &armstorageimportexport.IdentityDetails{
+	// 		Type: to.Ptr(armstorageimportexport.IdentityTypeNone),
+	// 	},
+	// 	Location: to.Ptr("West US"),
+	// 	Properties: &armstorageimportexport.JobDetails{
+	// 		BackupDriveManifest: to.Ptr(true),
+	// 		CancelRequested: to.Ptr(false),
+	// 		DiagnosticsPath: to.Ptr("waimportexport"),
+	// 		EncryptionKey: &armstorageimportexport.EncryptionKeyDetails{
+	// 			KekType: to.Ptr(armstorageimportexport.EncryptionKekTypeMicrosoftManaged),
+	// 		},
+	// 		Export: &armstorageimportexport.Export{
+	// 			BlobList: &armstorageimportexport.ExportBlobList{
+	// 				BlobPathPrefix: []*string{
+	// 					to.Ptr("/")},
+	// 				},
+	// 			},
+	// 			JobType: to.Ptr("Export"),
+	// 			LogLevel: to.Ptr("Verbose"),
+	// 			ProvisioningState: to.Ptr("Succeeded"),
+	// 			ReturnAddress: &armstorageimportexport.ReturnAddress{
+	// 				City: to.Ptr("Redmond"),
+	// 				CountryOrRegion: to.Ptr("USA"),
+	// 				Email: to.Ptr("Test@contoso.com"),
+	// 				Phone: to.Ptr("4250000000"),
+	// 				PostalCode: to.Ptr("98007"),
+	// 				RecipientName: to.Ptr("Test"),
+	// 				StateOrProvince: to.Ptr("wa"),
+	// 				StreetAddress1: to.Ptr("Street1"),
+	// 				StreetAddress2: to.Ptr("street2"),
+	// 			},
+	// 			ReturnShipping: &armstorageimportexport.ReturnShipping{
+	// 				CarrierAccountNumber: to.Ptr("989ffff"),
+	// 				CarrierName: to.Ptr("FedEx"),
+	// 			},
+	// 			ShippingInformation: &armstorageimportexport.ShippingInformation{
+	// 				AdditionalInformation: to.Ptr(""),
+	// 				City: to.Ptr("Santa Clara"),
+	// 				CountryOrRegion: to.Ptr("USA"),
+	// 				Phone: to.Ptr("408 0000 0000"),
+	// 				PostalCode: to.Ptr("95050"),
+	// 				RecipientName: to.Ptr("Microsoft Azure Import/Export Service"),
+	// 				StateOrProvince: to.Ptr("CA"),
+	// 				StreetAddress1: to.Ptr("2045 Lafayette Street"),
+	// 				StreetAddress2: to.Ptr(""),
+	// 			},
+	// 			State: to.Ptr("Creating"),
+	// 			StorageAccountID: to.Ptr("/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.ClassicStorage/storageAccounts/test"),
+	// 		},
+	// 		SystemData: &armstorageimportexport.SystemData{
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-02-03T01:01:01.1075056Z"); return t}()),
+	// 			CreatedBy: to.Ptr("string"),
+	// 			CreatedByType: to.Ptr(armstorageimportexport.CreatedByTypeUser),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-02-04T02:03:01.1974346Z"); return t}()),
+	// 			LastModifiedBy: to.Ptr("string"),
+	// 			LastModifiedByType: to.Ptr(armstorageimportexport.CreatedByTypeUser),
+	// 		},
+	// 	}
 }

b333805dbd3d4ac6e17ba291f466e36ff60d9f63
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-10 14:59:21 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/maintenance/armmaintenance/v1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-go/MaintenanceConfigurations_UpdateForResource.go
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-go/MaintenanceConfigurations_UpdateForResource.go
@@ -9,39 +9,53 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/maintenance/armmaintenance"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples/MaintenanceConfigurations_UpdateForResource.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples/MaintenanceConfigurations_UpdateForResource.json
 func ExampleConfigurationsClient_Update() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmaintenance.NewConfigurationsClient("5b4b650e-28b9-4790-b3ab-ddbd88d727c4", cred, nil)
+	clientFactory, err := armmaintenance.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.Update(ctx,
-		"examplerg",
-		"configuration1",
-		armmaintenance.Configuration{
-			Location: to.Ptr("westus2"),
-			Properties: &armmaintenance.ConfigurationProperties{
-				MaintenanceScope: to.Ptr(armmaintenance.MaintenanceScopeHost),
-				MaintenanceWindow: &armmaintenance.Window{
-					Duration:           to.Ptr("05:00"),
-					ExpirationDateTime: to.Ptr("9999-12-31 00:00"),
-					RecurEvery:         to.Ptr("Month Third Sunday"),
-					StartDateTime:      to.Ptr("2020-04-30 08:00"),
-					TimeZone:           to.Ptr("Pacific Standard Time"),
-				},
-				Namespace:  to.Ptr("Microsoft.Maintenance"),
-				Visibility: to.Ptr(armmaintenance.VisibilityCustom),
+	res, err := clientFactory.NewConfigurationsClient().Update(ctx, "examplerg", "configuration1", armmaintenance.Configuration{
+		Location: to.Ptr("westus2"),
+		Properties: &armmaintenance.ConfigurationProperties{
+			MaintenanceScope: to.Ptr(armmaintenance.MaintenanceScopeHost),
+			MaintenanceWindow: &armmaintenance.Window{
+				Duration:           to.Ptr("05:00"),
+				ExpirationDateTime: to.Ptr("9999-12-31 00:00"),
+				RecurEvery:         to.Ptr("Month Third Sunday"),
+				StartDateTime:      to.Ptr("2020-04-30 08:00"),
+				TimeZone:           to.Ptr("Pacific Standard Time"),
 			},
+			Namespace:  to.Ptr("Microsoft.Maintenance"),
+			Visibility: to.Ptr(armmaintenance.VisibilityCustom),
 		},
-		nil)
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Configuration = armmaintenance.Configuration{
+	// 	Name: to.Ptr("configuration1"),
+	// 	ID: to.Ptr("/subscriptions/5b4b650e-28b9-4790-b3ab-ddbd88d727c4/resourcegroups/examplerg/providers/Microsoft.Maintenance/maintenanceConfigurations/configuration1"),
+	// 	Location: to.Ptr("westus2"),
+	// 	Properties: &armmaintenance.ConfigurationProperties{
+	// 		MaintenanceScope: to.Ptr(armmaintenance.MaintenanceScopeHost),
+	// 		MaintenanceWindow: &armmaintenance.Window{
+	// 			Duration: to.Ptr("05:00"),
+	// 			ExpirationDateTime: to.Ptr("9999-12-31 00:00"),
+	// 			RecurEvery: to.Ptr("Month Third Sunday"),
+	// 			StartDateTime: to.Ptr("2020-04-30 08:00"),
+	// 			TimeZone: to.Ptr("Pacific Standard Time"),
+	// 		},
+	// 		Namespace: to.Ptr("Microsoft.Maintenance"),
+	// 		Visibility: to.Ptr(armmaintenance.VisibilityCustom),
+	// 	},
+	// }
 }
--- specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-go/Operations_List.go
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples-go/Operations_List.go
@@ -8,26 +8,31 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/maintenance/armmaintenance"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples/Operations_List.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2021-05-01/examples/Operations_List.json
 func ExampleOperationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armmaintenance.NewOperationsClient(cred, nil)
+	clientFactory, err := armmaintenance.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListPager(nil)
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.OperationsListResult = armmaintenance.OperationsListResult{
+		// 	Value: []*armmaintenance.Operation{
+		// 	},
+		// }
 	}
 }

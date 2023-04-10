743192a3c968600c61146593e792b385eb91de4c
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-10 14:59:13 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/iotcentral/armiotcentral/v1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples-go/Apps_CheckSubdomainAvailability.go
+++ specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples-go/Apps_CheckSubdomainAvailability.go
@@ -9,26 +9,28 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/iotcentral/armiotcentral"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples/Apps_CheckSubdomainAvailability.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples/Apps_CheckSubdomainAvailability.json
 func ExampleAppsClient_CheckSubdomainAvailability() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armiotcentral.NewAppsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	clientFactory, err := armiotcentral.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := client.CheckSubdomainAvailability(ctx,
-		armiotcentral.OperationInputs{
-			Name: to.Ptr("myiotcentralapp"),
-			Type: to.Ptr("IoTApps"),
-		},
-		nil)
+	res, err := clientFactory.NewAppsClient().CheckSubdomainAvailability(ctx, armiotcentral.OperationInputs{
+		Name: to.Ptr("myiotcentralapp"),
+		Type: to.Ptr("IoTApps"),
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AppAvailabilityInfo = armiotcentral.AppAvailabilityInfo{
+	// 	NameAvailable: to.Ptr(true),
+	// }
 }
--- specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples-go/Apps_CreateOrUpdate.go
+++ specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples-go/Apps_CreateOrUpdate.go
@@ -9,35 +9,31 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/iotcentral/armiotcentral"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples/Apps_CreateOrUpdate.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/iotcentral/resource-manager/Microsoft.IoTCentral/stable/2021-06-01/examples/Apps_CreateOrUpdate.json
 func ExampleAppsClient_BeginCreateOrUpdate() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armiotcentral.NewAppsClient("00000000-0000-0000-0000-000000000000", cred, nil)
+	clientFactory, err := armiotcentral.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := client.BeginCreateOrUpdate(ctx,
-		"resRg",
-		"myIoTCentralApp",
-		armiotcentral.App{
-			Location: to.Ptr("westus"),
-			Identity: &armiotcentral.SystemAssignedServiceIdentity{
-				Type: to.Ptr(armiotcentral.SystemAssignedServiceIdentityTypeSystemAssigned),
-			},
-			Properties: &armiotcentral.AppProperties{
-				DisplayName: to.Ptr("My IoT Central App"),
-				Subdomain:   to.Ptr("my-iot-central-app"),
-				Template:    to.Ptr("iotc-pnp-preview@1.0.0"),
-			},
-			SKU: &armiotcentral.AppSKUInfo{
-				Name: to.Ptr(armiotcentral.AppSKUST2),
-			},
+	poller, err := clientFactory.NewAppsClient().BeginCreateOrUpdate(ctx, "resRg", "myIoTCentralApp", armiotcentral.App{
+		Location: to.Ptr("westus"),
+		Identity: &armiotcentral.SystemAssignedServiceIdentity{
+			Type: to.Ptr(armiotcentral.SystemAssignedServiceIdentityTypeSystemAssigned),
 		},
-		nil)
+		Properties: &armiotcentral.AppProperties{
+			DisplayName: to.Ptr("My IoT Central App"),
+			Subdomain:   to.Ptr("my-iot-central-app"),
+			Template:    to.Ptr("iotc-pnp-preview@1.0.0"),
+		},
+		SKU: &armiotcentral.AppSKUInfo{
+			Name: to.Ptr(armiotcentral.AppSKUST2),
+		},
+	}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -45,6 +41,31 @@ func ExampleAppsClient_BeginCreateOrUpdate() {
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
-	// TODO: use response item
+	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.App = armiotcentral.App{
+	// 	Name: to.Ptr("myIoTCentralApp"),
+	// 	Type: to.Ptr("Microsoft.IoTCentral/IoTApps"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/resRg/providers/Microsoft.IoTCentral/IoTApps/myIoTCentralApp"),
+	// 	Location: to.Ptr("westus"),
+	// 	Tags: map[string]*string{
+	// 		"key": to.Ptr("value"),
+	// 	},
+	// 	Identity: &armiotcentral.SystemAssignedServiceIdentity{
+	// 		Type: to.Ptr(armiotcentral.SystemAssignedServiceIdentityTypeSystemAssigned),
+	// 		PrincipalID: to.Ptr("8988ab15-3e7a-4586-8a1c-ed07a73a53e9"),
+	// 		TenantID: to.Ptr("f686d426-8d16-42db-81b7-ab578e110ccd"),
+	// 	},
+	// 	Properties: &armiotcentral.AppProperties{
+	// 		ApplicationID: to.Ptr("6ebd8fd3-6e34-419e-908f-9be61ec6f6d6"),
+	// 		DisplayName: to.Ptr("My IoT Central App 2"),
+	// 		State: to.Ptr(armiotcentral.AppStateCreated),
+	// 		Subdomain: to.Ptr("my-iot-central-app-2"),
+	// 		Template: to.Ptr("iotc-pnp-preview@1.0.0"),
+	// 	},
+	// 	SKU: &armiotcentral.AppSKUInfo{
+	// 		Name: to.Ptr(armiotcentral.AppSKUST2),
+	// 	},
+	// }
 }

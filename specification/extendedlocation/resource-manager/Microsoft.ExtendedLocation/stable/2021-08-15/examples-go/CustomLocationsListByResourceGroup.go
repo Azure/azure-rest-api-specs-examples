16ce434eb3e0c50c6c444c43130731cb6e09b65e
azure-sdk <azuresdk@microsoft.com>
2023-04-10 06:45:31 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/extendedlocation/armextendedlocation/v1.1.0

--- specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples-go/CustomLocationsListByResourceGroup.go
+++ specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples-go/CustomLocationsListByResourceGroup.go
@@ -8,27 +8,90 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/extendedlocation/armextendedlocation"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples/CustomLocationsListByResourceGroup.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples/CustomLocationsListByResourceGroup.json
 func ExampleCustomLocationsClient_NewListByResourceGroupPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armextendedlocation.NewCustomLocationsClient("11111111-2222-3333-4444-555555555555", cred, nil)
+	clientFactory, err := armextendedlocation.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListByResourceGroupPager("testresourcegroup",
-		nil)
+	pager := clientFactory.NewCustomLocationsClient().NewListByResourceGroupPager("testresourcegroup", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.CustomLocationListResult = armextendedlocation.CustomLocationListResult{
+		// 	Value: []*armextendedlocation.CustomLocation{
+		// 		{
+		// 			Name: to.Ptr("customLocation01"),
+		// 			Type: to.Ptr("Microsoft.ExtendedLocation/customLocations"),
+		// 			ID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testresourcegroup/providers/Microsoft.ExtendedLocation/"),
+		// 			Location: to.Ptr("West US"),
+		// 			Identity: &armextendedlocation.Identity{
+		// 				Type: to.Ptr(armextendedlocation.ResourceIdentityTypeSystemAssigned),
+		// 				PrincipalID: to.Ptr("11111111-1111-1111-1111-111111111111"),
+		// 				TenantID: to.Ptr("111111-1111-1111-1111-111111111111"),
+		// 			},
+		// 			Properties: &armextendedlocation.CustomLocationProperties{
+		// 				Authentication: &armextendedlocation.CustomLocationPropertiesAuthentication{
+		// 					Type: to.Ptr("KubeConfig"),
+		// 				},
+		// 				ClusterExtensionIDs: []*string{
+		// 					to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testresourcegroup/providers/Microsoft.ContainerService/managedClusters/cluster01/Microsoft.KubernetesConfiguration/clusterExtensions/fooExtension")},
+		// 					DisplayName: to.Ptr("customLocationLocation01"),
+		// 					HostResourceID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testresourcegroup/providers/Microsoft.ContainerService/managedClusters/cluster01"),
+		// 					Namespace: to.Ptr("namespace01"),
+		// 					ProvisioningState: to.Ptr("Succeeded"),
+		// 				},
+		// 				SystemData: &armextendedlocation.SystemData{
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-04-24T18:53:29.0928001Z"); return t}()),
+		// 					CreatedBy: to.Ptr("string"),
+		// 					CreatedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-04-24T18:53:29.0928001Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("string"),
+		// 					LastModifiedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("customLocation02"),
+		// 				Type: to.Ptr("Microsoft.ExtendedLocation/customLocations"),
+		// 				ID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testresourcegroup/providers/Microsoft.ExtendedLocation/"),
+		// 				Location: to.Ptr("West US"),
+		// 				Identity: &armextendedlocation.Identity{
+		// 					Type: to.Ptr(armextendedlocation.ResourceIdentityTypeSystemAssigned),
+		// 					PrincipalID: to.Ptr("11111111-1111-1111-1111-111111111111"),
+		// 					TenantID: to.Ptr("111111-1111-1111-1111-111111111111"),
+		// 				},
+		// 				Properties: &armextendedlocation.CustomLocationProperties{
+		// 					Authentication: &armextendedlocation.CustomLocationPropertiesAuthentication{
+		// 						Type: to.Ptr("KubeConfig"),
+		// 					},
+		// 					ClusterExtensionIDs: []*string{
+		// 						to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testresourcegroup/providers/Microsoft.ContainerService/managedClusters/cluster02/Microsoft.KubernetesConfiguration/clusterExtensions/fooExtension")},
+		// 						DisplayName: to.Ptr("customLocationLocation02"),
+		// 						HostResourceID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testresourcegroup/providers/Microsoft.ContainerService/managedClusters/cluster02"),
+		// 						Namespace: to.Ptr("namespace02"),
+		// 						ProvisioningState: to.Ptr("Succeeded"),
+		// 					},
+		// 					SystemData: &armextendedlocation.SystemData{
+		// 						CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-24T18:53:29.0928001Z"); return t}()),
+		// 						CreatedBy: to.Ptr("string"),
+		// 						CreatedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 						LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-24T18:53:29.0928001Z"); return t}()),
+		// 						LastModifiedBy: to.Ptr("string"),
+		// 						LastModifiedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 					},
+		// 			}},
+		// 		}
 	}
 }
--- specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples-go/CustomLocationsListEnabledResourceTypes.go
+++ specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples-go/CustomLocationsListEnabledResourceTypes.go
@@ -8,28 +8,86 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/extendedlocation/armextendedlocation"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples/CustomLocationsListEnabledResourceTypes.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples/CustomLocationsListEnabledResourceTypes.json
 func ExampleCustomLocationsClient_NewListEnabledResourceTypesPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armextendedlocation.NewCustomLocationsClient("11111111-2222-3333-4444-555555555555", cred, nil)
+	clientFactory, err := armextendedlocation.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListEnabledResourceTypesPager("testresourcegroup",
-		"customLocation01",
-		nil)
+	pager := clientFactory.NewCustomLocationsClient().NewListEnabledResourceTypesPager("testresourcegroup", "customLocation01", nil)
 	for pager.More() {
-		nextResult, err := pager.NextPage(ctx)
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			// TODO: use page item
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
 			_ = v
 		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.EnabledResourceTypesListResult = armextendedlocation.EnabledResourceTypesListResult{
+		// 	Value: []*armextendedlocation.EnabledResourceType{
+		// 		{
+		// 			Name: to.Ptr("d016ecf26dae90594806aca3c1a6326c668357037f68103587edf2e657824737"),
+		// 			Type: to.Ptr("Microsoft.ExtendedLocation/customLocations/enabledResourceTypes"),
+		// 			ID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testrg/providers/Microsoft.ExtendedLocation/customLocations/customLocation01/enabledResourceTypes/d016ecf26dae90594806aca3c1a6326c668357037f68103587edf2e657824737"),
+		// 			Properties: &armextendedlocation.EnabledResourceTypeProperties{
+		// 				ClusterExtensionID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testrg/providers/Microsoft.Kubernetes/connectedClusters/cldfe2econnectedcluster/providers/Microsoft.KubernetesConfiguration/extensions/vmware-extension"),
+		// 				ExtensionType: to.Ptr("arc-vmware"),
+		// 				TypesMetadata: []*armextendedlocation.EnabledResourceTypePropertiesTypesMetadataItem{
+		// 					{
+		// 						APIVersion: to.Ptr("2020-01-01-preview"),
+		// 						ResourceProviderNamespace: to.Ptr("Microsoft.VMware"),
+		// 						ResourceType: to.Ptr("virtualMachines"),
+		// 					},
+		// 					{
+		// 						APIVersion: to.Ptr("2020-01-22-preview"),
+		// 						ResourceProviderNamespace: to.Ptr("Microsoft.VMware"),
+		// 						ResourceType: to.Ptr("virtualmachines"),
+		// 				}},
+		// 			},
+		// 			SystemData: &armextendedlocation.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-04-24T18:53:29.0928001Z"); return t}()),
+		// 				CreatedBy: to.Ptr("string"),
+		// 				CreatedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-04-24T18:53:29.0928001Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("string"),
+		// 				LastModifiedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 			},
+		// 		},
+		// 		{
+		// 			Name: to.Ptr("266e9d31e5be6be1e919574e25780d5783586d502f0b2cc422e0a228a34e00a6"),
+		// 			Type: to.Ptr("Microsoft.ExtendedLocation/customLocations/enabledResourceTypes"),
+		// 			ID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testrg/providers/Microsoft.ExtendedLocation/customLocations/customLocation01/enabledResourceTypes/266e9d31e5be6be1e919574e25780d5783586d502f0b2cc422e0a228a34e00a6"),
+		// 			Properties: &armextendedlocation.EnabledResourceTypeProperties{
+		// 				ClusterExtensionID: to.Ptr("/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/testrg/providers/Microsoft.Kubernetes/connectedClusters/cldfe2econnectedcluster/providers/Microsoft.KubernetesConfiguration/extensions/cassandra-extension"),
+		// 				ExtensionType: to.Ptr("cassandradatacentersoperator"),
+		// 				TypesMetadata: []*armextendedlocation.EnabledResourceTypePropertiesTypesMetadataItem{
+		// 					{
+		// 						APIVersion: to.Ptr("2020-01-01-preview"),
+		// 						ResourceProviderNamespace: to.Ptr("Microsoft.Cassandra"),
+		// 						ResourceType: to.Ptr("cassandraDataCenters"),
+		// 					},
+		// 					{
+		// 						APIVersion: to.Ptr("2020-01-22-preview"),
+		// 						ResourceProviderNamespace: to.Ptr("Microsoft.Cassandra"),
+		// 						ResourceType: to.Ptr("cassandrabackups"),
+		// 				}},
+		// 			},
+		// 			SystemData: &armextendedlocation.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-24T18:53:29.0928001Z"); return t}()),
+		// 				CreatedBy: to.Ptr("string"),
+		// 				CreatedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-24T18:53:29.0928001Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("string"),
+		// 				LastModifiedByType: to.Ptr(armextendedlocation.CreatedByTypeApplication),
+		// 			},
+		// 	}},
+		// }
 	}
 }

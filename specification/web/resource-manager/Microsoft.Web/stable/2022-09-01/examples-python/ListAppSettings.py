4f8fbd3ea2b5a33c774405979c375cfbb18bf6c9
azure-sdk <azuresdk@microsoft.com>
2023-04-20 12:22:20 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-web_7.1.0

---
+++ specification/web/resource-manager/Microsoft.Web/stable/2022-09-01/examples-python/ListAppSettings.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.web import WebSiteManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-web
+# USAGE
+    python list_app_settings.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = WebSiteManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="34adfa4f-cedf-4dc0-ba29-b6d1a69ab345",
+    )
+
+    response = client.web_apps.list_application_settings(
+        resource_group_name="testrg123",
+        name="sitef6141",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2022-09-01/examples/ListAppSettings.json
+if __name__ == "__main__":
+    main()

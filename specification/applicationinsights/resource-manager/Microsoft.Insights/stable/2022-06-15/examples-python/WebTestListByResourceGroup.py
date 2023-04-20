041518a0f54dd8f4a4050980269a52f2a05137fe
azure-sdk <azuresdk@microsoft.com>
2023-04-20 12:13:03 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-applicationinsights_4.0.0

---
+++ specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-06-15/examples-python/WebTestListByResourceGroup.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.applicationinsights import ApplicationInsightsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-applicationinsights
+# USAGE
+    python web_test_list_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ApplicationInsightsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.web_tests.list_by_resource_group(
+        resource_group_name="my-resource-group",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/applicationinsights/resource-manager/Microsoft.Insights/stable/2022-06-15/examples/WebTestListByResourceGroup.json
+if __name__ == "__main__":
+    main()

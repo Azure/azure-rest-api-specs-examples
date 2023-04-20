d6c99d1f04e5d9c642e9c7f3760da04cae21bdbc
azure-sdk <azuresdk@microsoft.com>
2023-04-20 12:17:16 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_9.1.0b2

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-01-preview/examples-python/CosmosDBDatabaseAccountFailoverPriorityChange.py
@@ -0,0 +1,39 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_database_account_failover_priority_change.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.database_accounts.begin_failover_priority_change(
+        resource_group_name="rg1",
+        account_name="ddb1-failover",
+        failover_parameters={
+            "failoverPolicies": [
+                {"failoverPriority": 0, "locationName": "eastus"},
+                {"failoverPriority": 1, "locationName": "westus"},
+            ]
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-01-preview/examples/CosmosDBDatabaseAccountFailoverPriorityChange.json
+if __name__ == "__main__":
+    main()

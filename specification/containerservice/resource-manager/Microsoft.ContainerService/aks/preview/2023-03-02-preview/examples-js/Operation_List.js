f9a45d14d22561f1d2fb5d4bfce064b90d2a4d4f
azure-sdk <azuresdk@microsoft.com>
2023-04-21 12:13:18 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_18.0.0-beta.3

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-03-02-preview/examples-js/Operation_List.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a list of operations.
+ *
+ * @summary Gets a list of operations.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-03-02-preview/examples/Operation_List.json
+ */
+async function listAvailableOperationsForTheContainerServiceResourceProvider() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.operations.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-03-02-preview/examples-js/TrustedAccessRoleBindings_Delete.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Delete a trusted access role binding.
+ *
+ * @summary Delete a trusted access role binding.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-03-02-preview/examples/TrustedAccessRoleBindings_Delete.json
+ */
+async function deleteATrustedAccessRoleBinding() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const trustedAccessRoleBindingName = "binding1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.trustedAccessRoleBindings.delete(
+    resourceGroupName,
+    resourceName,
+    trustedAccessRoleBindingName
+  );
+  console.log(result);
+}

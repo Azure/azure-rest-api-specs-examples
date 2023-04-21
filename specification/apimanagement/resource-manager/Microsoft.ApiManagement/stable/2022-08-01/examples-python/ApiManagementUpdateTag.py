b901b9c9b8c618d5ccc59100255798ee2c943e14
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-21 20:01:54 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-apimanagement_4.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/examples-python/ApiManagementUpdateTag.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.apimanagement import ApiManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-apimanagement
+# USAGE
+    python api_management_update_tag.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ApiManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.tag.update(
+        resource_group_name="rg1",
+        service_name="apimService1",
+        tag_id="temptag",
+        if_match="*",
+        parameters={"properties": {"displayName": "temp tag"}},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/examples/ApiManagementUpdateTag.json
+if __name__ == "__main__":
+    main()

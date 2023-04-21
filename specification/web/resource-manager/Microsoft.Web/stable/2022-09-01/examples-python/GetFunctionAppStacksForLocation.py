f79969ee98bb098d69bedee7aae2ab7077b5ee27
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-21 20:01:25 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-web_7.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/web/resource-manager/Microsoft.Web/stable/2022-09-01/examples-python/GetFunctionAppStacksForLocation.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.web import WebSiteManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-web
+# USAGE
+    python get_function_app_stacks_for_location.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = WebSiteManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.provider.get_function_app_stacks_for_location(
+        location="westus",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2022-09-01/examples/GetFunctionAppStacksForLocation.json
+if __name__ == "__main__":
+    main()

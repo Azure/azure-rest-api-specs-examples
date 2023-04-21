976bb59fdf25c9b66ca4f528df34da3b940df859
azure-sdk <azuresdk@microsoft.com>
2023-04-21 12:20:21 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_9.1.0

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-03-15/examples-python/CosmosDBMongoDBCollectionMigrateToAutoscale.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_mongo_db_collection_migrate_to_autoscale.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.mongo_db_resources.begin_migrate_mongo_db_collection_to_autoscale(
+        resource_group_name="rg1",
+        account_name="ddb1",
+        database_name="databaseName",
+        collection_name="collectionName",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-03-15/examples/CosmosDBMongoDBCollectionMigrateToAutoscale.json
+if __name__ == "__main__":
+    main()

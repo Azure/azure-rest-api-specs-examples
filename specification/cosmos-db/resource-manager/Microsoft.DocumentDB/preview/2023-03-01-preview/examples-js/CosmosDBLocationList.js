65bdb66991e0ff48221fe97996a01cc190ef5e2b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-22 20:01:56 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_15.4.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-01-preview/examples-js/CosmosDBLocationList.js
@@ -0,0 +1,20 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List Cosmos DB locations and their properties
+ *
+ * @summary List Cosmos DB locations and their properties
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-01-preview/examples/CosmosDBLocationList.json
+ */
+async function cosmosDbLocationList() {
+  const subscriptionId =
+    process.env["COSMOSDB_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.locations.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-01-preview/examples-js/CosmosDBManagedCassandraClusterListBySubscription.js
@@ -0,0 +1,19 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List all managed Cassandra clusters in this subscription.
+ *
+ * @summary List all managed Cassandra clusters in this subscription.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-01-preview/examples/CosmosDBManagedCassandraClusterListBySubscription.json
+ */
+async function cosmosDbManagedCassandraClusterListBySubscription() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subid";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.cassandraClusters.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

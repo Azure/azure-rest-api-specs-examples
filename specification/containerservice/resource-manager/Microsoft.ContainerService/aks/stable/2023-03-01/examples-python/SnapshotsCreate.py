9a719dce6ae732668c845bc8c90d25bec2a2d4f3
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-22 20:01:34 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-containerservice_22.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-03-01/examples-python/SnapshotsCreate.py
@@ -0,0 +1,42 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.containerservice import ContainerServiceClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerservice
+# USAGE
+    python snapshots_create.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerServiceClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid1",
+    )
+
+    response = client.snapshots.create_or_update(
+        resource_group_name="rg1",
+        resource_name="snapshot1",
+        parameters={
+            "location": "westus",
+            "properties": {
+                "creationData": {
+                    "sourceResourceId": "/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/managedClusters/cluster1/agentPools/pool0"
+                }
+            },
+            "tags": {"key1": "val1", "key2": "val2"},
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-03-01/examples/SnapshotsCreate.json
+if __name__ == "__main__":
+    main()

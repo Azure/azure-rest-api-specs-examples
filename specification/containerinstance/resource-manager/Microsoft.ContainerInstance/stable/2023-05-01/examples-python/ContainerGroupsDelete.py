39384dc00951b20d2ba230809e86fb6d2545c045
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-22 20:01:20 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-containerinstance_10.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2023-05-01/examples-python/ContainerGroupsDelete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.containerinstance import ContainerInstanceManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerinstance
+# USAGE
+    python container_groups_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerInstanceManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.container_groups.begin_delete(
+        resource_group_name="demo",
+        container_group_name="demo1",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2023-05-01/examples/ContainerGroupsDelete.json
+if __name__ == "__main__":
+    main()

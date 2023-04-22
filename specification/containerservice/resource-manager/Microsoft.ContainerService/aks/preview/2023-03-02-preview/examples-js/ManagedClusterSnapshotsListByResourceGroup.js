03b168d208d415d9c8c223e87f6ab4c149f89907
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-22 20:01:49 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_18.0.0-beta.3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-03-02-preview/examples-js/ManagedClusterSnapshotsListByResourceGroup.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists managed cluster snapshots in the specified subscription and resource group.
+ *
+ * @summary Lists managed cluster snapshots in the specified subscription and resource group.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-03-02-preview/examples/ManagedClusterSnapshotsListByResourceGroup.json
+ */
+async function listManagedClusterSnapshotsByResourceGroup() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.managedClusterSnapshots.listByResourceGroup(resourceGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

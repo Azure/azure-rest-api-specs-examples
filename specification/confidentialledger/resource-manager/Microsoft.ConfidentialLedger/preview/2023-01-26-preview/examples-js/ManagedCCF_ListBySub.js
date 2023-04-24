d0e00e823cfcbba4fad833f60748df34b6698992
azure-sdk <azuresdk@microsoft.com>
2023-04-24 12:13:40 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-confidentialledger_1.3.0-beta.1

---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-01-26-preview/examples-js/ManagedCCF_ListBySub.js
@@ -0,0 +1,20 @@
+const { ConfidentialLedgerClient } = require("@azure/arm-confidentialledger");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Retrieves the properties of all Managed CCF.
+ *
+ * @summary Retrieves the properties of all Managed CCF.
+ * x-ms-original-file: specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-01-26-preview/examples/ManagedCCF_ListBySub.json
+ */
+async function managedCcfListBySub() {
+  const subscriptionId =
+    process.env["CONFIDENTIALLEDGER_SUBSCRIPTION_ID"] || "0000000-0000-0000-0000-000000000001";
+  const credential = new DefaultAzureCredential();
+  const client = new ConfidentialLedgerClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.managedCCFOperations.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

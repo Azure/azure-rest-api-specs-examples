c7448e60b392c7e74b06ca6a28b70868cd492b3b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-25 20:01:09 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Monitor_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/monitor/resource-manager/Microsoft.Insights/stable/2022-06-01/examples-dotnet/DataCollectionRuleAssociationsDelete.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Monitor;
+
+// Generated from example definition: specification/monitor/resource-manager/Microsoft.Insights/stable/2022-06-01/examples/DataCollectionRuleAssociationsDelete.json
+// this example is just showing the usage of "DataCollectionRuleAssociations_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DataCollectionRuleAssociationResource created on azure
+// for more information of creating DataCollectionRuleAssociationResource, please refer to the document of DataCollectionRuleAssociationResource
+string resourceUri = "subscriptions/703362b3-f278-4e4b-9179-c76eaf41ffc2/resourceGroups/myResourceGroup/providers/Microsoft.Compute/virtualMachines/myVm";
+string associationName = "myAssociation";
+ResourceIdentifier dataCollectionRuleAssociationResourceId = DataCollectionRuleAssociationResource.CreateResourceIdentifier(resourceUri, associationName);
+DataCollectionRuleAssociationResource dataCollectionRuleAssociation = client.GetDataCollectionRuleAssociationResource(dataCollectionRuleAssociationResourceId);
+
+// invoke the operation
+await dataCollectionRuleAssociation.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");
---
+++ specification/monitor/resource-manager/Microsoft.Insights/stable/2022-06-01/examples-dotnet/DataCollectionRuleAssociationsListByDataCollectionEndpoint.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Monitor;
+using Azure.ResourceManager.Monitor.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/monitor/resource-manager/Microsoft.Insights/stable/2022-06-01/examples/DataCollectionRuleAssociationsListByDataCollectionEndpoint.json
+// this example is just showing the usage of "DataCollectionRuleAssociations_ListByDataCollectionEndpoint" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DataCollectionEndpointResource created on azure
+// for more information of creating DataCollectionEndpointResource, please refer to the document of DataCollectionEndpointResource
+string subscriptionId = "703362b3-f278-4e4b-9179-c76eaf41ffc2";
+string resourceGroupName = "myResourceGroup";
+string dataCollectionEndpointName = "myDataCollectionEndpointName";
+ResourceIdentifier dataCollectionEndpointResourceId = DataCollectionEndpointResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, dataCollectionEndpointName);
+DataCollectionEndpointResource dataCollectionEndpoint = client.GetDataCollectionEndpointResource(dataCollectionEndpointResourceId);
+
+// invoke the operation and iterate over the result
+await foreach (DataCollectionRuleAssociationResource item in dataCollectionEndpoint.GetDataCollectionRuleAssociationsAsync())
+{
+    // the variable item is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    DataCollectionRuleAssociationData resourceData = item.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
+
+Console.WriteLine($"Succeeded");

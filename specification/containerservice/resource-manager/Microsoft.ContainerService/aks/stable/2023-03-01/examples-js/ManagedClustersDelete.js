ba42357162ac5b4c4424569604d7ba1dbe3192b5
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-25 20:01:47 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_18.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-03-01/examples-js/ManagedClustersDelete.js
@@ -0,0 +1,18 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a managed cluster.
+ *
+ * @summary Deletes a managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-03-01/examples/ManagedClustersDelete.json
+ */
+async function deleteManagedCluster() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginDeleteAndWait(resourceGroupName, resourceName);
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-03-01/examples-js/SnapshotsGet.js
@@ -0,0 +1,18 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a snapshot.
+ *
+ * @summary Gets a snapshot.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-03-01/examples/SnapshotsGet.json
+ */
+async function getSnapshot() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "snapshot1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.snapshots.get(resourceGroupName, resourceName);
+  console.log(result);
+}

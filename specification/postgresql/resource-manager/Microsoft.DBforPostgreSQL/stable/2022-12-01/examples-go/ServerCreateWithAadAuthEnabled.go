3cd6712454629696a22d6c2623b0a60166fca53d
azure-sdk <azuresdk@microsoft.com>
2023-04-26 12:09:38 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/postgresql/armpostgresqlflexibleservers/v3.0.0

--- specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples-go/ServerCreateWithAadAuthEnabled.go
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples-go/ServerCreateWithAadAuthEnabled.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/postgresql/armpostgresqlflexibleservers/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/postgresql/armpostgresqlflexibleservers/v3"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/fce0b25dda01303f2c70de34031169b5d326998b/specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples/ServerCreateWithAadAuthEnabled.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/75ece9197dbac70ac0ba651c53a79c1841944be2/specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-12-01/examples/ServerCreateWithAadAuthEnabled.json
 func ExampleServersClient_BeginCreate_createANewServerWithActiveDirectoryAuthenticationEnabled() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -40,7 +40,7 @@ func ExampleServersClient_BeginCreate_createANewServerWithActiveDirectoryAuthent
 			},
 			CreateMode: to.Ptr(armpostgresqlflexibleservers.CreateModeCreate),
 			DataEncryption: &armpostgresqlflexibleservers.DataEncryption{
-				Type: to.Ptr(armpostgresqlflexibleservers.ArmServerKeyType("SystemManaged")),
+				Type: to.Ptr(armpostgresqlflexibleservers.ArmServerKeyTypeSystemManaged),
 			},
 			HighAvailability: &armpostgresqlflexibleservers.HighAvailability{
 				Mode: to.Ptr(armpostgresqlflexibleservers.HighAvailabilityModeZoneRedundant),

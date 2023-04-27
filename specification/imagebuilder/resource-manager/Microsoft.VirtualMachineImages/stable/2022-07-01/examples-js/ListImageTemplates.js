c0d4094464df9d46fa391a45f6a65f4bb1f28910
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-27 20:01:47 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-imagebuilder_3.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/imagebuilder/resource-manager/Microsoft.VirtualMachineImages/stable/2022-07-01/examples-js/ListImageTemplates.js
@@ -0,0 +1,19 @@
+const { ImageBuilderClient } = require("@azure/arm-imagebuilder");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets information about the VM image templates associated with the subscription.
+ *
+ * @summary Gets information about the VM image templates associated with the subscription.
+ * x-ms-original-file: specification/imagebuilder/resource-manager/Microsoft.VirtualMachineImages/stable/2022-07-01/examples/ListImageTemplates.json
+ */
+async function listImagesBySubscription() {
+  const subscriptionId = process.env["IMAGEBUILDER_SUBSCRIPTION_ID"] || "{subscription-id}";
+  const credential = new DefaultAzureCredential();
+  const client = new ImageBuilderClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.virtualMachineImageTemplates.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

0a63da5b73b2058925bedc74b8df8c12671da091
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-27 20:01:24 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_15.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-03-15/examples-js/CosmosDBManagedCassandraClusterListByResourceGroup.js
@@ -0,0 +1,21 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List all managed Cassandra clusters in this resource group.
+ *
+ * @summary List all managed Cassandra clusters in this resource group.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-03-15/examples/CosmosDBManagedCassandraClusterListByResourceGroup.json
+ */
+async function cosmosDbManagedCassandraClusterListByResourceGroup() {
+  const subscriptionId =
+    process.env["COSMOSDB_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "cassandra-prod-rg";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.cassandraClusters.listByResourceGroup(resourceGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-03-15/examples-js/CosmosDBMaterializedViewsBuilderServiceCreate.js
@@ -0,0 +1,29 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates a service.
+ *
+ * @summary Creates a service.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-03-15/examples/CosmosDBMaterializedViewsBuilderServiceCreate.json
+ */
+async function materializedViewsBuilderServiceCreate() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subid";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rg1";
+  const accountName = "ddb1";
+  const serviceName = "MaterializedViewsBuilder";
+  const createUpdateParameters = {
+    instanceCount: 1,
+    instanceSize: "Cosmos.D4s",
+    serviceType: "MaterializedViewsBuilder",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const result = await client.service.beginCreateAndWait(
+    resourceGroupName,
+    accountName,
+    serviceName,
+    createUpdateParameters
+  );
+  console.log(result);
+}

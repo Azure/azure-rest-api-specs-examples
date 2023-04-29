0c6706e0c5ab0bd3d9c598aa80226d899aa0a27f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-04-29 20:01:00 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Network_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/network/resource-manager/Microsoft.Network/stable/2022-09-01/examples-dotnet/DdosProtectionPlanGet.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Network;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/network/resource-manager/Microsoft.Network/stable/2022-09-01/examples/DdosProtectionPlanGet.json
+// this example is just showing the usage of "DdosProtectionPlans_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "subid";
+string resourceGroupName = "rg1";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this DdosProtectionPlanResource
+DdosProtectionPlanCollection collection = resourceGroupResource.GetDdosProtectionPlans();
+
+// invoke the operation
+string ddosProtectionPlanName = "test-plan";
+bool result = await collection.ExistsAsync(ddosProtectionPlanName);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/network/resource-manager/Microsoft.Network/stable/2022-09-01/examples-dotnet/ExpressRouteLinkGet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Network;
+
+// Generated from example definition: specification/network/resource-manager/Microsoft.Network/stable/2022-09-01/examples/ExpressRouteLinkGet.json
+// this example is just showing the usage of "ExpressRouteLinks_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ExpressRoutePortResource created on azure
+// for more information of creating ExpressRoutePortResource, please refer to the document of ExpressRoutePortResource
+string subscriptionId = "subid";
+string resourceGroupName = "rg1";
+string expressRoutePortName = "portName";
+ResourceIdentifier expressRoutePortResourceId = ExpressRoutePortResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, expressRoutePortName);
+ExpressRoutePortResource expressRoutePort = client.GetExpressRoutePortResource(expressRoutePortResourceId);
+
+// get the collection of this ExpressRouteLinkResource
+ExpressRouteLinkCollection collection = expressRoutePort.GetExpressRouteLinks();
+
+// invoke the operation
+string linkName = "linkName";
+bool result = await collection.ExistsAsync(linkName);
+
+Console.WriteLine($"Succeeded: {result}");

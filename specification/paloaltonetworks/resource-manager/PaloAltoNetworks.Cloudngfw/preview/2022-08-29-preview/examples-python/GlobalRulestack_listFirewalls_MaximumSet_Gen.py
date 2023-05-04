698adf35ac53e3039ea1b71ef2ecc5c92130241f
azure-sdk <azuresdk@microsoft.com>
2023-05-04 12:23:39 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-paloaltonetworksngfw_1.0.0b1

---
+++ specification/paloaltonetworks/resource-manager/PaloAltoNetworks.Cloudngfw/preview/2022-08-29-preview/examples-python/GlobalRulestack_listFirewalls_MaximumSet_Gen.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.paloaltonetworks import PaloAltoNetworksNgfwMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-paloaltonetworks
+# USAGE
+    python global_rulestack_list_firewalls_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PaloAltoNetworksNgfwMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.global_rulestack.list_firewalls(
+        global_rulestack_name="praval",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/paloaltonetworks/resource-manager/PaloAltoNetworks.Cloudngfw/preview/2022-08-29-preview/examples/GlobalRulestack_listFirewalls_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

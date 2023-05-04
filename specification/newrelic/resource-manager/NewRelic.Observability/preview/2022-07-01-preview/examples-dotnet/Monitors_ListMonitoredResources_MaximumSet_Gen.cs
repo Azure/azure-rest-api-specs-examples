95af51d912a69fe4e8f425a9be91dd772be7a9da
azure-sdk <azuresdk@microsoft.com>
2023-05-04 12:16:09 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.NewRelicObservability_1.0.0-beta.1

---
+++ specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples-dotnet/Monitors_ListMonitoredResources_MaximumSet_Gen.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.NewRelicObservability;
+using Azure.ResourceManager.NewRelicObservability.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples/Monitors_ListMonitoredResources_MaximumSet_Gen.json
+// this example is just showing the usage of "Monitors_ListMonitoredResources" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this NewRelicMonitorResource created on azure
+// for more information of creating NewRelicMonitorResource, please refer to the document of NewRelicMonitorResource
+string subscriptionId = "ddqonpqwjr";
+string resourceGroupName = "rgopenapi";
+string monitorName = "ipxmlcbonyxtolzejcjshkmlron";
+ResourceIdentifier newRelicMonitorResourceId = NewRelicMonitorResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, monitorName);
+NewRelicMonitorResource newRelicMonitorResource = client.GetNewRelicMonitorResource(newRelicMonitorResourceId);
+
+// invoke the operation and iterate over the result
+await foreach (MonitoredResource item in newRelicMonitorResource.GetMonitoredResourcesAsync())
+{
+    Console.WriteLine($"Succeeded: {item}");
+}
+
+Console.WriteLine($"Succeeded");
---
+++ specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples-dotnet/TagRules_CreateOrUpdate_MinimumSet_Gen.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.NewRelicObservability;
+using Azure.ResourceManager.NewRelicObservability.Models;
+
+// Generated from example definition: specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples/TagRules_CreateOrUpdate_MinimumSet_Gen.json
+// this example is just showing the usage of "TagRules_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this NewRelicMonitorResource created on azure
+// for more information of creating NewRelicMonitorResource, please refer to the document of NewRelicMonitorResource
+string subscriptionId = "ddqonpqwjr";
+string resourceGroupName = "rgopenapi";
+string monitorName = "ipxmlcbonyxtolzejcjshkmlron";
+ResourceIdentifier newRelicMonitorResourceId = NewRelicMonitorResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, monitorName);
+NewRelicMonitorResource newRelicMonitorResource = client.GetNewRelicMonitorResource(newRelicMonitorResourceId);
+
+// get the collection of this TagRuleResource
+TagRuleCollection collection = newRelicMonitorResource.GetTagRules();
+
+// invoke the operation
+string ruleSetName = "bxcantgzggsepbhqmedjqyrqeezmfb";
+TagRuleData data = new TagRuleData();
+ArmOperation<TagRuleResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, ruleSetName, data);
+TagRuleResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+TagRuleData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

c730e1ff86215fa6556ba5a75523413f4888438a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-06 20:01:40 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.KeyVault_1.2.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-02-01/examples-dotnet/DeletedManagedHsm_Purge.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.KeyVault;
+
+// Generated from example definition: specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-02-01/examples/DeletedManagedHsm_Purge.json
+// this example is just showing the usage of "ManagedHsms_PurgeDeleted" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DeletedManagedHsmResource created on azure
+// for more information of creating DeletedManagedHsmResource, please refer to the document of DeletedManagedHsmResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+AzureLocation location = new AzureLocation("westus");
+string name = "hsm1";
+ResourceIdentifier deletedManagedHsmResourceId = DeletedManagedHsmResource.CreateResourceIdentifier(subscriptionId, location, name);
+DeletedManagedHsmResource deletedManagedHsm = client.GetDeletedManagedHsmResource(deletedManagedHsmResourceId);
+
+// invoke the operation
+await deletedManagedHsm.PurgeDeletedAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");
---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-02-01/examples-dotnet/purgeDeletedVault.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.KeyVault;
+
+// Generated from example definition: specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-02-01/examples/purgeDeletedVault.json
+// this example is just showing the usage of "Vaults_PurgeDeleted" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DeletedKeyVaultResource created on azure
+// for more information of creating DeletedKeyVaultResource, please refer to the document of DeletedKeyVaultResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+AzureLocation location = new AzureLocation("westus");
+string vaultName = "sample-vault";
+ResourceIdentifier deletedKeyVaultResourceId = DeletedKeyVaultResource.CreateResourceIdentifier(subscriptionId, location, vaultName);
+DeletedKeyVaultResource deletedKeyVault = client.GetDeletedKeyVaultResource(deletedKeyVaultResourceId);
+
+// invoke the operation
+await deletedKeyVault.PurgeDeletedAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

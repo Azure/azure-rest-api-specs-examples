b94f6c906dedad81c3116f9d2cc0fffdb6eaa314
azure-sdk <azuresdk@microsoft.com>
2023-05-08 12:09:02 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_9.2.0

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-04-15/examples-python/CosmosDBManagedCassandraCommand.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_managed_cassandra_command.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.cassandra_clusters.begin_invoke_command(
+        resource_group_name="cassandra-prod-rg",
+        cluster_name="cassandra-prod",
+        body={"command": "nodetool status", "host": "10.0.1.12"},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-04-15/examples/CosmosDBManagedCassandraCommand.json
+if __name__ == "__main__":
+    main()

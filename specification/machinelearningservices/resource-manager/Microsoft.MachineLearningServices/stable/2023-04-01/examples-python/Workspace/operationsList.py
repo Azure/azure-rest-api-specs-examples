1b78aa011e448de3414504706c13486d45205d54
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-13 20:01:26 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-machinelearningservices_2.0.0b2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2023-04-01/examples-python/Workspace/operationsList.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.machinelearningservices import MachineLearningServicesMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-machinelearningservices
+# USAGE
+    python operations_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MachineLearningServicesMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.operations.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2023-04-01/examples/Workspace/operationsList.json
+if __name__ == "__main__":
+    main()

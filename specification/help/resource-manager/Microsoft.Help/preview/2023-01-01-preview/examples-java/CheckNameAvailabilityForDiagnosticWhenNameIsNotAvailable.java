effed6895389f8cc96c5ff06271b944a42d407cb
azure-sdk <azuresdk@microsoft.com>
2023-05-16 12:07:27 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-selfhelp_1.0.0-beta.1

---
+++ specification/help/resource-manager/Microsoft.Help/preview/2023-01-01-preview/examples-java/CheckNameAvailabilityForDiagnosticWhenNameIsNotAvailable.java
@@ -0,0 +1,22 @@
+import com.azure.resourcemanager.selfhelp.models.CheckNameAvailabilityRequest;
+
+/** Samples for Diagnostics CheckNameAvailability. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/help/resource-manager/Microsoft.Help/preview/2023-01-01-preview/examples/CheckNameAvailabilityForDiagnosticWhenNameIsNotAvailable.json
+     */
+    /**
+     * Sample code: Example when name is not available for a Diagnostic resource.
+     *
+     * @param manager Entry point to SelfHelpManager.
+     */
+    public static void exampleWhenNameIsNotAvailableForADiagnosticResource(
+        com.azure.resourcemanager.selfhelp.SelfHelpManager manager) {
+        manager
+            .diagnostics()
+            .checkNameAvailabilityWithResponse(
+                "subscriptions/0d0fcd2e-c4fd-4349-8497-200edb3923c6",
+                new CheckNameAvailabilityRequest().withName("sampleName").withType("Microsoft.Help/diagnostics"),
+                com.azure.core.util.Context.NONE);
+    }
+}

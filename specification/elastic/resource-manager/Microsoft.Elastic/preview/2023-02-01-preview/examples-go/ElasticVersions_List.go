76ee4507ec78387bb9a80086196adae90bf3de49
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-20 20:01:52 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/elastic/armelastic/v0.9.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples-go/ElasticVersions_List.go
+++ specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples-go/ElasticVersions_List.go
@@ -8,8 +8,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/elastic/armelastic"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/dbd896bc9a795bcb3ec7db0a340b517fd3059620/specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples/Operations_List.json
-func ExampleOperationsClient_NewListPager() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/dbd896bc9a795bcb3ec7db0a340b517fd3059620/specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples/ElasticVersions_List.json
+func ExampleVersionsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -19,7 +19,7 @@ func ExampleOperationsClient_NewListPager() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	pager := clientFactory.NewVersionsClient().NewListPager("myregion", nil)
 	for pager.More() {
 		page, err := pager.NextPage(ctx)
 		if err != nil {
@@ -30,15 +30,11 @@ func ExampleOperationsClient_NewListPager() {
 			_ = v
 		}
 		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
-		// page.OperationListResult = armelastic.OperationListResult{
-		// 	Value: []*armelastic.OperationResult{
+		// page.VersionsListResponse = armelastic.VersionsListResponse{
+		// 	Value: []*armelastic.VersionListFormat{
 		// 		{
-		// 			Name: to.Ptr("Microsoft.Elastic/monitors/write"),
-		// 			Display: &armelastic.OperationDisplay{
-		// 				Description: to.Ptr("Write monitors resource"),
-		// 				Operation: to.Ptr("write"),
-		// 				Provider: to.Ptr("Microsoft.Elastic"),
-		// 				Resource: to.Ptr("monitors"),
+		// 			Properties: &armelastic.VersionListProperties{
+		// 				Version: to.Ptr("version1"),
 		// 			},
 		// 	}},
 		// }

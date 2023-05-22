0142d8abbcbbaef6417b39c290c46ab533127cc8
azure-sdk <azuresdk@microsoft.com>
2023-05-22 12:21:17 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-devcenter_1.0.0

---
+++ specification/devcenter/resource-manager/Microsoft.DevCenter/stable/2023-04-01/examples-python/DevCenters_ListBySubscription.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.devcenter import DevCenterMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-devcenter
+# USAGE
+    python dev_centers_list_by_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DevCenterMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="0ac520ee-14c0-480f-b6c9-0a90c58ffff",
+    )
+
+    response = client.dev_centers.list_by_subscription()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/devcenter/resource-manager/Microsoft.DevCenter/stable/2023-04-01/examples/DevCenters_ListBySubscription.json
+if __name__ == "__main__":
+    main()

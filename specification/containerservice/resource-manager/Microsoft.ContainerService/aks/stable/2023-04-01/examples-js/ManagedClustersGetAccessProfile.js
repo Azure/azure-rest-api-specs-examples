02dad4d5156deb988f7f212e911bc85dd9114c75
azure-sdk <azuresdk@microsoft.com>
2023-05-23 12:25:27 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.0.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-04-01/examples-js/ManagedClustersGetAccessProfile.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to **WARNING**: This API will be deprecated. Instead use [ListClusterUserCredentials](https://docs.microsoft.com/rest/api/aks/managedclusters/listclusterusercredentials) or [ListClusterAdminCredentials](https://docs.microsoft.com/rest/api/aks/managedclusters/listclusteradmincredentials) .
+ *
+ * @summary **WARNING**: This API will be deprecated. Instead use [ListClusterUserCredentials](https://docs.microsoft.com/rest/api/aks/managedclusters/listclusterusercredentials) or [ListClusterAdminCredentials](https://docs.microsoft.com/rest/api/aks/managedclusters/listclusteradmincredentials) .
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-04-01/examples/ManagedClustersGetAccessProfile.json
+ */
+async function getManagedCluster() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const roleName = "clusterUser";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.getAccessProfile(
+    resourceGroupName,
+    resourceName,
+    roleName
+  );
+  console.log(result);
+}

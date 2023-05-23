988828d50ef97d308c34a1cc3a76b853661ead20
azure-sdk <azuresdk@microsoft.com>
2023-05-23 12:28:00 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_15.5.0

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-04-15/examples-js/CosmosDBSqlDedicatedGatewayServiceGet.js
@@ -0,0 +1,19 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the status of service.
+ *
+ * @summary Gets the status of service.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-04-15/examples/CosmosDBSqlDedicatedGatewayServiceGet.json
+ */
+async function sqlDedicatedGatewayServiceGet() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subid";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rg1";
+  const accountName = "ddb1";
+  const serviceName = "SqlDedicatedGateway";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const result = await client.service.get(resourceGroupName, accountName, serviceName);
+  console.log(result);
+}

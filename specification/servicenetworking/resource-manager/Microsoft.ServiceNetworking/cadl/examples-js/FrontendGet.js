438c9620ef8f3a92ce0f314af94425f61e0adef4
azure-sdk <azuresdk@microsoft.com>
2023-05-23 12:33:10 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-servicenetworking_1.0.0-beta.2

--- specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-js/FrontendGet.js
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-js/FrontendGet.js
@@ -2,16 +2,16 @@ const { ServiceNetworkingManagementClient } = require("@azure/arm-servicenetwork
 const { DefaultAzureCredential } = require("@azure/identity");
 
 /**
- * This sample demonstrates how to Get a Traffic Controller Frontend
+ * This sample demonstrates how to Get a Frontend
  *
- * @summary Get a Traffic Controller Frontend
+ * @summary Get a Frontend
  * x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/FrontendGet.json
  */
 async function getFrontend() {
   const subscriptionId = process.env["SERVICENETWORKING_SUBSCRIPTION_ID"] || "subid";
   const resourceGroupName = process.env["SERVICENETWORKING_RESOURCE_GROUP"] || "rg1";
-  const trafficControllerName = "TC1";
-  const frontendName = "publicIp1";
+  const trafficControllerName = "tc1";
+  const frontendName = "fe1";
   const credential = new DefaultAzureCredential();
   const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
   const result = await client.frontendsInterface.get(

bb1c91d4c8be9a3c86dfe6aebb64c97534313cd6
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-23 20:03:04 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-recoveryservices_2.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples-python/ListResources.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.recoveryservices import RecoveryServicesClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-recoveryservices
+# USAGE
+    python list_resources.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RecoveryServicesClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="77777777-b0c6-47a2-b37c-d8e65a629c18",
+    )
+
+    response = client.vaults.list_by_resource_group(
+        resource_group_name="Default-RecoveryServices-ResourceGroup",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples/ListResources.json
+if __name__ == "__main__":
+    main()

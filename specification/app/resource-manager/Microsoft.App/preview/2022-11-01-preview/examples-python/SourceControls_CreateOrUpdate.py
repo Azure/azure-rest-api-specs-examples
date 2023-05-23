d3e035fa0db1aa5afd0b180effc9f5b302adf864
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-23 20:02:12 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-appcontainers_3.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/app/resource-manager/Microsoft.App/preview/2022-11-01-preview/examples-python/SourceControls_CreateOrUpdate.py
@@ -0,0 +1,54 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appcontainers
+# USAGE
+    python source_controls_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerAppsAPIClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="651f8027-33e8-4ec4-97b4-f6e9f3dc8744",
+    )
+
+    response = client.container_apps_source_controls.begin_create_or_update(
+        resource_group_name="workerapps-rg-xj",
+        container_app_name="testcanadacentral",
+        source_control_name="current",
+        source_control_envelope={
+            "properties": {
+                "branch": "master",
+                "githubActionConfiguration": {
+                    "azureCredentials": {
+                        "clientId": "<clientid>",
+                        "clientSecret": "<clientsecret>",
+                        "tenantId": "<tenantid>",
+                    },
+                    "contextPath": "./",
+                    "image": "image/tag",
+                    "registryInfo": {
+                        "registryPassword": "<registrypassword>",
+                        "registryUrl": "xwang971reg.azurecr.io",
+                        "registryUserName": "xwang971reg",
+                    },
+                },
+                "repoUrl": "https://github.com/xwang971/ghatest",
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2022-11-01-preview/examples/SourceControls_CreateOrUpdate.json
+if __name__ == "__main__":
+    main()

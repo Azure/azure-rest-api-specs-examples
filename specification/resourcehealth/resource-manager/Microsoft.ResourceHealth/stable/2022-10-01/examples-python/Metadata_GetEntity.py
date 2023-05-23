3665f66967d57a1ed2d023bc4a632d78b8b7be29
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-23 20:02:27 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-resourcehealth_1.0.0b4
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/stable/2022-10-01/examples-python/Metadata_GetEntity.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.resourcehealth import ResourceHealthMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resourcehealth
+# USAGE
+    python metadata_get_entity.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ResourceHealthMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.metadata.get_entity(
+        name="status",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/stable/2022-10-01/examples/Metadata_GetEntity.json
+if __name__ == "__main__":
+    main()

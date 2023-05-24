9b4051366fc4e0f0f38c48de74f9b4fbc4a200ba
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-24 20:01:35 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-selfhelp_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/help/resource-manager/Microsoft.Help/preview/2023-01-01-preview/examples-js/CreateDiagnosticForKeyVaultResource.js
@@ -0,0 +1,18 @@
+const { HelpRP } = require("@azure/arm-selfhelp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Diagnostics tells you precisely the root cause of the issue and how to address it. You can get diagnostics once you discover and identify the relevant solution for your Azure issue.<br/><br/> You can create diagnostics using the ‘solutionId’  from Solution Discovery API response and ‘additionalParameters’ <br/><br/> <b>Note: </b>‘requiredParameterSets’ from Solutions Discovery API response must be passed via ‘additionalParameters’ as an input to Diagnostics API
+ *
+ * @summary Diagnostics tells you precisely the root cause of the issue and how to address it. You can get diagnostics once you discover and identify the relevant solution for your Azure issue.<br/><br/> You can create diagnostics using the ‘solutionId’  from Solution Discovery API response and ‘additionalParameters’ <br/><br/> <b>Note: </b>‘requiredParameterSets’ from Solutions Discovery API response must be passed via ‘additionalParameters’ as an input to Diagnostics API
+ * x-ms-original-file: specification/help/resource-manager/Microsoft.Help/preview/2023-01-01-preview/examples/CreateDiagnosticForKeyVaultResource.json
+ */
+async function createsADiagnosticForAKeyVaultResource() {
+  const scope =
+    "subscriptions/0d0fcd2e-c4fd-4349-8497-200edb3923c6/resourcegroups/myresourceGroup/providers/Microsoft.KeyVault/vaults/test-keyvault-non-read";
+  const diagnosticsResourceName = "VMNotWorkingInsight";
+  const credential = new DefaultAzureCredential();
+  const client = new HelpRP(credential);
+  const result = await client.diagnostics.beginCreateAndWait(scope, diagnosticsResourceName);
+  console.log(result);
+}

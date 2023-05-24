23daf084765067a4d738f549672da5a0a1a53a91
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-24 20:01:20 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-servicenetworking_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-js/AssociationPut.js
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples-js/AssociationPut.js
@@ -2,20 +2,22 @@ const { ServiceNetworkingManagementClient } = require("@azure/arm-servicenetwork
 const { DefaultAzureCredential } = require("@azure/identity");
 
 /**
- * This sample demonstrates how to Create a Traffic Controller Association
+ * This sample demonstrates how to Create a Association
  *
- * @summary Create a Traffic Controller Association
+ * @summary Create a Association
  * x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/AssociationPut.json
  */
 async function putAssociation() {
   const subscriptionId = process.env["SERVICENETWORKING_SUBSCRIPTION_ID"] || "subid";
   const resourceGroupName = process.env["SERVICENETWORKING_RESOURCE_GROUP"] || "rg1";
-  const trafficControllerName = "TC1";
-  const associationName = "associatedvnet-1";
+  const trafficControllerName = "tc1";
+  const associationName = "as1";
   const resource = {
     associationType: "subnets",
-    location: "West US",
-    subnet: { id: "subnetFullRef" },
+    location: "NorthCentralUS",
+    subnet: {
+      id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/vnet-tc/subnets/tc-subnet",
+    },
   };
   const credential = new DefaultAzureCredential();
   const client = new ServiceNetworkingManagementClient(credential, subscriptionId);

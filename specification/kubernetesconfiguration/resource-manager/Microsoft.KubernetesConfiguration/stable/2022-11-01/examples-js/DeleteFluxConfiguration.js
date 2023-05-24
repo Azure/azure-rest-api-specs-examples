d3fd3cab1b464df7b8b17bd2552afc18527703c6
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-24 20:02:20 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-kubernetesconfiguration_6.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples-js/DeleteFluxConfiguration.js
@@ -0,0 +1,27 @@
+const { SourceControlConfigurationClient } = require("@azure/arm-kubernetesconfiguration");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to This will delete the YAML file used to set up the Flux Configuration, thus stopping future sync from the source repo.
+ *
+ * @summary This will delete the YAML file used to set up the Flux Configuration, thus stopping future sync from the source repo.
+ * x-ms-original-file: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples/DeleteFluxConfiguration.json
+ */
+async function deleteFluxConfiguration() {
+  const subscriptionId = process.env["KUBERNETESCONFIGURATION_SUBSCRIPTION_ID"] || "subId1";
+  const resourceGroupName = process.env["KUBERNETESCONFIGURATION_RESOURCE_GROUP"] || "rg1";
+  const clusterRp = "Microsoft.Kubernetes";
+  const clusterResourceName = "connectedClusters";
+  const clusterName = "clusterName1";
+  const fluxConfigurationName = "srs-fluxconfig";
+  const credential = new DefaultAzureCredential();
+  const client = new SourceControlConfigurationClient(credential, subscriptionId);
+  const result = await client.fluxConfigurations.beginDeleteAndWait(
+    resourceGroupName,
+    clusterRp,
+    clusterResourceName,
+    clusterName,
+    fluxConfigurationName
+  );
+  console.log(result);
+}
---
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples-js/PatchFluxConfiguration.js
@@ -0,0 +1,46 @@
+const { SourceControlConfigurationClient } = require("@azure/arm-kubernetesconfiguration");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Update an existing Kubernetes Flux Configuration.
+ *
+ * @summary Update an existing Kubernetes Flux Configuration.
+ * x-ms-original-file: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples/PatchFluxConfiguration.json
+ */
+async function patchFluxConfiguration() {
+  const subscriptionId = process.env["KUBERNETESCONFIGURATION_SUBSCRIPTION_ID"] || "subId1";
+  const resourceGroupName = process.env["KUBERNETESCONFIGURATION_RESOURCE_GROUP"] || "rg1";
+  const clusterRp = "Microsoft.Kubernetes";
+  const clusterResourceName = "connectedClusters";
+  const clusterName = "clusterName1";
+  const fluxConfigurationName = "srs-fluxconfig";
+  const fluxConfigurationPatch = {
+    gitRepository: {
+      url: "https://github.com/jonathan-innis/flux2-kustomize-helm-example.git",
+    },
+    kustomizations: {
+      srsKustomization1: {},
+      srsKustomization2: {
+        path: "./test/alt-path",
+        dependsOn: [],
+        syncIntervalInSeconds: 300,
+      },
+      srsKustomization3: {
+        path: "./test/another-path",
+        syncIntervalInSeconds: 300,
+      },
+    },
+    suspend: true,
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new SourceControlConfigurationClient(credential, subscriptionId);
+  const result = await client.fluxConfigurations.beginUpdateAndWait(
+    resourceGroupName,
+    clusterRp,
+    clusterResourceName,
+    clusterName,
+    fluxConfigurationName,
+    fluxConfigurationPatch
+  );
+  console.log(result);
+}

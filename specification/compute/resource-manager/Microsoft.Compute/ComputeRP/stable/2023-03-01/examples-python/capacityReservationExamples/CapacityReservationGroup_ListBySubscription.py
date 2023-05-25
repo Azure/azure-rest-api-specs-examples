b48fe172dbdf4fb2ba67bed6d01395b3fd0abe7b
azure-sdk <azuresdk@microsoft.com>
2023-05-25 12:18:09 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_30.0.0

---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples-python/capacityReservationExamples/CapacityReservationGroup_ListBySubscription.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python capacity_reservation_group_list_by_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.capacity_reservation_groups.list_by_subscription()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples/capacityReservationExamples/CapacityReservationGroup_ListBySubscription.json
+if __name__ == "__main__":
+    main()

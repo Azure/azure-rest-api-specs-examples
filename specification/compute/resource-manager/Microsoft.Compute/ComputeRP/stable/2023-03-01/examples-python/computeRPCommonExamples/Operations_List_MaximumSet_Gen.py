08e8267fb4dec249629aba05a1763d1727f56611
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-26 20:01:02 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_30.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples-python/computeRPCommonExamples/Operations_List_MaximumSet_Gen.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python operations_list_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.operations.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples/computeRPCommonExamples/Operations_List_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()
---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples-python/dedicatedHostExamples/DedicatedHostGroup_ListByResourceGroup_MaximumSet_Gen.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python dedicated_host_group_list_by_resource_group_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.dedicated_host_groups.list_by_resource_group(
+        resource_group_name="rgcompute",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples/dedicatedHostExamples/DedicatedHostGroup_ListByResourceGroup_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

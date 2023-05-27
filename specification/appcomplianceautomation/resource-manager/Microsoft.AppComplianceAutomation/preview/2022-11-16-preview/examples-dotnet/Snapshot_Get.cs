3dea1df560807993d204833c885a6e9f6b012bb6
azure-sdk <azuresdk@microsoft.com>
2023-05-27 12:14:29 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.AppComplianceAutomation_1.0.0-beta.3

--- specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-dotnet/Snapshot_Get.cs
+++ specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-dotnet/Snapshot_Get.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.AppComplianceAutomation;
-using Azure.ResourceManager.AppComplianceAutomation.Models;
 
 // Generated from example definition: specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples/Snapshot_Get.json
 // this example is just showing the usage of "Snapshot_Get" operation, for the dependent resources, they will have to be created separately.
@@ -15,18 +13,17 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SnapshotResource created on azure
-// for more information of creating SnapshotResource, please refer to the document of SnapshotResource
+// this example assumes you already have this ReportResource created on azure
+// for more information of creating ReportResource, please refer to the document of ReportResource
 string reportName = "testReportName";
-string snapshotName = "testSnapshot";
-ResourceIdentifier snapshotResourceId = SnapshotResource.CreateResourceIdentifier(reportName, snapshotName);
-SnapshotResource snapshotResource = client.GetSnapshotResource(snapshotResourceId);
+ResourceIdentifier reportResourceId = ReportResource.CreateResourceIdentifier(reportName);
+ReportResource reportResource = client.GetReportResource(reportResourceId);
+
+// get the collection of this SnapshotResource
+SnapshotResourceCollection collection = reportResource.GetSnapshotResources();
 
 // invoke the operation
-SnapshotResource result = await snapshotResource.GetAsync();
+string snapshotName = "testSnapshot";
+bool result = await collection.ExistsAsync(snapshotName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-SnapshotResourceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

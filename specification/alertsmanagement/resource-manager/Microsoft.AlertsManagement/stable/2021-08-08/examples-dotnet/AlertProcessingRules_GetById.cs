3fba575fbd6f2ca63724b7a4e73eaab49d914de5
azure-sdk <azuresdk@microsoft.com>
2023-05-27 12:17:03 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.AlertsManagement_1.1.0-beta.1

--- specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/stable/2021-08-08/examples-dotnet/AlertProcessingRules_GetById.cs
+++ specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/stable/2021-08-08/examples-dotnet/AlertProcessingRules_GetById.cs
@@ -16,18 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this AlertProcessingRuleResource created on azure
+// for more information of creating AlertProcessingRuleResource, please refer to the document of AlertProcessingRuleResource
 string subscriptionId = "1e3ff1c0-771a-4119-a03b-be82a51e232d";
 string resourceGroupName = "alertscorrelationrg";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this AlertProcessingRuleResource
-AlertProcessingRuleCollection collection = resourceGroupResource.GetAlertProcessingRules();
+string alertProcessingRuleName = "DailySuppression";
+ResourceIdentifier alertProcessingRuleResourceId = AlertProcessingRuleResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, alertProcessingRuleName);
+AlertProcessingRuleResource alertProcessingRule = client.GetAlertProcessingRuleResource(alertProcessingRuleResourceId);
 
 // invoke the operation
-string alertProcessingRuleName = "DailySuppression";
-bool result = await collection.ExistsAsync(alertProcessingRuleName);
+AlertProcessingRuleResource result = await alertProcessingRule.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+AlertProcessingRuleData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

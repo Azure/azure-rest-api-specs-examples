f3e153d79f52d370b4ddcc0705085ceeaa835e6c
azure-sdk <azuresdk@microsoft.com>
2023-05-29 12:43:59 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Communication_1.2.0-beta.1

--- specification/communication/resource-manager/Microsoft.Communication/stable/2023-03-31/examples-dotnet/emailServices/get.cs
+++ specification/communication/resource-manager/Microsoft.Communication/stable/2023-03-31/examples-dotnet/emailServices/get.cs
@@ -5,7 +5,6 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Communication;
-using Azure.ResourceManager.Communication.Models;
 using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/communication/resource-manager/Microsoft.Communication/stable/2023-03-31/examples/emailServices/get.json
@@ -16,19 +15,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this EmailServiceResource created on azure
-// for more information of creating EmailServiceResource, please refer to the document of EmailServiceResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "11112222-3333-4444-5555-666677778888";
 string resourceGroupName = "MyResourceGroup";
-string emailServiceName = "MyEmailServiceResource";
-ResourceIdentifier emailServiceResourceId = EmailServiceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, emailServiceName);
-EmailServiceResource emailServiceResource = client.GetEmailServiceResource(emailServiceResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this EmailServiceResource
+EmailServiceResourceCollection collection = resourceGroupResource.GetEmailServiceResources();
 
 // invoke the operation
-EmailServiceResource result = await emailServiceResource.GetAsync();
+string emailServiceName = "MyEmailServiceResource";
+bool result = await collection.ExistsAsync(emailServiceName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-EmailServiceResourceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

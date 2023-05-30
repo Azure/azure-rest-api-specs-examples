e19718ef57bb4c193ea004340f01835842caabd2
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-30 20:02:38 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Consumption_1.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/consumption/resource-manager/Microsoft.Consumption/stable/2021-10-01/examples-dotnet/BudgetsList.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Consumption;
+
+// Generated from example definition: specification/consumption/resource-manager/Microsoft.Consumption/stable/2021-10-01/examples/BudgetsList.json
+// this example is just showing the usage of "Budgets_List" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ArmResource created on azure
+// for more information of creating ArmResource, please refer to the document of ArmResource
+
+// get the collection of this ConsumptionBudgetResource
+string scope = "subscriptions/00000000-0000-0000-0000-000000000000";
+ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", scope));
+ConsumptionBudgetCollection collection = client.GetConsumptionBudgets(scopeId);
+
+// invoke the operation and iterate over the result
+await foreach (ConsumptionBudgetResource item in collection.GetAllAsync())
+{
+    // the variable item is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ConsumptionBudgetData resourceData = item.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
+
+Console.WriteLine($"Succeeded");

c737e8744c18e09c2e7530f59bc4b0d964e8b898
azure-sdk <azuresdk@microsoft.com>
2023-05-30 12:40:38 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ArcScVmm_1.0.0-beta.3

--- specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples-dotnet/GetVMMServer.cs
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples-dotnet/GetVMMServer.cs
@@ -7,7 +7,6 @@ using Azure.ResourceManager;
 using Azure.ResourceManager.ArcScVmm;
 using Azure.ResourceManager.ArcScVmm.Models;
 using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.Resources.Models;
 
 // Generated from example definition: specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples/GetVMMServer.json
 // this example is just showing the usage of "VmmServers_Get" operation, for the dependent resources, they will have to be created separately.
@@ -17,18 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this ScVmmServerResource created on azure
+// for more information of creating ScVmmServerResource, please refer to the document of ScVmmServerResource
 string subscriptionId = "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
 string resourceGroupName = "testrg";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this ScVmmServerResource
-ScVmmServerCollection collection = resourceGroupResource.GetScVmmServers();
+string vmmServerName = "ContosoVMMServer";
+ResourceIdentifier scVmmServerResourceId = ScVmmServerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, vmmServerName);
+ScVmmServerResource scVmmServer = client.GetScVmmServerResource(scVmmServerResourceId);
 
 // invoke the operation
-string vmmServerName = "ContosoVMMServer";
-bool result = await collection.ExistsAsync(vmmServerName);
+ScVmmServerResource result = await scVmmServer.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ScVmmServerData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

f80e7d5b41ca51c6e495a1627c49b63e4bd5fbae
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-30 20:03:07 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DataLakeAnalytics_1.1.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples-dotnet/ComputePolicies_Get.cs
+++ specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/stable/2016-11-01/examples-dotnet/ComputePolicies_Get.cs
@@ -15,20 +15,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DataLakeAnalyticsComputePolicyResource created on azure
-// for more information of creating DataLakeAnalyticsComputePolicyResource, please refer to the document of DataLakeAnalyticsComputePolicyResource
+// this example assumes you already have this DataLakeAnalyticsAccountResource created on azure
+// for more information of creating DataLakeAnalyticsAccountResource, please refer to the document of DataLakeAnalyticsAccountResource
 string subscriptionId = "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
 string resourceGroupName = "contosorg";
 string accountName = "contosoadla";
-string computePolicyName = "test_policy";
-ResourceIdentifier dataLakeAnalyticsComputePolicyResourceId = DataLakeAnalyticsComputePolicyResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, computePolicyName);
-DataLakeAnalyticsComputePolicyResource dataLakeAnalyticsComputePolicy = client.GetDataLakeAnalyticsComputePolicyResource(dataLakeAnalyticsComputePolicyResourceId);
+ResourceIdentifier dataLakeAnalyticsAccountResourceId = DataLakeAnalyticsAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
+DataLakeAnalyticsAccountResource dataLakeAnalyticsAccount = client.GetDataLakeAnalyticsAccountResource(dataLakeAnalyticsAccountResourceId);
+
+// get the collection of this DataLakeAnalyticsComputePolicyResource
+DataLakeAnalyticsComputePolicyCollection collection = dataLakeAnalyticsAccount.GetDataLakeAnalyticsComputePolicies();
 
 // invoke the operation
-DataLakeAnalyticsComputePolicyResource result = await dataLakeAnalyticsComputePolicy.GetAsync();
+string computePolicyName = "test_policy";
+bool result = await collection.ExistsAsync(computePolicyName);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-DataLakeAnalyticsComputePolicyData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

38b8af3e39236ebf9be78b19cb18984f682a29b3
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-05-31 20:01:24 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ArcScVmm_1.0.0-beta.3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples-dotnet/GetVirtualNetwork.cs
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples-dotnet/GetVirtualNetwork.cs
@@ -5,8 +5,8 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.ArcScVmm;
+using Azure.ResourceManager.ArcScVmm.Models;
 using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.Resources.Models;
 
 // Generated from example definition: specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples/GetVirtualNetwork.json
 // this example is just showing the usage of "VirtualNetworks_Get" operation, for the dependent resources, they will have to be created separately.
@@ -16,18 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this ScVmmVirtualNetworkResource created on azure
+// for more information of creating ScVmmVirtualNetworkResource, please refer to the document of ScVmmVirtualNetworkResource
 string subscriptionId = "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
 string resourceGroupName = "testrg";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this ScVmmVirtualNetworkResource
-ScVmmVirtualNetworkCollection collection = resourceGroupResource.GetScVmmVirtualNetworks();
+string virtualNetworkName = "HRVirtualNetwork";
+ResourceIdentifier scVmmVirtualNetworkResourceId = ScVmmVirtualNetworkResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, virtualNetworkName);
+ScVmmVirtualNetworkResource scVmmVirtualNetwork = client.GetScVmmVirtualNetworkResource(scVmmVirtualNetworkResourceId);
 
 // invoke the operation
-string virtualNetworkName = "HRVirtualNetwork";
-bool result = await collection.ExistsAsync(virtualNetworkName);
+ScVmmVirtualNetworkResource result = await scVmmVirtualNetwork.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ScVmmVirtualNetworkData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

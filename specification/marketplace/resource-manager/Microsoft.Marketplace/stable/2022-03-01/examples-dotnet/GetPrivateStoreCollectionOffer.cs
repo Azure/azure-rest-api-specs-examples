4c732a7ea2a7a79eda0a06c254cf173151b57ca2
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-01 20:02:08 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Marketplace_1.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples-dotnet/GetPrivateStoreCollectionOffer.cs
+++ specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples-dotnet/GetPrivateStoreCollectionOffer.cs
@@ -5,7 +5,6 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Marketplace;
-using Azure.ResourceManager.Marketplace.Models;
 
 // Generated from example definition: specification/marketplace/resource-manager/Microsoft.Marketplace/stable/2022-03-01/examples/GetPrivateStoreCollectionOffer.json
 // this example is just showing the usage of "PrivateStoreCollectionOffer_Get" operation, for the dependent resources, they will have to be created separately.
@@ -15,19 +14,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this PrivateStoreOfferResource created on azure
-// for more information of creating PrivateStoreOfferResource, please refer to the document of PrivateStoreOfferResource
+// this example assumes you already have this PrivateStoreCollectionInfoResource created on azure
+// for more information of creating PrivateStoreCollectionInfoResource, please refer to the document of PrivateStoreCollectionInfoResource
 Guid privateStoreId = Guid.Parse("a0e28e55-90c4-41d8-8e34-bb7ef7775406");
 Guid collectionId = Guid.Parse("56a1a02d-8cf8-45df-bf37-d5f7120fcb3d");
-string offerId = "marketplacetestthirdparty.md-test-third-party-2";
-ResourceIdentifier privateStoreOfferResourceId = PrivateStoreOfferResource.CreateResourceIdentifier(privateStoreId, collectionId, offerId);
-PrivateStoreOfferResource privateStoreOffer = client.GetPrivateStoreOfferResource(privateStoreOfferResourceId);
+ResourceIdentifier privateStoreCollectionInfoResourceId = PrivateStoreCollectionInfoResource.CreateResourceIdentifier(privateStoreId, collectionId);
+PrivateStoreCollectionInfoResource privateStoreCollectionInfo = client.GetPrivateStoreCollectionInfoResource(privateStoreCollectionInfoResourceId);
+
+// get the collection of this PrivateStoreOfferResource
+PrivateStoreOfferCollection collection = privateStoreCollectionInfo.GetPrivateStoreOffers();
 
 // invoke the operation
-PrivateStoreOfferResource result = await privateStoreOffer.GetAsync();
+string offerId = "marketplacetestthirdparty.md-test-third-party-2";
+bool result = await collection.ExistsAsync(offerId);
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-PrivateStoreOfferData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+Console.WriteLine($"Succeeded: {result}");

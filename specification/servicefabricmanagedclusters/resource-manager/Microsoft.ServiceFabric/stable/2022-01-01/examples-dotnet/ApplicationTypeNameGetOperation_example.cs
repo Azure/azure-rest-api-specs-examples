e83bdccab70132fdacc45851dad67dd320aba48e
azure-sdk <azuresdk@microsoft.com>
2023-06-01 12:33:46 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceFabricManagedClusters_1.1.0-beta.1

--- specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/stable/2022-01-01/examples-dotnet/ApplicationTypeNameGetOperation_example.cs
+++ specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/stable/2022-01-01/examples-dotnet/ApplicationTypeNameGetOperation_example.cs
@@ -5,6 +5,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.ServiceFabricManagedClusters;
+using Azure.ResourceManager.ServiceFabricManagedClusters.Models;
 
 // Generated from example definition: specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/stable/2022-01-01/examples/ApplicationTypeNameGetOperation_example.json
 // this example is just showing the usage of "ApplicationTypes_Get" operation, for the dependent resources, they will have to be created separately.
@@ -14,19 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ServiceFabricManagedClusterResource created on azure
-// for more information of creating ServiceFabricManagedClusterResource, please refer to the document of ServiceFabricManagedClusterResource
+// this example assumes you already have this ServiceFabricManagedApplicationTypeResource created on azure
+// for more information of creating ServiceFabricManagedApplicationTypeResource, please refer to the document of ServiceFabricManagedApplicationTypeResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "resRg";
 string clusterName = "myCluster";
-ResourceIdentifier serviceFabricManagedClusterResourceId = ServiceFabricManagedClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName);
-ServiceFabricManagedClusterResource serviceFabricManagedCluster = client.GetServiceFabricManagedClusterResource(serviceFabricManagedClusterResourceId);
-
-// get the collection of this ServiceFabricManagedApplicationTypeResource
-ServiceFabricManagedApplicationTypeCollection collection = serviceFabricManagedCluster.GetServiceFabricManagedApplicationTypes();
+string applicationTypeName = "myAppType";
+ResourceIdentifier serviceFabricManagedApplicationTypeResourceId = ServiceFabricManagedApplicationTypeResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName, applicationTypeName);
+ServiceFabricManagedApplicationTypeResource serviceFabricManagedApplicationType = client.GetServiceFabricManagedApplicationTypeResource(serviceFabricManagedApplicationTypeResourceId);
 
 // invoke the operation
-string applicationTypeName = "myAppType";
-bool result = await collection.ExistsAsync(applicationTypeName);
+ServiceFabricManagedApplicationTypeResource result = await serviceFabricManagedApplicationType.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ServiceFabricManagedApplicationTypeData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

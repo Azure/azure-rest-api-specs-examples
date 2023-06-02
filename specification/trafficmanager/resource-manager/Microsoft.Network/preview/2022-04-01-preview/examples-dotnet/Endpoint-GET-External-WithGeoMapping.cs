01a8c773428b17bb16019656c866cb824b0d29a2
azure-sdk <azuresdk@microsoft.com>
2023-06-02 12:27:16 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.TrafficManager_1.1.0-beta.3

--- specification/trafficmanager/resource-manager/Microsoft.Network/preview/2022-04-01-preview/examples-dotnet/Endpoint-GET-External-WithGeoMapping.cs
+++ specification/trafficmanager/resource-manager/Microsoft.Network/preview/2022-04-01-preview/examples-dotnet/Endpoint-GET-External-WithGeoMapping.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Net;
 using System.Threading.Tasks;
 using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.TrafficManager;
-using Azure.ResourceManager.TrafficManager.Models;
 
 // Generated from example definition: specification/trafficmanager/resource-manager/Microsoft.Network/preview/2022-04-01-preview/examples/Endpoint-GET-External-WithGeoMapping.json
 // this example is just showing the usage of "Endpoints_Get" operation, for the dependent resources, they will have to be created separately.
@@ -16,20 +14,21 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this TrafficManagerProfileResource created on azure
-// for more information of creating TrafficManagerProfileResource, please refer to the document of TrafficManagerProfileResource
+// this example assumes you already have this TrafficManagerEndpointResource created on azure
+// for more information of creating TrafficManagerEndpointResource, please refer to the document of TrafficManagerEndpointResource
 string subscriptionId = "{subscription-id}";
 string resourceGroupName = "azuresdkfornetautoresttrafficmanager2191";
 string profileName = "azuresdkfornetautoresttrafficmanager8224";
-ResourceIdentifier trafficManagerProfileResourceId = TrafficManagerProfileResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, profileName);
-TrafficManagerProfileResource trafficManagerProfile = client.GetTrafficManagerProfileResource(trafficManagerProfileResourceId);
-
-// get the collection of this TrafficManagerEndpointResource
-TrafficManagerEndpointCollection collection = trafficManagerProfile.GetTrafficManagerEndpoints();
-
-// invoke the operation
 string endpointType = "ExternalEndpoints";
 string endpointName = "My%20external%20endpoint";
-bool result = await collection.ExistsAsync(endpointType, endpointName);
+ResourceIdentifier trafficManagerEndpointResourceId = TrafficManagerEndpointResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, profileName, endpointType, endpointName);
+TrafficManagerEndpointResource trafficManagerEndpoint = client.GetTrafficManagerEndpointResource(trafficManagerEndpointResourceId);
 
-Console.WriteLine($"Succeeded: {result}");
+// invoke the operation
+TrafficManagerEndpointResource result = await trafficManagerEndpoint.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+TrafficManagerEndpointData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
--- specification/trafficmanager/resource-manager/Microsoft.Network/preview/2022-04-01-preview/examples-dotnet/Profile-GET-WithEndpoints.cs
+++ specification/trafficmanager/resource-manager/Microsoft.Network/preview/2022-04-01-preview/examples-dotnet/Profile-GET-WithEndpoints.cs
@@ -16,18 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this TrafficManagerProfileResource created on azure
+// for more information of creating TrafficManagerProfileResource, please refer to the document of TrafficManagerProfileResource
 string subscriptionId = "{subscription-id}";
 string resourceGroupName = "azuresdkfornetautoresttrafficmanager1323";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this TrafficManagerProfileResource
-TrafficManagerProfileCollection collection = resourceGroupResource.GetTrafficManagerProfiles();
+string profileName = "azuresdkfornetautoresttrafficmanager3880";
+ResourceIdentifier trafficManagerProfileResourceId = TrafficManagerProfileResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, profileName);
+TrafficManagerProfileResource trafficManagerProfile = client.GetTrafficManagerProfileResource(trafficManagerProfileResourceId);
 
 // invoke the operation
-string profileName = "azuresdkfornetautoresttrafficmanager3880";
-bool result = await collection.ExistsAsync(profileName);
+TrafficManagerProfileResource result = await trafficManagerProfile.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+TrafficManagerProfileData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

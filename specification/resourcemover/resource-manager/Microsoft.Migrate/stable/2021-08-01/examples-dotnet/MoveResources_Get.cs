1dcdbd2cc7a64b5caeff826ecf391d6f830ab2f0
azure-sdk <azuresdk@microsoft.com>
2023-06-02 12:40:12 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceMover_1.1.0-beta.1

--- specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples-dotnet/MoveResources_Get.cs
+++ specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2021-08-01/examples-dotnet/MoveResources_Get.cs
@@ -15,19 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this MoverResourceSetResource created on azure
-// for more information of creating MoverResourceSetResource, please refer to the document of MoverResourceSetResource
+// this example assumes you already have this MoverResource created on azure
+// for more information of creating MoverResource, please refer to the document of MoverResource
 string subscriptionId = "subid";
 string resourceGroupName = "rg1";
 string moverResourceSetName = "movecollection1";
-ResourceIdentifier moverResourceSetResourceId = MoverResourceSetResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, moverResourceSetName);
-MoverResourceSetResource moverResourceSet = client.GetMoverResourceSetResource(moverResourceSetResourceId);
-
-// get the collection of this MoverResource
-MoverResourceCollection collection = moverResourceSet.GetMoverResources();
+string moverResourceName = "moveresourcename1";
+ResourceIdentifier moverResourceId = MoverResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, moverResourceSetName, moverResourceName);
+MoverResource moverResource = client.GetMoverResource(moverResourceId);
 
 // invoke the operation
-string moverResourceName = "moveresourcename1";
-bool result = await collection.ExistsAsync(moverResourceName);
+MoverResource result = await moverResource.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+MoverResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

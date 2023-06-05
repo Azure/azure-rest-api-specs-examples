713e11810ab1c2c7d1cda13234c3212613f954ff
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-05 20:01:51 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.IotCentral_1.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-dotnet/Apps_Get.cs
+++ specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-dotnet/Apps_Get.cs
@@ -17,18 +17,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this IotCentralAppResource created on azure
+// for more information of creating IotCentralAppResource, please refer to the document of IotCentralAppResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "resRg";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this IotCentralAppResource
-IotCentralAppCollection collection = resourceGroupResource.GetIotCentralApps();
+string resourceName = "myIoTCentralApp";
+ResourceIdentifier iotCentralAppResourceId = IotCentralAppResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
+IotCentralAppResource iotCentralApp = client.GetIotCentralAppResource(iotCentralAppResourceId);
 
 // invoke the operation
-string resourceName = "myIoTCentralApp";
-bool result = await collection.ExistsAsync(resourceName);
+IotCentralAppResource result = await iotCentralApp.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+IotCentralAppData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

b6dc489793a02c3d0264f551693533c9f38dce05
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-08 20:01:34 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DataShare_1.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/datashare/resource-manager/Microsoft.DataShare/stable/2021-08-01/examples-dotnet/Shares_Create.cs
+++ specification/datashare/resource-manager/Microsoft.DataShare/stable/2021-08-01/examples-dotnet/Shares_Create.cs
@@ -15,26 +15,23 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DataShareAccountResource created on azure
-// for more information of creating DataShareAccountResource, please refer to the document of DataShareAccountResource
+// this example assumes you already have this DataShareResource created on azure
+// for more information of creating DataShareResource, please refer to the document of DataShareResource
 string subscriptionId = "12345678-1234-1234-12345678abc";
 string resourceGroupName = "SampleResourceGroup";
 string accountName = "Account1";
-ResourceIdentifier dataShareAccountResourceId = DataShareAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-DataShareAccountResource dataShareAccount = client.GetDataShareAccountResource(dataShareAccountResourceId);
-
-// get the collection of this DataShareResource
-DataShareCollection collection = dataShareAccount.GetDataShares();
+string shareName = "Share1";
+ResourceIdentifier dataShareResourceId = DataShareResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, shareName);
+DataShareResource dataShare = client.GetDataShareResource(dataShareResourceId);
 
 // invoke the operation
-string shareName = "Share1";
 DataShareData data = new DataShareData()
 {
     Description = "share description",
     ShareKind = DataShareKind.CopyBased,
     Terms = "Confidential",
 };
-ArmOperation<DataShareResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, shareName, data);
+ArmOperation<DataShareResource> lro = await dataShare.UpdateAsync(WaitUntil.Completed, data);
 DataShareResource result = lro.Value;
 
 // the variable result is a resource, you could call other operations on this instance as well
--- specification/datashare/resource-manager/Microsoft.DataShare/stable/2021-08-01/examples-dotnet/Shares_Get.cs
+++ specification/datashare/resource-manager/Microsoft.DataShare/stable/2021-08-01/examples-dotnet/Shares_Get.cs
@@ -15,19 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DataShareAccountResource created on azure
-// for more information of creating DataShareAccountResource, please refer to the document of DataShareAccountResource
+// this example assumes you already have this DataShareResource created on azure
+// for more information of creating DataShareResource, please refer to the document of DataShareResource
 string subscriptionId = "12345678-1234-1234-12345678abc";
 string resourceGroupName = "SampleResourceGroup";
 string accountName = "Account1";
-ResourceIdentifier dataShareAccountResourceId = DataShareAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-DataShareAccountResource dataShareAccount = client.GetDataShareAccountResource(dataShareAccountResourceId);
-
-// get the collection of this DataShareResource
-DataShareCollection collection = dataShareAccount.GetDataShares();
+string shareName = "Share1";
+ResourceIdentifier dataShareResourceId = DataShareResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, shareName);
+DataShareResource dataShare = client.GetDataShareResource(dataShareResourceId);
 
 // invoke the operation
-string shareName = "Share1";
-bool result = await collection.ExistsAsync(shareName);
+DataShareResource result = await dataShare.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DataShareData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

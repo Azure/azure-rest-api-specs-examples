42ed36af06aaa897aa731fcc60ac3e19e1507cfe
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-08 20:01:57 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DeviceUpdate_1.0.0-beta.9
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/stable/2022-10-01/examples-dotnet/Instances/Instances_Get.cs
+++ specification/deviceupdate/resource-manager/Microsoft.DeviceUpdate/stable/2022-10-01/examples-dotnet/Instances/Instances_Get.cs
@@ -15,19 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DeviceUpdateAccountResource created on azure
-// for more information of creating DeviceUpdateAccountResource, please refer to the document of DeviceUpdateAccountResource
+// this example assumes you already have this DeviceUpdateInstanceResource created on azure
+// for more information of creating DeviceUpdateInstanceResource, please refer to the document of DeviceUpdateInstanceResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "test-rg";
 string accountName = "contoso";
-ResourceIdentifier deviceUpdateAccountResourceId = DeviceUpdateAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-DeviceUpdateAccountResource deviceUpdateAccount = client.GetDeviceUpdateAccountResource(deviceUpdateAccountResourceId);
-
-// get the collection of this DeviceUpdateInstanceResource
-DeviceUpdateInstanceCollection collection = deviceUpdateAccount.GetDeviceUpdateInstances();
+string instanceName = "blue";
+ResourceIdentifier deviceUpdateInstanceResourceId = DeviceUpdateInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, instanceName);
+DeviceUpdateInstanceResource deviceUpdateInstance = client.GetDeviceUpdateInstanceResource(deviceUpdateInstanceResourceId);
 
 // invoke the operation
-string instanceName = "blue";
-bool result = await collection.ExistsAsync(instanceName);
+DeviceUpdateInstanceResource result = await deviceUpdateInstance.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DeviceUpdateInstanceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

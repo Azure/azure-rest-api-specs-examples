f783a3b80b33bf8a1b4f08c1bc5d8cb88cbd89b7
azure-sdk <azuresdk@microsoft.com>
2023-06-20 12:16:43 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dataprotection_1.1.0

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples-js/ResourceGuardProxyCRUD/GetResourceGuardProxy.js
@@ -0,0 +1,24 @@
+const { DataProtectionClient } = require("@azure/arm-dataprotection");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Returns the ResourceGuardProxy object associated with the vault, and that matches the name in the request
+ *
+ * @summary Returns the ResourceGuardProxy object associated with the vault, and that matches the name in the request
+ * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples/ResourceGuardProxyCRUD/GetResourceGuardProxy.json
+ */
+async function getResourceGuardProxy() {
+  const subscriptionId =
+    process.env["DATAPROTECTION_SUBSCRIPTION_ID"] || "5e13b949-1218-4d18-8b99-7e12155ec4f7";
+  const resourceGroupName = process.env["DATAPROTECTION_RESOURCE_GROUP"] || "SampleResourceGroup";
+  const vaultName = "sampleVault";
+  const resourceGuardProxyName = "swaggerExample";
+  const credential = new DefaultAzureCredential();
+  const client = new DataProtectionClient(credential, subscriptionId);
+  const result = await client.dppResourceGuardProxy.get(
+    resourceGroupName,
+    vaultName,
+    resourceGuardProxyName
+  );
+  console.log(result);
+}
---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples-js/ResourceGuardProxyCRUD/ListResourceGuardProxy.js
@@ -0,0 +1,22 @@
+const { DataProtectionClient } = require("@azure/arm-dataprotection");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Returns the list of ResourceGuardProxies associated with the vault
+ *
+ * @summary Returns the list of ResourceGuardProxies associated with the vault
+ * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples/ResourceGuardProxyCRUD/ListResourceGuardProxy.json
+ */
+async function getResourceGuardProxies() {
+  const subscriptionId =
+    process.env["DATAPROTECTION_SUBSCRIPTION_ID"] || "5e13b949-1218-4d18-8b99-7e12155ec4f7";
+  const resourceGroupName = process.env["DATAPROTECTION_RESOURCE_GROUP"] || "SampleResourceGroup";
+  const vaultName = "sampleVault";
+  const credential = new DefaultAzureCredential();
+  const client = new DataProtectionClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.dppResourceGuardProxy.list(resourceGroupName, vaultName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

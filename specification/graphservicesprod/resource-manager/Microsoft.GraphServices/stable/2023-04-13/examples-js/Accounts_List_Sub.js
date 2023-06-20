4168b398c1e229c857040fdd5fc71f8e3eefcc49
azure-sdk <azuresdk@microsoft.com>
2023-06-20 12:19:16 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-graphservices_1.0.0

---
+++ specification/graphservicesprod/resource-manager/Microsoft.GraphServices/stable/2023-04-13/examples-js/Accounts_List_Sub.js
@@ -0,0 +1,20 @@
+const { GraphServices } = require("@azure/arm-graphservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Returns list of accounts belonging to a subscription.
+ *
+ * @summary Returns list of accounts belonging to a subscription.
+ * x-ms-original-file: specification/graphservicesprod/resource-manager/Microsoft.GraphServices/stable/2023-04-13/examples/Accounts_List_Sub.json
+ */
+async function getListOfAccountsBySubscription() {
+  const subscriptionId =
+    process.env["GRAPHSERVICES_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new GraphServices(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.accounts.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

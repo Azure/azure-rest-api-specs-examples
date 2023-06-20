0a676ea1debfcf1a17f8fc0f5b3d88454dd9b054
azure-sdk <azuresdk@microsoft.com>
2023-06-20 12:06:17 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-dataprotection_1.0.0-beta.4

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples-java/ResourceGuardProxyCRUD/GetResourceGuardProxy.java
@@ -0,0 +1,16 @@
+/** Samples for DppResourceGuardProxy Get. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples/ResourceGuardProxyCRUD/GetResourceGuardProxy.json
+     */
+    /**
+     * Sample code: Get ResourceGuardProxy.
+     *
+     * @param manager Entry point to DataProtectionManager.
+     */
+    public static void getResourceGuardProxy(com.azure.resourcemanager.dataprotection.DataProtectionManager manager) {
+        manager
+            .dppResourceGuardProxies()
+            .getWithResponse("SampleResourceGroup", "sampleVault", "swaggerExample", com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples-java/ResourceGuardProxyCRUD/PutResourceGuardProxy.java
@@ -0,0 +1,25 @@
+import com.azure.resourcemanager.dataprotection.models.ResourceGuardProxyBase;
+
+/** Samples for DppResourceGuardProxy CreateOrUpdate. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples/ResourceGuardProxyCRUD/PutResourceGuardProxy.json
+     */
+    /**
+     * Sample code: Create ResourceGuardProxy.
+     *
+     * @param manager Entry point to DataProtectionManager.
+     */
+    public static void createResourceGuardProxy(
+        com.azure.resourcemanager.dataprotection.DataProtectionManager manager) {
+        manager
+            .dppResourceGuardProxies()
+            .define("swaggerExample")
+            .withExistingBackupVault("SampleResourceGroup", "sampleVault")
+            .withProperties(
+                new ResourceGuardProxyBase()
+                    .withResourceGuardResourceId(
+                        "/subscriptions/f9e67185-f313-4e79-aa71-6458d429369d/resourceGroups/ResourceGuardSecurityAdminRG/providers/Microsoft.DataProtection/resourceGuards/ResourceGuardTestResource"))
+            .create();
+    }
+}

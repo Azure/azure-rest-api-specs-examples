3854dc44bdd8a2826e6744b8c6493bcdebda12c6
azure-sdk <azuresdk@microsoft.com>
2023-06-20 12:20:08 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples-js/AgentPoolsCreate_GPUMIG.js
@@ -0,0 +1,52 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates an agent pool in the specified managed cluster.
+ *
+ * @summary Creates or updates an agent pool in the specified managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples/AgentPoolsCreate_GPUMIG.json
+ */
+async function createAgentPoolWithGpumig() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const agentPoolName = "agentpool1";
+  const parameters = {
+    count: 3,
+    gpuInstanceProfile: "MIG2g",
+    kubeletConfig: {
+      allowedUnsafeSysctls: ["kernel.msg*", "net.core.somaxconn"],
+      cpuCfsQuota: true,
+      cpuCfsQuotaPeriod: "200ms",
+      cpuManagerPolicy: "static",
+      failSwapOn: false,
+      imageGcHighThreshold: 90,
+      imageGcLowThreshold: 70,
+      topologyManagerPolicy: "best-effort",
+    },
+    linuxOSConfig: {
+      swapFileSizeMB: 1500,
+      sysctls: {
+        kernelThreadsMax: 99999,
+        netCoreWmemDefault: 12345,
+        netIpv4IpLocalPortRange: "20000 60000",
+        netIpv4TcpTwReuse: true,
+      },
+      transparentHugePageDefrag: "madvise",
+      transparentHugePageEnabled: "always",
+    },
+    orchestratorVersion: "",
+    osType: "Linux",
+    vmSize: "Standard_ND96asr_v4",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.agentPools.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    resourceName,
+    agentPoolName,
+    parameters
+  );
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples-js/SnapshotsDelete.js
@@ -0,0 +1,18 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a snapshot.
+ *
+ * @summary Deletes a snapshot.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples/SnapshotsDelete.json
+ */
+async function deleteSnapshot() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "snapshot1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.snapshots.delete(resourceGroupName, resourceName);
+  console.log(result);
+}

b65f1be16bc5406fd37a132f80c748c7080d8352
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-21 20:03:45 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/storagecache/armstoragecache/v3.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-05-01/examples-go/AscOperations_Get.go
@@ -0,0 +1,36 @@
+package armstoragecache_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storagecache/armstoragecache/v3"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/e2b5f9323c4214408969a6e953b4075cfdc693b6/specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-05-01/examples/AscOperations_Get.json
+func ExampleAscOperationsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armstoragecache.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewAscOperationsClient().Get(ctx, "westus", "testoperationid", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AscOperation = armstoragecache.AscOperation{
+	// 	Name: to.Ptr("testoperationid"),
+	// 	EndTime: to.Ptr("2023-01-01T16:13:13.933Z"),
+	// 	ID: to.Ptr("/subscriptions/id/locations/westus/ascOperations/testoperationid"),
+	// 	StartTime: to.Ptr("2023-01-01T13:13:13.933Z"),
+	// 	Status: to.Ptr("Succeeded"),
+	// }
+}
---
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-05-01/examples-go/SpaceAllocation_Post.go
@@ -0,0 +1,44 @@
+package armstoragecache_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storagecache/armstoragecache/v3"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/e2b5f9323c4214408969a6e953b4075cfdc693b6/specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-05-01/examples/SpaceAllocation_Post.json
+func ExampleCachesClient_BeginSpaceAllocation() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armstoragecache.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewCachesClient().BeginSpaceAllocation(ctx, "scgroup", "sc1", &armstoragecache.CachesClientBeginSpaceAllocationOptions{SpaceAllocation: []*armstoragecache.StorageTargetSpaceAllocation{
+		{
+			Name:                 to.Ptr("st1"),
+			AllocationPercentage: to.Ptr[int32](25),
+		},
+		{
+			Name:                 to.Ptr("st2"),
+			AllocationPercentage: to.Ptr[int32](50),
+		},
+		{
+			Name:                 to.Ptr("st3"),
+			AllocationPercentage: to.Ptr[int32](25),
+		}},
+	})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

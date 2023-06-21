d707ca9fcef0bf1d8c648e046feaf6be4e80d673
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-21 20:02:37 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dataprotection_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples-js/ResourceGuardProxyCRUD/DeleteResourceGuardProxy.js
@@ -0,0 +1,24 @@
+const { DataProtectionClient } = require("@azure/arm-dataprotection");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes the ResourceGuardProxy
+ *
+ * @summary Deletes the ResourceGuardProxy
+ * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples/ResourceGuardProxyCRUD/DeleteResourceGuardProxy.json
+ */
+async function deleteResourceGuardProxy() {
+  const subscriptionId =
+    process.env["DATAPROTECTION_SUBSCRIPTION_ID"] || "5e13b949-1218-4d18-8b99-7e12155ec4f7";
+  const resourceGroupName = process.env["DATAPROTECTION_RESOURCE_GROUP"] || "SampleResourceGroup";
+  const vaultName = "sampleVault";
+  const resourceGuardProxyName = "swaggerExample";
+  const credential = new DefaultAzureCredential();
+  const client = new DataProtectionClient(credential, subscriptionId);
+  const result = await client.dppResourceGuardProxy.delete(
+    resourceGroupName,
+    vaultName,
+    resourceGuardProxyName
+  );
+  console.log(result);
+}

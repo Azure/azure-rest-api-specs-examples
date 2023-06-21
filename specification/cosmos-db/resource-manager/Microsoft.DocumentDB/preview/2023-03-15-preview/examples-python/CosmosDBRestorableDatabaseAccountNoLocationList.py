5a1dcb6ba1eb494ce5cfe895dddcaccf8772cc64
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:23:00 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_10.0.0b1

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-15-preview/examples-python/CosmosDBRestorableDatabaseAccountNoLocationList.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_restorable_database_account_no_location_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.restorable_database_accounts.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-15-preview/examples/CosmosDBRestorableDatabaseAccountNoLocationList.json
+if __name__ == "__main__":
+    main()
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-15-preview/examples-python/CosmosDBSqlMaterializedViewCreateUpdate.py
@@ -0,0 +1,64 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_sql_materialized_view_create_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.sql_resources.begin_create_update_sql_container(
+        resource_group_name="rg1",
+        account_name="ddb1",
+        database_name="databaseName",
+        container_name="mvContainerName",
+        create_update_sql_container_parameters={
+            "location": "West US",
+            "properties": {
+                "options": {},
+                "resource": {
+                    "id": "mvContainerName",
+                    "indexingPolicy": {
+                        "automatic": True,
+                        "excludedPaths": [],
+                        "includedPaths": [
+                            {
+                                "indexes": [
+                                    {"dataType": "String", "kind": "Range", "precision": -1},
+                                    {"dataType": "Number", "kind": "Range", "precision": -1},
+                                ],
+                                "path": "/*",
+                            }
+                        ],
+                        "indexingMode": "consistent",
+                    },
+                    "materializedViewDefinition": {
+                        "definition": "select * from ROOT",
+                        "sourceCollectionId": "sourceContainerName",
+                    },
+                    "partitionKey": {"kind": "Hash", "paths": ["/mvpk"]},
+                },
+            },
+            "tags": {},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-15-preview/examples/CosmosDBSqlMaterializedViewCreateUpdate.json
+if __name__ == "__main__":
+    main()

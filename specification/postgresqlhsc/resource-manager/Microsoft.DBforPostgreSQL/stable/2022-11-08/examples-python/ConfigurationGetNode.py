8eca3a587e4e87626528ce82c923385341e13c29
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:26:29 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdbforpostgresql_1.0.0b1

---
+++ specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples-python/ConfigurationGetNode.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdbforpostgresql import CosmosdbForPostgresqlMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdbforpostgresql
+# USAGE
+    python configuration_get_node.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosdbForPostgresqlMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
+    )
+
+    response = client.configurations.get_node(
+        resource_group_name="TestResourceGroup",
+        cluster_name="testcluster",
+        configuration_name="array_nulls",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/ConfigurationGetNode.json
+if __name__ == "__main__":
+    main()

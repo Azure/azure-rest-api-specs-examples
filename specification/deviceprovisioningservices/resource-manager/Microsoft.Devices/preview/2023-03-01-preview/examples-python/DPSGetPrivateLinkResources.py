d80bf0294f8d85bfb843e13614f4dab1b3e58d28
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:23:51 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-iothubprovisioningservices_1.2.0b2

---
+++ specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/preview/2023-03-01-preview/examples-python/DPSGetPrivateLinkResources.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.iothubprovisioningservices import IotDpsClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-iothubprovisioningservices
+# USAGE
+    python dps_get_private_link_resources.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = IotDpsClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="91d12660-3dec-467a-be2a-213b5544ddc0",
+    )
+
+    response = client.iot_dps_resource.get_private_link_resources(
+        resource_group_name="myResourceGroup",
+        resource_name="myFirstProvisioningService",
+        group_id="iotDps",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/preview/2023-03-01-preview/examples/DPSGetPrivateLinkResources.json
+if __name__ == "__main__":
+    main()

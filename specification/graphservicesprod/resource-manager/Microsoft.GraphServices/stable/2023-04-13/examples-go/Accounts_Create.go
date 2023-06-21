1c4e7b104f04e5e4ab638c77058b020948784f60
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-21 20:03:22 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/graphservices/armgraphservices/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/graphservicesprod/resource-manager/Microsoft.GraphServices/stable/2023-04-13/examples-go/Accounts_Create.go
@@ -0,0 +1,52 @@
+package armgraphservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/graphservices/armgraphservices"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0d41e635294dce73dfa99b07f3da4b68a9c9e29c/specification/graphservicesprod/resource-manager/Microsoft.GraphServices/stable/2023-04-13/examples/Accounts_Create.json
+func ExampleAccountsClient_BeginCreateAndUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armgraphservices.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewAccountsClient().BeginCreateAndUpdate(ctx, "testResourceGroupGRAM", "11111111-aaaa-1111-bbbb-1111111111111", armgraphservices.AccountResource{
+		Properties: &armgraphservices.AccountResourceProperties{
+			AppID: to.Ptr("11111111-aaaa-1111-bbbb-111111111111"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AccountResource = armgraphservices.AccountResource{
+	// 	Name: to.Ptr("11111111-aaaa-1111-bbbb-111111111111"),
+	// 	Type: to.Ptr("Microsoft.GraphServices/accounts"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testResourceGroupGRAM/providers/Microsoft.GraphServices/accounts/11111111-aaaa-1111-bbbb-111111111111"),
+	// 	Tags: map[string]*string{
+	// 		"tag1": to.Ptr("value1"),
+	// 		"tag2": to.Ptr("value2"),
+	// 	},
+	// 	Properties: &armgraphservices.AccountResourceProperties{
+	// 		AppID: to.Ptr("11111111-aaaa-1111-bbbb-111111111111"),
+	// 		BillingPlanID: to.Ptr("11111111-aaaa-1111-bbbb-111111111111"),
+	// 		ProvisioningState: to.Ptr(armgraphservices.ProvisioningStateSucceeded),
+	// 	},
+	// }
+}
---
+++ specification/graphservicesprod/resource-manager/Microsoft.GraphServices/stable/2023-04-13/examples-go/Accounts_Delete.go
@@ -0,0 +1,26 @@
+package armgraphservices_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/graphservices/armgraphservices"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0d41e635294dce73dfa99b07f3da4b68a9c9e29c/specification/graphservicesprod/resource-manager/Microsoft.GraphServices/stable/2023-04-13/examples/Accounts_Delete.json
+func ExampleAccountsClient_Delete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armgraphservices.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	_, err = clientFactory.NewAccountsClient().Delete(ctx, "testResourceGroupGRAM", "11111111-aaaa-1111-bbbb-111111111111", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+}

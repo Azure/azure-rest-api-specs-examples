db1934f2f043f3faa8e290285ea677b630d8baf4
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:14:40 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservicefleet_1.0.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-js/UpdateRuns_Delete.js
@@ -0,0 +1,23 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Delete a UpdateRun
+ *
+ * @summary Delete a UpdateRun
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/UpdateRuns_Delete.json
+ */
+async function deleteAnUpdateRunResource() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const fleetName = "fleet1";
+  const updateRunName = "run1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const result = await client.updateRuns.beginDeleteAndWait(
+    resourceGroupName,
+    fleetName,
+    updateRunName
+  );
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-js/UpdateRuns_ListByFleet.js
@@ -0,0 +1,22 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List UpdateRun resources by Fleet
+ *
+ * @summary List UpdateRun resources by Fleet
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/UpdateRuns_ListByFleet.json
+ */
+async function listsTheUpdateRunResourcesByFleet() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const fleetName = "fleet1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.updateRuns.listByFleet(resourceGroupName, fleetName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

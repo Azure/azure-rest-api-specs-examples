6b62dc60549f756a5e203b97efb6c1a1e436a63b
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:13:49 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-recoveryservices_5.3.0

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples-js/ListBySubscriptionIds.js
@@ -0,0 +1,20 @@
+const { RecoveryServicesClient } = require("@azure/arm-recoveryservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Fetches all the resources of the specified type in the subscription.
+ *
+ * @summary Fetches all the resources of the specified type in the subscription.
+ * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples/ListBySubscriptionIds.json
+ */
+async function listOfRecoveryServicesResourcesInSubscriptionId() {
+  const subscriptionId =
+    process.env["RECOVERYSERVICES_SUBSCRIPTION_ID"] || "77777777-b0c6-47a2-b37c-d8e65a629c18";
+  const credential = new DefaultAzureCredential();
+  const client = new RecoveryServicesClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.vaults.listBySubscriptionId()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

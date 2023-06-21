d41bc1407ac5e4e06356593811eb4f0d12d2ba01
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:29:07 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-trafficmanager_1.1.0

---
+++ specification/trafficmanager/resource-manager/Microsoft.Network/stable/2022-04-01/examples-python/Profile-DELETE.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.trafficmanager import TrafficManagerManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-trafficmanager
+# USAGE
+    python profile_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TrafficManagerManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.profiles.delete(
+        resource_group_name="azuresdkfornetautoresttrafficmanager1323",
+        profile_name="azuresdkfornetautoresttrafficmanager3880",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/trafficmanager/resource-manager/Microsoft.Network/stable/2022-04-01/examples/Profile-DELETE.json
+if __name__ == "__main__":
+    main()

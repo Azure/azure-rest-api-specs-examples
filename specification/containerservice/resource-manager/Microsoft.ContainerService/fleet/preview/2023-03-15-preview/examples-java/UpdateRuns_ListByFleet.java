fc78acd975bd0424888c9af2b3ad71ff50b04c0b
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:08:49 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-containerservicefleet_1.0.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-java/UpdateRuns_ListByFleet.java
@@ -0,0 +1,15 @@
+/** Samples for UpdateRuns ListByFleet. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/UpdateRuns_ListByFleet.json
+     */
+    /**
+     * Sample code: Lists the UpdateRun resources by fleet.
+     *
+     * @param manager Entry point to ContainerServiceFleetManager.
+     */
+    public static void listsTheUpdateRunResourcesByFleet(
+        com.azure.resourcemanager.containerservicefleet.ContainerServiceFleetManager manager) {
+        manager.updateRuns().listByFleet("rg1", "fleet1", com.azure.core.util.Context.NONE);
+    }
+}

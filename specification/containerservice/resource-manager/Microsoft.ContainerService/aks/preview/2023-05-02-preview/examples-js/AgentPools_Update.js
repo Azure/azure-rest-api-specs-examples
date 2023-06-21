7e2aa652d1c4e6931e068a1d01422e088bce9bac
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-21 20:02:05 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples-js/AgentPools_Update.js
@@ -0,0 +1,36 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates an agent pool in the specified managed cluster.
+ *
+ * @summary Creates or updates an agent pool in the specified managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples/AgentPools_Update.json
+ */
+async function updateAgentPool() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const agentPoolName = "agentpool1";
+  const parameters = {
+    count: 3,
+    enableAutoScaling: true,
+    maxCount: 2,
+    minCount: 2,
+    nodeTaints: ["Key1=Value1:NoSchedule"],
+    orchestratorVersion: "",
+    osType: "Linux",
+    scaleSetEvictionPolicy: "Delete",
+    scaleSetPriority: "Spot",
+    vmSize: "Standard_DS1_v2",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.agentPools.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    resourceName,
+    agentPoolName,
+    parameters
+  );
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples-js/PrivateEndpointConnectionsDelete.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a private endpoint connection.
+ *
+ * @summary Deletes a private endpoint connection.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-05-02-preview/examples/PrivateEndpointConnectionsDelete.json
+ */
+async function deletePrivateEndpointConnection() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const privateEndpointConnectionName = "privateendpointconnection1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.privateEndpointConnections.beginDeleteAndWait(
+    resourceGroupName,
+    resourceName,
+    privateEndpointConnectionName
+  );
+  console.log(result);
+}

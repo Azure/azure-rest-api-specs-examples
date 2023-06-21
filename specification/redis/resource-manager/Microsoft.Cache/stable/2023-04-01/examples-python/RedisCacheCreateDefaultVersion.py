c1f4cbc5a9dda135350e9cfae04aff3e7c00d942
azure-sdk <azuresdk@microsoft.com>
2023-06-21 12:27:22 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-redis_14.2.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2023-04-01/examples-python/RedisCacheCreateDefaultVersion.py
@@ -0,0 +1,47 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.redis import RedisManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-redis
+# USAGE
+    python redis_cache_create_default_version.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RedisManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.redis.begin_create(
+        resource_group_name="rg1",
+        name="cache1",
+        parameters={
+            "location": "West US",
+            "properties": {
+                "enableNonSslPort": True,
+                "minimumTlsVersion": "1.2",
+                "redisConfiguration": {"maxmemory-policy": "allkeys-lru"},
+                "replicasPerPrimary": 2,
+                "shardCount": 2,
+                "sku": {"capacity": 1, "family": "P", "name": "Premium"},
+                "staticIP": "192.168.0.5",
+                "subnetId": "/subscriptions/subid/resourceGroups/rg2/providers/Microsoft.Network/virtualNetworks/network1/subnets/subnet1",
+            },
+            "zones": ["1"],
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2023-04-01/examples/RedisCacheCreateDefaultVersion.json
+if __name__ == "__main__":
+    main()

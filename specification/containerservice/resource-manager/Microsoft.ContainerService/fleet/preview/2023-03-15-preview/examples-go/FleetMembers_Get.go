8524b5adba06adb2a2e05abe00aa2b3d489c7c55
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-21 20:04:00 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservicefleet/armcontainerservicefleet/v0.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-go/FleetMembers_Get.go
@@ -0,0 +1,47 @@
+package armcontainerservicefleet_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicefleet/armcontainerservicefleet"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/de14cb8751b978b1877597b13292818e80f8c661/specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/FleetMembers_Get.json
+func ExampleFleetMembersClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicefleet.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewFleetMembersClient().Get(ctx, "rg1", "fleet1", "member-1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.FleetMember = armcontainerservicefleet.FleetMember{
+	// 	Name: to.Ptr("member-1"),
+	// 	Type: to.Ptr("Microsoft.ContainerService/fleets/members"),
+	// 	ID: to.Ptr("/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/fleets/fleet-1/members/member-1"),
+	// 	SystemData: &armcontainerservicefleet.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-23T05:40:40.657Z"); return t}()),
+	// 		CreatedBy: to.Ptr("someUser"),
+	// 		CreatedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-23T05:40:40.657Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("someOtherUser"),
+	// 		LastModifiedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 	},
+	// 	ETag: to.Ptr("kd30rkdfo49="),
+	// 	Properties: &armcontainerservicefleet.FleetMemberProperties{
+	// 		ClusterResourceID: to.Ptr("/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/managedClusters/cluster-1"),
+	// 		ProvisioningState: to.Ptr(armcontainerservicefleet.FleetMemberProvisioningStateSucceeded),
+	// 	},
+	// }
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-go/UpdateRuns_Get.go
@@ -0,0 +1,96 @@
+package armcontainerservicefleet_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicefleet/armcontainerservicefleet"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/de14cb8751b978b1877597b13292818e80f8c661/specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/UpdateRuns_Get.json
+func ExampleUpdateRunsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicefleet.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewUpdateRunsClient().Get(ctx, "rg1", "fleet1", "run1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.UpdateRun = armcontainerservicefleet.UpdateRun{
+	// 	Name: to.Ptr("run1"),
+	// 	Type: to.Ptr("Microsoft.ContainerService/fleets/updateRuns"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/fleets/myFleet/updateRuns/run1"),
+	// 	SystemData: &armcontainerservicefleet.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-03-01T01:10:08.395Z"); return t}()),
+	// 		CreatedBy: to.Ptr("@contoso.com"),
+	// 		CreatedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-03-01T01:10:08.395Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("@contoso.com"),
+	// 		LastModifiedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 	},
+	// 	ETag: to.Ptr("\"EtagValue\""),
+	// 	Properties: &armcontainerservicefleet.UpdateRunProperties{
+	// 		ManagedClusterUpdate: &armcontainerservicefleet.ManagedClusterUpdate{
+	// 			Upgrade: &armcontainerservicefleet.ManagedClusterUpgradeSpec{
+	// 				Type: to.Ptr(armcontainerservicefleet.ManagedClusterUpgradeTypeFull),
+	// 				KubernetesVersion: to.Ptr("1.26.1"),
+	// 			},
+	// 		},
+	// 		ProvisioningState: to.Ptr(armcontainerservicefleet.UpdateRunProvisioningStateSucceeded),
+	// 		Status: &armcontainerservicefleet.UpdateRunStatus{
+	// 			Stages: []*armcontainerservicefleet.UpdateStageStatus{
+	// 				{
+	// 					Name: to.Ptr("stage1"),
+	// 					AfterStageWaitStatus: &armcontainerservicefleet.WaitStatus{
+	// 						Status: &armcontainerservicefleet.UpdateStatus{
+	// 							State: to.Ptr(armcontainerservicefleet.UpdateStateNotStarted),
+	// 						},
+	// 						WaitDurationInSeconds: to.Ptr[int32](3600),
+	// 					},
+	// 					Groups: []*armcontainerservicefleet.UpdateGroupStatus{
+	// 						{
+	// 							Name: to.Ptr("group-a"),
+	// 							Members: []*armcontainerservicefleet.MemberUpdateStatus{
+	// 								{
+	// 									Name: to.Ptr("member-one"),
+	// 									ClusterResourceID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myClusters/providers/Microsoft.ContainerService/managedClusters/myCluster"),
+	// 									Status: &armcontainerservicefleet.UpdateStatus{
+	// 										State: to.Ptr(armcontainerservicefleet.UpdateStateNotStarted),
+	// 									},
+	// 							}},
+	// 							Status: &armcontainerservicefleet.UpdateStatus{
+	// 								State: to.Ptr(armcontainerservicefleet.UpdateStateNotStarted),
+	// 							},
+	// 					}},
+	// 					Status: &armcontainerservicefleet.UpdateStatus{
+	// 						State: to.Ptr(armcontainerservicefleet.UpdateStateNotStarted),
+	// 					},
+	// 			}},
+	// 			Status: &armcontainerservicefleet.UpdateStatus{
+	// 				State: to.Ptr(armcontainerservicefleet.UpdateStateNotStarted),
+	// 			},
+	// 		},
+	// 		Strategy: &armcontainerservicefleet.UpdateRunStrategy{
+	// 			Stages: []*armcontainerservicefleet.UpdateStage{
+	// 				{
+	// 					Name: to.Ptr("stage1"),
+	// 					AfterStageWaitInSeconds: to.Ptr[int32](3600),
+	// 					Groups: []*armcontainerservicefleet.UpdateGroup{
+	// 						{
+	// 							Name: to.Ptr("group-a"),
+	// 					}},
+	// 			}},
+	// 		},
+	// 	},
+	// }
+}

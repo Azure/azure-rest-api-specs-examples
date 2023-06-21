4178aa68d17b7fec589f71c9b1744fbf48b72b35
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-21 20:03:37 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/dataprotection/armdataprotection/v2.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples-go/ResourceGuardProxyCRUD/UnlockDeleteResourceGuardProxy.go
@@ -0,0 +1,37 @@
+package armdataprotection_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dataprotection/armdataprotection/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/1b33e81bbdc28fcd6644a1315b8d7b1b6d030590/specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-01-01/examples/ResourceGuardProxyCRUD/UnlockDeleteResourceGuardProxy.json
+func ExampleDppResourceGuardProxyClient_UnlockDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armdataprotection.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewDppResourceGuardProxyClient().UnlockDelete(ctx, "SampleResourceGroup", "sampleVault", "swaggerExample", armdataprotection.UnlockDeleteRequest{
+		ResourceGuardOperationRequests: []*string{
+			to.Ptr("/subscriptions/f9e67185-f313-4e79-aa71-6458d429369d/resourceGroups/ResourceGuardSecurityAdminRG/providers/Microsoft.DataProtection/resourceGuards/ResourceGuardTestResource/deleteBackupInstanceRequests/default")},
+		ResourceToBeDeleted: to.Ptr("/subscriptions/5e13b949-1218-4d18-8b99-7e12155ec4f7/resourceGroups/SampleResourceGroup/providers/Microsoft.DataProtection/backupVaults/sampleVault/backupInstances/TestBI9779f4de"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.UnlockDeleteResponse = armdataprotection.UnlockDeleteResponse{
+	// 	UnlockDeleteExpiryTime: to.Ptr("2022-09-16T12:50:10.7039695Z"),
+	// }
+}

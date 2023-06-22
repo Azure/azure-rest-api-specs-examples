d41ae87bc48602515d426fb52cd9b276ac6fbae8
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-22 20:01:07 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-containerservicefleet_1.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-python/Fleets_ListCredentialsResult.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.containerservicefleet import ContainerServiceFleetMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerservicefleet
+# USAGE
+    python fleets_list_credentials_result.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerServiceFleetMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid1",
+    )
+
+    response = client.fleets.list_credentials(
+        resource_group_name="rg1",
+        fleet_name="fleet",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/Fleets_ListCredentialsResult.json
+if __name__ == "__main__":
+    main()

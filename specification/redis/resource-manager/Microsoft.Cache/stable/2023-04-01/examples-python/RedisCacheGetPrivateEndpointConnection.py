23f2990c6c150708d7f9292c27c63e55ca089677
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-22 20:01:29 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-redis_14.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2023-04-01/examples-python/RedisCacheGetPrivateEndpointConnection.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.redis import RedisManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-redis
+# USAGE
+    python redis_cache_get_private_endpoint_connection.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RedisManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscriptionId}",
+    )
+
+    response = client.private_endpoint_connections.get(
+        resource_group_name="rgtest01",
+        cache_name="cachetest01",
+        private_endpoint_connection_name="pectest01",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2023-04-01/examples/RedisCacheGetPrivateEndpointConnection.json
+if __name__ == "__main__":
+    main()
---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2023-04-01/examples-python/RedisCacheRegenerateKey.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.redis import RedisManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-redis
+# USAGE
+    python redis_cache_regenerate_key.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RedisManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.redis.regenerate_key(
+        resource_group_name="rg1",
+        name="cache1",
+        parameters={"keyType": "Primary"},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2023-04-01/examples/RedisCacheRegenerateKey.json
+if __name__ == "__main__":
+    main()

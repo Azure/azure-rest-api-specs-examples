5ab1edac0787d6486282f330f96472469f96668e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-22 20:02:43 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-recoveryservices_5.3.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples-js/PATCHVault_WithUserAssignedIdentity.js
@@ -0,0 +1,29 @@
+const { RecoveryServicesClient } = require("@azure/arm-recoveryservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates the vault.
+ *
+ * @summary Updates the vault.
+ * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples/PATCHVault_WithUserAssignedIdentity.json
+ */
+async function updateResourceWithUserAssignedIdentity() {
+  const subscriptionId =
+    process.env["RECOVERYSERVICES_SUBSCRIPTION_ID"] || "77777777-b0c6-47a2-b37c-d8e65a629c18";
+  const resourceGroupName = process.env["RECOVERYSERVICES_RESOURCE_GROUP"] || "HelloWorld";
+  const vaultName = "swaggerExample";
+  const vault = {
+    identity: {
+      type: "UserAssigned",
+      userAssignedIdentities: {
+        "/subscriptions/85bf5e8c30844f42Add2746ebb7e97b2/resourcegroups/defaultrg/providers/MicrosoftManagedIdentity/userAssignedIdentities/examplemsi":
+          {},
+      },
+    },
+    tags: { patchKey: "PatchKeyUpdated" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RecoveryServicesClient(credential, subscriptionId);
+  const result = await client.vaults.beginUpdateAndWait(resourceGroupName, vaultName, vault);
+  console.log(result);
+}
---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples-js/PUTVault.js
@@ -0,0 +1,30 @@
+const { RecoveryServicesClient } = require("@azure/arm-recoveryservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates a Recovery Services vault.
+ *
+ * @summary Creates or updates a Recovery Services vault.
+ * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-02-01/examples/PUTVault.json
+ */
+async function createOrUpdateRecoveryServicesVault() {
+  const subscriptionId =
+    process.env["RECOVERYSERVICES_SUBSCRIPTION_ID"] || "77777777-b0c6-47a2-b37c-d8e65a629c18";
+  const resourceGroupName =
+    process.env["RECOVERYSERVICES_RESOURCE_GROUP"] || "Default-RecoveryServices-ResourceGroup";
+  const vaultName = "swaggerExample";
+  const vault = {
+    identity: { type: "SystemAssigned" },
+    location: "West US",
+    properties: { publicNetworkAccess: "Enabled" },
+    sku: { name: "Standard" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RecoveryServicesClient(credential, subscriptionId);
+  const result = await client.vaults.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    vaultName,
+    vault
+  );
+  console.log(result);
+}

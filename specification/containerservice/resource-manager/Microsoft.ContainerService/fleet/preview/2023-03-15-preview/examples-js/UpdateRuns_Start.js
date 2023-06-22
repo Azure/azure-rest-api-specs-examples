267dedfa0e347fadc16c543d8956691c559c733f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-22 20:02:35 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservicefleet_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-js/UpdateRuns_Start.js
@@ -0,0 +1,24 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Starts an UpdateRun.
+ *
+ * @summary Starts an UpdateRun.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/UpdateRuns_Start.json
+ */
+async function startsAnUpdateRun() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const fleetName = "fleet1";
+  const updateRunName = "run1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const result = await client.updateRuns.beginStartAndWait(
+    resourceGroupName,
+    fleetName,
+    updateRunName
+  );
+  console.log(result);
+}

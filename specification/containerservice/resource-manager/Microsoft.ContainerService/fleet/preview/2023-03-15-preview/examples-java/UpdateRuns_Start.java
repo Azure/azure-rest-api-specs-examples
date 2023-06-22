8d0146b3a882044e7e19173fd18e4df2c6c775ba
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-22 20:03:19 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-containerservicefleet_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples-java/UpdateRuns_Start.java
@@ -0,0 +1,15 @@
+/** Samples for UpdateRuns Start. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-03-15-preview/examples/UpdateRuns_Start.json
+     */
+    /**
+     * Sample code: Starts an UpdateRun.
+     *
+     * @param manager Entry point to ContainerServiceFleetManager.
+     */
+    public static void startsAnUpdateRun(
+        com.azure.resourcemanager.containerservicefleet.ContainerServiceFleetManager manager) {
+        manager.updateRuns().start("rg1", "fleet1", "run1", null, com.azure.core.util.Context.NONE);
+    }
+}

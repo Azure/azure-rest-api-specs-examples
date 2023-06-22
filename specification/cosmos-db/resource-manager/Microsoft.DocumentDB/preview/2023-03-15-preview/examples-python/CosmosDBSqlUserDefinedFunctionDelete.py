678001118660a86248bb4f199647844bb0d5ef3d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-22 20:02:06 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_10.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-15-preview/examples-python/CosmosDBSqlUserDefinedFunctionDelete.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_sql_user_defined_function_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    client.sql_resources.begin_delete_sql_user_defined_function(
+        resource_group_name="rg1",
+        account_name="ddb1",
+        database_name="databaseName",
+        container_name="containerName",
+        user_defined_function_name="userDefinedFunctionName",
+    ).result()
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-03-15-preview/examples/CosmosDBSqlUserDefinedFunctionDelete.json
+if __name__ == "__main__":
+    main()

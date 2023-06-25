7e0cd4b53a8197758b1c1a6d0e44a80cc22012a4
azure-sdk <azuresdk@microsoft.com>
2023-06-25 12:08:09 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-selfhelp_1.0.0

---
+++ specification/help/resource-manager/Microsoft.Help/stable/2023-06-01/examples-python/ListOperations.py
@@ -0,0 +1,30 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.selfhelp import SelfHelpMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-selfhelp
+# USAGE
+    python list_operations.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SelfHelpMgmtClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.operations.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/help/resource-manager/Microsoft.Help/stable/2023-06-01/examples/ListOperations.json
+if __name__ == "__main__":
+    main()

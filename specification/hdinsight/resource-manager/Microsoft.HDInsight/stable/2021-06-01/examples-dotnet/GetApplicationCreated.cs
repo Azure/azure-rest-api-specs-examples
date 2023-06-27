1091f756be08b8b3a0db718cc7c233818c717ab1
azure-sdk <azuresdk@microsoft.com>
2023-06-27 12:15:08 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HDInsight_1.1.0

--- specification/hdinsight/resource-manager/Microsoft.HDInsight/stable/2021-06-01/examples-dotnet/GetApplicationCreated.cs
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/stable/2021-06-01/examples-dotnet/GetApplicationCreated.cs
@@ -15,19 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this HDInsightClusterResource created on azure
-// for more information of creating HDInsightClusterResource, please refer to the document of HDInsightClusterResource
+// this example assumes you already have this HDInsightApplicationResource created on azure
+// for more information of creating HDInsightApplicationResource, please refer to the document of HDInsightApplicationResource
 string subscriptionId = "subid";
 string resourceGroupName = "rg1";
 string clusterName = "cluster1";
-ResourceIdentifier hdInsightClusterResourceId = HDInsightClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName);
-HDInsightClusterResource hdInsightCluster = client.GetHDInsightClusterResource(hdInsightClusterResourceId);
-
-// get the collection of this HDInsightApplicationResource
-HDInsightApplicationCollection collection = hdInsightCluster.GetHDInsightApplications();
+string applicationName = "app";
+ResourceIdentifier hdInsightApplicationResourceId = HDInsightApplicationResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName, applicationName);
+HDInsightApplicationResource hdInsightApplication = client.GetHDInsightApplicationResource(hdInsightApplicationResourceId);
 
 // invoke the operation
-string applicationName = "app";
-bool result = await collection.ExistsAsync(applicationName);
+HDInsightApplicationResource result = await hdInsightApplication.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+HDInsightApplicationData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

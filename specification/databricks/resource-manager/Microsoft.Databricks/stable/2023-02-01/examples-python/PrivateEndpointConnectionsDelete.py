78b34b858cba23284e0ddf55caa199319e3cceab
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-06-30 20:01:14 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-databricks_2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/databricks/resource-manager/Microsoft.Databricks/stable/2023-02-01/examples-python/PrivateEndpointConnectionsDelete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.databricks import AzureDatabricksManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-databricks
+# USAGE
+    python private_endpoint_connections_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureDatabricksManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="11111111-1111-1111-1111-111111111111",
+    )
+
+    client.private_endpoint_connections.begin_delete(
+        resource_group_name="myResourceGroup",
+        workspace_name="myWorkspace",
+        private_endpoint_connection_name="myWorkspace.23456789-1111-1111-1111-111111111111",
+    ).result()
+
+
+# x-ms-original-file: specification/databricks/resource-manager/Microsoft.Databricks/stable/2023-02-01/examples/PrivateEndpointConnectionsDelete.json
+if __name__ == "__main__":
+    main()

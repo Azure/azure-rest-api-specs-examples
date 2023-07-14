1474fd59e107308cebf8de701351b4167a70800c
azure-sdk <azuresdk@microsoft.com>
2023-07-14 12:14:18 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-paloaltonetworksngfw_1.0.0

---
+++ specification/paloaltonetworks/resource-manager/PaloAltoNetworks.Cloudngfw/stable/2022-08-29/examples-python/PostRules_getCounters_MaximumSet_Gen.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.paloaltonetworksngfw import PaloAltoNetworksNgfwMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-paloaltonetworksngfw
+# USAGE
+    python post_rules_get_counters_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PaloAltoNetworksNgfwMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.post_rules.get_counters(
+        global_rulestack_name="lrs1",
+        priority="1",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/paloaltonetworks/resource-manager/PaloAltoNetworks.Cloudngfw/stable/2022-08-29/examples/PostRules_getCounters_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

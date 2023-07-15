24e9e4af2645ef26d983ee4d6bbcac9862d5c928
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-07-15 20:01:18 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-paloaltonetworksngfw_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/paloaltonetworks/resource-manager/PaloAltoNetworks.Cloudngfw/stable/2022-08-29/examples-python/CertificateObjectGlobalRulestack_Get_MinimumSet_Gen.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.paloaltonetworksngfw import PaloAltoNetworksNgfwMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-paloaltonetworksngfw
+# USAGE
+    python certificate_object_global_rulestack_get_minimum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PaloAltoNetworksNgfwMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.certificate_object_global_rulestack.get(
+        global_rulestack_name="praval",
+        name="armid1",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/paloaltonetworks/resource-manager/PaloAltoNetworks.Cloudngfw/stable/2022-08-29/examples/CertificateObjectGlobalRulestack_Get_MinimumSet_Gen.json
+if __name__ == "__main__":
+    main()

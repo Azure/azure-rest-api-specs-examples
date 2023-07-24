2d8296b221f5f4365f1388566ee1b93a3daa811b
azure-sdk <azuresdk@microsoft.com>
2023-07-24 12:22:39 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-hdinsight_9.0.0b2

---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2023-04-15-preview/examples-python/CreateHDInsightClusterWithCustomNetworkProperties.py
@@ -0,0 +1,97 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.hdinsight import HDInsightManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hdinsight
+# USAGE
+    python create_hd_insight_cluster_with_custom_network_properties.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HDInsightManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subId",
+    )
+
+    response = client.clusters.begin_create(
+        resource_group_name="rg1",
+        cluster_name="cluster1",
+        parameters={
+            "properties": {
+                "clusterDefinition": {
+                    "configurations": {
+                        "gateway": {
+                            "restAuthCredential.isEnabled": True,
+                            "restAuthCredential.password": "**********",
+                            "restAuthCredential.username": "admin",
+                        }
+                    },
+                    "kind": "hadoop",
+                },
+                "clusterVersion": "3.6",
+                "computeProfile": {
+                    "roles": [
+                        {
+                            "hardwareProfile": {"vmSize": "standard_d3"},
+                            "name": "headnode",
+                            "osProfile": {
+                                "linuxOperatingSystemProfile": {
+                                    "password": "**********",
+                                    "sshProfile": {"publicKeys": [{"certificateData": "**********"}]},
+                                    "username": "sshuser",
+                                }
+                            },
+                            "targetInstanceCount": 2,
+                            "virtualNetworkProfile": {
+                                "id": "/subscriptions/subId/resourceGroups/rg/providers/Microsoft.Network/virtualNetworks/vnetname",
+                                "subnet": "/subscriptions/subId/resourceGroups/rg/providers/Microsoft.Network/virtualNetworks/vnetname/subnets/vnetsubnet",
+                            },
+                        },
+                        {
+                            "hardwareProfile": {"vmSize": "standard_d3"},
+                            "name": "workernode",
+                            "osProfile": {
+                                "linuxOperatingSystemProfile": {
+                                    "password": "**********",
+                                    "sshProfile": {"publicKeys": [{"certificateData": "**********"}]},
+                                    "username": "sshuser",
+                                }
+                            },
+                            "targetInstanceCount": 2,
+                            "virtualNetworkProfile": {
+                                "id": "/subscriptions/subId/resourceGroups/rg/providers/Microsoft.Network/virtualNetworks/vnetname",
+                                "subnet": "/subscriptions/subId/resourceGroups/rg/providers/Microsoft.Network/virtualNetworks/vnetname/subnets/vnetsubnet",
+                            },
+                        },
+                    ]
+                },
+                "networkProperties": {"privateLink": "Enabled", "resourceProviderConnection": "Outbound"},
+                "osType": "Linux",
+                "storageProfile": {
+                    "storageaccounts": [
+                        {
+                            "container": "containername",
+                            "enableSecureChannel": True,
+                            "isDefault": True,
+                            "key": "storage account key",
+                            "name": "mystorage",
+                        }
+                    ]
+                },
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2023-04-15-preview/examples/CreateHDInsightClusterWithCustomNetworkProperties.json
+if __name__ == "__main__":
+    main()
---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2023-04-15-preview/examples-python/HDI_Clusters_UpdateGatewaySettings_Enable.py
@@ -0,0 +1,37 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.hdinsight import HDInsightManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hdinsight
+# USAGE
+    python hdi_clusters_update_gateway_settings_enable.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HDInsightManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    client.clusters.begin_update_gateway_settings(
+        resource_group_name="rg1",
+        cluster_name="cluster1",
+        parameters={
+            "restAuthCredential.isEnabled": True,
+            "restAuthCredential.password": "**********",
+            "restAuthCredential.username": "hadoop",
+        },
+    ).result()
+
+
+# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2023-04-15-preview/examples/HDI_Clusters_UpdateGatewaySettings_Enable.json
+if __name__ == "__main__":
+    main()

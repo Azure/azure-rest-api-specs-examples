ab87fbc5d3b58b99de1680f2649dc55718cc885c
azure-sdk <azuresdk@microsoft.com>
2023-07-24 12:18:02 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-elasticsan_1.0.0b2

---
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2022-12-01-preview/examples-python/ElasticSans_Create_MaximumSet_Gen.py
@@ -0,0 +1,43 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.elasticsan import ElasticSanMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-elasticsan
+# USAGE
+    python elastic_sans_create_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ElasticSanMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscriptionid",
+    )
+
+    response = client.elastic_sans.begin_create(
+        resource_group_name="resourcegroupname",
+        elastic_san_name="elasticsanname",
+        parameters={
+            "location": "South Central US",
+            "properties": {
+                "availabilityZones": ["1"],
+                "baseSizeTiB": 5,
+                "extendedCapacitySizeTiB": 25,
+                "sku": {"name": "Premium_LRS", "tier": "Premium"},
+            },
+            "tags": {"key9316": "ihndtieqibtob"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2022-12-01-preview/examples/ElasticSans_Create_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

f2ebba42d031a987b4469e4776ec8236c4a8dd21
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-07-25 20:01:23 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-kusto_3.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2023-05-02/examples-python/KustoScriptsListByDatabase.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.kusto import KustoManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-kusto
+# USAGE
+    python kusto_scripts_list_by_database.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = KustoManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="12345678-1234-1234-1234-123456789098",
+    )
+
+    response = client.scripts.list_by_database(
+        resource_group_name="kustorptest",
+        cluster_name="kustoCluster",
+        database_name="Kustodatabase8",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2023-05-02/examples/KustoScriptsListByDatabase.json
+if __name__ == "__main__":
+    main()

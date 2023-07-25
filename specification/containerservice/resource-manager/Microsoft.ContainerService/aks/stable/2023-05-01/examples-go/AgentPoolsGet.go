dd228321fb0ad2651afc46d1e7c23ce8fdde4062
azure-sdk <azuresdk@microsoft.com>
2023-07-25 12:11:48 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v4.1.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples-go/AgentPoolsGet.go
@@ -0,0 +1,46 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v4"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/c53808ba54beef57059371708f1fa6949a11a280/specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples/AgentPoolsGet.json
+func ExampleAgentPoolsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewAgentPoolsClient().Get(ctx, "rg1", "clustername1", "agentpool1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AgentPool = armcontainerservice.AgentPool{
+	// 	Name: to.Ptr("agentpool1"),
+	// 	ID: to.Ptr("/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/agentPools/agentpool1"),
+	// 	Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+	// 		Count: to.Ptr[int32](3),
+	// 		CurrentOrchestratorVersion: to.Ptr("1.9.6"),
+	// 		MaxPods: to.Ptr[int32](110),
+	// 		NodeImageVersion: to.Ptr("AKSUbuntu:1604:2020.03.11"),
+	// 		OrchestratorVersion: to.Ptr("1.9.6"),
+	// 		OSType: to.Ptr(armcontainerservice.OSTypeLinux),
+	// 		ProvisioningState: to.Ptr("Succeeded"),
+	// 		UpgradeSettings: &armcontainerservice.AgentPoolUpgradeSettings{
+	// 			MaxSurge: to.Ptr("33%"),
+	// 		},
+	// 		VMSize: to.Ptr("Standard_DS1_v2"),
+	// 	},
+	// }
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples-go/MaintenanceConfigurationsDelete_MaintenanceWindow.go
@@ -0,0 +1,26 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v4"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/c53808ba54beef57059371708f1fa6949a11a280/specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples/MaintenanceConfigurationsDelete_MaintenanceWindow.json
+func ExampleMaintenanceConfigurationsClient_Delete_deleteMaintenanceConfigurationForNodeOsUpgrade() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	_, err = clientFactory.NewMaintenanceConfigurationsClient().Delete(ctx, "rg1", "clustername1", "aksManagedNodeOSUpgradeSchedule", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+}

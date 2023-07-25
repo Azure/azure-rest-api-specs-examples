e6bf1c1a956e1cc599dc94077e4a07ba0c46d0a5
azure-sdk <azuresdk@microsoft.com>
2023-07-25 12:14:53 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/elasticsan/armelasticsan/v0.3.0

---
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2022-12-01-preview/examples-go/ElasticSans_Create_MaximumSet_Gen.go
@@ -0,0 +1,82 @@
+package armelasticsan_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/elasticsan/armelasticsan"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/c53808ba54beef57059371708f1fa6949a11a280/specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2022-12-01-preview/examples/ElasticSans_Create_MaximumSet_Gen.json
+func ExampleElasticSansClient_BeginCreate_elasticSansCreateMaximumSetGen() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armelasticsan.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewElasticSansClient().BeginCreate(ctx, "resourcegroupname", "elasticsanname", armelasticsan.ElasticSan{
+		Location: to.Ptr("South Central US"),
+		Tags: map[string]*string{
+			"key9316": to.Ptr("ihndtieqibtob"),
+		},
+		Properties: &armelasticsan.Properties{
+			AvailabilityZones: []*string{
+				to.Ptr("1")},
+			BaseSizeTiB:             to.Ptr[int64](5),
+			ExtendedCapacitySizeTiB: to.Ptr[int64](25),
+			SKU: &armelasticsan.SKU{
+				Name: to.Ptr(armelasticsan.SKUNamePremiumLRS),
+				Tier: to.Ptr(armelasticsan.SKUTierPremium),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ElasticSan = armelasticsan.ElasticSan{
+	// 	Name: to.Ptr("vfoatmakv"),
+	// 	Type: to.Ptr("Microsoft.ElasticSan/ElasticSans"),
+	// 	ID: to.Ptr("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ElasticSan/elasticSans/{elasticSanName}"),
+	// 	SystemData: &armelasticsan.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-03T09:59:45.919Z"); return t}()),
+	// 		CreatedBy: to.Ptr("otfifnrahdshqombvtg"),
+	// 		CreatedByType: to.Ptr(armelasticsan.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-03T09:59:45.919Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("jnaxavnlhrboshtidtib"),
+	// 		LastModifiedByType: to.Ptr(armelasticsan.CreatedByTypeUser),
+	// 	},
+	// 	Location: to.Ptr("France Central"),
+	// 	Tags: map[string]*string{
+	// 		"key5002": to.Ptr("lhag"),
+	// 	},
+	// 	Properties: &armelasticsan.Properties{
+	// 		AvailabilityZones: []*string{
+	// 			to.Ptr("1")},
+	// 			BaseSizeTiB: to.Ptr[int64](15),
+	// 			ExtendedCapacitySizeTiB: to.Ptr[int64](6),
+	// 			ProvisioningState: to.Ptr(armelasticsan.ProvisioningStatesSucceeded),
+	// 			SKU: &armelasticsan.SKU{
+	// 				Name: to.Ptr(armelasticsan.SKUNamePremiumLRS),
+	// 				Tier: to.Ptr(armelasticsan.SKUTierPremium),
+	// 			},
+	// 			TotalIops: to.Ptr[int64](22),
+	// 			TotalMBps: to.Ptr[int64](4),
+	// 			TotalSizeTiB: to.Ptr[int64](27),
+	// 			TotalVolumeSizeGiB: to.Ptr[int64](15),
+	// 			VolumeGroupCount: to.Ptr[int64](24),
+	// 		},
+	// 	}
+}
---
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2022-12-01-preview/examples-go/VolumeGroups_Create_MinimumSet_Gen.go
@@ -0,0 +1,59 @@
+package armelasticsan_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/elasticsan/armelasticsan"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/c53808ba54beef57059371708f1fa6949a11a280/specification/elasticsan/resource-manager/Microsoft.ElasticSan/preview/2022-12-01-preview/examples/VolumeGroups_Create_MinimumSet_Gen.json
+func ExampleVolumeGroupsClient_BeginCreate_volumeGroupsCreateMinimumSetGen() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armelasticsan.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewVolumeGroupsClient().BeginCreate(ctx, "resourcegroupname", "elasticsanname", "volumegroupname", armelasticsan.VolumeGroup{}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.VolumeGroup = armelasticsan.VolumeGroup{
+	// 	Name: to.Ptr("cr"),
+	// 	Type: to.Ptr("Microsoft.ElasticSan/elasticSans/volumeGroups"),
+	// 	ID: to.Ptr("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ElasticSan/elasticSans/{elasticSanName}/volumegroups/{volumeGroupName}"),
+	// 	SystemData: &armelasticsan.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-04T05:52:15.371Z"); return t}()),
+	// 		CreatedBy: to.Ptr("zloujisrarsox"),
+	// 		CreatedByType: to.Ptr(armelasticsan.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-04T05:52:15.371Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("lmdztjjcmdfzam"),
+	// 		LastModifiedByType: to.Ptr(armelasticsan.CreatedByTypeUser),
+	// 	},
+	// 	Properties: &armelasticsan.VolumeGroupProperties{
+	// 		Encryption: to.Ptr(armelasticsan.EncryptionTypeEncryptionAtRestWithPlatformKey),
+	// 		NetworkACLs: &armelasticsan.NetworkRuleSet{
+	// 			VirtualNetworkRules: []*armelasticsan.VirtualNetworkRule{
+	// 				{
+	// 					Action: to.Ptr("Allow"),
+	// 					VirtualNetworkResourceID: to.Ptr("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/virtualNetworks/{vnetName}/subnets/{subnetName}"),
+	// 					State: to.Ptr(armelasticsan.StateProvisioning),
+	// 			}},
+	// 		},
+	// 		ProtocolType: to.Ptr(armelasticsan.StorageTargetTypeIscsi),
+	// 		ProvisioningState: to.Ptr(armelasticsan.ProvisioningStatesSucceeded),
+	// 	},
+	// }
+}

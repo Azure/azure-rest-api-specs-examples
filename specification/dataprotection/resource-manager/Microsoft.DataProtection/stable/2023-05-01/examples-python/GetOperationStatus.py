0931a225c9bb2eb6ed16142023fd6bbfdb6580cc
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-07-25 20:01:38 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-dataprotection_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-05-01/examples-python/GetOperationStatus.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.dataprotection import DataProtectionMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dataprotection
+# USAGE
+    python get_operation_status.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataProtectionMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="0b352192-dcac-4cc7-992e-a96190ccc68c",
+    )
+
+    response = client.operation_status.get(
+        location="WestUS",
+        operation_id="MjkxOTMyODMtYTE3My00YzJjLTg5NjctN2E4MDIxNDA3NjA2OzdjNGE2ZWRjLWJjMmItNDRkYi1hYzMzLWY1YzEwNzk5Y2EyOA==",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-05-01/examples/GetOperationStatus.json
+if __name__ == "__main__":
+    main()

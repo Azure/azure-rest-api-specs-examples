98472a512f40e9d6e35afb668546c568e98e21a0
azure-sdk <azuresdk@microsoft.com>
2023-07-25 12:18:56 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.1.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples-js/AgentPoolsGetAgentPoolAvailableVersions.js
@@ -0,0 +1,21 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to See [supported Kubernetes versions](https://docs.microsoft.com/azure/aks/supported-kubernetes-versions) for more details about the version lifecycle.
+ *
+ * @summary See [supported Kubernetes versions](https://docs.microsoft.com/azure/aks/supported-kubernetes-versions) for more details about the version lifecycle.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples/AgentPoolsGetAgentPoolAvailableVersions.json
+ */
+async function getAvailableVersionsForAgentPool() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.agentPools.getAvailableAgentPoolVersions(
+    resourceGroupName,
+    resourceName
+  );
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples-js/MaintenanceConfigurationsList.js
@@ -0,0 +1,24 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a list of maintenance configurations in the specified managed cluster.
+ *
+ * @summary Gets a list of maintenance configurations in the specified managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples/MaintenanceConfigurationsList.json
+ */
+async function listMaintenanceConfigurationsByManagedCluster() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.maintenanceConfigurations.listByManagedCluster(
+    resourceGroupName,
+    resourceName
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

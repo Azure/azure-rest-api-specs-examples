0633b48796bee89b32e9779e6515a3c6b2ee3d4c
azure-sdk <azuresdk@microsoft.com>
2023-07-26 12:17:26 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-sphere_1.0.0b1

---
+++ specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples-python/PostClaimDevices.py
@@ -0,0 +1,39 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.sphere import AzureSphereMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-sphere
+# USAGE
+    python post_claim_devices.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureSphereMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    client.device_groups.begin_claim_devices(
+        resource_group_name="MyResourceGroup1",
+        catalog_name="MyCatalog1",
+        product_name="MyProduct1",
+        device_group_name="MyDeviceGroup1",
+        claim_devices_request={
+            "deviceIdentifiers": [
+                "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+            ]
+        },
+    ).result()
+
+
+# x-ms-original-file: specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples/PostClaimDevices.json
+if __name__ == "__main__":
+    main()

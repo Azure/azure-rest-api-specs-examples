8424af91fd3c4ba9071b794e1d32c30458a881a5
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-07-26 15:08:50 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.1.0 (#3181)
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples-js/ManagedClustersUpdateTags.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates tags on a managed cluster.
+ *
+ * @summary Updates tags on a managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-05-01/examples/ManagedClustersUpdateTags.json
+ */
+async function updateManagedClusterTags() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const parameters = { tags: { archv3: "", tier: "testing" } };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginUpdateTagsAndWait(
+    resourceGroupName,
+    resourceName,
+    parameters
+  );
+  console.log(result);
+}

7b560f64a206a2f98b0efa2cf7691f7b111d8e64
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-07-26 20:01:30 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storagemover_2.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/preview/2023-07-01-preview/examples-python/Projects_Get.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.storagemover import StorageMoverMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storagemover
+# USAGE
+    python projects_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageMoverMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="60bcfc77-6589-4da2-b7fd-f9ec9322cf95",
+    )
+
+    response = client.projects.get(
+        resource_group_name="examples-rg",
+        storage_mover_name="examples-storageMoverName",
+        project_name="examples-projectName",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/preview/2023-07-01-preview/examples/Projects_Get.json
+if __name__ == "__main__":
+    main()

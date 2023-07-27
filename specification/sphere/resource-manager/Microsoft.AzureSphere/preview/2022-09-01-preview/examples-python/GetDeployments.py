720e5e8fd17aabef36f3980f42a2e50b3bffed6b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-07-27 20:01:13 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-sphere_1.0.0b1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples-python/GetDeployments.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.sphere import AzureSphereMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-sphere
+# USAGE
+    python get_deployments.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureSphereMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.deployments.list_by_device_group(
+        resource_group_name="MyResourceGroup1",
+        catalog_name="MyCatalog1",
+        product_name="MyProduct1",
+        device_group_name="myDeviceGroup1",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples/GetDeployments.json
+if __name__ == "__main__":
+    main()

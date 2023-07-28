a9c38efeabb9c21d37338a7fd0a128330cee3baf
azure-sdk <azuresdk@microsoft.com>
2023-07-28 12:13:37 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-mobilenetwork_3.0.0

---
+++ specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2023-06-01/examples-js/AttachedDataNetworkDelete.js
@@ -0,0 +1,26 @@
+const { MobileNetworkManagementClient } = require("@azure/arm-mobilenetwork");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes the specified attached data network.
+ *
+ * @summary Deletes the specified attached data network.
+ * x-ms-original-file: specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2023-06-01/examples/AttachedDataNetworkDelete.json
+ */
+async function deleteAttachedDataNetworkResource() {
+  const subscriptionId =
+    process.env["MOBILENETWORK_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["MOBILENETWORK_RESOURCE_GROUP"] || "rg1";
+  const packetCoreControlPlaneName = "TestPacketCoreCP";
+  const packetCoreDataPlaneName = "TestPacketCoreDP";
+  const attachedDataNetworkName = "TestAttachedDataNetwork";
+  const credential = new DefaultAzureCredential();
+  const client = new MobileNetworkManagementClient(credential, subscriptionId);
+  const result = await client.attachedDataNetworks.beginDeleteAndWait(
+    resourceGroupName,
+    packetCoreControlPlaneName,
+    packetCoreDataPlaneName,
+    attachedDataNetworkName
+  );
+  console.log(result);
+}
---
+++ specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2023-06-01/examples-js/SimListBySimGroup.js
@@ -0,0 +1,22 @@
+const { MobileNetworkManagementClient } = require("@azure/arm-mobilenetwork");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets all the SIMs in a SIM group.
+ *
+ * @summary Gets all the SIMs in a SIM group.
+ * x-ms-original-file: specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2023-06-01/examples/SimListBySimGroup.json
+ */
+async function listSiMSInASimGroup() {
+  const subscriptionId =
+    process.env["MOBILENETWORK_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["MOBILENETWORK_RESOURCE_GROUP"] || "rg1";
+  const simGroupName = "testSimGroup";
+  const credential = new DefaultAzureCredential();
+  const client = new MobileNetworkManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.sims.listByGroup(resourceGroupName, simGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

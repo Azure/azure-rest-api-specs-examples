cd7551b3111e27ef5bdf2a9d22d216724d6f8b81
azure-sdk <azuresdk@microsoft.com>
2023-08-14 12:06:22 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.2.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-06-02-preview/examples-js/ManagedClustersResetServicePrincipalProfile.js
@@ -0,0 +1,26 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to This action cannot be performed on a cluster that is not using a service principal
+ *
+ * @summary This action cannot be performed on a cluster that is not using a service principal
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-06-02-preview/examples/ManagedClustersResetServicePrincipalProfile.json
+ */
+async function resetServicePrincipalProfile() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const parameters = {
+    clientId: "clientid",
+    secret: "secret",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginResetServicePrincipalProfileAndWait(
+    resourceGroupName,
+    resourceName,
+    parameters
+  );
+  console.log(result);
+}

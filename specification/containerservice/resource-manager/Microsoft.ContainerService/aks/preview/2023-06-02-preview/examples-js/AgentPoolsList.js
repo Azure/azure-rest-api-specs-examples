bb5ec6bfeb0cc8073c864f24015d99e60fcef8f0
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-08-15 20:01:15 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.2.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-06-02-preview/examples-js/AgentPoolsList.js
@@ -0,0 +1,21 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a list of agent pools in the specified managed cluster.
+ *
+ * @summary Gets a list of agent pools in the specified managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-06-02-preview/examples/AgentPoolsList.json
+ */
+async function listAgentPoolsByManagedCluster() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.agentPools.list(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

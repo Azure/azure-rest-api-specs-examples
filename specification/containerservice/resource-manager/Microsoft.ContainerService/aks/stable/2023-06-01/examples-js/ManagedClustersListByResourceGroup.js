0e44d03e248a398ef2c663a4280c030b85059e39
azure-sdk <azuresdk@microsoft.com>
2023-08-16 12:06:28 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.2.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-06-01/examples-js/ManagedClustersListByResourceGroup.js
@@ -0,0 +1,20 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists managed clusters in the specified subscription and resource group.
+ *
+ * @summary Lists managed clusters in the specified subscription and resource group.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-06-01/examples/ManagedClustersListByResourceGroup.json
+ */
+async function getManagedClustersByResourceGroup() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.managedClusters.listByResourceGroup(resourceGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-06-01/examples-js/PrivateLinkResourcesList.js
@@ -0,0 +1,18 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to To learn more about private clusters, see: https://docs.microsoft.com/azure/aks/private-clusters
+ *
+ * @summary To learn more about private clusters, see: https://docs.microsoft.com/azure/aks/private-clusters
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-06-01/examples/PrivateLinkResourcesList.json
+ */
+async function listPrivateLinkResourcesByManagedCluster() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.privateLinkResources.list(resourceGroupName, resourceName);
+  console.log(result);
+}

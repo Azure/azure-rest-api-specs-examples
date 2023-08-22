a9608fbe6b881a4d3720dcae56ec35902f1fb6cb
azure-sdk <azuresdk@microsoft.com>
2023-08-22 12:17:28 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.NetworkCloud_1.0.0

---
+++ specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2023-07-01/examples-dotnet/L2Networks_Delete.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.NetworkCloud;
+using Azure.ResourceManager.NetworkCloud.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2023-07-01/examples/L2Networks_Delete.json
+// this example is just showing the usage of "L2Networks_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this NetworkCloudL2NetworkResource created on azure
+// for more information of creating NetworkCloudL2NetworkResource, please refer to the document of NetworkCloudL2NetworkResource
+string subscriptionId = "123e4567-e89b-12d3-a456-426655440000";
+string resourceGroupName = "resourceGroupName";
+string l2NetworkName = "l2NetworkName";
+ResourceIdentifier networkCloudL2NetworkResourceId = NetworkCloudL2NetworkResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, l2NetworkName);
+NetworkCloudL2NetworkResource networkCloudL2Network = client.GetNetworkCloudL2NetworkResource(networkCloudL2NetworkResourceId);
+
+// invoke the operation
+await networkCloudL2Network.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

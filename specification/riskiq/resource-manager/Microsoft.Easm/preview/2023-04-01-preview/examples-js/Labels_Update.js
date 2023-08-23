3012d2ee2ff9811154c61585843077cf2c362013
azure-sdk <azuresdk@microsoft.com>
2023-08-23 12:22:26 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-defendereasm_1.0.0-beta.1

---
+++ specification/riskiq/resource-manager/Microsoft.Easm/preview/2023-04-01-preview/examples-js/Labels_Update.js
@@ -0,0 +1,20 @@
+const { EasmMgmtClient } = require("@azure/arm-defendereasm");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Update a Label.
+ *
+ * @summary Update a Label.
+ * x-ms-original-file: specification/riskiq/resource-manager/Microsoft.Easm/preview/2023-04-01-preview/examples/Labels_Update.json
+ */
+async function labels() {
+  const subscriptionId =
+    process.env["DEFENDEREASM_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["DEFENDEREASM_RESOURCE_GROUP"] || "dummyrg";
+  const workspaceName = "ThisisaWorkspace";
+  const labelName = "ThisisaLabel";
+  const credential = new DefaultAzureCredential();
+  const client = new EasmMgmtClient(credential, subscriptionId);
+  const result = await client.labels.update(resourceGroupName, workspaceName, labelName);
+  console.log(result);
+}

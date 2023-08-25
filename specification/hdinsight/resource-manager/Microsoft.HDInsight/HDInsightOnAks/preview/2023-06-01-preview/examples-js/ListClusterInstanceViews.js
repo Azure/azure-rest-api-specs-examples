6fe745e0ad95578543b47cd0934bb0400fff4024
azure-sdk <azuresdk@microsoft.com>
2023-08-25 12:20:42 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-hdinsightcontainers_1.0.0-beta.1

---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/HDInsightOnAks/preview/2023-06-01-preview/examples-js/ListClusterInstanceViews.js
@@ -0,0 +1,27 @@
+const { HDInsightContainersManagementClient } = require("@azure/arm-hdinsightcontainers");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists the lists of instance views
+ *
+ * @summary Lists the lists of instance views
+ * x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/HDInsightOnAks/preview/2023-06-01-preview/examples/ListClusterInstanceViews.json
+ */
+async function hdInsightClusterGetInstanceViews() {
+  const subscriptionId =
+    process.env["HDINSIGHT_SUBSCRIPTION_ID"] || "10e32bab-26da-4cc4-a441-52b318f824e6";
+  const resourceGroupName = process.env["HDINSIGHT_RESOURCE_GROUP"] || "rg1";
+  const clusterPoolName = "clusterPool1";
+  const clusterName = "cluster1";
+  const credential = new DefaultAzureCredential();
+  const client = new HDInsightContainersManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.clusters.listInstanceViews(
+    resourceGroupName,
+    clusterPoolName,
+    clusterName
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

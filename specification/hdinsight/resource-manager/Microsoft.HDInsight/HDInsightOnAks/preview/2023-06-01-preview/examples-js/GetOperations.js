89ded096f36415c5be1b10c944f44a6ea24a0cc9
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-08-26 20:01:29 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-hdinsightcontainers_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/HDInsightOnAks/preview/2023-06-01-preview/examples-js/GetOperations.js
@@ -0,0 +1,20 @@
+const { HDInsightContainersManagementClient } = require("@azure/arm-hdinsightcontainers");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Returns list of operations.
+ *
+ * @summary Returns list of operations.
+ * x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/HDInsightOnAks/preview/2023-06-01-preview/examples/GetOperations.json
+ */
+async function listOperations() {
+  const subscriptionId =
+    process.env["HDINSIGHT_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new HDInsightContainersManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.operations.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

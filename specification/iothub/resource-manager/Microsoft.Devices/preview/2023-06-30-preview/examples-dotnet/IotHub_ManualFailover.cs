b48a78ee9ca8aab79a310a0e3473bf9ba91c217f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-06 20:02:21 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.IotHub_1.1.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/iothub/resource-manager/Microsoft.Devices/preview/2023-06-30-preview/examples-dotnet/IotHub_ManualFailover.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.IotHub;
+using Azure.ResourceManager.IotHub.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/iothub/resource-manager/Microsoft.Devices/preview/2023-06-30-preview/examples/IotHub_ManualFailover.json
+// this example is just showing the usage of "IotHub_ManualFailover" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this IotHubDescriptionResource created on azure
+// for more information of creating IotHubDescriptionResource, please refer to the document of IotHubDescriptionResource
+string subscriptionId = "91d12660-3dec-467a-be2a-213b5544ddc0";
+string resourceGroupName = "myResourceGroup";
+string iotHubName = "testHub";
+ResourceIdentifier iotHubDescriptionResourceId = IotHubDescriptionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, iotHubName);
+IotHubDescriptionResource iotHubDescription = client.GetIotHubDescriptionResource(iotHubDescriptionResourceId);
+
+// invoke the operation
+IotHubFailoverContent content = new IotHubFailoverContent("testHub");
+await iotHubDescription.ManualFailoverIotHubAsync(WaitUntil.Completed, content);
+
+Console.WriteLine($"Succeeded");
---
+++ specification/iothub/resource-manager/Microsoft.Devices/preview/2023-06-30-preview/examples-dotnet/iothub_listbyrg.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.IotHub;
+using Azure.ResourceManager.IotHub.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/iothub/resource-manager/Microsoft.Devices/preview/2023-06-30-preview/examples/iothub_listbyrg.json
+// this example is just showing the usage of "IotHubResource_ListByResourceGroup" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "91d12660-3dec-467a-be2a-213b5544ddc0";
+string resourceGroupName = "myResourceGroup";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this IotHubDescriptionResource
+IotHubDescriptionCollection collection = resourceGroupResource.GetIotHubDescriptions();
+
+// invoke the operation and iterate over the result
+await foreach (IotHubDescriptionResource item in collection.GetAllAsync())
+{
+    // the variable item is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    IotHubDescriptionData resourceData = item.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
+
+Console.WriteLine($"Succeeded");

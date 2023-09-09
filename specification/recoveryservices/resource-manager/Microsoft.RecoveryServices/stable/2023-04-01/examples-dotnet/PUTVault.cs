9a09006eb81bb44621fd140b90c5cb1a9ebac407
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-09 20:01:18 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.RecoveryServices_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-04-01/examples-dotnet/PUTVault.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.RecoveryServices;
+using Azure.ResourceManager.RecoveryServices.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-04-01/examples/PUTVault.json
+// this example is just showing the usage of "Vaults_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "77777777-b0c6-47a2-b37c-d8e65a629c18";
+string resourceGroupName = "Default-RecoveryServices-ResourceGroup";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this RecoveryServicesVaultResource
+RecoveryServicesVaultCollection collection = resourceGroupResource.GetRecoveryServicesVaults();
+
+// invoke the operation
+string vaultName = "swaggerExample";
+RecoveryServicesVaultData data = new RecoveryServicesVaultData(new AzureLocation("West US"))
+{
+    Identity = new ManagedServiceIdentity("SystemAssigned"),
+    Properties = new RecoveryServicesVaultProperties()
+    {
+        PublicNetworkAccess = VaultPublicNetworkAccess.Enabled,
+    },
+    Sku = new RecoveryServicesSku(RecoveryServicesSkuName.Standard),
+};
+ArmOperation<RecoveryServicesVaultResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, vaultName, data);
+RecoveryServicesVaultResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+RecoveryServicesVaultData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-04-01/examples-dotnet/UpdateVaultExtendedInfo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.RecoveryServices;
+
+// Generated from example definition: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-04-01/examples/UpdateVaultExtendedInfo.json
+// this example is just showing the usage of "VaultExtendedInfo_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this RecoveryServicesVaultExtendedInfoResource created on azure
+// for more information of creating RecoveryServicesVaultExtendedInfoResource, please refer to the document of RecoveryServicesVaultExtendedInfoResource
+string subscriptionId = "77777777-b0c6-47a2-b37c-d8e65a629c18";
+string resourceGroupName = "Default-RecoveryServices-ResourceGroup";
+string vaultName = "swaggerExample";
+ResourceIdentifier recoveryServicesVaultExtendedInfoResourceId = RecoveryServicesVaultExtendedInfoResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, vaultName);
+RecoveryServicesVaultExtendedInfoResource recoveryServicesVaultExtendedInfo = client.GetRecoveryServicesVaultExtendedInfoResource(recoveryServicesVaultExtendedInfoResourceId);
+
+// invoke the operation
+RecoveryServicesVaultExtendedInfoData info = new RecoveryServicesVaultExtendedInfoData();
+RecoveryServicesVaultExtendedInfoResource result = await recoveryServicesVaultExtendedInfo.UpdateAsync(info);
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+RecoveryServicesVaultExtendedInfoData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

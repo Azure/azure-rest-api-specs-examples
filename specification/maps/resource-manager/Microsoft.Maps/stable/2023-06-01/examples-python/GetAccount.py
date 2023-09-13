b61b9cd1638d128cf75a6bef4909bb0d71157987
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-13 20:01:08 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-maps_2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/maps/resource-manager/Microsoft.Maps/stable/2023-06-01/examples-python/GetAccount.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.maps import AzureMapsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-maps
+# USAGE
+    python get_account.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureMapsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="21a9967a-e8a9-4656-a70b-96ff1c4d05a0",
+    )
+
+    response = client.accounts.get(
+        resource_group_name="myResourceGroup",
+        account_name="myMapsAccount",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/stable/2023-06-01/examples/GetAccount.json
+if __name__ == "__main__":
+    main()

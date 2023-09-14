26e3177a2a0100e484d3b1868afb3823a13fc908
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-14 20:01:16 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservicefleet_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-06-15-preview/examples-js/Fleets_PatchTags.js
@@ -0,0 +1,26 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Update a Fleet
+ *
+ * @summary Update a Fleet
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-06-15-preview/examples/Fleets_PatchTags.json
+ */
+async function updateAFleet() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const ifMatch = "dfjkwelr7384";
+  const fleetName = "fleet1";
+  const properties = { tags: { env: "prod", tier: "secure" } };
+  const options = { ifMatch };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const result = await client.fleets.beginUpdateAndWait(
+    resourceGroupName,
+    fleetName,
+    properties,
+    options
+  );
+  console.log(result);
+}

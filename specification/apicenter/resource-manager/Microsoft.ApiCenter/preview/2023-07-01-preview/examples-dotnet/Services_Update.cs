8d924a6e8bed8f7712e66905c7f9e2c3cf9f0eed
azure-sdk <azuresdk@microsoft.com>
2023-09-15 12:13:00 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ApiCenter_1.0.0-beta.1

---
+++ specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2023-07-01-preview/examples-dotnet/Services_Update.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ApiCenter;
+using Azure.ResourceManager.ApiCenter.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2023-07-01-preview/examples/Services_Update.json
+// this example is just showing the usage of "Services_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ApiCenterServiceResource created on azure
+// for more information of creating ApiCenterServiceResource, please refer to the document of ApiCenterServiceResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "contoso-resources";
+string serviceName = "contoso";
+ResourceIdentifier apiCenterServiceResourceId = ApiCenterServiceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, serviceName);
+ApiCenterServiceResource apiCenterService = client.GetApiCenterServiceResource(apiCenterServiceResourceId);
+
+// invoke the operation
+ApiCenterServicePatch patch = new ApiCenterServicePatch();
+ApiCenterServiceResource result = await apiCenterService.UpdateAsync(patch);
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ApiCenterServiceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

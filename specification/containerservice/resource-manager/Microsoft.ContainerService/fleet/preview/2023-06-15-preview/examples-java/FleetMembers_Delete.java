7f06c1999e14892778057f26db812c022f94dc0e
azure-sdk <azuresdk@microsoft.com>
2023-09-15 12:08:05 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-containerservicefleet_1.0.0-beta.2

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-06-15-preview/examples-java/FleetMembers_Delete.java
@@ -0,0 +1,15 @@
+/** Samples for FleetMembers Delete. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-06-15-preview/examples/FleetMembers_Delete.json
+     */
+    /**
+     * Sample code: Deletes a FleetMember resource asynchronously with a long running operation.
+     *
+     * @param manager Entry point to ContainerServiceFleetManager.
+     */
+    public static void deletesAFleetMemberResourceAsynchronouslyWithALongRunningOperation(
+        com.azure.resourcemanager.containerservicefleet.ContainerServiceFleetManager manager) {
+        manager.fleetMembers().delete("rg1", "fleet1", "member-1", null, com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-06-15-preview/examples-java/Fleets_ListBySub.java
@@ -0,0 +1,15 @@
+/** Samples for Fleets List. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-06-15-preview/examples/Fleets_ListBySub.json
+     */
+    /**
+     * Sample code: Lists the Fleet resources in a subscription.
+     *
+     * @param manager Entry point to ContainerServiceFleetManager.
+     */
+    public static void listsTheFleetResourcesInASubscription(
+        com.azure.resourcemanager.containerservicefleet.ContainerServiceFleetManager manager) {
+        manager.fleets().list(com.azure.core.util.Context.NONE);
+    }
+}

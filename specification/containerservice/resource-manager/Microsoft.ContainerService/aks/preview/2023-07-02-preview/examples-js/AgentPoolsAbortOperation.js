03fbf5897dd57431c70bdd4c5abe5336eb718ff8
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-16 20:01:59 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.4.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-07-02-preview/examples-js/AgentPoolsAbortOperation.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Aborts the currently running operation on the agent pool. The Agent Pool will be moved to a Canceling state and eventually to a Canceled state when cancellation finishes. If the operation completes before cancellation can take place, an error is returned.
+ *
+ * @summary Aborts the currently running operation on the agent pool. The Agent Pool will be moved to a Canceling state and eventually to a Canceled state when cancellation finishes. If the operation completes before cancellation can take place, an error is returned.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-07-02-preview/examples/AgentPoolsAbortOperation.json
+ */
+async function abortOperationOnAgentPool() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const agentPoolName = "agentpool1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.agentPools.beginAbortLatestOperationAndWait(
+    resourceGroupName,
+    resourceName,
+    agentPoolName
+  );
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-07-02-preview/examples-js/ManagedClustersCreate_SecurityProfile.js
@@ -0,0 +1,56 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates a managed cluster.
+ *
+ * @summary Creates or updates a managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-07-02-preview/examples/ManagedClustersCreate_SecurityProfile.json
+ */
+async function createManagedClusterWithSecurityProfileConfigured() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const parameters = {
+    agentPoolProfiles: [
+      {
+        name: "nodepool1",
+        type: "VirtualMachineScaleSets",
+        count: 3,
+        enableNodePublicIP: true,
+        mode: "System",
+        osType: "Linux",
+        vmSize: "Standard_DS2_v2",
+      },
+    ],
+    dnsPrefix: "dnsprefix1",
+    kubernetesVersion: "",
+    linuxProfile: {
+      adminUsername: "azureuser",
+      ssh: { publicKeys: [{ keyData: "keydata" }] },
+    },
+    location: "location1",
+    networkProfile: {
+      loadBalancerProfile: { managedOutboundIPs: { count: 2 } },
+      loadBalancerSku: "standard",
+      outboundType: "loadBalancer",
+    },
+    securityProfile: {
+      defender: {
+        logAnalyticsWorkspaceResourceId:
+          "/subscriptions/SUB_ID/resourcegroups/RG_NAME/providers/microsoft.operationalinsights/workspaces/WORKSPACE_NAME",
+        securityMonitoring: { enabled: true },
+      },
+    },
+    sku: { name: "Basic", tier: "Free" },
+    tags: { archv2: "", tier: "production" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    resourceName,
+    parameters
+  );
+  console.log(result);
+}

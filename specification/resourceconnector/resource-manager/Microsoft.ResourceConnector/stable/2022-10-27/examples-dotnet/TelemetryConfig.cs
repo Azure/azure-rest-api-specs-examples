f97c7aad10b0414d13d9f890d7a1a43ef123f244
azure-sdk <azuresdk@microsoft.com>
2023-09-16 12:09:42 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceConnector_1.0.0-beta.1

---
+++ specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples-dotnet/TelemetryConfig.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ResourceConnector;
+using Azure.ResourceManager.ResourceConnector.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/TelemetryConfig.json
+// this example is just showing the usage of "Appliances_GetTelemetryConfig" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "11111111-2222-3333-4444-555555555555";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation
+ApplianceTelemetryConfigResult result = await subscriptionResource.GetTelemetryConfigApplianceAsync();
+
+Console.WriteLine($"Succeeded: {result}");

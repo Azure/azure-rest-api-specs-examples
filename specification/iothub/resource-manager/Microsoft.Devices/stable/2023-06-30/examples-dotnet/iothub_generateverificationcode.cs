14050403c495e2ab1ed8a573732ad38d89020b38
azure-sdk <azuresdk@microsoft.com>
2023-09-18 12:11:29 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.IotHub_1.1.0

---
+++ specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples-dotnet/iothub_generateverificationcode.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.IotHub;
+using Azure.ResourceManager.IotHub.Models;
+
+// Generated from example definition: specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples/iothub_generateverificationcode.json
+// this example is just showing the usage of "Certificates_GenerateVerificationCode" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this IotHubCertificateDescriptionResource created on azure
+// for more information of creating IotHubCertificateDescriptionResource, please refer to the document of IotHubCertificateDescriptionResource
+string subscriptionId = "91d12660-3dec-467a-be2a-213b5544ddc0";
+string resourceGroupName = "myResourceGroup";
+string resourceName = "testHub";
+string certificateName = "cert";
+ResourceIdentifier iotHubCertificateDescriptionResourceId = IotHubCertificateDescriptionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, certificateName);
+IotHubCertificateDescriptionResource iotHubCertificateDescription = client.GetIotHubCertificateDescriptionResource(iotHubCertificateDescriptionResourceId);
+
+// invoke the operation
+string ifMatch = "AAAAAAAADGk=";
+IotHubCertificateWithNonceDescription result = await iotHubCertificateDescription.GenerateVerificationCodeAsync(ifMatch);
+
+Console.WriteLine($"Succeeded: {result}");

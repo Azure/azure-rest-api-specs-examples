1143d9bb1a992a2986224e3f781f803d3570902b
azure-sdk <azuresdk@microsoft.com>
2023-09-19 12:10:57 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-signalr_6.0.0-beta.1

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples-js/SignalRCustomDomains_List.js
@@ -0,0 +1,22 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List all custom domains.
+ *
+ * @summary List all custom domains.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples/SignalRCustomDomains_List.json
+ */
+async function signalRCustomDomainsList() {
+  const subscriptionId =
+    process.env["SIGNALR_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["SIGNALR_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.signalRCustomDomains.list(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples-js/SignalRReplicas_Update.js
@@ -0,0 +1,30 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Operation to update an exiting replica.
+ *
+ * @summary Operation to update an exiting replica.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples/SignalRReplicas_Update.json
+ */
+async function signalRReplicasUpdate() {
+  const subscriptionId =
+    process.env["SIGNALR_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["SIGNALR_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const replicaName = "mySignalRService-eastus";
+  const parameters = {
+    location: "eastus",
+    sku: { name: "Premium_P1", capacity: 1, tier: "Premium" },
+    tags: { key1: "value1" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const result = await client.signalRReplicas.beginUpdateAndWait(
+    resourceGroupName,
+    resourceName,
+    replicaName,
+    parameters
+  );
+  console.log(result);
+}

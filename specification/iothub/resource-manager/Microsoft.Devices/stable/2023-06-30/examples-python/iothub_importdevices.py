4f347c91eafa466e7955e9455bfc9a94cb6ed078
azure-sdk <azuresdk@microsoft.com>
2023-09-19 12:24:07 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-iothub_3.0.0

---
+++ specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples-python/iothub_importdevices.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.iothub import IotHubClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-iothub
+# USAGE
+    python iothub_importdevices.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = IotHubClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="91d12660-3dec-467a-be2a-213b5544ddc0",
+    )
+
+    response = client.iot_hub_resource.import_devices(
+        resource_group_name="myResourceGroup",
+        resource_name="testHub",
+        import_devices_parameters={"inputBlobContainerUri": "testBlob", "outputBlobContainerUri": "testBlob"},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples/iothub_importdevices.json
+if __name__ == "__main__":
+    main()

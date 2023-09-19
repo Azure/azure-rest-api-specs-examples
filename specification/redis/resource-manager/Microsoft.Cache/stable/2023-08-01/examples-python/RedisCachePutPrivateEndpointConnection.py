878a7ffc9527aec20fe0c2e460996805482045f0
azure-sdk <azuresdk@microsoft.com>
2023-09-19 12:19:34 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-redis_14.3.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2023-08-01/examples-python/RedisCachePutPrivateEndpointConnection.py
@@ -0,0 +1,37 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.redis import RedisManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-redis
+# USAGE
+    python redis_cache_put_private_endpoint_connection.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RedisManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscriptionId}",
+    )
+
+    response = client.private_endpoint_connections.begin_put(
+        resource_group_name="rgtest01",
+        cache_name="cachetest01",
+        private_endpoint_connection_name="pectest01",
+        properties={
+            "properties": {"privateLinkServiceConnectionState": {"description": "Auto-Approved", "status": "Approved"}}
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2023-08-01/examples/RedisCachePutPrivateEndpointConnection.json
+if __name__ == "__main__":
+    main()

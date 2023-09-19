958c4ce67d240e58a086dcc846e79b156390114a
azure-sdk <azuresdk@microsoft.com>
2023-09-19 12:23:14 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-signalr_2.0.0b1

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples-python/Usages_List.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.signalr import SignalRManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-signalr
+# USAGE
+    python usages_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SignalRManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.usages.list(
+        location="eastus",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples/Usages_List.json
+if __name__ == "__main__":
+    main()

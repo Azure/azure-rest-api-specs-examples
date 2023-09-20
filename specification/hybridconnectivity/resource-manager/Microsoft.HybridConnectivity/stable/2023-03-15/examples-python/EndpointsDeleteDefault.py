c733bdba3cda553b8e5db96b6d284bb1bec65a49
azure-sdk <azuresdk@microsoft.com>
2023-09-20 12:25:22 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-hybridconnectivity_1.0.0

---
+++ specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/stable/2023-03-15/examples-python/EndpointsDeleteDefault.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.hybridconnectivity import HybridConnectivityMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hybridconnectivity
+# USAGE
+    python endpoints_delete_default.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HybridConnectivityMgmtClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    client.endpoints.delete(
+        resource_uri="subscriptions/f5bcc1d9-23af-4ae9-aca1-041d0f593a63/resourceGroups/hybridRG/providers/Microsoft.HybridCompute/machines/testMachine",
+        endpoint_name="default",
+    )
+
+
+# x-ms-original-file: specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/stable/2023-03-15/examples/EndpointsDeleteDefault.json
+if __name__ == "__main__":
+    main()

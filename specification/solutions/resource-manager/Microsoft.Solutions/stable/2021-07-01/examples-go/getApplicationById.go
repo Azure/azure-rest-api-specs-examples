0c15129d017d13c2683fc76b0ee0ac75c4575297
azure-sdk <azuresdk@microsoft.com>
2023-09-20 12:10:35 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/solutions/armmanagedapplications/v2.0.0

--- specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/getApplicationById.go
+++ specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/getApplicationById.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"log"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/solutions/armmanagedapplications/v2"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/5fb045bd44f143bae17da2e01552ae531f77d0ba/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/createOrUpdateApplication.json
-func ExampleApplicationsClient_BeginCreateOrUpdate() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/5fb045bd44f143bae17da2e01552ae531f77d0ba/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/getApplicationById.json
+func ExampleApplicationsClient_GetByID() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -20,19 +19,9 @@ func ExampleApplicationsClient_BeginCreateOrUpdate() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := clientFactory.NewApplicationsClient().BeginCreateOrUpdate(ctx, "rg", "myManagedApplication", armmanagedapplications.Application{
-		Kind: to.Ptr("ServiceCatalog"),
-		Properties: &armmanagedapplications.ApplicationProperties{
-			ApplicationDefinitionID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.Solutions/applicationDefinitions/myAppDef"),
-			ManagedResourceGroupID:  to.Ptr("/subscriptions/subid/resourceGroups/myManagedRG"),
-		},
-	}, nil)
+	res, err := clientFactory.NewApplicationsClient().GetByID(ctx, "subscriptions/subid/resourceGroups/rg/providers/Microsoft.Solutions/applications/myManagedApplication", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
-	}
-	res, err := poller.PollUntilDone(ctx, nil)
-	if err != nil {
-		log.Fatalf("failed to pull the result: %v", err)
 	}
 	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
@@ -43,9 +32,27 @@ func ExampleApplicationsClient_BeginCreateOrUpdate() {
 	// 	ID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.Solutions/applications/myManagedApplication"),
 	// 	Kind: to.Ptr("ServiceCatalog"),
 	// 	Properties: &armmanagedapplications.ApplicationProperties{
-	// 		ApplicationDefinitionID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.Solutions/applicationDefinitions/myAppDef"),
+	// 		ApplicationDefinitionID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.Solutions/managedApplicationDefinitions/myAppDef"),
+	// 		Artifacts: []*armmanagedapplications.ApplicationArtifact{
+	// 		},
+	// 		Authorizations: []*armmanagedapplications.ApplicationAuthorization{
+	// 			{
+	// 				PrincipalID: to.Ptr("validprincipalguid"),
+	// 				RoleDefinitionID: to.Ptr("validroleguid"),
+	// 		}},
+	// 		CreatedBy: &armmanagedapplications.ApplicationClientDetails{
+	// 			ApplicationID: to.Ptr("ClientApplicationId"),
+	// 			Oid: to.Ptr("ClientOid"),
+	// 			Puid: to.Ptr("ClientPuid"),
+	// 		},
 	// 		ManagedResourceGroupID: to.Ptr("/subscriptions/subid/resourceGroups/myManagedRG"),
+	// 		ManagementMode: to.Ptr(armmanagedapplications.ApplicationManagementModeManaged),
 	// 		ProvisioningState: to.Ptr(armmanagedapplications.ProvisioningStateSucceeded),
+	// 		UpdatedBy: &armmanagedapplications.ApplicationClientDetails{
+	// 			ApplicationID: to.Ptr("ClientApplicationId"),
+	// 			Oid: to.Ptr("ClientOid"),
+	// 			Puid: to.Ptr("ClientPuid"),
+	// 		},
 	// 	},
 	// }
 }
--- specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/listAllowedUpgradePlans.go
+++ specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-go/listAllowedUpgradePlans.go
@@ -5,10 +5,10 @@ import (
 	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/solutions/armmanagedapplications"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/solutions/armmanagedapplications/v2"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/7a2ac91de424f271cf91cc8009f3fe9ee8249086/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/listAllowedUpgradePlans.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/5fb045bd44f143bae17da2e01552ae531f77d0ba/specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/listAllowedUpgradePlans.json
 func ExampleApplicationsClient_ListAllowedUpgradePlans() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -19,8 +19,21 @@ func ExampleApplicationsClient_ListAllowedUpgradePlans() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = clientFactory.NewApplicationsClient().ListAllowedUpgradePlans(ctx, "rg", "myManagedApplication", nil)
+	res, err := clientFactory.NewApplicationsClient().ListAllowedUpgradePlans(ctx, "rg", "myManagedApplication", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.AllowedUpgradePlansResult = armmanagedapplications.AllowedUpgradePlansResult{
+	// 	Value: []*armmanagedapplications.Plan{
+	// 		{
+	// 			Name: to.Ptr("name"),
+	// 			Product: to.Ptr("product"),
+	// 			PromotionCode: to.Ptr("promotionCode"),
+	// 			Publisher: to.Ptr("publisher"),
+	// 			Version: to.Ptr("version"),
+	// 	}},
+	// }
 }

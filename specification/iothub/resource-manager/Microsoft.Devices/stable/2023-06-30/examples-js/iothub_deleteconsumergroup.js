b28da30d1d0a82279a4c852976f7439869e0c97c
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-20 20:02:33 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-iothub_6.3.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples-js/iothub_deleteconsumergroup.js
@@ -0,0 +1,26 @@
+const { IotHubClient } = require("@azure/arm-iothub");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Delete a consumer group from an Event Hub-compatible endpoint in an IoT hub.
+ *
+ * @summary Delete a consumer group from an Event Hub-compatible endpoint in an IoT hub.
+ * x-ms-original-file: specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples/iothub_deleteconsumergroup.json
+ */
+async function iotHubResourceDeleteEventHubConsumerGroup() {
+  const subscriptionId =
+    process.env["IOTHUB_SUBSCRIPTION_ID"] || "91d12660-3dec-467a-be2a-213b5544ddc0";
+  const resourceGroupName = process.env["IOTHUB_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "testHub";
+  const eventHubEndpointName = "events";
+  const name = "test";
+  const credential = new DefaultAzureCredential();
+  const client = new IotHubClient(credential, subscriptionId);
+  const result = await client.iotHubResource.deleteEventHubConsumerGroup(
+    resourceGroupName,
+    resourceName,
+    eventHubEndpointName,
+    name
+  );
+  console.log(result);
+}
---
+++ specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples-js/iothub_listkeys.js
@@ -0,0 +1,22 @@
+const { IotHubClient } = require("@azure/arm-iothub");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get the security metadata for an IoT hub. For more information, see: https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security.
+ *
+ * @summary Get the security metadata for an IoT hub. For more information, see: https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security.
+ * x-ms-original-file: specification/iothub/resource-manager/Microsoft.Devices/stable/2023-06-30/examples/iothub_listkeys.json
+ */
+async function iotHubResourceListKeys() {
+  const subscriptionId =
+    process.env["IOTHUB_SUBSCRIPTION_ID"] || "91d12660-3dec-467a-be2a-213b5544ddc0";
+  const resourceGroupName = process.env["IOTHUB_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "testHub";
+  const credential = new DefaultAzureCredential();
+  const client = new IotHubClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.iotHubResource.listKeys(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

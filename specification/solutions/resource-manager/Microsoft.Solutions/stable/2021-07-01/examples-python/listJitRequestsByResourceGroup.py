918a91de62247f58553c0543cc458dff4aeea7b9
azure-sdk <azuresdk@microsoft.com>
2023-09-20 12:27:10 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-managedapplications_1.0.0b1

---
+++ specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-python/listJitRequestsByResourceGroup.py
@@ -0,0 +1,30 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.managedapplications import ManagedApplicationsMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-managedapplications
+# USAGE
+    python list_jit_requests_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ManagedApplicationsMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.jit_requests.list_by_subscription()
+    print(response)
+
+
+# x-ms-original-file: specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/listJitRequestsByResourceGroup.json
+if __name__ == "__main__":
+    main()
---
+++ specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples-python/updateApplication.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.managedapplications import ManagedApplicationsMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-managedapplications
+# USAGE
+    python update_application.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ManagedApplicationsMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.applications.begin_update(
+        resource_group_name="rg",
+        application_name="myManagedApplication",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/solutions/resource-manager/Microsoft.Solutions/stable/2021-07-01/examples/updateApplication.json
+if __name__ == "__main__":
+    main()

74338157818fd1c8e587d266b54e447adecc36da
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-21 20:01:55 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-communication_4.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2023-04-01-preview/examples-js/communicationServices/updateWithUserAssignedIdentity.js
@@ -0,0 +1,30 @@
+const { CommunicationServiceManagementClient } = require("@azure/arm-communication");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Operation to update an existing CommunicationService.
+ *
+ * @summary Operation to update an existing CommunicationService.
+ * x-ms-original-file: specification/communication/resource-manager/Microsoft.Communication/preview/2023-04-01-preview/examples/communicationServices/updateWithUserAssignedIdentity.json
+ */
+async function updateResourceToAddAUserAssignedManagedIdentity() {
+  const subscriptionId =
+    process.env["COMMUNICATION_SUBSCRIPTION_ID"] || "11112222-3333-4444-5555-666677778888";
+  const resourceGroupName = process.env["COMMUNICATION_RESOURCE_GROUP"] || "MyResourceGroup";
+  const communicationServiceName = "MyCommunicationResource";
+  const parameters = {
+    identity: {
+      type: "UserAssigned",
+      userAssignedIdentities: { "/user/assigned/resource/id": {} },
+    },
+    tags: { newTag: "newVal" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new CommunicationServiceManagementClient(credential, subscriptionId);
+  const result = await client.communicationServices.update(
+    resourceGroupName,
+    communicationServiceName,
+    parameters
+  );
+  console.log(result);
+}

f7f3ed592398d88a3010f99034cc71f3f13900c7
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-21 20:02:47 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/communication/armcommunication/v2.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2023-04-01-preview/examples-go/communicationServices/get.go
@@ -0,0 +1,46 @@
+package armcommunication_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/communication/armcommunication/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0d7b535d1273b18623ca0d63a6ebb0456dab95ba/specification/communication/resource-manager/Microsoft.Communication/preview/2023-04-01-preview/examples/communicationServices/get.json
+func ExampleServicesClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcommunication.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewServicesClient().Get(ctx, "MyResourceGroup", "MyCommunicationResource", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ServiceResource = armcommunication.ServiceResource{
+	// 	Name: to.Ptr("MyCommunicationResource"),
+	// 	Type: to.Ptr("Microsoft.Communication/CommunicationServices"),
+	// 	ID: to.Ptr("/subscriptions/11112222-3333-4444-5555-666677778888/resourceGroups/MyResourceGroup/providers/Microsoft.Communication/CommunicationServices/MyCommunicationResource"),
+	// 	Location: to.Ptr("Global"),
+	// 	Identity: &armcommunication.ManagedServiceIdentity{
+	// 		Type: to.Ptr(armcommunication.ManagedServiceIdentityTypeSystemAssigned),
+	// 		PrincipalID: to.Ptr("11111111-1111-1111-1111-111111111111"),
+	// 		TenantID: to.Ptr("22222222-2222-2222-2222-222222222222"),
+	// 	},
+	// 	Properties: &armcommunication.ServiceProperties{
+	// 		DataLocation: to.Ptr("United States"),
+	// 		HostName: to.Ptr("mycommunicationservice.comms.azure.net"),
+	// 		ProvisioningState: to.Ptr(armcommunication.CommunicationServicesProvisioningStateSucceeded),
+	// 		Version: to.Ptr("0.2.0"),
+	// 	},
+	// }
+}
---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2023-04-01-preview/examples-go/domains/listByEmailService.go
@@ -0,0 +1,74 @@
+package armcommunication_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/communication/armcommunication/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/0d7b535d1273b18623ca0d63a6ebb0456dab95ba/specification/communication/resource-manager/Microsoft.Communication/preview/2023-04-01-preview/examples/domains/listByEmailService.json
+func ExampleDomainsClient_NewListByEmailServiceResourcePager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcommunication.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewDomainsClient().NewListByEmailServiceResourcePager("MyResourceGroup", "MyEmailServiceResource", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.DomainResourceList = armcommunication.DomainResourceList{
+		// 	Value: []*armcommunication.DomainResource{
+		// 		{
+		// 			Name: to.Ptr("mydomain.com"),
+		// 			Type: to.Ptr("Microsoft.Communication/EmailServices/Domains"),
+		// 			ID: to.Ptr("/subscriptions/11112222-3333-4444-5555-666677778888/resourceGroups/MyResourceGroup/providers/Microsoft.Communication/EmailServices/MyEmailServiceResource/Domains/mydomain.com"),
+		// 			Location: to.Ptr("Global"),
+		// 			Properties: &armcommunication.DomainProperties{
+		// 				DataLocation: to.Ptr("United States"),
+		// 				DomainManagement: to.Ptr(armcommunication.DomainManagementCustomerManaged),
+		// 				FromSenderDomain: to.Ptr("mydomain.com"),
+		// 				MailFromSenderDomain: to.Ptr("mydomain.com"),
+		// 				ProvisioningState: to.Ptr(armcommunication.DomainsProvisioningStateSucceeded),
+		// 				VerificationRecords: &armcommunication.DomainPropertiesVerificationRecords{
+		// 					Domain: &armcommunication.DNSRecord{
+		// 						Name: to.Ptr("recordName"),
+		// 						Type: to.Ptr("TXT"),
+		// 						TTL: to.Ptr[int32](3600),
+		// 						Value: to.Ptr("recordValue"),
+		// 					},
+		// 					SPF: &armcommunication.DNSRecord{
+		// 						Name: to.Ptr("recordName"),
+		// 						Type: to.Ptr("TXT"),
+		// 						TTL: to.Ptr[int32](3600),
+		// 						Value: to.Ptr("recordValue"),
+		// 					},
+		// 				},
+		// 				VerificationStates: &armcommunication.DomainPropertiesVerificationStates{
+		// 					Domain: &armcommunication.VerificationStatusRecord{
+		// 						ErrorCode: to.Ptr(""),
+		// 						Status: to.Ptr(armcommunication.VerificationStatusVerified),
+		// 					},
+		// 					SPF: &armcommunication.VerificationStatusRecord{
+		// 						ErrorCode: to.Ptr(""),
+		// 						Status: to.Ptr(armcommunication.VerificationStatusNotStarted),
+		// 					},
+		// 				},
+		// 			},
+		// 	}},
+		// }
+	}
+}

8c7e6b0397427cac1e3fd518febbdf90f1ceeb6b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-21 20:01:25 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_30.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-07-01/examples-python/restorePointExamples/RestorePointCollection_ListBySubscription.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python restore_point_collection_list_by_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.restore_point_collections.list_all()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-07-01/examples/restorePointExamples/RestorePointCollection_ListBySubscription.json
+if __name__ == "__main__":
+    main()

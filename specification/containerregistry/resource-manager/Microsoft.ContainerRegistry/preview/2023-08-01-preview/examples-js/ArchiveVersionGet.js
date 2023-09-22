9a3bd4e0eea3444479df9d9e55cc7649aee87303
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-22 20:01:06 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerregistry_11.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-08-01-preview/examples-js/ArchiveVersionGet.js
@@ -0,0 +1,28 @@
+const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the properties of the archive version.
+ *
+ * @summary Gets the properties of the archive version.
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-08-01-preview/examples/ArchiveVersionGet.json
+ */
+async function archiveVersionGet() {
+  const subscriptionId =
+    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERREGISTRY_RESOURCE_GROUP"] || "myResourceGroup";
+  const registryName = "myRegistry";
+  const packageType = "rpm";
+  const archiveName = "myArchiveName";
+  const archiveVersionName = "myArchiveVersionName";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  const result = await client.archiveVersions.get(
+    resourceGroupName,
+    registryName,
+    packageType,
+    archiveName,
+    archiveVersionName
+  );
+  console.log(result);
+}
---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-08-01-preview/examples-js/RegistryCreate.js
@@ -0,0 +1,29 @@
+const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates a container registry with the specified parameters.
+ *
+ * @summary Creates a container registry with the specified parameters.
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-08-01-preview/examples/RegistryCreate.json
+ */
+async function registryCreate() {
+  const subscriptionId =
+    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERREGISTRY_RESOURCE_GROUP"] || "myResourceGroup";
+  const registryName = "myRegistry";
+  const registry = {
+    adminUserEnabled: true,
+    location: "westus",
+    sku: { name: "Standard" },
+    tags: { key: "value" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  const result = await client.registries.beginCreateAndWait(
+    resourceGroupName,
+    registryName,
+    registry
+  );
+  console.log(result);
+}

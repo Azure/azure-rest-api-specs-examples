6d7f94dd9f477b19a6dc8fa056ecd3a4a6e0a781
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-22 20:01:37 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/cosmosforpostgresql/armcosmosforpostgresql/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples-go/ClusterCreateMultiNode.go
+++ specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples-go/ClusterCreateMultiNode.go
@@ -9,8 +9,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cosmosforpostgresql/armcosmosforpostgresql"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/80c21c17b4a7aa57f637ee594f7cfd653255a7e0/specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/ClusterAddNode.json
-func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/80c21c17b4a7aa57f637ee594f7cfd653255a7e0/specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/ClusterCreateMultiNode.json
+func ExampleClustersClient_BeginCreate_createANewMultiNodeCluster() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -20,9 +20,25 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := clientFactory.NewClustersClient().BeginUpdate(ctx, "TestGroup", "testcluster", armcosmosforpostgresql.ClusterForUpdate{
-		Properties: &armcosmosforpostgresql.ClusterPropertiesForUpdate{
-			NodeCount: to.Ptr[int32](2),
+	poller, err := clientFactory.NewClustersClient().BeginCreate(ctx, "TestGroup", "testcluster-multinode", armcosmosforpostgresql.Cluster{
+		Location: to.Ptr("westus"),
+		Tags:     map[string]*string{},
+		Properties: &armcosmosforpostgresql.ClusterProperties{
+			AdministratorLoginPassword:      to.Ptr("password"),
+			CitusVersion:                    to.Ptr("11.1"),
+			CoordinatorEnablePublicIPAccess: to.Ptr(true),
+			CoordinatorServerEdition:        to.Ptr("GeneralPurpose"),
+			CoordinatorStorageQuotaInMb:     to.Ptr[int32](524288),
+			CoordinatorVCores:               to.Ptr[int32](4),
+			EnableHa:                        to.Ptr(true),
+			EnableShardsOnCoordinator:       to.Ptr(false),
+			NodeCount:                       to.Ptr[int32](3),
+			NodeEnablePublicIPAccess:        to.Ptr(false),
+			NodeServerEdition:               to.Ptr("MemoryOptimized"),
+			NodeStorageQuotaInMb:            to.Ptr[int32](524288),
+			NodeVCores:                      to.Ptr[int32](8),
+			PostgresqlVersion:               to.Ptr("15"),
+			PreferredPrimaryZone:            to.Ptr("1"),
 		},
 	}, nil)
 	if err != nil {
@@ -36,19 +52,22 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res.Cluster = armcosmosforpostgresql.Cluster{
-	// 	Name: to.Ptr("testcluster"),
+	// 	Name: to.Ptr("testcluster-multinode"),
 	// 	Type: to.Ptr("Microsoft.DBforPostgreSQL/serverGroupsv2"),
-	// 	ID: to.Ptr("/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestResourceGroup/providers/Microsoft.DBforPostgreSQL/serverGroupsv2/testcluster"),
+	// 	ID: to.Ptr("/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestResourceGroup/providers/Microsoft.DBforPostgreSQL/serverGroupsv2/testcluster-multinode"),
 	// 	SystemData: &armcosmosforpostgresql.SystemData{
-	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-01-01T17:18:19.1234567Z"); return t}()),
 	// 		CreatedBy: to.Ptr("user1"),
 	// 		CreatedByType: to.Ptr(armcosmosforpostgresql.CreatedByTypeUser),
-	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-01-02T17:18:19.1234567Z"); return t}()),
 	// 		LastModifiedBy: to.Ptr("user2"),
 	// 		LastModifiedByType: to.Ptr(armcosmosforpostgresql.CreatedByTypeUser),
 	// 	},
 	// 	Location: to.Ptr("westus"),
+	// 	Tags: map[string]*string{
+	// 	},
 	// 	Properties: &armcosmosforpostgresql.ClusterProperties{
+	// 		AdministratorLogin: to.Ptr("citus"),
 	// 		CitusVersion: to.Ptr("11.1"),
 	// 		CoordinatorEnablePublicIPAccess: to.Ptr(true),
 	// 		CoordinatorServerEdition: to.Ptr("GeneralPurpose"),
@@ -62,7 +81,7 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	// 			StartHour: to.Ptr[int32](0),
 	// 			StartMinute: to.Ptr[int32](0),
 	// 		},
-	// 		NodeCount: to.Ptr[int32](2),
+	// 		NodeCount: to.Ptr[int32](3),
 	// 		NodeEnablePublicIPAccess: to.Ptr(false),
 	// 		NodeServerEdition: to.Ptr("MemoryOptimized"),
 	// 		NodeStorageQuotaInMb: to.Ptr[int32](524288),
@@ -76,16 +95,20 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	// 		},
 	// 		ServerNames: []*armcosmosforpostgresql.ServerNameItem{
 	// 			{
-	// 				Name: to.Ptr("testcluster-c"),
-	// 				FullyQualifiedDomainName: to.Ptr("c.testcluster.postgres.database.azure.com"),
+	// 				Name: to.Ptr("testcluster-multinode-c"),
+	// 				FullyQualifiedDomainName: to.Ptr("c.testcluster-multinode.postgres.database.azure.com"),
 	// 			},
 	// 			{
-	// 				Name: to.Ptr("testcluster-w0"),
-	// 				FullyQualifiedDomainName: to.Ptr("w0.testcluster.postgres.database.azure.com"),
+	// 				Name: to.Ptr("testcluster-multinode-w0"),
+	// 				FullyQualifiedDomainName: to.Ptr("w0.testcluster-multinode.postgres.database.azure.com"),
 	// 			},
 	// 			{
-	// 				Name: to.Ptr("testcluster-w1"),
-	// 				FullyQualifiedDomainName: to.Ptr("w1.testcluster.postgres.database.azure.com"),
+	// 				Name: to.Ptr("testcluster-multinode-w2"),
+	// 				FullyQualifiedDomainName: to.Ptr("w0.testcluster-multinode.postgres.database.azure.com"),
+	// 			},
+	// 			{
+	// 				Name: to.Ptr("testcluster-multinode-w3"),
+	// 				FullyQualifiedDomainName: to.Ptr("w0.testcluster-multinode.postgres.database.azure.com"),
 	// 		}},
 	// 		State: to.Ptr("Provisioning"),
 	// 	},
--- specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples-go/ClusterCreateSingleNode.go
+++ specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples-go/ClusterCreateSingleNode.go
@@ -9,8 +9,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cosmosforpostgresql/armcosmosforpostgresql"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/80c21c17b4a7aa57f637ee594f7cfd653255a7e0/specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/ClusterAddNode.json
-func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/80c21c17b4a7aa57f637ee594f7cfd653255a7e0/specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/ClusterCreateSingleNode.json
+func ExampleClustersClient_BeginCreate_createANewSingleNodeCluster() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -20,9 +20,23 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := clientFactory.NewClustersClient().BeginUpdate(ctx, "TestGroup", "testcluster", armcosmosforpostgresql.ClusterForUpdate{
-		Properties: &armcosmosforpostgresql.ClusterPropertiesForUpdate{
-			NodeCount: to.Ptr[int32](2),
+	poller, err := clientFactory.NewClustersClient().BeginCreate(ctx, "TestGroup", "testcluster-singlenode", armcosmosforpostgresql.Cluster{
+		Location: to.Ptr("westus"),
+		Tags: map[string]*string{
+			"owner": to.Ptr("JohnDoe"),
+		},
+		Properties: &armcosmosforpostgresql.ClusterProperties{
+			AdministratorLoginPassword:      to.Ptr("password"),
+			CitusVersion:                    to.Ptr("11.3"),
+			CoordinatorEnablePublicIPAccess: to.Ptr(true),
+			CoordinatorServerEdition:        to.Ptr("GeneralPurpose"),
+			CoordinatorStorageQuotaInMb:     to.Ptr[int32](131072),
+			CoordinatorVCores:               to.Ptr[int32](8),
+			EnableHa:                        to.Ptr(true),
+			EnableShardsOnCoordinator:       to.Ptr(true),
+			NodeCount:                       to.Ptr[int32](0),
+			PostgresqlVersion:               to.Ptr("15"),
+			PreferredPrimaryZone:            to.Ptr("1"),
 		},
 	}, nil)
 	if err != nil {
@@ -36,36 +50,40 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res.Cluster = armcosmosforpostgresql.Cluster{
-	// 	Name: to.Ptr("testcluster"),
+	// 	Name: to.Ptr("testcluster-singlenode"),
 	// 	Type: to.Ptr("Microsoft.DBforPostgreSQL/serverGroupsv2"),
-	// 	ID: to.Ptr("/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestResourceGroup/providers/Microsoft.DBforPostgreSQL/serverGroupsv2/testcluster"),
+	// 	ID: to.Ptr("/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestResourceGroup/providers/Microsoft.DBforPostgreSQL/serverGroupsv2/testcluster-singlenode"),
 	// 	SystemData: &armcosmosforpostgresql.SystemData{
-	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-01-01T17:18:19.1234567Z"); return t}()),
 	// 		CreatedBy: to.Ptr("user1"),
 	// 		CreatedByType: to.Ptr(armcosmosforpostgresql.CreatedByTypeUser),
-	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-01-02T17:18:19.1234567Z"); return t}()),
 	// 		LastModifiedBy: to.Ptr("user2"),
 	// 		LastModifiedByType: to.Ptr(armcosmosforpostgresql.CreatedByTypeUser),
 	// 	},
 	// 	Location: to.Ptr("westus"),
+	// 	Tags: map[string]*string{
+	// 		"owner": to.Ptr("JohnDoe"),
+	// 	},
 	// 	Properties: &armcosmosforpostgresql.ClusterProperties{
-	// 		CitusVersion: to.Ptr("11.1"),
+	// 		AdministratorLogin: to.Ptr("citus"),
+	// 		CitusVersion: to.Ptr("11.3"),
 	// 		CoordinatorEnablePublicIPAccess: to.Ptr(true),
 	// 		CoordinatorServerEdition: to.Ptr("GeneralPurpose"),
-	// 		CoordinatorStorageQuotaInMb: to.Ptr[int32](524288),
-	// 		CoordinatorVCores: to.Ptr[int32](4),
+	// 		CoordinatorStorageQuotaInMb: to.Ptr[int32](131072),
+	// 		CoordinatorVCores: to.Ptr[int32](8),
 	// 		EnableHa: to.Ptr(true),
-	// 		EnableShardsOnCoordinator: to.Ptr(false),
+	// 		EnableShardsOnCoordinator: to.Ptr(true),
 	// 		MaintenanceWindow: &armcosmosforpostgresql.MaintenanceWindow{
 	// 			CustomWindow: to.Ptr("Disabled"),
 	// 			DayOfWeek: to.Ptr[int32](0),
 	// 			StartHour: to.Ptr[int32](0),
 	// 			StartMinute: to.Ptr[int32](0),
 	// 		},
-	// 		NodeCount: to.Ptr[int32](2),
+	// 		NodeCount: to.Ptr[int32](0),
 	// 		NodeEnablePublicIPAccess: to.Ptr(false),
 	// 		NodeServerEdition: to.Ptr("MemoryOptimized"),
-	// 		NodeStorageQuotaInMb: to.Ptr[int32](524288),
+	// 		NodeStorageQuotaInMb: to.Ptr[int32](131072),
 	// 		NodeVCores: to.Ptr[int32](8),
 	// 		PostgresqlVersion: to.Ptr("15"),
 	// 		PreferredPrimaryZone: to.Ptr("1"),
@@ -76,16 +94,8 @@ func ExampleClustersClient_BeginUpdate_scaleOutAddNewWorkerNodes() {
 	// 		},
 	// 		ServerNames: []*armcosmosforpostgresql.ServerNameItem{
 	// 			{
-	// 				Name: to.Ptr("testcluster-c"),
-	// 				FullyQualifiedDomainName: to.Ptr("c.testcluster.postgres.database.azure.com"),
-	// 			},
-	// 			{
-	// 				Name: to.Ptr("testcluster-w0"),
-	// 				FullyQualifiedDomainName: to.Ptr("w0.testcluster.postgres.database.azure.com"),
-	// 			},
-	// 			{
-	// 				Name: to.Ptr("testcluster-w1"),
-	// 				FullyQualifiedDomainName: to.Ptr("w1.testcluster.postgres.database.azure.com"),
+	// 				Name: to.Ptr("testcluster-singlenode-c"),
+	// 				FullyQualifiedDomainName: to.Ptr("c.testcluster-singlenode.postgres.database.azure.com"),
 	// 		}},
 	// 		State: to.Ptr("Provisioning"),
 	// 	},

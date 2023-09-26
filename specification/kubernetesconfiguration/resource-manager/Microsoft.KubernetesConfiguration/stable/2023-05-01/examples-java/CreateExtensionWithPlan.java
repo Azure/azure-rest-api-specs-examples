401ba26f11b5c0f1186c539981c89698950b0ffb
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-26 20:01:35 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-kubernetesconfiguration_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2023-05-01/examples-java/CreateExtensionWithPlan.java
@@ -0,0 +1,49 @@
+import com.azure.resourcemanager.kubernetesconfiguration.fluent.models.ExtensionInner;
+import com.azure.resourcemanager.kubernetesconfiguration.models.Plan;
+import java.util.HashMap;
+import java.util.Map;
+
+/** Samples for Extensions Create. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2023-05-01/examples/CreateExtensionWithPlan.json
+     */
+    /**
+     * Sample code: Create Extension with Plan.
+     *
+     * @param manager Entry point to SourceControlConfigurationManager.
+     */
+    public static void createExtensionWithPlan(
+        com.azure.resourcemanager.kubernetesconfiguration.SourceControlConfigurationManager manager) {
+        manager
+            .extensions()
+            .create(
+                "rg1",
+                "Microsoft.Kubernetes",
+                "connectedClusters",
+                "clusterName1",
+                "azureVote",
+                new ExtensionInner()
+                    .withPlan(
+                        new Plan()
+                            .withName("azure-vote-standard")
+                            .withPublisher("Microsoft")
+                            .withProduct("azure-vote-standard-offer-id"))
+                    .withExtensionType("azure-vote")
+                    .withAutoUpgradeMinorVersion(true)
+                    .withReleaseTrain("Preview"),
+                com.azure.core.util.Context.NONE);
+    }
+
+    // Use "Map.of" if available
+    @SuppressWarnings("unchecked")
+    private static <T> Map<String, T> mapOf(Object... inputs) {
+        Map<String, T> map = new HashMap<>();
+        for (int i = 0; i < inputs.length; i += 2) {
+            String key = (String) inputs[i];
+            T value = (T) inputs[i + 1];
+            map.put(key, value);
+        }
+        return map;
+    }
+}
---
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2023-05-01/examples-java/OperationsList.java
@@ -0,0 +1,15 @@
+/** Samples for Operations List. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2023-05-01/examples/OperationsList.json
+     */
+    /**
+     * Sample code: BatchAccountDelete.
+     *
+     * @param manager Entry point to SourceControlConfigurationManager.
+     */
+    public static void batchAccountDelete(
+        com.azure.resourcemanager.kubernetesconfiguration.SourceControlConfigurationManager manager) {
+        manager.operations().list(com.azure.core.util.Context.NONE);
+    }
+}

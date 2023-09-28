5c1f9dde13a455d84fd1a4d176b924a22b86c956
azure-sdk <azuresdk@microsoft.com>
2023-09-28 12:19:31 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_9.3.0

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-09-15/examples-python/CosmosDBGremlinDatabaseDelete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_gremlin_database_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    client.gremlin_resources.begin_delete_gremlin_database(
+        resource_group_name="rg1",
+        account_name="ddb1",
+        database_name="databaseName",
+    ).result()
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-09-15/examples/CosmosDBGremlinDatabaseDelete.json
+if __name__ == "__main__":
+    main()

cfb6271a50bc794c7eef6835c3c7a9422f52bb9e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-09-28 20:02:18 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-datafactory_1.0.0-beta.23
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples-java/ChangeDataCapture_Stop.java
@@ -0,0 +1,20 @@
+/** Samples for ChangeDataCapture Stop. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples/ChangeDataCapture_Stop.json
+     */
+    /**
+     * Sample code: ChangeDataCapture_Stop.
+     *
+     * @param manager Entry point to DataFactoryManager.
+     */
+    public static void changeDataCaptureStop(com.azure.resourcemanager.datafactory.DataFactoryManager manager) {
+        manager
+            .changeDataCaptures()
+            .stopWithResponse(
+                "exampleResourceGroup",
+                "exampleFactoryName",
+                "exampleChangeDataCapture",
+                com.azure.core.util.Context.NONE);
+    }
+}

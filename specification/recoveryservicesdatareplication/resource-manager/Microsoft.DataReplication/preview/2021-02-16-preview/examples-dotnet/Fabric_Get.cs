4ca7df275ba75f4013284cd002d4a0fadb6c72e9
azure-sdk <azuresdk@microsoft.com>
2023-09-30 12:12:31 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.RecoveryServicesDataReplication_1.0.0-beta.1

---
+++ specification/recoveryservicesdatareplication/resource-manager/Microsoft.DataReplication/preview/2021-02-16-preview/examples-dotnet/Fabric_Get.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.RecoveryServicesDataReplication;
+using Azure.ResourceManager.RecoveryServicesDataReplication.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/recoveryservicesdatareplication/resource-manager/Microsoft.DataReplication/preview/2021-02-16-preview/examples/Fabric_Get.json
+// this example is just showing the usage of "Fabric_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DataReplicationFabricResource created on azure
+// for more information of creating DataReplicationFabricResource, please refer to the document of DataReplicationFabricResource
+string subscriptionId = "930CEC23-4430-4513-B855-DBA237E2F3BF";
+string resourceGroupName = "rgrecoveryservicesdatareplication";
+string fabricName = "wPR";
+ResourceIdentifier dataReplicationFabricResourceId = DataReplicationFabricResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, fabricName);
+DataReplicationFabricResource dataReplicationFabric = client.GetDataReplicationFabricResource(dataReplicationFabricResourceId);
+
+// invoke the operation
+DataReplicationFabricResource result = await dataReplicationFabric.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DataReplicationFabricData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/recoveryservicesdatareplication/resource-manager/Microsoft.DataReplication/preview/2021-02-16-preview/examples-dotnet/Vault_Update.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.RecoveryServicesDataReplication;
+using Azure.ResourceManager.RecoveryServicesDataReplication.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/recoveryservicesdatareplication/resource-manager/Microsoft.DataReplication/preview/2021-02-16-preview/examples/Vault_Update.json
+// this example is just showing the usage of "Vault_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DataReplicationVaultResource created on azure
+// for more information of creating DataReplicationVaultResource, please refer to the document of DataReplicationVaultResource
+string subscriptionId = "930CEC23-4430-4513-B855-DBA237E2F3BF";
+string resourceGroupName = "rgrecoveryservicesdatareplication";
+string vaultName = "4";
+ResourceIdentifier dataReplicationVaultResourceId = DataReplicationVaultResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, vaultName);
+DataReplicationVaultResource dataReplicationVault = client.GetDataReplicationVaultResource(dataReplicationVaultResourceId);
+
+// invoke the operation
+DataReplicationVaultPatch patch = new DataReplicationVaultPatch()
+{
+    Tags =
+    {
+    ["key8872"] = "pvtc",
+    },
+    Properties = new DataReplicationVaultProperties()
+    {
+        VaultType = DataReplicationReplicationVaultType.DisasterRecovery,
+    },
+};
+ArmOperation<DataReplicationVaultResource> lro = await dataReplicationVault.UpdateAsync(WaitUntil.Completed, patch);
+DataReplicationVaultResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DataReplicationVaultData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

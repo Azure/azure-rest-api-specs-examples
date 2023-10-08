c965d40ccc0ce4abb3528ea16cd3c8e450d76cd8
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-08 20:01:52 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-redhatopenshift_1.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/stable/2023-09-04/examples-python/Secrets_Get.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-redhatopenshift
+# USAGE
+    python secrets_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureRedHatOpenShiftClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscriptionId",
+    )
+
+    response = client.secrets.get(
+        resource_group_name="resourceGroup",
+        resource_name="resourceName",
+        child_resource_name="childResourceName",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/stable/2023-09-04/examples/Secrets_Get.json
+if __name__ == "__main__":
+    main()

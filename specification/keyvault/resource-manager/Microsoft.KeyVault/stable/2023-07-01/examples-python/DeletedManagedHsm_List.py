76c5ebce2a7bf8bcab8ba8aab5964f8bcb76c410
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-19 20:01:36 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-keyvault_10.3.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-07-01/examples-python/DeletedManagedHsm_List.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.keyvault import KeyVaultManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-keyvault
+# USAGE
+    python deleted_managed_hsm_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = KeyVaultManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.managed_hsms.list_deleted()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-07-01/examples/DeletedManagedHsm_List.json
+if __name__ == "__main__":
+    main()
---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-07-01/examples-python/createSecret.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.keyvault import KeyVaultManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-keyvault
+# USAGE
+    python create_secret.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = KeyVaultManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.secrets.create_or_update(
+        resource_group_name="sample-group",
+        vault_name="sample-vault",
+        secret_name="secret-name",
+        parameters={"properties": {"value": "secret-value"}},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/keyvault/resource-manager/Microsoft.KeyVault/stable/2023-07-01/examples/createSecret.json
+if __name__ == "__main__":
+    main()

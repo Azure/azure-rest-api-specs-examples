6811338a945ce37007f1e34c611bda1c13c660cc
azure-sdk <azuresdk@microsoft.com>
2023-10-20 12:12:36 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.4.0-beta.2

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-08-02-preview/examples-js/MachineList.js
@@ -0,0 +1,22 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a list of machines in the specified agent pool.
+ *
+ * @summary Gets a list of machines in the specified agent pool.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-08-02-preview/examples/MachineList.json
+ */
+async function listMachinesInAnAgentpoolByManagedCluster() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const agentPoolName = "agentpool1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.machines.list(resourceGroupName, resourceName, agentPoolName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-08-02-preview/examples-js/ManagedClusterSnapshotsUpdateTags.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates tags on a managed cluster snapshot.
+ *
+ * @summary Updates tags on a managed cluster snapshot.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-08-02-preview/examples/ManagedClusterSnapshotsUpdateTags.json
+ */
+async function updateManagedClusterSnapshotTags() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "snapshot1";
+  const parameters = { tags: { key2: "new-val2", key3: "val3" } };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusterSnapshots.updateTags(
+    resourceGroupName,
+    resourceName,
+    parameters
+  );
+  console.log(result);
+}

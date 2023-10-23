ce61860b3efa943830d0e7e6f6a1fc5ff5dd60a5
azure-sdk <azuresdk@microsoft.com>
2023-10-23 12:27:19 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-storagemover_2.0.0

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples-js/Agents_Get.js
@@ -0,0 +1,20 @@
+const { StorageMoverClient } = require("@azure/arm-storagemover");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets an Agent resource.
+ *
+ * @summary Gets an Agent resource.
+ * x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples/Agents_Get.json
+ */
+async function agentsGet() {
+  const subscriptionId =
+    process.env["STORAGEMOVER_SUBSCRIPTION_ID"] || "60bcfc77-6589-4da2-b7fd-f9ec9322cf95";
+  const resourceGroupName = process.env["STORAGEMOVER_RESOURCE_GROUP"] || "examples-rg";
+  const storageMoverName = "examples-storageMoverName";
+  const agentName = "examples-agentName";
+  const credential = new DefaultAzureCredential();
+  const client = new StorageMoverClient(credential, subscriptionId);
+  const result = await client.agents.get(resourceGroupName, storageMoverName, agentName);
+  console.log(result);
+}
---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples-js/Endpoints_CreateOrUpdate_SmbMount.js
@@ -0,0 +1,38 @@
+const { StorageMoverClient } = require("@azure/arm-storagemover");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates an Endpoint resource, which represents a data transfer source or destination.
+ *
+ * @summary Creates or updates an Endpoint resource, which represents a data transfer source or destination.
+ * x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples/Endpoints_CreateOrUpdate_SmbMount.json
+ */
+async function endpointsCreateOrUpdateSmbMount() {
+  const subscriptionId =
+    process.env["STORAGEMOVER_SUBSCRIPTION_ID"] || "60bcfc77-6589-4da2-b7fd-f9ec9322cf95";
+  const resourceGroupName = process.env["STORAGEMOVER_RESOURCE_GROUP"] || "examples-rg";
+  const storageMoverName = "examples-storageMoverName";
+  const endpointName = "examples-endpointName";
+  const endpoint = {
+    properties: {
+      description: "Example SMB Mount Endpoint Description",
+      credentials: {
+        type: "AzureKeyVaultSmb",
+        passwordUri: "https://examples-azureKeyVault.vault.azure.net/secrets/examples-password",
+        usernameUri: "https://examples-azureKeyVault.vault.azure.net/secrets/examples-username",
+      },
+      endpointType: "SmbMount",
+      host: "0.0.0.0",
+      shareName: "examples-shareName",
+    },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new StorageMoverClient(credential, subscriptionId);
+  const result = await client.endpoints.createOrUpdate(
+    resourceGroupName,
+    storageMoverName,
+    endpointName,
+    endpoint
+  );
+  console.log(result);
+}

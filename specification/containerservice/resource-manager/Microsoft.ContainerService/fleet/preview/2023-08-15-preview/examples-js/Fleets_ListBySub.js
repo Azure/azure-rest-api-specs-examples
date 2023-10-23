90b0fc07cef803de111ec36ff6f1fe6bf619855f
azure-sdk <azuresdk@microsoft.com>
2023-10-23 12:39:02 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservicefleet_1.0.0-beta.3

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-08-15-preview/examples-js/Fleets_ListBySub.js
@@ -0,0 +1,19 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists fleets in the specified subscription.
+ *
+ * @summary Lists fleets in the specified subscription.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-08-15-preview/examples/Fleets_ListBySub.json
+ */
+async function listsTheFleetResourcesInASubscription() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.fleets.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-08-15-preview/examples-js/UpdateRuns_Stop.js
@@ -0,0 +1,24 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Stops an UpdateRun.
+ *
+ * @summary Stops an UpdateRun.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2023-08-15-preview/examples/UpdateRuns_Stop.json
+ */
+async function stopsAnUpdateRun() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const fleetName = "fleet1";
+  const updateRunName = "run1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const result = await client.updateRuns.beginStopAndWait(
+    resourceGroupName,
+    fleetName,
+    updateRunName
+  );
+  console.log(result);
+}

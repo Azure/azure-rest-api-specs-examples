5975bd17fdb41908721c29f741623baa3356b464
azure-sdk <azuresdk@microsoft.com>
2023-10-23 12:48:51 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-support_6.1.0b2

---
+++ specification/support/resource-manager/Microsoft.Support/preview/2022-09-01-preview/examples-python/ListChatTranscriptsForSubscriptionSupportTicket.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.support import MicrosoftSupport
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-support
+# USAGE
+    python list_chat_transcripts_for_subscription_support_ticket.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MicrosoftSupport(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.chat_transcripts.list(
+        support_ticket_name="testticket",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/support/resource-manager/Microsoft.Support/preview/2022-09-01-preview/examples/ListChatTranscriptsForSubscriptionSupportTicket.json
+if __name__ == "__main__":
+    main()
---
+++ specification/support/resource-manager/Microsoft.Support/preview/2022-09-01-preview/examples-python/ListWebCommunicationsForSubscriptionSupportTicketCreatedOnOrAfter.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.support import MicrosoftSupport
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-support
+# USAGE
+    python list_web_communications_for_subscription_support_ticket_created_on_or_after.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MicrosoftSupport(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.communications.list(
+        support_ticket_name="testticket",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/support/resource-manager/Microsoft.Support/preview/2022-09-01-preview/examples/ListWebCommunicationsForSubscriptionSupportTicketCreatedOnOrAfter.json
+if __name__ == "__main__":
+    main()

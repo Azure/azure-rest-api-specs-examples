3f019474150b8c53e455398185a4f7e49625f7b0
azure-sdk <azuresdk@microsoft.com>
2023-10-24 12:42:56 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storagemover_2.0.0

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples-python/JobDefinitions_List.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.storagemover import StorageMoverMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storagemover
+# USAGE
+    python job_definitions_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageMoverMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="60bcfc77-6589-4da2-b7fd-f9ec9322cf95",
+    )
+
+    response = client.job_definitions.list(
+        resource_group_name="examples-rg",
+        storage_mover_name="examples-storageMoverName",
+        project_name="examples-projectName",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples/JobDefinitions_List.json
+if __name__ == "__main__":
+    main()
---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples-python/JobRuns_Get.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.storagemover import StorageMoverMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storagemover
+# USAGE
+    python job_runs_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageMoverMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="60bcfc77-6589-4da2-b7fd-f9ec9322cf95",
+    )
+
+    response = client.job_runs.get(
+        resource_group_name="examples-rg",
+        storage_mover_name="examples-storageMoverName",
+        project_name="examples-projectName",
+        job_definition_name="examples-jobDefinitionName",
+        job_run_name="examples-jobRunName",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples/JobRuns_Get.json
+if __name__ == "__main__":
+    main()

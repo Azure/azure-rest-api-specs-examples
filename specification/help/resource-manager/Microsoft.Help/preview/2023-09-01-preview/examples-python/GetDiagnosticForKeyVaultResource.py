881595dd6ed2d144efc6a1607960b5d98d5a8a74
azure-sdk <azuresdk@microsoft.com>
2023-10-24 12:42:02 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-selfhelp_2.0.0b1

---
+++ specification/help/resource-manager/Microsoft.Help/preview/2023-09-01-preview/examples-python/GetDiagnosticForKeyVaultResource.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.selfhelp import SelfHelpMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-selfhelp
+# USAGE
+    python get_diagnostic_for_key_vault_resource.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SelfHelpMgmtClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.diagnostics.get(
+        scope="subscriptions/0d0fcd2e-c4fd-4349-8497-200edb3923c6/resourceGroups/myresourceGroup/providers/Microsoft.KeyVault/vaults/test-keyvault-non-read",
+        diagnostics_resource_name="VMNotWorkingInsight",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/help/resource-manager/Microsoft.Help/preview/2023-09-01-preview/examples/GetDiagnosticForKeyVaultResource.json
+if __name__ == "__main__":
+    main()

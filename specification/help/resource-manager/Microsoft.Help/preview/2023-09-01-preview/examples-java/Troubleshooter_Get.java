4d8f0f7ee44cfbc80feeafd836cd6ecfbb9e7268
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-24 20:04:22 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-selfhelp_1.1.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/help/resource-manager/Microsoft.Help/preview/2023-09-01-preview/examples-java/Troubleshooter_Get.java
@@ -0,0 +1,19 @@
+/** Samples for Troubleshooters Get. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/help/resource-manager/Microsoft.Help/preview/2023-09-01-preview/examples/Troubleshooter_Get.json
+     */
+    /**
+     * Sample code: Troubleshooters_Get.
+     *
+     * @param manager Entry point to SelfHelpManager.
+     */
+    public static void troubleshootersGet(com.azure.resourcemanager.selfhelp.SelfHelpManager manager) {
+        manager
+            .troubleshooters()
+            .getWithResponse(
+                "subscriptions/mySubscription/resourcegroups/myresourceGroup/providers/Microsoft.KeyVault/vaults/test-keyvault-rp",
+                "abf168ed-1b54-454a-86f6-e4b62253d3b1",
+                com.azure.core.util.Context.NONE);
+    }
+}

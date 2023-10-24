65a3b41e87a02b3a942c9e85fdf34676590c7940
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-24 20:02:14 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-support_6.1.0b2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/support/resource-manager/Microsoft.Support/preview/2022-09-01-preview/examples-python/CreateGenericQuotaTicket.py
@@ -0,0 +1,50 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.support import MicrosoftSupport
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-support
+# USAGE
+    python create_generic_quota_ticket.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MicrosoftSupport(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.support_tickets.begin_create(
+        support_ticket_name="testticket",
+        create_support_ticket_parameters={
+            "properties": {
+                "contactDetails": {
+                    "country": "usa",
+                    "firstName": "abc",
+                    "lastName": "xyz",
+                    "preferredContactMethod": "email",
+                    "preferredSupportLanguage": "en-US",
+                    "preferredTimeZone": "Pacific Standard Time",
+                    "primaryEmailAddress": "abc@contoso.com",
+                },
+                "description": "Increase the maximum throughput per container limit to 10000 for account foo bar",
+                "problemClassificationId": "/providers/Microsoft.Support/services/quota_service_guid/problemClassifications/cosmosdb_problemClassification_guid",
+                "serviceId": "/providers/Microsoft.Support/services/quota_service_guid",
+                "severity": "moderate",
+                "title": "my title",
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/support/resource-manager/Microsoft.Support/preview/2022-09-01-preview/examples/CreateGenericQuotaTicket.json
+if __name__ == "__main__":
+    main()

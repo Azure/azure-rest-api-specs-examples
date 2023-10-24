ce7368d22bae92e028d4358f28650568f1af3133
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-24 20:02:21 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-desktopvirtualization_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/desktopvirtualization/resource-manager/Microsoft.DesktopVirtualization/stable/2023-09-05/examples-python/ApplicationGroup_Delete.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.desktopvirtualization import DesktopVirtualizationMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-desktopvirtualization
+# USAGE
+    python application_group_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DesktopVirtualizationMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="daefabc0-95b4-48b3-b645-8a753a63c4fa",
+    )
+
+    client.application_groups.delete(
+        resource_group_name="resourceGroup1",
+        application_group_name="applicationGroup1",
+    )
+
+
+# x-ms-original-file: specification/desktopvirtualization/resource-manager/Microsoft.DesktopVirtualization/stable/2023-09-05/examples/ApplicationGroup_Delete.json
+if __name__ == "__main__":
+    main()

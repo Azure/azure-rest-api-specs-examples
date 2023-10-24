3f1bb70a71a8bf536634986995d1b6357fe498c8
azure-sdk <azuresdk@microsoft.com>
2023-10-24 12:34:41 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-signalr_6.0.0-beta.2

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples-js/SignalRCustomCertificates_List.js
@@ -0,0 +1,22 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List all custom certificates.
+ *
+ * @summary List all custom certificates.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/SignalRCustomCertificates_List.json
+ */
+async function signalRCustomCertificatesList() {
+  const subscriptionId =
+    process.env["SIGNALR_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["SIGNALR_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.signalRCustomCertificates.list(resourceGroupName, resourceName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

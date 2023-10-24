d3577452dc981f907366e5a06c46aff658f23218
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-24 20:04:07 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-storagemover_2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples-js/JobDefinitions_List.js
@@ -0,0 +1,27 @@
+const { StorageMoverClient } = require("@azure/arm-storagemover");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists all Job Definitions in a Project.
+ *
+ * @summary Lists all Job Definitions in a Project.
+ * x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples/JobDefinitions_List.json
+ */
+async function jobDefinitionsList() {
+  const subscriptionId =
+    process.env["STORAGEMOVER_SUBSCRIPTION_ID"] || "60bcfc77-6589-4da2-b7fd-f9ec9322cf95";
+  const resourceGroupName = process.env["STORAGEMOVER_RESOURCE_GROUP"] || "examples-rg";
+  const storageMoverName = "examples-storageMoverName";
+  const projectName = "examples-projectName";
+  const credential = new DefaultAzureCredential();
+  const client = new StorageMoverClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.jobDefinitions.list(
+    resourceGroupName,
+    storageMoverName,
+    projectName
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

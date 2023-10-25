a29e0867098bae241f1dc1d0667a1bdb31dcc86d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-25 20:02:41 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-signalr_6.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples-js/SignalRCustomDomains_Get.js
@@ -0,0 +1,20 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get a custom domain.
+ *
+ * @summary Get a custom domain.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/SignalRCustomDomains_Get.json
+ */
+async function signalRCustomDomainsGet() {
+  const subscriptionId =
+    process.env["SIGNALR_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["SIGNALR_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const name = "example";
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const result = await client.signalRCustomDomains.get(resourceGroupName, resourceName, name);
+  console.log(result);
+}
---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples-js/SignalR_Get.js
@@ -0,0 +1,19 @@
+const { SignalRManagementClient } = require("@azure/arm-signalr");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get the resource and its properties.
+ *
+ * @summary Get the resource and its properties.
+ * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/SignalR_Get.json
+ */
+async function signalRGet() {
+  const subscriptionId =
+    process.env["SIGNALR_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["SIGNALR_RESOURCE_GROUP"] || "myResourceGroup";
+  const resourceName = "mySignalRService";
+  const credential = new DefaultAzureCredential();
+  const client = new SignalRManagementClient(credential, subscriptionId);
+  const result = await client.signalR.get(resourceGroupName, resourceName);
+  console.log(result);
+}

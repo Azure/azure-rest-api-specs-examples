d3b291c25ee1a0ebf122e1d53aefa89a98086a20
azure-sdk <azuresdk@microsoft.com>
2023-10-25 12:32:27 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-connectedvmware_1.0.0

---
+++ specification/connectedvmware/resource-manager/Microsoft.ConnectedVMwarevSphere/stable/2023-10-01/examples-python/CreateVirtualMachineTemplate.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.connectedvmware import ConnectedVMwareMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-connectedvmware
+# USAGE
+    python create_virtual_machine_template.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ConnectedVMwareMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="fd3c3665-1729-4b7b-9a38-238e83b0f98b",
+    )
+
+    response = client.virtual_machine_templates.begin_create(
+        resource_group_name="testrg",
+        virtual_machine_template_name="WebFrontEndTemplate",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/connectedvmware/resource-manager/Microsoft.ConnectedVMwarevSphere/stable/2023-10-01/examples/CreateVirtualMachineTemplate.json
+if __name__ == "__main__":
+    main()

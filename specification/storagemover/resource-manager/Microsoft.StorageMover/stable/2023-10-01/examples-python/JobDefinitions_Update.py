fa997d6e6d6d130436cc4d0a66627480eb68db63
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-25 20:02:03 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storagemover_2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples-python/JobDefinitions_Update.py
@@ -0,0 +1,38 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.storagemover import StorageMoverMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storagemover
+# USAGE
+    python job_definitions_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageMoverMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="60bcfc77-6589-4da2-b7fd-f9ec9322cf95",
+    )
+
+    response = client.job_definitions.update(
+        resource_group_name="examples-rg",
+        storage_mover_name="examples-storageMoverName",
+        project_name="examples-projectName",
+        job_definition_name="examples-jobDefinitionName",
+        job_definition={
+            "properties": {"agentName": "updatedAgentName", "description": "Updated Job Definition Description"}
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-10-01/examples/JobDefinitions_Update.json
+if __name__ == "__main__":
+    main()

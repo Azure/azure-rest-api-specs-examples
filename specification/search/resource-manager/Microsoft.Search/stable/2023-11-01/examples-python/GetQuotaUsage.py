1f61675dbf3fdca68600eb0b97d5f798da2e83c5
azure-sdk <azuresdk@microsoft.com>
2023-10-25 12:33:22 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-search_9.1.0

---
+++ specification/search/resource-manager/Microsoft.Search/stable/2023-11-01/examples-python/GetQuotaUsage.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python get_quota_usage.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.usage_by_subscription_sku(
+        location="westus",
+        sku_name="free",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/search/resource-manager/Microsoft.Search/stable/2023-11-01/examples/GetQuotaUsage.json
+if __name__ == "__main__":
+    main()

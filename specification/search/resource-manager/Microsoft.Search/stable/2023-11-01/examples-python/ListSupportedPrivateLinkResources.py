e42ac9e9069aad7798bed4a830376586eb9d09cd
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-26 20:01:28 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-search_9.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/search/resource-manager/Microsoft.Search/stable/2023-11-01/examples-python/ListSupportedPrivateLinkResources.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python list_supported_private_link_resources.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.private_link_resources.list_supported(
+        resource_group_name="rg1",
+        search_service_name="mysearchservice",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/search/resource-manager/Microsoft.Search/stable/2023-11-01/examples/ListSupportedPrivateLinkResources.json
+if __name__ == "__main__":
+    main()
---
+++ specification/search/resource-manager/Microsoft.Search/stable/2023-11-01/examples-python/SearchUpdateServiceWithCmkEnforcement.py
@@ -0,0 +1,37 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python search_update_service_with_cmk_enforcement.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.services.update(
+        resource_group_name="rg1",
+        search_service_name="mysearchservice",
+        service={
+            "properties": {"encryptionWithCmk": {"enforcement": "Enabled"}, "replicaCount": 2},
+            "tags": {"app-name": "My e-commerce app", "new-tag": "Adding a new tag"},
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/search/resource-manager/Microsoft.Search/stable/2023-11-01/examples/SearchUpdateServiceWithCmkEnforcement.json
+if __name__ == "__main__":
+    main()

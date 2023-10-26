6993efba3536e122e6329de1c5a7e3791bc84263
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-26 20:02:22 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-08-01/examples-js/AgentPoolsDelete.js
@@ -0,0 +1,23 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes an agent pool in the specified managed cluster.
+ *
+ * @summary Deletes an agent pool in the specified managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-08-01/examples/AgentPoolsDelete.json
+ */
+async function deleteAgentPool() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const agentPoolName = "agentpool1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.agentPools.beginDeleteAndWait(
+    resourceGroupName,
+    resourceName,
+    agentPoolName
+  );
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-08-01/examples-js/ContainerServiceGetOSOptions.js
@@ -0,0 +1,17 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets supported OS options in the specified subscription.
+ *
+ * @summary Gets supported OS options in the specified subscription.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-08-01/examples/ContainerServiceGetOSOptions.json
+ */
+async function getContainerServiceOSOptions() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const location = "location1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.getOSOptions(location);
+  console.log(result);
+}

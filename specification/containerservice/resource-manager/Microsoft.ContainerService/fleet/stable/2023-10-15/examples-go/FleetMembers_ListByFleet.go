854c2e92b75e7ea1b45efb6eee79eb795bd52de8
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-28 20:01:43 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservicefleet/armcontainerservicefleet/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/stable/2023-10-15/examples-go/FleetMembers_ListByFleet.go
@@ -0,0 +1,55 @@
+package armcontainerservicefleet_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicefleet/armcontainerservicefleet"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/678aeba91061a67e9af8b9aaf41f85b8aea06c7c/specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/stable/2023-10-15/examples/FleetMembers_ListByFleet.json
+func ExampleFleetMembersClient_NewListByFleetPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicefleet.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewFleetMembersClient().NewListByFleetPager("rg1", "fleet1", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.FleetMemberListResult = armcontainerservicefleet.FleetMemberListResult{
+		// 	Value: []*armcontainerservicefleet.FleetMember{
+		// 		{
+		// 			Name: to.Ptr("member-1"),
+		// 			Type: to.Ptr("Microsoft.ContainerService/fleets/members"),
+		// 			ID: to.Ptr("/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/fleets/fleet-1/members/member-1"),
+		// 			SystemData: &armcontainerservicefleet.SystemData{
+		// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-23T05:40:40.657Z"); return t}()),
+		// 				CreatedBy: to.Ptr("someUser"),
+		// 				CreatedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+		// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-23T05:40:40.657Z"); return t}()),
+		// 				LastModifiedBy: to.Ptr("someOtherUser"),
+		// 				LastModifiedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+		// 			},
+		// 			ETag: to.Ptr("kd30rkdfo49="),
+		// 			Properties: &armcontainerservicefleet.FleetMemberProperties{
+		// 				ClusterResourceID: to.Ptr("/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/managedClusters/cluster-1"),
+		// 				ProvisioningState: to.Ptr(armcontainerservicefleet.FleetMemberProvisioningStateSucceeded),
+		// 			},
+		// 	}},
+		// }
+	}
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/stable/2023-10-15/examples-go/Fleets_Get.go
@@ -0,0 +1,51 @@
+package armcontainerservicefleet_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicefleet/armcontainerservicefleet"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/678aeba91061a67e9af8b9aaf41f85b8aea06c7c/specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/stable/2023-10-15/examples/Fleets_Get.json
+func ExampleFleetsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicefleet.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewFleetsClient().Get(ctx, "rg1", "fleet1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Fleet = armcontainerservicefleet.Fleet{
+	// 	Name: to.Ptr("fleet-1"),
+	// 	Type: to.Ptr("Microsoft.ContainerService/fleets"),
+	// 	ID: to.Ptr("/subscriptions/subid1/resourcegroups/rg1/providers/Microsoft.ContainerService/fleets/fleet-1"),
+	// 	SystemData: &armcontainerservicefleet.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-23T05:40:40.657Z"); return t}()),
+	// 		CreatedBy: to.Ptr("someUser"),
+	// 		CreatedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-03-23T05:40:40.657Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("someOtherUser"),
+	// 		LastModifiedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 	},
+	// 	Location: to.Ptr("East US"),
+	// 	Tags: map[string]*string{
+	// 		"archv2": to.Ptr(""),
+	// 		"tier": to.Ptr("production"),
+	// 	},
+	// 	ETag: to.Ptr("23ujdflewrj3="),
+	// 	Properties: &armcontainerservicefleet.FleetProperties{
+	// 		ProvisioningState: to.Ptr(armcontainerservicefleet.FleetProvisioningStateSucceeded),
+	// 	},
+	// }
+}

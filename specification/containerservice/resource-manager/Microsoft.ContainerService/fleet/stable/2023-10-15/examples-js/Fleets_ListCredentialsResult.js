277efc028b2058ad0abb03681b4383abdd46c155
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-10-29 20:01:35 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservicefleet_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/stable/2023-10-15/examples-js/Fleets_ListCredentialsResult.js
@@ -0,0 +1,18 @@
+const { ContainerServiceFleetClient } = require("@azure/arm-containerservicefleet");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists the user credentials of a Fleet.
+ *
+ * @summary Lists the user credentials of a Fleet.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/stable/2023-10-15/examples/Fleets_ListCredentialsResult.json
+ */
+async function listsTheUserCredentialsOfAFleet() {
+  const subscriptionId = process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "subid1";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const fleetName = "fleet";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceFleetClient(credential, subscriptionId);
+  const result = await client.fleets.listCredentials(resourceGroupName, fleetName);
+  console.log(result);
+}

425fee71fdc96da0da7686ad5a730035968fc4df
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-03 20:01:41 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceNetworking_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples-dotnet/TrafficControllerDelete.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ServiceNetworking;
+using Azure.ResourceManager.ServiceNetworking.Models;
+
+// Generated from example definition: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/TrafficControllerDelete.json
+// this example is just showing the usage of "TrafficControllerInterface_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this TrafficControllerResource created on azure
+// for more information of creating TrafficControllerResource, please refer to the document of TrafficControllerResource
+string subscriptionId = "subid";
+string resourceGroupName = "rg1";
+string trafficControllerName = "tc1";
+ResourceIdentifier trafficControllerResourceId = TrafficControllerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, trafficControllerName);
+TrafficControllerResource trafficController = client.GetTrafficControllerResource(trafficControllerResourceId);
+
+// invoke the operation
+await trafficController.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");
---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples-dotnet/TrafficControllerPut.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ServiceNetworking;
+
+// Generated from example definition: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/TrafficControllerPut.json
+// this example is just showing the usage of "TrafficControllerInterface_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "subid";
+string resourceGroupName = "rg1";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this TrafficControllerResource
+TrafficControllerCollection collection = resourceGroupResource.GetTrafficControllers();
+
+// invoke the operation
+string trafficControllerName = "tc1";
+TrafficControllerData data = new TrafficControllerData(new AzureLocation("NorthCentralUS"))
+{
+    Tags =
+    {
+    ["key1"] = "value1",
+    },
+};
+ArmOperation<TrafficControllerResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, trafficControllerName, data);
+TrafficControllerResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+TrafficControllerData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

d5a159e7ebc91867a5f9b2fbb63f7756fc137ead
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-08 20:01:17 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.NetApp_1.4.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-05-01/examples-dotnet/BackupPolicies_Get.cs
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-05-01/examples-dotnet/BackupPolicies_Get.cs
@@ -5,6 +5,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.NetApp;
+using Azure.ResourceManager.NetApp.Models;
 
 // Generated from example definition: specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-05-01/examples/BackupPolicies_Get.json
 // this example is just showing the usage of "BackupPolicies_Get" operation, for the dependent resources, they will have to be created separately.
@@ -14,31 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this NetAppAccountResource created on azure
-// for more information of creating NetAppAccountResource, please refer to the document of NetAppAccountResource
+// this example assumes you already have this NetAppBackupPolicyResource created on azure
+// for more information of creating NetAppBackupPolicyResource, please refer to the document of NetAppBackupPolicyResource
 string subscriptionId = "D633CC2E-722B-4AE1-B636-BBD9E4C60ED9";
 string resourceGroupName = "myRG";
 string accountName = "account1";
-ResourceIdentifier netAppAccountResourceId = NetAppAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-NetAppAccountResource netAppAccount = client.GetNetAppAccountResource(netAppAccountResourceId);
-
-// get the collection of this NetAppBackupPolicyResource
-NetAppBackupPolicyCollection collection = netAppAccount.GetNetAppBackupPolicies();
+string backupPolicyName = "backupPolicyName";
+ResourceIdentifier netAppBackupPolicyResourceId = NetAppBackupPolicyResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, backupPolicyName);
+NetAppBackupPolicyResource netAppBackupPolicy = client.GetNetAppBackupPolicyResource(netAppBackupPolicyResourceId);
 
 // invoke the operation
-string backupPolicyName = "backupPolicyName";
-NullableResponse<NetAppBackupPolicyResource> response = await collection.GetIfExistsAsync(backupPolicyName);
-NetAppBackupPolicyResource result = response.HasValue ? response.Value : null;
+NetAppBackupPolicyResource result = await netAppBackupPolicy.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine($"Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    NetAppBackupPolicyData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+NetAppBackupPolicyData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

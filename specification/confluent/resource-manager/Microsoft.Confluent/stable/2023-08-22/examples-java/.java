899998a2dd6928470c4836bb788a1f0c498f24f3
azure-sdk <azuresdk@microsoft.com>
2023-11-16 12:12:48 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-confluent_1.0.0

---
+++ specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples-java/.java
@@ -0,0 +1,15 @@
+/** Samples for Organization List. */
+public final class Main {
+    /*
+     * x-ms-original-file: specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples/
+     * Organization_ListBySubscription.json
+     */
+    /**
+     * Sample code: Organization_ListBySubscription.
+     *
+     * @param manager Entry point to ConfluentManager.
+     */
+    public static void organizationListBySubscription(com.azure.resourcemanager.confluent.ConfluentManager manager) {
+        manager.organizations().list(com.azure.core.util.Context.NONE);
+    }
+}

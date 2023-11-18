3bd0eaa55223dd0fd914d7050ab181666c29cd8b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-18 20:05:56 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Resources_1.7.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/resources/resource-manager/Microsoft.Solutions/stable/2019-07-01/examples-dotnet/getApplication.cs
+++ specification/resources/resource-manager/Microsoft.Solutions/stable/2019-07-01/examples-dotnet/getApplication.cs
@@ -26,6 +26,18 @@ ArmApplicationCollection collection = resourceGroupResource.GetArmApplications()
 
 // invoke the operation
 string applicationName = "myManagedApplication";
-bool result = await collection.ExistsAsync(applicationName);
+NullableResponse<ArmApplicationResource> response = await collection.GetIfExistsAsync(applicationName);
+ArmApplicationResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ArmApplicationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

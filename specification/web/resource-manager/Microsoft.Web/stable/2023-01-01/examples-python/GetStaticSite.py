244074f5b191561b7380222d2952824f4159d063
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-21 20:02:00 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-web_7.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/web/resource-manager/Microsoft.Web/stable/2023-01-01/examples-python/GetStaticSite.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.web import WebSiteManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-web
+# USAGE
+    python get_static_site.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = WebSiteManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="34adfa4f-cedf-4dc0-ba29-b6d1a69ab345",
+    )
+
+    response = client.static_sites.get_static_site(
+        resource_group_name="rg",
+        name="testStaticSite0",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2023-01-01/examples/GetStaticSite.json
+if __name__ == "__main__":
+    main()
---
+++ specification/web/resource-manager/Microsoft.Web/stable/2023-01-01/examples-python/ListWebAppSlots.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.web import WebSiteManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-web
+# USAGE
+    python list_web_app_slots.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = WebSiteManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="34adfa4f-cedf-4dc0-ba29-b6d1a69ab345",
+    )
+
+    response = client.web_apps.list_slots(
+        resource_group_name="testrg123",
+        name="sitef6141",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2023-01-01/examples/ListWebAppSlots.json
+if __name__ == "__main__":
+    main()

2b2aee3805c4899989c3e4b8a8be164b6f8824cb
azure-sdk <azuresdk@microsoft.com>
2023-11-21 12:22:18 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-confluent_2.0.0

---
+++ specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples-python/Organization_ListBySubscription.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.confluent import ConfluentManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-confluent
+# USAGE
+    python organization_list_by_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ConfluentManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.organization.list_by_subscription()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples/Organization_ListBySubscription.json
+if __name__ == "__main__":
+    main()

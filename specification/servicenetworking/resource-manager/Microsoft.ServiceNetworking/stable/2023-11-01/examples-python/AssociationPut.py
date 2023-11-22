13ee6ea776a938777431e35b32826670841803f6
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-22 20:03:18 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-servicenetworking_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples-python/AssociationPut.py
@@ -0,0 +1,43 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.servicenetworking import ServiceNetworkingMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-servicenetworking
+# USAGE
+    python association_put.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ServiceNetworkingMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.associations_interface.begin_create_or_update(
+        resource_group_name="rg1",
+        traffic_controller_name="tc1",
+        association_name="as1",
+        resource={
+            "location": "NorthCentralUS",
+            "properties": {
+                "associationType": "subnets",
+                "subnet": {
+                    "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/vnet-tc/subnets/tc-subnet"
+                },
+            },
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/AssociationPut.json
+if __name__ == "__main__":
+    main()

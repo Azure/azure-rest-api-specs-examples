730446efdd4c66ccffcbeca9bffdd80970a3b936
azure-sdk <azuresdk@microsoft.com>
2023-11-23 12:28:55 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/marketplaceordering/armmarketplaceordering/v1.2.0

--- specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-go/GetMarketplaceTerms.go
+++ specification/marketplaceordering/resource-manager/Microsoft.MarketplaceOrdering/stable/2021-01-01/examples-go/GetMarketplaceTerms.go
@@ -38,7 +38,7 @@ func ExampleMarketplaceAgreementsClient_Get() {
 	// 		PrivacyPolicyLink: to.Ptr("test.privacyPolicyLink"),
 	// 		Product: to.Ptr("offid"),
 	// 		Publisher: to.Ptr("pubid"),
-	// 		RetrieveDatetime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-08-15T11:33:07.12132Z"); return t}()),
+	// 		RetrieveDatetime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-08-15T11:33:07.121Z"); return t}()),
 	// 		Signature: to.Ptr("ASDFSDAFWEFASDGWERLWER"),
 	// 	},
 	// }

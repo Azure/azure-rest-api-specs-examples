6a725426b7bc5e5bc7cabf8c32dc6e4fc1147a62
azure-sdk <azuresdk@microsoft.com>
2023-11-23 12:33:56 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/communication/armcommunication/v2.2.0-beta.1

---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2023-06-01-preview/examples-go/communicationServices/get.go
@@ -0,0 +1,41 @@
+package armcommunication_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/communication/armcommunication/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/bf204aab860f2eb58a9d346b00d44760f2a9b0a2/specification/communication/resource-manager/Microsoft.Communication/preview/2023-06-01-preview/examples/communicationServices/get.json
+func ExampleServicesClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcommunication.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewServicesClient().Get(ctx, "MyResourceGroup", "MyCommunicationResource", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ServiceResource = armcommunication.ServiceResource{
+	// 	Name: to.Ptr("MyCommunicationResource"),
+	// 	Type: to.Ptr("Microsoft.Communication/CommunicationServices"),
+	// 	ID: to.Ptr("/subscriptions/11112222-3333-4444-5555-666677778888/resourceGroups/MyResourceGroup/providers/Microsoft.Communication/CommunicationServices/MyCommunicationResource"),
+	// 	Location: to.Ptr("Global"),
+	// 	Properties: &armcommunication.ServiceProperties{
+	// 		DataLocation: to.Ptr("United States"),
+	// 		HostName: to.Ptr("mycommunicationservice.comms.azure.net"),
+	// 		ProvisioningState: to.Ptr(armcommunication.CommunicationServicesProvisioningStateSucceeded),
+	// 		Version: to.Ptr("0.2.0"),
+	// 	},
+	// }
+}

495d7ce046a319deb2632962800b95fa6e938a56
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-24 20:03:23 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/resources/armpolicy/v0.9.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/resources/resource-manager/Microsoft.Authorization/preview/2022-07-01-preview/examples-go/createOrUpdatePolicyExemption.go
+++ specification/resources/resource-manager/Microsoft.Authorization/preview/2022-07-01-preview/examples-go/createOrUpdatePolicyExemption.go
@@ -55,10 +55,10 @@ func ExampleExemptionsClient_CreateOrUpdate_createOrUpdateAPolicyExemption() {
 	// 			to.Ptr("Limit_Skus")},
 	// 		},
 	// 		SystemData: &armpolicy.SystemData{
-	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-07-01T01:01:01.1075056Z"); return t}()),
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-07-01T01:01:01.107Z"); return t}()),
 	// 			CreatedBy: to.Ptr("string"),
 	// 			CreatedByType: to.Ptr(armpolicy.CreatedByTypeUser),
-	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-07-01T02:01:01.1075056Z"); return t}()),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-07-01T02:01:01.107Z"); return t}()),
 	// 			LastModifiedBy: to.Ptr("string"),
 	// 			LastModifiedByType: to.Ptr(armpolicy.CreatedByTypeUser),
 	// 		},

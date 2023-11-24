196adcefdefcc8f23a9fc4c30853c38ba660271d
azure-sdk <azuresdk@microsoft.com>
2023-11-24 12:37:51 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/servicenetworking/armservicenetworking/v1.0.0

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples-go/AssociationPut.go
@@ -0,0 +1,55 @@
+package armservicenetworking_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/servicenetworking/armservicenetworking"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/db9788dde7a0c2c0d82e4fdf5f7b4de3843937e3/specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/AssociationPut.json
+func ExampleAssociationsInterfaceClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armservicenetworking.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewAssociationsInterfaceClient().BeginCreateOrUpdate(ctx, "rg1", "tc1", "as1", armservicenetworking.Association{
+		Location: to.Ptr("NorthCentralUS"),
+		Properties: &armservicenetworking.AssociationProperties{
+			AssociationType: to.Ptr(armservicenetworking.AssociationTypeSubnets),
+			Subnet: &armservicenetworking.AssociationSubnet{
+				ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/vnet-tc/subnets/tc-subnet"),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Association = armservicenetworking.Association{
+	// 	Name: to.Ptr("associatedvnet-1"),
+	// 	Type: to.Ptr("Microsoft.ServiceNetworking/trafficControllers/associations"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourcegroups/rg1/providers/Microsoft.ServiceNetworking/trafficControllers/tc1/associations/as1"),
+	// 	Location: to.Ptr("NorthCentralUS"),
+	// 	Properties: &armservicenetworking.AssociationProperties{
+	// 		AssociationType: to.Ptr(armservicenetworking.AssociationTypeSubnets),
+	// 		ProvisioningState: to.Ptr(armservicenetworking.ProvisioningStateSucceeded),
+	// 		Subnet: &armservicenetworking.AssociationSubnet{
+	// 			ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/vnet-tc/subnets/tc-subnet"),
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples-go/FrontendPut.go
@@ -0,0 +1,47 @@
+package armservicenetworking_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/servicenetworking/armservicenetworking"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/db9788dde7a0c2c0d82e4fdf5f7b4de3843937e3/specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/FrontendPut.json
+func ExampleFrontendsInterfaceClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armservicenetworking.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewFrontendsInterfaceClient().BeginCreateOrUpdate(ctx, "rg1", "tc1", "fe1", armservicenetworking.Frontend{
+		Location:   to.Ptr("NorthCentralUS"),
+		Properties: &armservicenetworking.FrontendProperties{},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.Frontend = armservicenetworking.Frontend{
+	// 	Name: to.Ptr("fe1"),
+	// 	Type: to.Ptr("Microsoft.ServiceNetworking/trafficControllers/frontends"),
+	// 	ID: to.Ptr("/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.ServiceNetworking/trafficControllers/tc1/frontends/fe1"),
+	// 	Location: to.Ptr("NorthCentralUS"),
+	// 	Properties: &armservicenetworking.FrontendProperties{
+	// 		Fqdn: to.Ptr("test.net"),
+	// 		ProvisioningState: to.Ptr(armservicenetworking.ProvisioningStateSucceeded),
+	// 	},
+	// }
+}

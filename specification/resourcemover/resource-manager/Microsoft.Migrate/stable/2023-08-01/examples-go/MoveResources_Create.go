394b1c46718222c15d1c389b9f028c5abd966f8a
azure-sdk <azuresdk@microsoft.com>
2023-11-24 12:56:54 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/resourcemover/armresourcemover/v1.3.0

--- specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2023-08-01/examples-go/MoveResources_Create.go
+++ specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2023-08-01/examples-go/MoveResources_Create.go
@@ -78,10 +78,10 @@ func ExampleMoveResourcesClient_BeginCreate() {
 	// 			SourceID: to.Ptr("/subscriptions/subid/resourceGroups/eastusRG/providers/Microsoft.Compute/virtualMachines/eastusvm1"),
 	// 		},
 	// 		SystemData: &armresourcemover.SystemData{
-	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-01T15:06:54.2757906Z"); return t}()),
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-01T15:06:54.275Z"); return t}()),
 	// 			CreatedBy: to.Ptr("user@microsoft.com"),
 	// 			CreatedByType: to.Ptr(armresourcemover.CreatedByTypeUser),
-	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-27T17:16:24.3644126Z"); return t}()),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-08-27T17:16:24.364Z"); return t}()),
 	// 			LastModifiedBy: to.Ptr("user@microsoft.com"),
 	// 			LastModifiedByType: to.Ptr(armresourcemover.CreatedByTypeUser),
 	// 		},

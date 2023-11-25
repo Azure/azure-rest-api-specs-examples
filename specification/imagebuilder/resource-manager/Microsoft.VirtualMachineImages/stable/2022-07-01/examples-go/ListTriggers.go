355c4a1696a9dae53c9eb3370f9c81c7a5026265
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-25 20:04:07 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/virtualmachineimagebuilder/armvirtualmachineimagebuilder/v2.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/imagebuilder/resource-manager/Microsoft.VirtualMachineImages/stable/2022-07-01/examples-go/ListTriggers.go
+++ specification/imagebuilder/resource-manager/Microsoft.VirtualMachineImages/stable/2022-07-01/examples-go/ListTriggers.go
@@ -42,7 +42,7 @@ func ExampleTriggersClient_NewListByImageTemplatePager() {
 		// 				Status: &armvirtualmachineimagebuilder.TriggerStatus{
 		// 					Code: to.Ptr("Healthy"),
 		// 					Message: to.Ptr(""),
-		// 					Time: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-07-21T17:32:28Z"); return t}()),
+		// 					Time: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2022-07-21T17:32:28.000Z"); return t}()),
 		// 				},
 		// 			},
 		// 	}},

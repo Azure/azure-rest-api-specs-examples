dc11e479009e5bb6027e901aee453c2111de450e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-25 20:04:41 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/servicenetworking/armservicenetworking/v1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples-go/FrontendDelete.go
@@ -0,0 +1,30 @@
+package armservicenetworking_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/servicenetworking/armservicenetworking"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/db9788dde7a0c2c0d82e4fdf5f7b4de3843937e3/specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/FrontendDelete.json
+func ExampleFrontendsInterfaceClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armservicenetworking.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewFrontendsInterfaceClient().BeginDelete(ctx, "rg1", "tc1", "fe1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

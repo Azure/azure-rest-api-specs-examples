871fc6c9fd9597882d6fc15d5aa11f83562a77c7
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-25 20:03:32 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/storagecache/armstoragecache/v3.3.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-05-01/examples-go/StorageTargets_CreateOrUpdate.go
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2023-05-01/examples-go/StorageTargets_CreateOrUpdate.go
@@ -58,10 +58,10 @@ func ExampleStorageTargetsClient_BeginCreateOrUpdate_storageTargetsCreateOrUpdat
 	// 	Type: to.Ptr("Microsoft.StorageCache/Cache/StorageTarget"),
 	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/scgroup/providers/Microsoft.StorageCache/caches/sc1/storagetargets/st1"),
 	// 	SystemData: &armstoragecache.SystemData{
-	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.123Z"); return t}()),
 	// 		CreatedBy: to.Ptr("user1"),
 	// 		CreatedByType: to.Ptr(armstoragecache.CreatedByTypeUser),
-	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.123Z"); return t}()),
 	// 		LastModifiedBy: to.Ptr("user2"),
 	// 		LastModifiedByType: to.Ptr(armstoragecache.CreatedByTypeUser),
 	// 	},

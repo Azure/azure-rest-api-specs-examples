dc1a3c7000c821a9ad48790287e402ba06878b8d
azure-sdk <azuresdk@microsoft.com>
2023-11-26 12:34:04 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/datalake-analytics/armdatalakeanalytics/v0.8.0

--- specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/preview/2019-11-01-preview/examples-go/Accounts_Create.go
+++ specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/preview/2019-11-01-preview/examples-go/Accounts_Create.go
@@ -86,9 +86,9 @@ func ExampleAccountsClient_BeginCreate() {
 	// 	ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/rgaba12041/providers/Microsoft.DataLakeAnalytics/accounts/testaba15818"),
 	// 	Properties: &armdatalakeanalytics.AccountProperties{
 	// 		AccountID: to.Ptr("34adfa4f-cedf-4dc0-ba29-b6d1a69ab345"),
-	// 		CreationTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-04-14T13:21:56.6819037-07:00"); return t}()),
+	// 		CreationTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-04-14T20:21:56.681Z"); return t}()),
 	// 		Endpoint: to.Ptr("test_endpoint"),
-	// 		LastModifiedTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-04-14T13:21:56.6819037-07:00"); return t}()),
+	// 		LastModifiedTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2017-04-14T20:21:56.681Z"); return t}()),
 	// 		ProvisioningState: to.Ptr(armdatalakeanalytics.DataLakeAnalyticsAccountStatusSucceeded),
 	// 		State: to.Ptr(armdatalakeanalytics.DataLakeAnalyticsAccountStateActive),
 	// 		ComputePolicies: []*armdatalakeanalytics.ComputePolicy{

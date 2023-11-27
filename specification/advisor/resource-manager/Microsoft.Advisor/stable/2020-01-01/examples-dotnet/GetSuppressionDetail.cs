7224164b9cd00188d8a78f2b5d5c7b15842058b4
azure-sdk <azuresdk@microsoft.com>
2023-11-27 07:21:42 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Advisor_1.0.0-beta.4

--- specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-dotnet/GetSuppressionDetail.cs
+++ specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples-dotnet/GetSuppressionDetail.cs
@@ -5,6 +5,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Advisor;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/examples/GetSuppressionDetail.json
 // this example is just showing the usage of "Suppressions_Get" operation, for the dependent resources, they will have to be created separately.
@@ -14,18 +15,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceRecommendationBaseResource created on azure
-// for more information of creating ResourceRecommendationBaseResource, please refer to the document of ResourceRecommendationBaseResource
+// this example assumes you already have this SuppressionContractResource created on azure
+// for more information of creating SuppressionContractResource, please refer to the document of SuppressionContractResource
 string resourceUri = "resourceUri";
 string recommendationId = "recommendationId";
-ResourceIdentifier resourceRecommendationBaseResourceId = ResourceRecommendationBaseResource.CreateResourceIdentifier(resourceUri, recommendationId);
-ResourceRecommendationBaseResource resourceRecommendationBase = client.GetResourceRecommendationBaseResource(resourceRecommendationBaseResourceId);
-
-// get the collection of this SuppressionContractResource
-SuppressionContractCollection collection = resourceRecommendationBase.GetSuppressionContracts();
+string name = "suppressionName1";
+ResourceIdentifier suppressionContractResourceId = SuppressionContractResource.CreateResourceIdentifier(resourceUri, recommendationId, name);
+SuppressionContractResource suppressionContract = client.GetSuppressionContractResource(suppressionContractResourceId);
 
 // invoke the operation
-string name = "suppressionName1";
-bool result = await collection.ExistsAsync(name);
+SuppressionContractResource result = await suppressionContract.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+SuppressionContractData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

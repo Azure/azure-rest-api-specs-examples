7e94c8588ace0c5d3be8f7d2b04b5d2e83fa2dc3
azure-sdk <azuresdk@microsoft.com>
2023-11-27 05:43:59 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.AlertsManagement_1.1.0

--- specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/examples-dotnet/Alerts_GetById.cs
+++ specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/examples-dotnet/Alerts_GetById.cs
@@ -15,17 +15,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SubscriptionResource created on azure
-// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+// this example assumes you already have this ServiceAlertResource created on azure
+// for more information of creating ServiceAlertResource, please refer to the document of ServiceAlertResource
 string subscriptionId = "9e261de7-c804-4b9d-9ebf-6f50fe350a9a";
-ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
-SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
-
-// get the collection of this ServiceAlertResource
-ServiceAlertCollection collection = subscriptionResource.GetServiceAlerts();
+Guid alertId = Guid.Parse("66114d64-d9d9-478b-95c9-b789d6502100");
+ResourceIdentifier serviceAlertResourceId = ServiceAlertResource.CreateResourceIdentifier(subscriptionId, alertId);
+ServiceAlertResource serviceAlert = client.GetServiceAlertResource(serviceAlertResourceId);
 
 // invoke the operation
-Guid alertId = Guid.Parse("66114d64-d9d9-478b-95c9-b789d6502100");
-bool result = await collection.ExistsAsync(alertId);
+ServiceAlertResource result = await serviceAlert.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ServiceAlertData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
--- specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/examples-dotnet/SmartGroups_GetById.cs
+++ specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/examples-dotnet/SmartGroups_GetById.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -26,6 +27,18 @@ SmartGroupCollection collection = subscriptionResource.GetSmartGroups();
 
 // invoke the operation
 Guid smartGroupId = Guid.Parse("603675da-9851-4b26-854a-49fc53d32715");
-bool result = await collection.ExistsAsync(smartGroupId);
+NullableResponse<SmartGroupResource> response = await collection.GetIfExistsAsync(smartGroupId);
+SmartGroupResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SmartGroupData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

6edcb8b35e1ea1c3eaaf1488cd0a945917ca89cc
azure-sdk <azuresdk@microsoft.com>
2023-11-27 05:38:20 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.AppComplianceAutomation_1.0.0-beta.4

--- specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-dotnet/Snapshot_Get.cs
+++ specification/appcomplianceautomation/resource-manager/Microsoft.AppComplianceAutomation/preview/2022-11-16-preview/examples-dotnet/Snapshot_Get.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -24,6 +25,18 @@ SnapshotResourceCollection collection = reportResource.GetSnapshotResources();
 
 // invoke the operation
 string snapshotName = "testSnapshot";
-bool result = await collection.ExistsAsync(snapshotName);
+NullableResponse<SnapshotResource> response = await collection.GetIfExistsAsync(snapshotName);
+SnapshotResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SnapshotResourceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

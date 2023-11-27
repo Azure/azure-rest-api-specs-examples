904dd682a51bd6d1b3ac9404cc9af7c4f6112e52
azure-sdk <azuresdk@microsoft.com>
2023-11-27 02:37:09 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerregistry_11.0.0-beta.3

---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-11-01-preview/examples-js/RegistryCheckNameNotAvailable.js
@@ -0,0 +1,21 @@
+const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Checks whether the container registry name is available for use. The name must contain only alphanumeric characters, be globally unique, and between 5 and 50 characters in length.
+ *
+ * @summary Checks whether the container registry name is available for use. The name must contain only alphanumeric characters, be globally unique, and between 5 and 50 characters in length.
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-11-01-preview/examples/RegistryCheckNameNotAvailable.json
+ */
+async function registryCheckNameNotAvailable() {
+  const subscriptionId =
+    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const registryNameCheckRequest = {
+    name: "myRegistry",
+    type: "Microsoft.ContainerRegistry/registries",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  const result = await client.registries.checkNameAvailability(registryNameCheckRequest);
+  console.log(result);
+}
---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-11-01-preview/examples-js/RegistryCreateZoneRedundant.js
@@ -0,0 +1,29 @@
+const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates a container registry with the specified parameters.
+ *
+ * @summary Creates a container registry with the specified parameters.
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-11-01-preview/examples/RegistryCreateZoneRedundant.json
+ */
+async function registryCreateZoneRedundant() {
+  const subscriptionId =
+    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERREGISTRY_RESOURCE_GROUP"] || "myResourceGroup";
+  const registryName = "myRegistry";
+  const registry = {
+    location: "westus",
+    sku: { name: "Standard" },
+    tags: { key: "value" },
+    zoneRedundancy: "Enabled",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  const result = await client.registries.beginCreateAndWait(
+    resourceGroupName,
+    registryName,
+    registry
+  );
+  console.log(result);
+}

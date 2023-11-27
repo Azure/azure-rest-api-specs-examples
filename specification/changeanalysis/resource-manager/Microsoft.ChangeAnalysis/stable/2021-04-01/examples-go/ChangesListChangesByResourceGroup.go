13ab200556194f8bc7f16a9760ba51c08400ece3
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 10:03:55 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/changeanalysis/armchangeanalysis/v1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/changeanalysis/resource-manager/Microsoft.ChangeAnalysis/stable/2021-04-01/examples-go/ChangesListChangesByResourceGroup.go
+++ specification/changeanalysis/resource-manager/Microsoft.ChangeAnalysis/stable/2021-04-01/examples-go/ChangesListChangesByResourceGroup.go
@@ -55,7 +55,7 @@ func ExampleChangesClient_NewListChangesByResourceGroupPager() {
 		// 							OldValue: to.Ptr("old tag value"),
 		// 					}},
 		// 					ResourceID: to.Ptr("/subscriptions/4d962866-1e3f-47f2-bd18-450c08f914c1/resourceGroups/MyResourceGroup"),
-		// 					TimeStamp: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-04-26T02:17:41.39Z"); return t}()),
+		// 					TimeStamp: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-04-26T02:17:41.390Z"); return t}()),
 		// 				},
 		// 			},
 		// 			{
@@ -79,7 +79,7 @@ func ExampleChangesClient_NewListChangesByResourceGroupPager() {
 		// 								OldValue: to.Ptr("***Old Value Hidden***"),
 		// 						}},
 		// 						ResourceID: to.Ptr("/subscriptions/4d962866-1e3f-47f2-bd18-450c08f914c1/resourceGroups/MyResourceGroup/providers/Microsoft.Web/sites/mysite"),
-		// 						TimeStamp: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-04-26T02:17:41.39Z"); return t}()),
+		// 						TimeStamp: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-04-26T02:17:41.390Z"); return t}()),
 		// 					},
 		// 			}},
 		// 		}

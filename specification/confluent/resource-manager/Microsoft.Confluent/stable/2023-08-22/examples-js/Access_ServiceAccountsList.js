0a8d1332f285ba746ac8920656360ccfbd620dd9
azure-sdk <azuresdk@microsoft.com>
2023-11-27 02:38:22 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-confluent_3.0.0

---
+++ specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples-js/Access_ServiceAccountsList.js
@@ -0,0 +1,22 @@
+const { ConfluentManagementClient } = require("@azure/arm-confluent");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Organization service accounts details
+ *
+ * @summary Organization service accounts details
+ * x-ms-original-file: specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples/Access_ServiceAccountsList.json
+ */
+async function accessServiceAccountsList() {
+  const subscriptionId =
+    process.env["CONFLUENT_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONFLUENT_RESOURCE_GROUP"] || "myResourceGroup";
+  const organizationName = "myOrganization";
+  const body = {
+    searchFilters: { pageSize: "10", pageToken: "asc4fts4ft" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ConfluentManagementClient(credential, subscriptionId);
+  const result = await client.access.listServiceAccounts(resourceGroupName, organizationName, body);
+  console.log(result);
+}
---
+++ specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples-js/Organization_Update.js
@@ -0,0 +1,23 @@
+const { ConfluentManagementClient } = require("@azure/arm-confluent");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Update Organization resource
+ *
+ * @summary Update Organization resource
+ * x-ms-original-file: specification/confluent/resource-manager/Microsoft.Confluent/stable/2023-08-22/examples/Organization_Update.json
+ */
+async function confluentUpdate() {
+  const subscriptionId =
+    process.env["CONFLUENT_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONFLUENT_RESOURCE_GROUP"] || "myResourceGroup";
+  const organizationName = "myOrganization";
+  const body = {
+    tags: { client: "dev-client", env: "dev" },
+  };
+  const options = { body };
+  const credential = new DefaultAzureCredential();
+  const client = new ConfluentManagementClient(credential, subscriptionId);
+  const result = await client.organization.update(resourceGroupName, organizationName, options);
+  console.log(result);
+}

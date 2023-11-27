369ec7b4d56237baa4e278e96e0f9144ec4c2ac0
azure-sdk <azuresdk@microsoft.com>
2023-11-27 02:36:02 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-baremetalinfrastructure_1.0.0-beta.2

---
+++ specification/baremetalinfrastructure/resource-manager/Microsoft.BareMetalInfrastructure/preview/2023-08-04-preview/examples-js/AzureBareMetalStorageInstances_Get.js
@@ -0,0 +1,24 @@
+const { BareMetalInfrastructureClient } = require("@azure/arm-baremetalinfrastructure");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets an Azure Bare Metal Storage instance for the specified subscription, resource group, and instance name.
+ *
+ * @summary Gets an Azure Bare Metal Storage instance for the specified subscription, resource group, and instance name.
+ * x-ms-original-file: specification/baremetalinfrastructure/resource-manager/Microsoft.BareMetalInfrastructure/preview/2023-08-04-preview/examples/AzureBareMetalStorageInstances_Get.json
+ */
+async function getAnAzureBareMetalStorageInstance() {
+  const subscriptionId =
+    process.env["BAREMETALINFRASTRUCTURE_SUBSCRIPTION_ID"] ||
+    "f0f4887f-d13c-4943-a8ba-d7da28d2a3fd";
+  const resourceGroupName =
+    process.env["BAREMETALINFRASTRUCTURE_RESOURCE_GROUP"] || "myResourceGroup";
+  const azureBareMetalStorageInstanceName = "myAzureBareMetalStorageInstance";
+  const credential = new DefaultAzureCredential();
+  const client = new BareMetalInfrastructureClient(credential, subscriptionId);
+  const result = await client.azureBareMetalStorageInstances.get(
+    resourceGroupName,
+    azureBareMetalStorageInstanceName
+  );
+  console.log(result);
+}

b6d3d5eb6e502336391e3eb19122b7505609428d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 12:47:51 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/alertsmanagement/armalertsmanagement/v0.9.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/stable/2021-08-08/examples-go/AlertProcessingRules_Create_or_update_add_action_group_all_alerts_in_subscription.go
+++ specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/stable/2021-08-08/examples-go/AlertProcessingRules_Create_or_update_add_action_group_all_alerts_in_subscription.go
@@ -62,10 +62,10 @@ func ExampleAlertProcessingRulesClient_CreateOrUpdate_createOrUpdateARuleThatAdd
 	// 				to.Ptr("/subscriptions/subId1")},
 	// 			},
 	// 			SystemData: &armalertsmanagement.SystemData{
-	// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-02-12T22:05:09Z"); return t}()),
+	// 				CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-02-12T22:05:09.000Z"); return t}()),
 	// 				CreatedBy: to.Ptr("abc@microsoft.com"),
 	// 				CreatedByType: to.Ptr(armalertsmanagement.CreatedByTypeUser),
-	// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-02-13T16:15:34Z"); return t}()),
+	// 				LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-02-13T16:15:34.000Z"); return t}()),
 	// 				LastModifiedBy: to.Ptr("xyz@microsoft.com"),
 	// 				LastModifiedByType: to.Ptr(armalertsmanagement.CreatedByTypeUser),
 	// 			},

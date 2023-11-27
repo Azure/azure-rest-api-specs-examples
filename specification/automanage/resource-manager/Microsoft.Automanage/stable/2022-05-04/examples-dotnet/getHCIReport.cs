105743db2c677f1d2d5864b4d23f3f4caabc6604
azure-sdk <azuresdk@microsoft.com>
2023-11-27 12:42:56 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Automanage_1.1.0

--- specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-dotnet/getHCIReport.cs
+++ specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-dotnet/getHCIReport.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -27,6 +28,18 @@ AutomanageHciClusterConfigurationProfileAssignmentReportCollection collection = 
 
 // invoke the operation
 string reportName = "b4e9ee6b-1717-4ff0-a8d2-e6d72c33d5f4";
-bool result = await collection.ExistsAsync(reportName);
+NullableResponse<AutomanageHciClusterConfigurationProfileAssignmentReportResource> response = await collection.GetIfExistsAsync(reportName);
+AutomanageHciClusterConfigurationProfileAssignmentReportResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    AutomanageConfigurationProfileAssignmentReportData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

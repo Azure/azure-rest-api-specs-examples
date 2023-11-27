30b714c5253ffac937391c8a7b3ce13ac0e4783e
azure-sdk <azuresdk@microsoft.com>
2023-11-27 12:45:06 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ConfidentialLedger_1.1.0-beta.3

--- specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-01-26-preview/examples-dotnet/ManagedCCF_Get.cs
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-01-26-preview/examples-dotnet/ManagedCCF_Get.cs
@@ -29,6 +29,18 @@ ManagedCcfCollection collection = resourceGroupResource.GetManagedCcfs();
 
 // invoke the operation
 string appName = "DummyMccfAppName";
-bool result = await collection.ExistsAsync(appName);
+NullableResponse<ManagedCcfResource> response = await collection.GetIfExistsAsync(appName);
+ManagedCcfResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ManagedCcfData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

6f585fc354e3037a60a3361a928f1a687d32eec6
azure-sdk <azuresdk@microsoft.com>
2023-11-27 05:19:40 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.BotService_1.1.0

--- specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-dotnet/GetBot.cs
+++ specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-dotnet/GetBot.cs
@@ -16,19 +16,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this BotResource created on azure
-// for more information of creating BotResource, please refer to the document of BotResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "subscription-id";
 string resourceGroupName = "OneResourceGroupName";
-string resourceName = "samplebotname";
-ResourceIdentifier botResourceId = BotResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
-BotResource bot = client.GetBotResource(botResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this BotResource
+BotCollection collection = resourceGroupResource.GetBots();
 
 // invoke the operation
-BotResource result = await bot.GetAsync();
+string resourceName = "samplebotname";
+NullableResponse<BotResource> response = await collection.GetIfExistsAsync(resourceName);
+BotResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-BotData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    BotData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-dotnet/GetConnection.cs
+++ specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples-dotnet/GetConnection.cs
@@ -6,7 +6,6 @@ using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.BotService;
 using Azure.ResourceManager.BotService.Models;
-using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples/GetConnection.json
 // this example is just showing the usage of "BotConnection_Get" operation, for the dependent resources, they will have to be created separately.
@@ -16,20 +15,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this BotConnectionSettingResource created on azure
-// for more information of creating BotConnectionSettingResource, please refer to the document of BotConnectionSettingResource
+// this example assumes you already have this BotResource created on azure
+// for more information of creating BotResource, please refer to the document of BotResource
 string subscriptionId = "subscription-id";
 string resourceGroupName = "OneResourceGroupName";
 string resourceName = "samplebotname";
-string connectionName = "sampleConnection";
-ResourceIdentifier botConnectionSettingResourceId = BotConnectionSettingResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName, connectionName);
-BotConnectionSettingResource botConnectionSetting = client.GetBotConnectionSettingResource(botConnectionSettingResourceId);
+ResourceIdentifier botResourceId = BotResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
+BotResource bot = client.GetBotResource(botResourceId);
+
+// get the collection of this BotConnectionSettingResource
+BotConnectionSettingCollection collection = bot.GetBotConnectionSettings();
 
 // invoke the operation
-BotConnectionSettingResource result = await botConnectionSetting.GetAsync();
+string connectionName = "sampleConnection";
+NullableResponse<BotConnectionSettingResource> response = await collection.GetIfExistsAsync(connectionName);
+BotConnectionSettingResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-BotConnectionSettingData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    BotConnectionSettingData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

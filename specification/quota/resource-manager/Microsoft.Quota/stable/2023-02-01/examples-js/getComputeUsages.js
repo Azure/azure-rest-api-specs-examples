f27041ffd0e88fd74665f2519926969927135bac
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 12:47:09 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-quota_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/quota/resource-manager/Microsoft.Quota/stable/2023-02-01/examples-js/getComputeUsages.js
@@ -0,0 +1,20 @@
+const { AzureQuotaExtensionAPI } = require("@azure/arm-quota");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get a list of current usage for all resources for the scope specified.
+ *
+ * @summary Get a list of current usage for all resources for the scope specified.
+ * x-ms-original-file: specification/quota/resource-manager/Microsoft.Quota/stable/2023-02-01/examples/getComputeUsages.json
+ */
+async function quotasListUsagesForCompute() {
+  const scope =
+    "subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Compute/locations/eastus";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureQuotaExtensionAPI(credential);
+  const resArray = new Array();
+  for await (let item of client.usages.list(scope)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

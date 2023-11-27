9169c1f252f8b3c8ff7703f4f562d76bb7277137
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 20:01:04 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.CostManagement_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/cost-management/resource-manager/Microsoft.CostManagement/stable/2023-03-01/examples-dotnet/scheduledActions/scheduledAction-createOrUpdate-shared.cs
+++ specification/cost-management/resource-manager/Microsoft.CostManagement/stable/2023-03-01/examples-dotnet/scheduledActions/scheduledAction-createOrUpdate-shared.cs
@@ -15,16 +15,14 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ArmResource created on azure
-// for more information of creating ArmResource, please refer to the document of ArmResource
-
-// get the collection of this ScheduledActionResource
+// this example assumes you already have this ScheduledActionResource created on azure
+// for more information of creating ScheduledActionResource, please refer to the document of ScheduledActionResource
 string scope = "subscriptions/00000000-0000-0000-0000-000000000000";
-ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", scope));
-ScheduledActionCollection collection = client.GetScheduledActions(scopeId);
+string name = "monthlyCostByResource";
+ResourceIdentifier scheduledActionResourceId = ScheduledActionResource.CreateResourceIdentifier(scope, name);
+ScheduledActionResource scheduledAction = client.GetScheduledActionResource(scheduledActionResourceId);
 
 // invoke the operation
-string name = "monthlyCostByResource";
 ScheduledActionData data = new ScheduledActionData()
 {
     DisplayName = "Monthly Cost By Resource",
@@ -53,7 +51,7 @@ ScheduledActionData data = new ScheduledActionData()
     Kind = ScheduledActionKind.Email,
 };
 string ifMatch = "";
-ArmOperation<ScheduledActionResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name, data, ifMatch: ifMatch);
+ArmOperation<ScheduledActionResource> lro = await scheduledAction.UpdateAsync(WaitUntil.Completed, data, ifMatch: ifMatch);
 ScheduledActionResource result = lro.Value;
 
 // the variable result is a resource, you could call other operations on this instance as well

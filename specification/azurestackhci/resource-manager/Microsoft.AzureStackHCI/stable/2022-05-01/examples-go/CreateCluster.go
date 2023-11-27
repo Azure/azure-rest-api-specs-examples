6da9d27aedd4d317ca7c950bacbe1eeab07c0207
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 14:59:49 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/azurestackhci/armazurestackhci/v1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/stable/2022-05-01/examples-go/CreateCluster.go
+++ specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/stable/2022-05-01/examples-go/CreateCluster.go
@@ -59,10 +59,10 @@ func ExampleClustersClient_Create() {
 	// 		TrialDaysRemaining: to.Ptr[float32](30),
 	// 	},
 	// 	SystemData: &armazurestackhci.SystemData{
-	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.123Z"); return t}()),
 	// 		CreatedBy: to.Ptr("user1"),
 	// 		CreatedByType: to.Ptr(armazurestackhci.CreatedByTypeUser),
-	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.123Z"); return t}()),
 	// 		LastModifiedBy: to.Ptr("user2"),
 	// 		LastModifiedByType: to.Ptr(armazurestackhci.CreatedByTypeUser),
 	// 	},

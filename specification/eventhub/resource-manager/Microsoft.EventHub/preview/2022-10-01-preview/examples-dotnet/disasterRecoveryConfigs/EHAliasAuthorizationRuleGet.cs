9c8a348a39d4a69ec884edca595dc475877a6247
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 17:11:33 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.EventHubs_1.1.0-beta.4
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/eventhub/resource-manager/Microsoft.EventHub/preview/2022-10-01-preview/examples-dotnet/disasterRecoveryConfigs/EHAliasAuthorizationRuleGet.cs
+++ specification/eventhub/resource-manager/Microsoft.EventHub/preview/2022-10-01-preview/examples-dotnet/disasterRecoveryConfigs/EHAliasAuthorizationRuleGet.cs
@@ -4,6 +4,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.EventHubs;
+using Azure.ResourceManager.EventHubs.Models;
 
 // Generated from example definition: specification/eventhub/resource-manager/Microsoft.EventHub/preview/2022-10-01-preview/examples/disasterRecoveryConfigs/EHAliasAuthorizationRuleGet.json
 // this example is just showing the usage of "DisasterRecoveryConfigs_GetAuthorizationRule" operation, for the dependent resources, they will have to be created separately.
@@ -13,20 +14,21 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this EventHubsDisasterRecoveryResource created on azure
-// for more information of creating EventHubsDisasterRecoveryResource, please refer to the document of EventHubsDisasterRecoveryResource
+// this example assumes you already have this EventHubsDisasterRecoveryAuthorizationRuleResource created on azure
+// for more information of creating EventHubsDisasterRecoveryAuthorizationRuleResource, please refer to the document of EventHubsDisasterRecoveryAuthorizationRuleResource
 string subscriptionId = "exampleSubscriptionId";
 string resourceGroupName = "exampleResourceGroup";
 string namespaceName = "sdk-Namespace-9080";
 string @alias = "sdk-DisasterRecovery-4879";
-ResourceIdentifier eventHubsDisasterRecoveryResourceId = EventHubsDisasterRecoveryResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName, @alias);
-EventHubsDisasterRecoveryResource eventHubsDisasterRecovery = client.GetEventHubsDisasterRecoveryResource(eventHubsDisasterRecoveryResourceId);
-
-// get the collection of this EventHubsDisasterRecoveryAuthorizationRuleResource
-EventHubsDisasterRecoveryAuthorizationRuleCollection collection = eventHubsDisasterRecovery.GetEventHubsDisasterRecoveryAuthorizationRules();
+string authorizationRuleName = "sdk-Authrules-4879";
+ResourceIdentifier eventHubsDisasterRecoveryAuthorizationRuleResourceId = EventHubsDisasterRecoveryAuthorizationRuleResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName, @alias, authorizationRuleName);
+EventHubsDisasterRecoveryAuthorizationRuleResource eventHubsDisasterRecoveryAuthorizationRule = client.GetEventHubsDisasterRecoveryAuthorizationRuleResource(eventHubsDisasterRecoveryAuthorizationRuleResourceId);
 
 // invoke the operation
-string authorizationRuleName = "sdk-Authrules-4879";
-bool result = await collection.ExistsAsync(authorizationRuleName);
+EventHubsDisasterRecoveryAuthorizationRuleResource result = await eventHubsDisasterRecoveryAuthorizationRule.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+EventHubsAuthorizationRuleData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

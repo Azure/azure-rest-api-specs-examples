693245d7cc190cc034a548959eb0adb2b4d9576b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 17:12:38 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ApiCenter_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2023-07-01-preview/examples-dotnet/Services_Get.cs
+++ specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2023-07-01-preview/examples-dotnet/Services_Get.cs
@@ -5,7 +5,6 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.ApiCenter;
-using Azure.ResourceManager.ApiCenter.Models;
 using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2023-07-01-preview/examples/Services_Get.json
@@ -16,19 +15,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ApiCenterServiceResource created on azure
-// for more information of creating ApiCenterServiceResource, please refer to the document of ApiCenterServiceResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "contoso-resources";
-string serviceName = "contoso";
-ResourceIdentifier apiCenterServiceResourceId = ApiCenterServiceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, serviceName);
-ApiCenterServiceResource apiCenterService = client.GetApiCenterServiceResource(apiCenterServiceResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ApiCenterServiceResource
+ApiCenterServiceCollection collection = resourceGroupResource.GetApiCenterServices();
 
 // invoke the operation
-ApiCenterServiceResource result = await apiCenterService.GetAsync();
+string serviceName = "contoso";
+NullableResponse<ApiCenterServiceResource> response = await collection.GetIfExistsAsync(serviceName);
+ApiCenterServiceResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ApiCenterServiceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ApiCenterServiceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

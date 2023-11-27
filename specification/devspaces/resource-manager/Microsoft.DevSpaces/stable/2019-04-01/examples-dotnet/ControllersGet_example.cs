4a00c074bb082d9d65b22c4cb003db9450611823
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-27 20:01:47 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DevSpaces_1.0.0-beta.4
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/devspaces/resource-manager/Microsoft.DevSpaces/stable/2019-04-01/examples-dotnet/ControllersGet_example.cs
+++ specification/devspaces/resource-manager/Microsoft.DevSpaces/stable/2019-04-01/examples-dotnet/ControllersGet_example.cs
@@ -16,19 +16,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ControllerResource created on azure
-// for more information of creating ControllerResource, please refer to the document of ControllerResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
-string name = "myControllerResource";
-ResourceIdentifier controllerResourceId = ControllerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, name);
-ControllerResource controller = client.GetControllerResource(controllerResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ControllerResource
+ControllerCollection collection = resourceGroupResource.GetControllers();
 
 // invoke the operation
-ControllerResource result = await controller.GetAsync();
+string name = "myControllerResource";
+NullableResponse<ControllerResource> response = await collection.GetIfExistsAsync(name);
+ControllerResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ControllerData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ControllerData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

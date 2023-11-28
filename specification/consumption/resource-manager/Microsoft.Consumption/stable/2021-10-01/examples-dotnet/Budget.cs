eaa73e2169c2f0a90a792a3b02c9b90a45633fd0
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-11-28 10:07:04 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Consumption_1.1.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/consumption/resource-manager/Microsoft.Consumption/stable/2021-10-01/examples-dotnet/Budget.cs
+++ specification/consumption/resource-manager/Microsoft.Consumption/stable/2021-10-01/examples-dotnet/Budget.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -23,6 +24,18 @@ ConsumptionBudgetCollection collection = client.GetConsumptionBudgets(scopeId);
 
 // invoke the operation
 string budgetName = "TestBudget";
-bool result = await collection.ExistsAsync(budgetName);
+NullableResponse<ConsumptionBudgetResource> response = await collection.GetIfExistsAsync(budgetName);
+ConsumptionBudgetResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ConsumptionBudgetData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

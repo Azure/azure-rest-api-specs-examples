174729babccd779436608b91d3cd66cbf18fb9ac
azure-sdk <azuresdk@microsoft.com>
2023-11-30 12:26:48 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.IotCentral_1.1.0-beta.2

--- specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-dotnet/Apps_Get.cs
+++ specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-dotnet/Apps_Get.cs
@@ -17,19 +17,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this IotCentralAppResource created on azure
-// for more information of creating IotCentralAppResource, please refer to the document of IotCentralAppResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "resRg";
-string resourceName = "myIoTCentralApp";
-ResourceIdentifier iotCentralAppResourceId = IotCentralAppResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, resourceName);
-IotCentralAppResource iotCentralApp = client.GetIotCentralAppResource(iotCentralAppResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this IotCentralAppResource
+IotCentralAppCollection collection = resourceGroupResource.GetIotCentralApps();
 
 // invoke the operation
-IotCentralAppResource result = await iotCentralApp.GetAsync();
+string resourceName = "myIoTCentralApp";
+NullableResponse<IotCentralAppResource> response = await collection.GetIfExistsAsync(resourceName);
+IotCentralAppResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-IotCentralAppData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    IotCentralAppData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-dotnet/PrivateEndpointConnections_Get.cs
+++ specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-dotnet/PrivateEndpointConnections_Get.cs
@@ -28,6 +28,18 @@ IotCentralPrivateEndpointConnectionCollection collection = iotCentralApp.GetIotC
 
 // invoke the operation
 string privateEndpointConnectionName = "myIoTCentralAppEndpoint";
-bool result = await collection.ExistsAsync(privateEndpointConnectionName);
+NullableResponse<IotCentralPrivateEndpointConnectionResource> response = await collection.GetIfExistsAsync(privateEndpointConnectionName);
+IotCentralPrivateEndpointConnectionResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    IotCentralPrivateEndpointConnectionData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

b5fca8c433ebae0cffcf77573c9657f445bc2e01
azure-sdk <azuresdk@microsoft.com>
2023-11-30 12:57:53 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.FluidRelay_1.1.0

--- specification/fluidrelay/resource-manager/Microsoft.FluidRelay/stable/2022-06-01/examples-dotnet/FluidRelayServers_Get.cs
+++ specification/fluidrelay/resource-manager/Microsoft.FluidRelay/stable/2022-06-01/examples-dotnet/FluidRelayServers_Get.cs
@@ -6,7 +6,6 @@ using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.FluidRelay;
 using Azure.ResourceManager.FluidRelay.Models;
-using Azure.ResourceManager.Models;
 using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/fluidrelay/resource-manager/Microsoft.FluidRelay/stable/2022-06-01/examples/FluidRelayServers_Get.json
@@ -17,18 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this FluidRelayServerResource created on azure
+// for more information of creating FluidRelayServerResource, please refer to the document of FluidRelayServerResource
 string subscriptionId = "xxxx-xxxx-xxxx-xxxx";
 string resourceGroup = "myResourceGroup";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this FluidRelayServerResource
-FluidRelayServerCollection collection = resourceGroupResource.GetFluidRelayServers();
+string fluidRelayServerName = "myFluidRelayServer";
+ResourceIdentifier fluidRelayServerResourceId = FluidRelayServerResource.CreateResourceIdentifier(subscriptionId, resourceGroup, fluidRelayServerName);
+FluidRelayServerResource fluidRelayServer = client.GetFluidRelayServerResource(fluidRelayServerResourceId);
 
 // invoke the operation
-string fluidRelayServerName = "myFluidRelayServer";
-bool result = await collection.ExistsAsync(fluidRelayServerName);
+FluidRelayServerResource result = await fluidRelayServer.GetAsync();
 
-Console.WriteLine($"Succeeded: {result}");
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+FluidRelayServerData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

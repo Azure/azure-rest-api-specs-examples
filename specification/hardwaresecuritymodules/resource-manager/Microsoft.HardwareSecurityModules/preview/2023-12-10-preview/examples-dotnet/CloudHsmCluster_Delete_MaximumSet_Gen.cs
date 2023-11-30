b607296b50f1b9c099f25d298271651011a4e902
azure-sdk <azuresdk@microsoft.com>
2023-11-30 12:40:32 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HardwareSecurityModules_1.0.0-beta.2

---
+++ specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2023-12-10-preview/examples-dotnet/CloudHsmCluster_Delete_MaximumSet_Gen.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.HardwareSecurityModules;
+using Azure.ResourceManager.HardwareSecurityModules.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2023-12-10-preview/examples/CloudHsmCluster_Delete_MaximumSet_Gen.json
+// this example is just showing the usage of "CloudHsmClusters_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this CloudHsmClusterResource created on azure
+// for more information of creating CloudHsmClusterResource, please refer to the document of CloudHsmClusterResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "rgcloudhsm";
+string cloudHsmClusterName = "chsm1";
+ResourceIdentifier cloudHsmClusterResourceId = CloudHsmClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, cloudHsmClusterName);
+CloudHsmClusterResource cloudHsmCluster = client.GetCloudHsmClusterResource(cloudHsmClusterResourceId);
+
+// invoke the operation
+await cloudHsmCluster.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

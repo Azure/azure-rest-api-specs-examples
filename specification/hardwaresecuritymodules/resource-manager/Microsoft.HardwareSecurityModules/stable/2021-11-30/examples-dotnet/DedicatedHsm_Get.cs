43e7ecb2421496c898e8aabbbe4817c0ac9d798d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-01 20:02:52 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HardwareSecurityModules_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/stable/2021-11-30/examples-dotnet/DedicatedHsm_Get.cs
+++ specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/stable/2021-11-30/examples-dotnet/DedicatedHsm_Get.cs
@@ -16,30 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this DedicatedHsmResource created on azure
+// for more information of creating DedicatedHsmResource, please refer to the document of DedicatedHsmResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "hsm-group";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this DedicatedHsmResource
-DedicatedHsmCollection collection = resourceGroupResource.GetDedicatedHsms();
+string name = "hsm1";
+ResourceIdentifier dedicatedHsmResourceId = DedicatedHsmResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, name);
+DedicatedHsmResource dedicatedHsm = client.GetDedicatedHsmResource(dedicatedHsmResourceId);
 
 // invoke the operation
-string name = "hsm1";
-NullableResponse<DedicatedHsmResource> response = await collection.GetIfExistsAsync(name);
-DedicatedHsmResource result = response.HasValue ? response.Value : null;
+DedicatedHsmResource result = await dedicatedHsm.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine($"Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    DedicatedHsmData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DedicatedHsmData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

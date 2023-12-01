7bc8fe7ad265ce84b7505a2f33e4876d80348a77
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-01 20:01:32 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ExtendedLocations_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples-dotnet/CustomLocationsGet.cs
+++ specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/stable/2021-08-15/examples-dotnet/CustomLocationsGet.cs
@@ -29,6 +29,18 @@ CustomLocationCollection collection = resourceGroupResource.GetCustomLocations()
 
 // invoke the operation
 string resourceName = "customLocation01";
-bool result = await collection.ExistsAsync(resourceName);
+NullableResponse<CustomLocationResource> response = await collection.GetIfExistsAsync(resourceName);
+CustomLocationResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    CustomLocationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

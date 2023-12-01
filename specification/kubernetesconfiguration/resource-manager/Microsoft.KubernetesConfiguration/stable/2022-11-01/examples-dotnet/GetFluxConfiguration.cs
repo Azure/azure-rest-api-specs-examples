ae22949a60121eba6fd1173434e43bd514fdf8a0
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-01 20:03:58 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.KubernetesConfiguration_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples-dotnet/GetFluxConfiguration.cs
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/stable/2022-11-01/examples-dotnet/GetFluxConfiguration.cs
@@ -31,6 +31,18 @@ KubernetesFluxConfigurationCollection collection = resourceGroupResource.GetKube
 
 // invoke the operation
 string fluxConfigurationName = "srs-fluxconfig";
-bool result = await collection.ExistsAsync(fluxConfigurationName);
+NullableResponse<KubernetesFluxConfigurationResource> response = await collection.GetIfExistsAsync(fluxConfigurationName);
+KubernetesFluxConfigurationResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    KubernetesFluxConfigurationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

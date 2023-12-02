ee1d3bb4dfabc258c0bc21315830945254ef823e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-02 20:02:13 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StoragePool_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/storagepool/resource-manager/Microsoft.StoragePool/stable/2021-08-01/examples-dotnet/DiskPools_Get.cs
+++ specification/storagepool/resource-manager/Microsoft.StoragePool/stable/2021-08-01/examples-dotnet/DiskPools_Get.cs
@@ -17,19 +17,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DiskPoolResource created on azure
-// for more information of creating DiskPoolResource, please refer to the document of DiskPoolResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
-string diskPoolName = "myDiskPool";
-ResourceIdentifier diskPoolResourceId = DiskPoolResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, diskPoolName);
-DiskPoolResource diskPool = client.GetDiskPoolResource(diskPoolResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this DiskPoolResource
+DiskPoolCollection collection = resourceGroupResource.GetDiskPools();
 
 // invoke the operation
-DiskPoolResource result = await diskPool.GetAsync();
+string diskPoolName = "myDiskPool";
+NullableResponse<DiskPoolResource> response = await collection.GetIfExistsAsync(diskPoolName);
+DiskPoolResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-DiskPoolData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    DiskPoolData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

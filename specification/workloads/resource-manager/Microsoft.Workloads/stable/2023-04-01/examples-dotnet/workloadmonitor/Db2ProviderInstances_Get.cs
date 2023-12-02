10c562f9b2c69866a2a918907b631185c37cda81
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-02 20:03:10 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Workloads_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/workloads/resource-manager/Microsoft.Workloads/stable/2023-04-01/examples-dotnet/workloadmonitor/Db2ProviderInstances_Get.cs
+++ specification/workloads/resource-manager/Microsoft.Workloads/stable/2023-04-01/examples-dotnet/workloadmonitor/Db2ProviderInstances_Get.cs
@@ -28,6 +28,18 @@ SapProviderInstanceCollection collection = sapMonitor.GetSapProviderInstances();
 
 // invoke the operation
 string providerInstanceName = "myProviderInstance";
-bool result = await collection.ExistsAsync(providerInstanceName);
+NullableResponse<SapProviderInstanceResource> response = await collection.GetIfExistsAsync(providerInstanceName);
+SapProviderInstanceResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SapProviderInstanceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

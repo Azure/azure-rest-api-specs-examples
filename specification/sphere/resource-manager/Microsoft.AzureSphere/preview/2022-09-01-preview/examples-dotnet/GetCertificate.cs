49b772b93926f5e9211ea77ea17735813eb941e4
azure-sdk <azuresdk@microsoft.com>
2023-12-02 12:47:22 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Sphere_1.0.0-beta.2

--- specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples-dotnet/GetCertificate.cs
+++ specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples-dotnet/GetCertificate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -26,6 +27,18 @@ SphereCertificateCollection collection = sphereCatalog.GetSphereCertificates();
 
 // invoke the operation
 string serialNumber = "default";
-bool result = await collection.ExistsAsync(serialNumber);
+NullableResponse<SphereCertificateResource> response = await collection.GetIfExistsAsync(serialNumber);
+SphereCertificateResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SphereCertificateData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples-dotnet/GetDevice.cs
+++ specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples-dotnet/GetDevice.cs
@@ -5,7 +5,6 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Sphere;
-using Azure.ResourceManager.Sphere.Models;
 
 // Generated from example definition: specification/sphere/resource-manager/Microsoft.AzureSphere/preview/2022-09-01-preview/examples/GetDevice.json
 // this example is just showing the usage of "Devices_Get" operation, for the dependent resources, they will have to be created separately.
@@ -15,22 +14,33 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SphereDeviceResource created on azure
-// for more information of creating SphereDeviceResource, please refer to the document of SphereDeviceResource
+// this example assumes you already have this SphereDeviceGroupResource created on azure
+// for more information of creating SphereDeviceGroupResource, please refer to the document of SphereDeviceGroupResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "MyResourceGroup1";
 string catalogName = "MyCatalog1";
 string productName = "MyProduct1";
 string deviceGroupName = "myDeviceGroup1";
-string deviceName = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-ResourceIdentifier sphereDeviceResourceId = SphereDeviceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, catalogName, productName, deviceGroupName, deviceName);
-SphereDeviceResource sphereDevice = client.GetSphereDeviceResource(sphereDeviceResourceId);
+ResourceIdentifier sphereDeviceGroupResourceId = SphereDeviceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, catalogName, productName, deviceGroupName);
+SphereDeviceGroupResource sphereDeviceGroup = client.GetSphereDeviceGroupResource(sphereDeviceGroupResourceId);
+
+// get the collection of this SphereDeviceResource
+SphereDeviceCollection collection = sphereDeviceGroup.GetSphereDevices();
 
 // invoke the operation
-SphereDeviceResource result = await sphereDevice.GetAsync();
+string deviceName = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+NullableResponse<SphereDeviceResource> response = await collection.GetIfExistsAsync(deviceName);
+SphereDeviceResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-SphereDeviceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SphereDeviceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

977b647033fdc2697e6c057c69492749e64e7490
azure-sdk <azuresdk@microsoft.com>
2023-12-02 12:27:26 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.RecoveryServices_1.1.1

--- specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-04-01/examples-dotnet/GetPrivateLinkResources.cs
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2023-04-01/examples-dotnet/GetPrivateLinkResources.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -13,20 +14,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this RecoveryServicesPrivateLinkResource created on azure
-// for more information of creating RecoveryServicesPrivateLinkResource, please refer to the document of RecoveryServicesPrivateLinkResource
+// this example assumes you already have this RecoveryServicesVaultResource created on azure
+// for more information of creating RecoveryServicesVaultResource, please refer to the document of RecoveryServicesVaultResource
 string subscriptionId = "6c48fa17-39c7-45f1-90ac-47a587128ace";
 string resourceGroupName = "petesting";
 string vaultName = "pemsi-ecy-rsv2";
-string privateLinkResourceName = "backupResource";
-ResourceIdentifier recoveryServicesPrivateLinkResourceId = RecoveryServicesPrivateLinkResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, vaultName, privateLinkResourceName);
-RecoveryServicesPrivateLinkResource recoveryServicesPrivateLinkResource = client.GetRecoveryServicesPrivateLinkResource(recoveryServicesPrivateLinkResourceId);
+ResourceIdentifier recoveryServicesVaultResourceId = RecoveryServicesVaultResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, vaultName);
+RecoveryServicesVaultResource recoveryServicesVault = client.GetRecoveryServicesVaultResource(recoveryServicesVaultResourceId);
+
+// get the collection of this RecoveryServicesPrivateLinkResource
+RecoveryServicesPrivateLinkResourceCollection collection = recoveryServicesVault.GetRecoveryServicesPrivateLinkResources();
 
 // invoke the operation
-RecoveryServicesPrivateLinkResource result = await recoveryServicesPrivateLinkResource.GetAsync();
+string privateLinkResourceName = "backupResource";
+NullableResponse<RecoveryServicesPrivateLinkResource> response = await collection.GetIfExistsAsync(privateLinkResourceName);
+RecoveryServicesPrivateLinkResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-RecoveryServicesPrivateLinkResourceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    RecoveryServicesPrivateLinkResourceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

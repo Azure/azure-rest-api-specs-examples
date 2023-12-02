164789e1ce5e09fe4b469b1481937e4be9bc59c5
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-02 20:02:27 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StorageSync_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/storagesync/resource-manager/Microsoft.StorageSync/stable/2022-06-01/examples-dotnet/StorageSyncServices_Get.cs
+++ specification/storagesync/resource-manager/Microsoft.StorageSync/stable/2022-06-01/examples-dotnet/StorageSyncServices_Get.cs
@@ -16,19 +16,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this StorageSyncServiceResource created on azure
-// for more information of creating StorageSyncServiceResource, please refer to the document of StorageSyncServiceResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "52b8da2f-61e0-4a1f-8dde-336911f367fb";
 string resourceGroupName = "SampleResourceGroup_1";
-string storageSyncServiceName = "SampleStorageSyncService_1";
-ResourceIdentifier storageSyncServiceResourceId = StorageSyncServiceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, storageSyncServiceName);
-StorageSyncServiceResource storageSyncService = client.GetStorageSyncServiceResource(storageSyncServiceResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this StorageSyncServiceResource
+StorageSyncServiceCollection collection = resourceGroupResource.GetStorageSyncServices();
 
 // invoke the operation
-StorageSyncServiceResource result = await storageSyncService.GetAsync();
+string storageSyncServiceName = "SampleStorageSyncService_1";
+NullableResponse<StorageSyncServiceResource> response = await collection.GetIfExistsAsync(storageSyncServiceName);
+StorageSyncServiceResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-StorageSyncServiceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    StorageSyncServiceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

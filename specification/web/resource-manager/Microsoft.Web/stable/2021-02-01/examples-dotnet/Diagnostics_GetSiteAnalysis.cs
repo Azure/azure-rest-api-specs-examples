c4b0081fc73dbba52cb0859807840718d85ab21c
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-02 20:02:55 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.AppService_1.1.0-beta.4
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/web/resource-manager/Microsoft.Web/stable/2021-02-01/examples-dotnet/Diagnostics_GetSiteAnalysis.cs
+++ specification/web/resource-manager/Microsoft.Web/stable/2021-02-01/examples-dotnet/Diagnostics_GetSiteAnalysis.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -28,6 +29,18 @@ SiteSlotDiagnosticAnalysisCollection collection = siteSlotDiagnostic.GetSiteSlot
 
 // invoke the operation
 string analysisName = "appanalysis";
-bool result = await collection.ExistsAsync(analysisName);
+NullableResponse<SiteSlotDiagnosticAnalysisResource> response = await collection.GetIfExistsAsync(analysisName);
+SiteSlotDiagnosticAnalysisResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    WebSiteAnalysisDefinitionData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/web/resource-manager/Microsoft.Web/stable/2021-02-01/examples-dotnet/KubeEnvironments_Get.cs
+++ specification/web/resource-manager/Microsoft.Web/stable/2021-02-01/examples-dotnet/KubeEnvironments_Get.cs
@@ -5,7 +5,6 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.AppService;
-using Azure.ResourceManager.AppService.Models;
 using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/web/resource-manager/Microsoft.Web/stable/2021-02-01/examples/KubeEnvironments_Get.json
@@ -16,19 +15,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this KubeEnvironmentResource created on azure
-// for more information of creating KubeEnvironmentResource, please refer to the document of KubeEnvironmentResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "8efdecc5-919e-44eb-b179-915dca89ebf9";
 string resourceGroupName = "examplerg";
-string name = "jlaw-demo1";
-ResourceIdentifier kubeEnvironmentResourceId = KubeEnvironmentResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, name);
-KubeEnvironmentResource kubeEnvironment = client.GetKubeEnvironmentResource(kubeEnvironmentResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this KubeEnvironmentResource
+KubeEnvironmentCollection collection = resourceGroupResource.GetKubeEnvironments();
 
 // invoke the operation
-KubeEnvironmentResource result = await kubeEnvironment.GetAsync();
+string name = "jlaw-demo1";
+NullableResponse<KubeEnvironmentResource> response = await collection.GetIfExistsAsync(name);
+KubeEnvironmentResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-KubeEnvironmentData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    KubeEnvironmentData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

b3afeed11b9608a0468ea7114d6b9f3a86b615a7
azure-sdk <azuresdk@microsoft.com>
2023-12-02 12:49:24 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.SqlVirtualMachine_1.1.0

--- specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/stable/2022-02-01/examples-dotnet/GetAvailabilityGroupListener.cs
+++ specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/stable/2022-02-01/examples-dotnet/GetAvailabilityGroupListener.cs
@@ -29,6 +29,18 @@ AvailabilityGroupListenerCollection collection = sqlVmGroup.GetAvailabilityGroup
 
 // invoke the operation
 string availabilityGroupListenerName = "agl-test";
-bool result = await collection.ExistsAsync(availabilityGroupListenerName);
+NullableResponse<AvailabilityGroupListenerResource> response = await collection.GetIfExistsAsync(availabilityGroupListenerName);
+AvailabilityGroupListenerResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    AvailabilityGroupListenerData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/stable/2022-02-01/examples-dotnet/GetSqlVirtualMachine.cs
+++ specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/stable/2022-02-01/examples-dotnet/GetSqlVirtualMachine.cs
@@ -29,6 +29,18 @@ SqlVmCollection collection = resourceGroupResource.GetSqlVms();
 
 // invoke the operation
 string sqlVmName = "testvm";
-bool result = await collection.ExistsAsync(sqlVmName);
+NullableResponse<SqlVmResource> response = await collection.GetIfExistsAsync(sqlVmName);
+SqlVmResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SqlVmData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

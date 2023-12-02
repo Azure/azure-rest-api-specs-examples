a98c50dbae20616d4600e292f547bb785b261c4f
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-02 20:01:59 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Reservations_1.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/reservations/resource-manager/Microsoft.Capacity/stable/2020-10-25/examples-dotnet/getQuotaRequestStatusById.cs
+++ specification/reservations/resource-manager/Microsoft.Capacity/stable/2020-10-25/examples-dotnet/getQuotaRequestStatusById.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Reservations;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/reservations/resource-manager/Microsoft.Capacity/stable/2020-10-25/examples/getQuotaRequestStatusById.json
 // this example is just showing the usage of "QuotaRequestStatus_Get" operation, for the dependent resources, they will have to be created separately.
@@ -13,20 +15,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this QuotaRequestDetailResource created on azure
-// for more information of creating QuotaRequestDetailResource, please refer to the document of QuotaRequestDetailResource
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// get the collection of this QuotaRequestDetailResource
 string providerId = "Microsoft.Compute";
 AzureLocation location = new AzureLocation("eastus");
-Guid id = Guid.Parse("2B5C8515-37D8-4B6A-879B-CD641A2CF605");
-ResourceIdentifier quotaRequestDetailResourceId = QuotaRequestDetailResource.CreateResourceIdentifier(subscriptionId, providerId, location, id);
-QuotaRequestDetailResource quotaRequestDetail = client.GetQuotaRequestDetailResource(quotaRequestDetailResourceId);
+QuotaRequestDetailCollection collection = subscriptionResource.GetQuotaRequestDetails(providerId, location);
 
 // invoke the operation
-QuotaRequestDetailResource result = await quotaRequestDetail.GetAsync();
+Guid id = Guid.Parse("2B5C8515-37D8-4B6A-879B-CD641A2CF605");
+NullableResponse<QuotaRequestDetailResource> response = await collection.GetIfExistsAsync(id);
+QuotaRequestDetailResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-QuotaRequestDetailData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    QuotaRequestDetailData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

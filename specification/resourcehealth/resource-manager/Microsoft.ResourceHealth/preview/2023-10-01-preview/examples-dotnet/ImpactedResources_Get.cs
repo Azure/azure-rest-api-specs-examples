59a73bc8471d7e2f9283a5a0513bab1b7627714a
azure-sdk <azuresdk@microsoft.com>
2023-12-02 12:33:31 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceHealth_1.1.0-beta.3

--- specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/ImpactedResources_Get.cs
+++ specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/ImpactedResources_Get.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -14,30 +13,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceHealthEventResource created on azure
-// for more information of creating ResourceHealthEventResource, please refer to the document of ResourceHealthEventResource
+// this example assumes you already have this ResourceHealthEventImpactedResource created on azure
+// for more information of creating ResourceHealthEventImpactedResource, please refer to the document of ResourceHealthEventImpactedResource
 string subscriptionId = "subscriptionId";
 string eventTrackingId = "BC_1-FXZ";
-ResourceIdentifier resourceHealthEventResourceId = ResourceHealthEventResource.CreateResourceIdentifier(subscriptionId, eventTrackingId);
-ResourceHealthEventResource resourceHealthEvent = client.GetResourceHealthEventResource(resourceHealthEventResourceId);
-
-// get the collection of this ResourceHealthEventImpactedResource
-ResourceHealthEventImpactedResourceCollection collection = resourceHealthEvent.GetResourceHealthEventImpactedResources();
+string impactedResourceName = "abc-123-ghj-456";
+ResourceIdentifier resourceHealthEventImpactedResourceId = ResourceHealthEventImpactedResource.CreateResourceIdentifier(subscriptionId, eventTrackingId, impactedResourceName);
+ResourceHealthEventImpactedResource resourceHealthEventImpactedResource = client.GetResourceHealthEventImpactedResource(resourceHealthEventImpactedResourceId);
 
 // invoke the operation
-string impactedResourceName = "abc-123-ghj-456";
-NullableResponse<ResourceHealthEventImpactedResource> response = await collection.GetIfExistsAsync(impactedResourceName);
-ResourceHealthEventImpactedResource result = response.HasValue ? response.Value : null;
+ResourceHealthEventImpactedResource result = await resourceHealthEventImpactedResource.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine($"Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    ResourceHealthEventImpactedResourceData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ResourceHealthEventImpactedResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

e51145f47ebe6cda74dfffae48fd61c38dbb31c5
azure-sdk <azuresdk@microsoft.com>
2023-12-03 12:24:14 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ManagedServices_1.1.0

--- specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples-dotnet/GetRegistrationDefinition.cs
+++ specification/managedservices/resource-manager/Microsoft.ManagedServices/stable/2022-10-01/examples-dotnet/GetRegistrationDefinition.cs
@@ -16,18 +16,28 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ManagedServicesRegistrationResource created on azure
-// for more information of creating ManagedServicesRegistrationResource, please refer to the document of ManagedServicesRegistrationResource
+// this example assumes you already have this ArmResource created on azure
+// for more information of creating ArmResource, please refer to the document of ArmResource
+
+// get the collection of this ManagedServicesRegistrationResource
 string scope = "subscription/0afefe50-734e-4610-8a82-a144ahf49dea";
-string registrationId = "26c128c2-fefa-4340-9bb1-6e081c90ada2";
-ResourceIdentifier managedServicesRegistrationResourceId = ManagedServicesRegistrationResource.CreateResourceIdentifier(scope, registrationId);
-ManagedServicesRegistrationResource managedServicesRegistration = client.GetManagedServicesRegistrationResource(managedServicesRegistrationResourceId);
+ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", scope));
+ManagedServicesRegistrationCollection collection = client.GetManagedServicesRegistrations(scopeId);
 
 // invoke the operation
-ManagedServicesRegistrationResource result = await managedServicesRegistration.GetAsync();
+string registrationId = "26c128c2-fefa-4340-9bb1-6e081c90ada2";
+NullableResponse<ManagedServicesRegistrationResource> response = await collection.GetIfExistsAsync(registrationId);
+ManagedServicesRegistrationResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ManagedServicesRegistrationData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ManagedServicesRegistrationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

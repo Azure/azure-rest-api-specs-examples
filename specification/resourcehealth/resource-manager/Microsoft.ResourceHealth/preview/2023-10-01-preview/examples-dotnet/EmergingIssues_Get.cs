baadc875864ad8ed784dc294d77d68677dd8505a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-03 20:03:21 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceHealth_1.1.0-beta.3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/EmergingIssues_Get.cs
+++ specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/EmergingIssues_Get.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -14,17 +15,27 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ServiceEmergingIssueResource created on azure
-// for more information of creating ServiceEmergingIssueResource, please refer to the document of ServiceEmergingIssueResource
-EmergingIssueNameContent issueName = EmergingIssueNameContent.Default;
-ResourceIdentifier serviceEmergingIssueResourceId = ServiceEmergingIssueResource.CreateResourceIdentifier(issueName);
-ServiceEmergingIssueResource serviceEmergingIssue = client.GetServiceEmergingIssueResource(serviceEmergingIssueResourceId);
+// this example assumes you already have this TenantResource created on azure
+// for more information of creating TenantResource, please refer to the document of TenantResource
+var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+// get the collection of this ServiceEmergingIssueResource
+ServiceEmergingIssueCollection collection = tenantResource.GetServiceEmergingIssues();
 
 // invoke the operation
-ServiceEmergingIssueResource result = await serviceEmergingIssue.GetAsync();
+EmergingIssueNameContent issueName = EmergingIssueNameContent.Default;
+NullableResponse<ServiceEmergingIssueResource> response = await collection.GetIfExistsAsync(issueName);
+ServiceEmergingIssueResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ServiceEmergingIssueData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ServiceEmergingIssueData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/Metadata_GetEntity.cs
+++ specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/Metadata_GetEntity.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -14,27 +13,17 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this TenantResource created on azure
-// for more information of creating TenantResource, please refer to the document of TenantResource
-var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
-
-// get the collection of this ResourceHealthMetadataEntityResource
-ResourceHealthMetadataEntityCollection collection = tenantResource.GetResourceHealthMetadataEntities();
+// this example assumes you already have this ResourceHealthMetadataEntityResource created on azure
+// for more information of creating ResourceHealthMetadataEntityResource, please refer to the document of ResourceHealthMetadataEntityResource
+string name = "status";
+ResourceIdentifier resourceHealthMetadataEntityResourceId = ResourceHealthMetadataEntityResource.CreateResourceIdentifier(name);
+ResourceHealthMetadataEntityResource resourceHealthMetadataEntity = client.GetResourceHealthMetadataEntityResource(resourceHealthMetadataEntityResourceId);
 
 // invoke the operation
-string name = "status";
-NullableResponse<ResourceHealthMetadataEntityResource> response = await collection.GetIfExistsAsync(name);
-ResourceHealthMetadataEntityResource result = response.HasValue ? response.Value : null;
+ResourceHealthMetadataEntityResource result = await resourceHealthMetadataEntity.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine($"Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    ResourceHealthMetadataEntityData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ResourceHealthMetadataEntityData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

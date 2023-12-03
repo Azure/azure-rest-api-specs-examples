d12da9a1dcb527715f74a2921ab08b3c52c35c4c
azure-sdk <azuresdk@microsoft.com>
2023-12-03 12:51:45 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ManagementPartner_1.0.0-beta.4

--- specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples-dotnet/GetPartnerDetails.cs
+++ specification/managementpartner/resource-manager/Microsoft.ManagementPartner/preview/2018-02-01/examples-dotnet/GetPartnerDetails.cs
@@ -23,6 +23,18 @@ PartnerResponseCollection collection = tenantResource.GetPartnerResponses();
 
 // invoke the operation
 string partnerId = "123456";
-bool result = await collection.ExistsAsync(partnerId);
+NullableResponse<PartnerResponseResource> response = await collection.GetIfExistsAsync(partnerId);
+PartnerResponseResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    PartnerResponseData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

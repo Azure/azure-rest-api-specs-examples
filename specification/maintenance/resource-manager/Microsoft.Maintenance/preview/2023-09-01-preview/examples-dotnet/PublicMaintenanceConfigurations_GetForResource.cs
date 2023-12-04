bd6e34aa58bf3a91189cf5780b7e6e7b3f9f9f85
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-04 20:03:10 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Maintenance_1.2.0-beta.6
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/maintenance/resource-manager/Microsoft.Maintenance/preview/2023-09-01-preview/examples-dotnet/PublicMaintenanceConfigurations_GetForResource.cs
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/preview/2023-09-01-preview/examples-dotnet/PublicMaintenanceConfigurations_GetForResource.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Maintenance;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/maintenance/resource-manager/Microsoft.Maintenance/preview/2023-09-01-preview/examples/PublicMaintenanceConfigurations_GetForResource.json
 // this example is just showing the usage of "PublicMaintenanceConfigurations_Get" operation, for the dependent resources, they will have to be created separately.
@@ -13,18 +15,29 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this MaintenancePublicConfigurationResource created on azure
-// for more information of creating MaintenancePublicConfigurationResource, please refer to the document of MaintenancePublicConfigurationResource
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
 string subscriptionId = "5b4b650e-28b9-4790-b3ab-ddbd88d727c4";
-string resourceName = "configuration1";
-ResourceIdentifier maintenancePublicConfigurationResourceId = MaintenancePublicConfigurationResource.CreateResourceIdentifier(subscriptionId, resourceName);
-MaintenancePublicConfigurationResource maintenancePublicConfiguration = client.GetMaintenancePublicConfigurationResource(maintenancePublicConfigurationResourceId);
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// get the collection of this MaintenancePublicConfigurationResource
+MaintenancePublicConfigurationCollection collection = subscriptionResource.GetMaintenancePublicConfigurations();
 
 // invoke the operation
-MaintenancePublicConfigurationResource result = await maintenancePublicConfiguration.GetAsync();
+string resourceName = "configuration1";
+NullableResponse<MaintenancePublicConfigurationResource> response = await collection.GetIfExistsAsync(resourceName);
+MaintenancePublicConfigurationResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-MaintenanceConfigurationData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    MaintenanceConfigurationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

5121871d88ef8177c7e715e64dbf924f3f577e6b
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-04 20:04:23 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.NewRelicObservability_1.0.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/newrelic/resource-manager/NewRelic.Observability/stable/2022-07-01/examples-dotnet/Monitors_Get_MaximumSet_Gen.cs
+++ specification/newrelic/resource-manager/NewRelic.Observability/stable/2022-07-01/examples-dotnet/Monitors_Get_MaximumSet_Gen.cs
@@ -29,6 +29,18 @@ NewRelicMonitorResourceCollection collection = resourceGroupResource.GetNewRelic
 
 // invoke the operation
 string monitorName = "cdlymktqw";
-bool result = await collection.ExistsAsync(monitorName);
+NullableResponse<NewRelicMonitorResource> response = await collection.GetIfExistsAsync(monitorName);
+NewRelicMonitorResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    NewRelicMonitorResourceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

4ef9265c0a915f5362d3e7be6006cbef72b5020d
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-04 20:04:09 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Maps_1.1.0-beta.3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/maps/resource-manager/Microsoft.Maps/preview/2021-12-01-preview/examples-dotnet/GetMapsCreator.cs
+++ specification/maps/resource-manager/Microsoft.Maps/preview/2021-12-01-preview/examples-dotnet/GetMapsCreator.cs
@@ -28,6 +28,18 @@ MapsCreatorCollection collection = mapsAccount.GetMapsCreators();
 
 // invoke the operation
 string creatorName = "myCreator";
-bool result = await collection.ExistsAsync(creatorName);
+NullableResponse<MapsCreatorResource> response = await collection.GetIfExistsAsync(creatorName);
+MapsCreatorResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    MapsCreatorData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

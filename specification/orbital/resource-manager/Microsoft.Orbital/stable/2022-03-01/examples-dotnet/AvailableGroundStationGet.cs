54632dea068de956346e141dc46215ca4d50c920
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-05 20:02:20 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Orbital_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/orbital/resource-manager/Microsoft.Orbital/stable/2022-03-01/examples-dotnet/AvailableGroundStationGet.cs
+++ specification/orbital/resource-manager/Microsoft.Orbital/stable/2022-03-01/examples-dotnet/AvailableGroundStationGet.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Orbital;
+using Azure.ResourceManager.Orbital.Models;
+using Azure.ResourceManager.Resources;
 
 // Generated from example definition: specification/orbital/resource-manager/Microsoft.Orbital/stable/2022-03-01/examples/AvailableGroundStationGet.json
 // this example is just showing the usage of "AvailableGroundStations_Get" operation, for the dependent resources, they will have to be created separately.
@@ -13,18 +16,29 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this AvailableGroundStationResource created on azure
-// for more information of creating AvailableGroundStationResource, please refer to the document of AvailableGroundStationResource
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
 string subscriptionId = "c1be1141-a7c9-4aac-9608-3c2e2f1152c3";
-string groundStationName = "EASTUS2_0";
-ResourceIdentifier availableGroundStationResourceId = AvailableGroundStationResource.CreateResourceIdentifier(subscriptionId, groundStationName);
-AvailableGroundStationResource availableGroundStation = client.GetAvailableGroundStationResource(availableGroundStationResourceId);
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// get the collection of this AvailableGroundStationResource
+AvailableGroundStationCollection collection = subscriptionResource.GetAvailableGroundStations();
 
 // invoke the operation
-AvailableGroundStationResource result = await availableGroundStation.GetAsync();
+string groundStationName = "EASTUS2_0";
+NullableResponse<AvailableGroundStationResource> response = await collection.GetIfExistsAsync(groundStationName);
+AvailableGroundStationResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-AvailableGroundStationData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    AvailableGroundStationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

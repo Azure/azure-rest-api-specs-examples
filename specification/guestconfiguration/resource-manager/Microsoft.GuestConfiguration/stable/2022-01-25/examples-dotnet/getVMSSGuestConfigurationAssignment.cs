0a30895636898c37d70e989ec45dd8d91abd8430
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-05 20:01:42 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.GuestConfiguration_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/guestconfiguration/resource-manager/Microsoft.GuestConfiguration/stable/2022-01-25/examples-dotnet/getVMSSGuestConfigurationAssignment.cs
+++ specification/guestconfiguration/resource-manager/Microsoft.GuestConfiguration/stable/2022-01-25/examples-dotnet/getVMSSGuestConfigurationAssignment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -25,6 +26,18 @@ GuestConfigurationVmssAssignmentCollection collection = client.GetGuestConfigura
 
 // invoke the operation
 string name = "SecureProtocol";
-bool result = await collection.ExistsAsync(name);
+NullableResponse<GuestConfigurationVmssAssignmentResource> response = await collection.GetIfExistsAsync(name);
+GuestConfigurationVmssAssignmentResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    GuestConfigurationAssignmentData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

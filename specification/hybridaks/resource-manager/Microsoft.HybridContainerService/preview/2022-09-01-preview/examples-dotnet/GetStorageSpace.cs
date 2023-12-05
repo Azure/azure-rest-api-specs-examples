0828d8d646ee345e930605d545058b7ea8ecd677
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-05 20:01:50 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HybridContainerService_1.0.0-beta.3
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/hybridaks/resource-manager/Microsoft.HybridContainerService/preview/2022-09-01-preview/examples-dotnet/GetStorageSpace.cs
+++ specification/hybridaks/resource-manager/Microsoft.HybridContainerService/preview/2022-09-01-preview/examples-dotnet/GetStorageSpace.cs
@@ -28,6 +28,18 @@ StorageSpaceCollection collection = resourceGroupResource.GetStorageSpaces();
 
 // invoke the operation
 string storageSpacesName = "test-storage";
-bool result = await collection.ExistsAsync(storageSpacesName);
+NullableResponse<StorageSpaceResource> response = await collection.GetIfExistsAsync(storageSpacesName);
+StorageSpaceResource result = response.HasValue ? response.Value : null;
 
-Console.WriteLine($"Succeeded: {result}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    StorageSpaceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

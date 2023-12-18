02041293f26b7feeb2c3d76f08264b19ebb39b95
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-18 20:01:20 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-dataprotection_1.3.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples-python/PolicyCRUD/GetBackupPolicy.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.dataprotection import DataProtectionMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dataprotection
+# USAGE
+    python get_backup_policy.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataProtectionMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="04cf684a-d41f-4550-9f70-7708a3a2283b",
+    )
+
+    response = client.backup_policies.get(
+        resource_group_name="000pikumar",
+        vault_name="PrivatePreviewVault",
+        backup_policy_name="OSSDBPolicy",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples/PolicyCRUD/GetBackupPolicy.json
+if __name__ == "__main__":
+    main()

e33007995617e4d63def77fd26d77442d3a2481a
azure-sdk <azuresdk@microsoft.com>
2023-12-18 05:13:24 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-dataprotection_1.3.0

---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples-python/CheckfeatureSupport.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.dataprotection import DataProtectionMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dataprotection
+# USAGE
+    python checkfeature_support.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataProtectionMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="0b352192-dcac-4cc7-992e-a96190ccc68c",
+    )
+
+    response = client.data_protection.check_feature_support(
+        location="WestUS",
+        parameters={"featureType": "DataSourceType", "objectType": "FeatureValidationRequest"},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples/CheckfeatureSupport.json
+if __name__ == "__main__":
+    main()
---
+++ specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples-python/VaultCRUD/PutBackupVaultWithMSI.py
@@ -0,0 +1,44 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.dataprotection import DataProtectionMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dataprotection
+# USAGE
+    python put_backup_vault_with_msi.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataProtectionMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="0b352192-dcac-4cc7-992e-a96190ccc68c",
+    )
+
+    response = client.backup_vaults.begin_create_or_update(
+        resource_group_name="SampleResourceGroup",
+        vault_name="swaggerExample",
+        parameters={
+            "identity": {"type": "systemAssigned"},
+            "location": "WestUS",
+            "properties": {
+                "featureSettings": {"crossRegionRestoreSettings": {"state": "Enabled"}},
+                "monitoringSettings": {"azureMonitorAlertSettings": {"alertsForAllJobFailures": "Enabled"}},
+                "securitySettings": {"softDeleteSettings": {"retentionDurationInDays": 14, "state": "Enabled"}},
+                "storageSettings": [{"datastoreType": "VaultStore", "type": "LocallyRedundant"}],
+            },
+            "tags": {"key1": "val1"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples/VaultCRUD/PutBackupVaultWithMSI.json
+if __name__ == "__main__":
+    main()

e0954c94a0e60f691aa97b943612ffadac810bd7
azure-sdk <azuresdk@microsoft.com>
2023-12-18 12:14:06 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_30.4.0

---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples-python/proximityPlacementGroupExamples/ProximityPlacementGroup_Delete.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python proximity_placement_group_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    client.proximity_placement_groups.delete(
+        resource_group_name="myResourceGroup",
+        proximity_placement_group_name="myProximityPlacementGroup",
+    )
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples/proximityPlacementGroupExamples/ProximityPlacementGroup_Delete.json
+if __name__ == "__main__":
+    main()
---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples-python/sshPublicKeyExamples/SshPublicKey_Update_MaximumSet_Gen.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python ssh_public_key_update_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.ssh_public_keys.update(
+        resource_group_name="rgcompute",
+        ssh_public_key_name="aaaaaaaaaaaa",
+        parameters={"properties": {"publicKey": "{ssh-rsa public key}"}, "tags": {"key2854": "a"}},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples/sshPublicKeyExamples/SshPublicKey_Update_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

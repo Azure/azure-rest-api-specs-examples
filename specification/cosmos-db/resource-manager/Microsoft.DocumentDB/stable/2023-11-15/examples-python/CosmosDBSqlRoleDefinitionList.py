52b5882fbb6d0fbb9056b4612eb916ee0fac6425
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-20 20:01:33 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_9.4.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples-python/CosmosDBSqlRoleDefinitionList.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_sql_role_definition_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="mySubscriptionId",
+    )
+
+    response = client.sql_resources.list_sql_role_definitions(
+        resource_group_name="myResourceGroupName",
+        account_name="myAccountName",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples/CosmosDBSqlRoleDefinitionList.json
+if __name__ == "__main__":
+    main()

e5553489598ebe7bb8038e77016c88a2caaeb902
azure-sdk <azuresdk@microsoft.com>
2023-12-21 12:21:23 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_19.6.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-10-01/examples-js/MaintenanceConfigurationsGet.js
@@ -0,0 +1,24 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the specified maintenance configuration of a managed cluster.
+ *
+ * @summary Gets the specified maintenance configuration of a managed cluster.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-10-01/examples/MaintenanceConfigurationsGet.json
+ */
+async function getMaintenanceConfiguration() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const configName = "default";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.maintenanceConfigurations.get(
+    resourceGroupName,
+    resourceName,
+    configName,
+  );
+  console.log(result);
+}

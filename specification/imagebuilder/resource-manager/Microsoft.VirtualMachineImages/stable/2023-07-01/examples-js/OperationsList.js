b018221109e356326e130a049708dc44b9039569
azure-sdk <azuresdk@microsoft.com>
2023-12-21 12:22:32 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-imagebuilder_4.0.0

---
+++ specification/imagebuilder/resource-manager/Microsoft.VirtualMachineImages/stable/2023-07-01/examples-js/OperationsList.js
@@ -0,0 +1,20 @@
+const { ImageBuilderClient } = require("@azure/arm-imagebuilder");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists available operations for the Microsoft.VirtualMachineImages provider
+ *
+ * @summary Lists available operations for the Microsoft.VirtualMachineImages provider
+ * x-ms-original-file: specification/imagebuilder/resource-manager/Microsoft.VirtualMachineImages/stable/2023-07-01/examples/OperationsList.json
+ */
+async function retrieveOperationsList() {
+  const subscriptionId =
+    process.env["IMAGEBUILDER_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new ImageBuilderClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.operations.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

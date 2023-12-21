d914ac8730b8976e6ee0b49866d502fea7078501
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-21 20:04:24 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-communication_4.1.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2023-06-01-preview/examples-js/emailServices/get.js
@@ -0,0 +1,19 @@
+const { CommunicationServiceManagementClient } = require("@azure/arm-communication");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get the EmailService and its properties.
+ *
+ * @summary Get the EmailService and its properties.
+ * x-ms-original-file: specification/communication/resource-manager/Microsoft.Communication/preview/2023-06-01-preview/examples/emailServices/get.json
+ */
+async function getEmailServiceResource() {
+  const subscriptionId =
+    process.env["COMMUNICATION_SUBSCRIPTION_ID"] || "11112222-3333-4444-5555-666677778888";
+  const resourceGroupName = process.env["COMMUNICATION_RESOURCE_GROUP"] || "MyResourceGroup";
+  const emailServiceName = "MyEmailServiceResource";
+  const credential = new DefaultAzureCredential();
+  const client = new CommunicationServiceManagementClient(credential, subscriptionId);
+  const result = await client.emailServices.get(resourceGroupName, emailServiceName);
+  console.log(result);
+}
---
+++ specification/communication/resource-manager/Microsoft.Communication/preview/2023-06-01-preview/examples-js/senderUsernames/listByDomain.js
@@ -0,0 +1,27 @@
+const { CommunicationServiceManagementClient } = require("@azure/arm-communication");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List all valid sender usernames for a domains resource.
+ *
+ * @summary List all valid sender usernames for a domains resource.
+ * x-ms-original-file: specification/communication/resource-manager/Microsoft.Communication/preview/2023-06-01-preview/examples/senderUsernames/listByDomain.json
+ */
+async function getAllSenderUsernamesResourcesForADomain() {
+  const subscriptionId =
+    process.env["COMMUNICATION_SUBSCRIPTION_ID"] || "11112222-3333-4444-5555-666677778888";
+  const resourceGroupName = process.env["COMMUNICATION_RESOURCE_GROUP"] || "contosoResourceGroup";
+  const emailServiceName = "contosoEmailService";
+  const domainName = "contoso.com";
+  const credential = new DefaultAzureCredential();
+  const client = new CommunicationServiceManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.senderUsernames.listByDomains(
+    resourceGroupName,
+    emailServiceName,
+    domainName
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

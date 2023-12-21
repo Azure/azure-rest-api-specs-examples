7358f6761a9ad8f097813c3a86b8479f37b72515
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2023-12-21 20:03:49 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-healthcareapis_2.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-11-01/examples-python/legacy/ServiceGet.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.healthcareapis import HealthcareApisManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-healthcareapis
+# USAGE
+    python service_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HealthcareApisManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.services.get(
+        resource_group_name="rg1",
+        resource_name="service1",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-11-01/examples/legacy/ServiceGet.json
+if __name__ == "__main__":
+    main()

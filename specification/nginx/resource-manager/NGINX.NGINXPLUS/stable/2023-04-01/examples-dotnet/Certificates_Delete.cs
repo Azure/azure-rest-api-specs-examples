6364a138ca609fe643e17ad187bf8de035c00be1
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-01-04 20:02:54 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Nginx_1.0.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2023-04-01/examples-dotnet/Certificates_Delete.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Nginx;
+
+// Generated from example definition: specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2023-04-01/examples/Certificates_Delete.json
+// this example is just showing the usage of "Certificates_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this NginxCertificateResource created on azure
+// for more information of creating NginxCertificateResource, please refer to the document of NginxCertificateResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string deploymentName = "myDeployment";
+string certificateName = "default";
+ResourceIdentifier nginxCertificateResourceId = NginxCertificateResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, deploymentName, certificateName);
+NginxCertificateResource nginxCertificate = client.GetNginxCertificateResource(nginxCertificateResourceId);
+
+// invoke the operation
+await nginxCertificate.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

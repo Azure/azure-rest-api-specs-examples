f382f6abeabc9ea6243fd552f5bbab44137a609e
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-01-18 13:02:01 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HybridContainerService_1.0.0-beta.5
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples-dotnet/ProvisionedClusterInstanceListAdminKubeconfig.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.HybridContainerService;
+using Azure.ResourceManager.HybridContainerService.Models;
+
+// Generated from example definition: specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples/ProvisionedClusterInstanceListAdminKubeconfig.json
+// this example is just showing the usage of "provisionedClusterInstances_ListAdminKubeconfig" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ProvisionedClusterResource created on azure
+// for more information of creating ProvisionedClusterResource, please refer to the document of ProvisionedClusterResource
+string connectedClusterResourceUri = "subscriptions/fd3c3665-1729-4b7b-9a38-238e83b0f98b/resourceGroups/testrg/providers/Microsoft.Kubernetes/connectedClusters/test-hybridakscluster";
+ResourceIdentifier provisionedClusterResourceId = ProvisionedClusterResource.CreateResourceIdentifier(connectedClusterResourceUri);
+ProvisionedClusterResource provisionedCluster = client.GetProvisionedClusterResource(provisionedClusterResourceId);
+
+// invoke the operation
+ArmOperation<HybridContainerServiceCredentialListResult> lro = await provisionedCluster.GetAdminKubeconfigAsync(WaitUntil.Completed);
+HybridContainerServiceCredentialListResult result = lro.Value;
+
+Console.WriteLine($"Succeeded: {result}");

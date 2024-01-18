0979ced8394e3c8209433ce1dfac583d78809c43
azure-sdk <azuresdk@microsoft.com>
2024-01-18 04:51:23 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HybridContainerService_1.0.0-beta.5

---
+++ specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples-dotnet/GetHybridIdentityMetadata.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.HybridContainerService;
+
+// Generated from example definition: specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples/GetHybridIdentityMetadata.json
+// this example is just showing the usage of "HybridIdentityMetadata_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this HybridIdentityMetadataResource created on azure
+// for more information of creating HybridIdentityMetadataResource, please refer to the document of HybridIdentityMetadataResource
+string connectedClusterResourceUri = "subscriptions/fd3c3665-1729-4b7b-9a38-238e83b0f98b/resourceGroups/testrg/providers/Microsoft.Kubernetes/connectedClusters/test-hybridakscluster";
+ResourceIdentifier hybridIdentityMetadataResourceId = HybridIdentityMetadataResource.CreateResourceIdentifier(connectedClusterResourceUri);
+HybridIdentityMetadataResource hybridIdentityMetadata = client.GetHybridIdentityMetadataResource(hybridIdentityMetadataResourceId);
+
+// invoke the operation
+HybridIdentityMetadataResource result = await hybridIdentityMetadata.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+HybridIdentityMetadataData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

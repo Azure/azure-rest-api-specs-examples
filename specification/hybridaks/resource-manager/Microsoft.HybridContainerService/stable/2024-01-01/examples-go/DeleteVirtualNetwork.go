705df40ecdc2959352e11d4953b73cd842a5643c
azure-sdk <azuresdk@microsoft.com>
2024-01-24 12:12:33 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/hybridcontainerservice/armhybridcontainerservice/v1.0.0

---
+++ specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples-go/DeleteVirtualNetwork.go
@@ -0,0 +1,30 @@
+package armhybridcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridcontainerservice/armhybridcontainerservice"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/41e4538ed7bb3ceac3c1322c9455a0812ed110ac/specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples/DeleteVirtualNetwork.json
+func ExampleVirtualNetworksClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armhybridcontainerservice.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewVirtualNetworksClient().BeginDelete(ctx, "test-arcappliance-resgrp", "test-vnet-static", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

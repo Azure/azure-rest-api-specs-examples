67d8ec2990489823e56a6d3c837aad23d6b8b97d
azure-sdk <azuresdk@microsoft.com>
2024-01-26 12:33:19 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-appplatform_9.0.0

---
+++ specification/appplatform/resource-manager/Microsoft.AppPlatform/stable/2023-12-01/examples-python/Apps_GetResourceUploadUrl.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.appplatform import AppPlatformManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appplatform
+# USAGE
+    python apps_get_resource_upload_url.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AppPlatformManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.apps.get_resource_upload_url(
+        resource_group_name="myResourceGroup",
+        service_name="myservice",
+        app_name="myapp",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/appplatform/resource-manager/Microsoft.AppPlatform/stable/2023-12-01/examples/Apps_GetResourceUploadUrl.json
+if __name__ == "__main__":
+    main()
---
+++ specification/appplatform/resource-manager/Microsoft.AppPlatform/stable/2023-12-01/examples-python/Apps_ValidateDomain.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.appplatform import AppPlatformManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appplatform
+# USAGE
+    python apps_validate_domain.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AppPlatformManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.apps.validate_domain(
+        resource_group_name="myResourceGroup",
+        service_name="myservice",
+        app_name="myapp",
+        validate_payload={"name": "mydomain.io"},
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/appplatform/resource-manager/Microsoft.AppPlatform/stable/2023-12-01/examples/Apps_ValidateDomain.json
+if __name__ == "__main__":
+    main()

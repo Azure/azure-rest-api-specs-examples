cc01bdc177b7cdeeccb578dcb219d6695d41d10c
azure-sdk <azuresdk@microsoft.com>
2024-01-26 12:21:05 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_15.6.0

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples-js/CosmosDBDatabaseAccountGetUsages.js
@@ -0,0 +1,27 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Retrieves the usages (most recent data) for the given database account.
+ *
+ * @summary Retrieves the usages (most recent data) for the given database account.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples/CosmosDBDatabaseAccountGetUsages.json
+ */
+async function cosmosDbDatabaseAccountGetUsages() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subid";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rg1";
+  const accountName = "ddb1";
+  const filter = "$filter=name.value eq 'Storage'";
+  const options = { filter };
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.databaseAccounts.listUsages(
+    resourceGroupName,
+    accountName,
+    options,
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples-js/CosmosDBLocationGet.js
@@ -0,0 +1,18 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Get the properties of an existing Cosmos DB location
+ *
+ * @summary Get the properties of an existing Cosmos DB location
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples/CosmosDBLocationGet.json
+ */
+async function cosmosDbLocationGet() {
+  const subscriptionId =
+    process.env["COSMOSDB_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const location = "westus";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const result = await client.locations.get(location);
+  console.log(result);
+}

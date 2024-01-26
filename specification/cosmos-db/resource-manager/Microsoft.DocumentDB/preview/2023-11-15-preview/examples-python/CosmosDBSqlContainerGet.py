aa9aaa38f4480ada3f909fcf46ae8fd54d5c6150
azure-sdk <azuresdk@microsoft.com>
2024-01-26 12:35:17 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_10.0.0b2

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-11-15-preview/examples-python/CosmosDBSqlContainerGet.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_sql_container_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.sql_resources.get_sql_container(
+        resource_group_name="rgName",
+        account_name="ddb1",
+        database_name="databaseName",
+        container_name="containerName",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-11-15-preview/examples/CosmosDBSqlContainerGet.json
+if __name__ == "__main__":
+    main()
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-11-15-preview/examples-python/CosmosDBSqlStoredProcedureCreateUpdate.py
@@ -0,0 +1,39 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_sql_stored_procedure_create_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.sql_resources.begin_create_update_sql_stored_procedure(
+        resource_group_name="rg1",
+        account_name="ddb1",
+        database_name="databaseName",
+        container_name="containerName",
+        stored_procedure_name="storedProcedureName",
+        create_update_sql_stored_procedure_parameters={
+            "properties": {"options": {}, "resource": {"body": "body", "id": "storedProcedureName"}}
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2023-11-15-preview/examples/CosmosDBSqlStoredProcedureCreateUpdate.json
+if __name__ == "__main__":
+    main()

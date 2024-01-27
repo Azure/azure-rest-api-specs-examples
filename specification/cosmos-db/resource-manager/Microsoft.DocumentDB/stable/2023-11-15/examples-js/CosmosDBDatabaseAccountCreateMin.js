903107597e28fb819e53db7bd14a0f153ef048ff
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-01-27 20:02:45 +0800

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_15.6.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples-js/CosmosDBDatabaseAccountCreateMin.js
@@ -0,0 +1,34 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates or updates an Azure Cosmos DB database account. The "Update" method is preferred when performing updates on an account.
+ *
+ * @summary Creates or updates an Azure Cosmos DB database account. The "Update" method is preferred when performing updates on an account.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples/CosmosDBDatabaseAccountCreateMin.json
+ */
+async function cosmosDbDatabaseAccountCreateMin() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subid";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rg1";
+  const accountName = "ddb1";
+  const createUpdateParameters = {
+    createMode: "Default",
+    databaseAccountOfferType: "Standard",
+    location: "westus",
+    locations: [
+      {
+        failoverPriority: 0,
+        isZoneRedundant: false,
+        locationName: "southcentralus",
+      },
+    ],
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const result = await client.databaseAccounts.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    accountName,
+    createUpdateParameters,
+  );
+  console.log(result);
+}

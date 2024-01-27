fa8a1a315dfb834e619ce2dba7799c17eec83b8a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-01-27 20:02:24 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.LargeInstance_1.0.0-beta.1
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/azurelargeinstance/resource-manager/Microsoft.AzureLargeInstance/preview/2023-07-20-preview/examples-dotnet/AzureLargeInstance_Get.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.LargeInstance;
+using Azure.ResourceManager.LargeInstance.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/azurelargeinstance/resource-manager/Microsoft.AzureLargeInstance/preview/2023-07-20-preview/examples/AzureLargeInstance_Get.json
+// this example is just showing the usage of "AzureLargeInstance_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this LargeInstanceResource created on azure
+// for more information of creating LargeInstanceResource, please refer to the document of LargeInstanceResource
+string subscriptionId = "f0f4887f-d13c-4943-a8ba-d7da28d2a3fd";
+string resourceGroupName = "myResourceGroup";
+string azureLargeInstanceName = "myAzureLargeInstance";
+ResourceIdentifier largeInstanceResourceId = LargeInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, azureLargeInstanceName);
+LargeInstanceResource largeInstance = client.GetLargeInstanceResource(largeInstanceResourceId);
+
+// invoke the operation
+LargeInstanceResource result = await largeInstance.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+LargeInstanceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/azurelargeinstance/resource-manager/Microsoft.AzureLargeInstance/preview/2023-07-20-preview/examples-dotnet/AzureLargeStorageInstance_PatchTags_Delete.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.LargeInstance;
+using Azure.ResourceManager.LargeInstance.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/azurelargeinstance/resource-manager/Microsoft.AzureLargeInstance/preview/2023-07-20-preview/examples/AzureLargeStorageInstance_PatchTags_Delete.json
+// this example is just showing the usage of "AzureLargeStorageInstance_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this LargeStorageInstanceResource created on azure
+// for more information of creating LargeStorageInstanceResource, please refer to the document of LargeStorageInstanceResource
+string subscriptionId = "f0f4887f-d13c-4943-a8ba-d7da28d2a3fd";
+string resourceGroupName = "myResourceGroup";
+string azureLargeStorageInstanceName = "myALSInstance";
+ResourceIdentifier largeStorageInstanceResourceId = LargeStorageInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, azureLargeStorageInstanceName);
+LargeStorageInstanceResource largeStorageInstance = client.GetLargeStorageInstanceResource(largeStorageInstanceResourceId);
+
+// invoke the operation
+LargeStorageInstancePatch patch = new LargeStorageInstancePatch();
+LargeStorageInstanceResource result = await largeStorageInstance.UpdateAsync(patch);
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+LargeStorageInstanceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

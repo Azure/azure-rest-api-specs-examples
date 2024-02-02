961dbc17def3217668d8dedacb649cf0210b3e6a
azure-sdk <azuresdk@microsoft.com>
2024-02-02 12:08:27 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StorageCache_1.3.0-beta.1

---
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/preview/2023-11-01-preview/examples-dotnet/AscResourceUsages_Get.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.StorageCache;
+using Azure.ResourceManager.StorageCache.Models;
+
+// Generated from example definition: specification/storagecache/resource-manager/Microsoft.StorageCache/preview/2023-11-01-preview/examples/AscResourceUsages_Get.json
+// this example is just showing the usage of "AscUsages_List" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation and iterate over the result
+AzureLocation location = new AzureLocation("eastus");
+await foreach (StorageCacheUsage item in subscriptionResource.GetStorageCacheUsagesAsync(location))
+{
+    Console.WriteLine($"Succeeded: {item}");
+}
+
+Console.WriteLine($"Succeeded");
---
+++ specification/storagecache/resource-manager/Microsoft.StorageCache/preview/2023-11-01-preview/examples-dotnet/amlFilesystems_Delete.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.StorageCache;
+using Azure.ResourceManager.StorageCache.Models;
+
+// Generated from example definition: specification/storagecache/resource-manager/Microsoft.StorageCache/preview/2023-11-01-preview/examples/amlFilesystems_Delete.json
+// this example is just showing the usage of "amlFilesystems_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this AmlFileSystemResource created on azure
+// for more information of creating AmlFileSystemResource, please refer to the document of AmlFileSystemResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "scgroup";
+string amlFileSystemName = "fs1";
+ResourceIdentifier amlFileSystemResourceId = AmlFileSystemResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, amlFileSystemName);
+AmlFileSystemResource amlFileSystem = client.GetAmlFileSystemResource(amlFileSystemResourceId);
+
+// invoke the operation
+await amlFileSystem.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

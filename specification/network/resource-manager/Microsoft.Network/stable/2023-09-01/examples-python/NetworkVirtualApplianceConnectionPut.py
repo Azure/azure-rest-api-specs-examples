f0402b978f3d7683926351a4179ed8ddcac07cfa
azure-sdk <azuresdk@microsoft.com>
2024-02-22 12:18:19 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-network_25.3.0

---
+++ specification/network/resource-manager/Microsoft.Network/stable/2023-09-01/examples-python/NetworkVirtualApplianceConnectionPut.py
@@ -0,0 +1,62 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.network import NetworkManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-network
+# USAGE
+    python network_virtual_appliance_connection_put.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = NetworkManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.network_virtual_appliance_connections.begin_create_or_update(
+        resource_group_name="rg1",
+        network_virtual_appliance_name="nva1",
+        connection_name="connection1",
+        network_virtual_appliance_connection_parameters={
+            "properties": {
+                "asn": 64512,
+                "bgpPeerAddress": ["169.254.16.13", "169.254.16.14"],
+                "enableInternetSecurity": False,
+                "name": "connection1",
+                "routingConfiguration": {
+                    "associatedRouteTable": {
+                        "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualHubs/virtualHub1/hubRouteTables/hubRouteTable1"
+                    },
+                    "inboundRouteMap": {
+                        "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualHubs/virtualHub1/routeMaps/routeMap1"
+                    },
+                    "outboundRouteMap": {
+                        "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualHubs/virtualHub1/routeMaps/routeMap2"
+                    },
+                    "propagatedRouteTables": {
+                        "ids": [
+                            {
+                                "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualHubs/virtualHub1/hubRouteTables/hubRouteTable1"
+                            }
+                        ],
+                        "labels": ["label1"],
+                    },
+                },
+                "tunnelIdentifier": 0,
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2023-09-01/examples/NetworkVirtualApplianceConnectionPut.json
+if __name__ == "__main__":
+    main()

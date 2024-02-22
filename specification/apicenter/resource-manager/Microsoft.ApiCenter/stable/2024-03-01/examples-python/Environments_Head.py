904b868167e4b1bb244e9cb4c16c15bb7c43d6f2
azure-sdk <azuresdk@microsoft.com>
2024-02-22 12:13:00 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-apicenter_1.0.0

---
+++ specification/apicenter/resource-manager/Microsoft.ApiCenter/stable/2024-03-01/examples-python/Environments_Head.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.apicenter import ApiCenterMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-apicenter
+# USAGE
+    python environments_head.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ApiCenterMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.environments.head(
+        resource_group_name="contoso-resources",
+        service_name="contoso",
+        workspace_name="default",
+        environment_name="public",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/apicenter/resource-manager/Microsoft.ApiCenter/stable/2024-03-01/examples/Environments_Head.json
+if __name__ == "__main__":
+    main()

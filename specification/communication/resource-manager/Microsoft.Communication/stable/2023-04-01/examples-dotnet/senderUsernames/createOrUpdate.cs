f78f54e4ed2d82289ff0d289584ea8d3db88aae5
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-02-23 20:02:17 +0800

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Communication_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/communication/resource-manager/Microsoft.Communication/stable/2023-04-01/examples-dotnet/senderUsernames/createOrUpdate.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Communication;
+
+// Generated from example definition: specification/communication/resource-manager/Microsoft.Communication/stable/2023-04-01/examples/senderUsernames/createOrUpdate.json
+// this example is just showing the usage of "SenderUsernames_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this CommunicationDomainResource created on azure
+// for more information of creating CommunicationDomainResource, please refer to the document of CommunicationDomainResource
+string subscriptionId = "11112222-3333-4444-5555-666677778888";
+string resourceGroupName = "contosoResourceGroup";
+string emailServiceName = "contosoEmailService";
+string domainName = "contoso.com";
+ResourceIdentifier communicationDomainResourceId = CommunicationDomainResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, emailServiceName, domainName);
+CommunicationDomainResource communicationDomainResource = client.GetCommunicationDomainResource(communicationDomainResourceId);
+
+// get the collection of this SenderUsernameResource
+SenderUsernameResourceCollection collection = communicationDomainResource.GetSenderUsernameResources();
+
+// invoke the operation
+string senderUsername = "contosoNewsAlerts";
+SenderUsernameResourceData data = new SenderUsernameResourceData()
+{
+    Username = "contosoNewsAlerts",
+    DisplayName = "Contoso News Alerts",
+};
+ArmOperation<SenderUsernameResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, senderUsername, data);
+SenderUsernameResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+SenderUsernameResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

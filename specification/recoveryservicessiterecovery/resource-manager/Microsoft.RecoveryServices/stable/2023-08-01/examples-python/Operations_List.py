ed87ba137541963ca95f0206fc4c7de50a577429
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-02-23 20:01:49 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-recoveryservicessiterecovery_1.2.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/recoveryservicessiterecovery/resource-manager/Microsoft.RecoveryServices/stable/2023-08-01/examples-python/Operations_List.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.recoveryservicessiterecovery import SiteRecoveryManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-recoveryservicessiterecovery
+# USAGE
+    python operations_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SiteRecoveryManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="c183865e-6077-46f2-a3b1-deb0f4f4650a",
+        resource_group_name="resourceGroupPS1",
+        resource_name="RESOURCE_NAME",
+    )
+
+    response = client.operations.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/recoveryservicessiterecovery/resource-manager/Microsoft.RecoveryServices/stable/2023-08-01/examples/Operations_List.json
+if __name__ == "__main__":
+    main()

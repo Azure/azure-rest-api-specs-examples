0c81a01531a1bd421257a384b70b6258fd59233a
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-02-28 20:02:07 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/astro/armastro/v0.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/liftrastronomer/resource-manager/Astronomer.Astro/stable/2023-08-01/examples-go/Organizations_CreateOrUpdate_MaximumSet_Gen.go
@@ -0,0 +1,136 @@
+package armastro_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/astro/armastro"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d4205894880b989ede35d62d97c8e901ed14fb5a/specification/liftrastronomer/resource-manager/Astronomer.Astro/stable/2023-08-01/examples/Organizations_CreateOrUpdate_MaximumSet_Gen.json
+func ExampleOrganizationsClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armastro.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewOrganizationsClient().BeginCreateOrUpdate(ctx, "rgastronomer", "U.1-:7", armastro.OrganizationResource{
+		Location: to.Ptr("mhqthlsatwvqkl"),
+		Tags:     map[string]*string{},
+		Identity: &armastro.ManagedServiceIdentity{
+			Type:                   to.Ptr(armastro.ManagedServiceIdentityTypeNone),
+			UserAssignedIdentities: map[string]*armastro.UserAssignedIdentity{},
+		},
+		Properties: &armastro.LiftrBaseDataOrganizationProperties{
+			Marketplace: &armastro.LiftrBaseMarketplaceDetails{
+				OfferDetails: &armastro.LiftrBaseOfferDetails{
+					OfferID:     to.Ptr("krzkefmpxztqyusidzgpchfaswuyce"),
+					PlanID:      to.Ptr("kndxzygsanuiqzwbfbbvoipv"),
+					PlanName:    to.Ptr("pwqjwlq"),
+					PublisherID: to.Ptr("gfsqxygpnerxmvols"),
+					TermID:      to.Ptr("pwds"),
+					TermUnit:    to.Ptr("xyygyzcazkuelz"),
+				},
+				SubscriptionID:     to.Ptr("ntthclydlpqmasr"),
+				SubscriptionStatus: to.Ptr(armastro.MarketplaceSubscriptionStatusPendingFulfillmentStart),
+			},
+			PartnerOrganizationProperties: &armastro.LiftrBaseDataPartnerOrganizationProperties{
+				OrganizationID:   to.Ptr("lskgzdmziusgrsucv"),
+				OrganizationName: to.Ptr("3-"),
+				SingleSignOnProperties: &armastro.LiftrBaseSingleSignOnProperties{
+					AADDomains: []*string{
+						to.Ptr("kfbleh")},
+					EnterpriseAppID:   to.Ptr("mklfypyujwumgwdzae"),
+					ProvisioningState: to.Ptr(armastro.ResourceProvisioningStateSucceeded),
+					SingleSignOnState: to.Ptr(armastro.SingleSignOnStatesInitial),
+					SingleSignOnURL:   to.Ptr("ymmtzkyghvinvhgnqlzwrr"),
+				},
+				WorkspaceID:   to.Ptr("vcrupxwpaba"),
+				WorkspaceName: to.Ptr("9.:06"),
+			},
+			ProvisioningState: to.Ptr(armastro.ResourceProvisioningStateSucceeded),
+			User: &armastro.LiftrBaseUserDetails{
+				EmailAddress: to.Ptr(".K_@e7N-g1.xjqnbPs"),
+				FirstName:    to.Ptr("nfh"),
+				LastName:     to.Ptr("lazfbstcccykibvcrxpmglqam"),
+				PhoneNumber:  to.Ptr("inxkscllh"),
+				Upn:          to.Ptr("xtutvycpxjrtoftx"),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.OrganizationResource = armastro.OrganizationResource{
+	// 	Name: to.Ptr("ycyrfvupthkudm"),
+	// 	Type: to.Ptr("ldwwclcpqssjomo"),
+	// 	ID: to.Ptr("bhslekyvgkfomahtvjiin"),
+	// 	SystemData: &armastro.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-25T11:16:12.868Z"); return t}()),
+	// 		CreatedBy: to.Ptr("zw"),
+	// 		CreatedByType: to.Ptr(armastro.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-25T11:16:12.868Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("isirkhwcppaqoqzoebybzikzbzkjzf"),
+	// 		LastModifiedByType: to.Ptr(armastro.CreatedByTypeUser),
+	// 	},
+	// 	Location: to.Ptr("mhqthlsatwvqkl"),
+	// 	Tags: map[string]*string{
+	// 	},
+	// 	Identity: &armastro.ManagedServiceIdentity{
+	// 		Type: to.Ptr(armastro.ManagedServiceIdentityTypeNone),
+	// 		PrincipalID: to.Ptr("b5684bd7-7958-4c0e-9795-d686c31746d2"),
+	// 		TenantID: to.Ptr("b5684bd7-7958-4c0e-9795-d686c31746d2"),
+	// 		UserAssignedIdentities: map[string]*armastro.UserAssignedIdentity{
+	// 		},
+	// 	},
+	// 	Properties: &armastro.LiftrBaseDataOrganizationProperties{
+	// 		Marketplace: &armastro.LiftrBaseMarketplaceDetails{
+	// 			OfferDetails: &armastro.LiftrBaseOfferDetails{
+	// 				OfferID: to.Ptr("krzkefmpxztqyusidzgpchfaswuyce"),
+	// 				PlanID: to.Ptr("kndxzygsanuiqzwbfbbvoipv"),
+	// 				PlanName: to.Ptr("pwqjwlq"),
+	// 				PublisherID: to.Ptr("gfsqxygpnerxmvols"),
+	// 				TermID: to.Ptr("pwds"),
+	// 				TermUnit: to.Ptr("xyygyzcazkuelz"),
+	// 			},
+	// 			SubscriptionID: to.Ptr("ntthclydlpqmasr"),
+	// 			SubscriptionStatus: to.Ptr(armastro.MarketplaceSubscriptionStatusPendingFulfillmentStart),
+	// 		},
+	// 		PartnerOrganizationProperties: &armastro.LiftrBaseDataPartnerOrganizationProperties{
+	// 			OrganizationID: to.Ptr("lskgzdmziusgrsucv"),
+	// 			OrganizationName: to.Ptr("3-"),
+	// 			SingleSignOnProperties: &armastro.LiftrBaseSingleSignOnProperties{
+	// 				AADDomains: []*string{
+	// 					to.Ptr("kfbleh")},
+	// 					EnterpriseAppID: to.Ptr("mklfypyujwumgwdzae"),
+	// 					ProvisioningState: to.Ptr(armastro.ResourceProvisioningStateSucceeded),
+	// 					SingleSignOnState: to.Ptr(armastro.SingleSignOnStatesInitial),
+	// 					SingleSignOnURL: to.Ptr("ymmtzkyghvinvhgnqlzwrr"),
+	// 				},
+	// 				WorkspaceID: to.Ptr("vcrupxwpaba"),
+	// 				WorkspaceName: to.Ptr("9.:06"),
+	// 			},
+	// 			ProvisioningState: to.Ptr(armastro.ResourceProvisioningStateSucceeded),
+	// 			User: &armastro.LiftrBaseUserDetails{
+	// 				EmailAddress: to.Ptr(".K_@e7N-g1.xjqnbPs"),
+	// 				FirstName: to.Ptr("nfh"),
+	// 				LastName: to.Ptr("lazfbstcccykibvcrxpmglqam"),
+	// 				PhoneNumber: to.Ptr("inxkscllh"),
+	// 				Upn: to.Ptr("xtutvycpxjrtoftx"),
+	// 			},
+	// 		},
+	// 	}
+}
---
+++ specification/liftrastronomer/resource-manager/Astronomer.Astro/stable/2023-08-01/examples-go/Organizations_Delete_MaximumSet_Gen.go
@@ -0,0 +1,30 @@
+package armastro_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/astro/armastro"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d4205894880b989ede35d62d97c8e901ed14fb5a/specification/liftrastronomer/resource-manager/Astronomer.Astro/stable/2023-08-01/examples/Organizations_Delete_MaximumSet_Gen.json
+func ExampleOrganizationsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armastro.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewOrganizationsClient().BeginDelete(ctx, "rgastronomer", "q:", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

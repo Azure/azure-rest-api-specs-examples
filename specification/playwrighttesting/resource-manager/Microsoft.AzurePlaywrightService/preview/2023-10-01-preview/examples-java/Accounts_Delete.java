4cbdb48e0b84ae37ffb3f6a74b5646ba73fe415d
azure-sdk <azuresdk@microsoft.com>
2024-02-28 12:05:48 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-playwrighttesting_1.0.0-beta.2

--- specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples-java/Accounts_Delete.java
+++ specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples-java/Accounts_Delete.java
@@ -1,11 +1,16 @@
-/** Samples for Accounts Delete. */
+
+/**
+ * Samples for Accounts Delete.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples/Accounts_Delete.json
+     * x-ms-original-file:
+     * specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/
+     * examples/Accounts_Delete.json
      */
     /**
      * Sample code: Accounts_Delete.
-     *
+     * 
      * @param manager Entry point to PlaywrightTestingManager.
      */
     public static void accountsDelete(com.azure.resourcemanager.playwrighttesting.PlaywrightTestingManager manager) {

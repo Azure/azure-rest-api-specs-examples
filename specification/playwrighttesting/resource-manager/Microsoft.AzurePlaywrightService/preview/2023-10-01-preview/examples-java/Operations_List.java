75e8c1ace439203a85c49d30794c999db2a92733
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-02-29 20:01:49 +0800

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-playwrighttesting_1.0.0-beta.2
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples-java/Operations_List.java
+++ specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples-java/Operations_List.java
@@ -1,11 +1,16 @@
-/** Samples for Operations List. */
+
+/**
+ * Samples for Operations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples/Operations_List.json
+     * x-ms-original-file:
+     * specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/
+     * examples/Operations_List.json
      */
     /**
      * Sample code: Operations_List.
-     *
+     * 
      * @param manager Entry point to PlaywrightTestingManager.
      */
     public static void operationsList(com.azure.resourcemanager.playwrighttesting.PlaywrightTestingManager manager) {

e39e0f4871ab759bd16f18e8d828541ffa91d4a8
azure-sdk <azuresdk@microsoft.com>
2024-03-04 12:10:30 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-rdbms_10.2.0b15

---
+++ specification/mysql/resource-manager/Microsoft.DBforMySQL/Backups/preview/2023-10-01-preview/examples-python/BackupsListByServer.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.rdbms.mysql_flexibleservers import MySQLManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-rdbms
+# USAGE
+    python backups_list_by_server.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MySQLManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
+    )
+
+    response = client.backups.list_by_server(
+        resource_group_name="TestGroup",
+        server_name="mysqltestserver",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/mysql/resource-manager/Microsoft.DBforMySQL/Backups/preview/2023-10-01-preview/examples/BackupsListByServer.json
+if __name__ == "__main__":
+    main()

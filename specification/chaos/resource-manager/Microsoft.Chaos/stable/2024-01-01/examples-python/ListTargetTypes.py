8b87aebdf3d9c90d62c011f29d460a6b39590500
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-03-05 20:02:19 +0800

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-chaos_1.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/chaos/resource-manager/Microsoft.Chaos/stable/2024-01-01/examples-python/ListTargetTypes.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.chaos import ChaosManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-chaos
+# USAGE
+    python list_target_types.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ChaosManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="6b052e15-03d3-4f17-b2e1-be7f07588291",
+    )
+
+    response = client.target_types.list(
+        location_name="westus2",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/chaos/resource-manager/Microsoft.Chaos/stable/2024-01-01/examples/ListTargetTypes.json
+if __name__ == "__main__":
+    main()

fb0ac0a54b9fd4fd88a6fbd0c8beea1b30679707
azure-sdk <azuresdk@microsoft.com>
2024-03-05 12:07:35 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Batch_1.4.0

---
+++ specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples-dotnet/BatchAccountGet.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Batch;
+using Azure.ResourceManager.Batch.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples/BatchAccountGet.json
+// this example is just showing the usage of "BatchAccount_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this BatchAccountResource created on azure
+// for more information of creating BatchAccountResource, please refer to the document of BatchAccountResource
+string subscriptionId = "subid";
+string resourceGroupName = "default-azurebatch-japaneast";
+string accountName = "sampleacct";
+ResourceIdentifier batchAccountResourceId = BatchAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
+BatchAccountResource batchAccount = client.GetBatchAccountResource(batchAccountResourceId);
+
+// invoke the operation
+BatchAccountResource result = await batchAccount.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+BatchAccountData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples-dotnet/CertificateDelete.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Batch;
+using Azure.ResourceManager.Batch.Models;
+
+// Generated from example definition: specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples/CertificateDelete.json
+// this example is just showing the usage of "Certificate_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this BatchAccountCertificateResource created on azure
+// for more information of creating BatchAccountCertificateResource, please refer to the document of BatchAccountCertificateResource
+string subscriptionId = "subid";
+string resourceGroupName = "default-azurebatch-japaneast";
+string accountName = "sampleacct";
+string certificateName = "sha1-0a0e4f50d51beadeac1d35afc5116098e7902e6e";
+ResourceIdentifier batchAccountCertificateResourceId = BatchAccountCertificateResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, certificateName);
+BatchAccountCertificateResource batchAccountCertificate = client.GetBatchAccountCertificateResource(batchAccountCertificateResourceId);
+
+// invoke the operation
+await batchAccountCertificate.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

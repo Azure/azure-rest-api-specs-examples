90a11e5cd59dbbdb8668abdc5c99930d6c005a63
Azure-Fluent <57352629+Azure-Fluent@users.noreply.github.com>
2024-03-08 20:01:36 +0800

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/storageactions/armstorageactions/v0.1.0
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

---
+++ specification/storageactions/resource-manager/Microsoft.StorageActions/stable/2023-01-01/examples-go/storageTasksCrud/DeleteStorageTask.go
@@ -0,0 +1,30 @@
+package armstorageactions_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storageactions/armstorageactions"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/b43974e07d3204c4b6f8396627f5430994a7f7c9/specification/storageactions/resource-manager/Microsoft.StorageActions/stable/2023-01-01/examples/storageTasksCrud/DeleteStorageTask.json
+func ExampleStorageTasksClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armstorageactions.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewStorageTasksClient().BeginDelete(ctx, "res4228", "mytask1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

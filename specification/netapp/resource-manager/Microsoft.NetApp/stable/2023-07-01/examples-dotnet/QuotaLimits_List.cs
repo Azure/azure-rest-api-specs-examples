0d66eb82efb24913672604004f1bb66bb47a4dc7
azure-sdk <azuresdk@microsoft.com>
2024-03-16 12:09:41 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.NetApp_1.5.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-07-01/examples-dotnet/QuotaLimits_List.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.NetApp;
+using Azure.ResourceManager.NetApp.Models;
+using Azure.ResourceManager.Resources;
+
+// Generated from example definition: specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-07-01/examples/QuotaLimits_List.json
+// this example is just showing the usage of "NetAppResourceQuotaLimits_List" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "D633CC2E-722B-4AE1-B636-BBD9E4C60ED9";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation and iterate over the result
+AzureLocation location = new AzureLocation("eastus");
+await foreach (NetAppSubscriptionQuotaItem item in subscriptionResource.GetNetAppQuotaLimitsAsync(location))
+{
+    Console.WriteLine($"Succeeded: {item}");
+}
+
+Console.WriteLine($"Succeeded");
---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-07-01/examples-dotnet/VolumeGroups_Delete.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.NetApp;
+using Azure.ResourceManager.NetApp.Models;
+
+// Generated from example definition: specification/netapp/resource-manager/Microsoft.NetApp/stable/2023-07-01/examples/VolumeGroups_Delete.json
+// this example is just showing the usage of "VolumeGroups_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this NetAppVolumeGroupResource created on azure
+// for more information of creating NetAppVolumeGroupResource, please refer to the document of NetAppVolumeGroupResource
+string subscriptionId = "D633CC2E-722B-4AE1-B636-BBD9E4C60ED9";
+string resourceGroupName = "myRG";
+string accountName = "account1";
+string volumeGroupName = "group1";
+ResourceIdentifier netAppVolumeGroupResourceId = NetAppVolumeGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, volumeGroupName);
+NetAppVolumeGroupResource netAppVolumeGroup = client.GetNetAppVolumeGroupResource(netAppVolumeGroupResourceId);
+
+// invoke the operation
+await netAppVolumeGroup.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

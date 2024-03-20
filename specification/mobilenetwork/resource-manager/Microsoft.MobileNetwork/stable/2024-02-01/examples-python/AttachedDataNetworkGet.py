d77a2fb91713ca024da1a9d419372129bada2bd4
azure-sdk <azuresdk@microsoft.com>
2024-03-20 01:52:30 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-mobilenetwork_3.2.0

---
+++ specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2024-02-01/examples-python/AttachedDataNetworkGet.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.mobilenetwork import MobileNetworkManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-mobilenetwork
+# USAGE
+    python attached_data_network_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MobileNetworkManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.attached_data_networks.get(
+        resource_group_name="rg1",
+        packet_core_control_plane_name="TestPacketCoreCP",
+        packet_core_data_plane_name="TestPacketCoreDP",
+        attached_data_network_name="TestAttachedDataNetwork",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2024-02-01/examples/AttachedDataNetworkGet.json
+if __name__ == "__main__":
+    main()
---
+++ specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2024-02-01/examples-python/SimGroupListByResourceGroup.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.mobilenetwork import MobileNetworkManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-mobilenetwork
+# USAGE
+    python sim_group_list_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MobileNetworkManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.sim_groups.list_by_resource_group(
+        resource_group_name="rg1",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2024-02-01/examples/SimGroupListByResourceGroup.json
+if __name__ == "__main__":
+    main()

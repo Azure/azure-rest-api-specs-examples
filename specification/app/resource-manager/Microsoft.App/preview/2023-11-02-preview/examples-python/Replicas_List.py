717b9925a793554e64c09d455a2adb81f327c4ac
azure-sdk <azuresdk@microsoft.com>
2024-03-20 01:50:21 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-appcontainers_3.1.0b1

---
+++ specification/app/resource-manager/Microsoft.App/preview/2023-11-02-preview/examples-python/Replicas_List.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appcontainers
+# USAGE
+    python replicas_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerAppsAPIClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="651f8027-33e8-4ec4-97b4-f6e9f3dc8744",
+    )
+
+    response = client.container_apps_revision_replicas.list_replicas(
+        resource_group_name="workerapps-rg-xj",
+        container_app_name="myapp",
+        revision_name="myapp--0wlqy09",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2023-11-02-preview/examples/Replicas_List.json
+if __name__ == "__main__":
+    main()

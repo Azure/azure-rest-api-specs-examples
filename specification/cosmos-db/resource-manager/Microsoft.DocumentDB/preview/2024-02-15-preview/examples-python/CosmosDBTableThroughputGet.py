a89b5c86cd187177c574da3734e90578538114fc
azure-sdk <azuresdk@microsoft.com>
2024-03-20 01:49:38 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdb_10.0.0b3

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-02-15-preview/examples-python/CosmosDBTableThroughputGet.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_table_throughput_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.table_resources.get_table_throughput(
+        resource_group_name="rg1",
+        account_name="ddb1",
+        table_name="tableName",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-02-15-preview/examples/CosmosDBTableThroughputGet.json
+if __name__ == "__main__":
+    main()
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-02-15-preview/examples-python/throughputPool/CosmosDBThroughputPoolCreate.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdb
+# USAGE
+    python cosmos_db_throughput_pool_create.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosDBManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
+    )
+
+    response = client.throughput_pool.begin_create_or_update(
+        resource_group_name="rg1",
+        throughput_pool_name="tp1",
+        body={"location": "westus2", "properties": {"maxThroughput": 10000}, "tags": {}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-02-15-preview/examples/throughputPool/CosmosDBThroughputPoolCreate.json
+if __name__ == "__main__":
+    main()

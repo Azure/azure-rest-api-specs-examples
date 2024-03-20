3935f38f12455688c9a21c66c74ce47afa8b4172
azure-sdk <azuresdk@microsoft.com>
2024-03-20 01:56:45 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-batch_17.3.0

---
+++ specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples-python/ApplicationPackageList.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.batch import BatchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-batch
+# USAGE
+    python application_package_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = BatchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.application_package.list(
+        resource_group_name="default-azurebatch-japaneast",
+        account_name="sampleacct",
+        application_name="app1",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples/ApplicationPackageList.json
+if __name__ == "__main__":
+    main()

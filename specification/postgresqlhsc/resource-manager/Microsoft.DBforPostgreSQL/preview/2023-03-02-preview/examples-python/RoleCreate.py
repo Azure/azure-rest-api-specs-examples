f3c9eecaa30bba5694ceafdacbe3c2b23812fbb0
azure-sdk <azuresdk@microsoft.com>
2024-03-20 01:53:56 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cosmosdbforpostgresql_1.1.0b1

---
+++ specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/preview/2023-03-02-preview/examples-python/RoleCreate.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.cosmosdbforpostgresql import CosmosdbForPostgresqlMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cosmosdbforpostgresql
+# USAGE
+    python role_create.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CosmosdbForPostgresqlMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
+    )
+
+    response = client.roles.begin_create(
+        resource_group_name="TestGroup",
+        cluster_name="pgtestsvc4",
+        role_name="role1",
+        parameters={"properties": {"password": "password"}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/preview/2023-03-02-preview/examples/RoleCreate.json
+if __name__ == "__main__":
+    main()

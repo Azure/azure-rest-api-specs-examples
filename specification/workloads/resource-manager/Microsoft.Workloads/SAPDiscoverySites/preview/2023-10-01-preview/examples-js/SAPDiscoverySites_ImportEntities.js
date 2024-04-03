1e476af0929611b8ed7cf7e04d0204f4e15daa4b
azure-sdk <azuresdk@microsoft.com>
2024-04-03 11:08:38 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-migrationdiscoverysap_1.0.0-beta.1

---
+++ specification/workloads/resource-manager/Microsoft.Workloads/SAPDiscoverySites/preview/2023-10-01-preview/examples-js/SAPDiscoverySites_ImportEntities.js
@@ -0,0 +1,22 @@
+const { WorkloadsClient } = require("@azure/arm-migrationdiscoverysap");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Import your SAP systems' inventory using the [Discovery template](https://go.microsoft.com/fwlink/?linkid=2249111) into your SAP Migration discovery site resource and it's child resources, the SAP instances and Server instances.
+ *
+ * @summary Import your SAP systems' inventory using the [Discovery template](https://go.microsoft.com/fwlink/?linkid=2249111) into your SAP Migration discovery site resource and it's child resources, the SAP instances and Server instances.
+ * x-ms-original-file: specification/workloads/resource-manager/Microsoft.Workloads/SAPDiscoverySites/preview/2023-10-01-preview/examples/SAPDiscoverySites_ImportEntities.json
+ */
+async function importASapMigrationDiscoverySiteResourceAndItChildResources() {
+  const subscriptionId =
+    process.env["MIGRATIONDISCOVERY_SUBSCRIPTION_ID"] || "6d875e77-e412-4d7d-9af4-8895278b4443";
+  const resourceGroupName = process.env["MIGRATIONDISCOVERY_RESOURCE_GROUP"] || "test-rg";
+  const sapDiscoverySiteName = "SampleSite";
+  const credential = new DefaultAzureCredential();
+  const client = new WorkloadsClient(credential, subscriptionId);
+  const result = await client.sapDiscoverySites.beginImportEntitiesAndWait(
+    resourceGroupName,
+    sapDiscoverySiteName,
+  );
+  console.log(result);
+}

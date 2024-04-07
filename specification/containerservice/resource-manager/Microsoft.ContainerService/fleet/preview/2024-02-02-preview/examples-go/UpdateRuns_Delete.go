d79068f2deb5e88364b854aa12c6b86a659a67f6
azure-sdk <azuresdk@microsoft.com>
2024-04-07 11:06:29 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservicefleet/armcontainerservicefleet/v1.2.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-02-02-preview/examples-go/UpdateRuns_Delete.go
@@ -0,0 +1,30 @@
+package armcontainerservicefleet_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicefleet/armcontainerservicefleet"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/92de53a5f1e0e03c94b40475d2135d97148ed014/specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-02-02-preview/examples/UpdateRuns_Delete.json
+func ExampleUpdateRunsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicefleet.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewUpdateRunsClient().BeginDelete(ctx, "rg1", "fleet1", "run1", &armcontainerservicefleet.UpdateRunsClientBeginDeleteOptions{IfMatch: nil})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-02-02-preview/examples-go/UpdateStrategies_CreateOrUpdate.go
@@ -0,0 +1,78 @@
+package armcontainerservicefleet_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicefleet/armcontainerservicefleet"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/92de53a5f1e0e03c94b40475d2135d97148ed014/specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-02-02-preview/examples/UpdateStrategies_CreateOrUpdate.json
+func ExampleFleetUpdateStrategiesClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicefleet.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewFleetUpdateStrategiesClient().BeginCreateOrUpdate(ctx, "rg1", "fleet1", "strartegy1", armcontainerservicefleet.FleetUpdateStrategy{
+		Properties: &armcontainerservicefleet.FleetUpdateStrategyProperties{
+			Strategy: &armcontainerservicefleet.UpdateRunStrategy{
+				Stages: []*armcontainerservicefleet.UpdateStage{
+					{
+						Name:                    to.Ptr("stage1"),
+						AfterStageWaitInSeconds: to.Ptr[int32](3600),
+						Groups: []*armcontainerservicefleet.UpdateGroup{
+							{
+								Name: to.Ptr("group-a"),
+							}},
+					}},
+			},
+		},
+	}, &armcontainerservicefleet.FleetUpdateStrategiesClientBeginCreateOrUpdateOptions{IfMatch: nil,
+		IfNoneMatch: nil,
+	})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.FleetUpdateStrategy = armcontainerservicefleet.FleetUpdateStrategy{
+	// 	Name: to.Ptr("strategy1"),
+	// 	Type: to.Ptr("Microsoft.ContainerService/fleets/updateStrategies"),
+	// 	ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/fleets/myFleet/updateStrategies/strategy1"),
+	// 	SystemData: &armcontainerservicefleet.SystemData{
+	// 		CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-03-01T01:10:08.395Z"); return t}()),
+	// 		CreatedBy: to.Ptr("@contoso.com"),
+	// 		CreatedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 		LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-03-01T01:10:08.395Z"); return t}()),
+	// 		LastModifiedBy: to.Ptr("@contoso.com"),
+	// 		LastModifiedByType: to.Ptr(armcontainerservicefleet.CreatedByTypeUser),
+	// 	},
+	// 	ETag: to.Ptr("\"EtagValue\""),
+	// 	Properties: &armcontainerservicefleet.FleetUpdateStrategyProperties{
+	// 		ProvisioningState: to.Ptr(armcontainerservicefleet.FleetUpdateStrategyProvisioningStateSucceeded),
+	// 		Strategy: &armcontainerservicefleet.UpdateRunStrategy{
+	// 			Stages: []*armcontainerservicefleet.UpdateStage{
+	// 				{
+	// 					Name: to.Ptr("stage1"),
+	// 					AfterStageWaitInSeconds: to.Ptr[int32](3600),
+	// 					Groups: []*armcontainerservicefleet.UpdateGroup{
+	// 						{
+	// 							Name: to.Ptr("group-a"),
+	// 					}},
+	// 			}},
+	// 		},
+	// 	},
+	// }
+}

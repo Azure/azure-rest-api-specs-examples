2e247da0cf20defbb2eb5b3f22507281a95e4a08
azure-sdk <azuresdk@microsoft.com>
2024-04-11 11:07:56 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-containerservicefleet_1.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-02-02-preview/examples-java/UpdateStrategies_Get.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for FleetUpdateStrategies Get.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-02-02-preview/
+     * examples/UpdateStrategies_Get.json
+     */
+    /**
+     * Sample code: Get a FleetUpdateStrategy resource.
+     * 
+     * @param manager Entry point to ContainerServiceFleetManager.
+     */
+    public static void getAFleetUpdateStrategyResource(
+        com.azure.resourcemanager.containerservicefleet.ContainerServiceFleetManager manager) {
+        manager.fleetUpdateStrategies().getWithResponse("rg1", "fleet1", "strategy1", com.azure.core.util.Context.NONE);
+    }
+}

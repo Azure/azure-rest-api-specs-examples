902dde0e0bcb860cd259294c500517ccc60a3aa0
azure-sdk <azuresdk@microsoft.com>
2024-04-24 11:19:01 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-quota_2.0.0b1

---
+++ specification/quota/resource-manager/Microsoft.Quota/preview/2023-06-01-preview/examples-python/getComputeOneSkuQuotaLimit.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.quota import QuotaMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-quota
+# USAGE
+    python get_compute_one_sku_quota_limit.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = QuotaMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.quota.get(
+        resource_name="standardNDSFamily",
+        scope="subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Compute/locations/eastus",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/quota/resource-manager/Microsoft.Quota/preview/2023-06-01-preview/examples/getComputeOneSkuQuotaLimit.json
+if __name__ == "__main__":
+    main()

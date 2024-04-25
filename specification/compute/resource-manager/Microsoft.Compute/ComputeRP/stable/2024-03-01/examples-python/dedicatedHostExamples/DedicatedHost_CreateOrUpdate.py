695a99581d04485ce4c072261da0eb42c3acd2c5
azure-sdk <azuresdk@microsoft.com>
2024-04-25 11:17:53 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_31.0.0

---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples-python/dedicatedHostExamples/DedicatedHost_CreateOrUpdate.py
@@ -0,0 +1,43 @@
+from typing import Any, IO, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python dedicated_host_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.dedicated_hosts.begin_create_or_update(
+        resource_group_name="myResourceGroup",
+        host_group_name="myDedicatedHostGroup",
+        host_name="myDedicatedHost",
+        parameters={
+            "location": "westus",
+            "properties": {"platformFaultDomain": 1},
+            "sku": {"name": "DSv3-Type1"},
+            "tags": {"department": "HR"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/dedicatedHostExamples/DedicatedHost_CreateOrUpdate.json
+if __name__ == "__main__":
+    main()
---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples-python/imageExamples/Image_Update.py
@@ -0,0 +1,45 @@
+from typing import Any, IO, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python image_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.images.begin_update(
+        resource_group_name="myResourceGroup",
+        image_name="myImage",
+        parameters={
+            "properties": {
+                "hyperVGeneration": "V1",
+                "sourceVirtualMachine": {
+                    "id": "/subscriptions/{subscription-id}/resourceGroups/myResourceGroup/providers/Microsoft.Compute/virtualMachines/myVM"
+                },
+            },
+            "tags": {"department": "HR"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/imageExamples/Image_Update.json
+if __name__ == "__main__":
+    main()

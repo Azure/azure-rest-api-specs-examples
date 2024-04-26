fedebd9448535a3456da4f3f333d08e269a8ac17
azure-sdk <azuresdk@microsoft.com>
2024-04-26 11:17:06 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-standbypool_1.0.0-beta.1

---
+++ specification/standbypool/resource-manager/Microsoft.StandbyPool/preview/2023-12-01-preview/examples-js/StandbyContainerGroupPools_CreateOrUpdate.js
@@ -0,0 +1,41 @@
+const { StandbyPoolManagementClient } = require("@azure/arm-standbypool");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Create a StandbyContainerGroupPoolResource
+ *
+ * @summary Create a StandbyContainerGroupPoolResource
+ * x-ms-original-file: specification/standbypool/resource-manager/Microsoft.StandbyPool/preview/2023-12-01-preview/examples/StandbyContainerGroupPools_CreateOrUpdate.json
+ */
+async function standbyContainerGroupPoolsCreateOrUpdate() {
+  const subscriptionId =
+    process.env["STANDBYPOOL_SUBSCRIPTION_ID"] || "8CC31D61-82D7-4B2B-B9DC-6B924DE7D229";
+  const resourceGroupName = process.env["STANDBYPOOL_RESOURCE_GROUP"] || "rgstandbypool";
+  const standbyContainerGroupPoolName = "pool";
+  const resource = {
+    location: "West US",
+    properties: {
+      containerGroupProperties: {
+        containerGroupProfile: {
+          id: "/subscriptions/8CC31D61-82D7-4B2B-B9DC-6B924DE7D229/resourceGroups/rgstandbypool/providers/Microsoft.ContainerInstance/containerGroupProfiles/cgProfile",
+          revision: 1,
+        },
+        subnetIds: [
+          {
+            id: "/subscriptions/8cf6c1b6-c80f-437c-87ad-45fbaff54f73/resourceGroups/rgstandbypool/providers/Microsoft.Network/virtualNetworks/cgSubnet/subnets/cgSubnet",
+          },
+        ],
+      },
+      elasticityProfile: { maxReadyCapacity: 688, refillPolicy: "always" },
+    },
+    tags: {},
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new StandbyPoolManagementClient(credential, subscriptionId);
+  const result = await client.standbyContainerGroupPools.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    standbyContainerGroupPoolName,
+    resource,
+  );
+  console.log(result);
+}
---
+++ specification/standbypool/resource-manager/Microsoft.StandbyPool/preview/2023-12-01-preview/examples-js/StandbyContainerGroupPools_ListBySubscription.js
@@ -0,0 +1,20 @@
+const { StandbyPoolManagementClient } = require("@azure/arm-standbypool");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to List StandbyContainerGroupPoolResource resources by subscription ID
+ *
+ * @summary List StandbyContainerGroupPoolResource resources by subscription ID
+ * x-ms-original-file: specification/standbypool/resource-manager/Microsoft.StandbyPool/preview/2023-12-01-preview/examples/StandbyContainerGroupPools_ListBySubscription.json
+ */
+async function standbyContainerGroupPoolsListBySubscription() {
+  const subscriptionId =
+    process.env["STANDBYPOOL_SUBSCRIPTION_ID"] || "8CC31D61-82D7-4B2B-B9DC-6B924DE7D229";
+  const credential = new DefaultAzureCredential();
+  const client = new StandbyPoolManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.standbyContainerGroupPools.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

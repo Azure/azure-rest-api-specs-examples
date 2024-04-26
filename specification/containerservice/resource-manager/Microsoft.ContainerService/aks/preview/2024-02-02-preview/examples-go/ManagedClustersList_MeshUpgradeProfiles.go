0607b4e439c60ffa7adc048505802f1f5fba4671
azure-sdk <azuresdk@microsoft.com>
2024-04-26 11:07:41 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v5.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-02-02-preview/examples-go/ManagedClustersList_MeshUpgradeProfiles.go
@@ -0,0 +1,56 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v5"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/1dd99306d14fd6c602f47652a209a4a6812c368c/specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-02-02-preview/examples/ManagedClustersList_MeshUpgradeProfiles.json
+func ExampleManagedClustersClient_NewListMeshUpgradeProfilesPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewManagedClustersClient().NewListMeshUpgradeProfilesPager("rg1", "clustername1", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.MeshUpgradeProfileList = armcontainerservice.MeshUpgradeProfileList{
+		// 	Value: []*armcontainerservice.MeshUpgradeProfile{
+		// 		{
+		// 			Name: to.Ptr("istio"),
+		// 			Type: to.Ptr("Microsoft.ContainerService/managedClusters/meshUpgradeProfiles"),
+		// 			ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/meshUpgradeProfiles/istio"),
+		// 			Properties: &armcontainerservice.MeshUpgradeProfileProperties{
+		// 				CompatibleWith: []*armcontainerservice.CompatibleVersions{
+		// 					{
+		// 						Name: to.Ptr("kubernetes"),
+		// 						Versions: []*string{
+		// 							to.Ptr("1.23"),
+		// 							to.Ptr("1.24"),
+		// 							to.Ptr("1.25"),
+		// 							to.Ptr("1.26")},
+		// 					}},
+		// 					Revision: to.Ptr("asm-1-17"),
+		// 					Upgrades: []*string{
+		// 						to.Ptr("1-18")},
+		// 					},
+		// 			}},
+		// 		}
+	}
+}

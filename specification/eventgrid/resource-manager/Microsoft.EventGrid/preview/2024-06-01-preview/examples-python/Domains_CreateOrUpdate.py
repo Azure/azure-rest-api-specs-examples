8a445b59797d282101b2e1f0d3abc9e337728224
azure-sdk <azuresdk@microsoft.com>
2024-04-29 11:44:11 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-eventgrid_10.3.0b4

---
+++ specification/eventgrid/resource-manager/Microsoft.EventGrid/preview/2024-06-01-preview/examples-python/Domains_CreateOrUpdate.py
@@ -0,0 +1,47 @@
+from typing import Any, IO, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.eventgrid import EventGridManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-eventgrid
+# USAGE
+    python domains_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = EventGridManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="8f6b6269-84f2-4d09-9e31-1127efcd1e40",
+    )
+
+    response = client.domains.begin_create_or_update(
+        resource_group_name="examplerg",
+        domain_name="exampledomain1",
+        domain_info={
+            "location": "westus2",
+            "properties": {
+                "inboundIpRules": [
+                    {"action": "Allow", "ipMask": "12.18.30.15"},
+                    {"action": "Allow", "ipMask": "12.18.176.1"},
+                ],
+                "publicNetworkAccess": "Enabled",
+            },
+            "tags": {"tag1": "value1", "tag2": "value2"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/eventgrid/resource-manager/Microsoft.EventGrid/preview/2024-06-01-preview/examples/Domains_CreateOrUpdate.json
+if __name__ == "__main__":
+    main()

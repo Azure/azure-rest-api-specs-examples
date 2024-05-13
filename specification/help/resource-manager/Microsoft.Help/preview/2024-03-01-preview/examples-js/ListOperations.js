f7eb30c5862d4fcaa0b4805b79ba4417a6f75a6b
azure-sdk <azuresdk@microsoft.com>
2024-05-13 11:05:41 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-selfhelp_2.0.0-beta.4

--- specification/help/resource-manager/Microsoft.Help/preview/2024-03-01-preview/examples-js/ListOperations.js
+++ specification/help/resource-manager/Microsoft.Help/preview/2024-03-01-preview/examples-js/ListOperations.js
@@ -8,10 +8,8 @@ const { DefaultAzureCredential } = require("@azure/identity");
  * x-ms-original-file: specification/help/resource-manager/Microsoft.Help/preview/2024-03-01-preview/examples/ListOperations.json
  */
 async function listAllOperations() {
-  const subscriptionId =
-    process.env["SELFHELP_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();
-  const client = new HelpRP(credential, subscriptionId);
+  const client = new HelpRP(credential);
   const resArray = new Array();
   for await (let item of client.operations.list()) {
     resArray.push(item);

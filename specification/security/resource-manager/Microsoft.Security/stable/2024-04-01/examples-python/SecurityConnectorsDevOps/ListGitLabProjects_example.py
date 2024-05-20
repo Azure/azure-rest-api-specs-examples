cd99f9e2cae0fe93fb6226405f6677605c2b9262
azure-sdk <azuresdk@microsoft.com>
2024-05-20 11:18:35 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-security_7.0.0

---
+++ specification/security/resource-manager/Microsoft.Security/stable/2024-04-01/examples-python/SecurityConnectorsDevOps/ListGitLabProjects_example.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.security import SecurityCenter
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-security
+# USAGE
+    python list_git_lab_projects_example.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SecurityCenter(
+        credential=DefaultAzureCredential(),
+        subscription_id="0806e1cd-cfda-4ff8-b99c-2b0af42cffd3",
+    )
+
+    response = client.git_lab_projects.list(
+        resource_group_name="myRg",
+        security_connector_name="mySecurityConnectorName",
+        group_fq_name="myGitLabGroup$mySubGroup",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/security/resource-manager/Microsoft.Security/stable/2024-04-01/examples/SecurityConnectorsDevOps/ListGitLabProjects_example.json
+if __name__ == "__main__":
+    main()

1642a3c8886718610cfd7484a665f9dacd561dd4
azure-sdk <azuresdk@microsoft.com>
2024-05-20 11:05:12 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-recoveryservices_1.3.0

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2024-04-01/examples-java/ListOperations.java
@@ -0,0 +1,19 @@
+
+/**
+ * Samples for Operations List.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2024-04-01/examples/
+     * ListOperations.json
+     */
+    /**
+     * Sample code: ListOperations.
+     * 
+     * @param manager Entry point to RecoveryServicesManager.
+     */
+    public static void listOperations(com.azure.resourcemanager.recoveryservices.RecoveryServicesManager manager) {
+        manager.operations().list(com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2024-04-01/examples-java/PUTVault.java
@@ -0,0 +1,45 @@
+
+import com.azure.resourcemanager.recoveryservices.models.IdentityData;
+import com.azure.resourcemanager.recoveryservices.models.PublicNetworkAccess;
+import com.azure.resourcemanager.recoveryservices.models.ResourceIdentityType;
+import com.azure.resourcemanager.recoveryservices.models.Sku;
+import com.azure.resourcemanager.recoveryservices.models.SkuName;
+import com.azure.resourcemanager.recoveryservices.models.VaultProperties;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Samples for Vaults CreateOrUpdate.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2024-04-01/examples/PUTVault.
+     * json
+     */
+    /**
+     * Sample code: Create or Update Recovery Services vault.
+     * 
+     * @param manager Entry point to RecoveryServicesManager.
+     */
+    public static void createOrUpdateRecoveryServicesVault(
+        com.azure.resourcemanager.recoveryservices.RecoveryServicesManager manager) {
+        manager.vaults().define("swaggerExample").withRegion("West US")
+            .withExistingResourceGroup("Default-RecoveryServices-ResourceGroup")
+            .withIdentity(new IdentityData().withType(ResourceIdentityType.SYSTEM_ASSIGNED))
+            .withProperties(new VaultProperties().withPublicNetworkAccess(PublicNetworkAccess.ENABLED))
+            .withSku(new Sku().withName(SkuName.STANDARD)).create();
+    }
+
+    // Use "Map.of" if available
+    @SuppressWarnings("unchecked")
+    private static <T> Map<String, T> mapOf(Object... inputs) {
+        Map<String, T> map = new HashMap<>();
+        for (int i = 0; i < inputs.length; i += 2) {
+            String key = (String) inputs[i];
+            T value = (T) inputs[i + 1];
+            map.put(key, value);
+        }
+        return map;
+    }
+}

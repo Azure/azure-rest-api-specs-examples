f10344875a6fb2ba04f8f6dd550868662cdbef39
azure-sdk <azuresdk@microsoft.com>
2024-06-18 11:11:49 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storage_21.2.0

---
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2023-05-01/examples-python/StorageAccountDeleteBlobInventoryPolicy.py
@@ -0,0 +1,39 @@
+from typing import TYPE_CHECKING, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.storage import StorageManagementClient
+
+if TYPE_CHECKING:
+    # pylint: disable=unused-import,ungrouped-imports
+    from .. import models as _models
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storage
+# USAGE
+    python storage_account_delete_blob_inventory_policy.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    client.blob_inventory_policies.delete(
+        resource_group_name="res6977",
+        account_name="sto2527",
+        blob_inventory_policy_name="default",
+    )
+
+
+# x-ms-original-file: specification/storage/resource-manager/Microsoft.Storage/stable/2023-05-01/examples/StorageAccountDeleteBlobInventoryPolicy.json
+if __name__ == "__main__":
+    main()

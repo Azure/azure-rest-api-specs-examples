380e85bc6193b38635c65cbb2b1299a419b25e85
azure-sdk <azuresdk@microsoft.com>
2024-06-26 11:23:13 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-scvmm_1.0.0

---
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples-python/GuestAgents_Get_MinimumSet_Gen.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.scvmm import ScVmmMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-scvmm
+# USAGE
+    python guest_agents_get_minimum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ScVmmMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.guest_agents.get(
+        resource_uri="gtgclehcbsyave",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/GuestAgents_Get_MinimumSet_Gen.json
+if __name__ == "__main__":
+    main()
---
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples-python/InventoryItems_Get_MinimumSet_Gen.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.scvmm import ScVmmMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-scvmm
+# USAGE
+    python inventory_items_get_minimum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ScVmmMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="79332E5A-630B-480F-A266-A941C015AB19",
+    )
+
+    response = client.inventory_items.get(
+        resource_group_name="rgscvmm",
+        vmm_server_name="_",
+        inventory_item_resource_name="cacb8Ceb-efAC-bebb-ae7C-dec8C5Bb7100",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/InventoryItems_Get_MinimumSet_Gen.json
+if __name__ == "__main__":
+    main()

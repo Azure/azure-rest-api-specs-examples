d72950011ce9793eb679ea52941d6d0325e7c83c
azure-sdk <azuresdk@microsoft.com>
2024-06-26 11:06:27 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-oracledatabase_1.0.0-beta.2

--- specification/oracle/resource-manager/Oracle.Database/preview/2023-09-01-preview/examples-java/autonomousDatabaseBackup_listByParent.java
+++ specification/oracle/resource-manager/Oracle.Database/preview/2023-09-01-preview/examples-java/autonomousDatabaseBackup_listByParent.java
@@ -8,11 +8,11 @@ public final class Main {
      * autonomousDatabaseBackup_listByParent.json
      */
     /**
-     * Sample code: List Autonomous Database Backups by Autonomous Database.
+     * Sample code: AutonomousDatabaseBackups_ListByAutonomousDatabase.
      * 
      * @param manager Entry point to OracleDatabaseManager.
      */
-    public static void listAutonomousDatabaseBackupsByAutonomousDatabase(
+    public static void autonomousDatabaseBackupsListByAutonomousDatabase(
         com.azure.resourcemanager.oracledatabase.OracleDatabaseManager manager) {
         manager.autonomousDatabaseBackups().listByAutonomousDatabase("rg000", "databasedb1",
             com.azure.core.util.Context.NONE);

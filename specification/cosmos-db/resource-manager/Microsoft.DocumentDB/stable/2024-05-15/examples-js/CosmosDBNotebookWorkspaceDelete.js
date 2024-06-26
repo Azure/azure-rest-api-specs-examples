d8782cdf96f92cfd9d2326cea4dba6d068aab476
azure-sdk <azuresdk@microsoft.com>
2024-06-26 11:12:05 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_16.0.0

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2024-05-15/examples-js/CosmosDBNotebookWorkspaceDelete.js
@@ -0,0 +1,23 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes the notebook workspace for a Cosmos DB account.
+ *
+ * @summary Deletes the notebook workspace for a Cosmos DB account.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2024-05-15/examples/CosmosDBNotebookWorkspaceDelete.json
+ */
+async function cosmosDbNotebookWorkspaceDelete() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subid";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rg1";
+  const accountName = "ddb1";
+  const notebookWorkspaceName = "default";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const result = await client.notebookWorkspaces.beginDeleteAndWait(
+    resourceGroupName,
+    accountName,
+    notebookWorkspaceName,
+  );
+  console.log(result);
+}
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2024-05-15/examples-js/CosmosDBSqlClientEncryptionKeysList.js
@@ -0,0 +1,26 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists the ClientEncryptionKeys under an existing Azure Cosmos DB SQL database.
+ *
+ * @summary Lists the ClientEncryptionKeys under an existing Azure Cosmos DB SQL database.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2024-05-15/examples/CosmosDBSqlClientEncryptionKeysList.json
+ */
+async function cosmosDbClientEncryptionKeysList() {
+  const subscriptionId = process.env["COSMOSDB_SUBSCRIPTION_ID"] || "subId";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rgName";
+  const accountName = "accountName";
+  const databaseName = "databaseName";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.sqlResources.listClientEncryptionKeys(
+    resourceGroupName,
+    accountName,
+    databaseName,
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

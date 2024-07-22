364d16fad3267a675e041e163cd43c35a60719fe
azure-sdk <azuresdk@microsoft.com>
2024-07-22 11:13:18 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-search_9.2.0b2

---
+++ specification/search/resource-manager/Microsoft.Search/preview/2024-06-01-preview/examples-python/GetSharedPrivateLinkResource.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python get_shared_private_link_resource.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.shared_private_link_resources.get(
+        resource_group_name="rg1",
+        search_service_name="mysearchservice",
+        shared_private_link_resource_name="testResource",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/search/resource-manager/Microsoft.Search/preview/2024-06-01-preview/examples/GetSharedPrivateLinkResource.json
+if __name__ == "__main__":
+    main()
---
+++ specification/search/resource-manager/Microsoft.Search/preview/2024-06-01-preview/examples-python/SearchGetAdminKeys.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python search_get_admin_keys.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.admin_keys.get(
+        resource_group_name="rg1",
+        search_service_name="mysearchservice",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/search/resource-manager/Microsoft.Search/preview/2024-06-01-preview/examples/SearchGetAdminKeys.json
+if __name__ == "__main__":
+    main()

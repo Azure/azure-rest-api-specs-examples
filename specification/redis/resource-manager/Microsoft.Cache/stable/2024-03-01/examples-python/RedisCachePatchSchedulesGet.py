c4e5cf715be0d769b5b4da615a92c06f2d87d358
azure-sdk <azuresdk@microsoft.com>
2024-07-23 11:21:11 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-redis_14.4.0

---
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2024-03-01/examples-python/RedisCachePatchSchedulesGet.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.redis import RedisManagementClient
+
+if TYPE_CHECKING:
+    # pylint: disable=unused-import,ungrouped-imports
+    from .. import models as _models
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-redis
+# USAGE
+    python redis_cache_patch_schedules_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RedisManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.patch_schedules.get(
+        resource_group_name="rg1",
+        name="cache1",
+        default="default",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/redis/resource-manager/Microsoft.Cache/stable/2024-03-01/examples/RedisCachePatchSchedulesGet.json
+if __name__ == "__main__":
+    main()

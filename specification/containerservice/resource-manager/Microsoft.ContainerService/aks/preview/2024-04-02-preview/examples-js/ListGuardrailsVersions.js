616c1c826aedebc48b9f7c02e225fd770ce6f25a
azure-sdk <azuresdk@microsoft.com>
2024-07-24 11:11:52 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_20.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-04-02-preview/examples-js/ListGuardrailsVersions.js
@@ -0,0 +1,21 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Contains list of Guardrails version along with its support info and whether it is a default version.
+ *
+ * @summary Contains list of Guardrails version along with its support info and whether it is a default version.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-04-02-preview/examples/ListGuardrailsVersions.json
+ */
+async function listGuardrailsVersions() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const location = "location1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.managedClusters.listGuardrailsVersions(location)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

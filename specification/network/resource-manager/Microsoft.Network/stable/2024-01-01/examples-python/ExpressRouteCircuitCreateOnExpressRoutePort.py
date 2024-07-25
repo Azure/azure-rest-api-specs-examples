86e6ff6f1ac2fe366e98a0c601e15ef9015949cb
azure-sdk <azuresdk@microsoft.com>
2024-07-25 11:23:15 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-network_26.0.0

---
+++ specification/network/resource-manager/Microsoft.Network/stable/2024-01-01/examples-python/ExpressRouteCircuitCreateOnExpressRoutePort.py
@@ -0,0 +1,48 @@
+from typing import Any, IO, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.network import NetworkManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-network
+# USAGE
+    python express_route_circuit_create_on_express_route_port.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = NetworkManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.express_route_circuits.begin_create_or_update(
+        resource_group_name="rg1",
+        circuit_name="expressRouteCircuit1",
+        parameters={
+            "location": "westus",
+            "properties": {
+                "authorizationKey": "b0be57f5-1fba-463b-adec-ffe767354cdd",
+                "bandwidthInGbps": 10,
+                "enableDirectPortRateLimit": False,
+                "expressRoutePort": {
+                    "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/expressRoutePorts/portName"
+                },
+            },
+            "sku": {"family": "MeteredData", "name": "Premium_MeteredData", "tier": "Premium"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2024-01-01/examples/ExpressRouteCircuitCreateOnExpressRoutePort.json
+if __name__ == "__main__":
+    main()
---
+++ specification/network/resource-manager/Microsoft.Network/stable/2024-01-01/examples-python/VirtualNetworkPeeringCreate.py
@@ -0,0 +1,48 @@
+from typing import Any, IO, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.network import NetworkManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-network
+# USAGE
+    python virtual_network_peering_create.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = NetworkManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.virtual_network_peerings.begin_create_or_update(
+        resource_group_name="peerTest",
+        virtual_network_name="vnet1",
+        virtual_network_peering_name="peer",
+        virtual_network_peering_parameters={
+            "properties": {
+                "allowForwardedTraffic": True,
+                "allowGatewayTransit": False,
+                "allowVirtualNetworkAccess": True,
+                "remoteVirtualNetwork": {
+                    "id": "/subscriptions/subid/resourceGroups/peerTest/providers/Microsoft.Network/virtualNetworks/vnet2"
+                },
+                "useRemoteGateways": False,
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2024-01-01/examples/VirtualNetworkPeeringCreate.json
+if __name__ == "__main__":
+    main()

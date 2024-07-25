ae90ce90752b0909e4da80fbda20a15c43990e08
azure-sdk <azuresdk@microsoft.com>
2024-07-25 11:14:51 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/compute/armcompute/v6.0.0

--- specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples-go/virtualMachineExamples/VirtualMachine_Create_WithBootDiagnostics.go
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples-go/virtualMachineExamples/VirtualMachine_Create_WithBootDiagnostics.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v6"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/92de53a5f1e0e03c94b40475d2135d97148ed014/specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/virtualMachineExamples/VirtualMachine_Create_WithBootDiagnostics.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/81a4ee5a83ae38620c0e1404793caffe005d26e4/specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/virtualMachineExamples/VirtualMachine_Create_WithBootDiagnostics.json
 func ExampleVirtualMachinesClient_BeginCreateOrUpdate_createAVmWithBootDiagnostics() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {

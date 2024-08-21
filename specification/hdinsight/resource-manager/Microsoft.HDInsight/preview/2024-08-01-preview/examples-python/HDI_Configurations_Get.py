828a621577c30635a2b0d76d9e17577e9eb9ca10
azure-sdk <azuresdk@microsoft.com>
2024-08-21 11:28:08 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-hdinsight_9.0.0b3

---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2024-08-01-preview/examples-python/HDI_Configurations_Get.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.hdinsight import HDInsightManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hdinsight
+# USAGE
+    python hdi_configurations_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HDInsightManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.configurations.get(
+        resource_group_name="rg1",
+        cluster_name="cluster1",
+        configuration_name="core-site",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2024-08-01-preview/examples/HDI_Configurations_Get.json
+if __name__ == "__main__":
+    main()
---
+++ specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2024-08-01-preview/examples-python/ResizeLinuxHadoopCluster.py
@@ -0,0 +1,40 @@
+from typing import Any, IO, TYPE_CHECKING, Union
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.hdinsight import HDInsightManagementClient
+
+if TYPE_CHECKING:
+    # pylint: disable=unused-import,ungrouped-imports
+    from .. import models as _models
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hdinsight
+# USAGE
+    python resize_linux_hadoop_cluster.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = HDInsightManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    client.clusters.begin_resize(
+        resource_group_name="rg1",
+        cluster_name="cluster1",
+        role_name="workernode",
+        parameters={"targetInstanceCount": 10},
+    ).result()
+
+
+# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2024-08-01-preview/examples/ResizeLinuxHadoopCluster.json
+if __name__ == "__main__":
+    main()

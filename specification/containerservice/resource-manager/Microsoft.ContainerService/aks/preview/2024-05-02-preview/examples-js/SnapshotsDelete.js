f6924276e76bf5f92025a9cc5231d1b35c60c52e
azure-sdk <azuresdk@microsoft.com>
2024-08-23 11:11:48 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_21.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-05-02-preview/examples-js/SnapshotsDelete.js
@@ -0,0 +1,19 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes a snapshot.
+ *
+ * @summary Deletes a snapshot.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-05-02-preview/examples/SnapshotsDelete.json
+ */
+async function deleteSnapshot() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "snapshot1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.snapshots.delete(resourceGroupName, resourceName);
+  console.log(result);
+}

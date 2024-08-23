69a46404a96f1a54cb450716b839e9673037b896
azure-sdk <azuresdk@microsoft.com>
2024-08-23 11:22:38 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_33.0.0

---
+++ specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-07-01/examples-python/imageExamples/Image_ListByResourceGroup.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python image_list_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="{subscription-id}",
+    )
+
+    response = client.images.list_by_resource_group(
+        resource_group_name="myResourceGroup",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-07-01/examples/imageExamples/Image_ListByResourceGroup.json
+if __name__ == "__main__":
+    main()

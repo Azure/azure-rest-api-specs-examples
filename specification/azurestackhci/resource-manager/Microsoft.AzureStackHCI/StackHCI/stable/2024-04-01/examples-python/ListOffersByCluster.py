3f3df6118de1e817ff6df611a229365edeaf5d04
azure-sdk <azuresdk@microsoft.com>
2024-08-26 11:09:34 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-azurestackhci_8.0.0b4

---
+++ specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/StackHCI/stable/2024-04-01/examples-python/ListOffersByCluster.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.azurestackhci import AzureStackHCIClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-azurestackhci
+# USAGE
+    python list_offers_by_cluster.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureStackHCIClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="fd3c3665-1729-4b7b-9a38-238e83b0f98b",
+    )
+
+    response = client.offers.list_by_cluster(
+        resource_group_name="test-rg",
+        cluster_name="myCluster",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/StackHCI/stable/2024-04-01/examples/ListOffersByCluster.json
+if __name__ == "__main__":
+    main()

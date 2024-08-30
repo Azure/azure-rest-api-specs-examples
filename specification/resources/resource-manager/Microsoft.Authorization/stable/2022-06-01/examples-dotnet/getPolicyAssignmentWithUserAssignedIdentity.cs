370922b27e8ecc0fa246d6e2c7dbc48a2e86f5e6
azure-sdk <azuresdk@microsoft.com>
2024-08-30 11:12:44 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager_1.13.0

--- specification/resources/resource-manager/Microsoft.Authorization/stable/2022-06-01/examples-dotnet/getPolicyAssignmentWithUserAssignedIdentity.cs
+++ specification/resources/resource-manager/Microsoft.Authorization/stable/2022-06-01/examples-dotnet/getPolicyAssignmentWithUserAssignedIdentity.cs
@@ -1,6 +1,7 @@
 using Azure;
 using Azure.ResourceManager;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
@@ -16,18 +17,28 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this PolicyAssignmentResource created on azure
-// for more information of creating PolicyAssignmentResource, please refer to the document of PolicyAssignmentResource
+// this example assumes you already have this ArmResource created on azure
+// for more information of creating ArmResource, please refer to the document of ArmResource
+
+// get the collection of this PolicyAssignmentResource
 string scope = "subscriptions/ae640e6b-ba3e-4256-9d62-2993eecfa6f2";
-string policyAssignmentName = "EnforceNaming";
-ResourceIdentifier policyAssignmentResourceId = PolicyAssignmentResource.CreateResourceIdentifier(scope, policyAssignmentName);
-PolicyAssignmentResource policyAssignment = client.GetPolicyAssignmentResource(policyAssignmentResourceId);
+ResourceIdentifier scopeId = new ResourceIdentifier(string.Format("/{0}", scope));
+PolicyAssignmentCollection collection = client.GetGenericResource(scopeId).GetPolicyAssignments();
 
 // invoke the operation
-PolicyAssignmentResource result = await policyAssignment.GetAsync();
+string policyAssignmentName = "EnforceNaming";
+NullableResponse<PolicyAssignmentResource> response = await collection.GetIfExistsAsync(policyAssignmentName);
+PolicyAssignmentResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-PolicyAssignmentData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    PolicyAssignmentData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

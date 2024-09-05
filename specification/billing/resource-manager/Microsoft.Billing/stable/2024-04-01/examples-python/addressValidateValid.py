d460d6888bd8ef54375f4e37e912689ab4384b36
azure-sdk <azuresdk@microsoft.com>
2024-09-05 11:07:57 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-billing_7.0.0

---
+++ specification/billing/resource-manager/Microsoft.Billing/stable/2024-04-01/examples-python/addressValidateValid.py
@@ -0,0 +1,39 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.billing import BillingManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-billing
+# USAGE
+    python address_validate_valid.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = BillingManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.address.validate(
+        parameters={
+            "addressLine1": "1 Test",
+            "city": "bellevue",
+            "country": "us",
+            "postalCode": "12345",
+            "region": "wa",
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/billing/resource-manager/Microsoft.Billing/stable/2024-04-01/examples/addressValidateValid.json
+if __name__ == "__main__":
+    main()
---
+++ specification/billing/resource-manager/Microsoft.Billing/stable/2024-04-01/examples-python/billingPropertyGetMCA.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.billing import BillingManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-billing
+# USAGE
+    python billing_property_get_mca.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = BillingManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="11111111-1111-1111-1111-111111111111",
+    )
+
+    response = client.billing_property.get()
+    print(response)
+
+
+# x-ms-original-file: specification/billing/resource-manager/Microsoft.Billing/stable/2024-04-01/examples/billingPropertyGetMCA.json
+if __name__ == "__main__":
+    main()

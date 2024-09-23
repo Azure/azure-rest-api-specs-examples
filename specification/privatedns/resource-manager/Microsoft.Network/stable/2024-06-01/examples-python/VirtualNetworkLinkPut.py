a308909d91a95bba313d9b6bd28b38ca0ed6f820
azure-sdk <azuresdk@microsoft.com>
2024-09-23 11:08:55 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-privatedns_1.2.0

---
+++ specification/privatedns/resource-manager/Microsoft.Network/stable/2024-06-01/examples-python/VirtualNetworkLinkPut.py
@@ -0,0 +1,45 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.privatedns import PrivateDnsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-privatedns
+# USAGE
+    python virtual_network_link_put.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PrivateDnsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subscriptionId",
+    )
+
+    response = client.virtual_network_links.begin_create_or_update(
+        resource_group_name="resourceGroup1",
+        private_zone_name="privatezone1.com",
+        virtual_network_link_name="virtualNetworkLink1",
+        parameters={
+            "location": "Global",
+            "properties": {
+                "registrationEnabled": False,
+                "virtualNetwork": {
+                    "id": "/subscriptions/virtualNetworkSubscriptionId/resourceGroups/virtualNetworkResourceGroup/providers/Microsoft.Network/virtualNetworks/virtualNetworkName"
+                },
+            },
+            "tags": {"key1": "value1"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/privatedns/resource-manager/Microsoft.Network/stable/2024-06-01/examples/VirtualNetworkLinkPut.json
+if __name__ == "__main__":
+    main()

62f833e8facd8eb409098e2800c4169229f16ca1
azure-sdk <azuresdk@microsoft.com>
2024-09-24 11:11:58 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-fabric_1.0.0b1

---
+++ specification/fabric/resource-manager/Microsoft.Fabric/stable/2023-11-01/examples-python/FabricCapacities_Suspend.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.fabric import FabricMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-fabric
+# USAGE
+    python fabric_capacities_suspend.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = FabricMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.fabric_capacities.begin_suspend(
+        resource_group_name="TestRG",
+        capacity_name="azsdktest",
+    ).result()
+
+
+# x-ms-original-file: 2023-11-01/FabricCapacities_Suspend.json
+if __name__ == "__main__":
+    main()

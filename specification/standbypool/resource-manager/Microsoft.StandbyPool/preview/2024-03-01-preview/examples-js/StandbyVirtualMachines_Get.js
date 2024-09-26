3a8c80ade4ddd79ab287e3fd266e57b0f321e7b9
azure-sdk <azuresdk@microsoft.com>
2024-09-26 11:23:46 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-standbypool_1.0.0

---
+++ specification/standbypool/resource-manager/Microsoft.StandbyPool/preview/2024-03-01-preview/examples-js/StandbyVirtualMachines_Get.js
@@ -0,0 +1,16 @@
+const { StandbyPoolManagementClient } = require("@azure/arm-standbypool");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a StandbyVirtualMachineResource
+ *
+ * @summary get a StandbyVirtualMachineResource
+ * x-ms-original-file: 2024-03-01/StandbyVirtualMachines_Get.json
+ */
+async function standbyVirtualMachinesGet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000009";
+  const client = new StandbyPoolManagementClient(credential, subscriptionId);
+  const result = await client.standbyVirtualMachines.get("rgstandbypool", "pool", "virtualMachine");
+  console.log(result);
+}

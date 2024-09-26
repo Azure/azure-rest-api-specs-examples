54ab46dbab420cc82d5447b6fd6ecffd1af73a6e
azure-sdk <azuresdk@microsoft.com>
2024-09-26 11:27:45 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-elasticsan_1.1.0

---
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/stable/2024-05-01/examples-js/VolumeGroups_Update_MaximumSet_Gen.js
@@ -0,0 +1,50 @@
+const { ElasticSanManagement } = require("@azure/arm-elasticsan");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Update an VolumeGroup.
+ *
+ * @summary Update an VolumeGroup.
+ * x-ms-original-file: specification/elasticsan/resource-manager/Microsoft.ElasticSan/stable/2024-05-01/examples/VolumeGroups_Update_MaximumSet_Gen.json
+ */
+async function volumeGroupsUpdateMaximumSetGen() {
+  const subscriptionId = process.env["ELASTICSANS_SUBSCRIPTION_ID"] || "subscriptionid";
+  const resourceGroupName = process.env["ELASTICSANS_RESOURCE_GROUP"] || "resourcegroupname";
+  const elasticSanName = "elasticsanname";
+  const volumeGroupName = "volumegroupname";
+  const parameters = {
+    identity: { type: "None", userAssignedIdentities: { key1006: {} } },
+    properties: {
+      encryption: "EncryptionAtRestWithPlatformKey",
+      encryptionProperties: {
+        encryptionIdentity: {
+          encryptionUserAssignedIdentity: "gfhkfbozahmmwluqndfgxunssafa",
+        },
+        keyVaultProperties: {
+          keyName: "lunpapamzeimppgobraxjt",
+          keyVaultUri: "https://microsoft.com/a",
+          keyVersion: "oemygbnfmqhijmonkqfqmy",
+        },
+      },
+      enforceDataIntegrityCheckForIscsi: true,
+      networkAcls: {
+        virtualNetworkRules: [
+          {
+            action: "Allow",
+            virtualNetworkResourceId: "bkhwaiqvvaguymsmnzzbzz",
+          },
+        ],
+      },
+      protocolType: "Iscsi",
+    },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ElasticSanManagement(credential, subscriptionId);
+  const result = await client.volumeGroups.beginUpdateAndWait(
+    resourceGroupName,
+    elasticSanName,
+    volumeGroupName,
+    parameters,
+  );
+  console.log(result);
+}

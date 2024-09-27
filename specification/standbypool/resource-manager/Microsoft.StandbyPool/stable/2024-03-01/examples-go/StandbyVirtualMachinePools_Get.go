9cafad9e805d209acc18ce9fcc73981dd60c8e87
azure-sdk <azuresdk@microsoft.com>
2024-09-27 11:13:09 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/standbypool/armstandbypool/v1.0.0

---
+++ specification/standbypool/resource-manager/Microsoft.StandbyPool/stable/2024-03-01/examples-go/StandbyVirtualMachinePools_Get.go
@@ -0,0 +1,56 @@
+package armstandbypool_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/standbypool/armstandbypool"
+)
+
+// Generated from example definition: 2024-03-01/StandbyVirtualMachinePools_Get.json
+func ExampleStandbyVirtualMachinePoolsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armstandbypool.NewClientFactory("00000000-0000-0000-0000-000000000009", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewStandbyVirtualMachinePoolsClient().Get(ctx, "rgstandbypool", "pool", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armstandbypool.StandbyVirtualMachinePoolsClientGetResponse{
+	// 	StandbyVirtualMachinePoolResource: &armstandbypool.StandbyVirtualMachinePoolResource{
+	// 		Properties: &armstandbypool.StandbyVirtualMachinePoolResourceProperties{
+	// 			ElasticityProfile: &armstandbypool.StandbyVirtualMachinePoolElasticityProfile{
+	// 				MaxReadyCapacity: to.Ptr[int64](304),
+	// 				MinReadyCapacity: to.Ptr[int64](300),
+	// 			},
+	// 			VirtualMachineState: to.Ptr(armstandbypool.VirtualMachineStateRunning),
+	// 			AttachedVirtualMachineScaleSetID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/providers/Microsoft.Compute/virtualMachineScaleSets/myVmss"),
+	// 			ProvisioningState: to.Ptr(armstandbypool.ProvisioningStateSucceeded),
+	// 		},
+	// 		Tags: map[string]*string{
+	// 		},
+	// 		Location: to.Ptr("West US"),
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/providers/Microsoft.StandbyPool/standbyVirtualMachinePools/pool"),
+	// 		Name: to.Ptr("pool"),
+	// 		Type: to.Ptr("Microsoft.StandbyPool/standbyVirtualMachinePools"),
+	// 		SystemData: &armstandbypool.SystemData{
+	// 			CreatedBy: to.Ptr("pooluser@microsoft.com"),
+	// 			CreatedByType: to.Ptr(armstandbypool.CreatedByTypeUser),
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-09-07T16:33:22.210Z"); return t}()),
+	// 			LastModifiedBy: to.Ptr("pooluser@microsoft.com"),
+	// 			LastModifiedByType: to.Ptr(armstandbypool.CreatedByTypeUser),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-09-07T16:33:22.210Z"); return t}()),
+	// 		},
+	// 	},
+	// }
+}

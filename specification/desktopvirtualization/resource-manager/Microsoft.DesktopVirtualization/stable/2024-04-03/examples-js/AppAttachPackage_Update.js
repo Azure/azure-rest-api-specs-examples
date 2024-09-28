3f74c5bd2d813bfb60b18d5e5347b88a008cce3c
azure-sdk <azuresdk@microsoft.com>
2024-09-28 11:17:25 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-desktopvirtualization_1.2.0

---
+++ specification/desktopvirtualization/resource-manager/Microsoft.DesktopVirtualization/stable/2024-04-03/examples-js/AppAttachPackage_Update.js
@@ -0,0 +1,22 @@
+const { DesktopVirtualizationAPIClient } = require("@azure/arm-desktopvirtualization");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Update an App Attach Package
+ *
+ * @summary Update an App Attach Package
+ * x-ms-original-file: specification/desktopvirtualization/resource-manager/Microsoft.DesktopVirtualization/stable/2024-04-03/examples/AppAttachPackage_Update.json
+ */
+async function appAttachPackageUpdate() {
+  const subscriptionId =
+    process.env["DESKTOPVIRTUALIZATION_SUBSCRIPTION_ID"] || "daefabc0-95b4-48b3-b645-8a753a63c4fa";
+  const resourceGroupName = process.env["DESKTOPVIRTUALIZATION_RESOURCE_GROUP"] || "resourceGroup1";
+  const appAttachPackageName = "msixpackagefullname";
+  const credential = new DefaultAzureCredential();
+  const client = new DesktopVirtualizationAPIClient(credential, subscriptionId);
+  const result = await client.appAttachPackageOperations.update(
+    resourceGroupName,
+    appAttachPackageName,
+  );
+  console.log(result);
+}

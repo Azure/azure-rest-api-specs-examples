48a09b0317247ce7dca810a5d4d249483c62558a
azure-sdk <azuresdk@microsoft.com>
2024-09-28 11:06:03 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/cosmos/armcosmos/v4.0.0-beta.1

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-09-01-preview/examples-go/CosmosDBSqlContainerDelete.go
@@ -0,0 +1,30 @@
+package armcosmos_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cosmos/armcosmos/v4"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/1ad29756bd141a47cac770140105a706d065ae1b/specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-09-01-preview/examples/CosmosDBSqlContainerDelete.json
+func ExampleSQLResourcesClient_BeginDeleteSQLContainer() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcosmos.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewSQLResourcesClient().BeginDeleteSQLContainer(ctx, "rg1", "ddb1", "databaseName", "containerName", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-09-01-preview/examples-go/NetworkSecurityPerimeterConfigurationGet.go
@@ -0,0 +1,65 @@
+package armcosmos_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cosmos/armcosmos/v4"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/1ad29756bd141a47cac770140105a706d065ae1b/specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-09-01-preview/examples/NetworkSecurityPerimeterConfigurationGet.json
+func ExampleNetworkSecurityPerimeterConfigurationsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcosmos.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewNetworkSecurityPerimeterConfigurationsClient().Get(ctx, "res4410", "cosmosTest", "dbedb4e0-40e6-4145-81f3-f1314c150774.resourceAssociation1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.NetworkSecurityPerimeterConfiguration = armcosmos.NetworkSecurityPerimeterConfiguration{
+	// 	Name: to.Ptr("dbedb4e0-40e6-4145-81f3-f1314c150774.resourceAssociation1"),
+	// 	Type: to.Ptr("Microsoft.DocumentDB/databaseAccounts/networkSecurityPerimeterConfigurations"),
+	// 	ID: to.Ptr("/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/res4410/providers/Microsoft.DocumentDB/databaseAccounts/cosmosTest/networkSecurityPerimeterConfigurations/dbedb4e0-40e6-4145-81f3-f1314c150774.resourceAssociation1"),
+	// 	Properties: &armcosmos.NetworkSecurityPerimeterConfigurationProperties{
+	// 		NetworkSecurityPerimeter: &armcosmos.NetworkSecurityPerimeter{
+	// 			ID: to.Ptr("/subscriptions/dbedb4e0-40e6-4145-81f3-f1314c150774/resourceGroups/res4794/providers/Microsoft.Network/networkSecurityPerimeters/nsp1"),
+	// 			Location: to.Ptr("East US"),
+	// 			PerimeterGUID: to.Ptr("ce2d5953-5c15-40ca-9d51-cc3f4a63b0f5"),
+	// 		},
+	// 		Profile: &armcosmos.NetworkSecurityProfile{
+	// 			Name: to.Ptr("profile1"),
+	// 			AccessRules: []*armcosmos.AccessRule{
+	// 				{
+	// 					Name: to.Ptr("allowedSubscriptions"),
+	// 					Properties: &armcosmos.AccessRuleProperties{
+	// 						Direction: to.Ptr(armcosmos.AccessRuleDirectionInbound),
+	// 						Subscriptions: []*armcosmos.AccessRulePropertiesSubscriptionsItem{
+	// 							{
+	// 								ID: to.Ptr("/subscriptions/dbedb4e0-40e6-4145-81f3-f1314c150774"),
+	// 						}},
+	// 					},
+	// 			}},
+	// 			AccessRulesVersion: to.Ptr[int32](10),
+	// 			DiagnosticSettingsVersion: to.Ptr[int32](5),
+	// 			EnabledLogCategories: []*string{
+	// 				to.Ptr("NspPublicInboundPerimeterRulesAllowed"),
+	// 				to.Ptr("NspPublicInboundPerimeterRulesDenied")},
+	// 			},
+	// 			ProvisioningState: to.Ptr(armcosmos.NetworkSecurityPerimeterConfigurationProvisioningStateSucceeded),
+	// 			ResourceAssociation: &armcosmos.ResourceAssociation{
+	// 				Name: to.Ptr("resourceAssociation1"),
+	// 				AccessMode: to.Ptr(armcosmos.ResourceAssociationAccessModeEnforced),
+	// 			},
+	// 		},
+	// 	}
+}

019dbf0912a293e43e9f8162602440cf8e85b939
azure-sdk <azuresdk@microsoft.com>
2024-09-29 11:11:13 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-servicenetworking_1.1.0-beta.1

---
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/preview/2024-05-01-preview/examples-js/SecurityPolicyPut.js
@@ -0,0 +1,32 @@
+const { ServiceNetworkingManagementClient } = require("@azure/arm-servicenetworking");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Create a SecurityPolicy
+ *
+ * @summary Create a SecurityPolicy
+ * x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/preview/2024-05-01-preview/examples/SecurityPolicyPut.json
+ */
+async function putSecurityPolicy() {
+  const subscriptionId = process.env["SERVICENETWORKING_SUBSCRIPTION_ID"] || "subid";
+  const resourceGroupName = process.env["SERVICENETWORKING_RESOURCE_GROUP"] || "rg1";
+  const trafficControllerName = "tc1";
+  const securityPolicyName = "sp1";
+  const resource = {
+    location: "NorthCentralUS",
+    properties: {
+      wafPolicy: {
+        id: "/subscriptions/subid/resourcegroups/rg1/providers/Microsoft.Networking/applicationGatewayWebApplicationFirewallPolicies/wp-0",
+      },
+    },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
+  const result = await client.securityPoliciesInterface.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    trafficControllerName,
+    securityPolicyName,
+    resource,
+  );
+  console.log(result);
+}

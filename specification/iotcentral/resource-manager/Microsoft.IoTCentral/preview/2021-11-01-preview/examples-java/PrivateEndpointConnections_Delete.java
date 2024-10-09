287bb2378535761420e20fe0fb8da754695052f2
azure-sdk <azuresdk@microsoft.com>
2024-10-09 06:19:42 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-iotcentral_1.1.0-beta.2

--- specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-java/PrivateEndpointConnections_Delete.java
+++ specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples-java/PrivateEndpointConnections_Delete.java
@@ -1,19 +1,21 @@
-import com.azure.core.util.Context;
 
-/** Samples for PrivateEndpointConnections Delete. */
+/**
+ * Samples for PrivateEndpointConnections Delete.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples/PrivateEndpointConnections_Delete.json
+     * x-ms-original-file:
+     * specification/iotcentral/resource-manager/Microsoft.IoTCentral/preview/2021-11-01-preview/examples/
+     * PrivateEndpointConnections_Delete.json
      */
     /**
      * Sample code: PrivateEndpointConnections_Delete.
-     *
+     * 
      * @param manager Entry point to IotCentralManager.
      */
-    public static void privateEndpointConnectionsDelete(
-        com.azure.resourcemanager.iotcentral.IotCentralManager manager) {
-        manager
-            .privateEndpointConnections()
-            .delete("resRg", "myIoTCentralApp", "myIoTCentralAppEndpoint", Context.NONE);
+    public static void
+        privateEndpointConnectionsDelete(com.azure.resourcemanager.iotcentral.IotCentralManager manager) {
+        manager.privateEndpointConnections().delete("resRg", "myIoTCentralApp", "myIoTCentralAppEndpoint",
+            com.azure.core.util.Context.NONE);
     }
 }

bf1bb62c302f033771354bbd17a6133c651f13c2
azure-sdk <azuresdk@microsoft.com>
2024-10-09 06:18:02 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-servicelinker_1.0.0-beta.3

--- specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples-java/DeleteLink.java
+++ specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples-java/DeleteLink.java
@@ -1,21 +1,20 @@
-import com.azure.core.util.Context;
 
-/** Samples for Linker Delete. */
+/**
+ * Samples for Linker Delete.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples/DeleteLink.json
+     * x-ms-original-file:
+     * specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples/DeleteLink.json
      */
     /**
      * Sample code: DeleteLink.
-     *
+     * 
      * @param manager Entry point to ServiceLinkerManager.
      */
     public static void deleteLink(com.azure.resourcemanager.servicelinker.ServiceLinkerManager manager) {
-        manager
-            .linkers()
-            .delete(
-                "subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.Web/sites/test-app",
-                "linkName",
-                Context.NONE);
+        manager.linkers().delete(
+            "subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.Web/sites/test-app",
+            "linkName", com.azure.core.util.Context.NONE);
     }
 }
--- specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples-java/OperationsList.java
+++ specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples-java/OperationsList.java
@@ -1,16 +1,19 @@
-import com.azure.core.util.Context;
 
-/** Samples for Operations List. */
+/**
+ * Samples for Operations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples/OperationsList.json
+     * x-ms-original-file:
+     * specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples/OperationsList.
+     * json
      */
     /**
      * Sample code: GetConfiguration.
-     *
+     * 
      * @param manager Entry point to ServiceLinkerManager.
      */
     public static void getConfiguration(com.azure.resourcemanager.servicelinker.ServiceLinkerManager manager) {
-        manager.operations().list(Context.NONE);
+        manager.operations().list(com.azure.core.util.Context.NONE);
     }
 }

29111b53520984765a0d19a439e61e2d32385cbd
azure-sdk <azuresdk@microsoft.com>
2024-10-10 11:06:22 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-automanage_1.0.0-beta.2

--- specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-java/createOrUpdateConfigurationProfileHCIAssignment.java
+++ specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-java/createOrUpdateConfigurationProfileHCIAssignment.java
@@ -1,30 +1,27 @@
-import com.azure.core.util.Context;
+
 import com.azure.resourcemanager.automanage.fluent.models.ConfigurationProfileAssignmentInner;
 import com.azure.resourcemanager.automanage.models.ConfigurationProfileAssignmentProperties;
 
-/** Samples for ConfigurationProfileHciAssignments CreateOrUpdate. */
+/**
+ * Samples for ConfigurationProfileHciAssignments CreateOrUpdate.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples/createOrUpdateConfigurationProfileHCIAssignment.json
+     * x-ms-original-file: specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples/
+     * createOrUpdateConfigurationProfileHCIAssignment.json
      */
     /**
      * Sample code: Create or update a HCI configuration profile assignment.
-     *
+     * 
      * @param manager Entry point to AutomanageManager.
      */
     public static void createOrUpdateAHCIConfigurationProfileAssignment(
         com.azure.resourcemanager.automanage.AutomanageManager manager) {
-        manager
-            .configurationProfileHciAssignments()
-            .createOrUpdateWithResponse(
-                "myResourceGroupName",
-                "myClusterName",
-                "default",
-                new ConfigurationProfileAssignmentInner()
-                    .withProperties(
-                        new ConfigurationProfileAssignmentProperties()
-                            .withConfigurationProfile(
-                                "/providers/Microsoft.Automanage/bestPractices/AzureBestPracticesProduction")),
-                Context.NONE);
+        manager.configurationProfileHciAssignments().createOrUpdateWithResponse("myResourceGroupName", "myClusterName",
+            "default",
+            new ConfigurationProfileAssignmentInner()
+                .withProperties(new ConfigurationProfileAssignmentProperties().withConfigurationProfile(
+                    "/providers/Microsoft.Automanage/bestPractices/AzureBestPracticesProduction")),
+            com.azure.core.util.Context.NONE);
     }
 }
--- specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-java/getBestPracticeVersion.java
+++ specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples-java/getBestPracticeVersion.java
@@ -1,17 +1,21 @@
-import com.azure.core.util.Context;
 
-/** Samples for BestPracticesVersions Get. */
+/**
+ * Samples for BestPracticesVersions Get.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples/getBestPracticeVersion.json
+     * x-ms-original-file:
+     * specification/automanage/resource-manager/Microsoft.Automanage/stable/2022-05-04/examples/getBestPracticeVersion.
+     * json
      */
     /**
      * Sample code: Get an Automanage best practice version.
-     *
+     * 
      * @param manager Entry point to AutomanageManager.
      */
-    public static void getAnAutomanageBestPracticeVersion(
-        com.azure.resourcemanager.automanage.AutomanageManager manager) {
-        manager.bestPracticesVersions().getWithResponse("azureBestPracticesProduction", "version1", Context.NONE);
+    public static void
+        getAnAutomanageBestPracticeVersion(com.azure.resourcemanager.automanage.AutomanageManager manager) {
+        manager.bestPracticesVersions().getWithResponse("azureBestPracticesProduction", "version1",
+            com.azure.core.util.Context.NONE);
     }
 }

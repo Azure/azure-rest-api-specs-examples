f6e6002b61ab3d7770cfc026f017ed84cb534d03
azure-sdk <azuresdk@microsoft.com>
2024-10-10 11:08:02 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-extendedlocation_1.0.0-beta.3

--- specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/preview/2021-08-31-preview/examples-java/CustomLocationsListOperations.java
+++ specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/preview/2021-08-31-preview/examples-java/CustomLocationsListOperations.java
@@ -1,17 +1,20 @@
-import com.azure.core.util.Context;
 
-/** Samples for CustomLocations ListOperations. */
+/**
+ * Samples for CustomLocations ListOperations.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/preview/2021-08-31-preview/examples/CustomLocationsListOperations.json
+     * x-ms-original-file:
+     * specification/extendedlocation/resource-manager/Microsoft.ExtendedLocation/preview/2021-08-31-preview/examples/
+     * CustomLocationsListOperations.json
      */
     /**
      * Sample code: List Custom Locations operations.
-     *
+     * 
      * @param manager Entry point to CustomLocationsManager.
      */
-    public static void listCustomLocationsOperations(
-        com.azure.resourcemanager.extendedlocation.CustomLocationsManager manager) {
-        manager.customLocations().listOperations(Context.NONE);
+    public static void
+        listCustomLocationsOperations(com.azure.resourcemanager.extendedlocation.CustomLocationsManager manager) {
+        manager.customLocations().listOperations(com.azure.core.util.Context.NONE);
     }
 }

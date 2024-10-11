c89057282027b5cce15961c977ba307798e52177
azure-sdk <azuresdk@microsoft.com>
2024-10-11 11:06:18 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-relay_1.0.0-beta.3

--- specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples-java/HybridConnection/RelayHybridConnectionAutorizationRuleGet.java
+++ specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples-java/HybridConnection/RelayHybridConnectionAutorizationRuleGet.java
@@ -1,23 +1,20 @@
-import com.azure.core.util.Context;
 
-/** Samples for HybridConnections GetAuthorizationRule. */
+/**
+ * Samples for HybridConnections GetAuthorizationRule.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples/HybridConnection/RelayHybridConnectionAutorizationRuleGet.json
+     * x-ms-original-file:
+     * specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples/HybridConnection/
+     * RelayHybridConnectionAutorizationRuleGet.json
      */
     /**
      * Sample code: RelayHybridConnectionAutorizationRuleGet.
-     *
+     * 
      * @param manager Entry point to RelayManager.
      */
     public static void relayHybridConnectionAutorizationRuleGet(com.azure.resourcemanager.relay.RelayManager manager) {
-        manager
-            .hybridConnections()
-            .getAuthorizationRuleWithResponse(
-                "resourcegroup",
-                "example-RelayNamespace-01",
-                "example-Relay-Hybrid-01",
-                "example-RelayAuthRules-01",
-                Context.NONE);
+        manager.hybridConnections().getAuthorizationRuleWithResponse("resourcegroup", "example-RelayNamespace-01",
+            "example-Relay-Hybrid-01", "example-RelayAuthRules-01", com.azure.core.util.Context.NONE);
     }
 }
--- specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples-java/NameSpaces/RelayNameSpaceDelete.java
+++ specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples-java/NameSpaces/RelayNameSpaceDelete.java
@@ -1,16 +1,19 @@
-import com.azure.core.util.Context;
 
-/** Samples for Namespaces Delete. */
+/**
+ * Samples for Namespaces Delete.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples/NameSpaces/RelayNameSpaceDelete.json
+     * x-ms-original-file:
+     * specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples/NameSpaces/RelayNameSpaceDelete.
+     * json
      */
     /**
      * Sample code: RelayNameSpaceDelete.
-     *
+     * 
      * @param manager Entry point to RelayManager.
      */
     public static void relayNameSpaceDelete(com.azure.resourcemanager.relay.RelayManager manager) {
-        manager.namespaces().delete("resourcegroup", "example-RelayNamespace-01", Context.NONE);
+        manager.namespaces().delete("resourcegroup", "example-RelayNamespace-01", com.azure.core.util.Context.NONE);
     }
 }

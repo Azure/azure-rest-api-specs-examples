56c57e72094914e57e091de75a548921f878357d
azure-sdk <azuresdk@microsoft.com>
2024-10-14 11:15:55 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-networkfunction_1.0.0-beta.3

--- specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-java/AzureTrafficCollectorDelete.java
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples-java/AzureTrafficCollectorDelete.java
@@ -1,17 +1,20 @@
-import com.azure.core.util.Context;
 
-/** Samples for AzureTrafficCollectors Delete. */
+/**
+ * Samples for AzureTrafficCollectors Delete.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/AzureTrafficCollectorDelete.json
+     * x-ms-original-file:
+     * specification/networkfunction/resource-manager/Microsoft.NetworkFunction/stable/2022-11-01/examples/
+     * AzureTrafficCollectorDelete.json
      */
     /**
      * Sample code: Delete Traffic Collector.
-     *
+     * 
      * @param manager Entry point to AzureTrafficCollectorManager.
      */
-    public static void deleteTrafficCollector(
-        com.azure.resourcemanager.networkfunction.AzureTrafficCollectorManager manager) {
-        manager.azureTrafficCollectors().delete("rg1", "atc", Context.NONE);
+    public static void
+        deleteTrafficCollector(com.azure.resourcemanager.networkfunction.AzureTrafficCollectorManager manager) {
+        manager.azureTrafficCollectors().delete("rg1", "atc", com.azure.core.util.Context.NONE);
     }
 }

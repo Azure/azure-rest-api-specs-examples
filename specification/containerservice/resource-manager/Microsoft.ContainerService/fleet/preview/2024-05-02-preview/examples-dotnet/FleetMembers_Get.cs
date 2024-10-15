9c2cb277653a751d9dded55dfb4a4b574d963d3f
azure-sdk <azuresdk@microsoft.com>
2024-10-15 11:16:36 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ContainerServiceFleet_1.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-05-02-preview/examples-dotnet/FleetMembers_Get.cs
@@ -0,0 +1,44 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ContainerServiceFleet;
+
+// Generated from example definition: specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2024-05-02-preview/examples/FleetMembers_Get.json
+// this example is just showing the usage of "FleetMembers_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ContainerServiceFleetResource created on azure
+// for more information of creating ContainerServiceFleetResource, please refer to the document of ContainerServiceFleetResource
+string subscriptionId = "subid1";
+string resourceGroupName = "rg1";
+string fleetName = "fleet1";
+ResourceIdentifier containerServiceFleetResourceId = ContainerServiceFleetResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, fleetName);
+ContainerServiceFleetResource containerServiceFleet = client.GetContainerServiceFleetResource(containerServiceFleetResourceId);
+
+// get the collection of this ContainerServiceFleetMemberResource
+ContainerServiceFleetMemberCollection collection = containerServiceFleet.GetContainerServiceFleetMembers();
+
+// invoke the operation
+string fleetMemberName = "member-1";
+NullableResponse<ContainerServiceFleetMemberResource> response = await collection.GetIfExistsAsync(fleetMemberName);
+ContainerServiceFleetMemberResource result = response.HasValue ? response.Value : null;
+
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ContainerServiceFleetMemberData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

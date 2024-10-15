6275c68c9b7f296b9eacac661cfcee79982186d5
azure-sdk <azuresdk@microsoft.com>
2024-10-15 11:09:08 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ContainerRegistry_1.3.0-beta.1

--- specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/stable/2022-12-01/examples-dotnet/PrivateEndpointConnectionGet.cs
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/stable/2022-12-01/examples-dotnet/PrivateEndpointConnectionGet.cs
@@ -15,20 +15,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ContainerRegistryPrivateEndpointConnectionResource created on azure
-// for more information of creating ContainerRegistryPrivateEndpointConnectionResource, please refer to the document of ContainerRegistryPrivateEndpointConnectionResource
+// this example assumes you already have this ContainerRegistryResource created on azure
+// for more information of creating ContainerRegistryResource, please refer to the document of ContainerRegistryResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
 string registryName = "myRegistry";
-string privateEndpointConnectionName = "myConnection";
-ResourceIdentifier containerRegistryPrivateEndpointConnectionResourceId = ContainerRegistryPrivateEndpointConnectionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName, privateEndpointConnectionName);
-ContainerRegistryPrivateEndpointConnectionResource containerRegistryPrivateEndpointConnection = client.GetContainerRegistryPrivateEndpointConnectionResource(containerRegistryPrivateEndpointConnectionResourceId);
+ResourceIdentifier containerRegistryResourceId = ContainerRegistryResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName);
+ContainerRegistryResource containerRegistry = client.GetContainerRegistryResource(containerRegistryResourceId);
+
+// get the collection of this ContainerRegistryPrivateEndpointConnectionResource
+ContainerRegistryPrivateEndpointConnectionCollection collection = containerRegistry.GetContainerRegistryPrivateEndpointConnections();
 
 // invoke the operation
-ContainerRegistryPrivateEndpointConnectionResource result = await containerRegistryPrivateEndpointConnection.GetAsync();
+string privateEndpointConnectionName = "myConnection";
+NullableResponse<ContainerRegistryPrivateEndpointConnectionResource> response = await collection.GetIfExistsAsync(privateEndpointConnectionName);
+ContainerRegistryPrivateEndpointConnectionResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ContainerRegistryPrivateEndpointConnectionData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine($"Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ContainerRegistryPrivateEndpointConnectionData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

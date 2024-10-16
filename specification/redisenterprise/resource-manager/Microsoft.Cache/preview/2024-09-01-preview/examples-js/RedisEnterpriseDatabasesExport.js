05b0a5d4577899cd748e50b8a1ff1db4fa895864
azure-sdk <azuresdk@microsoft.com>
2024-10-16 11:07:06 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-redisenterprisecache_3.1.0-beta.2

---
+++ specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2024-09-01-preview/examples-js/RedisEnterpriseDatabasesExport.js
@@ -0,0 +1,28 @@
+const { RedisEnterpriseManagementClient } = require("@azure/arm-redisenterprisecache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Exports a database file from target database.
+ *
+ * @summary Exports a database file from target database.
+ * x-ms-original-file: specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2024-09-01-preview/examples/RedisEnterpriseDatabasesExport.json
+ */
+async function redisEnterpriseDatabasesExport() {
+  const subscriptionId =
+    process.env["REDISENTERPRISE_SUBSCRIPTION_ID"] || "e7b5a9d2-6b6a-4d2f-9143-20d9a10f5b8f";
+  const resourceGroupName = process.env["REDISENTERPRISE_RESOURCE_GROUP"] || "rg1";
+  const clusterName = "cache1";
+  const databaseName = "default";
+  const parameters = {
+    sasUri: "https://contosostorage.blob.core.window.net/urlToBlobContainer?sasKeyParameters",
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new RedisEnterpriseManagementClient(credential, subscriptionId);
+  const result = await client.databases.beginExportAndWait(
+    resourceGroupName,
+    clusterName,
+    databaseName,
+    parameters,
+  );
+  console.log(result);
+}
---
+++ specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2024-09-01-preview/examples-js/RedisEnterpriseGetPrivateEndpointConnection.js
@@ -0,0 +1,24 @@
+const { RedisEnterpriseManagementClient } = require("@azure/arm-redisenterprisecache");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets the specified private endpoint connection associated with the Redis Enterprise cluster.
+ *
+ * @summary Gets the specified private endpoint connection associated with the Redis Enterprise cluster.
+ * x-ms-original-file: specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2024-09-01-preview/examples/RedisEnterpriseGetPrivateEndpointConnection.json
+ */
+async function redisEnterpriseGetPrivateEndpointConnection() {
+  const subscriptionId =
+    process.env["REDISENTERPRISE_SUBSCRIPTION_ID"] || "e7b5a9d2-6b6a-4d2f-9143-20d9a10f5b8f";
+  const resourceGroupName = process.env["REDISENTERPRISE_RESOURCE_GROUP"] || "rg1";
+  const clusterName = "cache1";
+  const privateEndpointConnectionName = "pectest01";
+  const credential = new DefaultAzureCredential();
+  const client = new RedisEnterpriseManagementClient(credential, subscriptionId);
+  const result = await client.privateEndpointConnections.get(
+    resourceGroupName,
+    clusterName,
+    privateEndpointConnectionName,
+  );
+  console.log(result);
+}

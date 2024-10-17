dde88c2f39f7b166a335fa79a6fed537422137d6
azure-sdk <azuresdk@microsoft.com>
2024-10-17 11:22:56 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-mixedreality_1.0.0-beta.3

--- specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-java/proxy/ExposingAvailableOperations.java
+++ specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-java/proxy/ExposingAvailableOperations.java
@@ -1,11 +1,16 @@
-/** Samples for Operations List. */
+
+/**
+ * Samples for Operations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples/proxy/ExposingAvailableOperations.json
+     * x-ms-original-file:
+     * specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples/proxy/
+     * ExposingAvailableOperations.json
      */
     /**
      * Sample code: List available operations.
-     *
+     * 
      * @param manager Entry point to MixedRealityManager.
      */
     public static void listAvailableOperations(com.azure.resourcemanager.mixedreality.MixedRealityManager manager) {
--- specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-java/spatial-anchors/ListKeys.java
+++ specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples-java/spatial-anchors/ListKeys.java
@@ -1,16 +1,20 @@
-/** Samples for SpatialAnchorsAccounts ListKeys. */
+
+/**
+ * Samples for SpatialAnchorsAccounts ListKeys.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples/spatial-anchors/ListKeys.json
+     * x-ms-original-file:
+     * specification/mixedreality/resource-manager/Microsoft.MixedReality/stable/2021-01-01/examples/spatial-anchors/
+     * ListKeys.json
      */
     /**
      * Sample code: List spatial anchor account key.
-     *
+     * 
      * @param manager Entry point to MixedRealityManager.
      */
     public static void listSpatialAnchorAccountKey(com.azure.resourcemanager.mixedreality.MixedRealityManager manager) {
-        manager
-            .spatialAnchorsAccounts()
-            .listKeysWithResponse("MyResourceGroup", "MyAccount", com.azure.core.util.Context.NONE);
+        manager.spatialAnchorsAccounts().listKeysWithResponse("MyResourceGroup", "MyAccount",
+            com.azure.core.util.Context.NONE);
     }
 }

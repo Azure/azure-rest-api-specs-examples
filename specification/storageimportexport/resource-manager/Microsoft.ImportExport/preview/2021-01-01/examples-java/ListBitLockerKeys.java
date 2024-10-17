7c807f5ba8f75f3919e3e5014d62cf9473dc5171
azure-sdk <azuresdk@microsoft.com>
2024-10-17 11:35:31 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-storageimportexport_1.0.0-beta.3

--- specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples-java/ListBitLockerKeys.java
+++ specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples-java/ListBitLockerKeys.java
@@ -1,11 +1,16 @@
-/** Samples for BitLockerKeys List. */
+
+/**
+ * Samples for BitLockerKeys List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples/ListBitLockerKeys.json
+     * x-ms-original-file:
+     * specification/storageimportexport/resource-manager/Microsoft.ImportExport/preview/2021-01-01/examples/
+     * ListBitLockerKeys.json
      */
     /**
      * Sample code: List BitLocker Keys for drives in a job.
-     *
+     * 
      * @param manager Entry point to StorageImportExportManager.
      */
     public static void listBitLockerKeysForDrivesInAJob(

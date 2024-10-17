c933596b188a1449d7455ad86db0b64e012f9e59
azure-sdk <azuresdk@microsoft.com>
2024-10-17 11:24:31 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-subscription_1.0.0-beta.3

--- specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples-java/listLocations.java
+++ specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples-java/listLocations.java
@@ -1,11 +1,15 @@
-/** Samples for Subscriptions ListLocations. */
+
+/**
+ * Samples for Subscriptions ListLocations.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples/listLocations.json
+     * x-ms-original-file:
+     * specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples/listLocations.json
      */
     /**
      * Sample code: listLocations.
-     *
+     * 
      * @param manager Entry point to SubscriptionManager.
      */
     public static void listLocations(com.azure.resourcemanager.subscription.SubscriptionManager manager) {
--- specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples-java/listTenants.java
+++ specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples-java/listTenants.java
@@ -1,11 +1,15 @@
-/** Samples for Tenants List. */
+
+/**
+ * Samples for Tenants List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples/listTenants.json
+     * x-ms-original-file:
+     * specification/subscription/resource-manager/Microsoft.Subscription/stable/2016-06-01/examples/listTenants.json
      */
     /**
      * Sample code: listTenants.
-     *
+     * 
      * @param manager Entry point to SubscriptionManager.
      */
     public static void listTenants(com.azure.resourcemanager.subscription.SubscriptionManager manager) {

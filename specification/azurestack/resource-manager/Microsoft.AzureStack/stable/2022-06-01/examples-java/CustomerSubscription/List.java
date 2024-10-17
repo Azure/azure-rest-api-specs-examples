b9ab0f85b941f2660bae499aa5123d1671fa22c0
azure-sdk <azuresdk@microsoft.com>
2024-10-17 11:29:14 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-azurestack_1.0.0-beta.3

--- specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples-java/CustomerSubscription/List.java
+++ specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples-java/CustomerSubscription/List.java
@@ -1,11 +1,16 @@
-/** Samples for CustomerSubscriptions List. */
+
+/**
+ * Samples for CustomerSubscriptions List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples/CustomerSubscription/List.json
+     * x-ms-original-file:
+     * specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples/CustomerSubscription/
+     * List.json
      */
     /**
      * Sample code: Returns a list of products.
-     *
+     * 
      * @param manager Entry point to AzureStackManager.
      */
     public static void returnsAListOfProducts(com.azure.resourcemanager.azurestack.AzureStackManager manager) {
--- specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples-java/Registration/ListBySubscription.java
+++ specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples-java/Registration/ListBySubscription.java
@@ -1,11 +1,16 @@
-/** Samples for Registrations List. */
+
+/**
+ * Samples for Registrations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples/Registration/ListBySubscription.json
+     * x-ms-original-file:
+     * specification/azurestack/resource-manager/Microsoft.AzureStack/stable/2022-06-01/examples/Registration/
+     * ListBySubscription.json
      */
     /**
      * Sample code: Returns a list of all registrations under current subscription.
-     *
+     * 
      * @param manager Entry point to AzureStackManager.
      */
     public static void returnsAListOfAllRegistrationsUnderCurrentSubscription(

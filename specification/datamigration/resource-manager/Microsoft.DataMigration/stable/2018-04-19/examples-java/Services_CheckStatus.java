4e0ec9ec16231620739a3efa2529cf7299099c1c
azure-sdk <azuresdk@microsoft.com>
2024-10-17 11:30:48 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-datamigration_1.0.0-beta.3

--- specification/datamigration/resource-manager/Microsoft.DataMigration/stable/2018-04-19/examples-java/Services_CheckStatus.java
+++ specification/datamigration/resource-manager/Microsoft.DataMigration/stable/2018-04-19/examples-java/Services_CheckStatus.java
@@ -1,11 +1,16 @@
-/** Samples for Services CheckStatus. */
+
+/**
+ * Samples for Services CheckStatus.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/datamigration/resource-manager/Microsoft.DataMigration/stable/2018-04-19/examples/Services_CheckStatus.json
+     * x-ms-original-file:
+     * specification/datamigration/resource-manager/Microsoft.DataMigration/stable/2018-04-19/examples/
+     * Services_CheckStatus.json
      */
     /**
      * Sample code: Services_CheckStatus.
-     *
+     * 
      * @param manager Entry point to DataMigrationManager.
      */
     public static void servicesCheckStatus(com.azure.resourcemanager.datamigration.DataMigrationManager manager) {

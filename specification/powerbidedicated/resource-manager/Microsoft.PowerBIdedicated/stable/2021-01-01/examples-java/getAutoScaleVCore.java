2459e2c90d16b66aad9446c816963b49cbe9f3b9
azure-sdk <azuresdk@microsoft.com>
2024-10-17 11:40:13 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-powerbidedicated_1.0.0-beta.3

--- specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-java/getAutoScaleVCore.java
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-java/getAutoScaleVCore.java
@@ -1,17 +1,21 @@
-/** Samples for AutoScaleVCores GetByResourceGroup. */
+
+/**
+ * Samples for AutoScaleVCores GetByResourceGroup.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/getAutoScaleVCore.json
+     * x-ms-original-file:
+     * specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/
+     * getAutoScaleVCore.json
      */
     /**
      * Sample code: Get details of an auto scale v-core.
-     *
+     * 
      * @param manager Entry point to PowerBIDedicatedManager.
      */
-    public static void getDetailsOfAnAutoScaleVCore(
-        com.azure.resourcemanager.powerbidedicated.PowerBIDedicatedManager manager) {
-        manager
-            .autoScaleVCores()
-            .getByResourceGroupWithResponse("TestRG", "testvcore", com.azure.core.util.Context.NONE);
+    public static void
+        getDetailsOfAnAutoScaleVCore(com.azure.resourcemanager.powerbidedicated.PowerBIDedicatedManager manager) {
+        manager.autoScaleVCores().getByResourceGroupWithResponse("TestRG", "testvcore",
+            com.azure.core.util.Context.NONE);
     }
 }
--- specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-java/operations.java
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples-java/operations.java
@@ -1,11 +1,16 @@
-/** Samples for Operations List. */
+
+/**
+ * Samples for Operations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/operations.json
+     * x-ms-original-file:
+     * specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/stable/2021-01-01/examples/operations.
+     * json
      */
     /**
      * Sample code: List operations.
-     *
+     * 
      * @param manager Entry point to PowerBIDedicatedManager.
      */
     public static void listOperations(com.azure.resourcemanager.powerbidedicated.PowerBIDedicatedManager manager) {

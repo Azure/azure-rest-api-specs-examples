d0ec6e96c100d652f6b9deb6b276582625d96fad
azure-sdk <azuresdk@microsoft.com>
2024-10-18 11:16:31 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-operationsmanagement_1.0.0-beta.3

--- specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples-java/SolutionGet.java
+++ specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples-java/SolutionGet.java
@@ -1,11 +1,16 @@
-/** Samples for Solutions GetByResourceGroup. */
+
+/**
+ * Samples for Solutions GetByResourceGroup.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/examples/SolutionGet.json
+     * x-ms-original-file:
+     * specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/preview/2015-11-01-preview/
+     * examples/SolutionGet.json
      */
     /**
      * Sample code: SolutionGet.
-     *
+     * 
      * @param manager Entry point to OperationsManagementManager.
      */
     public static void solutionGet(com.azure.resourcemanager.operationsmanagement.OperationsManagementManager manager) {

76b79de6fabfd27ea9d5cd9c2514f32459272ff4
azure-sdk <azuresdk@microsoft.com>
2024-10-18 11:14:54 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-datalakestore_1.0.0-beta.3

--- specification/datalake-store/resource-manager/Microsoft.DataLakeStore/stable/2016-11-01/examples-java/Accounts_Get.java
+++ specification/datalake-store/resource-manager/Microsoft.DataLakeStore/stable/2016-11-01/examples-java/Accounts_Get.java
@@ -1,15 +1,20 @@
-/** Samples for Accounts GetByResourceGroup. */
+
+/**
+ * Samples for Accounts GetByResourceGroup.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/datalake-store/resource-manager/Microsoft.DataLakeStore/stable/2016-11-01/examples/Accounts_Get.json
+     * x-ms-original-file:
+     * specification/datalake-store/resource-manager/Microsoft.DataLakeStore/stable/2016-11-01/examples/Accounts_Get.
+     * json
      */
     /**
      * Sample code: Gets the specified Data Lake Store account.
-     *
+     * 
      * @param manager Entry point to DataLakeStoreManager.
      */
-    public static void getsTheSpecifiedDataLakeStoreAccount(
-        com.azure.resourcemanager.datalakestore.DataLakeStoreManager manager) {
+    public static void
+        getsTheSpecifiedDataLakeStoreAccount(com.azure.resourcemanager.datalakestore.DataLakeStoreManager manager) {
         manager.accounts().getByResourceGroupWithResponse("contosorg", "contosoadla", com.azure.core.util.Context.NONE);
     }
 }

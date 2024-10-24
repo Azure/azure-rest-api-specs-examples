ed7c39937eb0b1b1f8cd5fe26df01b7d98021725
azure-sdk <azuresdk@microsoft.com>
2024-10-24 11:20:36 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Avs_1.4.0

---
+++ specification/vmware/resource-manager/Microsoft.AVS/stable/2023-09-01/examples-dotnet/IscsiPaths_Delete.cs
@@ -0,0 +1,28 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Avs;
+
+// Generated from example definition: specification/vmware/resource-manager/Microsoft.AVS/stable/2023-09-01/examples/IscsiPaths_Delete.json
+// this example is just showing the usage of "IscsiPaths_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this IscsiPathResource created on azure
+// for more information of creating IscsiPathResource, please refer to the document of IscsiPathResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "group1";
+string privateCloudName = "cloud1";
+ResourceIdentifier iscsiPathResourceId = IscsiPathResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, privateCloudName);
+IscsiPathResource iscsiPath = client.GetIscsiPathResource(iscsiPathResourceId);
+
+// invoke the operation
+await iscsiPath.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine($"Succeeded");

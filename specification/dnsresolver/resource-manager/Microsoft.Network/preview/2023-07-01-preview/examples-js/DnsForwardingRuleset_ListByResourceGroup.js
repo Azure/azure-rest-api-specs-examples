e6820dd3c6392dc38d3e1e7a13f130c39b5e0950
azure-sdk <azuresdk@microsoft.com>
2024-10-25 11:17:00 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dnsresolver_1.2.0-beta.1

---
+++ specification/dnsresolver/resource-manager/Microsoft.Network/preview/2023-07-01-preview/examples-js/DnsForwardingRuleset_ListByResourceGroup.js
@@ -0,0 +1,21 @@
+const { DnsResolverManagementClient } = require("@azure/arm-dnsresolver");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Lists DNS forwarding rulesets within a resource group.
+ *
+ * @summary Lists DNS forwarding rulesets within a resource group.
+ * x-ms-original-file: specification/dnsresolver/resource-manager/Microsoft.Network/preview/2023-07-01-preview/examples/DnsForwardingRuleset_ListByResourceGroup.json
+ */
+async function listDnsForwardingRulesetsByResourceGroup() {
+  const subscriptionId =
+    process.env["DNSRESOLVER_SUBSCRIPTION_ID"] || "abdd4249-9f34-4cc6-8e42-c2e32110603e";
+  const resourceGroupName = process.env["DNSRESOLVER_RESOURCE_GROUP"] || "sampleResourceGroup";
+  const credential = new DefaultAzureCredential();
+  const client = new DnsResolverManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.dnsForwardingRulesets.listByResourceGroup(resourceGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/dnsresolver/resource-manager/Microsoft.Network/preview/2023-07-01-preview/examples-js/DnsResolverPolicyVirtualNetworkLink_Patch.js
@@ -0,0 +1,28 @@
+const { DnsResolverManagementClient } = require("@azure/arm-dnsresolver");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Updates a DNS resolver policy virtual network link.
+ *
+ * @summary Updates a DNS resolver policy virtual network link.
+ * x-ms-original-file: specification/dnsresolver/resource-manager/Microsoft.Network/preview/2023-07-01-preview/examples/DnsResolverPolicyVirtualNetworkLink_Patch.json
+ */
+async function updateDnsResolverPolicyVirtualNetworkLink() {
+  const subscriptionId =
+    process.env["DNSRESOLVER_SUBSCRIPTION_ID"] || "abdd4249-9f34-4cc6-8e42-c2e32110603e";
+  const resourceGroupName = process.env["DNSRESOLVER_RESOURCE_GROUP"] || "sampleResourceGroup";
+  const dnsResolverPolicyName = "sampleDnsResolverPolicy";
+  const dnsResolverPolicyVirtualNetworkLinkName = "sampleVirtualNetworkLink";
+  const parameters = {
+    tags: { key1: "value1" },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new DnsResolverManagementClient(credential, subscriptionId);
+  const result = await client.dnsResolverPolicyVirtualNetworkLinks.beginUpdateAndWait(
+    resourceGroupName,
+    dnsResolverPolicyName,
+    dnsResolverPolicyVirtualNetworkLinkName,
+    parameters,
+  );
+  console.log(result);
+}

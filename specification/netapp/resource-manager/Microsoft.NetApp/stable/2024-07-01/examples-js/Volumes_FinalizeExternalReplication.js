e4b7f77b39826a7ef684f239a8db566c61a62512
azure-sdk <azuresdk@microsoft.com>
2024-10-25 11:20:58 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-netapp_21.3.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2024-07-01/examples-js/Volumes_FinalizeExternalReplication.js
@@ -0,0 +1,26 @@
+const { NetAppManagementClient } = require("@azure/arm-netapp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Finalizes the migration of an external volume by releasing the replication and breaking the external cluster peering if no other migration is active.
+ *
+ * @summary Finalizes the migration of an external volume by releasing the replication and breaking the external cluster peering if no other migration is active.
+ * x-ms-original-file: specification/netapp/resource-manager/Microsoft.NetApp/stable/2024-07-01/examples/Volumes_FinalizeExternalReplication.json
+ */
+async function volumesFinalizeExternalReplication() {
+  const subscriptionId =
+    process.env["NETAPP_SUBSCRIPTION_ID"] || "D633CC2E-722B-4AE1-B636-BBD9E4C60ED9";
+  const resourceGroupName = process.env["NETAPP_RESOURCE_GROUP"] || "myRG";
+  const accountName = "account1";
+  const poolName = "pool1";
+  const volumeName = "volume1";
+  const credential = new DefaultAzureCredential();
+  const client = new NetAppManagementClient(credential, subscriptionId);
+  const result = await client.volumes.beginFinalizeExternalReplicationAndWait(
+    resourceGroupName,
+    accountName,
+    poolName,
+    volumeName,
+  );
+  console.log(result);
+}

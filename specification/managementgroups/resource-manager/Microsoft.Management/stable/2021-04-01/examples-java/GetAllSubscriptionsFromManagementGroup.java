ec1dd596400a50af1fdb978164ceefa9888c4926
azure-sdk <azuresdk@microsoft.com>
2024-10-28 11:09:26 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-managementgroups_1.0.0-beta.2

--- specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples-java/GetAllSubscriptionsFromManagementGroup.java
+++ specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples-java/GetAllSubscriptionsFromManagementGroup.java
@@ -1,17 +1,21 @@
-/** Samples for ManagementGroupSubscriptions GetSubscriptionsUnderManagementGroup. */
+
+/**
+ * Samples for ManagementGroupSubscriptions GetSubscriptionsUnderManagementGroup.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/GetAllSubscriptionsFromManagementGroup.json
+     * x-ms-original-file:
+     * specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/
+     * GetAllSubscriptionsFromManagementGroup.json
      */
     /**
      * Sample code: GetAllSubscriptionsFromManagementGroup.
-     *
+     * 
      * @param manager Entry point to ManagementGroupsManager.
      */
     public static void getAllSubscriptionsFromManagementGroup(
         com.azure.resourcemanager.managementgroups.ManagementGroupsManager manager) {
-        manager
-            .managementGroupSubscriptions()
-            .getSubscriptionsUnderManagementGroup("Group", null, com.azure.core.util.Context.NONE);
+        manager.managementGroupSubscriptions().getSubscriptionsUnderManagementGroup("Group", null,
+            com.azure.core.util.Context.NONE);
     }
 }
--- specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples-java/GetEntities.java
+++ specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples-java/GetEntities.java
@@ -1,11 +1,15 @@
-/** Samples for Entities List. */
+
+/**
+ * Samples for Entities List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/GetEntities.json
+     * x-ms-original-file:
+     * specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/GetEntities.json
      */
     /**
      * Sample code: GetEntities.
-     *
+     * 
      * @param manager Entry point to ManagementGroupsManager.
      */
     public static void getEntities(com.azure.resourcemanager.managementgroups.ManagementGroupsManager manager) {

2d59a3da6260ff2dab025542f239c6999aae9ccc
azure-sdk <azuresdk@microsoft.com>
2024-10-28 11:22:57 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-appcontainers_3.2.0b1

---
+++ specification/app/resource-manager/Microsoft.App/preview/2024-08-02-preview/examples-python/ContainerAppsPatches_Apply.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appcontainers
+# USAGE
+    python container_apps_patches_apply.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerAppsAPIClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.container_apps_patches.begin_apply(
+        resource_group_name="rg",
+        container_app_name="test-app",
+        patch_name="testPatch-25fe4b",
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2024-08-02-preview/examples/ContainerAppsPatches_Apply.json
+if __name__ == "__main__":
+    main()
---
+++ specification/app/resource-manager/Microsoft.App/preview/2024-08-02-preview/examples-python/FunctionsExtension_Post.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appcontainers
+# USAGE
+    python functions_extension_post.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerAppsAPIClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="34adfa4f-cedf-4dc0-ba29-b6d1a69ab345",
+    )
+
+    response = client.functions_extension.invoke_functions_host(
+        resource_group_name="rg",
+        container_app_name="testcontainerApp0",
+        revision_name="testcontainerApp0-pjxhsye",
+        function_app_name="testcontainerApp0",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2024-08-02-preview/examples/FunctionsExtension_Post.json
+if __name__ == "__main__":
+    main()

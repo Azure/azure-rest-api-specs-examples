1fecab388f42cf088cedbcafc491c3f1af94a4fa
azure-sdk <azuresdk@microsoft.com>
2024-11-20 12:04:44 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-appconfiguration_1.0.0

---
+++ specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples-java/ConfigurationStoresGetKeyValue.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for KeyValues Get.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples/
+     * ConfigurationStoresGetKeyValue.json
+     */
+    /**
+     * Sample code: KeyValues_Get.
+     * 
+     * @param manager Entry point to AppConfigurationManager.
+     */
+    public static void keyValuesGet(com.azure.resourcemanager.appconfiguration.AppConfigurationManager manager) {
+        manager.keyValues().getWithResponse("myResourceGroup", "contoso", "myKey$myLabel",
+            com.azure.core.util.Context.NONE);
+    }
+}

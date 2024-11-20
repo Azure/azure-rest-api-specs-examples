3a546bb39793ff14641f78f6096280db8a2baa26
azure-sdk <azuresdk@microsoft.com>
2024-11-20 12:08:09 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/appservice/armappservice/v4.1.0

---
+++ specification/web/resource-manager/Microsoft.DomainRegistration/stable/2024-04-01/examples-go/RenewDomain.go
@@ -0,0 +1,26 @@
+package armappservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appservice/armappservice/v4"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/b5d78da207e9c5d8f82e95224039867271f47cdf/specification/web/resource-manager/Microsoft.DomainRegistration/stable/2024-04-01/examples/RenewDomain.json
+func ExampleDomainsClient_Renew() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armappservice.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	_, err = clientFactory.NewDomainsClient().Renew(ctx, "RG", "example.com", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+}

91a58756ebc839a408acf7e93c7d8a362a20659e
azure-sdk <azuresdk@microsoft.com>
2024-11-20 12:09:19 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/devopsinfrastructure/armdevopsinfrastructure/v1.0.0

---
+++ specification/devopsinfrastructure/resource-manager/Microsoft.DevOpsInfrastructure/stable/2024-10-19/examples-go/DeletePool.go
@@ -0,0 +1,30 @@
+package armdevopsinfrastructure_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/devopsinfrastructure/armdevopsinfrastructure"
+)
+
+// Generated from example definition: 2024-10-19/DeletePool.json
+func ExamplePoolsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armdevopsinfrastructure.NewClientFactory("a2e95d27-c161-4b61-bda4-11512c14c2c2", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewPoolsClient().BeginDelete(ctx, "rg", "pool", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

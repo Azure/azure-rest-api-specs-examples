4b84ea73ecc915a4174b232428e4c0d217250fb6
azure-sdk <azuresdk@microsoft.com>
2024-11-21 12:20:29 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-appconfiguration_4.1.0

---
+++ specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples-js/ConfigurationStoresCreateSnapshot.js
@@ -0,0 +1,29 @@
+const { AppConfigurationManagementClient } = require("@azure/arm-appconfiguration");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Creates a snapshot. NOTE: This operation is intended for use in Azure Resource Manager (ARM) Template deployments. For all other scenarios involving App Configuration snapshots the data plane API should be used instead.
+ *
+ * @summary Creates a snapshot. NOTE: This operation is intended for use in Azure Resource Manager (ARM) Template deployments. For all other scenarios involving App Configuration snapshots the data plane API should be used instead.
+ * x-ms-original-file: specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples/ConfigurationStoresCreateSnapshot.json
+ */
+async function snapshotsCreate() {
+  const subscriptionId =
+    process.env["APPCONFIGURATION_SUBSCRIPTION_ID"] || "c80fb759-c965-4c6a-9110-9b2b2d038882";
+  const resourceGroupName = process.env["APPCONFIGURATION_RESOURCE_GROUP"] || "myResourceGroup";
+  const configStoreName = "contoso";
+  const snapshotName = "mySnapshot";
+  const body = {
+    filters: [{ key: "app1/*", label: "Production" }],
+    retentionPeriod: 3600,
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new AppConfigurationManagementClient(credential, subscriptionId);
+  const result = await client.snapshots.beginCreateAndWait(
+    resourceGroupName,
+    configStoreName,
+    snapshotName,
+    body,
+  );
+  console.log(result);
+}

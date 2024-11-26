294e323ab224ccd9d52c129c6994a128c8a702ee
azure-sdk <azuresdk@microsoft.com>
2024-11-26 12:18:22 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-terraform_1.0.0b1

---
+++ specification/terraform/resource-manager/Microsoft.AzureTerraform/preview/2023-07-01-preview/examples-python/ExportTerraform.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.terraform import TerraformMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-terraform
+# USAGE
+    python export_terraform.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TerraformMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.terraform.begin_export_terraform(
+        body={"resourceGroupName": "rg1", "type": "ExportResourceGroup"},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2023-07-01-preview/ExportTerraform.json
+if __name__ == "__main__":
+    main()

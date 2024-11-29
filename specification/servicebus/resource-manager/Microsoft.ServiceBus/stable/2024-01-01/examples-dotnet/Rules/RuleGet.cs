fd88aa647365024b8f00b2b03154611c3f13496f
azure-sdk <azuresdk@microsoft.com>
2024-11-29 12:09:48 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceBus_1.1.0

---
+++ specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2024-01-01/examples-dotnet/Rules/RuleGet.cs
@@ -0,0 +1,47 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ServiceBus.Models;
+using Azure.ResourceManager.ServiceBus;
+
+// Generated from example definition: specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2024-01-01/examples/Rules/RuleGet.json
+// this example is just showing the usage of "Rules_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ServiceBusSubscriptionResource created on azure
+// for more information of creating ServiceBusSubscriptionResource, please refer to the document of ServiceBusSubscriptionResource
+string subscriptionId = "5f750a97-50d9-4e36-8081-c9ee4c0210d4";
+string resourceGroupName = "ArunMonocle";
+string namespaceName = "sdk-Namespace-1319";
+string topicName = "sdk-Topics-2081";
+string subscriptionName = "sdk-Subscriptions-8691";
+ResourceIdentifier serviceBusSubscriptionResourceId = ServiceBusSubscriptionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName, topicName, subscriptionName);
+ServiceBusSubscriptionResource serviceBusSubscription = client.GetServiceBusSubscriptionResource(serviceBusSubscriptionResourceId);
+
+// get the collection of this ServiceBusRuleResource
+ServiceBusRuleCollection collection = serviceBusSubscription.GetServiceBusRules();
+
+// invoke the operation
+string ruleName = "sdk-Rules-6571";
+NullableResponse<ServiceBusRuleResource> response = await collection.GetIfExistsAsync(ruleName);
+ServiceBusRuleResource result = response.HasValue ? response.Value : null;
+
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ServiceBusRuleData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
---
+++ specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2024-01-01/examples-dotnet/disasterRecoveryConfigs/SBAliasAuthorizationRuleGet.cs
@@ -0,0 +1,35 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ServiceBus.Models;
+using Azure.ResourceManager.ServiceBus;
+
+// Generated from example definition: specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2024-01-01/examples/disasterRecoveryConfigs/SBAliasAuthorizationRuleGet.json
+// this example is just showing the usage of "DisasterRecoveryAuthorizationRules_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ServiceBusDisasterRecoveryAuthorizationRuleResource created on azure
+// for more information of creating ServiceBusDisasterRecoveryAuthorizationRuleResource, please refer to the document of ServiceBusDisasterRecoveryAuthorizationRuleResource
+string subscriptionId = "exampleSubscriptionId";
+string resourceGroupName = "exampleResourceGroup";
+string namespaceName = "sdk-Namespace-9080";
+string @alias = "sdk-DisasterRecovery-4879";
+string authorizationRuleName = "sdk-Authrules-4879";
+ResourceIdentifier serviceBusDisasterRecoveryAuthorizationRuleResourceId = ServiceBusDisasterRecoveryAuthorizationRuleResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName, @alias, authorizationRuleName);
+ServiceBusDisasterRecoveryAuthorizationRuleResource serviceBusDisasterRecoveryAuthorizationRule = client.GetServiceBusDisasterRecoveryAuthorizationRuleResource(serviceBusDisasterRecoveryAuthorizationRuleResourceId);
+
+// invoke the operation
+ServiceBusDisasterRecoveryAuthorizationRuleResource result = await serviceBusDisasterRecoveryAuthorizationRule.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ServiceBusAuthorizationRuleData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

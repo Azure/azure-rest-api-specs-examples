51b51fe71ed3c8e2a38ef646acfc8bea9e188d6a
azure-sdk <azuresdk@microsoft.com>
2024-12-04 12:13:51 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-confidentialledger_1.0.0-beta.5

---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-06-28-preview/examples-java/ConfidentialLedger_List.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for Ledger ListByResourceGroup.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-06-28-preview/
+     * examples/ConfidentialLedger_List.json
+     */
+    /**
+     * Sample code: ConfidentialLedgerList.
+     * 
+     * @param manager Entry point to ConfidentialLedgerManager.
+     */
+    public static void
+        confidentialLedgerList(com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
+        manager.ledgers().listByResourceGroup("DummyResourceGroupName", null, com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-06-28-preview/examples-java/ManagedCCF_Restore.java
@@ -0,0 +1,25 @@
+
+import com.azure.resourcemanager.confidentialledger.models.ManagedCcfRestore;
+
+/**
+ * Samples for ManagedCcf Restore.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2023-06-28-preview/
+     * examples/ManagedCCF_Restore.json
+     */
+    /**
+     * Sample code: ManagedCCFRestore.
+     * 
+     * @param manager Entry point to ConfidentialLedgerManager.
+     */
+    public static void
+        managedCCFRestore(com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
+        manager.managedCcfs().restore(
+            "DummyResourceGroupName", "DummyMccfAppName", new ManagedCcfRestore()
+                .withFileShareName("DummyFileShareName").withRestoreRegion("EastUS").withUri("DummySASUri"),
+            com.azure.core.util.Context.NONE);
+    }
+}

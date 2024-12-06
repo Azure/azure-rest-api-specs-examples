e1c281c6dbf7670d82b0c61054d28d38f84d340e
azure-sdk <azuresdk@microsoft.com>
2024-12-06 12:05:20 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-changeanalysis_1.1.0

--- specification/changeanalysis/resource-manager/Microsoft.ChangeAnalysis/stable/2021-04-01/examples-java/OperationsList.java
+++ specification/changeanalysis/resource-manager/Microsoft.ChangeAnalysis/stable/2021-04-01/examples-java/OperationsList.java
@@ -1,11 +1,16 @@
-/** Samples for Operations List. */
+
+/**
+ * Samples for Operations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/changeanalysis/resource-manager/Microsoft.ChangeAnalysis/stable/2021-04-01/examples/OperationsList.json
+     * x-ms-original-file:
+     * specification/changeanalysis/resource-manager/Microsoft.ChangeAnalysis/stable/2021-04-01/examples/OperationsList.
+     * json
      */
     /**
      * Sample code: Operations_List.
-     *
+     * 
      * @param manager Entry point to AzureChangeAnalysisManager.
      */
     public static void operationsList(com.azure.resourcemanager.changeanalysis.AzureChangeAnalysisManager manager) {

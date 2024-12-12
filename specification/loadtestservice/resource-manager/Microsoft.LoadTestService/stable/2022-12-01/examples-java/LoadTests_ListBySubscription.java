e736a4fcdeccb07373e5b27bae5086a34cf71734
azure-sdk <azuresdk@microsoft.com>
2024-12-12 12:05:46 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-loadtesting_1.1.0

--- specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-java/LoadTests_ListBySubscription.java
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-java/LoadTests_ListBySubscription.java
@@ -1,14 +1,20 @@
-/** Samples for LoadTests List. */
+
+/**
+ * Samples for LoadTests List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples/LoadTests_ListBySubscription.json
+     * x-ms-original-file:
+     * specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples/
+     * LoadTests_ListBySubscription.json
      */
     /**
-     * Sample code: LoadTests_ListBySubscription.
-     *
+     * Sample code: List LoadTestResource resources by subscription ID.
+     * 
      * @param manager Entry point to LoadTestManager.
      */
-    public static void loadTestsListBySubscription(com.azure.resourcemanager.loadtesting.LoadTestManager manager) {
+    public static void
+        listLoadTestResourceResourcesBySubscriptionID(com.azure.resourcemanager.loadtesting.LoadTestManager manager) {
         manager.loadTests().list(com.azure.core.util.Context.NONE);
     }
 }
--- specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-java/Operations_List.java
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples-java/Operations_List.java
@@ -1,14 +1,19 @@
-/** Samples for Operations List. */
+
+/**
+ * Samples for Operations List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples/Operations_List.json
+     * x-ms-original-file:
+     * specification/loadtestservice/resource-manager/Microsoft.LoadTestService/stable/2022-12-01/examples/
+     * Operations_List.json
      */
     /**
-     * Sample code: Operations_List.
-     *
+     * Sample code: List the operations for the provider.
+     * 
      * @param manager Entry point to LoadTestManager.
      */
-    public static void operationsList(com.azure.resourcemanager.loadtesting.LoadTestManager manager) {
+    public static void listTheOperationsForTheProvider(com.azure.resourcemanager.loadtesting.LoadTestManager manager) {
         manager.operations().list(com.azure.core.util.Context.NONE);
     }
 }

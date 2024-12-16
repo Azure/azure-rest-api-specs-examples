16c5e889d97f4421f817bfce5496d43770b9b249
azure-sdk <azuresdk@microsoft.com>
2024-12-16 12:08:37 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-servicefabricmanagedclusters_1.0.0-beta.3

---
+++ specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/preview/2024-09-01-preview/examples-java/ApplicationTypeVersionDeleteOperation_example.java
@@ -0,0 +1,21 @@
+
+/**
+ * Samples for ApplicationTypeVersions Delete.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/preview/2024-09-01-preview/
+     * examples/ApplicationTypeVersionDeleteOperation_example.json
+     */
+    /**
+     * Sample code: Delete an application type version.
+     * 
+     * @param manager Entry point to ServiceFabricManagedClustersManager.
+     */
+    public static void deleteAnApplicationTypeVersion(
+        com.azure.resourcemanager.servicefabricmanagedclusters.ServiceFabricManagedClustersManager manager) {
+        manager.applicationTypeVersions().delete("resRg", "myCluster", "myAppType", "1.0",
+            com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/preview/2024-09-01-preview/examples-java/ManagedClusterListBySubscriptionOperation_example.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for ManagedClusters List.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/preview/2024-09-01-preview/
+     * examples/ManagedClusterListBySubscriptionOperation_example.json
+     */
+    /**
+     * Sample code: List managed clusters.
+     * 
+     * @param manager Entry point to ServiceFabricManagedClustersManager.
+     */
+    public static void listManagedClusters(
+        com.azure.resourcemanager.servicefabricmanagedclusters.ServiceFabricManagedClustersManager manager) {
+        manager.managedClusters().list(com.azure.core.util.Context.NONE);
+    }
+}

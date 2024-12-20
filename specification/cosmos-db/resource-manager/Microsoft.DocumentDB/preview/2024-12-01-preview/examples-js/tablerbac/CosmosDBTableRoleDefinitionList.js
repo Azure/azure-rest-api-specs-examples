6432d2ea9870e20afe6cabacd47b350606b1e37c
azure-sdk <azuresdk@microsoft.com>
2024-12-20 12:20:53 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cosmosdb_16.3.0-beta.1

---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-12-01-preview/examples-js/tablerbac/CosmosDBTableRoleDefinitionList.js
@@ -0,0 +1,26 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Retrieves the list of all Azure Cosmos DB Table Role Definitions.
+ *
+ * @summary Retrieves the list of all Azure Cosmos DB Table Role Definitions.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-12-01-preview/examples/tablerbac/CosmosDBTableRoleDefinitionList.json
+ */
+async function cosmosDbTableRoleDefinitionList() {
+  const subscriptionId =
+    process.env["COSMOSDB_SUBSCRIPTION_ID"] || "ffffffff-ffff-ffff-ffff-ffffffffffff";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "myResourceGroupName";
+  const accountName = "myAccountName";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.tableResources.listTableRoleDefinitions(
+    resourceGroupName,
+    accountName,
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-12-01-preview/examples-js/throughputPool/CosmosDBThroughputPoolList.js
@@ -0,0 +1,22 @@
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to List all the ThroughputPools in a given resource group.
+ *
+ * @summary List all the ThroughputPools in a given resource group.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/preview/2024-12-01-preview/examples/throughputPool/CosmosDBThroughputPoolList.json
+ */
+async function cosmosDbThroughputPoolListByResourceGroup() {
+  const subscriptionId =
+    process.env["COSMOSDB_SUBSCRIPTION_ID"] || "ffffffff-ffff-ffff-ffff-ffffffffffff";
+  const resourceGroupName = process.env["COSMOSDB_RESOURCE_GROUP"] || "rgName";
+  const credential = new DefaultAzureCredential();
+  const client = new CosmosDBManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.throughputPools.listByResourceGroup(resourceGroupName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

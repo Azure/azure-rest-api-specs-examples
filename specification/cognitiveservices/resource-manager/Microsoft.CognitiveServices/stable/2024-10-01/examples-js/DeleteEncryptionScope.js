706463be58ab68d59ee0998a02bfe14076169e8d
azure-sdk <azuresdk@microsoft.com>
2024-12-20 12:14:11 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-cognitiveservices_7.6.0

---
+++ specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2024-10-01/examples-js/DeleteEncryptionScope.js
@@ -0,0 +1,24 @@
+const { CognitiveServicesManagementClient } = require("@azure/arm-cognitiveservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Deletes the specified encryptionScope associated with the Cognitive Services account.
+ *
+ * @summary Deletes the specified encryptionScope associated with the Cognitive Services account.
+ * x-ms-original-file: specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2024-10-01/examples/DeleteEncryptionScope.json
+ */
+async function deleteEncryptionScope() {
+  const subscriptionId =
+    process.env["COGNITIVESERVICES_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["COGNITIVESERVICES_RESOURCE_GROUP"] || "resourceGroupName";
+  const accountName = "accountName";
+  const encryptionScopeName = "encryptionScopeName";
+  const credential = new DefaultAzureCredential();
+  const client = new CognitiveServicesManagementClient(credential, subscriptionId);
+  const result = await client.encryptionScopes.beginDeleteAndWait(
+    resourceGroupName,
+    accountName,
+    encryptionScopeName,
+  );
+  console.log(result);
+}
---
+++ specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2024-10-01/examples-js/ListNetworkSecurityPerimeterConfigurations.js
@@ -0,0 +1,25 @@
+const { CognitiveServicesManagementClient } = require("@azure/arm-cognitiveservices");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to Gets a list of NSP configurations for an account.
+ *
+ * @summary Gets a list of NSP configurations for an account.
+ * x-ms-original-file: specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2024-10-01/examples/ListNetworkSecurityPerimeterConfigurations.json
+ */
+async function listNetworkSecurityPerimeterConfigurations() {
+  const subscriptionId =
+    process.env["COGNITIVESERVICES_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["COGNITIVESERVICES_RESOURCE_GROUP"] || "resourceGroupName";
+  const accountName = "accountName";
+  const credential = new DefaultAzureCredential();
+  const client = new CognitiveServicesManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.networkSecurityPerimeterConfigurations.list(
+    resourceGroupName,
+    accountName,
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

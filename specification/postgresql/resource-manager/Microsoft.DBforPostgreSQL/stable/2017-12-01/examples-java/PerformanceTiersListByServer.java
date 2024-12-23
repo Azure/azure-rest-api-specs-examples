b742ef4d3fd8c5eb5bf6b177ff01c9d42728395d
azure-sdk <azuresdk@microsoft.com>
2024-12-23 12:05:00 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-postgresql_1.1.0

--- specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples-java/PerformanceTiersListByServer.java
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples-java/PerformanceTiersListByServer.java
@@ -1,16 +1,19 @@
-import com.azure.core.util.Context;
 
-/** Samples for ServerBasedPerformanceTier List. */
+/**
+ * Samples for ServerBasedPerformanceTier List.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/PerformanceTiersListByServer.json
+     * x-ms-original-file:
+     * specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/
+     * PerformanceTiersListByServer.json
      */
     /**
      * Sample code: PerformanceTiersList.
-     *
+     * 
      * @param manager Entry point to PostgreSqlManager.
      */
     public static void performanceTiersList(com.azure.resourcemanager.postgresql.PostgreSqlManager manager) {
-        manager.serverBasedPerformanceTiers().list("TestGroup", "testserver", Context.NONE);
+        manager.serverBasedPerformanceTiers().list("TestGroup", "testserver", com.azure.core.util.Context.NONE);
     }
 }
--- specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples-java/ServerAdminGet.java
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples-java/ServerAdminGet.java
@@ -1,16 +1,19 @@
-import com.azure.core.util.Context;
 
-/** Samples for ServerAdministrators Get. */
+/**
+ * Samples for ServerAdministrators Get.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/ServerAdminGet.json
+     * x-ms-original-file:
+     * specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/ServerAdminGet.
+     * json
      */
     /**
      * Sample code: ServerAdministratorGet.
-     *
+     * 
      * @param manager Entry point to PostgreSqlManager.
      */
     public static void serverAdministratorGet(com.azure.resourcemanager.postgresql.PostgreSqlManager manager) {
-        manager.serverAdministrators().getWithResponse("testrg", "pgtestsvc4", Context.NONE);
+        manager.serverAdministrators().getWithResponse("testrg", "pgtestsvc4", com.azure.core.util.Context.NONE);
     }
 }

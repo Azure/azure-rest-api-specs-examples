38bb23e71fbcfeb0c3fe67a849e9c35e43c5989b
azure-sdk <azuresdk@microsoft.com>
2024-12-24 12:29:23 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-deviceregistry_1.0.0-beta.2

---
+++ specification/deviceregistry/resource-manager/Microsoft.DeviceRegistry/preview/2024-09-01-preview/examples-js/Delete_DiscoveredAsset.js
@@ -0,0 +1,15 @@
+const { DeviceRegistryManagementClient } = require("@azure/arm-deviceregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a DiscoveredAsset
+ *
+ * @summary delete a DiscoveredAsset
+ * x-ms-original-file: 2024-09-01-preview/Delete_DiscoveredAsset.json
+ */
+async function deleteDiscoveredAsset() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DeviceRegistryManagementClient(credential, subscriptionId);
+  await client.discoveredAssets.delete("myResourceGroup", "my-discoveredasset");
+}
---
+++ specification/deviceregistry/resource-manager/Microsoft.DeviceRegistry/preview/2024-09-01-preview/examples-js/Get_AssetEndpointProfile.js
@@ -0,0 +1,19 @@
+const { DeviceRegistryManagementClient } = require("@azure/arm-deviceregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a AssetEndpointProfile
+ *
+ * @summary get a AssetEndpointProfile
+ * x-ms-original-file: 2024-09-01-preview/Get_AssetEndpointProfile.json
+ */
+async function getAssetEndpointProfile() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DeviceRegistryManagementClient(credential, subscriptionId);
+  const result = await client.assetEndpointProfiles.get(
+    "myResourceGroup",
+    "my-assetendpointprofile",
+  );
+  console.log(result);
+}

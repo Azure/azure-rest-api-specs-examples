edc89004a27aa617f004d00dd3bd6763ae905dc7
azure-sdk <azuresdk@microsoft.com>
2024-12-26 12:12:04 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-confidentialledger_1.0.0

--- specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples-java/CheckNameAvailability.java
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples-java/CheckNameAvailability.java
@@ -1,24 +1,24 @@
-import com.azure.core.util.Context;
+
 import com.azure.resourcemanager.confidentialledger.models.CheckNameAvailabilityRequest;
 
-/** Samples for ResourceProvider CheckNameAvailability. */
+/**
+ * Samples for ResourceProvider CheckNameAvailability.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples/CheckNameAvailability.json
+     * x-ms-original-file:
+     * specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples/
+     * CheckNameAvailability.json
      */
     /**
      * Sample code: CheckNameAvailability.
-     *
+     * 
      * @param manager Entry point to ConfidentialLedgerManager.
      */
-    public static void checkNameAvailability(
-        com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
-        manager
-            .resourceProviders()
-            .checkNameAvailabilityWithResponse(
-                new CheckNameAvailabilityRequest()
-                    .withName("sample-name")
-                    .withType("Microsoft.ConfidentialLedger/ledgers"),
-                Context.NONE);
+    public static void
+        checkNameAvailability(com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
+        manager.resourceProviders().checkNameAvailabilityWithResponse(
+            new CheckNameAvailabilityRequest().withName("sample-name").withType("Microsoft.ConfidentialLedger/ledgers"),
+            com.azure.core.util.Context.NONE);
     }
 }
--- specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples-java/ConfidentialLedger_Delete.java
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples-java/ConfidentialLedger_Delete.java
@@ -1,17 +1,20 @@
-import com.azure.core.util.Context;
 
-/** Samples for Ledger Delete. */
+/**
+ * Samples for Ledger Delete.
+ */
 public final class Main {
     /*
-     * x-ms-original-file: specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples/ConfidentialLedger_Delete.json
+     * x-ms-original-file:
+     * specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/stable/2022-05-13/examples/
+     * ConfidentialLedger_Delete.json
      */
     /**
      * Sample code: ConfidentialLedgerDelete.
-     *
+     * 
      * @param manager Entry point to ConfidentialLedgerManager.
      */
-    public static void confidentialLedgerDelete(
-        com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
-        manager.ledgers().delete("DummyResourceGroupName", "DummyLedgerName", Context.NONE);
+    public static void
+        confidentialLedgerDelete(com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
+        manager.ledgers().delete("DummyResourceGroupName", "DummyLedgerName", com.azure.core.util.Context.NONE);
     }
 }

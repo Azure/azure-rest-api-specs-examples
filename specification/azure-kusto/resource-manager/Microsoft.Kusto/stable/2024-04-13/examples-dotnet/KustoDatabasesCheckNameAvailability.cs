7eb4d4f55889022c4eb891fdddbf36a20178cd7d
azure-sdk <azuresdk@microsoft.com>
2025-01-22 12:13:26 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Kusto_1.6.0

---
+++ specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2024-04-13/examples-dotnet/KustoDatabasesCheckNameAvailability.cs
@@ -0,0 +1,30 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Kusto.Models;
+using Azure.ResourceManager.Kusto;
+
+// Generated from example definition: specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2024-04-13/examples/KustoDatabasesCheckNameAvailability.json
+// this example is just showing the usage of "Databases_CheckNameAvailability" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this KustoClusterResource created on azure
+// for more information of creating KustoClusterResource, please refer to the document of KustoClusterResource
+string subscriptionId = "12345678-1234-1234-1234-123456789098";
+string resourceGroupName = "kustorptest";
+string clusterName = "kustoCluster";
+ResourceIdentifier kustoClusterResourceId = KustoClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, clusterName);
+KustoClusterResource kustoCluster = client.GetKustoClusterResource(kustoClusterResourceId);
+
+// invoke the operation
+KustoDatabaseNameAvailabilityContent content = new KustoDatabaseNameAvailabilityContent("database1", KustoDatabaseResourceType.MicrosoftKustoClustersDatabases);
+KustoNameAvailabilityResult result = await kustoCluster.CheckKustoDatabaseNameAvailabilityAsync(content);
+
+Console.WriteLine($"Succeeded: {result}");

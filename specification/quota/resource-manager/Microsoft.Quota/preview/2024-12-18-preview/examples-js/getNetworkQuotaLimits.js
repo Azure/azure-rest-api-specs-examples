feae51c73dc33d99826db1876fad65912191c264
azure-sdk <azuresdk@microsoft.com>
2025-01-26 12:08:40 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-quota_1.1.0-beta.2

---
+++ specification/quota/resource-manager/Microsoft.Quota/preview/2024-12-18-preview/examples-js/getNetworkQuotaLimits.js
@@ -0,0 +1,21 @@
+const { AzureQuotaExtensionAPI } = require("@azure/arm-quota");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Get a list of current quota limits of all resources for the specified scope. The response from this GET operation can be leveraged to submit requests to update a quota.
+ *
+ * @summary Get a list of current quota limits of all resources for the specified scope. The response from this GET operation can be leveraged to submit requests to update a quota.
+ * x-ms-original-file: specification/quota/resource-manager/Microsoft.Quota/preview/2024-12-18-preview/examples/getNetworkQuotaLimits.json
+ */
+async function quotasListQuotaLimitsForNetwork() {
+  const scope =
+    "subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Network/locations/eastus";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureQuotaExtensionAPI(credential);
+  const resArray = new Array();
+  for await (let item of client.quota.list(scope)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/quota/resource-manager/Microsoft.Quota/preview/2024-12-18-preview/examples-js/getNetworkUsages.js
@@ -0,0 +1,21 @@
+const { AzureQuotaExtensionAPI } = require("@azure/arm-quota");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Get a list of current usage for all resources for the scope specified.
+ *
+ * @summary Get a list of current usage for all resources for the scope specified.
+ * x-ms-original-file: specification/quota/resource-manager/Microsoft.Quota/preview/2024-12-18-preview/examples/getNetworkUsages.json
+ */
+async function quotasListUsagesForNetwork() {
+  const scope =
+    "subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Network/locations/eastus";
+  const credential = new DefaultAzureCredential();
+  const client = new AzureQuotaExtensionAPI(credential);
+  const resArray = new Array();
+  for await (let item of client.usages.list(scope)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

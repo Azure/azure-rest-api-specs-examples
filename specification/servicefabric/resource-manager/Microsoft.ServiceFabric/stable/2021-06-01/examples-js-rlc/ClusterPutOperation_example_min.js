a31a0e3368453448f18f60c583722c1ade906b1b
azure-sdk <azuresdk@microsoft.com>
2025-02-11 12:07:46 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure-rest/arm-servicefabric_1.0.0-beta.2

--- specification/servicefabric/resource-manager/Microsoft.ServiceFabric/stable/2021-06-01/examples-js-rlc/ClusterPutOperation_example_min.js
+++ specification/servicefabric/resource-manager/Microsoft.ServiceFabric/stable/2021-06-01/examples-js-rlc/ClusterPutOperation_example_min.js
@@ -59,7 +59,7 @@ async function putAClusterWithMinimumParameters() {
       clusterName
     )
     .put(parameters);
-  const poller = getLongRunningPoller(client, initialResponse);
+  const poller = await getLongRunningPoller(client, initialResponse);
   const result = await poller.pollUntilDone();
   console.log(result);
 }

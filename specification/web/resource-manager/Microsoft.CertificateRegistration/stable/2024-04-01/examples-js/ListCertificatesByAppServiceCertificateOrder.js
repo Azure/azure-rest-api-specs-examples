1f6dc550a9d52004e3b1832dcc496ea4b174ea98
azure-sdk <azuresdk@microsoft.com>
2025-02-13 12:05:49 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-appservice_16.0.0

---
+++ specification/web/resource-manager/Microsoft.CertificateRegistration/stable/2024-04-01/examples-js/ListCertificatesByAppServiceCertificateOrder.js
@@ -0,0 +1,26 @@
+const { WebSiteManagementClient } = require("@azure/arm-appservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Description for List all certificates associated with a certificate order.
+ *
+ * @summary Description for List all certificates associated with a certificate order.
+ * x-ms-original-file: specification/web/resource-manager/Microsoft.CertificateRegistration/stable/2024-04-01/examples/ListCertificatesByAppServiceCertificateOrder.json
+ */
+async function listCertificatesByAppServiceCertificate() {
+  const subscriptionId =
+    process.env["APPSERVICE_SUBSCRIPTION_ID"] || "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
+  const resourceGroupName = process.env["APPSERVICE_RESOURCE_GROUP"] || "testrg123";
+  const certificateOrderName = "SampleCertificateOrderName";
+  const credential = new DefaultAzureCredential();
+  const client = new WebSiteManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.appServiceCertificateOrders.listCertificates(
+    resourceGroupName,
+    certificateOrderName,
+  )) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/web/resource-manager/Microsoft.Web/stable/2024-04-01/examples-js/ListCustomHostNameSites.js
@@ -0,0 +1,21 @@
+const { WebSiteManagementClient } = require("@azure/arm-appservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Get custom hostnames under this subscription
+ *
+ * @summary Get custom hostnames under this subscription
+ * x-ms-original-file: specification/web/resource-manager/Microsoft.Web/stable/2024-04-01/examples/ListCustomHostNameSites.json
+ */
+async function getCustomHostnamesUnderSubscription() {
+  const subscriptionId =
+    process.env["APPSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new WebSiteManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.listCustomHostNameSites()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

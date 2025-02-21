1be7ee75c78569479860ca7aea1b0e05b5b43509
azure-sdk <azuresdk@microsoft.com>
2025-02-21 12:08:42 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-networkcloud_1.1.0

---
+++ specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2024-07-01/examples-java/Clusters_Patch_SecretArchive.java
@@ -0,0 +1,43 @@
+
+import com.azure.resourcemanager.networkcloud.models.Cluster;
+import com.azure.resourcemanager.networkcloud.models.ClusterSecretArchive;
+import com.azure.resourcemanager.networkcloud.models.ClusterSecretArchiveEnabled;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Samples for Clusters Update.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2024-07-01/examples/
+     * Clusters_Patch_SecretArchive.json
+     */
+    /**
+     * Sample code: Patch secret archive.
+     * 
+     * @param manager Entry point to NetworkCloudManager.
+     */
+    public static void patchSecretArchive(com.azure.resourcemanager.networkcloud.NetworkCloudManager manager) {
+        Cluster resource = manager.clusters()
+            .getByResourceGroupWithResponse("resourceGroupName", "clusterName", com.azure.core.util.Context.NONE)
+            .getValue();
+        resource.update().withTags(mapOf("key1", "fakeTokenPlaceholder", "key2", "fakeTokenPlaceholder"))
+            .withSecretArchive(new ClusterSecretArchive().withKeyVaultId("fakeTokenPlaceholder")
+                .withUseKeyVault(ClusterSecretArchiveEnabled.TRUE))
+            .apply();
+    }
+
+    // Use "Map.of" if available
+    @SuppressWarnings("unchecked")
+    private static <T> Map<String, T> mapOf(Object... inputs) {
+        Map<String, T> map = new HashMap<>();
+        for (int i = 0; i < inputs.length; i += 2) {
+            String key = (String) inputs[i];
+            T value = (T) inputs[i + 1];
+            map.put(key, value);
+        }
+        return map;
+    }
+}
---
+++ specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2024-07-01/examples-java/StorageAppliances_Delete.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for StorageAppliances Delete.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2024-07-01/examples/
+     * StorageAppliances_Delete.json
+     */
+    /**
+     * Sample code: Delete storage appliance.
+     * 
+     * @param manager Entry point to NetworkCloudManager.
+     */
+    public static void deleteStorageAppliance(com.azure.resourcemanager.networkcloud.NetworkCloudManager manager) {
+        manager.storageAppliances().delete("resourceGroupName", "storageApplianceName",
+            com.azure.core.util.Context.NONE);
+    }
+}

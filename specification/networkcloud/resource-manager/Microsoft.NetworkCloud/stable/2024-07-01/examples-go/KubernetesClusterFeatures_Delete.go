cf7d8c4a9df7e041cf0fc1bec78ff5de33b16ea9
azure-sdk <azuresdk@microsoft.com>
2025-02-24 12:11:54 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/networkcloud/armnetworkcloud/v1.2.0

---
+++ specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2024-07-01/examples-go/KubernetesClusterFeatures_Delete.go
@@ -0,0 +1,30 @@
+package armnetworkcloud_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/networkcloud/armnetworkcloud"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/c0a12a75b702054cf1e7fcd8c014d0fc116dea6e/specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2024-07-01/examples/KubernetesClusterFeatures_Delete.json
+func ExampleKubernetesClusterFeaturesClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armnetworkcloud.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewKubernetesClusterFeaturesClient().BeginDelete(ctx, "resourceGroupName", "kubernetesClusterName", "featureName", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

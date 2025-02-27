b1761744830e1ed1076995240a04ef1a3023eb15
azure-sdk <azuresdk@microsoft.com>
2025-02-27 12:18:17 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-impactreporting_1.0.0b1

---
+++ specification/impact/resource-manager/Microsoft.Impact/preview/2024-05-01-preview/examples-python/ImpactCategories_Get.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.impactreporting import ImpactReportingMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-impactreporting
+# USAGE
+    python impact_categories_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ImpactReportingMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.impact_categories.get(
+        impact_category_name="ARMOperation.Create",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2024-05-01-preview/ImpactCategories_Get.json
+if __name__ == "__main__":
+    main()
---
+++ specification/impact/resource-manager/Microsoft.Impact/preview/2024-05-01-preview/examples-python/WorkloadImpact_Get.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.impactreporting import ImpactReportingMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-impactreporting
+# USAGE
+    python workload_impact_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ImpactReportingMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.workload_impacts.get(
+        workload_impact_name="impact-001",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2024-05-01-preview/WorkloadImpact_Get.json
+if __name__ == "__main__":
+    main()

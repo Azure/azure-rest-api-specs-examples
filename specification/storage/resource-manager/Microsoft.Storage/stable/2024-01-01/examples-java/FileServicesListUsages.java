e62e9c702740604fa48fc2d806dd31f2b7391e0d
azure-sdk <azuresdk@microsoft.com>
2025-02-28 12:08:23 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager_2.48.0

---
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples-java/FileServicesListUsages.java
@@ -0,0 +1,19 @@
+
+/**
+ * Samples for FileServices ListServiceUsages.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples/FileServicesListUsages.json
+     */
+    /**
+     * Sample code: ListFileServiceUsages.
+     * 
+     * @param azure The entry point for accessing resource management APIs in Azure.
+     */
+    public static void listFileServiceUsages(com.azure.resourcemanager.AzureResourceManager azure) {
+        azure.storageAccounts().manager().serviceClient().getFileServices().listServiceUsages("res4410", "sto8607",
+            null, com.azure.core.util.Context.NONE);
+    }
+}

7bf696fb25106e82e0edecb0473221c3cc1b168a
azure-sdk <azuresdk@microsoft.com>
2025-03-11 11:26:48 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceGraph_1.1.0-beta.3

--- specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples-dotnet/ResourcesHistoryGet.cs
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples-dotnet/ResourcesHistoryGet.cs
@@ -1,10 +1,12 @@
+using Azure;
+using Azure.ResourceManager;
 using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager;
-using Azure.ResourceManager.ResourceGraph;
 using Azure.ResourceManager.ResourceGraph.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ResourceGraph;
 
 // Generated from example definition: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples/ResourcesHistoryGet.json
 // this example is just showing the usage of "ResourcesHistory" operation, for the dependent resources, they will have to be created separately.
@@ -14,19 +16,14 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this TenantResource created on azure
-// for more information of creating TenantResource, please refer to the document of TenantResource
-var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
 
 // invoke the operation
-ResourcesHistoryContent content = new ResourcesHistoryContent()
+ResourcesHistoryContent content = new ResourcesHistoryContent
 {
-    Subscriptions =
-    {
-    "a7f33fdb-e646-4f15-89aa-3a360210861e"
-    },
+    Subscriptions = { "a7f33fdb-e646-4f15-89aa-3a360210861e" },
     Query = "where name =~ 'cpu-utilization' | project id, name, properties",
-    Options = new ResourcesHistoryRequestOptions()
+    Options = new ResourcesHistoryRequestOptions
     {
         Interval = new DateTimeInterval(DateTimeOffset.Parse("2020-11-12T01:00:00.0000000Z"), DateTimeOffset.Parse("2020-11-12T01:25:00.0000000Z")),
     },
--- specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples-dotnet/ResourcesMgBasicQuery.cs
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples-dotnet/ResourcesMgBasicQuery.cs
@@ -1,10 +1,12 @@
+using Azure;
+using Azure.ResourceManager;
 using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager;
-using Azure.ResourceManager.ResourceGraph;
 using Azure.ResourceManager.ResourceGraph.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ResourceGraph;
 
 // Generated from example definition: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/preview/2021-06-01-preview/examples/ResourcesMgBasicQuery.json
 // this example is just showing the usage of "Resources" operation, for the dependent resources, they will have to be created separately.
@@ -14,17 +16,12 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this TenantResource created on azure
-// for more information of creating TenantResource, please refer to the document of TenantResource
-var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
 
 // invoke the operation
 ResourceQueryContent content = new ResourceQueryContent("Resources | project id, name, type, location, tags | limit 3")
 {
-    ManagementGroups =
-    {
-    "e927f598-c1d4-4f72-8541-95d83a6a4ac8","ProductionMG"
-    },
+    ManagementGroups = { "e927f598-c1d4-4f72-8541-95d83a6a4ac8", "ProductionMG" },
 };
 ResourceQueryResult result = await tenantResource.GetResourcesAsync(content);
 

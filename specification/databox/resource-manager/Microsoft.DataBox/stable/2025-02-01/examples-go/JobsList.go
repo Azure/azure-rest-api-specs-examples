3d44267ba3d30027e0001d94db37f97a1c34c94b
azure-sdk <azuresdk@microsoft.com>
2025-03-20 11:04:57 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/databox/armdatabox/v2.2.0

---
+++ specification/databox/resource-manager/Microsoft.DataBox/stable/2025-02-01/examples-go/JobsList.go
@@ -0,0 +1,97 @@
+package armdatabox_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/databox/armdatabox/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/4cecf11996f2bfc7958f6d0de814badab23f9720/specification/databox/resource-manager/Microsoft.DataBox/stable/2025-02-01/examples/JobsList.json
+func ExampleJobsClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armdatabox.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewJobsClient().NewListPager(&armdatabox.JobsClientListOptions{SkipToken: nil})
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page.JobResourceList = armdatabox.JobResourceList{
+		// 	Value: []*armdatabox.JobResource{
+		// 		{
+		// 			Identity: &armdatabox.ResourceIdentity{
+		// 				Type: to.Ptr("None"),
+		// 			},
+		// 			Location: to.Ptr("australiaeast"),
+		// 			SKU: &armdatabox.SKU{
+		// 				Name: to.Ptr(armdatabox.SKUNameDataBoxDisk),
+		// 				Model: to.Ptr(armdatabox.ModelNameDataBoxDisk),
+		// 			},
+		// 			Tags: map[string]*string{
+		// 			},
+		// 			Name: to.Ptr("TestJobName1"),
+		// 			Type: to.Ptr("Microsoft.DataBox/jobs"),
+		// 			ID: to.Ptr("/subscriptions/YourSubscriptionId/resourcegroups/TestResourceGroupName/providers/Microsoft.DataBox/jobs/TestJobName1"),
+		// 			Properties: &armdatabox.JobProperties{
+		// 				CancellationReason: to.Ptr("Old job which is still in ordered state cancelled by the service"),
+		// 				DeliveryInfo: &armdatabox.JobDeliveryInfo{
+		// 					ScheduledDateTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "1-01-01T00:00:00.000Z"); return t}()),
+		// 				},
+		// 				DeliveryType: to.Ptr(armdatabox.JobDeliveryTypeNonScheduled),
+		// 				IsCancellable: to.Ptr(false),
+		// 				IsCancellableWithoutFee: to.Ptr(false),
+		// 				IsDeletable: to.Ptr(true),
+		// 				IsPrepareToShipEnabled: to.Ptr(false),
+		// 				IsShippingAddressEditable: to.Ptr(false),
+		// 				StartTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2018-08-30T09:14:39.774Z"); return t}()),
+		// 				Status: to.Ptr(armdatabox.StageNameCancelled),
+		// 				TransferType: to.Ptr(armdatabox.TransferTypeImportToAzure),
+		// 			},
+		// 		},
+		// 		{
+		// 			Identity: &armdatabox.ResourceIdentity{
+		// 				Type: to.Ptr("None"),
+		// 			},
+		// 			Location: to.Ptr("australiaeast"),
+		// 			SKU: &armdatabox.SKU{
+		// 				Name: to.Ptr(armdatabox.SKUNameDataBoxDisk),
+		// 				Model: to.Ptr(armdatabox.ModelNameDataBoxDisk),
+		// 			},
+		// 			Tags: map[string]*string{
+		// 			},
+		// 			Name: to.Ptr("TestJobName2"),
+		// 			Type: to.Ptr("Microsoft.DataBox/jobs"),
+		// 			ID: to.Ptr("/subscriptions/YourSubscriptionId/resourcegroups/TestResourceGroupName/providers/Microsoft.DataBox/jobs/TestJobName2"),
+		// 			Properties: &armdatabox.JobProperties{
+		// 				CancellationReason: to.Ptr("Old job which is still in ordered state cancelled by the service"),
+		// 				DeliveryInfo: &armdatabox.JobDeliveryInfo{
+		// 					ScheduledDateTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "1-01-01T00:00:00.000Z"); return t}()),
+		// 				},
+		// 				DeliveryType: to.Ptr(armdatabox.JobDeliveryTypeNonScheduled),
+		// 				IsCancellable: to.Ptr(false),
+		// 				IsCancellableWithoutFee: to.Ptr(false),
+		// 				IsDeletable: to.Ptr(true),
+		// 				IsPrepareToShipEnabled: to.Ptr(false),
+		// 				IsShippingAddressEditable: to.Ptr(false),
+		// 				StartTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2018-09-10T12:33:22.257Z"); return t}()),
+		// 				Status: to.Ptr(armdatabox.StageNameCancelled),
+		// 				TransferType: to.Ptr(armdatabox.TransferTypeImportToAzure),
+		// 			},
+		// 	}},
+		// }
+	}
+}
---
+++ specification/databox/resource-manager/Microsoft.DataBox/stable/2025-02-01/examples-go/RegionConfiguration.go
@@ -0,0 +1,72 @@
+package armdatabox_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/databox/armdatabox/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/4cecf11996f2bfc7958f6d0de814badab23f9720/specification/databox/resource-manager/Microsoft.DataBox/stable/2025-02-01/examples/RegionConfiguration.json
+func ExampleServiceClient_RegionConfiguration() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armdatabox.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewServiceClient().RegionConfiguration(ctx, "westus", armdatabox.RegionConfigurationRequest{
+		DeviceCapabilityRequest: &armdatabox.DeviceCapabilityRequest{
+			Model:   to.Ptr(armdatabox.ModelNameDataBoxDisk),
+			SKUName: to.Ptr(armdatabox.SKUNameDataBoxDisk),
+		},
+		ScheduleAvailabilityRequest: &armdatabox.ScheduleAvailabilityRequest{
+			Model:           to.Ptr(armdatabox.ModelNameDataBox),
+			SKUName:         to.Ptr(armdatabox.SKUNameDataBox),
+			StorageLocation: to.Ptr("westus"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.RegionConfigurationResponse = armdatabox.RegionConfigurationResponse{
+	// 	DeviceCapabilityResponse: &armdatabox.DeviceCapabilityResponse{
+	// 		DeviceCapabilityDetails: []*armdatabox.DeviceCapabilityDetails{
+	// 			{
+	// 				HardwareEncryption: to.Ptr(armdatabox.HardwareEncryptionEnabled),
+	// 		}},
+	// 	},
+	// 	ScheduleAvailabilityResponse: &armdatabox.ScheduleAvailabilityResponse{
+	// 		AvailableDates: []*time.Time{
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-11T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-12T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-13T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-14T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-15T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-16T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-17T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-18T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-19T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-20T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-21T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-22T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-23T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-24T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-25T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-26T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-27T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-28T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-29T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-30T00:00:00.000Z"); return t}()),
+	// 			to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2019-07-31T00:00:00.000Z"); return t}())},
+	// 		},
+	// 	}
+}

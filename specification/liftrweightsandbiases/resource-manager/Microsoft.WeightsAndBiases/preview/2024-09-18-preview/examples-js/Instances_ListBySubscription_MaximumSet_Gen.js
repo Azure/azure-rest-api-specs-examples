74a88d7a0acb2b13510028b370ccabbee23a4adb
azure-sdk <azuresdk@microsoft.com>
2025-03-28 11:28:09 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-weightsandbiases_1.0.0-beta.1

---
+++ specification/liftrweightsandbiases/resource-manager/Microsoft.WeightsAndBiases/preview/2024-09-18-preview/examples-js/Instances_ListBySubscription_MaximumSet_Gen.js
@@ -0,0 +1,20 @@
+const { WeightsAndBiasesClient } = require("@azure/arm-weightsandbiases");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list InstanceResource resources by subscription ID
+ *
+ * @summary list InstanceResource resources by subscription ID
+ * x-ms-original-file: 2024-09-18-preview/Instances_ListBySubscription_MaximumSet_Gen.json
+ */
+async function instancesListBySubscriptionGeneratedByMaximumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "0BCB047F-CB71-4DFE-B0BD-88519F411C2F";
+  const client = new WeightsAndBiasesClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.instances.listBySubscription()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

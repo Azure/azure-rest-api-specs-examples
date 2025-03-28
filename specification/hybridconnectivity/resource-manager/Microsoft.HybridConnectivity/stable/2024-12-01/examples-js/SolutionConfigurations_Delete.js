6e1dcab7ad895b51304e422387111281e794c623
azure-sdk <azuresdk@microsoft.com>
2025-03-28 11:17:32 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-hybridconnectivity_2.0.0-beta.1

---
+++ specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/stable/2024-12-01/examples-js/SolutionConfigurations_Delete.js
@@ -0,0 +1,15 @@
+const { HybridConnectivityManagementAPI } = require("@azure/arm-hybridconnectivity");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a SolutionConfiguration
+ *
+ * @summary delete a SolutionConfiguration
+ * x-ms-original-file: 2024-12-01/SolutionConfigurations_Delete.json
+ */
+async function solutionConfigurationsDelete() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-00000000000";
+  const client = new HybridConnectivityManagementAPI(credential, subscriptionId);
+  await client.solutionConfigurations.delete("ymuj", "stu");
+}
---
+++ specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/stable/2024-12-01/examples-js/SolutionConfigurations_Update.js
@@ -0,0 +1,18 @@
+const { HybridConnectivityManagementAPI } = require("@azure/arm-hybridconnectivity");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to update a SolutionConfiguration
+ *
+ * @summary update a SolutionConfiguration
+ * x-ms-original-file: 2024-12-01/SolutionConfigurations_Update.json
+ */
+async function solutionConfigurationsUpdate() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-00000000000";
+  const client = new HybridConnectivityManagementAPI(credential, subscriptionId);
+  const result = await client.solutionConfigurations.update("ymuj", "dxt", {
+    properties: { solutionType: "myzljlstvmgkp", solutionSettings: {} },
+  });
+  console.log(result);
+}

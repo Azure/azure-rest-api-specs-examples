2a5fbc1a47e529c00b9737ca7f443696a681df37
azure-sdk <azuresdk@microsoft.com>
2025-03-28 11:16:13 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-agricultureplatform_1.0.0-beta.1

---
+++ specification/agricultureplatform/resource-manager/Microsoft.AgriculturePlatform/preview/2024-06-01-preview/examples-js/AgriService_Get_MaximumSet_Gen.js
@@ -0,0 +1,16 @@
+const { AgriculturePlatformClient } = require("@azure/arm-agricultureplatform");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a AgriServiceResource
+ *
+ * @summary get a AgriServiceResource
+ * x-ms-original-file: 2024-06-01-preview/AgriService_Get_MaximumSet_Gen.json
+ */
+async function agriServiceGet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "83D293F5-DEFD-4D48-B120-1DC713BE338A";
+  const client = new AgriculturePlatformClient(credential, subscriptionId);
+  const result = await client.agriService.get("rgopenapi", "abc123");
+  console.log(result);
+}
---
+++ specification/agricultureplatform/resource-manager/Microsoft.AgriculturePlatform/preview/2024-06-01-preview/examples-js/Operations_List_MaximumSet_Gen.js
@@ -0,0 +1,20 @@
+const { AgriculturePlatformClient } = require("@azure/arm-agricultureplatform");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2024-06-01-preview/Operations_List_MaximumSet_Gen.json
+ */
+async function operationsList() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-00000000000";
+  const client = new AgriculturePlatformClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

846e0a4142681f63d843e515ff0f5a3528d9ada3
azure-sdk <azuresdk@microsoft.com>
2025-04-08 11:05:23 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-search_3.3.0-beta.2

---
+++ specification/search/resource-manager/Microsoft.Search/preview/2025-02-01-preview/examples-js/SearchListServicesBySubscription.js
@@ -0,0 +1,20 @@
+const { SearchManagementClient } = require("@azure/arm-search");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Gets a list of all Search services in the given subscription.
+ *
+ * @summary Gets a list of all Search services in the given subscription.
+ * x-ms-original-file: specification/search/resource-manager/Microsoft.Search/preview/2025-02-01-preview/examples/SearchListServicesBySubscription.json
+ */
+async function searchListServicesBySubscription() {
+  const subscriptionId = process.env["SEARCH_SUBSCRIPTION_ID"] || "subid";
+  const credential = new DefaultAzureCredential();
+  const client = new SearchManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (let item of client.services.listBySubscription()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}

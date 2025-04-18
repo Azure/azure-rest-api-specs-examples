8486c46e726bf3e3644b195f86e1263ec0d77d87
azure-sdk <azuresdk@microsoft.com>
2025-04-18 11:15:15 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ContainerRegistry_1.3.0-beta.3

---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2025-03-01-preview/examples-dotnet/ArchiveGet.cs
@@ -0,0 +1,35 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ContainerRegistry.Models;
+using Azure.ResourceManager.ContainerRegistry;
+
+// Generated from example definition: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2025-03-01-preview/examples/ArchiveGet.json
+// this example is just showing the usage of "Archives_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ContainerRegistryArchiveResource created on azure
+// for more information of creating ContainerRegistryArchiveResource, please refer to the document of ContainerRegistryArchiveResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string registryName = "myRegistry";
+string packageType = "myPackageType";
+string archiveName = "myArchiveName";
+ResourceIdentifier containerRegistryArchiveResourceId = ContainerRegistryArchiveResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName, packageType, archiveName);
+ContainerRegistryArchiveResource containerRegistryArchive = client.GetContainerRegistryArchiveResource(containerRegistryArchiveResourceId);
+
+// invoke the operation
+ContainerRegistryArchiveResource result = await containerRegistryArchive.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ContainerRegistryArchiveData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2025-03-01-preview/examples-dotnet/CacheRuleUpdate.cs
@@ -0,0 +1,39 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ContainerRegistry.Models;
+using Azure.ResourceManager.ContainerRegistry;
+
+// Generated from example definition: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2025-03-01-preview/examples/CacheRuleUpdate.json
+// this example is just showing the usage of "CacheRules_Update" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ContainerRegistryCacheRuleResource created on azure
+// for more information of creating ContainerRegistryCacheRuleResource, please refer to the document of ContainerRegistryCacheRuleResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "myResourceGroup";
+string registryName = "myRegistry";
+string cacheRuleName = "myCacheRule";
+ResourceIdentifier containerRegistryCacheRuleResourceId = ContainerRegistryCacheRuleResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName, cacheRuleName);
+ContainerRegistryCacheRuleResource containerRegistryCacheRule = client.GetContainerRegistryCacheRuleResource(containerRegistryCacheRuleResourceId);
+
+// invoke the operation
+ContainerRegistryCacheRulePatch patch = new ContainerRegistryCacheRulePatch
+{
+    CredentialSetResourceId = new ResourceIdentifier("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry/credentialSets/myCredentialSet2"),
+};
+ArmOperation<ContainerRegistryCacheRuleResource> lro = await containerRegistryCacheRule.UpdateAsync(WaitUntil.Completed, patch);
+ContainerRegistryCacheRuleResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ContainerRegistryCacheRuleData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

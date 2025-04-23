513db1447bff9c0d358b958b583c13da6462056d
azure-sdk <azuresdk@microsoft.com>
2025-04-23 11:12:53 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Storage_1.4.1

--- specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples-dotnet/StorageAccountGetProperties.cs
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples-dotnet/StorageAccountGetProperties.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager.Models;
-using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.Resources.Models;
 using Azure.ResourceManager.Storage.Models;
 using Azure.ResourceManager.Storage;
 
@@ -18,30 +16,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this StorageAccountResource created on azure
+// for more information of creating StorageAccountResource, please refer to the document of StorageAccountResource
 string subscriptionId = "{subscription-id}";
 string resourceGroupName = "res9407";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this StorageAccountResource
-StorageAccountCollection collection = resourceGroupResource.GetStorageAccounts();
+string accountName = "sto8596";
+ResourceIdentifier storageAccountResourceId = StorageAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
+StorageAccountResource storageAccount = client.GetStorageAccountResource(storageAccountResourceId);
 
 // invoke the operation
-string accountName = "sto8596";
-NullableResponse<StorageAccountResource> response = await collection.GetIfExistsAsync(accountName);
-StorageAccountResource result = response.HasValue ? response.Value : null;
+StorageAccountResource result = await storageAccount.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    StorageAccountData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+StorageAccountData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

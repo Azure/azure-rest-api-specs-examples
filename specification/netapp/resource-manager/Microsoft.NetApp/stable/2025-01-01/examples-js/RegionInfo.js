7d85bd5a87c72add723a39df9882e308f02db90b
azure-sdk <azuresdk@microsoft.com>
2025-04-24 06:09:40 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-netapp_21.5.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/stable/2025-01-01/examples-js/RegionInfo.js
@@ -0,0 +1,19 @@
+const { NetAppManagementClient } = require("@azure/arm-netapp");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Provides storage to network proximity and logical zone mapping information.
+ *
+ * @summary Provides storage to network proximity and logical zone mapping information.
+ * x-ms-original-file: specification/netapp/resource-manager/Microsoft.NetApp/stable/2025-01-01/examples/RegionInfo.json
+ */
+async function regionInfoQuery() {
+  const subscriptionId =
+    process.env["NETAPP_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const location = "eastus";
+  const credential = new DefaultAzureCredential();
+  const client = new NetAppManagementClient(credential, subscriptionId);
+  const result = await client.netAppResource.queryRegionInfo(location);
+  console.log(result);
+}

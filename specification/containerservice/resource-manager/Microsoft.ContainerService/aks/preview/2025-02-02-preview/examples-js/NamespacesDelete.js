2905fd90b1dd47ac77e186c3a887edf27bff2e05
azure-sdk <azuresdk@microsoft.com>
2025-04-25 11:13:27 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_22.0.0-beta.2

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2025-02-02-preview/examples-js/NamespacesDelete.js
@@ -0,0 +1,25 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Deletes a namespace.
+ *
+ * @summary Deletes a namespace.
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2025-02-02-preview/examples/NamespacesDelete.json
+ */
+async function deleteNamespace() {
+  const subscriptionId =
+    process.env["CONTAINERSERVICE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["CONTAINERSERVICE_RESOURCE_GROUP"] || "rg1";
+  const resourceName = "clustername1";
+  const namespaceName = "namespace1";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.namespaces.beginDeleteAndWait(
+    resourceGroupName,
+    resourceName,
+    namespaceName,
+  );
+  console.log(result);
+}

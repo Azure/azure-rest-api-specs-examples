d5e520b9002bebf635856f06d26312fd6882d9be
azure-sdk <azuresdk@microsoft.com>
2025-04-25 11:18:49 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-appcontainers_2.2.0

---
+++ specification/app/resource-manager/Microsoft.App/stable/2025-01-01/examples-js/JavaComponents_Get_ServiceBind.js
@@ -0,0 +1,21 @@
+const { ContainerAppsAPIClient } = require("@azure/arm-appcontainers");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Get a Java Component.
+ *
+ * @summary Get a Java Component.
+ * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2025-01-01/examples/JavaComponents_Get_ServiceBind.json
+ */
+async function getJavaComponentWithServiceBinds() {
+  const subscriptionId =
+    process.env["APPCONTAINERS_SUBSCRIPTION_ID"] || "8efdecc5-919e-44eb-b179-915dca89ebf9";
+  const resourceGroupName = process.env["APPCONTAINERS_RESOURCE_GROUP"] || "examplerg";
+  const environmentName = "myenvironment";
+  const name = "myjavacomponent";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerAppsAPIClient(credential, subscriptionId);
+  const result = await client.javaComponents.get(resourceGroupName, environmentName, name);
+  console.log(result);
+}

bb13e2244ed1dbe2164087d1ae8e8b48581cf8f3
azure-sdk <azuresdk@microsoft.com>
2025-04-25 11:32:25 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.DurableTask_1.0.0-beta.1

---
+++ specification/durabletask/resource-manager/Microsoft.DurableTask/preview/2025-04-01-preview/examples-dotnet/Schedulers_Get.cs
@@ -0,0 +1,33 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.DurableTask.Models;
+using Azure.ResourceManager.DurableTask;
+
+// Generated from example definition: 2025-04-01-preview/Schedulers_Get.json
+// this example is just showing the usage of "Scheduler_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this DurableTaskSchedulerResource created on azure
+// for more information of creating DurableTaskSchedulerResource, please refer to the document of DurableTaskSchedulerResource
+string subscriptionId = "EE9BD735-67CE-4A90-89C4-439D3F6A4C93";
+string resourceGroupName = "rgopenapi";
+string schedulerName = "testscheduler";
+ResourceIdentifier durableTaskSchedulerResourceId = DurableTaskSchedulerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, schedulerName);
+DurableTaskSchedulerResource durableTaskScheduler = client.GetDurableTaskSchedulerResource(durableTaskSchedulerResourceId);
+
+// invoke the operation
+DurableTaskSchedulerResource result = await durableTaskScheduler.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DurableTaskSchedulerData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

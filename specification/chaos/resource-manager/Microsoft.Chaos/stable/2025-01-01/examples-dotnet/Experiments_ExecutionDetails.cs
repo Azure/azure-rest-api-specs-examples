76c8a789a6d04b3e00e87ba4d6249f8ef55e8363
azure-sdk <azuresdk@microsoft.com>
2025-05-01 11:15:02 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Chaos_1.1.0-beta.3

---
+++ specification/chaos/resource-manager/Microsoft.Chaos/stable/2025-01-01/examples-dotnet/Experiments_ExecutionDetails.cs
@@ -0,0 +1,30 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Chaos.Models;
+using Azure.ResourceManager.Chaos;
+
+// Generated from example definition: 2025-01-01/Experiments_ExecutionDetails.json
+// this example is just showing the usage of "ExperimentExecutions_ExecutionDetails" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ChaosExperimentExecutionResource created on azure
+// for more information of creating ChaosExperimentExecutionResource, please refer to the document of ChaosExperimentExecutionResource
+string subscriptionId = "6b052e15-03d3-4f17-b2e1-be7f07588291";
+string resourceGroupName = "exampleRG";
+string experimentName = "exampleExperiment";
+string executionId = "f24500ad-744e-4a26-864b-b76199eac333";
+ResourceIdentifier chaosExperimentExecutionResourceId = ChaosExperimentExecutionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, experimentName, executionId);
+ChaosExperimentExecutionResource chaosExperimentExecution = client.GetChaosExperimentExecutionResource(chaosExperimentExecutionResourceId);
+
+// invoke the operation
+ExperimentExecutionDetails result = await chaosExperimentExecution.ExecutionDetailsAsync();
+
+Console.WriteLine($"Succeeded: {result}");

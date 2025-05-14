2e6651222e9cb9023bc005cd94493760b5b617de
azure-sdk <azuresdk@microsoft.com>
2025-05-14 11:09:58 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-carbonoptimization_1.0.0-beta.1

---
+++ specification/carbon/resource-manager/Microsoft.Carbon/stable/2025-04-01/examples-js/listOperations.js
@@ -0,0 +1,19 @@
+const { CarbonOptimizationManagementClient } = require("@azure/arm-carbonoptimization");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2025-04-01/listOperations.json
+ */
+async function operationsList() {
+  const credential = new DefaultAzureCredential();
+  const client = new CarbonOptimizationManagementClient(credential);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
---
+++ specification/carbon/resource-manager/Microsoft.Carbon/stable/2025-04-01/examples-js/queryCarbonEmissionsSubscriptionItemDetailsReportReport.js
@@ -0,0 +1,34 @@
+const { CarbonOptimizationManagementClient } = require("@azure/arm-carbonoptimization");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to aPI for Carbon Emissions Reports
+ *
+ * @summary aPI for Carbon Emissions Reports
+ * x-ms-original-file: 2025-04-01/queryCarbonEmissionsSubscriptionItemDetailsReportReport.json
+ */
+async function queryCarbonEmissionSubscriptionsItemDetailsReport() {
+  const credential = new DefaultAzureCredential();
+  const client = new CarbonOptimizationManagementClient(credential);
+  const result = await client.carbonService.queryCarbonEmissionReports({
+    reportType: "ItemDetailsReport",
+    subscriptionList: [
+      "00000000-0000-0000-0000-000000000000",
+      "00000000-0000-0000-0000-000000000001,",
+      "00000000-0000-0000-0000-000000000002",
+      "00000000-0000-0000-0000-000000000003",
+      "00000000-0000-0000-0000-000000000004",
+      "00000000-0000-0000-0000-000000000005",
+      "00000000-0000-0000-0000-000000000006",
+      "00000000-0000-0000-0000-000000000007",
+      "00000000-0000-0000-0000-000000000008",
+    ],
+    carbonScopeList: ["Scope1", "Scope3"],
+    dateRange: { start: "2024-05-01", end: "2024-05-01" },
+    categoryType: "Subscription",
+    orderBy: "LatestMonthEmissions",
+    sortDirection: "Desc",
+    pageSize: 100,
+  });
+  console.log(result);
+}

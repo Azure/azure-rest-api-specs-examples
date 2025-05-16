4709e5627b36985816b96444c991ed87adb32a2c
azure-sdk <azuresdk@microsoft.com>
2025-05-16 11:09:12 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-confidentialledger_1.1.0-beta.1

---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2024-09-19-preview/examples-java/ManagedCCF_ListBySub.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for ManagedCcf List.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/preview/2024-09-19-preview/
+     * examples/ManagedCCF_ListBySub.json
+     */
+    /**
+     * Sample code: ManagedCCFListBySub.
+     * 
+     * @param manager Entry point to ConfidentialLedgerManager.
+     */
+    public static void
+        managedCCFListBySub(com.azure.resourcemanager.confidentialledger.ConfidentialLedgerManager manager) {
+        manager.managedCcfs().list(null, com.azure.core.util.Context.NONE);
+    }
+}

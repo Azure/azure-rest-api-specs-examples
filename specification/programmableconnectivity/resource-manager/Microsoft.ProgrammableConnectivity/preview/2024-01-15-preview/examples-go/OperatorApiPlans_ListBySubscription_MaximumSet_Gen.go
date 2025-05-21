909b9d493e5de76bdaf5ab22fd4b69a1e6e0b3a3
azure-sdk <azuresdk@microsoft.com>
2025-05-21 11:09:12 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/programmableconnectivity/armprogrammableconnectivity/v0.1.0

---
+++ specification/programmableconnectivity/resource-manager/Microsoft.ProgrammableConnectivity/preview/2024-01-15-preview/examples-go/OperatorApiPlans_ListBySubscription_MaximumSet_Gen.go
@@ -0,0 +1,76 @@
+package armprogrammableconnectivity_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/programmableconnectivity/armprogrammableconnectivity"
+)
+
+// Generated from example definition: 2024-01-15-preview/OperatorApiPlans_ListBySubscription_MaximumSet_Gen.json
+func ExampleOperatorAPIPlansClient_NewListBySubscriptionPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armprogrammableconnectivity.NewClientFactory("B976474B-99FA-4C25-A3BD-8B05C3C3D07A", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewOperatorAPIPlansClient().NewListBySubscriptionPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armprogrammableconnectivity.OperatorAPIPlansClientListBySubscriptionResponse{
+		// 	OperatorAPIPlanListResult: armprogrammableconnectivity.OperatorAPIPlanListResult{
+		// 		Value: []*armprogrammableconnectivity.OperatorAPIPlan{
+		// 			{
+		// 				Properties: &armprogrammableconnectivity.OperatorAPIPlanProperties{
+		// 					OperatorName: to.Ptr("csisc"),
+		// 					CamaraAPIName: to.Ptr("vfugujiismnjtcwrzkdvxaj"),
+		// 					SupportedLocations: []*string{
+		// 						to.Ptr("m"),
+		// 					},
+		// 					OperatorRegions: []*string{
+		// 						to.Ptr("licmuodzevtmowxdzzbstssmghl"),
+		// 					},
+		// 					Markets: []*string{
+		// 						to.Ptr("yncxinbbsipilayubuq"),
+		// 					},
+		// 					Limits: to.Ptr("tdcuvjvnyctdlcxebcrcwquq"),
+		// 					MarketplaceProperties: &armprogrammableconnectivity.MarketplaceProperties{
+		// 						OfferID: to.Ptr("zlhnteqphbk"),
+		// 						LegacyOfferID: to.Ptr("x"),
+		// 						PublisherID: to.Ptr("zxlhmlhmkhuqggwmzyxg"),
+		// 						PlanID: to.Ptr("ukdqvgtelddrssctjfbqjospcshfcg"),
+		// 						TermID: to.Ptr("uuwqguxsrfnl"),
+		// 					},
+		// 					ProvisioningState: to.Ptr(armprogrammableconnectivity.ProvisioningStateSucceeded),
+		// 				},
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-00000000000/providers/Microsoft.ProgrammableConnectivity/operatorApiPlans/livmzrh"),
+		// 				Name: to.Ptr("bfrnaknndvhdkmaqgfwcmnxjsjtg"),
+		// 				Type: to.Ptr("hclykzxrxyojkpfxnckjkcndlf"),
+		// 				SystemData: &armprogrammableconnectivity.SystemData{
+		// 					CreatedBy: to.Ptr("kuprrapuolhnvju"),
+		// 					CreatedByType: to.Ptr(armprogrammableconnectivity.CreatedByTypeUser),
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2024-01-30T16:41:38.838Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("lsmrhxnvkpmrxncylgqpkr"),
+		// 					LastModifiedByType: to.Ptr(armprogrammableconnectivity.CreatedByTypeUser),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2024-01-30T16:41:38.838Z"); return t}()),
+		// 				},
+		// 			},
+		// 		},
+		// 		NextLink: to.Ptr("https://microsoft.com/a"),
+		// 	},
+		// }
+	}
+}

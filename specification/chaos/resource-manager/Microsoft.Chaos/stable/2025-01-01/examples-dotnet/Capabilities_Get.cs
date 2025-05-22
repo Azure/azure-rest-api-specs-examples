55b82512cca356a36c34de3b31b3b8fd1e9088c7
azure-sdk <azuresdk@microsoft.com>
2025-05-22 11:16:00 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Chaos_1.1.0

--- specification/chaos/resource-manager/Microsoft.Chaos/stable/2025-01-01/examples-dotnet/Capabilities_Get.cs
+++ specification/chaos/resource-manager/Microsoft.Chaos/stable/2025-01-01/examples-dotnet/Capabilities_Get.cs
@@ -14,23 +14,34 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ChaosCapabilityResource created on azure
-// for more information of creating ChaosCapabilityResource, please refer to the document of ChaosCapabilityResource
+// this example assumes you already have this ChaosTargetResource created on azure
+// for more information of creating ChaosTargetResource, please refer to the document of ChaosTargetResource
 string subscriptionId = "6b052e15-03d3-4f17-b2e1-be7f07588291";
 string resourceGroupName = "exampleRG";
 string parentProviderNamespace = "Microsoft.Compute";
 string parentResourceType = "virtualMachines";
 string parentResourceName = "exampleVM";
 string targetName = "Microsoft-VirtualMachine";
-string capabilityName = "Shutdown-1.0";
-ResourceIdentifier chaosCapabilityResourceId = ChaosCapabilityResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, parentProviderNamespace, parentResourceType, parentResourceName, targetName, capabilityName);
-ChaosCapabilityResource chaosCapability = client.GetChaosCapabilityResource(chaosCapabilityResourceId);
+ResourceIdentifier chaosTargetResourceId = ChaosTargetResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, parentProviderNamespace, parentResourceType, parentResourceName, targetName);
+ChaosTargetResource chaosTarget = client.GetChaosTargetResource(chaosTargetResourceId);
+
+// get the collection of this ChaosCapabilityResource
+ChaosCapabilityCollection collection = chaosTarget.GetChaosCapabilities();
 
 // invoke the operation
-ChaosCapabilityResource result = await chaosCapability.GetAsync();
+string capabilityName = "Shutdown-1.0";
+NullableResponse<ChaosCapabilityResource> response = await collection.GetIfExistsAsync(capabilityName);
+ChaosCapabilityResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ChaosCapabilityData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ChaosCapabilityData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

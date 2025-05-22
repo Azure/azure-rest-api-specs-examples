0102405c25373ab480f059f7ac3a4f1c02251e3c
azure-sdk <azuresdk@microsoft.com>
2025-05-22 11:21:35 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-kubernetesconfiguration-extensiontypes_1.0.0b1

---
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/extensionTypes/preview/2024-11-01-preview/examples-python/ListExtensionTypesByLocation.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.kubernetesconfiguration.extensiontypes import KubernetesConfigurationExtensionTypesMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-kubernetesconfiguration-extensiontypes
+# USAGE
+    python list_extension_types_by_location.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = KubernetesConfigurationExtensionTypesMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subId1",
+    )
+
+    response = client.extension_types.location_list(
+        location="westus2",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/extensionTypes/preview/2024-11-01-preview/examples/ListExtensionTypesByLocation.json
+if __name__ == "__main__":
+    main()

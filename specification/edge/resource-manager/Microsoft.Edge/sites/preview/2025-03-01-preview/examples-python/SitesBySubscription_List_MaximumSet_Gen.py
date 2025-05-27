727b2a702597e070b1b7b329f40a5507131e6f7e
azure-sdk <azuresdk@microsoft.com>
2025-05-27 11:25:06 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-sitemanager_1.0.0b1

---
+++ specification/edge/resource-manager/Microsoft.Edge/sites/preview/2025-03-01-preview/examples-python/SitesBySubscription_List_MaximumSet_Gen.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.sitemanager import SiteManagerMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-sitemanager
+# USAGE
+    python sites_by_subscription_list_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SiteManagerMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.sites_by_subscription.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2025-03-01-preview/SitesBySubscription_List_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

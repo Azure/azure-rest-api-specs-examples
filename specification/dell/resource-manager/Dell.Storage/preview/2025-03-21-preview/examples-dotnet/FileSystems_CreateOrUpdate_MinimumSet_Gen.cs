18e6cde08bd0305351e783119ef024edcf557a0c
azure-sdk <azuresdk@microsoft.com>
2025-05-27 11:19:59 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Dell.Storage_1.0.0-beta.1

---
+++ specification/dell/resource-manager/Dell.Storage/preview/2025-03-21-preview/examples-dotnet/FileSystems_CreateOrUpdate_MinimumSet_Gen.cs
@@ -0,0 +1,52 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Dell.Storage.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Dell.Storage;
+
+// Generated from example definition: 2025-03-21-preview/FileSystems_CreateOrUpdate_MinimumSet_Gen.json
+// this example is just showing the usage of "FileSystemResource_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "BF7E7352-2FE4-4163-9CF7-5FF8EC2E9B92";
+string resourceGroupName = "rgDell";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this DellFileSystemResource
+DellFileSystemCollection collection = resourceGroupResource.GetDellFileSystems();
+
+// invoke the operation
+string filesystemName = "abcd";
+DellFileSystemData data = new DellFileSystemData(new AzureLocation("tbcvhxzpgrijtdygkttnfswwtacs"))
+{
+    Properties = new DellFileSystemProperties(
+    new DellFileSystemMarketplaceDetails("lgozf", "pzhjvibxqgeqkndqnjlduwnxqbr", "planeName")
+    {
+        PrivateOfferId = "privateOfferId",
+    },
+    new ResourceIdentifier("yp"),
+    "10.0.0.1/24",
+    new DellFileSystemUserDetails("hoznewwtzmyjzctzosfuh"),
+    "fhewkj",
+    new DellFileSystemEncryptionProperties(DellFileSystemEncryptionType.MicrosoftManagedKeysMmk)),
+};
+ArmOperation<DellFileSystemResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, filesystemName, data);
+DellFileSystemResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+DellFileSystemData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

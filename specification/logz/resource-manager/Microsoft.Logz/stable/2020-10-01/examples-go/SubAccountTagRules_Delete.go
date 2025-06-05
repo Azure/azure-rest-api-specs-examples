624b97513b5e185353d6eaa2e6ece692e9ff41ff
azure-sdk <azuresdk@microsoft.com>
2025-06-05 11:17:28 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/logz/armlogz/v1.2.1

---
+++ specification/logz/resource-manager/Microsoft.Logz/stable/2020-10-01/examples-go/SubAccountTagRules_Delete.go
@@ -0,0 +1,26 @@
+package armlogz_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/logz/armlogz"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/d55b8005f05b040b852c15e74a0f3e36494a15e1/specification/logz/resource-manager/Microsoft.Logz/stable/2020-10-01/examples/SubAccountTagRules_Delete.json
+func ExampleSubAccountTagRulesClient_Delete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armlogz.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	_, err = clientFactory.NewSubAccountTagRulesClient().Delete(ctx, "myResourceGroup", "myMonitor", "SubAccount1", "default", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+}

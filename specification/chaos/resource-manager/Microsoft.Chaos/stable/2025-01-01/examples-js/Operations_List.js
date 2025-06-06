96301f1bfe422756729c40c66a03fba25458d483
azure-sdk <azuresdk@microsoft.com>
2025-06-06 11:10:48 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-chaos_2.0.0

---
+++ specification/chaos/resource-manager/Microsoft.Chaos/stable/2025-01-01/examples-js/Operations_List.js
@@ -0,0 +1,20 @@
+const { ChaosManagementClient } = require("@azure/arm-chaos");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2025-01-01/Operations_List.json
+ */
+async function listsAllChaosStudioOperations() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-00000000000";
+  const client = new ChaosManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.operations.listAll()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

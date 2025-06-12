c37e1040e1e7f63102dfd9bba4fca4e117e5e9db
azure-sdk <azuresdk@microsoft.com>
2025-06-12 11:16:28 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-onlineexperimentation_1.0.0b1

---
+++ specification/onlineexperimentation/resource-manager/Microsoft.OnlineExperimentation/preview/2025-05-31-preview/examples-python/OnlineExperimentationWorkspaces_Delete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.onlineexperimentation import OnlineExperimentationMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-onlineexperimentation
+# USAGE
+    python online_experimentation_workspaces_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = OnlineExperimentationMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.online_experimentation_workspaces.begin_delete(
+        resource_group_name="res9871",
+        workspace_name="expworkspace3",
+    ).result()
+
+
+# x-ms-original-file: 2025-05-31-preview/OnlineExperimentationWorkspaces_Delete.json
+if __name__ == "__main__":
+    main()

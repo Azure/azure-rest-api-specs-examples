7f1c4daf8f36f0fe3403937e059566504cd04f24
azure-sdk <azuresdk@microsoft.com>
2025-06-17 11:15:11 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-resource_24.0.0

---
+++ specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2024-07-01/examples-python/PostDeploymentValidateOnSubscription.py
@@ -0,0 +1,41 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.resource.resources import ResourceManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resource
+# USAGE
+    python post_deployment_validate_on_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ResourceManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000001",
+    )
+
+    response = client.deployments.begin_validate_at_subscription_scope(
+        deployment_name="my-deployment",
+        parameters={
+            "location": "eastus",
+            "properties": {
+                "mode": "Incremental",
+                "parameters": {},
+                "templateLink": {"uri": "https://example.com/exampleTemplate.json"},
+            },
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2024-07-01/examples/PostDeploymentValidateOnSubscription.json
+if __name__ == "__main__":
+    main()

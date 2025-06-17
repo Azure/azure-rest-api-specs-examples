516a134107415f3873f09cf176bae7b6afd26ad7
azure-sdk <azuresdk@microsoft.com>
2025-06-17 11:07:36 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/hybridcompute/armhybridcompute/v2.1.0-beta.2

---
+++ specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2025-02-19-preview/examples-go/extension/ExtensionMetadataV2_Get.go
@@ -0,0 +1,45 @@
+package armhybridcompute_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridcompute/armhybridcompute/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/4a2bb0762eaad11e725516708483598e0c12cabb/specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2025-02-19-preview/examples/extension/ExtensionMetadataV2_Get.json
+func ExampleExtensionMetadataV2Client_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armhybridcompute.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewExtensionMetadataV2Client().Get(ctx, "EastUS", "microsoft.azure.monitor", "azuremonitorlinuxagent", "1.33.0", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res.ExtensionValueV2 = armhybridcompute.ExtensionValueV2{
+	// 	ID: to.Ptr("/providers/Microsoft.HybridCompute/locations/eastus/publishers/microsoft.azure.monitor/extensionTypes/azuremonitorlinuxagent/versions/1.33.0"),
+	// 	Properties: &armhybridcompute.ExtensionValueV2Properties{
+	// 		ExtensionType: to.Ptr("azuremonitorlinuxagent"),
+	// 		Publisher: to.Ptr("microsoft.azure.monitor"),
+	// 		Version: to.Ptr("1.33.0"),
+	// 		Architecture: []*string{
+	// 			to.Ptr("x64")},
+	// 			ExtensionSignatureURI: to.Ptr("https://eastus.his.arc.azure.com/extensions/extensionsignatures/azuremonitorlinuxagent__1.33.0.zip"),
+	// 			ExtensionUris: []*string{
+	// 				to.Ptr("https://aaaaaaaaaaaaaaaaaaaa.blob.core.windows.net/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa_1.33.0.zip"),
+	// 				to.Ptr("https://bbbbbbbbbbbbbbbbbbbb.blob.core.windows.net/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb_1.33.0.zip"),
+	// 				to.Ptr("https://cccccccccccccccccccc.blob.core.windows.net/cccccccccccccccccccccccccccccccccccc/cccccccccccccccccccccccccccccccccccc_1.33.0.zip")},
+	// 				OperatingSystem: to.Ptr("Linux"),
+	// 			},
+	// 		}
+}
---
+++ specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2025-02-19-preview/examples-go/privateLinkScope/PrivateLinkScopes_Delete.go
@@ -0,0 +1,30 @@
+package armhybridcompute_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridcompute/armhybridcompute/v2"
+)
+
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/4a2bb0762eaad11e725516708483598e0c12cabb/specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2025-02-19-preview/examples/privateLinkScope/PrivateLinkScopes_Delete.json
+func ExamplePrivateLinkScopesClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armhybridcompute.NewClientFactory("<subscription-id>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewPrivateLinkScopesClient().BeginDelete(ctx, "my-resource-group", "my-privatelinkscope", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

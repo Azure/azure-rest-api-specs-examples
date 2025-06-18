b918ff72a5c6e5b29632e440e7d84bf733ab1c45
azure-sdk <azuresdk@microsoft.com>
2025-06-18 11:07:26 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-networkcloud_2.0.0

---
+++ specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2025-02-01/examples-java/Clusters_ListByResourceGroup.java
@@ -0,0 +1,20 @@
+
+/**
+ * Samples for Clusters ListByResourceGroup.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2025-02-01/examples/
+     * Clusters_ListByResourceGroup.json
+     */
+    /**
+     * Sample code: List clusters for resource group.
+     * 
+     * @param manager Entry point to NetworkCloudManager.
+     */
+    public static void
+        listClustersForResourceGroup(com.azure.resourcemanager.networkcloud.NetworkCloudManager manager) {
+        manager.clusters().listByResourceGroup("resourceGroupName", com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2025-02-01/examples-java/Racks_ListByResourceGroup.java
@@ -0,0 +1,19 @@
+
+/**
+ * Samples for Racks ListByResourceGroup.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file:
+     * specification/networkcloud/resource-manager/Microsoft.NetworkCloud/stable/2025-02-01/examples/
+     * Racks_ListByResourceGroup.json
+     */
+    /**
+     * Sample code: List racks for resource group.
+     * 
+     * @param manager Entry point to NetworkCloudManager.
+     */
+    public static void listRacksForResourceGroup(com.azure.resourcemanager.networkcloud.NetworkCloudManager manager) {
+        manager.racks().listByResourceGroup("resourceGroupName", com.azure.core.util.Context.NONE);
+    }
+}

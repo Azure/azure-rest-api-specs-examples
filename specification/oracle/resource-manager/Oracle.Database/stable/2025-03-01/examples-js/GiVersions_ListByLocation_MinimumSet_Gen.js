e7a98211ef580bf2bb0f87f0114ee1210a8986b9
azure-sdk <azuresdk@microsoft.com>
2025-06-19 11:15:19 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-oracledatabase_2.0.0

---
+++ specification/oracle/resource-manager/Oracle.Database/stable/2025-03-01/examples-js/GiVersions_ListByLocation_MinimumSet_Gen.js
@@ -0,0 +1,20 @@
+const { OracleDatabaseManagementClient } = require("@azure/arm-oracledatabase");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list GiVersion resources by SubscriptionLocationResource
+ *
+ * @summary list GiVersion resources by SubscriptionLocationResource
+ * x-ms-original-file: 2025-03-01/GiVersions_ListByLocation_MinimumSet_Gen.json
+ */
+async function listGiVersionsByLocationGeneratedByMinimumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.giVersions.listByLocation("eastus")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
---
+++ specification/oracle/resource-manager/Oracle.Database/stable/2025-03-01/examples-js/exaInfra_addStorageCapacity.js
@@ -0,0 +1,16 @@
+const { OracleDatabaseManagementClient } = require("@azure/arm-oracledatabase");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to perform add storage capacity on exadata infra
+ *
+ * @summary perform add storage capacity on exadata infra
+ * x-ms-original-file: 2025-03-01/exaInfra_addStorageCapacity.json
+ */
+async function cloudExadataInfrastructuresAddStorageCapacity() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  const result = await client.cloudExadataInfrastructures.addStorageCapacity("rg000", "infra1");
+  console.log(result);
+}

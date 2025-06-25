17d2b4ae78d6b35e529c18c33fa46394f1a80f1e
azure-sdk <azuresdk@microsoft.com>
2025-06-25 11:07:05 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dnsresolver_1.2.0

---
+++ specification/dnsresolver/resource-manager/Microsoft.Network/stable/2025-05-01/examples-js/DnsResolverDomainList_Get.js
@@ -0,0 +1,23 @@
+const { DnsResolverManagementClient } = require("@azure/arm-dnsresolver");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Gets properties of a DNS resolver domain list.
+ *
+ * @summary Gets properties of a DNS resolver domain list.
+ * x-ms-original-file: specification/dnsresolver/resource-manager/Microsoft.Network/stable/2025-05-01/examples/DnsResolverDomainList_Get.json
+ */
+async function retrieveDnsResolverDomainListWithLessThan1000Domains() {
+  const subscriptionId =
+    process.env["DNSRESOLVER_SUBSCRIPTION_ID"] || "abdd4249-9f34-4cc6-8e42-c2e32110603e";
+  const resourceGroupName = process.env["DNSRESOLVER_RESOURCE_GROUP"] || "sampleResourceGroup";
+  const dnsResolverDomainListName = "sampleDnsResolverDomainList";
+  const credential = new DefaultAzureCredential();
+  const client = new DnsResolverManagementClient(credential, subscriptionId);
+  const result = await client.dnsResolverDomainLists.get(
+    resourceGroupName,
+    dnsResolverDomainListName,
+  );
+  console.log(result);
+}

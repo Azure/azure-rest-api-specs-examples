4e1748230416157c82ae34c9677db44417d8f89d
azure-sdk <azuresdk@microsoft.com>
2025-07-01 11:16:00 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-cloudhealth_1.0.0b1

---
+++ specification/cloudhealth/resource-manager/Microsoft.CloudHealth/preview/2025-05-01-preview/examples-python/HealthModels_Delete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.cloudhealth import CloudHealthMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-cloudhealth
+# USAGE
+    python health_models_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = CloudHealthMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.health_models.begin_delete(
+        resource_group_name="rgopenapi",
+        health_model_name="model1",
+    ).result()
+
+
+# x-ms-original-file: 2025-05-01-preview/HealthModels_Delete.json
+if __name__ == "__main__":
+    main()

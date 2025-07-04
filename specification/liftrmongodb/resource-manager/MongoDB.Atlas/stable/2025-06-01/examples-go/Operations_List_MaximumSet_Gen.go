7628b24571f2f6d2153b8ad239ee99dc770939ea
azure-sdk <azuresdk@microsoft.com>
2025-07-04 11:11:20 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/mongodbatlas/armmongodbatlas/v1.0.0

---
+++ specification/liftrmongodb/resource-manager/MongoDB.Atlas/stable/2025-06-01/examples-go/Operations_List_MaximumSet_Gen.go
@@ -0,0 +1,53 @@
+package armmongodbatlas_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mongodbatlas/armmongodbatlas"
+)
+
+// Generated from example definition: 2025-06-01/Operations_List_MaximumSet_Gen.json
+func ExampleOperationsClient_NewListPager_operationsListMaximumSet() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armmongodbatlas.NewClientFactory("<subscriptionID>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armmongodbatlas.OperationsClientListResponse{
+		// 	OperationListResult: armmongodbatlas.OperationListResult{
+		// 		Value: []*armmongodbatlas.Operation{
+		// 			{
+		// 				Name: to.Ptr("glurvmlhuxhlxhmjoiycpicdrjmsd"),
+		// 				IsDataAction: to.Ptr(true),
+		// 				Display: &armmongodbatlas.OperationDisplay{
+		// 					Provider: to.Ptr("ybkvitxcbahxljn"),
+		// 					Resource: to.Ptr("wsdbqubnkmfbclp"),
+		// 					Operation: to.Ptr("vsix"),
+		// 					Description: to.Ptr("ichocwjfmgzbiliknwwnasvdsnowu"),
+		// 				},
+		// 				Origin: to.Ptr(armmongodbatlas.OriginUser),
+		// 				ActionType: to.Ptr(armmongodbatlas.ActionTypeInternal),
+		// 			},
+		// 		},
+		// 		NextLink: to.Ptr("https://microsoft.com/a"),
+		// 	},
+		// }
+	}
+}
---
+++ specification/liftrmongodb/resource-manager/MongoDB.Atlas/stable/2025-06-01/examples-go/Organizations_Delete_MaximumSet_Gen.go
@@ -0,0 +1,30 @@
+package armmongodbatlas_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mongodbatlas/armmongodbatlas"
+)
+
+// Generated from example definition: 2025-06-01/Organizations_Delete_MaximumSet_Gen.json
+func ExampleOrganizationsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armmongodbatlas.NewClientFactory("4AFC1287-D389-4265-B2D4-59B96A45CACC", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewOrganizationsClient().BeginDelete(ctx, "rgopenapi", "U.1-:7", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

0e5c851b04382bb76be080d1d5f4989551748ef3
azure-sdk <azuresdk@microsoft.com>
2025-07-18 11:15:13 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-hardwaresecuritymodules_2.0.0

---
+++ specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/stable/2025-03-31/examples-js/DedicatedHsm_Get.js
@@ -0,0 +1,16 @@
+const { AzureDedicatedHSMResourceProvider } = require("@azure/arm-hardwaresecuritymodules");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets the specified Azure dedicated HSM.
+ *
+ * @summary gets the specified Azure dedicated HSM.
+ * x-ms-original-file: 2025-03-31/DedicatedHsm_Get.json
+ */
+async function getADedicatedHSM() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new AzureDedicatedHSMResourceProvider(credential, subscriptionId);
+  const result = await client.dedicatedHsm.get("hsm-group", "hsm1");
+  console.log(result);
+}

ed45259cf55a3d6fe2bb506b7e73a2a694dfd762
azure-sdk <azuresdk@microsoft.com>
2025-07-23 11:05:26 +0000

    [Automation] Collect examples from azure-sdk-for-java#azure-resourcemanager-storagediscovery_1.0.0-beta.1

---
+++ specification/storagediscovery/resource-manager/Microsoft.StorageDiscovery/preview/2025-06-01-preview/examples-java/StorageDiscoveryWorkspaces_ListByResourceGroup.java
@@ -0,0 +1,18 @@
+
+/**
+ * Samples for StorageDiscoveryWorkspaces ListByResourceGroup.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file: 2025-06-01-preview/StorageDiscoveryWorkspaces_ListByResourceGroup.json
+     */
+    /**
+     * Sample code: List StorageDiscoveryWorkspaces by Resource Group.
+     * 
+     * @param manager Entry point to StorageDiscoveryManager.
+     */
+    public static void listStorageDiscoveryWorkspacesByResourceGroup(
+        com.azure.resourcemanager.storagediscovery.StorageDiscoveryManager manager) {
+        manager.storageDiscoveryWorkspaces().listByResourceGroup("sample-rg", com.azure.core.util.Context.NONE);
+    }
+}

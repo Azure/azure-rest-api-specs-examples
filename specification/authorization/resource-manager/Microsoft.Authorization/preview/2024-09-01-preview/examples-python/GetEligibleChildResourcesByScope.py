7526f123fecd5529b7bd78c8acd974c5cd1fa88f
azure-sdk <azuresdk@microsoft.com>
2025-07-24 11:16:34 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-authorization_5.0.0b1

---
+++ specification/authorization/resource-manager/Microsoft.Authorization/preview/2024-09-01-preview/examples-python/GetEligibleChildResourcesByScope.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.authorization import AuthorizationManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-authorization
+# USAGE
+    python get_eligible_child_resources_by_scope.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AuthorizationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.eligible_child_resources.get(
+        scope="providers/Microsoft.Subscription/subscriptions/dfa2a084-766f-4003-8ae1-c4aeb893a99f",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/authorization/resource-manager/Microsoft.Authorization/preview/2024-09-01-preview/examples/GetEligibleChildResourcesByScope.json
+if __name__ == "__main__":
+    main()

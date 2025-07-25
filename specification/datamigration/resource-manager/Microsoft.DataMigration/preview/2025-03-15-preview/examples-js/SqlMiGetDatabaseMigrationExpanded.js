49406aab34642fb47e104f8db3208a490fdf6fb0
azure-sdk <azuresdk@microsoft.com>
2025-07-25 11:19:19 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-datamigration_3.0.0-beta.4

---
+++ specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2025-03-15-preview/examples-js/SqlMiGetDatabaseMigrationExpanded.js
@@ -0,0 +1,28 @@
+const { DataMigrationManagementClient } = require("@azure/arm-datamigration");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Retrieve the specified database migration for a given SQL Managed Instance.
+ *
+ * @summary Retrieve the specified database migration for a given SQL Managed Instance.
+ * x-ms-original-file: specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2025-03-15-preview/examples/SqlMiGetDatabaseMigrationExpanded.json
+ */
+async function getSqlMiDatabaseMigrationWithTheExpandParameter() {
+  const subscriptionId =
+    process.env["DATAMIGRATION_SUBSCRIPTION_ID"] || "00000000-1111-2222-3333-444444444444";
+  const resourceGroupName = process.env["DATAMIGRATION_RESOURCE_GROUP"] || "testrg";
+  const managedInstanceName = "managedInstance1";
+  const targetDbName = "db1";
+  const expand = "MigrationStatusDetails";
+  const options = { expand };
+  const credential = new DefaultAzureCredential();
+  const client = new DataMigrationManagementClient(credential, subscriptionId);
+  const result = await client.databaseMigrationsSqlMi.get(
+    resourceGroupName,
+    managedInstanceName,
+    targetDbName,
+    options,
+  );
+  console.log(result);
+}

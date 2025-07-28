664cc7a2c3734e36e11791046d3b4e2c362cac6f
azure-sdk <azuresdk@microsoft.com>
2025-07-28 11:15:20 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.AppConfiguration_1.4.1

--- specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples-dotnet/ConfigurationStoresGetPrivateEndpointConnection.cs
+++ specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples-dotnet/ConfigurationStoresGetPrivateEndpointConnection.cs
@@ -15,31 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this AppConfigurationStoreResource created on azure
-// for more information of creating AppConfigurationStoreResource, please refer to the document of AppConfigurationStoreResource
+// this example assumes you already have this AppConfigurationPrivateEndpointConnectionResource created on azure
+// for more information of creating AppConfigurationPrivateEndpointConnectionResource, please refer to the document of AppConfigurationPrivateEndpointConnectionResource
 string subscriptionId = "c80fb759-c965-4c6a-9110-9b2b2d038882";
 string resourceGroupName = "myResourceGroup";
 string configStoreName = "contoso";
-ResourceIdentifier appConfigurationStoreResourceId = AppConfigurationStoreResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, configStoreName);
-AppConfigurationStoreResource appConfigurationStore = client.GetAppConfigurationStoreResource(appConfigurationStoreResourceId);
-
-// get the collection of this AppConfigurationPrivateEndpointConnectionResource
-AppConfigurationPrivateEndpointConnectionCollection collection = appConfigurationStore.GetAppConfigurationPrivateEndpointConnections();
+string privateEndpointConnectionName = "myConnection";
+ResourceIdentifier appConfigurationPrivateEndpointConnectionResourceId = AppConfigurationPrivateEndpointConnectionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, configStoreName, privateEndpointConnectionName);
+AppConfigurationPrivateEndpointConnectionResource appConfigurationPrivateEndpointConnection = client.GetAppConfigurationPrivateEndpointConnectionResource(appConfigurationPrivateEndpointConnectionResourceId);
 
 // invoke the operation
-string privateEndpointConnectionName = "myConnection";
-NullableResponse<AppConfigurationPrivateEndpointConnectionResource> response = await collection.GetIfExistsAsync(privateEndpointConnectionName);
-AppConfigurationPrivateEndpointConnectionResource result = response.HasValue ? response.Value : null;
+AppConfigurationPrivateEndpointConnectionResource result = await appConfigurationPrivateEndpointConnection.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    AppConfigurationPrivateEndpointConnectionData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+AppConfigurationPrivateEndpointConnectionData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

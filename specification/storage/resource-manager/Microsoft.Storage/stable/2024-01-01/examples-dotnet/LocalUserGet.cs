43d220c491c0812dff5aa62971c8341b4de30a03
azure-sdk <azuresdk@microsoft.com>
2025-07-28 11:13:18 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Storage_1.4.4

--- specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples-dotnet/LocalUserGet.cs
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples-dotnet/LocalUserGet.cs
@@ -15,31 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this StorageAccountResource created on azure
-// for more information of creating StorageAccountResource, please refer to the document of StorageAccountResource
+// this example assumes you already have this StorageAccountLocalUserResource created on azure
+// for more information of creating StorageAccountLocalUserResource, please refer to the document of StorageAccountLocalUserResource
 string subscriptionId = "{subscription-id}";
 string resourceGroupName = "res6977";
 string accountName = "sto2527";
-ResourceIdentifier storageAccountResourceId = StorageAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-StorageAccountResource storageAccount = client.GetStorageAccountResource(storageAccountResourceId);
-
-// get the collection of this StorageAccountLocalUserResource
-StorageAccountLocalUserCollection collection = storageAccount.GetStorageAccountLocalUsers();
+string username = "user1";
+ResourceIdentifier storageAccountLocalUserResourceId = StorageAccountLocalUserResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, username);
+StorageAccountLocalUserResource storageAccountLocalUser = client.GetStorageAccountLocalUserResource(storageAccountLocalUserResourceId);
 
 // invoke the operation
-string username = "user1";
-NullableResponse<StorageAccountLocalUserResource> response = await collection.GetIfExistsAsync(username);
-StorageAccountLocalUserResource result = response.HasValue ? response.Value : null;
+StorageAccountLocalUserResource result = await storageAccountLocalUser.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    StorageAccountLocalUserData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+StorageAccountLocalUserData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

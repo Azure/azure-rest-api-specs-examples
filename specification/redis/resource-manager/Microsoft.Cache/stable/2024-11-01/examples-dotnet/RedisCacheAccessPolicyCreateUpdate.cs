cd88159e9351f525c258ddd9c8f5e187b314a651
azure-sdk <azuresdk@microsoft.com>
2025-07-29 11:19:14 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Redis_1.5.1

--- specification/redis/resource-manager/Microsoft.Cache/stable/2024-11-01/examples-dotnet/RedisCacheAccessPolicyCreateUpdate.cs
+++ specification/redis/resource-manager/Microsoft.Cache/stable/2024-11-01/examples-dotnet/RedisCacheAccessPolicyCreateUpdate.cs
@@ -14,24 +14,21 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this RedisResource created on azure
-// for more information of creating RedisResource, please refer to the document of RedisResource
+// this example assumes you already have this RedisCacheAccessPolicyResource created on azure
+// for more information of creating RedisCacheAccessPolicyResource, please refer to the document of RedisCacheAccessPolicyResource
 string subscriptionId = "subid";
 string resourceGroupName = "rg1";
 string cacheName = "cache1";
-ResourceIdentifier redisResourceId = RedisResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, cacheName);
-RedisResource redis = client.GetRedisResource(redisResourceId);
-
-// get the collection of this RedisCacheAccessPolicyResource
-RedisCacheAccessPolicyCollection collection = redis.GetRedisCacheAccessPolicies();
+string accessPolicyName = "accessPolicy1";
+ResourceIdentifier redisCacheAccessPolicyResourceId = RedisCacheAccessPolicyResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, cacheName, accessPolicyName);
+RedisCacheAccessPolicyResource redisCacheAccessPolicy = client.GetRedisCacheAccessPolicyResource(redisCacheAccessPolicyResourceId);
 
 // invoke the operation
-string accessPolicyName = "accessPolicy1";
 RedisCacheAccessPolicyData data = new RedisCacheAccessPolicyData
 {
     Permissions = "+get +hget",
 };
-ArmOperation<RedisCacheAccessPolicyResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, accessPolicyName, data);
+ArmOperation<RedisCacheAccessPolicyResource> lro = await redisCacheAccessPolicy.UpdateAsync(WaitUntil.Completed, data);
 RedisCacheAccessPolicyResource result = lro.Value;
 
 // the variable result is a resource, you could call other operations on this instance as well

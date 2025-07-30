62d7ea838897c8b63b1a2bffa48494df2bc27e87
azure-sdk <azuresdk@microsoft.com>
2025-07-30 11:24:31 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-recoveryservicesdatareplication_1.0.0

---
+++ specification/recoveryservicesdatareplication/resource-manager/Microsoft.DataReplication/stable/2024-09-01/examples-python/ProtectedItem_PlannedFailover.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.recoveryservicesdatareplication import RecoveryServicesDataReplicationMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-recoveryservicesdatareplication
+# USAGE
+    python protected_item_planned_failover.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RecoveryServicesDataReplicationMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.protected_item.begin_planned_failover(
+        resource_group_name="rgrecoveryservicesdatareplication",
+        vault_name="4",
+        protected_item_name="d",
+        body={"properties": {"customProperties": {"instanceType": "PlannedFailoverModelCustomProperties"}}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2024-09-01/ProtectedItem_PlannedFailover.json
+if __name__ == "__main__":
+    main()

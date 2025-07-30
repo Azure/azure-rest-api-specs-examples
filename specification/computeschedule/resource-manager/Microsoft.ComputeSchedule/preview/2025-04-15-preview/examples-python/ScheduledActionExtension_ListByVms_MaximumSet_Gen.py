25f518f61ee4e6baa0e9acd529e2275d7d86b087
azure-sdk <azuresdk@microsoft.com>
2025-07-30 11:23:10 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-computeschedule_1.2.0b1

---
+++ specification/computeschedule/resource-manager/Microsoft.ComputeSchedule/preview/2025-04-15-preview/examples-python/ScheduledActionExtension_ListByVms_MaximumSet_Gen.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.computeschedule import ComputeScheduleMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-computeschedule
+# USAGE
+    python scheduled_action_extension_list_by_vms_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeScheduleMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.scheduled_action_extension.list_by_vms(
+        resource_uri="sazvpabfud",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2025-04-15-preview/ScheduledActionExtension_ListByVms_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

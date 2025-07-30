530b0c6f9591813a7b8309fe0f8d8e96eea9ec89
azure-sdk <azuresdk@microsoft.com>
2025-07-30 11:14:34 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-recoveryservicesdatareplication_1.0.0

---
+++ specification/recoveryservicesdatareplication/resource-manager/Microsoft.DataReplication/stable/2024-09-01/examples-js/Policy_Delete.js
@@ -0,0 +1,17 @@
+const {
+  AzureSiteRecoveryManagementServiceAPI,
+} = require("@azure/arm-recoveryservicesdatareplication");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to removes the policy.
+ *
+ * @summary removes the policy.
+ * x-ms-original-file: 2024-09-01/Policy_Delete.json
+ */
+async function deletesThePolicy() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "930CEC23-4430-4513-B855-DBA237E2F3BF";
+  const client = new AzureSiteRecoveryManagementServiceAPI(credential, subscriptionId);
+  await client.policy.delete("rgrecoveryservicesdatareplication", "4", "wqfscsdv");
+}

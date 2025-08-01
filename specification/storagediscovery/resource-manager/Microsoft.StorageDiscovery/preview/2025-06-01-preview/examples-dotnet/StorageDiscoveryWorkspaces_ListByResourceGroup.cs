3e6e96bb0de2e5d4ea190bf3fb081719010d461b
azure-sdk <azuresdk@microsoft.com>
2025-08-01 11:08:19 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StorageDiscovery_1.0.0-beta.1

---
+++ specification/storagediscovery/resource-manager/Microsoft.StorageDiscovery/preview/2025-06-01-preview/examples-dotnet/StorageDiscoveryWorkspaces_ListByResourceGroup.cs
@@ -0,0 +1,39 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.StorageDiscovery.Models;
+using Azure.ResourceManager.StorageDiscovery;
+
+// Generated from example definition: 2025-06-01-preview/StorageDiscoveryWorkspaces_ListByResourceGroup.json
+// this example is just showing the usage of "StorageDiscoveryWorkspace_ListByResourceGroup" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "b79cb3ba-745e-5d9a-8903-4a02327a7e09";
+string resourceGroupName = "sample-rg";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this StorageDiscoveryWorkspaceResource
+StorageDiscoveryWorkspaceCollection collection = resourceGroupResource.GetStorageDiscoveryWorkspaces();
+
+// invoke the operation and iterate over the result
+await foreach (StorageDiscoveryWorkspaceResource item in collection.GetAllAsync())
+{
+    // the variable item is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    StorageDiscoveryWorkspaceData resourceData = item.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
+
+Console.WriteLine("Succeeded");

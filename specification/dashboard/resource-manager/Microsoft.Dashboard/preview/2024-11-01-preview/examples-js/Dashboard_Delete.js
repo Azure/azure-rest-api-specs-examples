aeecca8a49127eec0f0802147738110367eac391
azure-sdk <azuresdk@microsoft.com>
2025-08-08 11:07:08 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dashboard_2.0.0-beta.1

---
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/preview/2024-11-01-preview/examples-js/Dashboard_Delete.js
@@ -0,0 +1,15 @@
+const { DashboardManagementClient } = require("@azure/arm-dashboard");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a dashboard for Grafana resource.
+ *
+ * @summary delete a dashboard for Grafana resource.
+ * x-ms-original-file: 2024-11-01-preview/Dashboard_Delete.json
+ */
+async function dashboardDelete() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DashboardManagementClient(credential, subscriptionId);
+  await client.managedDashboards.delete("myResourceGroup", "myDashboard");
+}
---
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/preview/2024-11-01-preview/examples-js/IntegrationFabrics_Update.js
@@ -0,0 +1,24 @@
+const { DashboardManagementClient } = require("@azure/arm-dashboard");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to update a IntegrationFabric
+ *
+ * @summary update a IntegrationFabric
+ * x-ms-original-file: 2024-11-01-preview/IntegrationFabrics_Update.json
+ */
+async function integrationFabricsUpdate() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DashboardManagementClient(credential, subscriptionId);
+  const result = await client.integrationFabrics.update(
+    "myResourceGroup",
+    "myWorkspace",
+    "sampleIntegration",
+    {
+      properties: { scenarios: ["scenario1"] },
+      tags: { Environment: "Dev 2" },
+    },
+  );
+  console.log(result);
+}

071aa13ed2c22933712e3888cc9f67fce6e79e8b
azure-sdk <azuresdk@microsoft.com>
2025-08-12 11:19:57 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MachineLearning_1.2.3

--- specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2024-04-01/examples-dotnet/Registry/ModelVersion/get.cs
+++ specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2024-04-01/examples-dotnet/Registry/ModelVersion/get.cs
@@ -15,21 +15,32 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this MachineLearningRegistryModelVersionResource created on azure
-// for more information of creating MachineLearningRegistryModelVersionResource, please refer to the document of MachineLearningRegistryModelVersionResource
+// this example assumes you already have this MachineLearningRegistryModelContainerResource created on azure
+// for more information of creating MachineLearningRegistryModelContainerResource, please refer to the document of MachineLearningRegistryModelContainerResource
 string subscriptionId = "00000000-1111-2222-3333-444444444444";
 string resourceGroupName = "test-rg";
 string registryName = "my-aml-registry";
 string modelName = "string";
-string version = "string";
-ResourceIdentifier machineLearningRegistryModelVersionResourceId = MachineLearningRegistryModelVersionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName, modelName, version);
-MachineLearningRegistryModelVersionResource machineLearningRegistryModelVersion = client.GetMachineLearningRegistryModelVersionResource(machineLearningRegistryModelVersionResourceId);
+ResourceIdentifier machineLearningRegistryModelContainerResourceId = MachineLearningRegistryModelContainerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName, modelName);
+MachineLearningRegistryModelContainerResource machineLearningRegistryModelContainer = client.GetMachineLearningRegistryModelContainerResource(machineLearningRegistryModelContainerResourceId);
+
+// get the collection of this MachineLearningRegistryModelVersionResource
+MachineLearningRegistryModelVersionCollection collection = machineLearningRegistryModelContainer.GetMachineLearningRegistryModelVersions();
 
 // invoke the operation
-MachineLearningRegistryModelVersionResource result = await machineLearningRegistryModelVersion.GetAsync();
+string version = "string";
+NullableResponse<MachineLearningRegistryModelVersionResource> response = await collection.GetIfExistsAsync(version);
+MachineLearningRegistryModelVersionResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-MachineLearningModelVersionData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    MachineLearningModelVersionData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

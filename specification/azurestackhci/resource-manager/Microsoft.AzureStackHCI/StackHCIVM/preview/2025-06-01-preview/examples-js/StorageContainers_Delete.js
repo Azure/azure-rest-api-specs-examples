50eb1ab75a9c7b24ac4c6a7ef7a57baaa231b91e
azure-sdk <azuresdk@microsoft.com>
2025-08-21 11:10:04 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-azurestackhcivm_1.0.0-beta.1

---
+++ specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/StackHCIVM/preview/2025-06-01-preview/examples-js/StorageContainers_Delete.js
@@ -0,0 +1,15 @@
+const { AzureStackHCIVMManagementClient } = require("@azure/arm-azurestackhcivm");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to the operation to delete a storage container.
+ *
+ * @summary the operation to delete a storage container.
+ * x-ms-original-file: 2025-06-01-preview/StorageContainers_Delete.json
+ */
+async function deleteStorageContainer() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
+  const client = new AzureStackHCIVMManagementClient(credential, subscriptionId);
+  await client.storageContainers.delete("test-rg", "Default_Container");
+}

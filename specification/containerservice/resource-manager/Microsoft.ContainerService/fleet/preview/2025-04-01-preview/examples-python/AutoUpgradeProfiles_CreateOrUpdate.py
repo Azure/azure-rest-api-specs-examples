5a26a47387cd5891d19614bbf66afc1f0c5a4158
azure-sdk <azuresdk@microsoft.com>
2025-08-28 11:26:56 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-containerservicefleet_4.0.0b1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2025-04-01-preview/examples-python/AutoUpgradeProfiles_CreateOrUpdate.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.containerservicefleet import ContainerServiceFleetMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerservicefleet
+# USAGE
+    python auto_upgrade_profiles_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerServiceFleetMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.auto_upgrade_profiles.begin_create_or_update(
+        resource_group_name="rg1",
+        fleet_name="fleet1",
+        auto_upgrade_profile_name="autoupgradeprofile1",
+        resource={"properties": {"channel": "Stable", "longTermSupport": False, "targetKubernetesVersion": ""}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2025-04-01-preview/AutoUpgradeProfiles_CreateOrUpdate.json
+if __name__ == "__main__":
+    main()
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/preview/2025-04-01-preview/examples-python/UpdateRuns_CreateOrUpdate.py
@@ -0,0 +1,61 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.containerservicefleet import ContainerServiceFleetMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerservicefleet
+# USAGE
+    python update_runs_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerServiceFleetMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.update_runs.begin_create_or_update(
+        resource_group_name="rg1",
+        fleet_name="fleet1",
+        update_run_name="run1",
+        resource={
+            "properties": {
+                "managedClusterUpdate": {
+                    "nodeImageSelection": {"type": "Latest"},
+                    "upgrade": {"kubernetesVersion": "1.26.1", "type": "Full"},
+                },
+                "strategy": {
+                    "stages": [
+                        {
+                            "afterGates": [{"displayName": "gate after stage1", "type": "Approval"}],
+                            "afterStageWaitInSeconds": 3600,
+                            "beforeGates": [{"displayName": "gate before stage1", "type": "Approval"}],
+                            "groups": [
+                                {
+                                    "afterGates": [{"displayName": "gate after group-a", "type": "Approval"}],
+                                    "beforeGates": [{"displayName": "gate before group-a", "type": "Approval"}],
+                                    "name": "group-a",
+                                }
+                            ],
+                            "name": "stage1",
+                        }
+                    ]
+                },
+                "updateStrategyId": "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/fleets/myFleet/updateStrategies/strategy1",
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2025-04-01-preview/UpdateRuns_CreateOrUpdate.json
+if __name__ == "__main__":
+    main()

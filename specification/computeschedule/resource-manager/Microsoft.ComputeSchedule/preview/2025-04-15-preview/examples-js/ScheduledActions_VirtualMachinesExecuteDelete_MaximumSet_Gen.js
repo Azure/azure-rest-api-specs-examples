59011ce19693c2472446501fb7e36c7c52084483
azure-sdk <azuresdk@microsoft.com>
2025-08-29 11:09:18 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-computeschedule_1.2.0-beta.1

---
+++ specification/computeschedule/resource-manager/Microsoft.ComputeSchedule/preview/2025-04-15-preview/examples-js/ScheduledActions_VirtualMachinesExecuteDelete_MaximumSet_Gen.js
@@ -0,0 +1,28 @@
+const { ComputeScheduleClient } = require("@azure/arm-computeschedule");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to virtualMachinesExecuteDelete: Execute delete operation for a batch of virtual machines, this operation is triggered as soon as Computeschedule receives it.
+ *
+ * @summary virtualMachinesExecuteDelete: Execute delete operation for a batch of virtual machines, this operation is triggered as soon as Computeschedule receives it.
+ * x-ms-original-file: 2025-04-15-preview/ScheduledActions_VirtualMachinesExecuteDelete_MaximumSet_Gen.json
+ */
+async function scheduledActionsVirtualMachinesExecuteDeleteMaximumSetGenGeneratedByMaximumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "0505D8E4-D41A-48FB-9CA5-4AF8D93BE75F";
+  const client = new ComputeScheduleClient(credential, subscriptionId);
+  const result = await client.scheduledActions.virtualMachinesExecuteDelete("east", {
+    executionParameters: {
+      retryPolicy: { retryCount: 2, retryWindowInMinutes: 4 },
+    },
+    resources: {
+      ids: [
+        "/subscriptions/YourSubscriptionId/resourceGroups/YourResourceGroupName/providers/Microsoft.Compute/virtualMachines/testResource3",
+        "/subscriptions/YourSubscriptionId/resourceGroups/YourResourceGroupName/providers/Microsoft.Compute/virtualMachines/testResource4",
+      ],
+    },
+    correlationid: "dfe927c5-16a6-40b7-a0f7-8524975ed642",
+    forceDeletion: false,
+  });
+  console.log(result);
+}

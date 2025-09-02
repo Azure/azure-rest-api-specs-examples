050d07d05c3e5007108a4a189aa5a5a5184f365f
azure-sdk <azuresdk@microsoft.com>
2025-09-02 11:08:07 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-workloadorchestration_1.0.0-beta.1

---
+++ specification/edge/resource-manager/Microsoft.Edge/configurationmanager/stable/2025-06-01/examples-js/DynamicSchemas_ListBySchema_MaximumSet_Gen.js
@@ -0,0 +1,23 @@
+const { WorkloadOrchestrationManagementClient } = require("@azure/arm-workloadorchestration");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list by Schema
+ *
+ * @summary list by Schema
+ * x-ms-original-file: 2025-06-01/DynamicSchemas_ListBySchema_MaximumSet_Gen.json
+ */
+async function dynamicSchemasListBySchemaMaximumSet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "9D54FE4C-00AF-4836-8F48-B6A9C4E47192";
+  const client = new WorkloadOrchestrationManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.dynamicSchemas.listBySchema(
+    "rgconfigurationmanager",
+    "testname",
+  )) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
---
+++ specification/edge/resource-manager/Microsoft.Edge/configurationmanager/stable/2025-06-01/examples-js/Jobs_Get_MaximumSet_Gen.js
@@ -0,0 +1,16 @@
+const { WorkloadOrchestrationManagementClient } = require("@azure/arm-workloadorchestration");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a Job resource
+ *
+ * @summary get a Job resource
+ * x-ms-original-file: 2025-06-01/Jobs_Get_MaximumSet_Gen.json
+ */
+async function jobsGetMaximumSet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-00000000000";
+  const client = new WorkloadOrchestrationManagementClient(credential, subscriptionId);
+  const result = await client.jobs.get("gt", "jobsName");
+  console.log(result);
+}

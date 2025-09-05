5eb3155ecc5c438013c8d456ac3e43227f8ca66b
azure-sdk <azuresdk@microsoft.com>
2025-09-05 11:15:30 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Playwright_1.0.0

---
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/playwright/stable/2025-09-01/examples-dotnet/PlaywrightWorkspaces_CheckNameAvailability.cs
@@ -0,0 +1,33 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Playwright.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Playwright;
+
+// Generated from example definition: 2025-09-01/PlaywrightWorkspaces_CheckNameAvailability.json
+// this example is just showing the usage of "PlaywrightWorkspaces_CheckPlaywrightNameAvailability" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation
+PlaywrightNameAvailabilityContent content = new PlaywrightNameAvailabilityContent
+{
+    Name = "dummyName",
+    Type = "Microsoft.LoadTestService/PlaywrightWorkspaces",
+};
+PlaywrightNameAvailabilityResult result = await subscriptionResource.CheckPlaywrightNameAvailabilityAsync(content);
+
+Console.WriteLine($"Succeeded: {result}");

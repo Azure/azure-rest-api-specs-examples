b169ab55b5f6be256e9b957aa74b41c6fd91e46b
azure-sdk <azuresdk@microsoft.com>
2025-09-05 11:17:20 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-iotfirmwaredefense_2.0.0

---
+++ specification/fist/resource-manager/Microsoft.IoTFirmwareDefense/stable/2025-08-02/examples-python/Workspaces_Update_MaximumSet_Gen.py
@@ -0,0 +1,38 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.iotfirmwaredefense import IoTFirmwareDefenseMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-iotfirmwaredefense
+# USAGE
+    python workspaces_update_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = IoTFirmwareDefenseMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.workspaces.update(
+        resource_group_name="rgiotfirmwaredefense",
+        workspace_name="exampleWorkspaceName",
+        properties={
+            "sku": {"capacity": 25, "family": "Free", "name": "Free", "size": "Free", "tier": "Free"},
+            "tags": {},
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: 2025-08-02/Workspaces_Update_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

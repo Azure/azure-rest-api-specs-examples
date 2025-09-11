ba5eb671522afc9f18b16c3e0fc1229c01e14ec9
azure-sdk <azuresdk@microsoft.com>
2025-09-11 11:14:11 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ComputeSchedule_1.1.0

---
+++ specification/computeschedule/resource-manager/Microsoft.ComputeSchedule/stable/2025-05-01/examples-dotnet/ScheduledActions_VirtualMachinesExecuteHibernate_MinimumSet_Gen.cs
@@ -0,0 +1,31 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ComputeSchedule.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ComputeSchedule;
+
+// Generated from example definition: 2025-05-01/ScheduledActions_VirtualMachinesExecuteHibernate_MinimumSet_Gen.json
+// this example is just showing the usage of "ScheduledActions_ExecuteVirtualMachineHibernate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "0505D8E4-D41A-48FB-9CA5-4AF8D93BE75F";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation
+AzureLocation locationparameter = new AzureLocation("klbbfkgnebotnwosvbsze");
+ExecuteHibernateContent content = new ExecuteHibernateContent(new ScheduledActionExecutionParameterDetail(), new UserRequestResources(new ResourceIdentifier[] { new ResourceIdentifier("/subscriptions/YourSubscriptionId/resourceGroups/YourResourceGroupName/providers/Microsoft.Compute/virtualMachines/testResource3") }), "jmdiz");
+HibernateResourceOperationResult result = await subscriptionResource.ExecuteVirtualMachineHibernateAsync(locationparameter, content);
+
+Console.WriteLine($"Succeeded: {result}");
---
+++ specification/computeschedule/resource-manager/Microsoft.ComputeSchedule/stable/2025-05-01/examples-dotnet/ScheduledActions_VirtualMachinesSubmitHibernate_MaximumSet_Gen.cs
@@ -0,0 +1,45 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ComputeSchedule.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ComputeSchedule;
+
+// Generated from example definition: 2025-05-01/ScheduledActions_VirtualMachinesSubmitHibernate_MaximumSet_Gen.json
+// this example is just showing the usage of "ScheduledActions_SubmitVirtualMachineHibernate" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
+string subscriptionId = "0505D8E4-D41A-48FB-9CA5-4AF8D93BE75F";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// invoke the operation
+AzureLocation locationparameter = new AzureLocation("sgwaluihhyqoxbfskudgqcikbns");
+SubmitHibernateContent content = new SubmitHibernateContent(new UserRequestSchedule(ScheduledActionDeadlineType.Unknown)
+{
+    Deadline = DateTimeOffset.Parse("2025-04-15T19:47:04.403Z"),
+    UserRequestDeadline = DateTimeOffset.Parse("2025-04-15T19:47:04.403Z"),
+    Timezone = "qacufsmctpgjozovlsihrzoctatcsj",
+    UserRequestTimezone = "upnmayfebiadztdktxzq",
+}, new ScheduledActionExecutionParameterDetail
+{
+    OptimizationPreference = ScheduledActionOptimizationPreference.Cost,
+    RetryPolicy = new UserRequestRetryPolicy
+    {
+        RetryCount = 25,
+        RetryWindowInMinutes = 4,
+    },
+}, new UserRequestResources(new ResourceIdentifier[] { new ResourceIdentifier("/subscriptions/YourSubscriptionId/resourceGroups/YourResourceGroupName/providers/Microsoft.Compute/virtualMachines/testResource3") }), "htqivutynuoslvbp");
+HibernateResourceOperationResult result = await subscriptionResource.SubmitVirtualMachineHibernateAsync(locationparameter, content);
+
+Console.WriteLine($"Succeeded: {result}");

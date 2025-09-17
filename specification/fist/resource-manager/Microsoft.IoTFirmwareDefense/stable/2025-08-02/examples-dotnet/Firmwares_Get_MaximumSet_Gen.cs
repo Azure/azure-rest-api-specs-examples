5152857074c19a5d45b2be956d796f9f55cfab5a
azure-sdk <azuresdk@microsoft.com>
2025-09-17 11:14:56 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.IotFirmwareDefense_1.1.0

---
+++ specification/fist/resource-manager/Microsoft.IoTFirmwareDefense/stable/2025-08-02/examples-dotnet/Firmwares_Get_MaximumSet_Gen.cs
@@ -0,0 +1,34 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.IotFirmwareDefense.Models;
+using Azure.ResourceManager.IotFirmwareDefense;
+
+// Generated from example definition: specification/fist/resource-manager/Microsoft.IoTFirmwareDefense/stable/2025-08-02/examples/Firmwares_Get_MaximumSet_Gen.json
+// this example is just showing the usage of "Firmwares_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this IotFirmwareResource created on azure
+// for more information of creating IotFirmwareResource, please refer to the document of IotFirmwareResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "rgiotfirmwaredefense";
+string workspaceName = "exampleWorkspaceName";
+string firmwareId = "00000000-0000-0000-0000-000000000000";
+ResourceIdentifier iotFirmwareResourceId = IotFirmwareResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName, firmwareId);
+IotFirmwareResource iotFirmware = client.GetIotFirmwareResource(iotFirmwareResourceId);
+
+// invoke the operation
+IotFirmwareResource result = await iotFirmware.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+IotFirmwareData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

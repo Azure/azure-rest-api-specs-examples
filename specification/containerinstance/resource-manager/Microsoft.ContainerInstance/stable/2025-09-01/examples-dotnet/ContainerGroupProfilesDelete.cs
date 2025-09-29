7826c36c97e77a51c08fdf33dced95a8e61232d2
azure-sdk <azuresdk@microsoft.com>
2025-09-29 11:20:31 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ContainerInstance_1.3.0

---
+++ specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2025-09-01/examples-dotnet/ContainerGroupProfilesDelete.cs
@@ -0,0 +1,29 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ContainerInstance.Models;
+using Azure.ResourceManager.ContainerInstance;
+
+// Generated from example definition: specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2025-09-01/examples/ContainerGroupProfilesDelete.json
+// this example is just showing the usage of "CGProfile_Delete" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ContainerGroupProfileResource created on azure
+// for more information of creating ContainerGroupProfileResource, please refer to the document of ContainerGroupProfileResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "demo";
+string containerGroupProfileName = "demo1";
+ResourceIdentifier containerGroupProfileResourceId = ContainerGroupProfileResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, containerGroupProfileName);
+ContainerGroupProfileResource containerGroupProfile = client.GetContainerGroupProfileResource(containerGroupProfileResourceId);
+
+// invoke the operation
+await containerGroupProfile.DeleteAsync(WaitUntil.Completed);
+
+Console.WriteLine("Succeeded");

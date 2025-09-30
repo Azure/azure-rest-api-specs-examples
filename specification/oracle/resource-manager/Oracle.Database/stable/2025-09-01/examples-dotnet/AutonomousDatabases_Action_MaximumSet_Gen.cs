6030897e8b7dbedf38f0d71743e00043a311ffe1
azure-sdk <azuresdk@microsoft.com>
2025-09-30 11:14:34 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.OracleDatabase_1.2.0

---
+++ specification/oracle/resource-manager/Oracle.Database/stable/2025-09-01/examples-dotnet/AutonomousDatabases_Action_MaximumSet_Gen.cs
@@ -0,0 +1,35 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.OracleDatabase.Models;
+using Azure.ResourceManager.OracleDatabase;
+
+// Generated from example definition: 2025-09-01/AutonomousDatabases_Action_MaximumSet_Gen.json
+// this example is just showing the usage of "AutonomousDatabases_Action" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this AutonomousDatabaseResource created on azure
+// for more information of creating AutonomousDatabaseResource, please refer to the document of AutonomousDatabaseResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "rgopenapi";
+string autonomousdatabasename = "databasedb1";
+ResourceIdentifier autonomousDatabaseResourceId = AutonomousDatabaseResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, autonomousdatabasename);
+AutonomousDatabaseResource autonomousDatabase = client.GetAutonomousDatabaseResource(autonomousDatabaseResourceId);
+
+// invoke the operation
+AutonomousDatabaseLifecycleAction body = new AutonomousDatabaseLifecycleAction(AutonomousDatabaseLifecycleActionEnum.Start);
+ArmOperation<AutonomousDatabaseResource> lro = await autonomousDatabase.ActionAsync(WaitUntil.Completed, body);
+AutonomousDatabaseResource result = lro.Value;
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+AutonomousDatabaseData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

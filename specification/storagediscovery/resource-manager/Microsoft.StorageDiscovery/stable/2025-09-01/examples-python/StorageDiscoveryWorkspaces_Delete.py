53b8ffc4392c381346be4b050e71588913b019b3
azure-sdk <azuresdk@microsoft.com>
2025-10-02 11:13:52 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storagediscovery_1.0.0

---
+++ specification/storagediscovery/resource-manager/Microsoft.StorageDiscovery/stable/2025-09-01/examples-python/StorageDiscoveryWorkspaces_Delete.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.storagediscovery import StorageDiscoveryMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storagediscovery
+# USAGE
+    python storage_discovery_workspaces_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageDiscoveryMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.storage_discovery_workspaces.delete(
+        resource_group_name="sample-rg",
+        storage_discovery_workspace_name="sampleworkspace",
+    )
+
+
+# x-ms-original-file: 2025-09-01/StorageDiscoveryWorkspaces_Delete.json
+if __name__ == "__main__":
+    main()

23a07d948251f37f36fa74adc5f56d9d2e75c27f
azure-sdk <azuresdk@microsoft.com>
2025-10-08 11:05:23 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/mongocluster/armmongocluster/v1.1.0-beta.2

---
+++ specification/mongocluster/resource-manager/Microsoft.DocumentDB/MongoCluster/preview/2025-08-01-preview/examples-go/MongoClusters_FirewallRuleCreate.go
@@ -0,0 +1,59 @@
+package armmongocluster_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mongocluster/armmongocluster"
+)
+
+// Generated from example definition: 2025-08-01-preview/MongoClusters_FirewallRuleCreate.json
+func ExampleFirewallRulesClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armmongocluster.NewClientFactory("ffffffff-ffff-ffff-ffff-ffffffffffff", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewFirewallRulesClient().BeginCreateOrUpdate(ctx, "TestGroup", "myMongoCluster", "rule1", armmongocluster.FirewallRule{
+		Properties: &armmongocluster.FirewallRuleProperties{
+			StartIPAddress: to.Ptr("0.0.0.0"),
+			EndIPAddress:   to.Ptr("255.255.255.255"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armmongocluster.FirewallRulesClientCreateOrUpdateResponse{
+	// 	FirewallRule: &armmongocluster.FirewallRule{
+	// 		ID: to.Ptr("/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestResourceGroup/providers/Microsoft.DocumentDB/mongoClusters/myMongoCluster/firewallRules/rule1"),
+	// 		Name: to.Ptr("rule1"),
+	// 		Type: to.Ptr("/Microsoft.DocumentDB/mongoClusters/firewallRules"),
+	// 		SystemData: &armmongocluster.SystemData{
+	// 			CreatedBy: to.Ptr("user1"),
+	// 			CreatedByType: to.Ptr(armmongocluster.CreatedByTypeUser),
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+	// 			LastModifiedBy: to.Ptr("user2"),
+	// 			LastModifiedByType: to.Ptr(armmongocluster.CreatedByTypeUser),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+	// 		},
+	// 		Properties: &armmongocluster.FirewallRuleProperties{
+	// 			ProvisioningState: to.Ptr(armmongocluster.ProvisioningStateSucceeded),
+	// 			StartIPAddress: to.Ptr("0.0.0.0"),
+	// 			EndIPAddress: to.Ptr("255.255.255.255"),
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/mongocluster/resource-manager/Microsoft.DocumentDB/MongoCluster/preview/2025-08-01-preview/examples-go/MongoClusters_NameAvailability.go
@@ -0,0 +1,38 @@
+package armmongocluster_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mongocluster/armmongocluster"
+)
+
+// Generated from example definition: 2025-08-01-preview/MongoClusters_NameAvailability.json
+func ExampleMongoClustersClient_CheckNameAvailability_checksAndConfirmsTheMongoClusterNameIsAvailabilityForUse() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armmongocluster.NewClientFactory("ffffffff-ffff-ffff-ffff-ffffffffffff", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewMongoClustersClient().CheckNameAvailability(ctx, "westus2", armmongocluster.CheckNameAvailabilityRequest{
+		Name: to.Ptr("newmongocluster"),
+		Type: to.Ptr("Microsoft.DocumentDB/mongoClusters"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armmongocluster.MongoClustersClientCheckNameAvailabilityResponse{
+	// 	CheckNameAvailabilityResponse: &armmongocluster.CheckNameAvailabilityResponse{
+	// 		NameAvailable: to.Ptr(true),
+	// 	},
+	// }
+}

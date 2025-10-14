b34cb156a0861ffc26a7d2fd652ff87c05fe5fc3
azure-sdk <azuresdk@microsoft.com>
2025-10-14 11:12:25 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StorageMover_1.3.0

---
+++ specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2025-07-01/examples-dotnet/Agents_Get_MinimumSet.cs
@@ -0,0 +1,34 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.StorageMover.Models;
+using Azure.ResourceManager.StorageMover;
+
+// Generated from example definition: 2025-07-01/Agents_Get_MinimumSet.json
+// this example is just showing the usage of "Agent_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this StorageMoverAgentResource created on azure
+// for more information of creating StorageMoverAgentResource, please refer to the document of StorageMoverAgentResource
+string subscriptionId = "60bcfc77-6589-4da2-b7fd-f9ec9322cf95";
+string resourceGroupName = "examples-rg";
+string storageMoverName = "examples-storageMoverName";
+string agentName = "examples-agentName";
+ResourceIdentifier storageMoverAgentResourceId = StorageMoverAgentResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, storageMoverName, agentName);
+StorageMoverAgentResource storageMoverAgent = client.GetStorageMoverAgentResource(storageMoverAgentResourceId);
+
+// invoke the operation
+StorageMoverAgentResource result = await storageMoverAgent.GetAsync();
+
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+StorageMoverAgentData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

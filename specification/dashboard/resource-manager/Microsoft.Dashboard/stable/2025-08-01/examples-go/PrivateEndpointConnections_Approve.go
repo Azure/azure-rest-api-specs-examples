3b924e95f9e70d25ed72c06b9df4ec86790cc8a4
azure-sdk <azuresdk@microsoft.com>
2025-10-22 11:21:42 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/dashboard/armdashboard/v2.0.0

---
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2025-08-01/examples-go/PrivateEndpointConnections_Approve.go
@@ -0,0 +1,30 @@
+package armdashboard_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dashboard/armdashboard/v2"
+)
+
+// Generated from example definition: 2025-08-01/PrivateEndpointConnections_Approve.json
+func ExamplePrivateEndpointConnectionsClient_BeginApprove() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armdashboard.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewPrivateEndpointConnectionsClient().BeginApprove(ctx, "myResourceGroup", "myWorkspace", "myConnection", armdashboard.PrivateEndpointConnection{}, &armdashboard.PrivateEndpointConnectionsClientBeginApproveOptions{})
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+}

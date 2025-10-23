e139691ad9930b8d5d34bc65274ff9e1d81a72ed
azure-sdk <azuresdk@microsoft.com>
2025-10-23 11:18:47 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-storage_19.1.0

---
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2025-06-01/examples-js/FileSharesList_PaidBursting.js
@@ -0,0 +1,22 @@
+const { StorageManagementClient } = require("@azure/arm-storage");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Lists all shares.
+ *
+ * @summary Lists all shares.
+ * x-ms-original-file: specification/storage/resource-manager/Microsoft.Storage/stable/2025-06-01/examples/FileSharesList_PaidBursting.json
+ */
+async function listSharesPaidBursting() {
+  const subscriptionId = process.env["STORAGE_SUBSCRIPTION_ID"] || "{subscription-id}";
+  const resourceGroupName = process.env["STORAGE_RESOURCE_GROUP"] || "res9290";
+  const accountName = "sto1590";
+  const credential = new DefaultAzureCredential();
+  const client = new StorageManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.fileShares.list(resourceGroupName, accountName)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
---
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2025-06-01/examples-js/FileSharesPut_PaidBursting.js
@@ -0,0 +1,32 @@
+const { StorageManagementClient } = require("@azure/arm-storage");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Creates a new share under the specified account as described by request body. The share resource includes metadata and properties for that share. It does not include a list of the files contained by the share.
+ *
+ * @summary Creates a new share under the specified account as described by request body. The share resource includes metadata and properties for that share. It does not include a list of the files contained by the share.
+ * x-ms-original-file: specification/storage/resource-manager/Microsoft.Storage/stable/2025-06-01/examples/FileSharesPut_PaidBursting.json
+ */
+async function putSharesWithPaidBursting() {
+  const subscriptionId = process.env["STORAGE_SUBSCRIPTION_ID"] || "{subscription-id}";
+  const resourceGroupName = process.env["STORAGE_RESOURCE_GROUP"] || "res346";
+  const accountName = "sto666";
+  const shareName = "share1235";
+  const fileShare = {
+    fileSharePaidBursting: {
+      paidBurstingEnabled: true,
+      paidBurstingMaxBandwidthMibps: 10340,
+      paidBurstingMaxIops: 102400,
+    },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new StorageManagementClient(credential, subscriptionId);
+  const result = await client.fileShares.create(
+    resourceGroupName,
+    accountName,
+    shareName,
+    fileShare,
+  );
+  console.log(result);
+}

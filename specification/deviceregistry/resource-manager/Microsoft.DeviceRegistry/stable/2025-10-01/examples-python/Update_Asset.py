c665be9ca6b179eb654ed7c71049e7ec078c49ac
azure-sdk <azuresdk@microsoft.com>
2025-10-26 11:09:58 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-deviceregistry_1.1.0

---
+++ specification/deviceregistry/resource-manager/Microsoft.DeviceRegistry/stable/2025-10-01/examples-python/Update_Asset.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.deviceregistry import DeviceRegistryMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-deviceregistry
+# USAGE
+    python update_asset.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DeviceRegistryMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.assets.begin_update(
+        resource_group_name="myResourceGroup",
+        asset_name="my-asset",
+        properties={"properties": {"displayName": "NewAssetDisplayName", "enabled": False}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2025-10-01/Update_Asset.json
+if __name__ == "__main__":
+    main()

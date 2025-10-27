069c514e8fec023166900d19dbb5c8253626ffa0
azure-sdk <azuresdk@microsoft.com>
2025-10-27 11:10:30 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-recoveryservices_4.0.0

---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/RecoveryServices/stable/2025-08-01/examples-python/ListReplicationUsages.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.recoveryservices import RecoveryServicesClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-recoveryservices
+# USAGE
+    python list_replication_usages.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RecoveryServicesClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.replication_usages.list(
+        resource_group_name="avrai7517RG1",
+        vault_name="avrai7517Vault1",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2025-08-01/ListReplicationUsages.json
+if __name__ == "__main__":
+    main()
---
+++ specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/RecoveryServices/stable/2025-08-01/examples-python/PUTVault_WithSourceScanConfiguration.py
@@ -0,0 +1,48 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.recoveryservices import RecoveryServicesClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-recoveryservices
+# USAGE
+    python put_vault_with_source_scan_configuration.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = RecoveryServicesClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.vaults.begin_create_or_update(
+        resource_group_name="Default-RecoveryServices-ResourceGroup",
+        vault_name="swaggerExample",
+        vault={
+            "identity": {"type": "SystemAssigned"},
+            "location": "West US",
+            "properties": {
+                "publicNetworkAccess": "Enabled",
+                "securitySettings": {
+                    "sourceScanConfiguration": {
+                        "sourceScanIdentity": {"operationIdentityType": "SystemAssigned"},
+                        "state": "Enabled",
+                    }
+                },
+            },
+            "sku": {"name": "Standard"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2025-08-01/PUTVault_WithSourceScanConfiguration.json
+if __name__ == "__main__":
+    main()

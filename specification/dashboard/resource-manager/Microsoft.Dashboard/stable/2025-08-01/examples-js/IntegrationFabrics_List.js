5b0625d682e85f71b9039d1b9cd3bf1e4a91a3c5
azure-sdk <azuresdk@microsoft.com>
2025-10-29 11:07:30 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-dashboard_2.0.0

---
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2025-08-01/examples-js/IntegrationFabrics_List.js
@@ -0,0 +1,20 @@
+const { DashboardManagementClient } = require("@azure/arm-dashboard");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list IntegrationFabric resources by ManagedGrafana
+ *
+ * @summary list IntegrationFabric resources by ManagedGrafana
+ * x-ms-original-file: 2025-08-01/IntegrationFabrics_List.json
+ */
+async function integrationFabricsList() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DashboardManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.integrationFabrics.list("myResourceGroup", "myWorkspace")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

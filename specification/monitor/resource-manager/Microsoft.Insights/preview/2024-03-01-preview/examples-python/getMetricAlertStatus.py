81f9bd0bd6db1c29edf157d7f7a6cf81c52236ba
azure-sdk <azuresdk@microsoft.com>
2025-11-17 12:24:39 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-monitor_8.0.0b2

---
+++ specification/monitor/resource-manager/Microsoft.Insights/preview/2024-03-01-preview/examples-python/getMetricAlertStatus.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.monitor import MonitorManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-monitor
+# USAGE
+    python get_metric_alert_status.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = MonitorManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="14ddf0c5-77c5-4b53-84f6-e1fa43ad68f7",
+    )
+
+    response = client.metric_alerts_status.list(
+        resource_group_name="gigtest",
+        rule_name="chiricutin",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/monitor/resource-manager/Microsoft.Insights/preview/2024-03-01-preview/examples/getMetricAlertStatus.json
+if __name__ == "__main__":
+    main()

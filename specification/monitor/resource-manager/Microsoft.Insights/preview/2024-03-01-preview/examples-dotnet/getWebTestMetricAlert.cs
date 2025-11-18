bc0759bbf4d344c0ef55d9c2dff9a02a70e120d5
azure-sdk <azuresdk@microsoft.com>
2025-11-18 12:25:27 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Monitor_1.4.0-beta.4

---
+++ specification/monitor/resource-manager/Microsoft.Insights/preview/2024-03-01-preview/examples-dotnet/getWebTestMetricAlert.cs
@@ -0,0 +1,47 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Monitor.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Monitor;
+
+// Generated from example definition: specification/monitor/resource-manager/Microsoft.Insights/preview/2024-03-01-preview/examples/getWebTestMetricAlert.json
+// this example is just showing the usage of "MetricAlerts_Get" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+string subscriptionId = "12345678-1234-1234-1234-123456789101";
+string resourceGroupName = "rg-example";
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this MetricAlertResource
+MetricAlertCollection collection = resourceGroupResource.GetMetricAlerts();
+
+// invoke the operation
+string ruleName = "webtest-name-example";
+NullableResponse<MetricAlertResource> response = await collection.GetIfExistsAsync(ruleName);
+MetricAlertResource result = response.HasValue ? response.Value : null;
+
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    MetricAlertData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

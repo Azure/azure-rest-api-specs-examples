56d635e1804318122700e5b76863ff22b9a4a0c7
azure-sdk <azuresdk@microsoft.com>
2025-11-24 12:17:12 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-resourcegraph_8.0.1

---
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/stable/2021-03-01/examples-python/GraphQueryGet.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.resourcegraph import ResourceGraphClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resourcegraph
+# USAGE
+    python graph_query_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ResourceGraphClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.graph_query.get(
+        subscription_id="024e2271-06fa-46b6-9079-f1ed3c7b070e",
+        resource_group_name="my-resource-group",
+        resource_name="MyDockerVMs",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/stable/2021-03-01/examples/GraphQueryGet.json
+if __name__ == "__main__":
+    main()
---
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/stable/2021-03-01/examples-python/GraphQueryList.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.resourcegraph import ResourceGraphClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resourcegraph
+# USAGE
+    python graph_query_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ResourceGraphClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.graph_query.list_by_subscription(
+        subscription_id="024e2271-06fa-46b6-9079-f1ed3c7b070e",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/stable/2021-03-01/examples/GraphQueryList.json
+if __name__ == "__main__":
+    main()

11cdf7706dfe47a660b9090f09c30e4035b29e75
azure-sdk <azuresdk@microsoft.com>
2025-11-25 12:23:20 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-postgresqlflexibleservers_2.0.0

---
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2025-08-01/examples-python/BackupsAutomaticAndOnDemandGet.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.postgresqlflexibleservers import PostgreSQLManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-postgresqlflexibleservers
+# USAGE
+    python backups_automatic_and_on_demand_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PostgreSQLManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
+    )
+
+    response = client.backups_automatic_and_on_demand.get(
+        resource_group_name="exampleresourcegroup",
+        server_name="exampleserver",
+        backup_name="backup_638830782181266873",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2025-08-01/examples/BackupsAutomaticAndOnDemandGet.json
+if __name__ == "__main__":
+    main()
---
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2025-08-01/examples-python/VirtualEndpointDelete.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.postgresqlflexibleservers import PostgreSQLManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-postgresqlflexibleservers
+# USAGE
+    python virtual_endpoint_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PostgreSQLManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
+    )
+
+    client.virtual_endpoints.begin_delete(
+        resource_group_name="exampleresourcegroup",
+        server_name="exampleserver",
+        virtual_endpoint_name="examplebasename",
+    ).result()
+
+
+# x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2025-08-01/examples/VirtualEndpointDelete.json
+if __name__ == "__main__":
+    main()

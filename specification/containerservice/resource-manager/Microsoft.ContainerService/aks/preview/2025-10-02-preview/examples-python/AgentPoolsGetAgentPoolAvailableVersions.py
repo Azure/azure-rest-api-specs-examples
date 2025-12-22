c4744c9cd8492d20f1c4f8b1916d36f6a34cf220
azure-sdk <azuresdk@microsoft.com>
2025-12-22 12:23:52 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-containerservice_41.0.0b3

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2025-10-02-preview/examples-python/AgentPoolsGetAgentPoolAvailableVersions.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.containerservice import ContainerServiceClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerservice
+# USAGE
+    python agent_pools_get_agent_pool_available_versions.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerServiceClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="00000000-0000-0000-0000-000000000000",
+    )
+
+    response = client.agent_pools.get_available_agent_pool_versions(
+        resource_group_name="rg1",
+        resource_name="clustername1",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2025-10-02-preview/examples/AgentPoolsGetAgentPoolAvailableVersions.json
+if __name__ == "__main__":
+    main()

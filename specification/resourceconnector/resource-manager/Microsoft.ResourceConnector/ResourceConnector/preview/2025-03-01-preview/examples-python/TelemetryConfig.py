6164a325c0eade61be1070a7abc23698746662e6
azure-sdk <azuresdk@microsoft.com>
2026-01-15 12:14:10 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-resourceconnector_2.0.0b1

---
+++ specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/ResourceConnector/preview/2025-03-01-preview/examples-python/TelemetryConfig.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.resourceconnector import ResourceConnectorMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resourceconnector
+# USAGE
+    python telemetry_config.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ResourceConnectorMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.appliances.get_telemetry_config()
+    print(response)
+
+
+# x-ms-original-file: 2025-03-01-preview/TelemetryConfig.json
+if __name__ == "__main__":
+    main()

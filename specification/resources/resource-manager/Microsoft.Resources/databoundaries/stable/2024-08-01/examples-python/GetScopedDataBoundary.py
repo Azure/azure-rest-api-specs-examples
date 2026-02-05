06dde3f8afb4104a3dec362ef23e8ade1013a9ca
azure-sdk <azuresdk@microsoft.com>
2026-02-05 12:51:17 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-resource-databoundaries_1.0.0b1

---
+++ specification/resources/resource-manager/Microsoft.Resources/databoundaries/stable/2024-08-01/examples-python/GetScopedDataBoundary.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.resource.databoundaries import DataBoundaryMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resource-databoundaries
+# USAGE
+    python get_scoped_data_boundary.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataBoundaryMgmtClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.data_boundaries.get_scope(
+        scope="subscriptions/11111111-1111-1111-1111-111111111111/resourcegroups/my-resource-group",
+        default="default",
+    )
+    print(response)
+
+
+# x-ms-original-file: specification/resources/resource-manager/Microsoft.Resources/databoundaries/stable/2024-08-01/examples/GetScopedDataBoundary.json
+if __name__ == "__main__":
+    main()

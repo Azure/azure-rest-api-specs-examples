9daa79cf465dfeb59671068989758cd7d712b6c6
azure-sdk <azuresdk@microsoft.com>
2026-02-14 12:12:22 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-postgresqlflexibleservers_3.0.0b1

---
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/preview/2026-01-01-preview/examples-python/FirewallRulesCreateOrUpdate.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.postgresqlflexibleservers import PostgreSQLManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-postgresqlflexibleservers
+# USAGE
+    python firewall_rules_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PostgreSQLManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.firewall_rules.begin_create_or_update(
+        resource_group_name="exampleresourcegroup",
+        server_name="exampleserver",
+        firewall_rule_name="examplefirewallrule",
+        parameters={"properties": {"endIpAddress": "255.255.255.255", "startIpAddress": "0.0.0.0"}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2026-01-01-preview/FirewallRulesCreateOrUpdate.json
+if __name__ == "__main__":
+    main()
---
+++ specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/preview/2026-01-01-preview/examples-python/PrivateLinkResourcesGet.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.postgresqlflexibleservers import PostgreSQLManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-postgresqlflexibleservers
+# USAGE
+    python private_link_resources_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PostgreSQLManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.private_link_resources.get(
+        resource_group_name="exampleresourcegroup",
+        server_name="exampleserver",
+        group_name="exampleprivatelink",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2026-01-01-preview/PrivateLinkResourcesGet.json
+if __name__ == "__main__":
+    main()

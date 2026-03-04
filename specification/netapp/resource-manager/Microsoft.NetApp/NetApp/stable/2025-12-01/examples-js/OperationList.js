1237211c12587707f119b47f61314ed2a7d1aaf1
azure-sdk <azuresdk@microsoft.com>
2026-03-04 12:31:23 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-netapp_23.0.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/NetApp/stable/2025-12-01/examples-js/OperationList.js
@@ -0,0 +1,19 @@
+const { NetAppManagementClient } = require("@azure/arm-netapp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2025-12-01/OperationList.json
+ */
+async function operationList() {
+  const credential = new DefaultAzureCredential();
+  const client = new NetAppManagementClient(credential);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

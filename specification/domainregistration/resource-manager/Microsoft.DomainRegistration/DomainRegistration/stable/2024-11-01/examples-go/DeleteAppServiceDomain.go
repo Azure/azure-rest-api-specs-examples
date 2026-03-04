97ea093d0d263c0c3649b8092d1fd6c4cb157a4b
azure-sdk <azuresdk@microsoft.com>
2026-03-04 12:28:48 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/domainregistration/armdomainregistration/v0.1.0

--- specification/domainregistration/resource-manager/Microsoft.DomainRegistration/DomainRegistration/stable/2024-11-01/examples-go/DeleteAppServiceDomain.go
+++ specification/domainregistration/resource-manager/Microsoft.DomainRegistration/DomainRegistration/stable/2024-11-01/examples-go/DeleteAppServiceDomain.go
@@ -1,4 +1,4 @@
-package armappservice_test
+package armdomainregistration_test
 
 import (
 	"context"
@@ -6,22 +6,28 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appservice/armappservice/v5"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/domainregistration/armdomainregistration"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/9f4cb2884f1948b879ecfb3f410e8cbc8805c213/specification/domainregistration/resource-manager/Microsoft.DomainRegistration/DomainRegistration/stable/2024-11-01/examples/DeleteAppServiceDomain.json
+// Generated from example definition: 2024-11-01/DeleteAppServiceDomain.json
 func ExampleDomainsClient_Delete() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	clientFactory, err := armappservice.NewClientFactory("<subscription-id>", cred, nil)
+	clientFactory, err := armdomainregistration.NewClientFactory("34adfa4f-cedf-4dc0-ba29-b6d1a69ab345", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	_, err = clientFactory.NewDomainsClient().Delete(ctx, "testrg123", "example.com", &armappservice.DomainsClientDeleteOptions{ForceHardDeleteDomain: to.Ptr(true)})
+	res, err := clientFactory.NewDomainsClient().Delete(ctx, "testrg123", "example.com", &armdomainregistration.DomainsClientDeleteOptions{
+		ForceHardDeleteDomain: to.Ptr(true)})
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armdomainregistration.DomainsClientDeleteResponse{
+	// }
 }
--- specification/domainregistration/resource-manager/Microsoft.DomainRegistration/DomainRegistration/stable/2024-11-01/examples-go/ListTopLevelDomains.go
+++ specification/domainregistration/resource-manager/Microsoft.DomainRegistration/DomainRegistration/stable/2024-11-01/examples-go/ListTopLevelDomains.go
@@ -1,21 +1,21 @@
-package armappservice_test
+package armdomainregistration_test
 
 import (
 	"context"
 	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appservice/armappservice/v5"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/domainregistration/armdomainregistration"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/9f4cb2884f1948b879ecfb3f410e8cbc8805c213/specification/domainregistration/resource-manager/Microsoft.DomainRegistration/DomainRegistration/stable/2024-11-01/examples/ListTopLevelDomains.json
+// Generated from example definition: 2024-11-01/ListTopLevelDomains.json
 func ExampleTopLevelDomainsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	clientFactory, err := armappservice.NewClientFactory("<subscription-id>", cred, nil)
+	clientFactory, err := armdomainregistration.NewClientFactory("34adfa4f-cedf-4dc0-ba29-b6d1a69ab345", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
@@ -30,80 +30,83 @@ func ExampleTopLevelDomainsClient_NewListPager() {
 			_ = v
 		}
 		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
-		// page.TopLevelDomainCollection = armappservice.TopLevelDomainCollection{
-		// 	Value: []*armappservice.TopLevelDomain{
-		// 		{
-		// 			Name: to.Ptr("com"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/com"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(true),
+		// page = armdomainregistration.TopLevelDomainsClientListResponse{
+		// 	TopLevelDomainCollection: armdomainregistration.TopLevelDomainCollection{
+		// 		Value: []*armdomainregistration.TopLevelDomain{
+		// 			{
+		// 				Name: to.Ptr("com"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/com"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(true),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("net"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/net"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(true),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("co.uk"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/co.uk"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(false),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("org"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/org"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(true),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("nl"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/nl"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(true),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("in"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/in"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(false),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("biz"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/biz"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(true),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("org.uk"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/org.uk"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(false),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("co.in"),
+		// 				Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
+		// 				ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/co.in"),
+		// 				Properties: &armdomainregistration.TopLevelDomainProperties{
+		// 					Privacy: to.Ptr(false),
+		// 				},
 		// 			},
 		// 		},
-		// 		{
-		// 			Name: to.Ptr("net"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/net"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(true),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("co.uk"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/co.uk"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(false),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("org"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/org"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(true),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("nl"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/nl"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(true),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("in"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/in"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(false),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("biz"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/biz"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(true),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("org.uk"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/org.uk"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(false),
-		// 			},
-		// 		},
-		// 		{
-		// 			Name: to.Ptr("co.in"),
-		// 			Type: to.Ptr("Microsoft.DomainRegistration/topLevelDomains"),
-		// 			ID: to.Ptr("/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/providers/Microsoft.DomainRegistration/topLevelDomains/co.in"),
-		// 			Properties: &armappservice.TopLevelDomainProperties{
-		// 				Privacy: to.Ptr(false),
-		// 			},
-		// 	}},
+		// 	},
 		// }
 	}
 }

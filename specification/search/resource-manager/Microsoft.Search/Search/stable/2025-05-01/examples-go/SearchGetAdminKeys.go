656ef4c695194da184a99b7c3b4f34fd8defd42b
azure-sdk <azuresdk@microsoft.com>
2026-03-16 11:07:54 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/search/armsearch/v2.0.0-beta.1

---
+++ specification/search/resource-manager/Microsoft.Search/Search/stable/2025-05-01/examples-go/SearchGetAdminKeys.go
@@ -0,0 +1,35 @@
+package armsearch_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/search/armsearch/v2"
+)
+
+// Generated from example definition: 2025-05-01/SearchGetAdminKeys.json
+func ExampleAdminKeysClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armsearch.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewAdminKeysClient().Get(ctx, "rg1", "mysearchservice", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armsearch.AdminKeysClientGetResponse{
+	// 	AdminKeyResult: &armsearch.AdminKeyResult{
+	// 		PrimaryKey: to.Ptr("<your primary admin API key>"),
+	// 		SecondaryKey: to.Ptr("<your secondary admin API key>"),
+	// 	},
+	// }
+}

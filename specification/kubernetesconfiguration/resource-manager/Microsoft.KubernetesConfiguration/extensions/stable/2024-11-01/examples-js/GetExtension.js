a76de412834fc0fb16ee186788cdb37b9313f682
azure-sdk <azuresdk@microsoft.com>
2026-03-19 11:19:59 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-kubernetesconfiguration-extensions_1.0.0-beta.2

--- specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/extensions/stable/2024-11-01/examples-js/GetExtension.js
+++ specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/extensions/stable/2024-11-01/examples-js/GetExtension.js
@@ -1,28 +1,22 @@
 const { ExtensionsClient } = require("@azure/arm-kubernetesconfiguration-extensions");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv/config");
 
 /**
- * This sample demonstrates how to Gets Kubernetes Cluster Extension.
+ * This sample demonstrates how to gets Kubernetes Cluster Extension.
  *
- * @summary Gets Kubernetes Cluster Extension.
- * x-ms-original-file: specification/kubernetesconfiguration/resource-manager/Microsoft.KubernetesConfiguration/extensions/stable/2024-11-01/examples/GetExtension.json
+ * @summary gets Kubernetes Cluster Extension.
+ * x-ms-original-file: 2024-11-01/GetExtension.json
  */
 async function getExtension() {
-  const subscriptionId = process.env["KUBERNETESCONFIGURATION_SUBSCRIPTION_ID"] || "subId1";
-  const resourceGroupName = process.env["KUBERNETESCONFIGURATION_RESOURCE_GROUP"] || "rg1";
-  const clusterRp = "Microsoft.Kubernetes";
-  const clusterResourceName = "connectedClusters";
-  const clusterName = "clusterName1";
-  const extensionName = "ClusterMonitor";
   const credential = new DefaultAzureCredential();
+  const subscriptionId = "subId1";
   const client = new ExtensionsClient(credential, subscriptionId);
   const result = await client.extensions.get(
-    resourceGroupName,
-    clusterRp,
-    clusterResourceName,
-    clusterName,
-    extensionName,
+    "rg1",
+    "Microsoft.Kubernetes",
+    "connectedClusters",
+    "clusterName1",
+    "ClusterMonitor",
   );
   console.log(result);
 }

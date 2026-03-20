f9d8c9488e4a414a7eecc92005bc77bd5241c9b1
azure-sdk <azuresdk@microsoft.com>
2026-03-20 11:25:12 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerregistry_12.0.0

--- specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/stable/2025-11-01/examples-js/ImportImageByManifestDigest.js
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/stable/2025-11-01/examples-js/ImportImageByManifestDigest.js
@@ -1,20 +1,17 @@
 const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv/config");
 
 /**
- * This sample demonstrates how to Copies an image to this container registry from the specified container registry.
+ * This sample demonstrates how to copies an image to this container registry from the specified container registry.
  *
- * @summary Copies an image to this container registry from the specified container registry.
- * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/stable/2025-11-01/examples/ImportImageByManifestDigest.json
+ * @summary copies an image to this container registry from the specified container registry.
+ * x-ms-original-file: 2025-11-01/ImportImageByManifestDigest.json
  */
 async function importImageByManifestDigest() {
-  const subscriptionId =
-    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
-  const resourceGroupName = process.env["CONTAINERREGISTRY_RESOURCE_GROUP"] || "myResourceGroup";
-  const registryName = "myRegistry";
-  const parameters = {
-    mode: "Force",
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  await client.registries.importImage("myResourceGroup", "myRegistry", {
     source: {
       resourceId:
         "/subscriptions/10000000-0000-0000-0000-000000000000/resourceGroups/sourceResourceGroup/providers/Microsoft.ContainerRegistry/registries/sourceRegistry",
@@ -23,13 +20,6 @@ async function importImageByManifestDigest() {
     },
     targetTags: ["targetRepository:targetTag"],
     untaggedTargetRepositories: ["targetRepository1"],
-  };
-  const credential = new DefaultAzureCredential();
-  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
-  const result = await client.registries.beginImportImageAndWait(
-    resourceGroupName,
-    registryName,
-    parameters,
-  );
-  console.log(result);
+    mode: "Force",
+  });
 }

2994dcde2fcc14205af9925f4cf365a5708b53c2
azure-sdk <azuresdk@microsoft.com>
2026-03-21 11:37:05 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-keyvault_14.0.0

---
+++ specification/keyvault/resource-manager/Microsoft.KeyVault/KeyVault/stable/2026-02-01/examples-python/getKeyVersion.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.keyvault import KeyVaultManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-keyvault
+# USAGE
+    python get_key_version.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = KeyVaultManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.keys.get_version(
+        resource_group_name="sample-group",
+        vault_name="sample-vault-name",
+        key_name="sample-key-name",
+        key_version="fd618d9519b74f9aae94ade66b876acc",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2026-02-01/getKeyVersion.json
+if __name__ == "__main__":
+    main()

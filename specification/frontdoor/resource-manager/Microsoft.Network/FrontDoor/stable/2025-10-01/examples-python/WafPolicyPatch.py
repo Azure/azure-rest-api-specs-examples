89492976b54eb6b40a001cd935b1d916911a283e
azure-sdk <azuresdk@microsoft.com>
2026-03-26 11:27:09 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-frontdoor_2.0.0b1

---
+++ specification/frontdoor/resource-manager/Microsoft.Network/FrontDoor/stable/2025-10-01/examples-python/WafPolicyPatch.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.frontdoor import FrontDoorManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-frontdoor
+# USAGE
+    python waf_policy_patch.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = FrontDoorManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.policies.begin_update(
+        resource_group_name="rg1",
+        policy_name="Policy1",
+        parameters={"tags": {"key1": "value1", "key2": "value2"}},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2025-10-01/WafPolicyPatch.json
+if __name__ == "__main__":
+    main()

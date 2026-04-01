0089d1c47b8e6755fafa34fbef776619fc8f439c
azure-sdk <azuresdk@microsoft.com>
2026-04-01 12:04:47 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-search_10.0.0b1

---
+++ specification/search/resource-manager/Microsoft.Search/Search/preview/2026-03-01-preview/examples-python/SearchCheckNameAvailability.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python search_check_name_availability.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.services.check_name_availability(
+        check_name_availability_input={"name": "mysearchservice", "type": "searchServices"},
+    )
+    print(response)
+
+
+# x-ms-original-file: 2026-03-01-preview/SearchCheckNameAvailability.json
+if __name__ == "__main__":
+    main()
---
+++ specification/search/resource-manager/Microsoft.Search/Search/preview/2026-03-01-preview/examples-python/SearchUpdateServiceToAllowAccessFromPublicCustomIPsAndBypass.py
@@ -0,0 +1,45 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.search import SearchManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-search
+# USAGE
+    python search_update_service_to_allow_access_from_public_custom_ips_and_bypass.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SearchManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.services.update(
+        resource_group_name="rg1",
+        search_service_name="mysearchservice",
+        service={
+            "properties": {
+                "networkRuleSet": {
+                    "bypass": "AzurePortal",
+                    "ipRules": [{"value": "123.4.5.6"}, {"value": "123.4.6.0/18"}],
+                },
+                "partitionCount": 1,
+                "publicNetworkAccess": "Enabled",
+                "replicaCount": 3,
+            }
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: 2026-03-01-preview/SearchUpdateServiceToAllowAccessFromPublicCustomIPsAndBypass.json
+if __name__ == "__main__":
+    main()

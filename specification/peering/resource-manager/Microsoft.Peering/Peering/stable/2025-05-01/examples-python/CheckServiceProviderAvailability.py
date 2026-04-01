fbf91239490db227d035ff40fca2d388db6e8055
azure-sdk <azuresdk@microsoft.com>
2026-04-01 12:07:58 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-peering_2.0.0b2

---
+++ specification/peering/resource-manager/Microsoft.Peering/Peering/stable/2025-05-01/examples-python/CheckServiceProviderAvailability.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.peering import PeeringManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-peering
+# USAGE
+    python check_service_provider_availability.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PeeringManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.check_service_provider_availability(
+        check_service_provider_availability_input={
+            "peeringServiceLocation": "peeringServiceLocation1",
+            "peeringServiceProvider": "peeringServiceProvider1",
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: 2025-05-01/CheckServiceProviderAvailability.json
+if __name__ == "__main__":
+    main()

34890c1bd504a863a132757469f2a14e35b56f87
azure-sdk <azuresdk@microsoft.com>
2026-04-01 11:45:18 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v9.1.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-01-02-preview/examples-go/OperationStatusResultList.go
@@ -0,0 +1,58 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-01-02-preview/OperationStatusResultList.json
+func ExampleOperationStatusResultClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewOperationStatusResultClient().NewListPager("rg1", "clustername1", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armcontainerservice.OperationStatusResultClientListResponse{
+		// 	OperationStatusResultList: armcontainerservice.OperationStatusResultList{
+		// 		Value: []*armcontainerservice.OperationStatusResult{
+		// 			{
+		// 				Name: to.Ptr("d11edb09-6e27-429f-9fe5-17baf773bc4a"),
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/operations/d11edb09-6e27-429f-9fe5-17baf773bc4a"),
+		// 				PercentComplete: to.Ptr[float64](40),
+		// 				StartTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-26T12:14:26.3179428Z"); return t}()),
+		// 				Status: to.Ptr("InProgress"),
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("d11edb09-6e27-429f-9fe5-17baf773bc4b"),
+		// 				EndTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-26T12:14:50.3179428Z"); return t}()),
+		// 				Error: &armcontainerservice.ErrorDetail{
+		// 					Code: to.Ptr("ReconcileAgentPoolIdentityError"),
+		// 					Message: to.Ptr("Reconcile agent pool nodepool1 identity failed"),
+		// 				},
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/operations/d11edb09-6e27-429f-9fe5-17baf773bc4b"),
+		// 				StartTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2023-07-26T12:14:26.3179428Z"); return t}()),
+		// 				Status: to.Ptr("Failed"),
+		// 			},
+		// 		},
+		// 	},
+		// }
+	}
+}

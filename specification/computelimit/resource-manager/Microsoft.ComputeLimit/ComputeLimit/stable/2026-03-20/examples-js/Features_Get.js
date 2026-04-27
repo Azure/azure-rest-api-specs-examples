98469d09a0aa9efe9e426a42badaf3fc3c3149f2
azure-sdk <azuresdk@microsoft.com>
2026-04-27 05:17:46 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-computelimit_1.0.0

---
+++ specification/computelimit/resource-manager/Microsoft.ComputeLimit/ComputeLimit/stable/2026-03-20/examples-js/Features_Get.js
@@ -0,0 +1,16 @@
+const { ComputeLimitClient } = require("@azure/arm-computelimit");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets the properties of a compute limit feature.
+ *
+ * @summary gets the properties of a compute limit feature.
+ * x-ms-original-file: 2026-03-20/Features_Get.json
+ */
+async function getFeature() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "74219ad7-63fc-442f-8037-4b43c627c07d";
+  const client = new ComputeLimitClient(credential, subscriptionId);
+  const result = await client.features.get("eastus", "VmCategoryQuota");
+  console.log(result);
+}

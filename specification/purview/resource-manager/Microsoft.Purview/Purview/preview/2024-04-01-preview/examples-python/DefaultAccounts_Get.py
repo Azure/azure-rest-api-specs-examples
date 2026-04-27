45aa324a9ec92fd8b9319130ed77cf5301f76b30
azure-sdk <azuresdk@microsoft.com>
2026-04-27 08:14:21 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-purview_1.1.0b2

---
+++ specification/purview/resource-manager/Microsoft.Purview/Purview/preview/2024-04-01-preview/examples-python/DefaultAccounts_Get.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.purview import PurviewManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-purview
+# USAGE
+    python default_accounts_get.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PurviewManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.default_accounts.get(
+        scope_tenant_id="ee85a74c-405e-4adc-bb47-ffa8ca0c9f31",
+        scope_type="Tenant",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2024-04-01-preview/DefaultAccounts_Get.json
+if __name__ == "__main__":
+    main()
---
+++ specification/purview/resource-manager/Microsoft.Purview/Purview/preview/2024-04-01-preview/examples-python/PrivateEndpointConnections_CreateOrUpdate.py
@@ -0,0 +1,43 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.purview import PurviewManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-purview
+# USAGE
+    python private_endpoint_connections_create_or_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PurviewManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.private_endpoint_connections.begin_create_or_update(
+        resource_group_name="SampleResourceGroup",
+        account_name="account1",
+        private_endpoint_connection_name="privateEndpointConnection1",
+        request={
+            "properties": {
+                "privateLinkServiceConnectionState": {
+                    "description": "Approved by `johndoe@company.com <mailto:johndoe@company.com>`_",
+                    "status": "Approved",
+                }
+            }
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2024-04-01-preview/PrivateEndpointConnections_CreateOrUpdate.json
+if __name__ == "__main__":
+    main()

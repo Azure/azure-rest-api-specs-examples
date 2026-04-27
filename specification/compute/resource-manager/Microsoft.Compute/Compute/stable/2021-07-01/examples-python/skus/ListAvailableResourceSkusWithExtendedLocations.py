33d48fbcd0ace172f85a89293a2003b4c58047d1
azure-sdk <azuresdk@microsoft.com>
2026-04-27 08:09:31 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-compute_38.0.0b1

---
+++ specification/compute/resource-manager/Microsoft.Compute/Compute/stable/2021-07-01/examples-python/skus/ListAvailableResourceSkusWithExtendedLocations.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.compute import ComputeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-compute
+# USAGE
+    python list_available_resource_skus_with_extended_locations.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ComputeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.resource_skus.list()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2021-07-01/skus/ListAvailableResourceSkusWithExtendedLocations.json
+if __name__ == "__main__":
+    main()

7ff0ee739a82ec95b1816dc23ae9327a3adfd6d1
azure-sdk <azuresdk@microsoft.com>
2026-04-27 08:12:45 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-maps_3.0.0b1

---
+++ specification/maps/resource-manager/Microsoft.Maps/Maps/preview/2025-10-01-preview/examples-python/CreateAccountGen2.py
@@ -0,0 +1,45 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.maps import AzureMapsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-maps
+# USAGE
+    python create_account_gen2.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureMapsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.accounts.create_or_update(
+        resource_group_name="myResourceGroup",
+        account_name="myMapsAccount",
+        maps_account={
+            "kind": "Gen2",
+            "location": "eastus",
+            "properties": {
+                "cors": {"corsRules": [{"allowedOrigins": ["http://www.contoso.com", "http://www.fabrikam.com"]}]},
+                "disableLocalAuth": True,
+                "locations": [{"locationName": "northeurope"}],
+            },
+            "sku": {"name": "G2"},
+            "tags": {"test": "true"},
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: 2025-10-01-preview/CreateAccountGen2.json
+if __name__ == "__main__":
+    main()

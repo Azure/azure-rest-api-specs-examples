97be5f027df2f5fd1b4e5b6805c5e0be20f680fa
azure-sdk <azuresdk@microsoft.com>
2026-04-27 05:15:46 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_25.1.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2026-02-01/examples-js/ManagedClustersCreate_Premium.js
@@ -0,0 +1,47 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to creates or updates a managed cluster.
+ *
+ * @summary creates or updates a managed cluster.
+ * x-ms-original-file: 2026-02-01/ManagedClustersCreate_Premium.json
+ */
+async function createManagedClusterWithLongTermSupport() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.createOrUpdate("rg1", "clustername1", {
+    location: "location1",
+    addonProfiles: {},
+    agentPoolProfiles: [
+      {
+        name: "nodepool1",
+        type: "VirtualMachineScaleSets",
+        count: 3,
+        enableEncryptionAtHost: true,
+        enableNodePublicIP: true,
+        mode: "System",
+        osType: "Linux",
+        vmSize: "Standard_DS2_v2",
+      },
+    ],
+    apiServerAccessProfile: { disableRunCommand: true },
+    autoScalerProfile: { scaleDownDelayAfterAdd: "15m", scanInterval: "20s" },
+    dnsPrefix: "dnsprefix1",
+    enableRbac: true,
+    kubernetesVersion: "",
+    linuxProfile: { adminUsername: "azureuser", ssh: { publicKeys: [{ keyData: "keydata" }] } },
+    networkProfile: {
+      loadBalancerProfile: { managedOutboundIPs: { count: 2 } },
+      loadBalancerSku: "standard",
+      outboundType: "loadBalancer",
+    },
+    servicePrincipalProfile: { clientId: "clientid", secret: "secret" },
+    supportPlan: "AKSLongTermSupport",
+    windowsProfile: { adminPassword: "replacePassword1234$", adminUsername: "azureuser" },
+    sku: { name: "Base", tier: "Premium" },
+    tags: { archv2: "", tier: "production" },
+  });
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2026-02-01/examples-js/ManagedClustersCreate_UserAssignedNATGateway.js
@@ -0,0 +1,42 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to creates or updates a managed cluster.
+ *
+ * @summary creates or updates a managed cluster.
+ * x-ms-original-file: 2026-02-01/ManagedClustersCreate_UserAssignedNATGateway.json
+ */
+async function createManagedClusterWithUserAssignedNATGatewayAsOutboundType() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.createOrUpdate("rg1", "clustername1", {
+    location: "location1",
+    addonProfiles: {},
+    agentPoolProfiles: [
+      {
+        name: "nodepool1",
+        type: "VirtualMachineScaleSets",
+        count: 3,
+        enableNodePublicIP: false,
+        mode: "System",
+        osType: "Linux",
+        vmSize: "Standard_DS2_v2",
+      },
+    ],
+    autoScalerProfile: { scaleDownDelayAfterAdd: "15m", scanInterval: "20s" },
+    diskEncryptionSetID:
+      "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Compute/diskEncryptionSets/des",
+    dnsPrefix: "dnsprefix1",
+    enableRbac: true,
+    kubernetesVersion: "",
+    linuxProfile: { adminUsername: "azureuser", ssh: { publicKeys: [{ keyData: "keydata" }] } },
+    networkProfile: { loadBalancerSku: "standard", outboundType: "userAssignedNATGateway" },
+    servicePrincipalProfile: { clientId: "clientid", secret: "secret" },
+    windowsProfile: { adminPassword: "replacePassword1234$", adminUsername: "azureuser" },
+    sku: { name: "Basic", tier: "Free" },
+    tags: { archv2: "", tier: "production" },
+  });
+  console.log(result);
+}

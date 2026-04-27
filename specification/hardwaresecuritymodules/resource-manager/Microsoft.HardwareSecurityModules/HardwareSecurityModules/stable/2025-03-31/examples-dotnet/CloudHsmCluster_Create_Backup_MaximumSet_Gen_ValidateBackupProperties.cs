fd5ee0bbaa77dad665dc9cddb70072a693e10a2b
azure-sdk <azuresdk@microsoft.com>
2026-04-27 05:47:45 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.HardwareSecurityModules_1.0.1

---
+++ specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/HardwareSecurityModules/stable/2025-03-31/examples-dotnet/CloudHsmCluster_Create_Backup_MaximumSet_Gen_ValidateBackupProperties.cs
@@ -0,0 +1,35 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.HardwareSecurityModules.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.HardwareSecurityModules;
+
+// Generated from example definition: 2025-03-31/CloudHsmCluster_Create_Backup_MaximumSet_Gen_ValidateBackupProperties.json
+// this example is just showing the usage of "CloudHsmClusters_ValidateBackupProperties" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this CloudHsmClusterResource created on azure
+// for more information of creating CloudHsmClusterResource, please refer to the document of CloudHsmClusterResource
+string subscriptionId = "00000000-0000-0000-0000-000000000000";
+string resourceGroupName = "rgcloudhsm";
+string cloudHsmClusterName = "chsm1";
+ResourceIdentifier cloudHsmClusterResourceId = CloudHsmClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, cloudHsmClusterName);
+CloudHsmClusterResource cloudHsmCluster = client.GetCloudHsmClusterResource(cloudHsmClusterResourceId);
+
+// invoke the operation
+CloudHsmClusterBackupContent content = new CloudHsmClusterBackupContent(new Uri("https://myaccount.blob.core.windows.net/sascontainer/sasContainer"))
+{
+    Token = "se=2018-02-01T00%3A00Z&spr=https&sv=2017-04-17&sr=b&sig=REDACTED",
+};
+ArmOperation<CloudHsmClusterBackupResult> lro = await cloudHsmCluster.ValidateBackupPropertiesAsync(WaitUntil.Completed, content: content);
+CloudHsmClusterBackupResult result = lro.Value;
+
+Console.WriteLine($"Succeeded: {result}");

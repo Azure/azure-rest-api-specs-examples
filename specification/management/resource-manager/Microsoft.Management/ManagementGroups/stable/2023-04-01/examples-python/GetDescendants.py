df63bf200beee65fedb117ece91149d4432977e9
azure-sdk <azuresdk@microsoft.com>
2026-04-27 08:17:35 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-managementgroups_2.0.0b1

---
+++ specification/management/resource-manager/Microsoft.Management/ManagementGroups/stable/2023-04-01/examples-python/GetDescendants.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.managementgroups import ManagementGroupsMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-managementgroups
+# USAGE
+    python get_descendants.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ManagementGroupsMgmtClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.management_groups.get_descendants(
+        group_id="20000000-0000-0000-0000-000000000000",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2023-04-01/GetDescendants.json
+if __name__ == "__main__":
+    main()

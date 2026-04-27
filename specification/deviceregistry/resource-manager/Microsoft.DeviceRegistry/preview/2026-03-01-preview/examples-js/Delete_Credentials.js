b9abc6c470ead723e3ace0127cff5e81373cc920
azure-sdk <azuresdk@microsoft.com>
2026-04-27 05:25:45 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-deviceregistry_1.2.0-beta.1

---
+++ specification/deviceregistry/resource-manager/Microsoft.DeviceRegistry/preview/2026-03-01-preview/examples-js/Delete_Credentials.js
@@ -0,0 +1,15 @@
+const { DeviceRegistryManagementClient } = require("@azure/arm-deviceregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a Credential
+ *
+ * @summary delete a Credential
+ * x-ms-original-file: 2026-03-01-preview/Delete_Credentials.json
+ */
+async function deleteCredentials() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DeviceRegistryManagementClient(credential, subscriptionId);
+  await client.credentials.delete("rgdeviceregistry", "mynamespace");
+}

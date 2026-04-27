76a828d2a1292bb761a13ebfda68596882b655f6
azure-sdk <azuresdk@microsoft.com>
2026-04-27 05:13:43 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-relationships_1.0.0-beta.1

---
+++ specification/relationships/resource-manager/Microsoft.Relationships/preview/2023-09-01-preview/examples-js/Operations_List_MaximumSet_Gen.js
@@ -0,0 +1,19 @@
+const { RelationshipsClient } = require("@azure/arm-relationships");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2023-09-01-preview/Operations_List_MaximumSet_Gen.json
+ */
+async function operationsList() {
+  const credential = new DefaultAzureCredential();
+  const client = new RelationshipsClient(credential);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

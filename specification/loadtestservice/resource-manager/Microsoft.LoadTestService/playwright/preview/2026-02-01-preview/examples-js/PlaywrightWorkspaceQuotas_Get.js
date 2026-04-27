744deb35ac35038712ba11b3ada9f8ac5e2c3413
azure-sdk <azuresdk@microsoft.com>
2026-04-27 05:29:44 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-playwright_1.1.0-beta.1

---
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/playwright/preview/2026-02-01-preview/examples-js/PlaywrightWorkspaceQuotas_Get.js
@@ -0,0 +1,20 @@
+const { PlaywrightManagementClient } = require("@azure/arm-playwright");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets a Playwright workspace quota resource by name.
+ *
+ * @summary gets a Playwright workspace quota resource by name.
+ * x-ms-original-file: 2026-02-01-preview/PlaywrightWorkspaceQuotas_Get.json
+ */
+async function playwrightWorkspaceQuotasGet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new PlaywrightManagementClient(credential, subscriptionId);
+  const result = await client.playwrightWorkspaceQuotas.get(
+    "dummyrg",
+    "myWorkspace",
+    "ExecutionMinutes",
+  );
+  console.log(result);
+}
---
+++ specification/loadtestservice/resource-manager/Microsoft.LoadTestService/playwright/preview/2026-02-01-preview/examples-js/PlaywrightWorkspaces_ListBySubscription.js
@@ -0,0 +1,20 @@
+const { PlaywrightManagementClient } = require("@azure/arm-playwright");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list PlaywrightWorkspace resources by subscription ID
+ *
+ * @summary list PlaywrightWorkspace resources by subscription ID
+ * x-ms-original-file: 2026-02-01-preview/PlaywrightWorkspaces_ListBySubscription.json
+ */
+async function playwrightWorkspacesListBySubscription() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new PlaywrightManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.playwrightWorkspaces.listBySubscription()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

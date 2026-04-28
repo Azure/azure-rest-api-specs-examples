a8adb3ceb9f86b5e9e2861dfd53e00e366ffcd47
azure-sdk <azuresdk@microsoft.com>
2026-04-28 11:14:27 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-monitorslis_1.0.0-beta.1

---
+++ specification/monitoringservice/resource-manager/Microsoft.Monitor/Slis/preview/2025-03-01-preview/examples-js/Slis_Delete.js
@@ -0,0 +1,14 @@
+const { MonitorClient } = require("@azure/arm-monitorslis");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to deletes an SLI resource.
+ *
+ * @summary deletes an SLI resource.
+ * x-ms-original-file: 2025-03-01-preview/Slis_Delete.json
+ */
+async function deleteSli() {
+  const credential = new DefaultAzureCredential();
+  const client = new MonitorClient(credential);
+  await client.slis.delete("testSG", "testSli");
+}
---
+++ specification/monitoringservice/resource-manager/Microsoft.Monitor/Slis/preview/2025-03-01-preview/examples-js/Slis_ListByParent.js
@@ -0,0 +1,19 @@
+const { MonitorClient } = require("@azure/arm-monitorslis");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to lists all SLI resources under a parent resource.
+ *
+ * @summary lists all SLI resources under a parent resource.
+ * x-ms-original-file: 2025-03-01-preview/Slis_ListByParent.json
+ */
+async function slisListByParent() {
+  const credential = new DefaultAzureCredential();
+  const client = new MonitorClient(credential);
+  const resArray = new Array();
+  for await (const item of client.slis.listByParent("testSG")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

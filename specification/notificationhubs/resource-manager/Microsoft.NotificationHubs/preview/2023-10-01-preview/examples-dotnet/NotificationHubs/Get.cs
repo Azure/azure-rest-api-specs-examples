12b050e09be9cbc8a41eb8fbe9a6d474841fb8ea
azure-sdk <azuresdk@microsoft.com>
2026-04-29 12:11:05 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.NotificationHubs_1.2.0-beta.3

--- specification/notificationhubs/resource-manager/Microsoft.NotificationHubs/preview/2023-10-01-preview/examples-dotnet/NotificationHubs/Get.cs
+++ specification/notificationhubs/resource-manager/Microsoft.NotificationHubs/preview/2023-10-01-preview/examples-dotnet/NotificationHubs/Get.cs
@@ -2,10 +2,8 @@ using Azure;
 using Azure.ResourceManager;
 using System;
 using System.Threading.Tasks;
-using System.Xml;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.NotificationHubs.Models;
 using Azure.ResourceManager.NotificationHubs;
 
 // Generated from example definition: specification/notificationhubs/resource-manager/Microsoft.NotificationHubs/preview/2023-10-01-preview/examples/NotificationHubs/Get.json
@@ -16,20 +14,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this NotificationHubResource created on azure
-// for more information of creating NotificationHubResource, please refer to the document of NotificationHubResource
+// this example assumes you already have this NotificationHubNamespaceResource created on azure
+// for more information of creating NotificationHubNamespaceResource, please refer to the document of NotificationHubNamespaceResource
 string subscriptionId = "29cfa613-cbbc-4512-b1d6-1b3a92c7fa40";
 string resourceGroupName = "5ktrial";
 string namespaceName = "nh-sdk-ns";
-string notificationHubName = "nh-sdk-hub";
-ResourceIdentifier notificationHubResourceId = NotificationHubResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName, notificationHubName);
-NotificationHubResource notificationHub = client.GetNotificationHubResource(notificationHubResourceId);
+ResourceIdentifier notificationHubNamespaceResourceId = NotificationHubNamespaceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName);
+NotificationHubNamespaceResource notificationHubNamespace = client.GetNotificationHubNamespaceResource(notificationHubNamespaceResourceId);
+
+// get the collection of this NotificationHubResource
+NotificationHubCollection collection = notificationHubNamespace.GetNotificationHubs();
 
 // invoke the operation
-NotificationHubResource result = await notificationHub.GetAsync();
+string notificationHubName = "nh-sdk-hub";
+NullableResponse<NotificationHubResource> response = await collection.GetIfExistsAsync(notificationHubName);
+NotificationHubResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-NotificationHubData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    NotificationHubData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

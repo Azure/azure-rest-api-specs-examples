7a7b1d40700842e51436ad202bc5b9e9e59c0294
azure-sdk <azuresdk@microsoft.com>
2026-04-29 11:20:25 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-recoveryservices-siterecovery_6.0.0-beta.1

---
+++ specification/recoveryservicessiterecovery/resource-manager/Microsoft.RecoveryServices/SiteRecovery/stable/2025-08-01/examples-js/ReplicationPolicies_Get.js
@@ -0,0 +1,20 @@
+const { SiteRecoveryManagementClient } = require("@azure/arm-recoveryservices-siterecovery");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets the details of a replication policy.
+ *
+ * @summary gets the details of a replication policy.
+ * x-ms-original-file: 2025-08-01/ReplicationPolicies_Get.json
+ */
+async function getsTheRequestedPolicy() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "c183865e-6077-46f2-a3b1-deb0f4f4650a";
+  const client = new SiteRecoveryManagementClient(credential, subscriptionId);
+  const result = await client.replicationPolicies.get(
+    "resourceGroupPS1",
+    "vault1",
+    "protectionprofile1",
+  );
+  console.log(result);
+}
---
+++ specification/recoveryservicessiterecovery/resource-manager/Microsoft.RecoveryServices/SiteRecovery/stable/2025-08-01/examples-js/ReplicationProtectionClusters_Get.js
@@ -0,0 +1,22 @@
+const { SiteRecoveryManagementClient } = require("@azure/arm-recoveryservices-siterecovery");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets the details of an ASR replication protection cluster.
+ *
+ * @summary gets the details of an ASR replication protection cluster.
+ * x-ms-original-file: 2025-08-01/ReplicationProtectionClusters_Get.json
+ */
+async function getsTheDetailsOfAReplicationProtectionCluster() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "c183865e-6077-46f2-a3b1-deb0f4f4650a";
+  const client = new SiteRecoveryManagementClient(credential, subscriptionId);
+  const result = await client.replicationProtectionClusters.get(
+    "resourceGroupPS1",
+    "vault1",
+    "eastus",
+    "eastus-container",
+    "cluster1",
+  );
+  console.log(result);
+}

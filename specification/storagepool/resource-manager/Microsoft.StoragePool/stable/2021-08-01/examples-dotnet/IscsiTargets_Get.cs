a30a8dd46cac2726562ff1f607c1cc7ee1cd3325
azure-sdk <azuresdk@microsoft.com>
2026-04-30 12:20:55 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.StoragePool_1.1.2

--- specification/storagepool/resource-manager/Microsoft.StoragePool/stable/2021-08-01/examples-dotnet/IscsiTargets_Get.cs
+++ specification/storagepool/resource-manager/Microsoft.StoragePool/stable/2021-08-01/examples-dotnet/IscsiTargets_Get.cs
@@ -15,20 +15,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this DiskPoolIscsiTargetResource created on azure
-// for more information of creating DiskPoolIscsiTargetResource, please refer to the document of DiskPoolIscsiTargetResource
+// this example assumes you already have this DiskPoolResource created on azure
+// for more information of creating DiskPoolResource, please refer to the document of DiskPoolResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
 string diskPoolName = "myDiskPool";
-string iscsiTargetName = "myIscsiTarget";
-ResourceIdentifier diskPoolIscsiTargetResourceId = DiskPoolIscsiTargetResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, diskPoolName, iscsiTargetName);
-DiskPoolIscsiTargetResource diskPoolIscsiTarget = client.GetDiskPoolIscsiTargetResource(diskPoolIscsiTargetResourceId);
+ResourceIdentifier diskPoolResourceId = DiskPoolResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, diskPoolName);
+DiskPoolResource diskPool = client.GetDiskPoolResource(diskPoolResourceId);
+
+// get the collection of this DiskPoolIscsiTargetResource
+DiskPoolIscsiTargetCollection collection = diskPool.GetDiskPoolIscsiTargets();
 
 // invoke the operation
-DiskPoolIscsiTargetResource result = await diskPoolIscsiTarget.GetAsync();
+string iscsiTargetName = "myIscsiTarget";
+NullableResponse<DiskPoolIscsiTargetResource> response = await collection.GetIfExistsAsync(iscsiTargetName);
+DiskPoolIscsiTargetResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-DiskPoolIscsiTargetData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    DiskPoolIscsiTargetData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

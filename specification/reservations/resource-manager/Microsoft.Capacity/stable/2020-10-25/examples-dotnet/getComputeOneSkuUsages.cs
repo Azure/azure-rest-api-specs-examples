a3692b79a501ea4637a584662230bfaa77e16765
azure-sdk <azuresdk@microsoft.com>
2026-04-30 11:59:26 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Reservations_1.4.2

--- specification/reservations/resource-manager/Microsoft.Capacity/stable/2020-10-25/examples-dotnet/getComputeOneSkuUsages.cs
+++ specification/reservations/resource-manager/Microsoft.Capacity/stable/2020-10-25/examples-dotnet/getComputeOneSkuUsages.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager.Reservations.Models;
+using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Reservations;
 
 // Generated from example definition: specification/reservations/resource-manager/Microsoft.Capacity/stable/2020-10-25/examples/getComputeOneSkuUsages.json
@@ -15,20 +16,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ReservationQuotaResource created on azure
-// for more information of creating ReservationQuotaResource, please refer to the document of ReservationQuotaResource
+// this example assumes you already have this SubscriptionResource created on azure
+// for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
+ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
+SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
+
+// get the collection of this ReservationQuotaResource
 string providerId = "Microsoft.Compute";
 AzureLocation location = new AzureLocation("eastus");
-string resourceName = "standardNDSFamily";
-ResourceIdentifier reservationQuotaResourceId = ReservationQuotaResource.CreateResourceIdentifier(subscriptionId, providerId, location, resourceName);
-ReservationQuotaResource reservationQuota = client.GetReservationQuotaResource(reservationQuotaResourceId);
+ReservationQuotaCollection collection = subscriptionResource.GetAllReservationQuota(providerId, location);
 
 // invoke the operation
-ReservationQuotaResource result = await reservationQuota.GetAsync();
+string resourceName = "standardNDSFamily";
+NullableResponse<ReservationQuotaResource> response = await collection.GetIfExistsAsync(resourceName);
+ReservationQuotaResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ReservationQuotaData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ReservationQuotaData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/reservations/resource-manager/Microsoft.Capacity/stable/2022-11-01/examples-dotnet/GetReservationOrderDetailsWithExpandPlanInformation.cs
+++ specification/reservations/resource-manager/Microsoft.Capacity/stable/2022-11-01/examples-dotnet/GetReservationOrderDetailsWithExpandPlanInformation.cs
@@ -1,11 +1,11 @@
 using Azure;
 using Azure.ResourceManager;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager.Reservations.Models;
-using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Reservations;
 
 // Generated from example definition: specification/reservations/resource-manager/Microsoft.Capacity/stable/2022-11-01/examples/GetReservationOrderDetailsWithExpandPlanInformation.json
@@ -16,26 +16,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
-
-// get the collection of this ReservationOrderResource
-ReservationOrderCollection collection = tenantResource.GetReservationOrders();
+// this example assumes you already have this ReservationOrderResource created on azure
+// for more information of creating ReservationOrderResource, please refer to the document of ReservationOrderResource
+Guid reservationOrderId = Guid.Parse("a075419f-44cc-497f-b68a-14ee811d48b9");
+ResourceIdentifier reservationOrderResourceId = ReservationOrderResource.CreateResourceIdentifier(reservationOrderId);
+ReservationOrderResource reservationOrder = client.GetReservationOrderResource(reservationOrderResourceId);
 
 // invoke the operation
-Guid reservationOrderId = Guid.Parse("a075419f-44cc-497f-b68a-14ee811d48b9");
 string expand = "schedule";
-NullableResponse<ReservationOrderResource> response = await collection.GetIfExistsAsync(reservationOrderId, expand: expand);
-ReservationOrderResource result = response.HasValue ? response.Value : null;
+ReservationOrderResource result = await reservationOrder.GetAsync(expand: expand);
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    ReservationOrderData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ReservationOrderData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

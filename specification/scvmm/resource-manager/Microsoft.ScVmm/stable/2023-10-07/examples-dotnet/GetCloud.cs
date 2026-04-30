07a9dab593fa4de56e073e6c3d49b3718e0bdde3
azure-sdk <azuresdk@microsoft.com>
2026-04-30 11:45:37 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ScVmm_1.0.0

--- specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples-dotnet/GetCloud.cs
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples-dotnet/GetCloud.cs
@@ -4,7 +4,8 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.ScVmm.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
 using Azure.ResourceManager.ScVmm;
 
 // Generated from example definition: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/GetCloud.json
@@ -15,19 +16,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ScVmmCloudResource created on azure
-// for more information of creating ScVmmCloudResource, please refer to the document of ScVmmCloudResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
 string resourceGroupName = "testrg";
-string cloudResourceName = "HRCloud";
-ResourceIdentifier scVmmCloudResourceId = ScVmmCloudResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, cloudResourceName);
-ScVmmCloudResource scVmmCloud = client.GetScVmmCloudResource(scVmmCloudResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ScVmmCloudResource
+ScVmmCloudCollection collection = resourceGroupResource.GetScVmmClouds();
 
 // invoke the operation
-ScVmmCloudResource result = await scVmmCloud.GetAsync();
+string cloudResourceName = "HRCloud";
+NullableResponse<ScVmmCloudResource> response = await collection.GetIfExistsAsync(cloudResourceName);
+ScVmmCloudResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ScVmmCloudData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ScVmmCloudData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}
--- specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples-dotnet/GetVirtualMachineTemplate.cs
+++ specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples-dotnet/GetVirtualMachineTemplate.cs
@@ -4,7 +4,8 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.ScVmm.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
 using Azure.ResourceManager.ScVmm;
 
 // Generated from example definition: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/GetVirtualMachineTemplate.json
@@ -15,19 +16,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ScVmmVirtualMachineTemplateResource created on azure
-// for more information of creating ScVmmVirtualMachineTemplateResource, please refer to the document of ScVmmVirtualMachineTemplateResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
 string resourceGroupName = "testrg";
-string virtualMachineTemplateName = "HRVirtualMachineTemplate";
-ResourceIdentifier scVmmVirtualMachineTemplateResourceId = ScVmmVirtualMachineTemplateResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, virtualMachineTemplateName);
-ScVmmVirtualMachineTemplateResource scVmmVirtualMachineTemplate = client.GetScVmmVirtualMachineTemplateResource(scVmmVirtualMachineTemplateResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ScVmmVirtualMachineTemplateResource
+ScVmmVirtualMachineTemplateCollection collection = resourceGroupResource.GetScVmmVirtualMachineTemplates();
 
 // invoke the operation
-ScVmmVirtualMachineTemplateResource result = await scVmmVirtualMachineTemplate.GetAsync();
+string virtualMachineTemplateName = "HRVirtualMachineTemplate";
+NullableResponse<ScVmmVirtualMachineTemplateResource> response = await collection.GetIfExistsAsync(virtualMachineTemplateName);
+ScVmmVirtualMachineTemplateResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ScVmmVirtualMachineTemplateData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ScVmmVirtualMachineTemplateData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

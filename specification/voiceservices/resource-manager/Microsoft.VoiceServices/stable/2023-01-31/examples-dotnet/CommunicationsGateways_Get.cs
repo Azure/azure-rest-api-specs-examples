503730b9e260b09307371b927c11d0e396b009c3
azure-sdk <azuresdk@microsoft.com>
2026-04-30 12:15:08 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.VoiceServices_1.0.3

--- specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples-dotnet/CommunicationsGateways_Get.cs
+++ specification/voiceservices/resource-manager/Microsoft.VoiceServices/stable/2023-01-31/examples-dotnet/CommunicationsGateways_Get.cs
@@ -4,7 +4,6 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.VoiceServices.Models;
 using Azure.ResourceManager.VoiceServices;
 
@@ -16,30 +15,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupResource created on azure
-// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
+// this example assumes you already have this VoiceServicesCommunicationsGatewayResource created on azure
+// for more information of creating VoiceServicesCommunicationsGatewayResource, please refer to the document of VoiceServicesCommunicationsGatewayResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "testrg";
-ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-// get the collection of this VoiceServicesCommunicationsGatewayResource
-VoiceServicesCommunicationsGatewayCollection collection = resourceGroupResource.GetVoiceServicesCommunicationsGateways();
+string communicationsGatewayName = "myname";
+ResourceIdentifier voiceServicesCommunicationsGatewayResourceId = VoiceServicesCommunicationsGatewayResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, communicationsGatewayName);
+VoiceServicesCommunicationsGatewayResource voiceServicesCommunicationsGateway = client.GetVoiceServicesCommunicationsGatewayResource(voiceServicesCommunicationsGatewayResourceId);
 
 // invoke the operation
-string communicationsGatewayName = "myname";
-NullableResponse<VoiceServicesCommunicationsGatewayResource> response = await collection.GetIfExistsAsync(communicationsGatewayName);
-VoiceServicesCommunicationsGatewayResource result = response.HasValue ? response.Value : null;
+VoiceServicesCommunicationsGatewayResource result = await voiceServicesCommunicationsGateway.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    VoiceServicesCommunicationsGatewayData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+VoiceServicesCommunicationsGatewayData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

1327e911a77835add36a6bb5b557899ffab1bb4e
azure-sdk <azuresdk@microsoft.com>
2026-04-30 11:56:35 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.WorkloadsSapVirtualInstance_1.0.0

--- specification/workloads/resource-manager/Microsoft.Workloads/SAPVirtualInstance/stable/2024-09-01/examples-dotnet/SapVirtualInstances_Get.cs
+++ specification/workloads/resource-manager/Microsoft.Workloads/SAPVirtualInstance/stable/2024-09-01/examples-dotnet/SapVirtualInstances_Get.cs
@@ -4,7 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.WorkloadsSapVirtualInstance.Models;
+using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.WorkloadsSapVirtualInstance;
 
 // Generated from example definition: 2024-09-01/SapVirtualInstances_Get.json
@@ -15,19 +15,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this SapVirtualInstanceResource created on azure
-// for more information of creating SapVirtualInstanceResource, please refer to the document of SapVirtualInstanceResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "8e17e36c-42e9-4cd5-a078-7b44883414e0";
 string resourceGroupName = "test-rg";
-string sapVirtualInstanceName = "X00";
-ResourceIdentifier sapVirtualInstanceResourceId = SapVirtualInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, sapVirtualInstanceName);
-SapVirtualInstanceResource sapVirtualInstance = client.GetSapVirtualInstanceResource(sapVirtualInstanceResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this SapVirtualInstanceResource
+SapVirtualInstanceCollection collection = resourceGroupResource.GetSapVirtualInstances();
 
 // invoke the operation
-SapVirtualInstanceResource result = await sapVirtualInstance.GetAsync();
+string sapVirtualInstanceName = "X00";
+NullableResponse<SapVirtualInstanceResource> response = await collection.GetIfExistsAsync(sapVirtualInstanceName);
+SapVirtualInstanceResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-SapVirtualInstanceData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    SapVirtualInstanceData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

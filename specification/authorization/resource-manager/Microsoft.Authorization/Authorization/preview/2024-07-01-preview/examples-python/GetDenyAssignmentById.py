75c917b9461abea9c496c601a24b3431026cfc62
azure-sdk <azuresdk@microsoft.com>
2026-05-07 12:22:05 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-authorization_5.0.0b2

---
+++ specification/authorization/resource-manager/Microsoft.Authorization/Authorization/preview/2024-07-01-preview/examples-python/GetDenyAssignmentById.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.authorization import AuthorizationManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-authorization
+# USAGE
+    python get_deny_assignment_by_id.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AuthorizationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.deny_assignments.get_by_id(
+        deny_assignment_id="subscriptions/a925f2f7-5c63-4b7b-8799-25a5f97bc3b2/resourceGroups/rgname/providers/Microsoft.Authorization/denyAssignments/denyAssignmentId",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2024-07-01-preview/GetDenyAssignmentById.json
+if __name__ == "__main__":
+    main()
---
+++ specification/authorization/resource-manager/Microsoft.Authorization/Authorization/stable/2022-04-01/examples-python/RoleAssignments_ListForSubscription.py
@@ -0,0 +1,32 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.authorization import AuthorizationManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-authorization
+# USAGE
+    python role_assignments_list_for_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AuthorizationManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.role_assignments.list_for_subscription()
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2022-04-01/RoleAssignments_ListForSubscription.json
+if __name__ == "__main__":
+    main()

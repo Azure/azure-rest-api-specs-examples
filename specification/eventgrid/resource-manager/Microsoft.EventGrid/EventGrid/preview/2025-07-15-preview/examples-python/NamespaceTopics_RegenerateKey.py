15de50b4e94d90c912c07c99a22b6d9c3cada9c4
azure-sdk <azuresdk@microsoft.com>
2026-05-12 05:42:02 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-eventgrid_10.5.0b2

---
+++ specification/eventgrid/resource-manager/Microsoft.EventGrid/EventGrid/preview/2025-07-15-preview/examples-python/NamespaceTopics_RegenerateKey.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.eventgrid import EventGridManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-eventgrid
+# USAGE
+    python namespace_topics_regenerate_key.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = EventGridManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.namespace_topics.begin_regenerate_key(
+        resource_group_name="examplerg",
+        namespace_name="examplenamespace2",
+        topic_name="examplenamespacetopic2",
+        regenerate_key_request={"keyName": "key1"},
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2025-07-15-preview/NamespaceTopics_RegenerateKey.json
+if __name__ == "__main__":
+    main()

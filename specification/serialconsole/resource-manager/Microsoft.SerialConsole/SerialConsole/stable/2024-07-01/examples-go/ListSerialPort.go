d0ae7b9775c3c7b8802245362b63f432cbfac9c1
azure-sdk <azuresdk@microsoft.com>
2026-05-13 03:31:12 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/serialconsole/armserialconsole/v2.0.0-beta.1

---
+++ specification/serialconsole/resource-manager/Microsoft.SerialConsole/SerialConsole/stable/2024-07-01/examples-go/ListSerialPort.go
@@ -0,0 +1,44 @@
+package armserialconsole_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/serialconsole/armserialconsole/v2"
+)
+
+// Generated from example definition: 2024-07-01/ListSerialPort.json
+func ExampleSerialPortsClient_List() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armserialconsole.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewSerialPortsClient().List(ctx, "myResourceGroup", "Microsoft.Compute", "virtualMachines", "myVM", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armserialconsole.SerialPortsClientListResponse{
+	// 	SerialPortListResult: armserialconsole.SerialPortListResult{
+	// 		Value: []*armserialconsole.SerialPort{
+	// 			{
+	// 				Name: to.Ptr("0"),
+	// 				Type: to.Ptr("Microsoft.SerialConsole/serialPorts"),
+	// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/myResourceGroup/providers/Microsoft.Compute/virtualMachines/myVM/providers/Microsoft.SerialConsole/serialPorts/0"),
+	// 				Properties: &armserialconsole.SerialPortProperties{
+	// 					ConnectionState: to.Ptr(armserialconsole.SerialPortConnectionStateInactive),
+	// 					State: to.Ptr(armserialconsole.SerialPortStateEnabled),
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+}

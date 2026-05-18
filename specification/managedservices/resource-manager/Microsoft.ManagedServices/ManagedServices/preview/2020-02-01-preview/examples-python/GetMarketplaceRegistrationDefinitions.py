1df6134cae00dd0526aeafa243ada651b4fa7a31
azure-sdk <azuresdk@microsoft.com>
2026-05-18 11:20:51 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-managedservices_6.0.1

---
+++ specification/managedservices/resource-manager/Microsoft.ManagedServices/ManagedServices/preview/2020-02-01-preview/examples-python/GetMarketplaceRegistrationDefinitions.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.managedservices import ManagedServicesClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-managedservices
+# USAGE
+    python get_marketplace_registration_definitions.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ManagedServicesClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.marketplace_registration_definitions.list(
+        scope="subscription/0afefe50-734e-4610-8a82-a144ahf49dea",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: specification/managedservices/resource-manager/Microsoft.ManagedServices/ManagedServices/preview/2020-02-01-preview/examples/GetMarketplaceRegistrationDefinitions.json
+if __name__ == "__main__":
+    main()
---
+++ specification/managedservices/resource-manager/Microsoft.ManagedServices/ManagedServices/preview/2020-02-01-preview/examples-python/GetOperations.py
@@ -0,0 +1,30 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.managedservices import ManagedServicesClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-managedservices
+# USAGE
+    python get_operations.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ManagedServicesClient(
+        credential=DefaultAzureCredential(),
+    )
+
+    response = client.operations.list()
+    print(response)
+
+
+# x-ms-original-file: specification/managedservices/resource-manager/Microsoft.ManagedServices/ManagedServices/preview/2020-02-01-preview/examples/GetOperations.json
+if __name__ == "__main__":
+    main()

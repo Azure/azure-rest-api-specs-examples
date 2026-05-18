108aba9b5fae1b894d57bc3e74d6adbd02d17d28
azure-sdk <azuresdk@microsoft.com>
2026-05-18 11:06:20 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/confidentialledger/armconfidentialledger/v1.3.0-beta.4

---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/ConfidentialLedger/preview/2025-06-10-preview/examples-go/ConfidentialLedger_Restore.go
@@ -0,0 +1,43 @@
+package armconfidentialledger_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/confidentialledger/armconfidentialledger"
+)
+
+// Generated from example definition: 2025-06-10-preview/ConfidentialLedger_Restore.json
+func ExampleLedgerClient_BeginRestore() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armconfidentialledger.NewClientFactory("0000000-0000-0000-0000-000000000001", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewLedgerClient().BeginRestore(ctx, "DummyResourceGroupName", "DummyLedgerName", armconfidentialledger.Restore{
+		FileShareName: to.Ptr("DummyFileShareName"),
+		RestoreRegion: to.Ptr("EastUS"),
+		URI:           to.Ptr("DummySASUri"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armconfidentialledger.LedgerClientRestoreResponse{
+	// 	RestoreResponse: armconfidentialledger.RestoreResponse{
+	// 		Message: to.Ptr("Restore Created for Ledger : DummyLedgerName."),
+	// 	},
+	// }
+}
---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/ConfidentialLedger/preview/2025-06-10-preview/examples-go/Operations_Get.go
@@ -0,0 +1,50 @@
+package armconfidentialledger_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/confidentialledger/armconfidentialledger"
+)
+
+// Generated from example definition: 2025-06-10-preview/Operations_Get.json
+func ExampleOperationsClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armconfidentialledger.NewClientFactory("<subscriptionID>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armconfidentialledger.OperationsClientListResponse{
+		// 	ResourceProviderOperationList: armconfidentialledger.ResourceProviderOperationList{
+		// 		NextLink: to.Ptr("string"),
+		// 		Value: []*armconfidentialledger.ResourceProviderOperationDefinition{
+		// 			{
+		// 				Name: to.Ptr("Microsoft.ConfidentialLedger/ledgers/read"),
+		// 				Display: &armconfidentialledger.ResourceProviderOperationDisplay{
+		// 					Description: to.Ptr("View the properties of a Confidential Ledger"),
+		// 					Operation: to.Ptr("Get Confidential Ledger properties"),
+		// 					Resource: to.Ptr("ledger"),
+		// 				},
+		// 				IsDataAction: to.Ptr(false),
+		// 			},
+		// 		},
+		// 	},
+		// }
+	}
+}

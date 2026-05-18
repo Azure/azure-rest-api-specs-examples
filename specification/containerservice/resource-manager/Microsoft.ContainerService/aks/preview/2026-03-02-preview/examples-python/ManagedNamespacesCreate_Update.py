235da997290adb7313b7b462bd22960f60fac83e
azure-sdk <azuresdk@microsoft.com>
2026-05-18 11:17:31 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-containerservice_41.3.0b1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-03-02-preview/examples-python/ManagedNamespacesCreate_Update.py
@@ -0,0 +1,52 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.containerservice import ContainerServiceClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-containerservice
+# USAGE
+    python managed_namespaces_create_update.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerServiceClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.managed_namespaces.begin_create_or_update(
+        resource_group_name="rg1",
+        resource_name="clustername1",
+        managed_namespace_name="namespace1",
+        parameters={
+            "location": "eastus2",
+            "properties": {
+                "adoptionPolicy": "IfIdentical",
+                "annotations": {"annatationKey": "annatationValue"},
+                "defaultNetworkPolicy": {"egress": "AllowAll", "ingress": "AllowSameNamespace"},
+                "defaultResourceQuota": {
+                    "cpuLimit": "3m",
+                    "cpuRequest": "3m",
+                    "memoryLimit": "5Gi",
+                    "memoryRequest": "5Gi",
+                },
+                "deletePolicy": "Keep",
+                "labels": {"kubernetes.io/metadata.name": "true"},
+            },
+            "tags": {"tagKey1": "tagValue1"},
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2026-03-02-preview/ManagedNamespacesCreate_Update.json
+if __name__ == "__main__":
+    main()

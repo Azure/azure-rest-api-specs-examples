8766dd26748b12d7c8138c8c20befab133e26dee
azure-sdk <azuresdk@microsoft.com>
2026-05-19 11:25:38 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-operationsmanagement_1.0.1

---
+++ specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/OperationsManagement/preview/2015-11-01-preview/examples-python/ManagementConfigurationListForSubscription.py
@@ -0,0 +1,31 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.operationsmanagement import OperationsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-operationsmanagement
+# USAGE
+    python management_configuration_list_for_subscription.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = OperationsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="subid",
+    )
+
+    response = client.management_configurations.list_by_subscription()
+    print(response)
+
+
+# x-ms-original-file: specification/operationsmanagement/resource-manager/Microsoft.OperationsManagement/OperationsManagement/preview/2015-11-01-preview/examples/ManagementConfigurationListForSubscription.json
+if __name__ == "__main__":
+    main()

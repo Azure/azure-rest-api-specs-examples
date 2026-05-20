4e4865a1918288219a92c7f96cc0662ec17665d8
azure-sdk <azuresdk@microsoft.com>
2026-05-20 11:20:07 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storage_25.0.0

---
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2025-08-01/examples-python/BlobContainersExtendImmutabilityPolicy.py
@@ -0,0 +1,37 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.storage import StorageManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storage
+# USAGE
+    python blob_containers_extend_immutability_policy.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.blob_containers.extend_immutability_policy(
+        resource_group_name="res6238",
+        account_name="sto232",
+        container_name="container5023",
+        etag="str",
+        match_condition=~azure.core.MatchConditions,
+    )
+    print(response)
+
+
+# x-ms-original-file: 2025-08-01/BlobContainersExtendImmutabilityPolicy.json
+if __name__ == "__main__":
+    main()

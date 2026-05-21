ef7c3c195089cb5ddbbffb70fb457dfa44905a9a
azure-sdk <azuresdk@microsoft.com>
2026-05-21 11:39:28 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-hybridkubernetes_2.0.0b1

---
+++ specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/stable/2026-05-01/examples-python/DeleteClusterExample.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.hybridkubernetes import ConnectedKubernetesClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-hybridkubernetes
+# USAGE
+    python delete_cluster_example.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ConnectedKubernetesClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.connected_cluster.begin_delete(
+        resource_group_name="k8sc-rg",
+        cluster_name="testCluster",
+    ).result()
+
+
+# x-ms-original-file: 2026-05-01/DeleteClusterExample.json
+if __name__ == "__main__":
+    main()

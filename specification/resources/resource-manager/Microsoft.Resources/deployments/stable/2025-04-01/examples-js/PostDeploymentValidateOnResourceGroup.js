539e3888905ce8e53d44494bc749e92dd2ed9e10
azure-sdk <azuresdk@microsoft.com>
2026-05-21 11:24:59 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-resourcesdeployments_1.0.0-beta.2

--- specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2025-04-01/examples-js/PostDeploymentValidateOnResourceGroup.js
+++ specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2025-04-01/examples-js/PostDeploymentValidateOnResourceGroup.js
@@ -1,19 +1,17 @@
 const { DeploymentsClient } = require("@azure/arm-resourcesdeployments");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv/config");
 
 /**
- * This sample demonstrates how to Validates whether the specified template is syntactically correct and will be accepted by Azure Resource Manager..
+ * This sample demonstrates how to validates whether the specified template is syntactically correct and will be accepted by Azure Resource Manager..
  *
- * @summary Validates whether the specified template is syntactically correct and will be accepted by Azure Resource Manager..
- * x-ms-original-file: specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2025-04-01/examples/PostDeploymentValidateOnResourceGroup.json
+ * @summary validates whether the specified template is syntactically correct and will be accepted by Azure Resource Manager..
+ * x-ms-original-file: 2025-04-01/PostDeploymentValidateOnResourceGroup.json
  */
 async function validatesATemplateAtResourceGroupScope() {
-  const subscriptionId =
-    process.env["RESOURCES_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000001";
-  const resourceGroupName = process.env["RESOURCES_RESOURCE_GROUP"] || "my-resource-group";
-  const deploymentName = "my-deployment";
-  const parameters = {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000001";
+  const client = new DeploymentsClient(credential, subscriptionId);
+  const result = await client.deployments.validate("my-resource-group", "my-deployment", {
     properties: {
       mode: "Incremental",
       parameters: {},
@@ -23,13 +21,6 @@ async function validatesATemplateAtResourceGroupScope() {
         uri: "https://example.com/exampleTemplate.json",
       },
     },
-  };
-  const credential = new DefaultAzureCredential();
-  const client = new DeploymentsClient(credential, subscriptionId);
-  const result = await client.deployments.beginValidateAndWait(
-    resourceGroupName,
-    deploymentName,
-    parameters,
-  );
+  });
   console.log(result);
 }
--- specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2025-04-01/examples-js/PutDeploymentResourceGroupTemplateSpecsWithId.js
+++ specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2025-04-01/examples-js/PutDeploymentResourceGroupTemplateSpecsWithId.js
@@ -1,19 +1,17 @@
 const { DeploymentsClient } = require("@azure/arm-resourcesdeployments");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv/config");
 
 /**
- * This sample demonstrates how to You can provide the template and parameters directly in the request or link to JSON files.
+ * This sample demonstrates how to you can provide the template and parameters directly in the request or link to JSON files.
  *
- * @summary You can provide the template and parameters directly in the request or link to JSON files.
- * x-ms-original-file: specification/resources/resource-manager/Microsoft.Resources/deployments/stable/2025-04-01/examples/PutDeploymentResourceGroupTemplateSpecsWithId.json
+ * @summary you can provide the template and parameters directly in the request or link to JSON files.
+ * x-ms-original-file: 2025-04-01/PutDeploymentResourceGroupTemplateSpecsWithId.json
  */
 async function createADeploymentThatWillDeployATemplateSpecWithTheGivenResourceId() {
-  const subscriptionId =
-    process.env["RESOURCES_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000001";
-  const resourceGroupName = process.env["RESOURCES_RESOURCE_GROUP"] || "my-resource-group";
-  const deploymentName = "my-deployment";
-  const parameters = {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000001";
+  const client = new DeploymentsClient(credential, subscriptionId);
+  const result = await client.deployments.createOrUpdate("my-resource-group", "my-deployment", {
     properties: {
       mode: "Incremental",
       parameters: {},
@@ -21,13 +19,6 @@ async function createADeploymentThatWillDeployATemplateSpecWithTheGivenResourceI
         id: "/subscriptions/00000000-0000-0000-0000-000000000001/resourceGroups/my-resource-group/providers/Microsoft.Resources/TemplateSpecs/TemplateSpec-Name/versions/v1",
       },
     },
-  };
-  const credential = new DefaultAzureCredential();
-  const client = new DeploymentsClient(credential, subscriptionId);
-  const result = await client.deployments.beginCreateOrUpdateAndWait(
-    resourceGroupName,
-    deploymentName,
-    parameters,
-  );
+  });
   console.log(result);
 }

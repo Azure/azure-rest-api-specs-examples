f0603bdc7dc15fc294b00b584c3ac407f3f1145e
azure-sdk <azuresdk@microsoft.com>
2026-05-22 11:27:34 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/signalr/armsignalr/v1.3.0-beta.2

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/SignalRService/preview/2025-01-01-preview/examples-go/SignalRCustomCertificates_Delete.go
@@ -0,0 +1,31 @@
+package armsignalr_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/signalr/armsignalr"
+)
+
+// Generated from example definition: 2025-01-01-preview/SignalRCustomCertificates_Delete.json
+func ExampleCustomCertificatesClient_Delete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armsignalr.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewCustomCertificatesClient().Delete(ctx, "myResourceGroup", "mySignalRService", "myCert", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armsignalr.CustomCertificatesClientDeleteResponse{
+	// }
+}
---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/SignalRService/preview/2025-01-01-preview/examples-go/SignalR_CheckNameAvailability.go
@@ -0,0 +1,40 @@
+package armsignalr_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/signalr/armsignalr"
+)
+
+// Generated from example definition: 2025-01-01-preview/SignalR_CheckNameAvailability.json
+func ExampleClient_CheckNameAvailability() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armsignalr.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewClient().CheckNameAvailability(ctx, "eastus", armsignalr.NameAvailabilityParameters{
+		Name: to.Ptr("mySignalRService"),
+		Type: to.Ptr("Microsoft.SignalRService/SignalR"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armsignalr.ClientCheckNameAvailabilityResponse{
+	// 	NameAvailability: armsignalr.NameAvailability{
+	// 		Message: to.Ptr("The name is already taken. Please try a different name."),
+	// 		NameAvailable: to.Ptr(false),
+	// 		Reason: to.Ptr("AlreadyExists"),
+	// 	},
+	// }
+}

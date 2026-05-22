5a71cc958f93b73e9b70a2690feba64f603938d9
azure-sdk <azuresdk@microsoft.com>
2026-05-22 11:45:17 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-iothub_5.0.0b3

---
+++ specification/iothub/resource-manager/Microsoft.Devices/IoTHub/preview/2026-03-01-preview/examples-python/IotHub_ManualFailover.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.iothub import IotHubClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-iothub
+# USAGE
+    python iot_hub_manual_failover.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = IotHubClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.iot_hub.begin_manual_failover(
+        iot_hub_name="testHub",
+        resource_group_name="myResourceGroup",
+        failover_input={"failoverRegion": "testHub"},
+    ).result()
+
+
+# x-ms-original-file: 2026-03-01-preview/IotHub_ManualFailover.json
+if __name__ == "__main__":
+    main()

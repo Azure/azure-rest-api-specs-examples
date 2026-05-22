73efccdb8eda303a05b6f249e590e36c4ca70a19
azure-sdk <azuresdk@microsoft.com>
2026-05-22 11:22:58 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/maintenance/armmaintenance/v1.4.0-beta.2

---
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/Maintenance/preview/2023-10-01-preview/examples-go/MaintenanceConfigurations_DeleteForResource.go
@@ -0,0 +1,39 @@
+package armmaintenance_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/maintenance/armmaintenance"
+)
+
+// Generated from example definition: 2023-10-01-preview/MaintenanceConfigurations_DeleteForResource.json
+func ExampleConfigurationsClient_Delete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armmaintenance.NewClientFactory("5b4b650e-28b9-4790-b3ab-ddbd88d727c4", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewConfigurationsClient().Delete(ctx, "examplerg", "example1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armmaintenance.ConfigurationsClientDeleteResponse{
+	// 	Configuration: &armmaintenance.Configuration{
+	// 		Name: to.Ptr("configuration1"),
+	// 		ID: to.Ptr("/subscriptions/5b4b650e-28b9-4790-b3ab-ddbd88d727c4/resourcegroups/examplerg/providers/Microsoft.Maintenance/maintenanceConfigurations/configuration1"),
+	// 		Location: to.Ptr("westus2"),
+	// 		Properties: &armmaintenance.ConfigurationProperties{
+	// 			Namespace: to.Ptr("Microsoft.Maintenance"),
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/maintenance/resource-manager/Microsoft.Maintenance/Maintenance/preview/2023-10-01-preview/examples-go/Operations_List.go
@@ -0,0 +1,40 @@
+package armmaintenance_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/maintenance/armmaintenance"
+)
+
+// Generated from example definition: 2023-10-01-preview/Operations_List.json
+func ExampleOperationsClient_NewListPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armmaintenance.NewClientFactory("<subscriptionID>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armmaintenance.OperationsClientListResponse{
+		// 	OperationsListResult: armmaintenance.OperationsListResult{
+		// 		Value: []*armmaintenance.Operation{
+		// 		},
+		// 	},
+		// }
+	}
+}

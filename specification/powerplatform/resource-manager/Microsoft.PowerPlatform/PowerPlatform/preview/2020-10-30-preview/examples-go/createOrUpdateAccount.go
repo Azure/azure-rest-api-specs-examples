657f9a4e7acadf264e90bbe654e9040445aece76
azure-sdk <azuresdk@microsoft.com>
2026-05-25 11:05:40 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/powerplatform/armpowerplatform/v0.4.0

---
+++ specification/powerplatform/resource-manager/Microsoft.PowerPlatform/PowerPlatform/preview/2020-10-30-preview/examples-go/createOrUpdateAccount.go
@@ -0,0 +1,60 @@
+package armpowerplatform_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/powerplatform/armpowerplatform"
+)
+
+// Generated from example definition: 2020-10-30-preview/createOrUpdateAccount.json
+func ExampleAccountsClient_CreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armpowerplatform.NewClientFactory("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewAccountsClient().CreateOrUpdate(ctx, "account", "resourceGroup", armpowerplatform.Account{
+		Location: to.Ptr("East US"),
+		Properties: &armpowerplatform.AccountProperties{
+			Description: to.Ptr("Description of the account."),
+		},
+		Tags: map[string]*string{
+			"Organization": to.Ptr("Administration"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armpowerplatform.AccountsClientCreateOrUpdateResponse{
+	// 	Account: armpowerplatform.Account{
+	// 		Name: to.Ptr("account"),
+	// 		Type: to.Ptr("Microsoft.PowerPlatform/accounts"),
+	// 		ID: to.Ptr("/subscriptions/subid/resourceGroups/resourceGroup/providers/Microsoft.PowerPlatform/accounts/account"),
+	// 		Location: to.Ptr("East US"),
+	// 		Properties: &armpowerplatform.AccountProperties{
+	// 			Description: to.Ptr("Description of the account."),
+	// 		},
+	// 		SystemData: &armpowerplatform.SystemData{
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+	// 			CreatedBy: to.Ptr("user1"),
+	// 			CreatedByType: to.Ptr(armpowerplatform.CreatedByTypeUser),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+	// 			LastModifiedBy: to.Ptr("user2"),
+	// 			LastModifiedByType: to.Ptr(armpowerplatform.CreatedByTypeUser),
+	// 		},
+	// 		Tags: map[string]*string{
+	// 			"Organization": to.Ptr("Administration"),
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/powerplatform/resource-manager/Microsoft.PowerPlatform/PowerPlatform/preview/2020-10-30-preview/examples-go/listAccountsByResourceGroup.go
@@ -0,0 +1,74 @@
+package armpowerplatform_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/powerplatform/armpowerplatform"
+)
+
+// Generated from example definition: 2020-10-30-preview/listAccountsByResourceGroup.json
+func ExampleAccountsClient_NewListByResourceGroupPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armpowerplatform.NewClientFactory("subid", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewAccountsClient().NewListByResourceGroupPager("rg", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armpowerplatform.AccountsClientListByResourceGroupResponse{
+		// 	AccountList: armpowerplatform.AccountList{
+		// 		Value: []*armpowerplatform.Account{
+		// 			{
+		// 				Name: to.Ptr("account1"),
+		// 				Type: to.Ptr("Microsoft.PowerPlatform/accounts"),
+		// 				ID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.PowerPlatform/accounts/account1"),
+		// 				Location: to.Ptr("East US"),
+		// 				Properties: &armpowerplatform.AccountProperties{
+		// 					Description: to.Ptr("Description of account 1."),
+		// 				},
+		// 				SystemData: &armpowerplatform.SystemData{
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+		// 					CreatedBy: to.Ptr("user1"),
+		// 					CreatedByType: to.Ptr(armpowerplatform.CreatedByTypeUser),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("user2"),
+		// 					LastModifiedByType: to.Ptr(armpowerplatform.CreatedByTypeUser),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("account2"),
+		// 				Type: to.Ptr("Microsoft.PowerPlatform/accounts"),
+		// 				ID: to.Ptr("/subscriptions/subid/resourceGroups/rg/providers/Microsoft.PowerPlatform/accounts/account2"),
+		// 				Location: to.Ptr("East US"),
+		// 				Properties: &armpowerplatform.AccountProperties{
+		// 					Description: to.Ptr("Description of account 2."),
+		// 				},
+		// 				SystemData: &armpowerplatform.SystemData{
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T17:18:19.1234567Z"); return t}()),
+		// 					CreatedBy: to.Ptr("user1"),
+		// 					CreatedByType: to.Ptr(armpowerplatform.CreatedByTypeUser),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-02T17:18:19.1234567Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("user2"),
+		// 					LastModifiedByType: to.Ptr(armpowerplatform.CreatedByTypeUser),
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// }
+	}
+}

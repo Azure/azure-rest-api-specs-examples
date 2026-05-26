4c560f7a4a984d3e544d793128a895da425233be
azure-sdk <azuresdk@microsoft.com>
2026-05-26 11:23:17 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-serialconsole_3.0.0-beta.1

---
+++ specification/serialconsole/resource-manager/Microsoft.SerialConsole/SerialConsole/stable/2024-07-01/examples-js/GetSerialPort.js
@@ -0,0 +1,22 @@
+const { MicrosoftSerialConsoleClient } = require("@azure/arm-serialconsole");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets the configured settings for a serial port
+ *
+ * @summary gets the configured settings for a serial port
+ * x-ms-original-file: 2024-07-01/GetSerialPort.json
+ */
+async function getTheSerialPortForAParentResource() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new MicrosoftSerialConsoleClient(credential, subscriptionId);
+  const result = await client.serialPorts.get(
+    "myResourceGroup",
+    "Microsoft.Compute",
+    "virtualMachines",
+    "myVM",
+    "0",
+  );
+  console.log(result);
+}

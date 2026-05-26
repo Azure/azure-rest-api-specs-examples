5ecd4bad4df572a4b6ba4c073351d4a1aa5e7f5b
azure-sdk <azuresdk@microsoft.com>
2026-05-26 11:29:57 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-databoxedge_3.0.0b2

---
+++ specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/DataBoxEdge/stable/2023-12-01/examples-python/GetDiagnosticProactiveLogCollectionSettings.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.databoxedge import DataBoxEdgeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-databoxedge
+# USAGE
+    python get_diagnostic_proactive_log_collection_settings.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataBoxEdgeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.diagnostic_settings.get_diagnostic_proactive_log_collection_settings(
+        device_name="testedgedevice",
+        resource_group_name="GroupForEdgeAutomation",
+    )
+    print(response)
+
+
+# x-ms-original-file: 2023-12-01/GetDiagnosticProactiveLogCollectionSettings.json
+if __name__ == "__main__":
+    main()
---
+++ specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/DataBoxEdge/stable/2023-12-01/examples-python/InstallUpdatesPost.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.databoxedge import DataBoxEdgeManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-databoxedge
+# USAGE
+    python install_updates_post.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DataBoxEdgeManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.devices.begin_install_updates(
+        device_name="testedgedevice",
+        resource_group_name="GroupForEdgeAutomation",
+    ).result()
+
+
+# x-ms-original-file: 2023-12-01/InstallUpdatesPost.json
+if __name__ == "__main__":
+    main()

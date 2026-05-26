77a58ee5bad8ed0fa930810c2a829107d81ffde9
azure-sdk <azuresdk@microsoft.com>
2026-05-26 11:20:26 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/webpubsub/armwebpubsub/v1.4.0-beta.1

---
+++ specification/webpubsub/resource-manager/Microsoft.SignalRService/SignalRService/preview/2025-01-01-preview/examples-go/WebPubSub_Restart.go
@@ -0,0 +1,30 @@
+package armwebpubsub_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/webpubsub/armwebpubsub"
+)
+
+// Generated from example definition: 2025-01-01-preview/WebPubSub_Restart.json
+func ExampleClient_BeginRestart() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armwebpubsub.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewClient().BeginRestart(ctx, "myResourceGroup", "myWebPubSubService", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+}

243e5ae7f2b98ebfea114c1d76ab9d0c721869b3
azure-sdk <azuresdk@microsoft.com>
2026-05-26 11:27:40 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-attestation_3.0.0-beta.1

---
+++ specification/attestation/resource-manager/Microsoft.Attestation/Attestation/stable/2021-06-01/examples-js/Get_AttestationProvidersListByResourceGroup.js
@@ -0,0 +1,16 @@
+const { AttestationManagementClient } = require("@azure/arm-attestation");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to returns attestation providers list in a resource group.
+ *
+ * @summary returns attestation providers list in a resource group.
+ * x-ms-original-file: 2021-06-01/Get_AttestationProvidersListByResourceGroup.json
+ */
+async function attestationProvidersListByResourceGroup() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "6c96b33e-f5b8-40a6-9011-5cb1c58b0915";
+  const client = new AttestationManagementClient(credential, subscriptionId);
+  const result = await client.attestationProviders.listByResourceGroup("testrg1");
+  console.log(result);
+}

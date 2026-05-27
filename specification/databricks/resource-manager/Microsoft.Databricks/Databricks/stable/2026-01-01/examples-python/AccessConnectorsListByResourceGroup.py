5219a477a5f379310ef03caad1120bb6463f1951
azure-sdk <azuresdk@microsoft.com>
2026-05-27 11:34:42 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-databricks_3.0.0b1

---
+++ specification/databricks/resource-manager/Microsoft.Databricks/Databricks/stable/2026-01-01/examples-python/AccessConnectorsListByResourceGroup.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.databricks import AzureDatabricksManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-databricks
+# USAGE
+    python access_connectors_list_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = AzureDatabricksManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.access_connectors.list_by_resource_group(
+        resource_group_name="rg",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2026-01-01/AccessConnectorsListByResourceGroup.json
+if __name__ == "__main__":
+    main()

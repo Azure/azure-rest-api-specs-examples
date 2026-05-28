acb32bc01c942ef63d757e20c7a2264f22c9fddd
azure-sdk <azuresdk@microsoft.com>
2026-05-28 11:19:11 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-appcontainers_5.0.0b1

---
+++ specification/app/resource-manager/Microsoft.App/ContainerApps/stable/2026-01-01/examples-python/ConnectedEnvironmentsStorages_Delete.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-appcontainers
+# USAGE
+    python connected_environments_storages_delete.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ContainerAppsAPIClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.connected_environments_storages.begin_delete(
+        resource_group_name="examplerg",
+        connected_environment_name="env",
+        storage_name="jlaw-demo1",
+    ).result()
+
+
+# x-ms-original-file: 2026-01-01/ConnectedEnvironmentsStorages_Delete.json
+if __name__ == "__main__":
+    main()

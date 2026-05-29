ff22e47872ecd900cd50905d38cb9556cce8c8ce
azure-sdk <azuresdk@microsoft.com>
2026-05-29 11:11:36 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-selfhelp_2.0.0-beta.7

---
+++ specification/help/resource-manager/Microsoft.Help/Help/preview/2024-03-01-preview/examples-js/CheckNameAvailabilityForDiagnosticWhenNameIsAvailable.js
@@ -0,0 +1,18 @@
+const { HelpRP } = require("@azure/arm-selfhelp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to this API is used to check the uniqueness of a resource name used for a diagnostic, troubleshooter or solutions
+ *
+ * @summary this API is used to check the uniqueness of a resource name used for a diagnostic, troubleshooter or solutions
+ * x-ms-original-file: 2024-03-01-preview/CheckNameAvailabilityForDiagnosticWhenNameIsAvailable.json
+ */
+async function exampleWhenNameIsAvailableForADiagnosticResource() {
+  const credential = new DefaultAzureCredential();
+  const client = new HelpRP(credential);
+  const result = await client.checkNameAvailability.checkAvailability(
+    "subscriptions/0d0fcd2e-c4fd-4349-8497-200edb3923c6",
+    { checkNameAvailabilityRequest: { name: "sampleName", type: "Microsoft.Help/diagnostics" } },
+  );
+  console.log(result);
+}
---
+++ specification/help/resource-manager/Microsoft.Help/Help/preview/2024-03-01-preview/examples-js/ListOperations.js
@@ -0,0 +1,19 @@
+const { HelpRP } = require("@azure/arm-selfhelp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2024-03-01-preview/ListOperations.json
+ */
+async function listAllOperations() {
+  const credential = new DefaultAzureCredential();
+  const client = new HelpRP(credential);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

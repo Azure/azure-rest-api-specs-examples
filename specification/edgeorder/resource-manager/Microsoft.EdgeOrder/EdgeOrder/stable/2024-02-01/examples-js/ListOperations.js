5f0712dc2ebd75df007b995500ba1e79da297a4a
azure-sdk <azuresdk@microsoft.com>
2026-06-01 11:18:15 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-edgeorder_1.0.0-beta.1

---
+++ specification/edgeorder/resource-manager/Microsoft.EdgeOrder/EdgeOrder/stable/2024-02-01/examples-js/ListOperations.js
@@ -0,0 +1,20 @@
+const { EdgeOrderClient } = require("@azure/arm-edgeorder");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2024-02-01/ListOperations.json
+ */
+async function listOperations() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new EdgeOrderClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

c85226f4a2e16a918971502ab0f503e0c473c398
azure-sdk <azuresdk@microsoft.com>
2026-06-02 08:33:04 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-confidentialledger_2.0.0-beta.1

---
+++ specification/confidentialledger/resource-manager/Microsoft.ConfidentialLedger/ConfidentialLedger/stable/2026-02-23/examples-js/ConfidentialLedger_List.js
@@ -0,0 +1,20 @@
+const { ConfidentialLedgerClient } = require("@azure/arm-confidentialledger");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to retrieves the properties of all Confidential Ledgers.
+ *
+ * @summary retrieves the properties of all Confidential Ledgers.
+ * x-ms-original-file: 2026-02-23/ConfidentialLedger_List.json
+ */
+async function confidentialLedgerList() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "0000000-0000-0000-0000-000000000001";
+  const client = new ConfidentialLedgerClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.ledger.listByResourceGroup("DummyResourceGroupName")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

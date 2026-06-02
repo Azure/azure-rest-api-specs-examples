052a2b4d38443c6d34b5348add3e1de16765fe6d
azure-sdk <azuresdk@microsoft.com>
2026-06-02 11:10:32 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-resourcegraph_8.1.0b4

---
+++ specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/ResourceGraph/stable/2024-04-01/examples-python/ResourcesFilterQuery.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.resourcegraph import ResourceGraphClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-resourcegraph
+# USAGE
+    python resources_filter_query.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = ResourceGraphClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.resources(
+        query={
+            "query": "Resources | project id, name, type, location | where type =~ 'Microsoft.Compute/virtualMachines' | limit 3",
+            "subscriptions": ["cfbbd179-59d2-4052-aa06-9270a38aa9d6"],
+        },
+    )
+    print(response)
+
+
+# x-ms-original-file: 2024-04-01/ResourcesFilterQuery.json
+if __name__ == "__main__":
+    main()

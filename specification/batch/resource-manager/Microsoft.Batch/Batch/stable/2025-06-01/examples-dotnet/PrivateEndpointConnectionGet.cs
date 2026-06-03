7bf1551252052ad96313ec222324a0ec280750c0
azure-sdk <azuresdk@microsoft.com>
2026-06-03 11:45:09 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Batch_1.7.0

--- specification/batch/resource-manager/Microsoft.Batch/Batch/stable/2025-06-01/examples-dotnet/PrivateEndpointConnectionGet.cs
+++ specification/batch/resource-manager/Microsoft.Batch/Batch/stable/2025-06-01/examples-dotnet/PrivateEndpointConnectionGet.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Azure.ResourceManager.Batch.Models;
 using Azure.ResourceManager.Batch;
 
 // Generated from example definition: specification/batch/resource-manager/Microsoft.Batch/Batch/stable/2025-06-01/examples/PrivateEndpointConnectionGet.json
@@ -14,31 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this BatchAccountResource created on azure
-// for more information of creating BatchAccountResource, please refer to the document of BatchAccountResource
+// this example assumes you already have this BatchPrivateEndpointConnectionResource created on azure
+// for more information of creating BatchPrivateEndpointConnectionResource, please refer to the document of BatchPrivateEndpointConnectionResource
 string subscriptionId = "12345678-1234-1234-1234-123456789012";
 string resourceGroupName = "default-azurebatch-japaneast";
 string accountName = "sampleacct";
-ResourceIdentifier batchAccountResourceId = BatchAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-BatchAccountResource batchAccount = client.GetBatchAccountResource(batchAccountResourceId);
-
-// get the collection of this BatchPrivateEndpointConnectionResource
-BatchPrivateEndpointConnectionCollection collection = batchAccount.GetBatchPrivateEndpointConnections();
+string privateEndpointConnectionName = "testprivateEndpointConnection5testprivateEndpointConnection5.24d6b4b5-e65c-4330-bbe9-3a290d62f8e0";
+ResourceIdentifier batchPrivateEndpointConnectionResourceId = BatchPrivateEndpointConnectionResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, privateEndpointConnectionName);
+BatchPrivateEndpointConnectionResource batchPrivateEndpointConnection = client.GetBatchPrivateEndpointConnectionResource(batchPrivateEndpointConnectionResourceId);
 
 // invoke the operation
-string privateEndpointConnectionName = "testprivateEndpointConnection5testprivateEndpointConnection5.24d6b4b5-e65c-4330-bbe9-3a290d62f8e0";
-NullableResponse<BatchPrivateEndpointConnectionResource> response = await collection.GetIfExistsAsync(privateEndpointConnectionName);
-BatchPrivateEndpointConnectionResource result = response.HasValue ? response.Value : null;
+BatchPrivateEndpointConnectionResource result = await batchPrivateEndpointConnection.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    BatchPrivateEndpointConnectionData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+BatchPrivateEndpointConnectionData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
--- specification/batch/resource-manager/Microsoft.Batch/Batch/stable/2025-06-01/examples-dotnet/PrivateLinkResourceGet.cs
+++ specification/batch/resource-manager/Microsoft.Batch/Batch/stable/2025-06-01/examples-dotnet/PrivateLinkResourceGet.cs
@@ -14,31 +14,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this BatchAccountResource created on azure
-// for more information of creating BatchAccountResource, please refer to the document of BatchAccountResource
+// this example assumes you already have this BatchPrivateLinkResource created on azure
+// for more information of creating BatchPrivateLinkResource, please refer to the document of BatchPrivateLinkResource
 string subscriptionId = "12345678-1234-1234-1234-123456789012";
 string resourceGroupName = "default-azurebatch-japaneast";
 string accountName = "sampleacct";
-ResourceIdentifier batchAccountResourceId = BatchAccountResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-BatchAccountResource batchAccount = client.GetBatchAccountResource(batchAccountResourceId);
-
-// get the collection of this BatchPrivateLinkResource
-BatchPrivateLinkResourceCollection collection = batchAccount.GetBatchPrivateLinkResources();
+string privateLinkResourceName = "batchAccount";
+ResourceIdentifier batchPrivateLinkResourceId = BatchPrivateLinkResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName, privateLinkResourceName);
+BatchPrivateLinkResource batchPrivateLinkResource = client.GetBatchPrivateLinkResource(batchPrivateLinkResourceId);
 
 // invoke the operation
-string privateLinkResourceName = "batchAccount";
-NullableResponse<BatchPrivateLinkResource> response = await collection.GetIfExistsAsync(privateLinkResourceName);
-BatchPrivateLinkResource result = response.HasValue ? response.Value : null;
+BatchPrivateLinkResource result = await batchPrivateLinkResource.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    BatchPrivateLinkResourceData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+BatchPrivateLinkResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

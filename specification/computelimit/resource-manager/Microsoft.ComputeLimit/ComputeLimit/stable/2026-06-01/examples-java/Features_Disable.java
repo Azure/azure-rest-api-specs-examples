f4c690afccb663c2207bc56779edb24ccb2facb8
azure-sdk <azuresdk@microsoft.com>
2026-06-03 11:33:08 +0000

    [Automation] Collect examples from azure-sdk-for-java#com.azure.resourcemanager+azure-resourcemanager-computelimit_1.1.0

---
+++ specification/computelimit/resource-manager/Microsoft.ComputeLimit/ComputeLimit/stable/2026-06-01/examples-java/Features_Disable.java
@@ -0,0 +1,17 @@
+
+/**
+ * Samples for Features Disable.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file: 2026-06-01/Features_Disable.json
+     */
+    /**
+     * Sample code: Disable feature.
+     * 
+     * @param manager Entry point to ComputeLimitManager.
+     */
+    public static void disableFeature(com.azure.resourcemanager.computelimit.ComputeLimitManager manager) {
+        manager.features().disable("eastus", "VmCategoryQuota", com.azure.core.util.Context.NONE);
+    }
+}
---
+++ specification/computelimit/resource-manager/Microsoft.ComputeLimit/ComputeLimit/stable/2026-06-01/examples-java/VmFamilies_Get.java
@@ -0,0 +1,17 @@
+
+/**
+ * Samples for VmFamilies Get.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file: 2026-06-01/VmFamilies_Get.json
+     */
+    /**
+     * Sample code: Get a VM family.
+     * 
+     * @param manager Entry point to ComputeLimitManager.
+     */
+    public static void getAVMFamily(com.azure.resourcemanager.computelimit.ComputeLimitManager manager) {
+        manager.vmFamilies().getWithResponse("eastus", "standardDSv2Family", com.azure.core.util.Context.NONE);
+    }
+}

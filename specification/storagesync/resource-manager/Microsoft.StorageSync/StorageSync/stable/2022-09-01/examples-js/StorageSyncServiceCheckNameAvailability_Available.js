2c6a733bbd3114f01b46973756d62882c59fd31b
azure-sdk <azuresdk@microsoft.com>
2026-06-04 11:14:40 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-storagesync_10.0.0-beta.1

---
+++ specification/storagesync/resource-manager/Microsoft.StorageSync/StorageSync/stable/2022-09-01/examples-js/StorageSyncServiceCheckNameAvailability_Available.js
@@ -0,0 +1,19 @@
+const { MicrosoftStorageSync } = require("@azure/arm-storagesync");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to check the give namespace name availability.
+ *
+ * @summary check the give namespace name availability.
+ * x-ms-original-file: 2022-09-01/StorageSyncServiceCheckNameAvailability_Available.json
+ */
+async function storageSyncServiceCheckNameAvailabilityAvailable() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "5c6bc8e1-1eaf-4192-94d8-58ce463ac86c";
+  const client = new MicrosoftStorageSync(credential, subscriptionId);
+  const result = await client.storageSyncServices.checkNameAvailability("westus", {
+    name: "newstoragesyncservicename",
+    type: "Microsoft.StorageSync/storageSyncServices",
+  });
+  console.log(result);
+}

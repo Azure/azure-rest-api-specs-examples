1626af4fdbee2f662ebe468f08b8f6c3064d9b45
azure-sdk <azuresdk@microsoft.com>
2026-06-09 11:25:55 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-monitorworkspaces_1.0.0-beta.1

---
+++ specification/monitoringservice/resource-manager/Microsoft.Monitor/Accounts/stable/2025-10-03/examples-js/Issue_List_MaximumSet_Gen.js
@@ -0,0 +1,20 @@
+const { MonitorClient } = require("@azure/arm-monitorworkspaces");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list all issues under the parent
+ *
+ * @summary list all issues under the parent
+ * x-ms-original-file: 2025-10-03/Issue_List_MaximumSet_Gen.json
+ */
+async function issueListMaximumSet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "aceaa046-91f0-492a-96dc-45e10a9183dc";
+  const client = new MonitorClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.issue.list("rg1", "myWorkspace")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
---
+++ specification/monitoringservice/resource-manager/Microsoft.Monitor/Accounts/stable/2025-10-03/examples-js/Operations_List.js
@@ -0,0 +1,20 @@
+const { MonitorClient } = require("@azure/arm-monitorworkspaces");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list the operations for the provider
+ *
+ * @summary list the operations for the provider
+ * x-ms-original-file: 2025-10-03/Operations_List.json
+ */
+async function listsTheMicrosoftMonitorResourceProviderOperations() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new MonitorClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

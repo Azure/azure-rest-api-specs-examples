90fdeb3e35047fd7d369b302ba26a59a85de3162
azure-sdk <azuresdk@microsoft.com>
2026-06-09 11:19:15 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-devhub_1.0.0-beta.5

---
+++ specification/developerhub/resource-manager/Microsoft.DevHub/DeveloperHub/preview/2025-03-01-preview/examples-js/GitHubOAuth.js
@@ -0,0 +1,18 @@
+const { DeveloperHubServiceClient } = require("@azure/arm-devhub");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets GitHubOAuth info used to authenticate users with the Developer Hub GitHub App.
+ *
+ * @summary gets GitHubOAuth info used to authenticate users with the Developer Hub GitHub App.
+ * x-ms-original-file: 2025-03-01-preview/GitHubOAuth.json
+ */
+async function gitHubOAuth() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DeveloperHubServiceClient(credential, subscriptionId);
+  const result = await client.gitHubOAuth("eastus2euap", {
+    parameters: { redirectUrl: "https://ms.portal.azure.com/aks" },
+  });
+  console.log(result);
+}
---
+++ specification/developerhub/resource-manager/Microsoft.DevHub/DeveloperHub/preview/2025-03-01-preview/examples-js/GitHubOAuth_List.js
@@ -0,0 +1,16 @@
+const { DeveloperHubServiceClient } = require("@azure/arm-devhub");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to callback URL to hit once authenticated with GitHub App to have the service store the OAuth token.
+ *
+ * @summary callback URL to hit once authenticated with GitHub App to have the service store the OAuth token.
+ * x-ms-original-file: 2025-03-01-preview/GitHubOAuth_List.json
+ */
+async function listGitHubOAuth() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DeveloperHubServiceClient(credential, subscriptionId);
+  const result = await client.listGitHubOAuth("eastus2euap");
+  console.log(result);
+}

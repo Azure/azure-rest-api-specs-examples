2e0f3023e4ba6feeb13f769363feca5aaa63ae54
azure-sdk <azuresdk@microsoft.com>
2026-06-11 11:36:16 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-newrelicobservability_2.0.0-beta.2

--- specification/newrelic/resource-manager/NewRelic.Observability/preview/2025-05-01-preview/examples-js/Monitors_SwitchBilling_MinimumSet_Gen.js
+++ specification/newrelic/resource-manager/NewRelic.Observability/preview/2025-05-01-preview/examples-js/Monitors_SwitchBilling_MinimumSet_Gen.js
@@ -1,23 +1,18 @@
 const { NewRelicObservability } = require("@azure/arm-newrelicobservability");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv/config");
 
 /**
- * This sample demonstrates how to Switches the billing for the New Relic Monitor resource to be billed by Azure Marketplace
+ * This sample demonstrates how to switches the billing for the New Relic Monitor resource to be billed by Azure Marketplace
  *
- * @summary Switches the billing for the New Relic Monitor resource to be billed by Azure Marketplace
- * x-ms-original-file: specification/newrelic/resource-manager/NewRelic.Observability/preview/2025-05-01-preview/examples/Monitors_SwitchBilling_MinimumSet_Gen.json
+ * @summary switches the billing for the New Relic Monitor resource to be billed by Azure Marketplace
+ * x-ms-original-file: 2025-05-01-preview/Monitors_SwitchBilling_MinimumSet_Gen.json
  */
 async function monitorsSwitchBillingMinimumSetGen() {
-  const subscriptionId =
-    process.env["NEWRELICOBSERVABILITY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
-  const resourceGroupName = process.env["NEWRELICOBSERVABILITY_RESOURCE_GROUP"] || "rgNewRelic";
-  const monitorName = "fhcjxnxumkdlgpwanewtkdnyuz";
-  const request = {
-    userEmail: "ruxvg@xqkmdhrnoo.hlmbpm",
-  };
   const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const client = new NewRelicObservability(credential, subscriptionId);
-  const result = await client.monitors.switchBilling(resourceGroupName, monitorName, request);
+  const result = await client.monitors.switchBilling("rgNewRelic", "fhcjxnxumkdlgpwanewtkdnyuz", {
+    userEmail: "ruxvg@xqkmdhrnoo.hlmbpm",
+  });
   console.log(result);
 }
--- specification/newrelic/resource-manager/NewRelic.Observability/preview/2025-05-01-preview/examples-js/Monitors_VmHostPayload_MaximumSet_Gen.js
+++ specification/newrelic/resource-manager/NewRelic.Observability/preview/2025-05-01-preview/examples-js/Monitors_VmHostPayload_MaximumSet_Gen.js
@@ -1,20 +1,16 @@
 const { NewRelicObservability } = require("@azure/arm-newrelicobservability");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv/config");
 
 /**
- * This sample demonstrates how to Returns the payload that needs to be passed in the request body for installing the New Relic agent on a VM, providing the necessary configuration details
+ * This sample demonstrates how to returns the payload that needs to be passed in the request body for installing the New Relic agent on a VM, providing the necessary configuration details
  *
- * @summary Returns the payload that needs to be passed in the request body for installing the New Relic agent on a VM, providing the necessary configuration details
- * x-ms-original-file: specification/newrelic/resource-manager/NewRelic.Observability/preview/2025-05-01-preview/examples/Monitors_VmHostPayload_MaximumSet_Gen.json
+ * @summary returns the payload that needs to be passed in the request body for installing the New Relic agent on a VM, providing the necessary configuration details
+ * x-ms-original-file: 2025-05-01-preview/Monitors_VmHostPayload_MaximumSet_Gen.json
  */
-async function monitorsVMHostPayloadMaximumSetGen() {
-  const subscriptionId =
-    process.env["NEWRELICOBSERVABILITY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
-  const resourceGroupName = process.env["NEWRELICOBSERVABILITY_RESOURCE_GROUP"] || "rgopenapi";
-  const monitorName = "ipxmlcbonyxtolzejcjshkmlron";
+async function monitorsVmHostPayloadMaximumSetGen() {
   const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const client = new NewRelicObservability(credential, subscriptionId);
-  const result = await client.monitors.vmHostPayload(resourceGroupName, monitorName);
+  const result = await client.monitors.vmHostPayload("rgopenapi", "ipxmlcbonyxtolzejcjshkmlron");
   console.log(result);
 }

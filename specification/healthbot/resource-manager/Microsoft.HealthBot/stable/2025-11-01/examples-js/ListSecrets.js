fa8f1df22687f86e5ab57ecc642a71641796ddbd
azure-sdk <azuresdk@microsoft.com>
2026-06-12 11:17:19 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-healthbot_3.0.0

---
+++ specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2025-11-01/examples-js/ListSecrets.js
@@ -0,0 +1,16 @@
+const { HealthbotClient } = require("@azure/arm-healthbot");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list all secrets of a HealthBot.
+ *
+ * @summary list all secrets of a HealthBot.
+ * x-ms-original-file: 2025-11-01/ListSecrets.json
+ */
+async function botListSecrets() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new HealthbotClient(credential, subscriptionId);
+  const result = await client.bots.listSecrets("healthbotClient", "samplebotname");
+  console.log(result);
+}
---
+++ specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2025-11-01/examples-js/ResourceDeletionDelete.js
@@ -0,0 +1,15 @@
+const { HealthbotClient } = require("@azure/arm-healthbot");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a HealthBot.
+ *
+ * @summary delete a HealthBot.
+ * x-ms-original-file: 2025-11-01/ResourceDeletionDelete.json
+ */
+async function botDelete() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new HealthbotClient(credential, subscriptionId);
+  await client.bots.delete("healthbotClient", "samplebotname");
+}

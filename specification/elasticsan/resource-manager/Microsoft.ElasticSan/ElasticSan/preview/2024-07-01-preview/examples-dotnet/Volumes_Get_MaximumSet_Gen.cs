aed91ff08eef550a06c1457c3b0799e5f69a7b3c
azure-sdk <azuresdk@microsoft.com>
2026-06-13 11:32:03 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ElasticSan_1.2.1

--- specification/elasticsan/resource-manager/Microsoft.ElasticSan/ElasticSan/preview/2024-07-01-preview/examples-dotnet/Volumes_Get_MaximumSet_Gen.cs
+++ specification/elasticsan/resource-manager/Microsoft.ElasticSan/ElasticSan/preview/2024-07-01-preview/examples-dotnet/Volumes_Get_MaximumSet_Gen.cs
@@ -15,32 +15,21 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ElasticSanVolumeGroupResource created on azure
-// for more information of creating ElasticSanVolumeGroupResource, please refer to the document of ElasticSanVolumeGroupResource
+// this example assumes you already have this ElasticSanVolumeResource created on azure
+// for more information of creating ElasticSanVolumeResource, please refer to the document of ElasticSanVolumeResource
 string subscriptionId = "subscriptionid";
 string resourceGroupName = "resourcegroupname";
 string elasticSanName = "elasticsanname";
 string volumeGroupName = "volumegroupname";
-ResourceIdentifier elasticSanVolumeGroupResourceId = ElasticSanVolumeGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, elasticSanName, volumeGroupName);
-ElasticSanVolumeGroupResource elasticSanVolumeGroup = client.GetElasticSanVolumeGroupResource(elasticSanVolumeGroupResourceId);
-
-// get the collection of this ElasticSanVolumeResource
-ElasticSanVolumeCollection collection = elasticSanVolumeGroup.GetElasticSanVolumes();
+string volumeName = "volumename";
+ResourceIdentifier elasticSanVolumeResourceId = ElasticSanVolumeResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, elasticSanName, volumeGroupName, volumeName);
+ElasticSanVolumeResource elasticSanVolume = client.GetElasticSanVolumeResource(elasticSanVolumeResourceId);
 
 // invoke the operation
-string volumeName = "volumename";
-NullableResponse<ElasticSanVolumeResource> response = await collection.GetIfExistsAsync(volumeName);
-ElasticSanVolumeResource result = response.HasValue ? response.Value : null;
+ElasticSanVolumeResource result = await elasticSanVolume.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    ElasticSanVolumeData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ElasticSanVolumeData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

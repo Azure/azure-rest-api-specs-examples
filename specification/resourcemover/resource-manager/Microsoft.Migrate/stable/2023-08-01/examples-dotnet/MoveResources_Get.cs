d81b5e1c20473c726d5b2e9c8923c86f61ac3358
azure-sdk <azuresdk@microsoft.com>
2026-06-13 11:43:25 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceMover_1.1.2-beta.4

--- specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2023-08-01/examples-dotnet/MoveResources_Get.cs
+++ specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2023-08-01/examples-dotnet/MoveResources_Get.cs
@@ -15,31 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this MoverResourceSetResource created on azure
-// for more information of creating MoverResourceSetResource, please refer to the document of MoverResourceSetResource
+// this example assumes you already have this MoverResource created on azure
+// for more information of creating MoverResource, please refer to the document of MoverResource
 string subscriptionId = "subid";
 string resourceGroupName = "rg1";
 string moverResourceSetName = "movecollection1";
-ResourceIdentifier moverResourceSetResourceId = MoverResourceSetResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, moverResourceSetName);
-MoverResourceSetResource moverResourceSet = client.GetMoverResourceSetResource(moverResourceSetResourceId);
-
-// get the collection of this MoverResource
-MoverResourceCollection collection = moverResourceSet.GetMoverResources();
+string moverResourceName = "moveresourcename1";
+ResourceIdentifier moverResourceId = MoverResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, moverResourceSetName, moverResourceName);
+MoverResource moverResource = client.GetMoverResource(moverResourceId);
 
 // invoke the operation
-string moverResourceName = "moveresourcename1";
-NullableResponse<MoverResource> response = await collection.GetIfExistsAsync(moverResourceName);
-MoverResource result = response.HasValue ? response.Value : null;
+MoverResource result = await moverResource.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    MoverResourceData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+MoverResourceData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

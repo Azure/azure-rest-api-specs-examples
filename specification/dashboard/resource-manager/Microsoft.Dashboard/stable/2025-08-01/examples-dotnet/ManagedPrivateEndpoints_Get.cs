de1e2030c35775b26a195b42e3e422433e0a32ff
azure-sdk <azuresdk@microsoft.com>
2026-06-13 11:52:37 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.Grafana_1.2.0-beta.4

--- specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2025-08-01/examples-dotnet/ManagedPrivateEndpoints_Get.cs
+++ specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2025-08-01/examples-dotnet/ManagedPrivateEndpoints_Get.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Azure.ResourceManager.Grafana.Models;
 using Azure.ResourceManager.Grafana;
 
 // Generated from example definition: 2025-08-01/ManagedPrivateEndpoints_Get.json
@@ -14,31 +15,20 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ManagedGrafanaResource created on azure
-// for more information of creating ManagedGrafanaResource, please refer to the document of ManagedGrafanaResource
+// this example assumes you already have this ManagedPrivateEndpointModelResource created on azure
+// for more information of creating ManagedPrivateEndpointModelResource, please refer to the document of ManagedPrivateEndpointModelResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
 string workspaceName = "myWorkspace";
-ResourceIdentifier managedGrafanaResourceId = ManagedGrafanaResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName);
-ManagedGrafanaResource managedGrafana = client.GetManagedGrafanaResource(managedGrafanaResourceId);
-
-// get the collection of this ManagedPrivateEndpointModelResource
-ManagedPrivateEndpointModelCollection collection = managedGrafana.GetManagedPrivateEndpointModels();
+string managedPrivateEndpointName = "myMPEName";
+ResourceIdentifier managedPrivateEndpointModelResourceId = ManagedPrivateEndpointModelResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, workspaceName, managedPrivateEndpointName);
+ManagedPrivateEndpointModelResource managedPrivateEndpointModel = client.GetManagedPrivateEndpointModelResource(managedPrivateEndpointModelResourceId);
 
 // invoke the operation
-string managedPrivateEndpointName = "myMPEName";
-NullableResponse<ManagedPrivateEndpointModelResource> response = await collection.GetIfExistsAsync(managedPrivateEndpointName);
-ManagedPrivateEndpointModelResource result = response.HasValue ? response.Value : null;
+ManagedPrivateEndpointModelResource result = await managedPrivateEndpointModel.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    ManagedPrivateEndpointModelData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ManagedPrivateEndpointModelData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

726ca91a1fc646c2954a741b5d9f410412aaedcd
azure-sdk <azuresdk@microsoft.com>
2026-06-13 11:40:59 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ServiceNetworking_1.2.0-beta.2

--- specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/ServiceNetworking/preview/2025-03-01-preview/examples-dotnet/AssociationGet.cs
+++ specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/ServiceNetworking/preview/2025-03-01-preview/examples-dotnet/AssociationGet.cs
@@ -4,7 +4,6 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.ServiceNetworking.Models;
 using Azure.ResourceManager.ServiceNetworking;
 
 // Generated from example definition: 2025-03-01-preview/AssociationGet.json
@@ -15,20 +14,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this TrafficControllerAssociationResource created on azure
-// for more information of creating TrafficControllerAssociationResource, please refer to the document of TrafficControllerAssociationResource
+// this example assumes you already have this TrafficControllerResource created on azure
+// for more information of creating TrafficControllerResource, please refer to the document of TrafficControllerResource
 string subscriptionId = "subid";
 string resourceGroupName = "rg1";
 string trafficControllerName = "tc1";
-string associationName = "as1";
-ResourceIdentifier trafficControllerAssociationResourceId = TrafficControllerAssociationResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, trafficControllerName, associationName);
-TrafficControllerAssociationResource trafficControllerAssociation = client.GetTrafficControllerAssociationResource(trafficControllerAssociationResourceId);
+ResourceIdentifier trafficControllerResourceId = TrafficControllerResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, trafficControllerName);
+TrafficControllerResource trafficController = client.GetTrafficControllerResource(trafficControllerResourceId);
+
+// get the collection of this TrafficControllerAssociationResource
+TrafficControllerAssociationCollection collection = trafficController.GetTrafficControllerAssociations();
 
 // invoke the operation
-TrafficControllerAssociationResource result = await trafficControllerAssociation.GetAsync();
+string associationName = "as1";
+NullableResponse<TrafficControllerAssociationResource> response = await collection.GetIfExistsAsync(associationName);
+TrafficControllerAssociationResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-TrafficControllerAssociationData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    TrafficControllerAssociationData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

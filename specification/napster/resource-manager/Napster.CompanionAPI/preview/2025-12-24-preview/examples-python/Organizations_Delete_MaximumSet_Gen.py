e174e37bcb0f0e8564ae9b4af6bc401a3cfa8b8e
azure-sdk <azuresdk@microsoft.com>
2026-06-15 11:08:26 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-napsteromniagentapi_1.0.0b1

---
+++ specification/napster/resource-manager/Napster.CompanionAPI/preview/2025-12-24-preview/examples-python/Organizations_Delete_MaximumSet_Gen.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.napsteromniagentapi import NapsterOmniAgentApiMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-napsteromniagentapi
+# USAGE
+    python organizations_delete_maximum_set_gen.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = NapsterOmniAgentApiMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.organizations.begin_delete(
+        resource_group_name="rgopenapi",
+        organizationname="contosoOrg",
+    ).result()
+
+
+# x-ms-original-file: 2025-12-24-preview/Organizations_Delete_MaximumSet_Gen.json
+if __name__ == "__main__":
+    main()

52c88aa91afeba875cf345ca0591611ec5202f5f
azure-sdk <azuresdk@microsoft.com>
2026-06-18 11:40:45 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.MongoCluster_1.1.0-beta.1

--- specification/mongocluster/resource-manager/Microsoft.DocumentDB/MongoCluster/stable/2025-09-01/examples-dotnet/MongoClusters_Get.cs
+++ specification/mongocluster/resource-manager/Microsoft.DocumentDB/MongoCluster/stable/2025-09-01/examples-dotnet/MongoClusters_Get.cs
@@ -6,6 +6,7 @@ using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager.Models;
 using Azure.ResourceManager.MongoCluster.Models;
+using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.MongoCluster;
 
 // Generated from example definition: 2025-09-01/MongoClusters_Get.json
@@ -16,19 +17,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this MongoClusterResource created on azure
-// for more information of creating MongoClusterResource, please refer to the document of MongoClusterResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "ffffffff-ffff-ffff-ffff-ffffffffffff";
 string resourceGroupName = "TestResourceGroup";
-string mongoClusterName = "myMongoCluster";
-ResourceIdentifier mongoClusterResourceId = MongoClusterResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, mongoClusterName);
-MongoClusterResource mongoCluster = client.GetMongoClusterResource(mongoClusterResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this MongoClusterResource
+MongoClusterCollection collection = resourceGroupResource.GetMongoClusters();
 
 // invoke the operation
-MongoClusterResource result = await mongoCluster.GetAsync();
+string mongoClusterName = "myMongoCluster";
+NullableResponse<MongoClusterResource> response = await collection.GetIfExistsAsync(mongoClusterName);
+MongoClusterResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-MongoClusterData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    MongoClusterData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

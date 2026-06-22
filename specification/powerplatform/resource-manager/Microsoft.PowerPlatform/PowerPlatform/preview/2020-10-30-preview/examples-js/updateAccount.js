5483dc3382eeb5947d39ea821edda426c165f163
azure-sdk <azuresdk@microsoft.com>
2026-06-22 11:08:07 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-powerplatform_1.0.0-beta.1

---
+++ specification/powerplatform/resource-manager/Microsoft.PowerPlatform/PowerPlatform/preview/2020-10-30-preview/examples-js/updateAccount.js
@@ -0,0 +1,20 @@
+const { PowerPlatformClient } = require("@azure/arm-powerplatform");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to updates an account.
+ *
+ * @summary updates an account.
+ * x-ms-original-file: 2020-10-30-preview/updateAccount.json
+ */
+async function updateAccount() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new PowerPlatformClient(credential, subscriptionId);
+  const result = await client.accounts.update("account", "resourceGroup", {
+    location: "East US",
+    description: "Description of account.",
+    tags: { Organization: "Administration" },
+  });
+  console.log(result);
+}

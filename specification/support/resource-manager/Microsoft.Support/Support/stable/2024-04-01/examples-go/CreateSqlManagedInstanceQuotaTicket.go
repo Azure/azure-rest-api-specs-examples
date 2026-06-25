f418456ec351872113d530721146ebabdfb457d2
azure-sdk <azuresdk@microsoft.com>
2026-06-25 11:10:08 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/support/armsupport/v2.0.0

--- specification/support/resource-manager/Microsoft.Support/Support/stable/2024-04-01/examples-go/CreateSqlManagedInstanceQuotaTicket.go
+++ specification/support/resource-manager/Microsoft.Support/Support/stable/2024-04-01/examples-go/CreateSqlManagedInstanceQuotaTicket.go
@@ -59,13 +59,13 @@ func ExampleTicketsClient_BeginCreate_createATicketToRequestQuotaIncreaseForAzur
 	}
 	res, err := poller.PollUntilDone(ctx, nil)
 	if err != nil {
-		log.Fatalf("failed to pull the result: %v", err)
+		log.Fatalf("failed to poll the result: %v", err)
 	}
 	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res = armsupport.TicketsClientCreateResponse{
-	// 	TicketDetails: &armsupport.TicketDetails{
+	// 	TicketDetails: armsupport.TicketDetails{
 	// 		Name: to.Ptr("testticket"),
 	// 		Type: to.Ptr("Microsoft.Support/supportTickets"),
 	// 		ID: to.Ptr("/subscriptions/132d901f-189d-4381-9214-fe68e27e05a1/providers/Microsoft.Support/supportTickets/testticket"),

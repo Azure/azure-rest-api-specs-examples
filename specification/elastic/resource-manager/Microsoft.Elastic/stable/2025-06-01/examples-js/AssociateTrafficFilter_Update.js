0435b40dd54b0a9a9ab930a446802bb16007406c
azure-sdk <azuresdk@microsoft.com>
2026-06-25 11:12:56 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-elastic_3.0.0-beta.1

---
+++ specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples-js/AssociateTrafficFilter_Update.js
@@ -0,0 +1,17 @@
+const { MicrosoftElastic } = require("@azure/arm-elastic");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to associate a traffic filter with your Elastic monitor resource to control and manage network traffic.
+ *
+ * @summary associate a traffic filter with your Elastic monitor resource to control and manage network traffic.
+ * x-ms-original-file: 2025-06-01/AssociateTrafficFilter_Update.json
+ */
+async function associateTrafficFilterAssociate() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new MicrosoftElastic(credential, subscriptionId);
+  await client.associateTrafficFilter.associate("myResourceGroup", "myMonitor", {
+    rulesetId: "31d91b5afb6f4c2eaaf104c97b1991dd",
+  });
+}
---
+++ specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples-js/IPTrafficFilter_Create.js
@@ -0,0 +1,17 @@
+const { MicrosoftElastic } = require("@azure/arm-elastic");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to create and associate an IP filter with your Elastic monitor resource to control and manage network traffic.
+ *
+ * @summary create and associate an IP filter with your Elastic monitor resource to control and manage network traffic.
+ * x-ms-original-file: 2025-06-01/IPTrafficFilter_Create.json
+ */
+async function createAndAssociateIPFilterCreate() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new MicrosoftElastic(credential, subscriptionId);
+  await client.createAndAssociateIPFilter.create("myResourceGroup", "myMonitor", {
+    ips: "192.168.131.0, 192.168.132.6/22",
+  });
+}

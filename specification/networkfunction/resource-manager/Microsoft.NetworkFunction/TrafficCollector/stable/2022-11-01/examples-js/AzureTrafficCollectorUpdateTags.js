b38951ebfb48fb78b4d55f271dc3a80464e3ae64
azure-sdk <azuresdk@microsoft.com>
2026-06-26 11:21:28 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-networkfunction_3.0.0-beta.1

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/TrafficCollector/stable/2022-11-01/examples-js/AzureTrafficCollectorUpdateTags.js
@@ -0,0 +1,18 @@
+const { AzureTrafficCollectorClient } = require("@azure/arm-networkfunction");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to updates the specified Azure Traffic Collector tags.
+ *
+ * @summary updates the specified Azure Traffic Collector tags.
+ * x-ms-original-file: 2022-11-01/AzureTrafficCollectorUpdateTags.json
+ */
+async function updateTrafficCollectorTags() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new AzureTrafficCollectorClient(credential, subscriptionId);
+  const result = await client.azureTrafficCollectors.updateTags("rg1", "atc", {
+    tags: { key1: "value1", key2: "value2" },
+  });
+  console.log(result);
+}

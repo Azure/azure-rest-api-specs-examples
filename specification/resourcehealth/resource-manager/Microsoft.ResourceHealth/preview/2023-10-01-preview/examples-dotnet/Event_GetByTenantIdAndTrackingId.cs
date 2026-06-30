dd231cb8a30c827c7cdc9e4c6d6e8d208f1e85dc
azure-sdk <azuresdk@microsoft.com>
2026-06-30 11:51:45 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ResourceHealth_1.1.0

--- specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/Event_GetByTenantIdAndTrackingId.cs
+++ specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples-dotnet/Event_GetByTenantIdAndTrackingId.cs
@@ -4,7 +4,6 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.ResourceHealth;
 
 // Generated from example definition: specification/resourcehealth/resource-manager/Microsoft.ResourceHealth/preview/2023-10-01-preview/examples/Event_GetByTenantIdAndTrackingId.json
@@ -15,27 +14,19 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
-
-// get the collection of this TenantResourceHealthEventResource
-TenantResourceHealthEventCollection collection = tenantResource.GetTenantResourceHealthEvents();
+// this example assumes you already have this TenantResourceHealthEventResource created on azure
+// for more information of creating TenantResourceHealthEventResource, please refer to the document of TenantResourceHealthEventResource
+string eventTrackingId = "eventTrackingId";
+ResourceIdentifier tenantResourceHealthEventResourceId = TenantResourceHealthEventResource.CreateResourceIdentifier(eventTrackingId);
+TenantResourceHealthEventResource tenantResourceHealthEvent = client.GetTenantResourceHealthEventResource(tenantResourceHealthEventResourceId);
 
 // invoke the operation
-string eventTrackingId = "eventTrackingId";
 string filter = "properties/status eq 'Active'";
 string queryStartTime = "7/10/2022";
-NullableResponse<TenantResourceHealthEventResource> response = await collection.GetIfExistsAsync(eventTrackingId, filter: filter, queryStartTime: queryStartTime);
-TenantResourceHealthEventResource result = response.HasValue ? response.Value : null;
+TenantResourceHealthEventResource result = await tenantResourceHealthEvent.GetAsync(filter: filter, queryStartTime: queryStartTime);
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    ResourceHealthEventData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+ResourceHealthEventData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");

d1bcc2d319db8fdfe13ecdc54d50f3f12d9ad63b
azure-sdk <azuresdk@microsoft.com>
2026-06-30 11:09:35 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/programenrollment/armprogramenrollment/v0.1.0

---
+++ specification/programenrollment/resource-manager/Microsoft.ProgramEnrollment/ProgramEnrollment/preview/2026-03-01-preview/examples-go/EduEnrollments_Update.go
@@ -0,0 +1,66 @@
+package armprogramenrollment_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/programenrollment/armprogramenrollment"
+)
+
+// Generated from example definition: 2026-03-01-preview/EduEnrollments_Update.json
+func ExampleEduEnrollmentsClient_Update() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armprogramenrollment.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewEduEnrollmentsClient().Update(ctx, "testrg", "default", armprogramenrollment.EduEnrollmentPatch{
+		Tags: map[string]*string{
+			"env": to.Ptr("test"),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armprogramenrollment.EduEnrollmentsClientUpdateResponse{
+	// 	EduEnrollment: armprogramenrollment.EduEnrollment{
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testrg/providers/Microsoft.ProgramEnrollment/eduEnrollments/default"),
+	// 		Name: to.Ptr("default"),
+	// 		Type: to.Ptr("Microsoft.ProgramEnrollment/eduEnrollments"),
+	// 		Location: to.Ptr("eastus"),
+	// 		Tags: map[string]*string{
+	// 			"env": to.Ptr("test"),
+	// 		},
+	// 		Properties: &armprogramenrollment.EduEnrollmentProperties{
+	// 			ProvisioningState: to.Ptr(armprogramenrollment.ProvisioningStateSucceeded),
+	// 			Domains: []*armprogramenrollment.DomainGroup{
+	// 				{
+	// 					DomainNames: []*string{
+	// 						to.Ptr("university.edu"),
+	// 						to.Ptr("college.edu"),
+	// 					},
+	// 					TenantID: to.Ptr("00000000-0000-0000-0000-000000000001"),
+	// 					State: to.Ptr(armprogramenrollment.DomainGroupStateSucceeded),
+	// 				},
+	// 			},
+	// 		},
+	// 		SystemData: &armprogramenrollment.SystemData{
+	// 			CreatedBy: to.Ptr("user@example.com"),
+	// 			CreatedByType: to.Ptr(armprogramenrollment.CreatedByTypeUser),
+	// 			CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2024-01-01T00:00:00.000Z"); return t}()),
+	// 			LastModifiedBy: to.Ptr("user@example.com"),
+	// 			LastModifiedByType: to.Ptr(armprogramenrollment.CreatedByTypeUser),
+	// 			LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2024-01-02T00:00:00.000Z"); return t}()),
+	// 		},
+	// 	},
+	// }
+}

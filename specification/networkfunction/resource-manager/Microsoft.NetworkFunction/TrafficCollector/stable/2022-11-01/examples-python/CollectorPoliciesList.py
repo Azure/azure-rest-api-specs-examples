b31e1a17bea6dd424a324df52904597ec07c9112
azure-sdk <azuresdk@microsoft.com>
2026-06-30 11:34:11 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-networkfunction_1.0.0

---
+++ specification/networkfunction/resource-manager/Microsoft.NetworkFunction/TrafficCollector/stable/2022-11-01/examples-python/CollectorPoliciesList.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.networkfunction import TrafficCollectorMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-networkfunction
+# USAGE
+    python collector_policies_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = TrafficCollectorMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.collector_policies.list(
+        resource_group_name="rg1",
+        azure_traffic_collector_name="atc",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2022-11-01/CollectorPoliciesList.json
+if __name__ == "__main__":
+    main()

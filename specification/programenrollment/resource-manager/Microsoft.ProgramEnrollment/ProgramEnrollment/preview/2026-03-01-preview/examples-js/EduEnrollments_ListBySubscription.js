6567ce9fe07951af8a2e4f4e8f2bf37fe0acdee6
azure-sdk <azuresdk@microsoft.com>
2026-06-30 11:18:14 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-programenrollment_1.0.0-beta.1

---
+++ specification/programenrollment/resource-manager/Microsoft.ProgramEnrollment/ProgramEnrollment/preview/2026-03-01-preview/examples-js/EduEnrollments_ListBySubscription.js
@@ -0,0 +1,20 @@
+const { ProgramEnrollmentClient } = require("@azure/arm-programenrollment");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to lists the edu enrollments in a subscription.
+ *
+ * @summary lists the edu enrollments in a subscription.
+ * x-ms-original-file: 2026-03-01-preview/EduEnrollments_ListBySubscription.json
+ */
+async function listEduEnrollmentsBySubscription() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ProgramEnrollmentClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.eduEnrollments.listBySubscription()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
---
+++ specification/programenrollment/resource-manager/Microsoft.ProgramEnrollment/ProgramEnrollment/preview/2026-03-01-preview/examples-js/EduEnrollments_Update.js
@@ -0,0 +1,16 @@
+const { ProgramEnrollmentClient } = require("@azure/arm-programenrollment");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to updates the specified edu enrollment.
+ *
+ * @summary updates the specified edu enrollment.
+ * x-ms-original-file: 2026-03-01-preview/EduEnrollments_Update.json
+ */
+async function updateAnEduEnrollment() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ProgramEnrollmentClient(credential, subscriptionId);
+  const result = await client.eduEnrollments.update("testrg", "default", { tags: { env: "test" } });
+  console.log(result);
+}

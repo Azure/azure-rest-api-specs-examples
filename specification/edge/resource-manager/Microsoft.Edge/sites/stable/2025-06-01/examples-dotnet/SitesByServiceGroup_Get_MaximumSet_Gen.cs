af62716d1fd75f6b9b4d277381e90db305eed8c4
azure-sdk <azuresdk@microsoft.com>
2026-07-01 11:52:10 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.SiteManager_1.0.1

--- specification/edge/resource-manager/Microsoft.Edge/sites/stable/2025-06-01/examples-dotnet/SitesByServiceGroup_Get_MaximumSet_Gen.cs
+++ specification/edge/resource-manager/Microsoft.Edge/sites/stable/2025-06-01/examples-dotnet/SitesByServiceGroup_Get_MaximumSet_Gen.cs
@@ -15,25 +15,18 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// get the collection of this ServiceGroupEdgeSiteResource
+// this example assumes you already have this ServiceGroupEdgeSiteResource created on azure
+// for more information of creating ServiceGroupEdgeSiteResource, please refer to the document of ServiceGroupEdgeSiteResource
 string servicegroupName = "string";
-string scope = $"/providers/Microsoft.Management/serviceGroups/{servicegroupName}";
-ServiceGroupEdgeSiteCollection collection = client.GetServiceGroupEdgeSites(new ResourceIdentifier(scope));
+string siteName = "string";
+ResourceIdentifier serviceGroupEdgeSiteResourceId = ServiceGroupEdgeSiteResource.CreateResourceIdentifier(servicegroupName, siteName);
+ServiceGroupEdgeSiteResource serviceGroupEdgeSite = client.GetServiceGroupEdgeSiteResource(serviceGroupEdgeSiteResourceId);
 
 // invoke the operation
-string siteName = "string";
-NullableResponse<ServiceGroupEdgeSiteResource> response = await collection.GetIfExistsAsync(siteName);
-ServiceGroupEdgeSiteResource result = response.HasValue ? response.Value : null;
+ServiceGroupEdgeSiteResource result = await serviceGroupEdgeSite.GetAsync();
 
-if (result == null)
-{
-    Console.WriteLine("Succeeded with null as result");
-}
-else
-{
-    // the variable result is a resource, you could call other operations on this instance as well
-    // but just for demo, we get its data from this resource instance
-    EdgeSiteData resourceData = result.Data;
-    // for demo we just print out the id
-    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-}
+// the variable result is a resource, you could call other operations on this instance as well
+// but just for demo, we get its data from this resource instance
+EdgeSiteData resourceData = result.Data;
+// for demo we just print out the id
+Console.WriteLine($"Succeeded on id: {resourceData.Id}");
--- specification/edge/resource-manager/Microsoft.Edge/sites/stable/2025-06-01/examples-dotnet/Sites_Get_MaximumSet_Gen.cs
+++ specification/edge/resource-manager/Microsoft.Edge/sites/stable/2025-06-01/examples-dotnet/Sites_Get_MaximumSet_Gen.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.SiteManager.Models;
 using Azure.ResourceManager.SiteManager;
 
@@ -15,19 +16,30 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ResourceGroupEdgeSiteResource created on azure
-// for more information of creating ResourceGroupEdgeSiteResource, please refer to the document of ResourceGroupEdgeSiteResource
+// this example assumes you already have this ResourceGroupResource created on azure
+// for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
 string subscriptionId = "0154f7fe-df09-4981-bf82-7ad5c1f596eb";
 string resourceGroupName = "rgsites";
-string siteName = "string";
-ResourceIdentifier resourceGroupEdgeSiteResourceId = ResourceGroupEdgeSiteResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, siteName);
-ResourceGroupEdgeSiteResource resourceGroupEdgeSite = client.GetResourceGroupEdgeSiteResource(resourceGroupEdgeSiteResourceId);
+ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
+ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
+
+// get the collection of this ResourceGroupEdgeSiteResource
+ResourceGroupEdgeSiteCollection collection = resourceGroupResource.GetResourceGroupEdgeSites();
 
 // invoke the operation
-ResourceGroupEdgeSiteResource result = await resourceGroupEdgeSite.GetAsync();
+string siteName = "string";
+NullableResponse<ResourceGroupEdgeSiteResource> response = await collection.GetIfExistsAsync(siteName);
+ResourceGroupEdgeSiteResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-EdgeSiteData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    EdgeSiteData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

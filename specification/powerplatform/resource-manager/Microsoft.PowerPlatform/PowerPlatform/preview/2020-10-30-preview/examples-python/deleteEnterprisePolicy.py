6b13988f961c40262a3877e9b6a0ab88ae4e3218
azure-sdk <azuresdk@microsoft.com>
2026-07-01 11:25:41 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-powerplatform_1.0.0b1

---
+++ specification/powerplatform/resource-manager/Microsoft.PowerPlatform/PowerPlatform/preview/2020-10-30-preview/examples-python/deleteEnterprisePolicy.py
@@ -0,0 +1,33 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.powerplatform import PowerPlatformMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-powerplatform
+# USAGE
+    python delete_enterprise_policy.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PowerPlatformMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    client.enterprise_policies.delete(
+        resource_group_name="resourceGroup",
+        enterprise_policy_name="enterprisePolicy",
+    )
+
+
+# x-ms-original-file: 2020-10-30-preview/deleteEnterprisePolicy.json
+if __name__ == "__main__":
+    main()
---
+++ specification/powerplatform/resource-manager/Microsoft.PowerPlatform/PowerPlatform/preview/2020-10-30-preview/examples-python/listEnterprisePoliciesByResourceGroup.py
@@ -0,0 +1,34 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.powerplatform import PowerPlatformMgmtClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-powerplatform
+# USAGE
+    python list_enterprise_policies_by_resource_group.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = PowerPlatformMgmtClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.enterprise_policies.list_by_resource_group(
+        resource_group_name="rg1",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2020-10-30-preview/listEnterprisePoliciesByResourceGroup.json
+if __name__ == "__main__":
+    main()

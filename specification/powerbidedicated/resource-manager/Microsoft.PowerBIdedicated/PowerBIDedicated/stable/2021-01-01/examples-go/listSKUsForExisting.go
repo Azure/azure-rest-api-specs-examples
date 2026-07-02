f6f0a92b13f7feefcf8ecfad1718724c7511b140
azure-sdk <azuresdk@microsoft.com>
2026-07-02 11:17:29 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/powerbidedicated/armpowerbidedicated/v2.0.0

--- specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/PowerBIDedicated/stable/2021-01-01/examples-go/listSKUsForExisting.go
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/PowerBIDedicated/stable/2021-01-01/examples-go/listSKUsForExisting.go
@@ -27,7 +27,7 @@ func ExampleCapacitiesClient_ListSKUsForCapacity() {
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res = armpowerbidedicated.CapacitiesClientListSKUsForCapacityResponse{
-	// 	SKUEnumerationForExistingResourceResult: &armpowerbidedicated.SKUEnumerationForExistingResourceResult{
+	// 	SKUEnumerationForExistingResourceResult: armpowerbidedicated.SKUEnumerationForExistingResourceResult{
 	// 		Value: []*armpowerbidedicated.SKUDetailsForExistingResource{
 	// 			{
 	// 				SKU: &armpowerbidedicated.CapacitySKU{
--- specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/PowerBIDedicated/stable/2021-01-01/examples-go/updateToGen2.go
+++ specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/PowerBIDedicated/stable/2021-01-01/examples-go/updateToGen2.go
@@ -37,13 +37,13 @@ func ExampleCapacitiesClient_BeginUpdate_updateCapacityToGeneration2() {
 	}
 	res, err := poller.PollUntilDone(ctx, nil)
 	if err != nil {
-		log.Fatalf("failed to pull the result: %v", err)
+		log.Fatalf("failed to poll the result: %v", err)
 	}
 	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res = armpowerbidedicated.CapacitiesClientUpdateResponse{
-	// 	DedicatedCapacity: &armpowerbidedicated.DedicatedCapacity{
+	// 	DedicatedCapacity: armpowerbidedicated.DedicatedCapacity{
 	// 		Name: to.Ptr("azsdktest"),
 	// 		ID: to.Ptr("/subscriptions/613192d7-503f-477a-9cfe-4efc3ee2bd60/resourceGroups/TestRG/providers/Microsoft.PowerBIDedicated/capacities/azsdktest"),
 	// 		Location: to.Ptr("West US"),

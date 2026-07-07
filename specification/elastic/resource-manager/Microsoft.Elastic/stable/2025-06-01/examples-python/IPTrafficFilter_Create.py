874368899b0afdd1ce9fb91b87d9cf0aa509c743
azure-sdk <azuresdk@microsoft.com>
2026-07-07 11:27:10 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-elastic_3.0.0b1

--- specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples-python/IPTrafficFilter_Create.py
+++ specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples-python/IPTrafficFilter_Create.py
@@ -1,6 +1,6 @@
 from azure.identity import DefaultAzureCredential
 
-from azure.mgmt.elastic import MicrosoftElastic
+from azure.mgmt.elastic import ElasticMgmtClient
 
 """
 # PREREQUISITES
@@ -17,9 +17,9 @@ from azure.mgmt.elastic import MicrosoftElastic
 
 
 def main():
-    client = MicrosoftElastic(
+    client = ElasticMgmtClient(
         credential=DefaultAzureCredential(),
-        subscription_id="00000000-0000-0000-0000-000000000000",
+        subscription_id="SUBSCRIPTION_ID",
     )
 
     client.create_and_associate_ip_filter.begin_create(
@@ -28,6 +28,6 @@ def main():
     ).result()
 
 
-# x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples/IPTrafficFilter_Create.json
+# x-ms-original-file: 2025-06-01/IPTrafficFilter_Create.json
 if __name__ == "__main__":
     main()
--- specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples-python/OpenAI_Get.py
+++ specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples-python/OpenAI_Get.py
@@ -1,6 +1,6 @@
 from azure.identity import DefaultAzureCredential
 
-from azure.mgmt.elastic import MicrosoftElastic
+from azure.mgmt.elastic import ElasticMgmtClient
 
 """
 # PREREQUISITES
@@ -17,9 +17,9 @@ from azure.mgmt.elastic import MicrosoftElastic
 
 
 def main():
-    client = MicrosoftElastic(
+    client = ElasticMgmtClient(
         credential=DefaultAzureCredential(),
-        subscription_id="00000000-0000-0000-0000-000000000000",
+        subscription_id="SUBSCRIPTION_ID",
     )
 
     response = client.open_ai.get(
@@ -30,6 +30,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples/OpenAI_Get.json
+# x-ms-original-file: 2025-06-01/OpenAI_Get.json
 if __name__ == "__main__":
     main()

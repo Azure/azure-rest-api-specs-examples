9a8939a3172f286b857b71895ede2beba31e564d
azure-sdk <azuresdk@microsoft.com>
2026-07-10 11:38:47 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-dns_10.0.0b1

---
+++ specification/dns/resource-manager/Microsoft.Network/Dns/preview/2023-07-01-preview/examples-python/ListPTRRecordset.py
@@ -0,0 +1,36 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.dns import DnsManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-dns
+# USAGE
+    python list_ptr_recordset.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = DnsManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.record_sets.list_by_type(
+        resource_group_name="rg1",
+        zone_name="0.0.127.in-addr.arpa",
+        record_type="PTR",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2023-07-01-preview/ListPTRRecordset.json
+if __name__ == "__main__":
+    main()

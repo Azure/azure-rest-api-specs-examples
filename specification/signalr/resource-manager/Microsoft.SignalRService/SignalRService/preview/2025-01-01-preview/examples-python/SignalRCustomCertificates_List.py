4401ae48e8ca00e78609e92a8a61f2d62c704ca2
azure-sdk <azuresdk@microsoft.com>
2026-07-10 11:48:06 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-signalr_2.0.0b3

---
+++ specification/signalr/resource-manager/Microsoft.SignalRService/SignalRService/preview/2025-01-01-preview/examples-python/SignalRCustomCertificates_List.py
@@ -0,0 +1,35 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.signalr import SignalRManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-signalr
+# USAGE
+    python signal_rcustom_certificates_list.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = SignalRManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.signal_rcustom_certificates.list(
+        resource_group_name="myResourceGroup",
+        resource_name="mySignalRService",
+    )
+    for item in response:
+        print(item)
+
+
+# x-ms-original-file: 2025-01-01-preview/SignalRCustomCertificates_List.json
+if __name__ == "__main__":
+    main()

753acbee169e118324b18c69325555694297b186
azure-sdk <azuresdk@microsoft.com>
2026-07-17 11:10:35 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/compute/armbulkactions/v0.1.0

---
+++ specification/compute/resource-manager/Microsoft.Compute/Bulkactions/stable/2026-06-06/examples-go/VirtualMachineBulkOperations_BulkStart_MinimumSet_Gen.go
@@ -0,0 +1,44 @@
+package armbulkactions_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armbulkactions"
+)
+
+// Generated from example definition: 2026-06-06/VirtualMachineBulkOperations_BulkStart_MinimumSet_Gen.json
+func ExampleVirtualMachineBulkOperationsClient_BulkStartOperation_virtualMachineBulkOperationsBulkStartGeneratedByMinimumSetRule() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armbulkactions.NewClientFactory("401789D7-9B98-4B5A-AF58-808C415E37B4", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewVirtualMachineBulkOperationsClient().BulkStartOperation(ctx, "myResourceGroup", "eastus2euap", armbulkactions.ExecuteStartContent{
+		ExecutionParameters: &armbulkactions.ExecutionParameters{},
+		Resources: &armbulkactions.Resources{
+			IDs: []*string{
+				to.Ptr("/subscriptions/12345678-1234-1234-1234-123456789012/resourceGroups/myResourceGroup/providers/Microsoft.Compute/virtualMachines/myVM"),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armbulkactions.VirtualMachineBulkOperationsClientBulkStartOperationResponse{
+	// 	StartResourceOperationResponse: armbulkactions.StartResourceOperationResponse{
+	// 		Type: to.Ptr("VirtualMachine"),
+	// 		Location: to.Ptr("eastus2euap"),
+	// 		Description: to.Ptr("Start Resource request"),
+	// 	},
+	// }
+}

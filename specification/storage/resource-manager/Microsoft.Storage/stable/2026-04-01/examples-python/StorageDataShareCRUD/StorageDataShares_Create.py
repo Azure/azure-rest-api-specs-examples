a42414fe31f46193f7afdef658fd600266692d25
azure-sdk <azuresdk@microsoft.com>
2026-07-20 11:27:37 +0000

    [Automation] Collect examples from azure-sdk-for-python#azure-mgmt-storage_25.1.0

---
+++ specification/storage/resource-manager/Microsoft.Storage/stable/2026-04-01/examples-python/StorageDataShareCRUD/StorageDataShares_Create.py
@@ -0,0 +1,49 @@
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.storage import StorageManagementClient
+
+"""
+# PREREQUISITES
+    pip install azure-identity
+    pip install azure-mgmt-storage
+# USAGE
+    python storage_data_shares_create.py
+
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
+    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
+    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
+    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
+"""
+
+
+def main():
+    client = StorageManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
+    )
+
+    response = client.data_shares.begin_create(
+        resource_group_name="testrg",
+        account_name="teststorageaccount",
+        data_share_name="testdatashare",
+        resource={
+            "location": "eastus",
+            "properties": {
+                "accessPolicies": [
+                    {
+                        "permission": "Read",
+                        "principalId": "00000000-0000-0000-0000-000000000000",
+                        "tenantId": "00000000-0000-0000-0000-000000000000",
+                    }
+                ],
+                "assets": [{"assetPath": "/container/folder/foo", "displayName": "virtualFoo"}],
+                "description": "Dummy data share",
+            },
+        },
+    ).result()
+    print(response)
+
+
+# x-ms-original-file: 2026-04-01/StorageDataShareCRUD/StorageDataShares_Create.json
+if __name__ == "__main__":
+    main()

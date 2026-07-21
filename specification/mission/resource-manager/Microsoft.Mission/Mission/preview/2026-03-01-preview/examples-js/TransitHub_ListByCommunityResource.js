d70d2157121c94e62751059690cac407f693a7ad
azure-sdk <azuresdk@microsoft.com>
2026-07-21 11:07:50 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-enclave_1.0.0-beta.1

---
+++ specification/mission/resource-manager/Microsoft.Mission/Mission/preview/2026-03-01-preview/examples-js/TransitHub_ListByCommunityResource.js
@@ -0,0 +1,23 @@
+const { MissionClient } = require("@azure/arm-enclave");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list TransitHubResource resources by CommunityResource
+ *
+ * @summary list TransitHubResource resources by CommunityResource
+ * x-ms-original-file: 2026-03-01-preview/TransitHub_ListByCommunityResource.json
+ */
+async function transitHubListByCommunityResource() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "CA1CB369-DD26-4DB2-9D43-9AFEF0F22093";
+  const client = new MissionClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.transitHub.listByCommunityResource(
+    "rgopenapi",
+    "TestMyCommunity",
+  )) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

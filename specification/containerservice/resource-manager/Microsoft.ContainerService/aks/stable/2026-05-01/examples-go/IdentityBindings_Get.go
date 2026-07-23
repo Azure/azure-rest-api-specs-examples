acca592f00e1faddcfd10002108af07c8e5164f0
azure-sdk <azuresdk@microsoft.com>
2026-07-23 11:06:59 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v9.4.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2026-05-01/examples-go/IdentityBindings_Get.go
@@ -0,0 +1,49 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-05-01/IdentityBindings_Get.json
+func ExampleIdentityBindingsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewIdentityBindingsClient().Get(ctx, "rg1", "clustername1", "identitybinding1", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armcontainerservice.IdentityBindingsClientGetResponse{
+	// 	IdentityBinding: armcontainerservice.IdentityBinding{
+	// 		Name: to.Ptr("identitybinding1"),
+	// 		Type: to.Ptr("Microsoft.ContainerService/managedClusters/identityBindings"),
+	// 		ETag: to.Ptr("string"),
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/identityBindings/identitybinding1"),
+	// 		Properties: &armcontainerservice.IdentityBindingProperties{
+	// 			ManagedIdentity: &armcontainerservice.IdentityBindingManagedIdentityProfile{
+	// 				ClientID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 				ObjectID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 				ResourceID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ManagedIdentity/userAssignedIdentities/identity1"),
+	// 				TenantID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 			},
+	// 			OidcIssuer: &armcontainerservice.IdentityBindingOidcIssuerProfile{
+	// 				OidcIssuerURL: to.Ptr("https://oidc-endpoint"),
+	// 			},
+	// 			ProvisioningState: to.Ptr(armcontainerservice.IdentityBindingProvisioningStateSucceeded),
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2026-05-01/examples-go/ManagedClustersResetAADProfile.go
@@ -0,0 +1,41 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-05-01/ManagedClustersResetAADProfile.json
+func ExampleManagedClustersClient_BeginResetAADProfile() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewManagedClustersClient().BeginResetAADProfile(ctx, "rg1", "clustername1", armcontainerservice.ManagedClusterAADProfile{
+		ClientAppID:     to.Ptr("clientappid"),
+		ServerAppID:     to.Ptr("serverappid"),
+		ServerAppSecret: to.Ptr("serverappsecret"),
+		TenantID:        to.Ptr("tenantid"),
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armcontainerservice.ManagedClustersClientResetAADProfileResponse{
+	// }
+}

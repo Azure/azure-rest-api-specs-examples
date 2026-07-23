99a73c28594477cc9f63dbdc745e5a2a717644d7
azure-sdk <azuresdk@microsoft.com>
2026-07-23 11:20:24 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-netapp_24.2.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/NetApp/stable/2026-05-01/examples-js/Caches_ResetSmbPassword.js
@@ -0,0 +1,21 @@
+const { NetAppManagementClient } = require("@azure/arm-netapp");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to resets the SMB password for the cache
+ *
+ * @summary resets the SMB password for the cache
+ * x-ms-original-file: 2026-05-01/Caches_ResetSmbPassword.json
+ */
+async function cachesResetSmbPassword() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new NetAppManagementClient(credential, subscriptionId);
+  const result = await client.caches.resetSmbPassword(
+    "myResourceGroup",
+    "account1",
+    "pool1",
+    "cache1",
+  );
+  console.log(result);
+}

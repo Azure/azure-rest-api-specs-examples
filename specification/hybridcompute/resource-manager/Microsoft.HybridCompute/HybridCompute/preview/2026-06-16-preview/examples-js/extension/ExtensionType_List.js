19804842463f852bb098c5400e6f3c3e5ca094e5
azure-sdk <azuresdk@microsoft.com>
2026-07-23 11:17:54 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-hybridcompute_5.0.0-beta.3

---
+++ specification/hybridcompute/resource-manager/Microsoft.HybridCompute/HybridCompute/preview/2026-06-16-preview/examples-js/extension/ExtensionType_List.js
@@ -0,0 +1,19 @@
+const { HybridComputeManagementClient } = require("@azure/arm-hybridcompute");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets all Extension types based on location and publisher
+ *
+ * @summary gets all Extension types based on location and publisher
+ * x-ms-original-file: 2026-06-16-preview/extension/ExtensionType_List.json
+ */
+async function getAListOfExtensionTypes() {
+  const credential = new DefaultAzureCredential();
+  const client = new HybridComputeManagementClient(credential);
+  const resArray = new Array();
+  for await (const item of client.extensionType.list("EastUS", "microsoft.azure.monitor")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}

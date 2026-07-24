c5c16a80075780c73d5a0e0b328153d4f1ee5b4b
azure-sdk <azuresdk@microsoft.com>
2026-07-24 11:27:29 +0000

    [Automation] Collect examples from azure-sdk-for-java#com.azure.resourcemanager+azure-resourcemanager-netapp_2.4.0

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/NetApp/stable/2026-05-01/examples-java/Caches_ResetSmbPassword.java
@@ -0,0 +1,18 @@
+
+/**
+ * Samples for Caches ResetSmbPassword.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file: 2026-05-01/Caches_ResetSmbPassword.json
+     */
+    /**
+     * Sample code: Caches_ResetSmbPassword.
+     * 
+     * @param manager Entry point to NetAppFilesManager.
+     */
+    public static void cachesResetSmbPassword(com.azure.resourcemanager.netapp.NetAppFilesManager manager) {
+        manager.caches().resetSmbPassword("myResourceGroup", "account1", "pool1", "cache1",
+            com.azure.core.util.Context.NONE);
+    }
+}

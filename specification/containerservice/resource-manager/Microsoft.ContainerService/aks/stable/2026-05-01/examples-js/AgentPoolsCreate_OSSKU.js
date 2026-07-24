c7f9de0d050605cc65f3414d04cdc15df7128134
azure-sdk <azuresdk@microsoft.com>
2026-07-24 11:13:24 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_25.4.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2026-05-01/examples-js/AgentPoolsCreate_OSSKU.js
@@ -0,0 +1,43 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to creates or updates an agent pool in the specified managed cluster.
+ *
+ * @summary creates or updates an agent pool in the specified managed cluster.
+ * x-ms-original-file: 2026-05-01/AgentPoolsCreate_OSSKU.json
+ */
+async function createAgentPoolWithOssku() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.agentPools.createOrUpdate("rg1", "clustername1", "agentpool1", {
+    count: 3,
+    kubeletConfig: {
+      allowedUnsafeSysctls: ["kernel.msg*", "net.core.somaxconn"],
+      cpuCfsQuota: true,
+      cpuCfsQuotaPeriod: "200ms",
+      cpuManagerPolicy: "static",
+      failSwapOn: false,
+      imageGcHighThreshold: 90,
+      imageGcLowThreshold: 70,
+      topologyManagerPolicy: "best-effort",
+    },
+    linuxOSConfig: {
+      swapFileSizeMB: 1500,
+      sysctls: {
+        kernelThreadsMax: 99999,
+        netCoreWmemDefault: 12345,
+        netIpv4IpLocalPortRange: "20000 60000",
+        netIpv4TcpTwReuse: true,
+      },
+      transparentHugePageDefrag: "madvise",
+      transparentHugePageEnabled: "always",
+    },
+    orchestratorVersion: "",
+    osSKU: "AzureLinux",
+    osType: "Linux",
+    vmSize: "Standard_DS2_v2",
+  });
+  console.log(result);
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2026-05-01/examples-js/ManagedClustersGet_MeshRevisionProfile.js
@@ -0,0 +1,16 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to contains extra metadata on the revision, including supported revisions, cluster compatibility and available upgrades
+ *
+ * @summary contains extra metadata on the revision, including supported revisions, cluster compatibility and available upgrades
+ * x-ms-original-file: 2026-05-01/ManagedClustersGet_MeshRevisionProfile.json
+ */
+async function getAMeshRevisionProfileForAMeshMode() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.managedClusters.getMeshRevisionProfile("location1", "istio");
+  console.log(result);
+}

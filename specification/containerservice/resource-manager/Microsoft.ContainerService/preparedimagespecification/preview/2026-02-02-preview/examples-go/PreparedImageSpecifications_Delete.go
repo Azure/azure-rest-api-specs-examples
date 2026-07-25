666b485416b434b529392c9c5201b7369c6cd9b3
azure-sdk <azuresdk@microsoft.com>
2026-07-25 11:06:59 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservicepreparedimgspec/armcontainerservicepreparedimgspec/v0.1.0

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/preparedimagespecification/preview/2026-02-02-preview/examples-go/PreparedImageSpecifications_Delete.go
@@ -0,0 +1,30 @@
+package armcontainerservicepreparedimgspec_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservicepreparedimgspec/armcontainerservicepreparedimgspec"
+)
+
+// Generated from example definition: 2026-02-02-preview/PreparedImageSpecifications_Delete.json
+func ExamplePreparedImageSpecificationsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservicepreparedimgspec.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewPreparedImageSpecificationsClient().BeginDelete(ctx, "rg1", "my-prepared-image-specification", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+}

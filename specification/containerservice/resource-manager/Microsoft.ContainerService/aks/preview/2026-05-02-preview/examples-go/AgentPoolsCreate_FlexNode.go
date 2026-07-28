6b8cd49dff1575b636a41cc16d8d9aa455c35627
azure-sdk <azuresdk@microsoft.com>
2026-07-28 11:09:23 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v9.5.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-05-02-preview/examples-go/AgentPoolsCreate_FlexNode.go
@@ -0,0 +1,53 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-05-02-preview/AgentPoolsCreate_FlexNode.json
+func ExampleAgentPoolsClient_BeginCreateOrUpdate_createFlexNodeAgentPool() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewAgentPoolsClient().BeginCreateOrUpdate(ctx, "rg1", "clustername1", "flexnode1", armcontainerservice.AgentPool{
+		Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+			Mode:                to.Ptr(armcontainerservice.AgentPoolModeUser),
+			OrchestratorVersion: to.Ptr("1.32"),
+			Type:                to.Ptr(armcontainerservice.AgentPoolTypeFlexNodes),
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armcontainerservice.AgentPoolsClientCreateOrUpdateResponse{
+	// 	AgentPool: armcontainerservice.AgentPool{
+	// 		Name: to.Ptr("flexnode1"),
+	// 		Type: to.Ptr("Microsoft.ContainerService/managedClusters/agentPools"),
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/agentPools/flexnode1"),
+	// 		Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+	// 			Mode: to.Ptr(armcontainerservice.AgentPoolModeUser),
+	// 			OrchestratorVersion: to.Ptr("1.32"),
+	// 			ProvisioningState: to.Ptr("Succeeded"),
+	// 			Type: to.Ptr(armcontainerservice.AgentPoolTypeFlexNodes),
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-05-02-preview/examples-go/AlertConfigurations_ListByManagedCluster.go
@@ -0,0 +1,60 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-05-02-preview/AlertConfigurations_ListByManagedCluster.json
+func ExampleAlertConfigurationsClient_NewListByManagedClusterPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewAlertConfigurationsClient().NewListByManagedClusterPager("rg1", "clustername1", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armcontainerservice.AlertConfigurationsClientListByManagedClusterResponse{
+		// 	AlertConfigurationListResult: armcontainerservice.AlertConfigurationListResult{
+		// 		Value: []*armcontainerservice.AlertConfiguration{
+		// 			{
+		// 				Name: to.Ptr("alertconfig1"),
+		// 				Type: to.Ptr("Microsoft.ContainerService/managedClusters/alertConfigurations"),
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/alertConfigurations/alertconfig1"),
+		// 				Properties: &armcontainerservice.AlertConfigurationProperties{
+		// 					Mode: to.Ptr(armcontainerservice.AlertConfigurationModeManaged),
+		// 					Notification: &armcontainerservice.AlertNotification{
+		// 						ActionGroupID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Insights/actionGroups/actiongroup1"),
+		// 					},
+		// 					ProvisioningState: to.Ptr(armcontainerservice.AlertConfigurationProvisioningStateSucceeded),
+		// 				},
+		// 				SystemData: &armcontainerservice.SystemData{
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2025-01-01T00:00:00.0000000Z"); return t}()),
+		// 					CreatedBy: to.Ptr("user@example.com"),
+		// 					CreatedByType: to.Ptr(armcontainerservice.CreatedByTypeUser),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2025-01-01T00:00:00.0000000Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("user@example.com"),
+		// 					LastModifiedByType: to.Ptr(armcontainerservice.CreatedByTypeUser),
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// }
+	}
+}

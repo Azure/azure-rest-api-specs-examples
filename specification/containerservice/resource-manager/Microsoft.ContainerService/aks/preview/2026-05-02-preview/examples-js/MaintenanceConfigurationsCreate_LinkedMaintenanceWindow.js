048472f1787df3331b9ea028cd5e929cb18d0964
azure-sdk <azuresdk@microsoft.com>
2026-07-28 11:16:09 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerservice_25.5.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-05-02-preview/examples-js/MaintenanceConfigurationsCreate_LinkedMaintenanceWindow.js
@@ -0,0 +1,24 @@
+const { ContainerServiceClient } = require("@azure/arm-containerservice");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to creates or updates a maintenance configuration in the specified managed cluster.
+ *
+ * @summary creates or updates a maintenance configuration in the specified managed cluster.
+ * x-ms-original-file: 2026-05-02-preview/MaintenanceConfigurationsCreate_LinkedMaintenanceWindow.json
+ */
+async function createALinkedMaintenanceConfiguration() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  const result = await client.maintenanceConfigurations.createOrUpdate(
+    "rg1",
+    "clustername1",
+    "aksManagedAutoUpgradeSchedule",
+    {
+      maintenanceWindowId:
+        "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/maintenanceWindows/myMaintenanceWindow",
+    },
+  );
+  console.log(result);
+}

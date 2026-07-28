d28b3b66efd8d567a8ff73adeba68e80df6691ee
azure-sdk <azuresdk@microsoft.com>
2026-07-28 11:28:00 +0000

    [Automation] Collect examples from azure-sdk-for-java#com.azure.resourcemanager+azure-resourcemanager-containerservice_2.63.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-05-02-preview/examples-java/AlertConfigurations_Get.java
@@ -0,0 +1,19 @@
+
+/**
+ * Samples for AlertConfigurations Get.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file: 2026-05-02-preview/AlertConfigurations_Get.json
+     */
+    /**
+     * Sample code: Get Alert Configuration.
+     * 
+     * @param manager Entry point to ContainerServiceManager.
+     */
+    public static void
+        getAlertConfiguration(com.azure.resourcemanager.containerservice.ContainerServiceManager manager) {
+        manager.serviceClient().getAlertConfigurations().getWithResponse("rg1", "clustername1", "alertconfig1",
+            com.azure.core.util.Context.NONE);
+    }
+}

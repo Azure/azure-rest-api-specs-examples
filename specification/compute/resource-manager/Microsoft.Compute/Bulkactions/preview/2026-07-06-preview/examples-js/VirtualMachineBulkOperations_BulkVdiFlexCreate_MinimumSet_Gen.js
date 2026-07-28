1dda4236cc1740176f1400c289a18dee638717a2
azure-sdk <azuresdk@microsoft.com>
2026-07-28 11:13:53 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-compute-bulkactions_1.0.0-beta.2

---
+++ specification/compute/resource-manager/Microsoft.Compute/Bulkactions/preview/2026-07-06-preview/examples-js/VirtualMachineBulkOperations_BulkVdiFlexCreate_MinimumSet_Gen.js
@@ -0,0 +1,30 @@
+const { ComputeClient } = require("@azure/arm-compute-bulkactions");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to bulkVdiFlexCreate: Bulk create  operation for a batch of virtual machines, this operation supports flex properties to give options on Sku and zone selection.
+ *
+ * @summary bulkVdiFlexCreate: Bulk create  operation for a batch of virtual machines, this operation supports flex properties to give options on Sku and zone selection.
+ * x-ms-original-file: 2026-07-06-preview/VirtualMachineBulkOperations_BulkVdiFlexCreate_MinimumSet_Gen.json
+ */
+async function virtualMachineBulkOperationsBulkVdiFlexCreateExampleGeneratedByMinimumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "1FBA3C66-5C9C-4391-B72F-9F52735FC9F2";
+  const client = new ComputeClient(credential, subscriptionId);
+  const result = await client.virtualMachineBulkOperations.bulkVdiFlexCreateOperation(
+    "rgBulkactions",
+    "useast2euap",
+    {
+      resourceConfigParameters: {
+        resourceCount: 10,
+        flexProperties: {
+          vmSizeProfiles: [{ name: "Standard_D2ads_v5", rank: 7 }],
+          osType: "Windows",
+          priorityProfile: {},
+        },
+      },
+      executionParameters: {},
+    },
+  );
+  console.log(result);
+}

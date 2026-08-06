3a1afe6e5292665a94676789ee68d52c1260eb62
azure-sdk <azuresdk@microsoft.com>
2026-08-06 11:14:53 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-containerserviceaimanager_1.0.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aimanager/preview/2026-05-02-preview/examples-js/ModelSources_Delete.js
@@ -0,0 +1,17 @@
+const { ContainerServiceClient } = require("@azure/arm-containerserviceaimanager");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a ModelSource
+ *
+ * @summary delete a ModelSource
+ * x-ms-original-file: 2026-05-02-preview/ModelSources_Delete.json
+ */
+async function modelSourcesDeleteMaximumSet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerServiceClient(credential, subscriptionId);
+  await client.modelSources.delete("rgaimanagers", "aimanager1", "huggingface", {
+    ifMatch: '"abc123def456"',
+  });
+}

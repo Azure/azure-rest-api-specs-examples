c449097d69bc843b54e49d8a3617e1df8a716030
azure-sdk <azuresdk@microsoft.com>
2026-08-06 11:11:38 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/hardwaresecuritymodules/armhardwaresecuritymodules/v3.1.0-beta.1

---
+++ specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/HardwareSecurityModules/preview/2025-12-01-preview/examples-go/CloudHsmClusterPrivateEndpointConnection_Delete_MaximumSet_Gen.go
@@ -0,0 +1,30 @@
+package armhardwaresecuritymodules_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hardwaresecuritymodules/armhardwaresecuritymodules/v3"
+)
+
+// Generated from example definition: 2025-12-01-preview/CloudHsmClusterPrivateEndpointConnection_Delete_MaximumSet_Gen.json
+func ExampleCloudHsmClusterPrivateEndpointConnectionsClient_BeginDelete() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armhardwaresecuritymodules.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewCloudHsmClusterPrivateEndpointConnectionsClient().BeginDelete(ctx, "rgcloudhsm", "chsm1", "sample-pec", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	_, err = poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+}
---
+++ specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/HardwareSecurityModules/preview/2025-12-01-preview/examples-go/PaymentHsmClusterPrivateEndpointConnection_ListByPaymentHsmCluster_MaximumSet_Gen.go
@@ -0,0 +1,94 @@
+package armhardwaresecuritymodules_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hardwaresecuritymodules/armhardwaresecuritymodules/v3"
+)
+
+// Generated from example definition: 2025-12-01-preview/PaymentHsmClusterPrivateEndpointConnection_ListByPaymentHsmCluster_MaximumSet_Gen.json
+func ExamplePaymentHsmClusterPrivateEndpointConnectionsClient_NewListByPaymentHsmClusterPager() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armhardwaresecuritymodules.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewPaymentHsmClusterPrivateEndpointConnectionsClient().NewListByPaymentHsmClusterPager("rgpaymenthsm", "phsm1", nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armhardwaresecuritymodules.PaymentHsmClusterPrivateEndpointConnectionsClientListByPaymentHsmClusterResponse{
+		// 	PaymentHsmClusterPrivateEndpointConnectionListResult: armhardwaresecuritymodules.PaymentHsmClusterPrivateEndpointConnectionListResult{
+		// 		Value: []*armhardwaresecuritymodules.PaymentHsmClusterPrivateEndpointConnection{
+		// 			{
+		// 				Name: to.Ptr("sample-pec1"),
+		// 				Type: to.Ptr("Microsoft.HardwareSecurityModules/paymentHsmClusters/privateEndpointConnections"),
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rgpaymenthsm/providers/Microsoft.HardwareSecurityModules/paymentHsmClusters/phsm1/privateEndpointConnections/sample-pec1"),
+		// 				Properties: &armhardwaresecuritymodules.PrivateEndpointConnectionProperties{
+		// 					GroupIDs: []*string{
+		// 						to.Ptr("management"),
+		// 					},
+		// 					PrivateEndpoint: &armhardwaresecuritymodules.PrivateEndpoint{
+		// 						ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rgpaymenthsm/providers/Microsoft.Network/privateEndpoints/sample-pec1"),
+		// 					},
+		// 					PrivateLinkServiceConnectionState: &armhardwaresecuritymodules.PrivateLinkServiceConnectionState{
+		// 						Description: to.Ptr("This was automatically approved by user1234@contoso.com"),
+		// 						ActionsRequired: to.Ptr("None"),
+		// 						Status: to.Ptr(armhardwaresecuritymodules.PrivateEndpointServiceConnectionStatusApproved),
+		// 					},
+		// 					ProvisioningState: to.Ptr(armhardwaresecuritymodules.PrivateEndpointConnectionProvisioningStateSucceeded),
+		// 				},
+		// 				SystemData: &armhardwaresecuritymodules.SystemData{
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T12:00:00.0000000Z"); return t}()),
+		// 					CreatedBy: to.Ptr("User1"),
+		// 					CreatedByType: to.Ptr(armhardwaresecuritymodules.CreatedByTypeUser),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T12:00:00.0000000Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("User2"),
+		// 					LastModifiedByType: to.Ptr(armhardwaresecuritymodules.CreatedByTypeUser),
+		// 				},
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("sample-pec2"),
+		// 				Type: to.Ptr("Microsoft.HardwareSecurityModules/paymentHsmClusters/privateEndpointConnections"),
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rgpaymenthsm/providers/Microsoft.HardwareSecurityModules/paymentHsmClusters/phsm1/privateEndpointConnections/sample-pec2"),
+		// 				Properties: &armhardwaresecuritymodules.PrivateEndpointConnectionProperties{
+		// 					GroupIDs: []*string{
+		// 						to.Ptr("application"),
+		// 					},
+		// 					PrivateEndpoint: &armhardwaresecuritymodules.PrivateEndpoint{
+		// 						ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rgpaymenthsm/providers/Microsoft.Network/privateEndpoints/sample-pec2"),
+		// 					},
+		// 					PrivateLinkServiceConnectionState: &armhardwaresecuritymodules.PrivateLinkServiceConnectionState{
+		// 						Description: to.Ptr("This was automatically approved by user1234@contoso.com"),
+		// 						ActionsRequired: to.Ptr("None"),
+		// 						Status: to.Ptr(armhardwaresecuritymodules.PrivateEndpointServiceConnectionStatusApproved),
+		// 					},
+		// 					ProvisioningState: to.Ptr(armhardwaresecuritymodules.PrivateEndpointConnectionProvisioningStateSucceeded),
+		// 				},
+		// 				SystemData: &armhardwaresecuritymodules.SystemData{
+		// 					CreatedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T12:00:00.0000000Z"); return t}()),
+		// 					CreatedBy: to.Ptr("User1"),
+		// 					CreatedByType: to.Ptr(armhardwaresecuritymodules.CreatedByTypeUser),
+		// 					LastModifiedAt: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2020-01-01T12:00:00.0000000Z"); return t}()),
+		// 					LastModifiedBy: to.Ptr("User2"),
+		// 					LastModifiedByType: to.Ptr(armhardwaresecuritymodules.CreatedByTypeUser),
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// }
+	}
+}

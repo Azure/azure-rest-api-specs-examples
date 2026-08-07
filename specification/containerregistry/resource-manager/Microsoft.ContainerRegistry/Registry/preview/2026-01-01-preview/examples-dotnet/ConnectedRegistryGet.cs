8e78f3e77d7e36a140228baaf7e5f215d2921d1b
azure-sdk <azuresdk@microsoft.com>
2026-08-07 11:47:08 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ContainerRegistry_1.5.0-beta.3

--- specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/preview/2026-01-01-preview/examples-dotnet/ConnectedRegistryGet.cs
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/preview/2026-01-01-preview/examples-dotnet/ConnectedRegistryGet.cs
@@ -16,20 +16,31 @@ TokenCredential cred = new DefaultAzureCredential();
 // authenticate your client
 ArmClient client = new ArmClient(cred);
 
-// this example assumes you already have this ConnectedRegistryResource created on azure
-// for more information of creating ConnectedRegistryResource, please refer to the document of ConnectedRegistryResource
+// this example assumes you already have this ContainerRegistryResource created on azure
+// for more information of creating ContainerRegistryResource, please refer to the document of ContainerRegistryResource
 string subscriptionId = "00000000-0000-0000-0000-000000000000";
 string resourceGroupName = "myResourceGroup";
 string registryName = "myRegistry";
-string connectedRegistryName = "myConnectedRegistry";
-ResourceIdentifier connectedRegistryResourceId = ConnectedRegistryResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName, connectedRegistryName);
-ConnectedRegistryResource connectedRegistry = client.GetConnectedRegistryResource(connectedRegistryResourceId);
+ResourceIdentifier containerRegistryResourceId = ContainerRegistryResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName);
+ContainerRegistryResource containerRegistry = client.GetContainerRegistryResource(containerRegistryResourceId);
+
+// get the collection of this ConnectedRegistryResource
+ConnectedRegistryCollection collection = containerRegistry.GetConnectedRegistries();
 
 // invoke the operation
-ConnectedRegistryResource result = await connectedRegistry.GetAsync();
+string connectedRegistryName = "myConnectedRegistry";
+NullableResponse<ConnectedRegistryResource> response = await collection.GetIfExistsAsync(connectedRegistryName);
+ConnectedRegistryResource result = response.HasValue ? response.Value : null;
 
-// the variable result is a resource, you could call other operations on this instance as well
-// but just for demo, we get its data from this resource instance
-ConnectedRegistryData resourceData = result.Data;
-// for demo we just print out the id
-Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+if (result == null)
+{
+    Console.WriteLine("Succeeded with null as result");
+}
+else
+{
+    // the variable result is a resource, you could call other operations on this instance as well
+    // but just for demo, we get its data from this resource instance
+    ConnectedRegistryData resourceData = result.Data;
+    // for demo we just print out the id
+    Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+}

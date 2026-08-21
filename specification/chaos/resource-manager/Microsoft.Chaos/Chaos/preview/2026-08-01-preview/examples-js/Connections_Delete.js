3ebe0b5e4de9e18696b517036659d0cf03b0898b
azure-sdk <azuresdk@microsoft.com>
2026-08-21 11:15:50 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-chaos_2.1.0-beta.2

---
+++ specification/chaos/resource-manager/Microsoft.Chaos/Chaos/preview/2026-08-01-preview/examples-js/Connections_Delete.js
@@ -0,0 +1,15 @@
+const { ChaosManagementClient } = require("@azure/arm-chaos");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a connection.
+ *
+ * @summary delete a connection.
+ * x-ms-original-file: 2026-08-01-preview/Connections_Delete.json
+ */
+async function deleteAConnectionInAWorkspace() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "6b052e15-03d3-4f17-b2e1-be7f07588291";
+  const client = new ChaosManagementClient(credential, subscriptionId);
+  await client.connections.delete("exampleRG", "exampleWorkspace", "aksClusterConnection");
+}

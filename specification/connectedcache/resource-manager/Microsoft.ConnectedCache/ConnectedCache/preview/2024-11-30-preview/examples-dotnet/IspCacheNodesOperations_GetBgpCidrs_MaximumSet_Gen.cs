dbf1ef80a36829eb61dd034d928c7e2e19f3181b
azure-sdk <azuresdk@microsoft.com>
2026-08-25 11:52:31 +0000

    [Automation] Collect examples from azure-sdk-for-net#Azure.ResourceManager.ConnectedCache_1.0.0

---
+++ specification/connectedcache/resource-manager/Microsoft.ConnectedCache/ConnectedCache/preview/2024-11-30-preview/examples-dotnet/IspCacheNodesOperations_GetBgpCidrs_MaximumSet_Gen.cs
@@ -0,0 +1,30 @@
+using Azure;
+using Azure.ResourceManager;
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.ConnectedCache.Models;
+using Azure.ResourceManager.ConnectedCache;
+
+// Generated from example definition: 2024-11-30-preview/IspCacheNodesOperations_GetBgpCidrs_MaximumSet_Gen.json
+// this example is just showing the usage of "IspCacheNodesOperations_GetBgpCidrs" operation, for the dependent resources, they will have to be created separately.
+
+// get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+TokenCredential cred = new DefaultAzureCredential();
+// authenticate your client
+ArmClient client = new ArmClient(cred);
+
+// this example assumes you already have this IspCacheNodeResource created on azure
+// for more information of creating IspCacheNodeResource, please refer to the document of IspCacheNodeResource
+string subscriptionId = "12345678-1234-1234-1234-123456789098";
+string resourceGroupName = "rgConnectedCache";
+string customerResourceName = "MccRPTest1";
+string cacheNodeResourceName = "MCCCachenode1";
+ResourceIdentifier ispCacheNodeResourceId = IspCacheNodeResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, customerResourceName, cacheNodeResourceName);
+IspCacheNodeResource ispCacheNode = client.GetIspCacheNodeResource(ispCacheNodeResourceId);
+
+// invoke the operation
+MccCacheNodeBgpCidrDetails result = await ispCacheNode.GetBgpCidrsAsync();
+
+Console.WriteLine($"Succeeded: {result}");

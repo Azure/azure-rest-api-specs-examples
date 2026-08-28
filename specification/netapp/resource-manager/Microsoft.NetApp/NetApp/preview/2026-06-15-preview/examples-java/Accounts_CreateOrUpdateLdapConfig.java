0d20fb6e712231a556ca1d5360460852091881f4
azure-sdk <azuresdk@microsoft.com>
2026-08-28 11:41:42 +0000

    [Automation] Collect examples from azure-sdk-for-java#com.azure.resourcemanager+azure-resourcemanager-netapp_2.5.0-beta.2

---
+++ specification/netapp/resource-manager/Microsoft.NetApp/NetApp/preview/2026-06-15-preview/examples-java/Accounts_CreateOrUpdateLdapConfig.java
@@ -0,0 +1,35 @@
+
+import com.azure.resourcemanager.netapp.models.BindAuthenticationLevel;
+import com.azure.resourcemanager.netapp.models.BindPasswordAkvConfig;
+import com.azure.resourcemanager.netapp.models.LdapConfiguration;
+import com.azure.resourcemanager.netapp.models.SecureLdapType;
+import java.util.Arrays;
+
+/**
+ * Samples for Accounts CreateOrUpdate.
+ */
+public final class Main {
+    /*
+     * x-ms-original-file: 2026-06-15-preview/Accounts_CreateOrUpdateLdapConfig.json
+     */
+    /**
+     * Sample code: Accounts_CreateOrUpdateLdapConfig.
+     * 
+     * @param manager Entry point to NetAppFilesManager.
+     */
+    public static void accountsCreateOrUpdateLdapConfig(com.azure.resourcemanager.netapp.NetAppFilesManager manager) {
+        manager.accounts().define("account1").withRegion("eastus").withExistingResourceGroup("myRG")
+            .withLdapConfiguration(new LdapConfiguration().withDomain("example.com")
+                .withLdapServers(Arrays.asList("192.0.2.1", "192.0.2.2"))
+                .withSecureLdapType(SecureLdapType.LDAP_OVER_TLS).withServerCACertificate("<REDACTED>")
+                .withCertificateCNHost("ldap.krypton.com").withLdapPort(636)
+                .withUserDN("OU=subdirectory,OU=directory,DC=example,DC=com")
+                .withGroupDN("OU=subdirectory,OU=directory,DC=example,DC=com")
+                .withNetGroupDN("OU=subdirectory,OU=directory,DC=example,DC=com")
+                .withBindAuthenticationLevel(BindAuthenticationLevel.SIMPLE).withBindDN("cn=user,dc=domain,dc=com")
+                .withBindPasswordAkvConfig(new BindPasswordAkvConfig().withAzureKeyVaultUri("fakeTokenPlaceholder")
+                    .withSecretName("fakeTokenPlaceholder").withUserAssignedIdentity(
+                        "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myRG/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myIdentity")))
+            .create();
+    }
+}

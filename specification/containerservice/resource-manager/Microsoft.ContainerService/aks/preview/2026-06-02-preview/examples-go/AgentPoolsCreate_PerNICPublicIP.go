060ece49d47b5f1d8ab0a197eccd21ac2ae165e5
azure-sdk <azuresdk@microsoft.com>
2026-09-02 11:10:29 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerservice/armcontainerservice/v9.6.0-beta.1

---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-06-02-preview/examples-go/AgentPoolsCreate_PerNICPublicIP.go
@@ -0,0 +1,111 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-06-02-preview/AgentPoolsCreate_PerNICPublicIP.json
+func ExampleAgentPoolsClient_BeginCreateOrUpdate_createAgentPoolWithPerNicPublicIPConfiguration() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	poller, err := clientFactory.NewAgentPoolsClient().BeginCreateOrUpdate(ctx, "rg1", "clustername1", "agentpool1", armcontainerservice.AgentPool{
+		Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+			Count:               to.Ptr[int32](3),
+			OrchestratorVersion: to.Ptr(""),
+			OSType:              to.Ptr(armcontainerservice.OSTypeLinux),
+			VMSize:              to.Ptr("Standard_D8s_v3"),
+			NetworkProfile: &armcontainerservice.AgentPoolNetworkProfile{
+				SecondaryNetworkInterfaces: []*armcontainerservice.AgentPoolNetworkInterface{
+					{
+						Type:                        to.Ptr(armcontainerservice.AgentPoolNetworkInterfaceTypeStandard),
+						VnetSubnetID:                to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/myVNet/subnets/secondary-subnet-1"),
+						EnableAcceleratedNetworking: to.Ptr(true),
+						PublicIPAddressConfiguration: &armcontainerservice.AgentPoolNICPublicIPAddressConfiguration{
+							PublicIPAddressVersion: to.Ptr(armcontainerservice.AgentPoolNICPublicIPAddressVersionIPv4),
+							IPTags: []*armcontainerservice.IPTag{
+								{
+									IPTagType: to.Ptr("FirstPartyUsage"),
+									Tag:       to.Ptr("teams"),
+								},
+							},
+						},
+					},
+					{
+						Type:                        to.Ptr(armcontainerservice.AgentPoolNetworkInterfaceTypeStandard),
+						VnetSubnetID:                to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/myVNet/subnets/secondary-subnet-2"),
+						EnableAcceleratedNetworking: to.Ptr(true),
+						PublicIPAddressConfiguration: &armcontainerservice.AgentPoolNICPublicIPAddressConfiguration{
+							PublicIPAddressVersion: to.Ptr(armcontainerservice.AgentPoolNICPublicIPAddressVersionIPv4),
+							PublicIPPrefixID:       to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Network/publicIPPrefixes/myPrefix"),
+						},
+					},
+				},
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to poll the result: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armcontainerservice.AgentPoolsClientCreateOrUpdateResponse{
+	// 	AgentPool: armcontainerservice.AgentPool{
+	// 		Name: to.Ptr("agentpool1"),
+	// 		Type: to.Ptr("Microsoft.ContainerService/managedClusters/agentPools"),
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/agentPools/agentpool1"),
+	// 		Properties: &armcontainerservice.ManagedClusterAgentPoolProfileProperties{
+	// 			Count: to.Ptr[int32](3),
+	// 			CurrentOrchestratorVersion: to.Ptr("1.9.6"),
+	// 			MaxPods: to.Ptr[int32](110),
+	// 			OrchestratorVersion: to.Ptr("1.9.6"),
+	// 			OSType: to.Ptr(armcontainerservice.OSTypeLinux),
+	// 			ProvisioningState: to.Ptr("Succeeded"),
+	// 			VMSize: to.Ptr("Standard_D8s_v3"),
+	// 			NetworkProfile: &armcontainerservice.AgentPoolNetworkProfile{
+	// 				SecondaryNetworkInterfaces: []*armcontainerservice.AgentPoolNetworkInterface{
+	// 					{
+	// 						Type: to.Ptr(armcontainerservice.AgentPoolNetworkInterfaceTypeStandard),
+	// 						VnetSubnetID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/myVNet/subnets/secondary-subnet-1"),
+	// 						EnableAcceleratedNetworking: to.Ptr(true),
+	// 						PublicIPAddressConfiguration: &armcontainerservice.AgentPoolNICPublicIPAddressConfiguration{
+	// 							PublicIPAddressVersion: to.Ptr(armcontainerservice.AgentPoolNICPublicIPAddressVersionIPv4),
+	// 							IPTags: []*armcontainerservice.IPTag{
+	// 								{
+	// 									IPTagType: to.Ptr("FirstPartyUsage"),
+	// 									Tag: to.Ptr("teams"),
+	// 								},
+	// 							},
+	// 						},
+	// 					},
+	// 					{
+	// 						Type: to.Ptr(armcontainerservice.AgentPoolNetworkInterfaceTypeStandard),
+	// 						VnetSubnetID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/myVNet/subnets/secondary-subnet-2"),
+	// 						EnableAcceleratedNetworking: to.Ptr(true),
+	// 						PublicIPAddressConfiguration: &armcontainerservice.AgentPoolNICPublicIPAddressConfiguration{
+	// 							PublicIPAddressVersion: to.Ptr(armcontainerservice.AgentPoolNICPublicIPAddressVersionIPv4),
+	// 							PublicIPPrefixID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Network/publicIPPrefixes/myPrefix"),
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+}
---
+++ specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2026-06-02-preview/examples-go/MaintenanceConfigurationsCreate_Update_MaintenanceWindow.go
@@ -0,0 +1,96 @@
+package armcontainerservice_test
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
+)
+
+// Generated from example definition: 2026-06-02-preview/MaintenanceConfigurationsCreate_Update_MaintenanceWindow.json
+func ExampleMaintenanceConfigurationsClient_CreateOrUpdate_createUpdateMaintenanceConfigurationWithMaintenanceWindow() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerservice.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewMaintenanceConfigurationsClient().CreateOrUpdate(ctx, "rg1", "clustername1", "aksManagedAutoUpgradeSchedule", armcontainerservice.MaintenanceConfiguration{
+		Properties: &armcontainerservice.MaintenanceConfigurationProperties{
+			MaintenanceWindow: &armcontainerservice.MaintenanceWindow{
+				DurationHours: to.Ptr[int32](10),
+				NotAllowedDates: []*armcontainerservice.DateSpan{
+					{
+						End:   to.Ptr(time.Date(2023, time.February, 25, 0, 0, 0, 0, time.UTC)),
+						Start: to.Ptr(time.Date(2023, time.February, 18, 0, 0, 0, 0, time.UTC)),
+					},
+					{
+						End:   to.Ptr(time.Date(2024, time.January, 5, 0, 0, 0, 0, time.UTC)),
+						Start: to.Ptr(time.Date(2023, time.December, 23, 0, 0, 0, 0, time.UTC)),
+					},
+				},
+				Schedule: &armcontainerservice.Schedule{
+					RelativeMonthly: &armcontainerservice.RelativeMonthlySchedule{
+						DayOfWeek:      to.Ptr(armcontainerservice.WeekDayMonday),
+						IntervalMonths: to.Ptr[int32](3),
+						WeekIndex:      to.Ptr(armcontainerservice.TypeFirst),
+					},
+				},
+				StartDate: to.Ptr(time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC)),
+				StartTime: to.Ptr("08:30"),
+				UTCOffset: to.Ptr("+05:30"),
+			},
+		},
+	}, nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armcontainerservice.MaintenanceConfigurationsClientCreateOrUpdateResponse{
+	// 	MaintenanceConfiguration: armcontainerservice.MaintenanceConfiguration{
+	// 		Name: to.Ptr("aksManagedAutoUpgradeSchedule"),
+	// 		Type: to.Ptr("Microsoft.ContainerService/managedClusters/maintenanceConfigurations"),
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.ContainerService/managedClusters/clustername1/maintenanceConfigurations/default"),
+	// 		Properties: &armcontainerservice.MaintenanceConfigurationProperties{
+	// 			MaintenanceWindow: &armcontainerservice.MaintenanceWindow{
+	// 				DurationHours: to.Ptr[int32](10),
+	// 				NotAllowedDates: []*armcontainerservice.DateSpan{
+	// 					{
+	// 						End: to.Ptr(time.Date(2023, time.February, 25, 0, 0, 0, 0, time.UTC)),
+	// 						Start: to.Ptr(time.Date(2023, time.February, 18, 0, 0, 0, 0, time.UTC)),
+	// 					},
+	// 					{
+	// 						End: to.Ptr(time.Date(2024, time.January, 5, 0, 0, 0, 0, time.UTC)),
+	// 						Start: to.Ptr(time.Date(2023, time.December, 23, 0, 0, 0, 0, time.UTC)),
+	// 					},
+	// 				},
+	// 				Schedule: &armcontainerservice.Schedule{
+	// 					Weekly: &armcontainerservice.WeeklySchedule{
+	// 						DayOfWeek: to.Ptr(armcontainerservice.WeekDayMonday),
+	// 						IntervalWeeks: to.Ptr[int32](3),
+	// 					},
+	// 				},
+	// 				StartDate: to.Ptr(time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC)),
+	// 				StartTime: to.Ptr("08:30"),
+	// 				UTCOffset: to.Ptr("+05:30"),
+	// 			},
+	// 		},
+	// 		SystemData: &armcontainerservice.SystemData{
+	// 			CreatedAt: to.Ptr(time.Date(2020, time.January, 1, 17, 18, 19, 123456700, time.UTC)),
+	// 			CreatedBy: to.Ptr("user1"),
+	// 			CreatedByType: to.Ptr(armcontainerservice.CreatedByTypeUser),
+	// 			LastModifiedAt: to.Ptr(time.Date(2020, time.January, 2, 17, 18, 19, 123456700, time.UTC)),
+	// 			LastModifiedBy: to.Ptr("user2"),
+	// 			LastModifiedByType: to.Ptr(armcontainerservice.CreatedByTypeUser),
+	// 		},
+	// 	},
+	// }
+}

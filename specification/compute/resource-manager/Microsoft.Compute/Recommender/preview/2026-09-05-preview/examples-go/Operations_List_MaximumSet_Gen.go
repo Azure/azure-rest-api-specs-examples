fe21693f20e29ab4762f1329b9954e49d99de78f
azure-sdk <azuresdk@microsoft.com>
2026-09-02 11:12:34 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/compute/armrecommender/v0.3.0

---
+++ specification/compute/resource-manager/Microsoft.Compute/Recommender/preview/2026-09-05-preview/examples-go/Operations_List_MaximumSet_Gen.go
@@ -0,0 +1,65 @@
+package armrecommender_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armrecommender"
+)
+
+// Generated from example definition: 2026-09-05-preview/Operations_List_MaximumSet_Gen.json
+func ExampleOperationsClient_NewListPager_operationsListMaximumSetGen() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armrecommender.NewClientFactory("<subscriptionID>", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+		for _, v := range page.Value {
+			// You could use page here. We use blank identifier for just demo purposes.
+			_ = v
+		}
+		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+		// page = armrecommender.OperationsClientListResponse{
+		// 	OperationListResult: armrecommender.OperationListResult{
+		// 		Value: []*armrecommender.Operation{
+		// 			{
+		// 				Name: to.Ptr("Microsoft.Compute/locations/skuMixPlacementScores/read"),
+		// 				IsDataAction: to.Ptr(false),
+		// 				Display: &armrecommender.OperationDisplay{
+		// 					Provider: to.Ptr("Microsoft Compute"),
+		// 					Resource: to.Ptr("Mix Placement Scores"),
+		// 					Operation: to.Ptr("Get Mix Placement Scores"),
+		// 					Description: to.Ptr("Gets the metadata of Mix Placement Scores."),
+		// 				},
+		// 				Origin: to.Ptr(armrecommender.OriginUserSystem),
+		// 				ActionType: to.Ptr(armrecommender.ActionTypeInternal),
+		// 			},
+		// 			{
+		// 				Name: to.Ptr("Microsoft.Compute/locations/skuMixPlacementScores/generate/action"),
+		// 				IsDataAction: to.Ptr(false),
+		// 				Display: &armrecommender.OperationDisplay{
+		// 					Provider: to.Ptr("Microsoft Compute"),
+		// 					Resource: to.Ptr("Mix Placement Scores"),
+		// 					Operation: to.Ptr("Generate Mix Placement Scores"),
+		// 					Description: to.Ptr("Generates placement scores for VM SKU mix placement."),
+		// 				},
+		// 				Origin: to.Ptr(armrecommender.OriginUserSystem),
+		// 				ActionType: to.Ptr(armrecommender.ActionTypeInternal),
+		// 			},
+		// 		},
+		// 		NextLink: to.Ptr("https://microsoft.com/a"),
+		// 	},
+		// }
+	}
+}

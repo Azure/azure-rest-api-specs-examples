8a82c5976585a849f75485360b3c16cde7855ee1
azure-sdk <azuresdk@microsoft.com>
2026-09-10 11:14:05 +0000

    [Automation] Collect examples from azure-sdk-for-go#sdk/resourcemanager/containerregistry/armcontainerregistry/v3.1.0-beta.3

---
+++ specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/preview/2026-09-01-preview/examples-go/ConnectedRegistryResync.go
@@ -0,0 +1,81 @@
+package armcontainerregistry_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerregistry/armcontainerregistry/v3"
+)
+
+// Generated from example definition: 2026-09-01-preview/ConnectedRegistryResync.json
+func ExampleConnectedRegistriesClient_Resync() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	ctx := context.Background()
+	clientFactory, err := armcontainerregistry.NewClientFactory("00000000-0000-0000-0000-000000000000", cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	res, err := clientFactory.NewConnectedRegistriesClient().Resync(ctx, "myResourceGroup", "myRegistry", "myConnectedRegistry", nil)
+	if err != nil {
+		log.Fatalf("failed to finish the request: %v", err)
+	}
+	// You could use response here. We use blank identifier for just demo purposes.
+	_ = res
+	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
+	// res = armcontainerregistry.ConnectedRegistriesClientResyncResponse{
+	// 	ConnectedRegistry: armcontainerregistry.ConnectedRegistry{
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry/connectedRegistries/myConnectedRegistry"),
+	// 		Name: to.Ptr("myConnectedRegistry"),
+	// 		Type: to.Ptr("Microsoft.ContainerRegistry/registries/connectedRegistries"),
+	// 		Properties: &armcontainerregistry.ConnectedRegistryProperties{
+	// 			Mode: to.Ptr(armcontainerregistry.ConnectedRegistryModeReadWrite),
+	// 			Parent: &armcontainerregistry.ParentProperties{
+	// 				SyncProperties: &armcontainerregistry.SyncProperties{
+	// 					TokenID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry/tokens/syncToken"),
+	// 					Schedule: to.Ptr("0 0 */10 * *"),
+	// 					MessageTTL: to.Ptr("P30D"),
+	// 					SyncWindow: to.Ptr("P2D"),
+	// 				},
+	// 			},
+	// 			ClientTokenIDs: []*string{
+	// 				to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry/tokens/client1Token"),
+	// 				to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry/tokens/client2Token"),
+	// 			},
+	// 			Logging: &armcontainerregistry.LoggingProperties{
+	// 				LogLevel: to.Ptr(armcontainerregistry.LogLevelDebug),
+	// 				AuditLogStatus: to.Ptr(armcontainerregistry.AuditLogStatusEnabled),
+	// 			},
+	// 			StatusDetails: []*armcontainerregistry.StatusDetailProperties{
+	// 				{
+	// 					Type: to.Ptr("Disk"),
+	// 					Code: to.Ptr("DiskCapacityMetrics"),
+	// 					Description: to.Ptr("Disk space available on the connected registry."),
+	// 					Timestamp: to.Ptr(time.Date(2026, time.September, 1, 0, 0, 0, 0, time.UTC)),
+	// 					CorrelationID: to.Ptr("00000000-0000-0000-0000-000000000000"),
+	// 					TotalGib: to.Ptr[float64](1024),
+	// 					AvailableGib: to.Ptr[float64](512.5),
+	// 				},
+	// 			},
+	// 			NotificationsList: []*string{
+	// 				to.Ptr("hello-world:*:*"),
+	// 				to.Ptr("sample/repo/*:1.0:*"),
+	// 			},
+	// 			GarbageCollection: &armcontainerregistry.GarbageCollectionProperties{
+	// 				Enabled: to.Ptr(true),
+	// 				Schedule: to.Ptr("0 5 * * *"),
+	// 			},
+	// 			RegistrySyncResult: &armcontainerregistry.RegistrySyncResult{
+	// 				SyncTrigger: to.Ptr(armcontainerregistry.SyncTriggerInitialSync),
+	// 				SyncState: to.Ptr(armcontainerregistry.SyncStateSucceeded),
+	// 				LastSyncStartTime: to.Ptr(time.Date(2026, time.January, 1, 1, 0, 0, 0, time.UTC)),
+	// 				LastSyncEndTime: to.Ptr(time.Date(2026, time.January, 1, 1, 1, 0, 0, time.UTC)),
+	// 				LastSuccessfulSyncEndTime: to.Ptr(time.Date(2026, time.January, 1, 1, 1, 0, 0, time.UTC)),
+	// 			},
+	// 		},
+	// 	},
+	// }
+}

08641b1a109308d23e334cd25a5243aafe80d026
azure-sdk <azuresdk@microsoft.com>
2026-09-17 11:21:36 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-sql_12.0.0-beta.1

---
+++ specification/sql/resource-manager/Microsoft.Sql/SQL/preview/2025-08-01-preview/examples-js/ResourceGroupBasedLongTermRetentionBackupListByLocationWithPagination.js
@@ -0,0 +1,23 @@
+const { SqlManagementClient } = require("@azure/arm-sql");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to lists the long term retention backups for a given location based on resource group.
+ *
+ * @summary lists the long term retention backups for a given location based on resource group.
+ * x-ms-original-file: 2025-08-01-preview/ResourceGroupBasedLongTermRetentionBackupListByLocationWithPagination.json
+ */
+async function getLongTermRetentionBackupsUnderTheLocationBasedOnResourceGroupWithPagination() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-1111-2222-3333-444444444444";
+  const client = new SqlManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.longTermRetentionBackups.listByResourceGroupLocation(
+    "testResourceGroup",
+    "japaneast",
+  )) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
---
+++ specification/sql/resource-manager/Microsoft.Sql/SQL/preview/2025-08-01-preview/examples-js/SyncMemberCreateWithIdentity.js
@@ -0,0 +1,38 @@
+const { SqlManagementClient } = require("@azure/arm-sql");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to creates or updates a sync member.
+ *
+ * @summary creates or updates a sync member.
+ * x-ms-original-file: 2025-08-01-preview/SyncMemberCreateWithIdentity.json
+ */
+async function createANewSyncMemberWithUserAssignedIdentity() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-1111-2222-3333-444444444444";
+  const client = new SqlManagementClient(credential, subscriptionId);
+  const result = await client.syncMembers.createOrUpdate(
+    "syncgroupcrud-65440",
+    "syncgroupcrud-8475",
+    "syncgroupcrud-4328",
+    "syncgroupcrud-3187",
+    "syncmembercrud-4879",
+    {
+      identity: {
+        type: "UserAssigned",
+        userAssignedIdentities: {
+          "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/group1/providers/Microsoft.ManagedIdentity/userAssignedIdentities/test-umi":
+            {},
+        },
+      },
+      databaseType: "AzureSqlDatabase",
+      serverName: "syncgroupcrud-3379.database.windows.net",
+      databaseName: "syncgroupcrud-7421",
+      syncDirection: "Bidirectional",
+      usePrivateLinkConnection: true,
+      syncMemberAzureDatabaseResourceId:
+        "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/syncgroupcrud-65440/providers/Microsoft.Sql/servers/syncgroupcrud-8475/databases/syncgroupcrud-4328",
+    },
+  );
+  console.log(result);
+}

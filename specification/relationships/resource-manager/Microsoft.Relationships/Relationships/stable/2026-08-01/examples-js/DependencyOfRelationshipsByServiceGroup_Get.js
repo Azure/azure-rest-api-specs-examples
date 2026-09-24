2d8925bb0bad64d37fadf69df56a88cb68288a6f
azure-sdk <azuresdk@microsoft.com>
2026-09-24 11:18:26 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-relationships_1.0.0

---
+++ specification/relationships/resource-manager/Microsoft.Relationships/Relationships/stable/2026-08-01/examples-js/DependencyOfRelationshipsByServiceGroup_Get.js
@@ -0,0 +1,18 @@
+const { RelationshipsClient } = require("@azure/arm-relationships");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a DependencyOfRelationship
+ *
+ * @summary get a DependencyOfRelationship
+ * x-ms-original-file: 2026-08-01/DependencyOfRelationshipsByServiceGroup_Get.json
+ */
+async function dependencyOfRelationshipsByServiceGroupGet() {
+  const credential = new DefaultAzureCredential();
+  const client = new RelationshipsClient(credential);
+  const result = await client.dependencyOfRelationshipsByServiceGroup.get(
+    "myServiceGroup",
+    "relationshipOne",
+  );
+  console.log(result);
+}

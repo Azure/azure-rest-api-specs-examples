b47a32e4f7090f0cfee1f2590a1f94c3d8bb9546
azure-sdk <azuresdk@microsoft.com>
2026-09-25 11:10:23 +0000

    [Automation] Collect examples from azure-sdk-for-js#@azure/arm-oracledatabase_4.0.0

---
+++ specification/oracle/resource-manager/Oracle.Database/OracleDatabase/stable/2026-06-01/examples-js/DatabaseSystemShapeResources_Get_MaximumSet_Gen.js
@@ -0,0 +1,16 @@
+const { OracleDatabaseManagementClient } = require("@azure/arm-oracledatabase");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a DatabaseSystemShape
+ *
+ * @summary get a DatabaseSystemShape
+ * x-ms-original-file: 2026-06-01/DatabaseSystemShapeResources_Get_MaximumSet_Gen.json
+ */
+async function databaseSystemShapeResourcesGetMaximumSetGenGeneratedByMaximumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  const result = await client.databaseSystemShapeResources.get("eastus", "resource1");
+  console.log(result);
+}
---
+++ specification/oracle/resource-manager/Oracle.Database/OracleDatabase/stable/2026-06-01/examples-js/NetworkAnchors_Delete_MaximumSet_Gen.js
@@ -0,0 +1,15 @@
+const { OracleDatabaseManagementClient } = require("@azure/arm-oracledatabase");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a NetworkAnchor
+ *
+ * @summary delete a NetworkAnchor
+ * x-ms-original-file: 2026-06-01/NetworkAnchors_Delete_MaximumSet_Gen.json
+ */
+async function networkAnchorsDeleteMaximumSetGenGeneratedByMaximumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  await client.networkAnchors.delete("rgopenapi", "resource1");
+}
